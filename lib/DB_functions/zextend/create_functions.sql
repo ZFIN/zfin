3d09532aa77552738ced394c2482af86d5976752
Dave Clements <clements@zfin.org>
2000-06-20 22:37:34 +0000

    Changed path of shared object where functions are defined.  Shared ojbect file now resides on a disk that is mounted on the production server.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@60 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/zextend/create_functions.sql
+++ lib/DB_functions/zextend/create_functions.sql
@@ -3,13 +3,13 @@
 -- must reload all in a particular source file.
 -- FOLLOWING LINE JUST FOR TESTING---LEAVE IN AS COMMENT FOR NOW Mar 8, 2000 AEK
 connect to 'ztest' user 'informix' using 'KilAsq.';
-drop function lower;
-drop function upper;
+-- drop function lower;
+-- drop function upper;
 drop function sysexec;
 drop function replace;
 drop function html_breaks (lvarchar);
 drop function html_breaks (html);
-drop function html_to_lvarchar(html);
+-- drop function html_to_lvarchar(html);
 drop function conc;
 drop function get_id;
 drop function get_id_test;
@@ -24,21 +24,21 @@ drop function webhtml_like(html,lvarchar);
 -- create function lower(str lvarchar)
 -- returning lvarchar
 -- external name
--- "/research/zfin/develop/informix/zextend/zfin/zextend.o(lower)"
+-- "/research/zfin/chromix/private/lib/zextend.o(lower)"
 -- language c
 -- end function;
 
 -- create function upper(str lvarchar)
 -- returning lvarchar
 -- external name
--- "/research/zfin/develop/informix/zextend/zfin/zextend.so(upper)"
+-- "/research/zfin/chromix/private/lib/zextend.so(upper)"
 -- language c
 -- end function;
 
 create function sysexec(key lvarchar, args lvarchar)
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
@@ -46,21 +46,21 @@ create function replace(old lvarchar, new lvarchar, src lvarchar, n
 integer)
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function html_breaks(lvarchar)
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function html_breaks(html)
 returning html
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so(html_breaks_html)"
+"/research/zfin/chromix/private/lib/zextend.so(html_breaks_html)"
 language c
 end function;
 
@@ -68,69 +68,69 @@ create function conc(lvarchar, lvarchar)
 returning lvarchar
 with (handlesnulls)
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function get_id(lvarchar)
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function get_id_test(name lvarchar, n integer)
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function expr(date)
 returning date
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function expr(datetime year to fraction)
 returning datetime year to fraction
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function get_random_cookie()
 returning lvarchar
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function now()
 returning datetime year to fraction
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function todays_date()
 returning date
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function position(lvarchar, lvarchar)
 returning integer
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so"
+"/research/zfin/chromix/private/lib/zextend.so"
 language c
 end function;
 
 create function webhtml_like(html, lvarchar)
 returning boolean
 external name
-"/research/zfin/develop/informix/zextend/zfin/zextend.so(webhtml_like)"
+"/research/zfin/chromix/private/lib/zextend.so(webhtml_like)"
 language c
 end function;
