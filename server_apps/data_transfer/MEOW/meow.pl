7cbcd34cd8a90745847c617654d5a9ddf1c797c1
Dave Clements <clements@zfin.org>
2001-02-06 19:06:21 +0000

    Script to call from cron to dump data for MEOW DB.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@280 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/MEOW/meow.pl
@@ -0,0 +1,12 @@
+#!/local/bin/perl 
+#  Script to create files for MEOW database
+#  output files are written to /research/zfin/chromix/www/home/transfer/MEOW
+$ENV{"DBDATE"}="Y4MD-";
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+chdir "<!--|ROOT_PATH|-->/server_apps/data_transfer/MEOW";
+system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> MEOW_dump.sql");
+
+
