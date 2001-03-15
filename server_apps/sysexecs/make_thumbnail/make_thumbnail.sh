caba0e95f2851673c82261757dd050139146d6a9
Dave Clements <clements@zfin.org>
2001-03-15 20:52:28 +0000

    Cleaned up script.  Not sure when it last worked.  Script was assuming that the filename passed to it did not have a .gif on the end, when it fact it did.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@384 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
+++ server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
@@ -1,5 +1,4 @@
 #!/local/bin/perl
 @args = @ARGV; @ARGV = ();
 system("/private/apps/alchemy/solaris/alchemy", @args);
-system("chmod 644 $args[1].gif");
-system("cp $args[1].gif $args[1]");
+system("chmod 644 $args[1]");
