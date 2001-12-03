b427b3aab83e650805549d87ca0b3a98c97e2e18
Judy Sprague <judys@zfin.org>
2001-12-03 21:42:27 +0000

    Original revision of pl file as cron job - creating accession and mappign data files for John P.   - for work with Sanger Center sequencing data.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@1181 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/JohnP/johnp.pl
@@ -0,0 +1,12 @@
+#!/local/bin/perl 
+#  Script to create files for John Postlethwait for checking SC sequence data
+#  output files are written to /research/zfin/chromix/www/home/transfer/JohnP
+$ENV{"DBDATE"}="Y4MD-";
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+chdir "<!--|ROOT_PATH|-->/server_apps/data_transfer/JohnP";
+system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> create_files_for_johnp.sql");
+
+
