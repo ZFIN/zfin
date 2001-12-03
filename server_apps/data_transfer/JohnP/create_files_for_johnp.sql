89c0fc6062e11f67e54fb52bada0ef2c5938d1bd
Judy Sprague <judys@zfin.org>
2001-12-03 21:42:49 +0000

    Original revision.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@1182 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/JohnP/create_files_for_johnp.sql
@@ -0,0 +1,6 @@
+UNLOAD to '/research/zfin/chromix/ftp/pub/transfer/JohnP/accession.txt'
+  DELIMITER "	" select distinct zdb_id, abbrev, acc_num  from all_markers, OUTER db_link  where ((mtype = 'EST') or (mtype = 'GENE'))  and linked_recid = zdb_id and db_name = 'Genbank' order by 1;
+
+
+UNLOAD to '/research/zfin/chromix/ftp/pub/transfer/JohnP/mappings.txt' 
+  DELIMITER "	" select distinct  zdb_id, target_abbrev, OR_lg, lg_location, metric from public_paneled_markers  where ((zdb_id like '%EST%') or (zdb_id like '%GENE%')) order by 1;
