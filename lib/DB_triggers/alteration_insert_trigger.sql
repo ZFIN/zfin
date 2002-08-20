9f832152188f4d41652273e839fbc0eb31257aea
Dave Clements <clements@zfin.org>
2002-08-20 22:37:07 +0000

    Populates alt_allele_order column whenever an alteration is inserted.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2208 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/alteration_insert_trigger.sql
@@ -0,0 +1,9 @@
+drop trigger alteration_insert_trigger;
+
+create trigger alteration_insert_trigger 
+  insert on alteration 
+    referencing new as new_alteration
+    for each row (
+        execute function zero_pad(new_alteration.allele) 
+	  into alteration.alt_allele_order
+    );
