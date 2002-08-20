87a054b5beb2640d5f39329334792659c6da8613
Dave Clements <clements@zfin.org>
2002-08-20 22:37:59 +0000

    Populates fish_name_order and fish_allele_order columns whenever a fish is inserted.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2211 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/fish_insert_trigger.sql
@@ -0,0 +1,11 @@
+drop trigger fish_insert_trigger;
+
+create trigger fish_insert_trigger 
+  insert on fish 
+    referencing new as new_fish
+    for each row (
+        execute function zero_pad(new_fish.name) 
+	  into fish.fish_name_order,
+        execute function zero_pad(new_fish.allele) 
+	  into fish.fish_allele_order
+    );
