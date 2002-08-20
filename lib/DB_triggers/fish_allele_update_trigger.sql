b171f54351ce7699aeed36716cc7460cb2aaae3c
Dave Clements <clements@zfin.org>
2002-08-20 22:37:48 +0000

    Populates fish_allele_order column whenever fish.allele is updated.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2210 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/fish_allele_update_trigger.sql
@@ -0,0 +1,9 @@
+drop trigger fish_allele_update_trigger;
+
+create trigger fish_allele_update_trigger 
+  update of allele on fish 
+    referencing new as new_fish
+    for each row (
+        execute function zero_pad(new_fish.allele)
+	  into fish.fish_allele_order
+    );
