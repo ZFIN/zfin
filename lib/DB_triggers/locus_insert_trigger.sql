a6e22d6599c23f7030268397f22c6163b790eda9
Dave Clements <clements@zfin.org>
2002-08-20 22:38:38 +0000

    Populates locus_abbreb_order whenever a locus is inserted.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2214 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/locus_insert_trigger.sql
@@ -0,0 +1,9 @@
+drop trigger locus_insert_trigger;
+
+create trigger locus_insert_trigger 
+  insert on locus 
+    referencing new as new_locus
+    for each row (
+        execute function zero_pad(new_locus.abbrev)
+	  into locus.locus_abbrev_order
+    );
