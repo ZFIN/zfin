7e391d3f04e797a4206153c6011dc249d7fd647c
Kevin Schaper <kschaper@zfin.org>
2002-10-09 21:55:24 +0000

    added JDBC support
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2403 908b024a-c82b-635c-c293-f2b15b23277c

--- client_apps/Map/zmapper/MarkerBuilder.java
+++ client_apps/Map/zmapper/MarkerBuilder.java
@@ -6,7 +6,8 @@ package zmapper;
 */
 
 import java.util.*;
-
+import java.sql.*;
+import com.informix.jdbc.*;
 
 public class MarkerBuilder {
 
@@ -49,9 +50,83 @@ public class MarkerBuilder {
 		}	
 		return V;
 	}
+
+	public Vector getMarkers(String query_string) {
+		return getMarkersJDBC(query_string);
+		//return getMarkersJS(query_string);
+	}
 	
 	
-	public Vector getMarkers(String query_string) 	{
+	public Vector getMarkersJDBC(String query_string) {
+		Vector V = new Vector();
+		Vector results = new Vector();
+		String CC = "sswo";
+		
+		String C = "<!--|ZFIN_COOKIE|-->";
+		C = cook(C);
+		String newUrl = "jdbc:informix-sqli://<!--|DOMAIN_NAME|-->:<!--|INFORMIX_PORT|-->/<!--|DB_NAME|-->:INFORMIXSERVER=<!--|INFORMIX_SERVER|-->;user=zfinner;pa"+CC + "r" + "d="+ C;
+
+    	Connection conn = null;
+		
+		try { Class.forName("com.informix.jdbc.IfxDriver"); } 
+		catch (Exception e) { System.err.println("ERROR: failed to load Informix JDBC driver. - " + e);	}
+
+		try {  conn = DriverManager.getConnection(newUrl);  } 
+		catch (SQLException e) { System.out.println("ERROR: failed to connect! - " + e); } 
+
+
+
+	try {
+		Statement selectMarkers = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+		selectMarkers.setFetchSize(200);
+		selectMarkers.setFetchDirection(ResultSet.FETCH_FORWARD);	
+		ResultSet r = selectMarkers.executeQuery(query_string);
+			
+		String zdb_id, abbrev, mtype, target_abbrev, metric, lg_location, OR_lg, mghframework;
+//		int lg_location, OR_lg;
+//		boolean mghframework;
+		Marker M;
+		
+		while(r.next()) {
+			zdb_id = r.getString(1);
+			abbrev = r.getString(2);
+			mtype = r.getString(3);
+			target_abbrev = r.getString(4);
+			lg_location = r.getString(5);
+			OR_lg = r.getString(6);
+			mghframework = r.getString(7);
+			metric = r.getString(8);
+
+			M = new Marker(zdb_id, abbrev, mtype, target_abbrev, lg_location, OR_lg, mghframework, metric);
+			results.add(M);
+		}
+
+
+	    r.close();
+	    selectMarkers.close();
+	} catch (SQLException e) {
+		System.out.println("ERROR: Fetch statement failed: " + e.getMessage());
+	}
+
+	if (query_string.indexOf("desc;") >= 0) { //they're ordered descending and have to be reversed..
+		Vector VV = new Vector();
+		int i = results.size() - 1; //fencepost...
+		while(i >= 0) 	{
+			VV.addElement(results.elementAt(i));
+			i--;
+			
+			}
+		
+
+		results = VV;
+		
+		}
+	
+	return results;
+  }
+	
+	
+	public Vector getMarkersJS(String query_string) 	{
 		Vector V = SQL.select(8, query_string);
 		Vector results = new Vector();
 		Marker M;
@@ -86,6 +161,34 @@ public class MarkerBuilder {
 		
 		return results;
 	}
- 
+	
+	public String cook(String C) {
+		int f, l;
+		String fS, lS;
+		char fC,lC;
+		f = 0;
+		l = C.length() - 1;
+		
+		
+		int i = C.length()/2;
+
+		char[] arr = C.toCharArray();
+		
+		while (i > 0) {
+			fC = arr[f];
+			lC = arr[l];
+			arr[f] = lC;
+			arr[l] = fC;
+			i--;
+			f++;
+			l--;
+		}
+		C = String.valueOf(arr);
+		return C;
+
+		
+		
+	}
+	
 
 }
