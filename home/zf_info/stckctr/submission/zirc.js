80487ea430ebe8b9a65d367f685387ea856278b8
Timothy Mason <tmason@zebrafish.org>
2002-11-14 19:38:45 +0000

    fixed some javascript problems
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2472 908b024a-c82b-635c-c293-f2b15b23277c

--- home/zf_info/stckctr/submission/zirc.js
+++ home/zf_info/stckctr/submission/zirc.js
@@ -63,14 +63,15 @@ function emptyFormCheck() {
 					var elementValue = document.forms[i].elements[j].value;
   				} else if (document.forms[i].elements[j].type.indexOf("select") != -1) {
    					var elementValue = "";
-   					for(k=0;k<document.forms[i].elements[j].length;k++)
+   					for(k=0;k<document.forms[i].elements[j].options.length;k++)
     					if (document.forms[i].elements[j].options[k].selected)
      					elementValue += k+"|";
-  				} else if ((document.forms[i].elements[j].type == "checkbox")||(document.forms[i].elements[j].type == "radio")) {
-					if (document.forms[i].elements[j].checked != true)
-   						elementValue = "";
-					else
-						elementValue = document.forms[i].elements[j].checked;
+  				} else if ((document.forms[i].elements[j].type == "checkbox")
+							||(document.forms[i].elements[j].type == "radio")) {
+							if (document.forms[i].elements[j].checked != true)
+								elementValue = "";
+							else
+								elementValue = document.forms[i].elements[j].checked;
   					}
 				formValues += elementValue;
 			} //end of for (j=0;j<document.forms[i].elements.length; j++) { 	
@@ -107,80 +108,83 @@ var formCheck = emptyFormCheck();
   var zircCookie = "";
   for (i=0;i<document.forms.length;i++) { 
    for (j=0;j<document.forms[i].elements.length; j++) { 
-    var cookieElement = document.forms[i].elements[j].name; 
+    var elementName = document.forms[i].elements[j].name; 
 	if ((document.forms[i].elements[j].type == "text")
 		|| (document.forms[i].elements[j].type == "password")
-		|| (document.forms[i].elements[j].type == "textarea")
-		|| (document.forms[i].elements[j].type == "radio")) {
-	var elementValue = document.forms[i].elements[j].value;
+		|| (document.forms[i].elements[j].type == "hidden")
+		|| (document.forms[i].elements[j].type == "textarea")) {
+		var elementValue = document.forms[i].elements[j].value;
   } else if (document.forms[i].elements[j].type.indexOf("select") != -1) {
-   var elementValue = "";
-   for(k=0;k<document.forms[i].elements[j].length;k++)
-    if (document.forms[i].elements[j].options[k].selected)
-     elementValue += k+"|";
-  } else if (document.forms[i].elements[j].type == "checkbox") {
-   var elementValue = document.forms[i].elements[j].checked;
-  } else if (document.forms[i].elements[j].type == "radio") {
-      		var elementValue = document.forms[i].elements[j].value
-       		} 
-  var elementPair = (cookieElement + "=" + elementValue);
+   	elementValue = "";
+   		for(k=0;k<document.forms[i].elements[j].options.length;k++) {
+    		if (document.forms[i].elements[j].options[k].selected){
+       			elementValue += k + "|";
+	     		}
+	   	} //end of for(k=0;k<document.forms[i].elements[j].options.length;k++) {
+  } else if ((document.forms[i].elements[j].type == "checkbox")
+         	|| (document.forms[i].elements[j].type == "radio"))  {
+				elementValue = document.forms[i].elements[j].checked;
+  } else if (document.forms[i].elements[j].type == "button") {
+      		var elementValue = "";
+  } 
+var elementPair = (elementName + "=" + elementValue); 
 	// These next lines loop together the long string that makes the cookie (e[0]=v[0]&e[1]=v[1]&...)
 	if (zircCookie == "") {
-		zircCookie = elementPair
+		zircCookie = elementPair;
+		} else {
+		zircCookie = zircCookie + "&" + elementPair;
 		} 
-	else {
-		zircCookie = zircCookie + "&" + elementPair
-		} //end of if (zircCookie == "")
 	} //end of for (j=0;j<document.forms[i].elements.length; j++) { 	
    } //end of for (i=0;i<document.forms.length;i++)
    Set_Cookie(cookieName, zircCookie, expires); //set the cookie
  } //end of if (bakeOK == "yes")
 if (nextPage) {
- window.location.href=nextPage;
- }
+   window.location.href = nextPage;
+ 	}
  } // end of else emptyFormCheck
 }   //end of function saveValues() 
 
 
 function storedValues(cookieName) {
-  var val = getCookie(cookieName);
-	if (val) {
-	var a = val.split(escape('&')); //parsing name/value pairs
-	for (var x=0; x<a.length; x++){
-		a[x] = a[x].split(escape('=')); //breaking pairs into an array
+  var cookieValue = getCookie(cookieName);
+	if (cookieValue) {
+	var valueArray = cookieValue.split(escape('&')); //parsing name/value pairs
+	for (var x=0; x<valueArray.length; x++){
+		valueArray[x] = valueArray[x].split(escape('=')); //breaking pairs into an array
 		}
   for (i=0;i<document.forms.length;i++) {
 	for (j=0;j<document.forms[i].elements.length;j++) { 
-			var elementName = a[j].slice(0,1); //find the name in this pair
-			var elementValue = a[j].slice(1,2); //find value in this pair
+			var elementName = valueArray[j].slice(0,1); //find the name in this pair
+			var elementValue = valueArray[j].slice(1,2); //find value in this pair
 			var elementValue = unescape(elementValue); //this makes it readable
 			if (document.forms[i].elements[j].name == elementName) {
-     			if ((document.forms[i].elements[j].type == "text") || 
+			  if ((document.forms[i].elements[j].type == "text") || 
 				    (document.forms[i].elements[j].type == "password") || 
-					(document.forms[i].elements[j].type == "textarea"))
+					(document.forms[i].elements[j].type == "textarea")) {
       					document.forms[i].elements[j].value = elementValue; 
-				else if (document.forms[i].elements[j].type.indexOf("select") != -1) {
+				} else if (document.forms[i].elements[j].type.indexOf("select") != -1) {
       				document.forms[i].elements[j].selectedIndex = -1;
-      					while (((pos = elementValue.indexOf("|")) != -1) && (elementValue.length > 1)) {
-       						sel = parseInt(elementValue.substring(0,pos));
-       						elementValue = elementValue.substring(pos+1,elementValue.length);
-       						if (sel < document.forms[i].elements[j].length) {
-        						document.forms[i].elements[j].options[sel].selected = true;
+      					while (((ourPosition = elementValue.indexOf("|")) != -1) && (elementValue.length > 1)) {
+       						var ourSelection = parseInt(elementValue.substring(0,ourPosition));
+       						elementValue = elementValue.substring(ourPosition+1,elementValue.length);
+       						if (ourSelection < document.forms[i].elements[j].length) {
+        						document.forms[i].elements[j].options[ourSelection].selected = true;
 								}
-      				}
-     			}else if ((document.forms[i].elements[j].type == "checkbox")
+      					}//end of while (((ourPosition...
+     			} else if ((document.forms[i].elements[j].type == "checkbox")
 							||(document.forms[i].elements[j].type == "radio")) {
-      					if (elementValue=="true") {					
-							document.forms[i].elements[j].checked = true;
-							}
+								if (elementValue == "true") {					
+									document.forms[i].elements[j].checked = true;
+								}
 				}
 			}// end of if (document.forms[i].elements[j].name == elementName) {
 		}	//end of for (j=0;j<document.forms[i].elements.length;j++) {
 	}   // end of for (i=0;i<document.forms.length;i++) { 
- }  // end of if (val) {
+ }  // end of if (cookieValue) {
 }  //end of function storedValues() 
 
 function saveForm (cookieName, nextPage) {
+var zircCookie = "";
 //checking for data...
 var formCheck = emptyFormCheck();
  if (formCheck != true) {
@@ -189,39 +193,49 @@ var formCheck = emptyFormCheck();
 	}
  else {   
 // after checking for data we continue...
-var zircCookie = "";
   for (i=0;i<document.forms.length;i++) { 
    for (j=0;j<document.forms[i].elements.length; j++) { 
-    var cookieElement = document.forms[i].elements[j].name; 
-  if ((document.forms[i].elements[j].type == "text")
+    var elementName = document.forms[i].elements[j].name; 
+	if ((document.forms[i].elements[j].type == "text")
 		|| (document.forms[i].elements[j].type == "password")
-		|| (document.forms[i].elements[j].type == "textarea")){
-	var elementValue = document.forms[i].elements[j].value;
+		|| (document.forms[i].elements[j].type == "textarea")) {
+			var elementValue = document.forms[i].elements[j].value;
   } else if (document.forms[i].elements[j].type.indexOf("select") != -1) {
-		var elementValue = "";
-		for(k=0;k<document.forms[i].elements[j].length;k++) {
-			if (document.forms[i].elements[j].options[k].selected) {
-     			   elementValue = document.forms[i].elements[j].options[k].text;}
-		}//end of for(k= 
-  } 
-//note that these types are not entirely supported 
-  else if ((document.forms[i].elements[j].type == "checkbox") || 
-			   (document.forms[i].elements[j].type == "radio")) {
-					var elementValue = document.forms[i].elements[j].checked;
-  } 
-//after gathering the name and value we continue...
-  var elementPair = (cookieElement + "=" + elementValue);
+   		var elementValue = ""; 
+		var elementSelected = "";
+   			for(k=0;k<document.forms[i].elements[j].options.length;k++){			
+				if (document.forms[i].elements[j].options[k].selected == true){
+        			elementSelected += k + "|"; //+= is used here to allow for multiple selections
+					elementValue += document.forms[i].elements[j].options[k].value + "|";
+					} 
+				} //end of for(k=0;k<document.forms[i].elements[j].length;k++){
+  } else if ((document.forms[i].elements[j].type == "checkbox")
+            ||(document.forms[i].elements[j].type == "radio")) {
+			   elementValue = "";
+				if (document.forms[i].elements[j].checked == true ) {
+              		elementValue = document.forms[i].elements[j].value;
+			  	}
+  } else if (document.forms[i].elements[j].type == "button") {
+       			elementValue = "";
+  }
+  //eliminate items that were not selected and/or entered
+  if (elementValue) {
+     var elementPair = (elementName + "=" + elementValue); 
 	// These next lines loop together the long string that makes the cookie (e[0]=v[0]&e[1]=v[1]&...)
-	if (zircCookie == "") {
-		zircCookie = elementPair;} 
-	else {
-		zircCookie = zircCookie + "&" + elementPair;} //end of if (zircCookie == "")
+		if (zircCookie == "") {
+			zircCookie = elementPair;
+			} else {
+			zircCookie = zircCookie + "&" + elementPair;
+			}
+	  } //end of if (elementValue) {
 	} //end of for (j=0;j<document.forms[i].elements.length; j++) { 	
    } //end of for (i=0;i<document.forms.length;i++)
+//after gathering the name and value we continue...
 var expires = new Date((new Date()).getTime() + 1 *24*60*60*1000);//days*hrs*min*sec*millisec -- this cookie expires in 1 day
    Set_Cookie(cookieName, zircCookie, expires); //set the cookie
 if (nextPage) {
- window.location.href=nextPage;}
+ 	window.location.href=nextPage;
+	}
  } // end of else emptyFormCheck
 }   //end of function saveForm() 
 
@@ -229,18 +243,18 @@ function formValues(formCookieName) {
 	for (i=0; i<document.forms.length; i++) {
 	var cookieValue = getCookie(formCookieName);
 	if (cookieValue) {
-	var a = cookieValue.split(escape('&')); //parsing name/value pairs
-		for (x=0; x<a.length; x++) {
-			a[x] = a[x].split(escape('=')); //breaking pairs into an array
-			if (a[x]) {
-				var cookieElementName = a[x].slice(0,1); //find the name in this value pair
-				var cookieElementValue = a[x].slice(1,2); //find the value in this value pair
+	var cookieArray = cookieValue.split(escape('&')); //parsing name/value pairs
+		for (x=0; x<cookieArray.length; x++) {
+			cookieArray[x] = cookieArray[x].split(escape('=')); //breaking pairs into an array
+			if (cookieArray[x]) {
+				var elementName = cookieArray[x].slice(0,1); //find the name in this value pair
+				var elementValue = cookieArray[x].slice(1,2); //find the value in this value pair
 				}
 				for (e=0; e<document.forms[i].elements.length; e++) {
 			      if ((document.forms[i].elements[e].type != "submit") &&
 				  	  (document.forms[i].elements[e].type != "button")) {
-				  		if (cookieElementName == document.forms[i].elements[e].name){
-							document.forms[i].elements[e].value = unescape(cookieElementValue);
+				  		if (elementName == document.forms[i].elements[e].name){
+							document.forms[i].elements[e].value = unescape(elementValue);
 						}
 					}
 				}  //end of for (e=0; e<document.forms[i].elements.length; e++) {
