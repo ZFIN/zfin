7dd0ee830cbe7c590738a2065b3eeee095edf6a0
Brock Sprunge <bsprunge@zfin.org>
2002-12-05 23:37:34 +0000

    *** empty log message ***
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2507 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/GO/go.pl
@@ -0,0 +1,97 @@
+#!/local/bin/perl 
+#  Script to create RefSeq/LocusLink links in the database
+
+use DBI;
+$mailprog = '/usr/lib/sendmail -t -oi -oem';
+
+#set environment variables
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+
+### open a handle on the db
+my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
+                       '', 
+                       '', 
+		       {AutoCommit => 1,RaiseError => 1}
+		      )
+  || emailError("Failed while connecting to <!--|DB_NAME|--> "); 
+
+chdir "<!--|ROOT_PATH|-->/server_apps/data_transfer/GO/";
+
+
+&GOReport();
+&sendReport();
+
+exit;
+
+
+
+sub emailError()
+  {
+    &writeReport($_[0]);
+    &sendReport();
+    exit;
+  }
+
+
+sub writeReport()
+  {
+    open (REPORT, ">>report") or die "cannot open report";
+    print REPORT "$_[0] \n\n";
+    close (REPORT);
+  }
+
+
+
+sub GOReport()
+  {
+    system("/bin/rm -f report");
+    open (REPORT, "report") or die "can not open report";
+
+    print REPORT "DBLINKS\n";
+
+    my $cur = $dbh->prepare('select mrkr_zdb_id, 
+                                    mrkr_abbrev,
+                                    goterm_go_id,
+                                    mrkrgoev_source_zdb_id,
+                                    goev_name,
+                                    get_date_from_id(mrkr_go_zdb_id)
+                             from   marker,
+                                    marker_go_term,
+                                    go_term,
+                                    marker_go_term_evidence,
+                                    go_evidence_code
+                             where  mrkr_zdb_id = mrkrgo_mrkr_zdb_id
+                               and  mrkrgo_go_term_zdb_id = goterm_zdb_id
+                               and  mrkrgo_zdb_id = mrkrgoev_mrkrgo_zdb_id
+                               and  mrkrgoev_evidence_code = goev_code;'
+			   );
+    $cur->execute;
+    my($mrkr_id, $mrkr_abbrev, $go_id, $source_id, $ev_name, $ev_date);
+    $cur->bind_columns(\$mrkr_id, \$mrkr_abbrev, \$go_id, \$source_id, \$ev_name, \$ev_date);
+    while ($cur->fetch)
+    {
+      print REPORT "ZFIN\t$mrkr_id\t$mrkr_abbrev\t\t$go_id\t$source_id\t$ev_name\t\t\t\tgene\ttaxon:7955\t$ev_date\n";
+    }
+    print REPORT "\n";
+    close(REPORT);
+  }
+
+
+sub sendReport()
+  {
+    open(MAIL, "| $mailprog") || die "cannot open mailprog $mailprog, stopped";
+    open(REPORT, "report") || die "cannot open report";
+
+    print MAIL "To: bsprunge\@cs.uoregon.edu\n";
+    print MAIL "Subject: GO report\n";
+    while($line = <REPORT>)
+    {
+      print MAIL $line;
+    }
+    close (REPORT);
+    close (MAIL);
+    $dbh->disconnect();
+  }
