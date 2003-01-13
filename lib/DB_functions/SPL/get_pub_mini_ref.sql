d1b1fa2d6b38b0c53fe04f258b0e331248b95326
Dave Clements <clements@zfin.org>
2003-01-13 21:15:23 +0000

    Added get_pub_mini_ref function.  Used to populate the pub_mini_ref column.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2573 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/get_pub_mini_ref.sql
@@ -0,0 +1,53 @@
+drop function get_pub_mini_ref;
+
+create function get_pub_mini_ref(pubZdbId varchar(50))
+
+  returning varchar(60);
+
+  define authorList       lvarchar;
+  define pubYear varchar(15);
+  define miniRef varchar(60);
+  define lname    varchar(60);
+  define delim    char(1);
+  define ch       char(1);
+  define len      int;
+  define index    int;
+  define first    boolean;
+
+  let authorList = "";
+  let pubYear = "";
+  let miniRef = "";
+  let lname = "";
+  let ch = "";
+        
+  let delim = ",";
+  let first = "t";
+
+  select authors, year(pub_date)
+    into authorList, pubYear
+    from publication
+    where zdb_id = pubZdbId;
+
+  let len = length(authorList);
+  for index = 1 to len
+    let ch = substr(authorList, index, 1);
+    if (ch = delim) and (first) then  
+      let lname = substr(authorList,1,(index-1));
+      let first = "f";
+    elif (ch = delim) and (not first) then    
+      let lname = lname || " <i>et al</i>"; 
+      exit for;
+    end if    
+  end for
+
+  if lname != "" then   
+    let miniRef = lname || ", " || pubYear;
+  else
+    let miniRef = NULL;
+  end if
+
+  return miniRef;          
+
+end function;
+
+update statistics for function get_pub_mini_ref;
