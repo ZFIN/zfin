9ee9b31ac9ccc1384dbe8094168c020f14c5c45f
Dave Clements <clements@zfin.org>
2003-03-27 17:14:56 +0000

    Dropped the lib/DB_functions/Java directory and all functions defined in it. ZFIN is movinig from 32-bit Informix 9.21 to 64-bit Informix 9.30 and the 64-bit version of Informix does not support Java in the server.  All Java functions were converted to C.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2786 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/Java/drop_functions.sql
+++
@@ -1,9 +0,0 @@
--- drop functions
-
-drop function zero_pad(varchar(255), int);
-drop function zero_pad(varchar(255));
-
--- uninstall the Java UDR jar file 
-
-execute procedure remove_jar("zeropad_jar", 0);
-
