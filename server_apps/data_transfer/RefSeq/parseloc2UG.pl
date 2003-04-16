dbf42dc8b7a86b82a272e788a497c42c4bfadb71
Brock Sprunge <bsprunge@zfin.org>
2003-04-16 17:40:54 +0000

    Remove organism prefix from ID.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2814 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseloc2UG.pl
+++ server_apps/data_transfer/RefSeq/parseloc2UG.pl
@@ -2,7 +2,7 @@
 
 
 open (CUT, "cut -f 1,2 loc2UG |") or die "cut loc2UG failed";
-open (UNL, ">loc2UG.unl") or die "cannot open loc2UG.unl";
+open (UNL, ">loc2UG.unl") or die 'cannot open loc2UG.unl';
 
 while ($line = <CUT>) {
 
@@ -10,7 +10,8 @@ while ($line = <CUT>) {
   ($ll_id,$uni_gene) = split ' ',$line,2;
 
   if ( $uni_gene =~ /^Dr/ ) {
-    print UNL "$ll_id|$uni_gene|\n";
+    $uni_num = substr($uni_gene,3);   #remove 'Dr.'
+    print UNL "$ll_id|$uni_num|\n";
   }
 }
 
