b6e0ac67dd1afd8e98f9ae880d8d6e91e61a65e4
Dave Clements <clements@zfin.org>
2003-05-05 15:57:20 +0000

    Added "Somitogenesis" articles to list of duplicate pubs to ignore. Commented out 2nd ZDB ID in each duplicate pub.  The code will now detect when a duplicate becomes a triplicate.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2879 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata.pl
+++ server_apps/DB_maintenance/validatedata.pl
@@ -1711,30 +1711,31 @@ sub pubTitlesAreUnique($) {
 	
   logHeader ("Checking for duplicate publication titles (ignoring known valid dups)");
 	
-  # Hmmm.  We could only exclude 1 record from each pair.  That would 
-  # achieve the same effect.
+  # Only need to exclude 1 record per pair.  That means if a duplicate
+  # becomes a triplicate, we will detect it.
 
   my $sql1 = '
     select title 
       from publication 
       where zdb_id not in (
-	"ZDB-PUB-021016-117", "ZDB-PUB-961014-110",
-	"ZDB-PUB-961014-169", "ZDB-PUB-961014-170",
-	"ZDB-PUB-021016-125", "ZDB-PUB-961014-1217",
-	"ZDB-PUB-021016-60",  "ZDB-PUB-991014-9",
-	"ZDB-PUB-000125-1",   "ZDB-PUB-990525-2",
-	"ZDB-PUB-961014-288", "ZDB-PUB-961014-289",
-	"ZDB-PUB-010718-37",  "ZDB-PUB-010912-30",
-	"ZDB-PUB-981110-12",  "ZDB-PUB-990218-4",
-	"ZDB-PUB-021016-112", "ZDB-PUB-961014-758", "ZDB-PUB-961014-759",
-	"ZDB-PUB-000125-3",   "ZDB-PUB-010131-19",
-	"ZDB-PUB-021015-13",  "ZDB-PUB-030211-13",
-	"ZDB-PUB-961014-1233","ZDB-PUB-961014-1234",
-	"ZDB-PUB-961014-106", "ZDB-PUB-961014-107",
-	"ZDB-PUB-010417-9",   "ZDB-PUB-990414-35",
-	"ZDB-PUB-010711-2",   "ZDB-PUB-010814-8",
-	"ZDB-PUB-000824-10",  "ZDB-PUB-990824-40",
-	"ZDB-PUB-010912-1",   "ZDB-PUB-021017-13"
+	"ZDB-PUB-021016-117", { "ZDB-PUB-961014-110", }
+	"ZDB-PUB-961014-169", { "ZDB-PUB-961014-170", }
+	"ZDB-PUB-021016-125", { "ZDB-PUB-961014-1217", }
+	"ZDB-PUB-021016-60",  { "ZDB-PUB-991014-9", }
+	"ZDB-PUB-000125-1",   { "ZDB-PUB-990525-2", }
+	"ZDB-PUB-961014-288", { "ZDB-PUB-961014-289", }
+	"ZDB-PUB-010718-37",  { "ZDB-PUB-010912-30", }
+	"ZDB-PUB-981110-12",  { "ZDB-PUB-990218-4", }
+	"ZDB-PUB-021016-112", "ZDB-PUB-961014-758", { "ZDB-PUB-961014-759", }
+	"ZDB-PUB-000125-3",   { "ZDB-PUB-010131-19", }
+	"ZDB-PUB-021015-13",  { "ZDB-PUB-030211-13", }
+	"ZDB-PUB-961014-1233",{ "ZDB-PUB-961014-1234", }
+	"ZDB-PUB-961014-106", { "ZDB-PUB-961014-107", }
+	"ZDB-PUB-010417-9",   { "ZDB-PUB-990414-35", }
+	"ZDB-PUB-010711-2",   { "ZDB-PUB-010814-8", }
+	"ZDB-PUB-000824-10",  { "ZDB-PUB-990824-40", }
+	"ZDB-PUB-010912-1",   { "ZDB-PUB-021017-13" }
+        "ZDB-PUB-980420-9"    { "ZDB-PUB-030425-13" }
       )
       group by title 
       having count(*) > 1 
