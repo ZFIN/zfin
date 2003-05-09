2b47e5d52177391c4a665c1156ef6c3c8fa1577a
Peiran Song <peirans@zfin.org>
2003-05-09 18:16:09 +0000

    fix column name unconsistence
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2881 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/JohnP/create_files_for_johnp.sql
+++ server_apps/data_transfer/JohnP/create_files_for_johnp.sql
@@ -1,6 +1,6 @@
 UNLOAD to '<!--|FTP_ROOT|-->/pub/transfer/JohnP/accession.txt'
-  DELIMITER "	" select distinct mrkr_zdb_id, abbrev, acc_num  from marker, OUTER db_link  where ((mrkr_type = 'EST') or (mrkr_type = 'GENE'))  and linked_recid = mrkr_zdb_id and db_name = 'Genbank' order by 1;
+  DELIMITER "	" select distinct mrkr_zdb_id, mrkr_abbrev, acc_num  from marker, OUTER db_link  where ((mrkr_type = 'EST') or (mrkr_type = 'GENE'))  and linked_recid = mrkr_zdb_id and db_name = 'Genbank' order by 1;
 
 
 UNLOAD to '<!--|FTP_ROOT|-->/pub/transfer/JohnP/mappings.txt' 
-  DELIMITER "	" select distinct  marker_id, p.abbrev, or_lg, lg_location, metric from mapped_marker mm, panels p where mm.refcross_id = p.zdb_id and ((marker_id like '%EST%') or (marker_id like '%GENE%')) order by 1;
+  DELIMITER "	" select distinct  marker_id, p.abbrev, or_lg, lg_location, mm.metric from mapped_marker mm, panels p where mm.refcross_id = p.zdb_id and ((marker_id like '%EST%') or (marker_id like '%GENE%')) order by 1;
