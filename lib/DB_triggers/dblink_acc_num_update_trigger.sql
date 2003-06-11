17e4ad38862b64fecd62390543b7bed4178ff6f4
Dave Clements <clements@zfin.org>
2003-06-11 21:48:04 +0000

    Update trigger for the db_link.acc_num column.  Initial version causes the dblink_acc_num_display column to be populated.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2942 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/dblink_acc_num_update_trigger.sql
@@ -0,0 +1,10 @@
+drop trigger dblink_acc_num_update_trigger;
+
+create trigger dblink_acc_num_update_trigger
+  update of acc_num on db_link
+  referencing new as new_db_link
+  for each row (
+    execute function
+      get_dblink_acc_num_display(new_db_link.db_name, new_db_link.acc_num)
+      into dblink_acc_num_display
+  );
