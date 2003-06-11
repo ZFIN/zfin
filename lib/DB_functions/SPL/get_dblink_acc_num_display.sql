b80e5af1d14883b39e4903bfced55852a6fc3a27
Dave Clements <clements@zfin.org>
2003-06-11 21:45:47 +0000

    Routine to reformat a DB_LINK.ACC_NUM value into something that is more display friendly.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@2941 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/get_dblink_acc_num_display.sql
@@ -0,0 +1,68 @@
+drop function get_dblink_acc_num_display;
+
+create function get_dblink_acc_num_display(dblinkDbName varchar(50),
+					   dblinkAccNum varchar(50))
+  returning varchar(50);
+
+  -- For most db links we display the link as db_name:acc_num
+  -- However, for a few databases, the acc_num is not a good choice
+  -- for display, either because it has too much information, or not
+  -- enough.  This function takes in a db name and accession number
+  -- and returns the accession number in a format that is better for
+  -- displaying.
+  -- For most databases, the accession number is returned in exactly
+  -- the same format it came in in.
+
+  -- Both of the input params are part of the PK of db_link.
+  -- Therefore they can't be NULL.
+  -- However, it is possible that the acc_num is not well formed,
+  -- in which case this routine may return a NULL.
+
+  define len integer;
+  define index integer;
+  define ch char(1);
+  define contigId like db_link.dblink_acc_num_display;
+  define dblinkAccNumDisplay like db_link.dblink_acc_num_display;
+
+  if (dblinkDbName = "WEB_FPC") then
+    -- The acc_num will look something like
+    --   12595&marker=stID23165.16
+    -- or, generally
+    --   contig_id&marker=marker_id
+    -- In this case we only want to condense this down to:
+    --   contig_id:marker_id
+    -- Scan until hitting the &
+
+    let len = length(dblinkAccNum);
+    let index = 1;
+    let ch = substr(dblinkAccNum, index, 1);
+    while (index < len and ch <> "&")
+      let index = index + 1;
+      let ch = substr(dblinkAccNum, index, 1);
+    end while;
+
+    let contigId = substr(dblinkAccNum, 1, index - 1);
+
+    -- check for "&marker=" and then pull the remainder of the string
+
+    if (substr(dblinkAccNum, index, 8) = "&marker=") then
+      let dblinkAccNumDisplay = 
+		substr(dblinkAccNum, index + 8) || "," || contigId;
+    else
+      -- not well formatted, return null
+      let dblinkAccNumDisplay = NULL::varchar(50);
+    end if
+
+  elif (dblinkDbName = "WashUZ") then
+    -- we need to prepend "wz" to the acc_num
+    let dblinkAccNumDisplay = "wz" || dblinkAccNum;
+  else
+    -- spew out exactly what came in
+    let dblinkAccNumDisplay = dblinkAccNum;
+  end if;
+
+  return dblinkAccNumDisplay;
+
+end function;
+
+update statistics for function get_dblink_acc_num_display;
