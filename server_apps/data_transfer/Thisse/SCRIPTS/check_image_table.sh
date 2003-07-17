f136e64cf6e001f0ff426eda8ad3653c80a90728
Brock Sprunge <bsprunge@zfin.org>
2003-07-17 16:55:58 +0000

    Adjust parsing for New Thisse Template with additional columns.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3008 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Thisse/SCRIPTS/check_image_table.sh
+++ server_apps/data_transfer/Thisse/SCRIPTS/check_image_table.sh
@@ -2,7 +2,7 @@
 	
 echo "looking for rows in images.csv that that don't have .jpg and .txt files"
 echo ""
-foreach f ( `cat images.csv | cut -f1 -d,| tr -d \" `) 
+foreach f ( `cat images.csv | cut -f2 -d,| tr -d \" `) 
 	if ( "`ls Imagesdir/'$f'`" != "Imagesdir/$f") echo $f
 	if ( "`ls Imagesdir/'$f:r.txt'`" != "Imagesdir/$f:r.txt") echo $f:r.txt
 end
@@ -12,7 +12,7 @@ echo ""
 
 echo "looking for .jpg sent but not included in the images.csv table"
 
-cat images.csv | cut -f1 -d\. | tr -d \" | sort >! imgtab.list
+cat images.csv | cut -f1 -d\. | cut -f2 -d, |tr -d \" | sort >! imgtab.list
 cat imagedim.unl | cut -f1 -d\| | sort >! imgdir.list
 diff imgtab.list imgdir.list  |grep '^>' | cut -c 3-
 echo ""
