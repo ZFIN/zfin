9cfb8068ffda191eed733c0b712ca6dccecab6ae
Tom Conlin <tomc@zfin.org>
2003-07-22 22:07:50 +0000

    include FPC exchange files
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3024 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Sanger/SangerFiles.sql
+++ server_apps/data_transfer/Sanger/SangerFiles.sql
@@ -1,31 +1,9 @@
--- Script to create data files for public download.
+-- Script to create data files for Sanger to download.
 --
--- We extract several different kinds of information:
--- 
--- All genetic markers (includes genes, ests, sslps, etc.)
---	zfin id, name, symbol, type, OR_lg
---	
--- Synonyms  (for any item in all genetic markers file) There may be multiple lines 	
---   per zfin id
---	zfin id, synonym 
---
--- Orthology - separate files for: D	
---   zebrafish - human
---	zfin id , zebrafish symbol, human symbol, OMIM id, LocusLink id
---   zebrafish - mouse
---	zfin id , zebrafish symbol, mouse symbol, MGI id, LocusLink id
---   zebrafish - fly
---	zfin id,  zebrafish symbol, fly symbol,  Flybase id
---   zebrafish - yeast
---	zfin id,  zebrafish symbol, yeast symbol,  SGD id
---
--- Gene Onotology-
---	A copy of the file we send to GO.
---
--- Gene Expression
---	gene zfin id, gene symbol, probe zfin id ,probe symbol,probe genbank accession,expression type, expression pattern zfin id
 
-
+--
+-- Vega Expression
+--	gene id, gene symbol, probe id ,probe symbol,probe genbank accession,expression type, expression pattern zfin id
 
 -- generate a file with genes and associated expression patterns
 
@@ -40,12 +18,12 @@ UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Sanger/VegaXpat.txt'
 	   xpat_zdb_id[1,26]		xpad_zdb_id, 
 	   recattrib_source_zdb_id[1,26] pub_zdb
  from marker gene, marker probe, 
- 	  expression_pattern_assay, 
- 	  expression_pattern, 
+	  expression_pattern_assay, 
+	  expression_pattern, 
 	  marker_relationship,
 	  db_link,
 	  record_attribution
-	   
+
  where gene.mrkr_zdb_id = mrel_mrkr_1_zdb_id 
  and mrel_mrkr_2_zdb_id = xpat_probe_zdb_id 
  and xpat_assay_name = xpatassay_name
@@ -53,7 +31,7 @@ UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Sanger/VegaXpat.txt'
  and probe.mrkr_zdb_id = linked_recid
  and db_name = 'Genbank'
  and xpat_zdb_id = recattrib_data_zdb_id
- {
+ { have decided against including gene accessions till asked
  union
   select gene.mrkr_zdb_id[1,26]	gene_zdb,
 	   gene.mrkr_abbrev[1,20]	gene_sym,
@@ -80,4 +58,25 @@ UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Sanger/VegaXpat.txt'
  }
  order by 7,1,3;
 
--- clean up
+-- FPC Alias and Mapping
+
+select dalias_alias,mrkr_zdb_id,mrkr_abbrev 
+from data_alias, marker, record_attribution 
+where recattrib_source_zdb_id = 'ZDB-PUB-030703-2'
+and   recattrib_data_zdb_id   = dalias_zdb_id
+and   dalias_data_zdb_id      = mrkr_zdb_id
+order by 1,2,3
+into temp tmp_fpc_alias with no log;
+
+-- Sanger id, symbol_panel, LG, location, metric
+UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Sanger/FPC_mapping.unl'
+select dalias_alias, mrkr_abbrev || '_' || p.abbrev , or_lg, lg_location, mm.metric
+from tmp_fpc_alias, mapped_marker mm ,panels p
+where marker_id = mrkr_zdb_id
+and   p.zdb_id = refcross_id;
+
+-- Sanger id, ZFIN id, zfin symbol
+UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Sanger/FPC_alias.unl'
+select * from tmp_fpc_alias;
+
+drop table tmp_fpc_alias;
