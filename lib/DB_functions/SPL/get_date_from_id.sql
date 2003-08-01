c75a20e7f820ec32f35d6ee9ac564f3e3df2d66d
Brock Sprunge <bsprunge@zfin.org>
2003-08-01 17:04:58 +0000

    Modify the parameter list to include the return format for the date. Return formats: YYYYMMDD and MM/DD/YYYY
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3055 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/get_date_from_id.sql
+++ lib/DB_functions/SPL/get_date_from_id.sql
@@ -1,32 +1,40 @@
 drop function get_date_from_id;
 
 create function
-get_date_from_id(zdbId varchar(50))
+get_date_from_id(zdbId varchar(50),format varchar(10))
 
-  returning varchar(8);  -- date format YYYYMMDD.
+  returning varchar(10);  
 
-  -- Given a ZDB ID, gets the name of the object associated with that ZDB ID.
-  -- If the object does not have a name per se, then its ZDB ID is returned
-  --   as the name.
-  -- Returns NULL if ZDB ID does not point to a record.
+  -- Given a ZDB ID, extracts and returns the date portion
+  -- Returns NULL if Object Type is invalid.
 
   define century	varchar(2);
   define zdb_date	varchar(6);
-  define dateFromId	varchar(8);
+  define dateFromId	varchar(10);
   define offset		integer;
   define objType	varchar(20);
 
   let objType = get_obj_type (zdbId);
   let offset = length(objType) + 6;
   let zdb_date = substr(zdbId,offset,6);
-  
+
+  --| Determine Century |--
   if (substr(zdb_date,0,1) = "9") then
       let century = "19";
   else
       let century = "20";
-  end if
-
-  let dateFromId = century || zdb_date;
+  end if  
+  
+    
+  IF (format = "MM/DD/YYYY") THEN
+      -- date format MM/DD/YYYY.
+      let dateFromId = substr(zdb_date,3,2) || '/' || substr(zdb_date,5,2) || '/' || century || substr(zdb_date,0,2);
+  
+  ELSE
+      -- date format YYYYMMDD.
+      let dateFromId = century || zdb_date || '  ';
+  
+  END IF
 
   return dateFromId;
 
