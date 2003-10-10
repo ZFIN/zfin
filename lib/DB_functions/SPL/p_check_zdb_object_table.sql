f60fd628bc9d2811a6151398d70049e121c04f93
Sierra Moxon <smoxon@zfin.org>
2003-10-10 17:05:44 +0000

    entered new procedure to CVS
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3193 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_check_zdb_object_table.sql
@@ -0,0 +1,43 @@
+begin work;
+
+  --P_CHECK_ZDB_OBJECT_TABLE
+  ------------------------------------------------------
+  --procedure that checks to make sure tables and columns in 
+  --zdb_object_type table exist in systables and syscolumns.
+  --REPLACES:
+  --sub zdbObjectHomeTableColumnExist 
+
+  drop procedure p_check_zdb_object_table;
+  create procedure p_check_zdb_object_table (vTableName varchar(128), 
+	  				     vColumnName varchar(35))
+ 
+  define vOkInSystables		integer;
+  define vOkInSyscolumns	integer;
+  define vTableId		integer;
+
+  let vTableId = (select tabid 
+		   from systables 
+		   where tabname = vTableName);
+
+  let vOkInSystables = (select count(*) 
+			  from systables 
+			  where tabname = vTableName);
+
+  if vOkInSystables < 1 then
+    raise exception -746, 0, "FAIL!: table name not in systables";
+  else 
+	let vOkInSyscolumns = (select count(*) 
+	 			 from syscolumns, systables 
+  				 where syscolumns.tabid = systables.tabid
+			         and systables.tabid = vTable_id
+				 and syscolumns.colname = vColumnName);
+
+	if vOkInSyscolumns < 1 then
+	  raise exception -746, 0, "FAIL!: column name not in syscolumns";
+	end if;
+
+  end if;
+ 
+  end procedure;
+
+commit work;
