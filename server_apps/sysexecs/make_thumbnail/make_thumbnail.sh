98fbbbf8f2ee7e0c849ea84885346823be965e9b
Tom Conlin <tomc@zfin.org>
2003-10-16 01:57:05 +0000

    making more secure
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3227 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
+++ server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
@@ -1,8 +1,9 @@
-#!/local/bin/perl
-my $fullImageFile = $ARGV[0];
-my $thumbnailFile = $ARGV[1];
-
-$ENV{PATH} = "/local/apps/netpbm/bin:$ENV{PATH}";
+#!/local/bin/perl -wT
+$ENV{PATH} = "/local/apps/netpbm/bin:/bin";
+$ARGV[0] =~  m/([A-Za-z\d\-\_\$\+\=\~\.\,\ \/]+)/;
+my $fullImageFile = $1;
+$ARGV[1] =~  m/([A-Za-z\d\-\_\$\+\=\~\.\,\ \/]+)/;
+my $thumbnailFile = $1;
 
 system("/local/apps/netpbm/bin/anytopnm $fullImageFile | " .
        "/local/apps/netpbm/bin/pnmscale -h 64 | " .
