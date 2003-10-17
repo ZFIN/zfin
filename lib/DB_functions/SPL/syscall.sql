f9c76074a644ac6db664d860bf4dc2260c7052cd
Tom Conlin <tomc@zfin.org>
2003-10-17 00:10:52 +0000

    removed failed experiment
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3239 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/syscall.sql
+++ lib/DB_functions/SPL/syscall.sql
@@ -2,10 +2,12 @@
 -- this may replace sysexec()
   
 drop procedure syscall;
-create procedure syscall (cmd varchar(100), arg varchar(100))
+create dba procedure syscall (cmd varchar(100), arg varchar(100))
 	define command varchar(201);
 	let command = cmd || " " ||arg;
 	system command;
 end procedure;
-
 update statistics for procedure syscall;
+grant execute on procedure "tomc".syscall  to "public" as "tomc";
+
+
