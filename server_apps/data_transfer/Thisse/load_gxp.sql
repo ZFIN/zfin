61ba0e6cf020daf2e107b799ce316aa8a51819dc
Brock Sprunge <bsprunge@zfin.org>
2003-12-01 16:45:32 +0000

    reverse ordering of columns pcr_amp & library.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3344 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Thisse/load_gxp.sql
+++ server_apps/data_transfer/Thisse/load_gxp.sql
@@ -24,9 +24,9 @@ create temp table probes_tmp (
    genename varchar(120)default null, -- 11/17 changed from abbrev to ZDB-id
    gb5p varchar (50),
    gb3p varchar (50),
-   pcr_amp varchar(200),
-   library varchar(80),
    vector varchar(80),
+   library varchar(80),
+   pcr_amp varchar(200),
    insert_kb float,
    cloning_site varchar(20),
    digest varchar(20),
