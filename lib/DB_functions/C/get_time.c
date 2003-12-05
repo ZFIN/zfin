592188606a6bcbac64a170d75226e7f56d47e81e
Peiran Song <peirans@zfin.org>
2003-12-05 22:13:08 +0000

    Add function to get current time from system in the format of hh:mm:ss.sss
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3392 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/C/get_time.c
@@ -0,0 +1,28 @@
+/*
+ * Defines get_time() function.
+ *
+ */
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
+#include <mi.h>
+#include <milib.h>
+
+mi_lvarchar *
+get_time() 
+{
+  time_t timer;
+  struct timeval tv;
+  char   tbuf[30], sbuf[20], msbuf[10];
+
+  time(&timer);
+  gettimeofday(&tv, NULL);
+
+  cftime(sbuf, "%T", &timer);
+ 
+  sprintf(msbuf, "%d", tv.tv_usec/1000);
+  sprintf(tbuf, "%s.%s", sbuf, msbuf);
+
+  return (mi_string_to_lvarchar(tbuf));
+
+}
