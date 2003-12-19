9c111efbadbff42ec5f21fb0814b02c7fbb03dc2
Sierra Moxon <smoxon@zfin.org>
2003-12-19 17:31:58 +0000

    added regen_genomics, regen_fishsearch, regen_oevdisp to nightly run.
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3438 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/regen.sh
+++ server_apps/DB_maintenance/regen.sh
@@ -9,6 +9,9 @@ setenv INFORMIXSQLHOSTS ${INFORMIXDIR}/etc/<!--|SQLHOSTS_FILE|-->
 setenv LD_LIBRARY_PATH ${INFORMIXDIR}/lib:${INFORMIXDIR}/lib/esql
 setenv PATH <!--|INFORMIX_DIR|-->/bin:$PATH
 
-echo "Starting regen_maps at `date`"
+echo "Starting regen_maps,genomics,anatomy,oevdisp,panel,fishsearch at `date`"
 echo 'execute function regen_maps()' | dbaccess <!--|DB_NAME|-->
+echo 'execute function regen_genomics()' | dbaccess <!--|DB_NAME|-->
+echo 'execute function regen_oevdisp()' | dbaccess <!--|DB_NAME|-->
+echo 'execute function regen_fishsearch()' | dbaccess <!--|DB_NAME|-->
 echo "Finished at `date`"
