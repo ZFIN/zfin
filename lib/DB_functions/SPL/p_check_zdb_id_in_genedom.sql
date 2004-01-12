385d41b9d31bbbd587c29aed1892448eaedc4ec9
Sierra Moxon <smoxon@zfin.org>
2004-01-12 22:46:44 +0000

    procedure to check that a zdb_id is in the marker_type_group: "GENEDOM" BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3463 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_check_zdb_id_in_genedom.sql
@@ -0,0 +1,25 @@
+--P_check_zdbid_in_gendome.sql
+--------------------------------
+  --procedure that checks to see that the object type of a zdb_id 
+  --exists in the marker_type_group 'GENEDOM' 
+
+  drop procedure p_check_zdb_id_in_gendome ;
+  create procedure p_check_zdb_id_in_gendome (vZdbId varchar(50))
+  
+      define vType	varchar(10) ;
+      define vOk	integer ;
+
+      let vType = get_obj_type(vZdbId) ;
+  
+      let vOk = (select count(*)
+	               from marker_type_group_member
+		       where mtgrpmem_mrkr_type_group = 'GENEDOM'
+		       and mtgrpmem_mrkr_type = vType ) ;
+      if vOk == 0
+
+      then 
+        raise exception -746, 0, 'FAIL!: oevdisp_gene_zdb_id must have type valid in marker_group: GENEDOM' ;
+    
+      end if ;
+
+  end procedure ;
