097139daf0fbfc162d0ef764558af4a7967518d3
Tom Conlin <tomc@zfin.org>
2004-02-05 19:11:31 +0000

    BUGZID: none
    uninterleve mysql args
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3507 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Ensembl/fetch_ensdarg.sh
+++ server_apps/data_transfer/Ensembl/fetch_ensdarg.sh
@@ -1,9 +1,9 @@
 #! /bin/tcsh
 
 # where the first arg is like 'danio_rerio_core_19_2'
-set database=$1;
+set db=$1;
 
-/local/bin/mysql -P3306 -u anonymous -h -D$database ensembldb.ensembl.org -si < fetch_ensdarg.sql |\
+/local/bin/mysql -P3306 -u anonymous -h ensembldb.ensembl.org -D$db -si < fetch_ensdarg.sql |\
 sed 's/^\(ZDB-GENE-[0-9\-]*\).*\(ENSDARG[0-9]*\).*/\1|\2|/g' >!  ensdarg.unl;
 
 dbaccess <!--|DB_NAME|--> load_ensdarg.sql;
