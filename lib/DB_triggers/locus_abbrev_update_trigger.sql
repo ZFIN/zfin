e0a09f0abab1c9fffa1fbf5f26c2d916307ce2ea
Sierra Moxon <smoxon@zfin.org>
2004-03-12 01:47:38 +0000

    revert changes
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3596 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/locus_abbrev_update_trigger.sql
@@ -0,0 +1,9 @@
+create trigger locus_abbrev_insert_trigger 
+  update of abbrev on locus 
+    referencing new as new_locus
+    for each row (
+	execute function scrub_char(new_locus.abbrev)
+          into abbrev,
+        execute function zero_pad(new_locus.abbrev)
+	  into locus.locus_abbrev_order
+    );
