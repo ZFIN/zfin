51ea5e656bd9367203fe67f4219b7485f7c2b1ba
Sierra Moxon <smoxon@zfin.org>
2004-03-15 17:41:32 +0000

    removed (2nd try: see log) BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3603 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/locus_name_update_trigger.sql
+++
@@ -1,7 +0,0 @@
-create trigger locus_name_update_trigger
-    update of locus_name on locus 
-    referencing new as new_locus
-    for each row (
-	execute function scrub_char(new_locus.locus_name)
-	  into locus_name
-    );
