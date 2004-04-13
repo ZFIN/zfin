6eebca66ac5080a5583d482b89f89fd0d7a2f1b9
Brock Sprunge <bsprunge@zfin.org>
2004-04-13 18:01:46 +0000

    BUGZID: 61
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3709 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/db_link_insert_trigger.sql
+++ lib/DB_triggers/db_link_insert_trigger.sql
@@ -3,8 +3,14 @@ create trigger db_link_insert_trigger
   referencing new as new_db_link
   for each row (
     execute function 
-      scrub_char(new_db_link.acc_num) into acc_num,
+      scrub_char(new_db_link.dblink_acc_num) into dblink_acc_num,
+    execute procedure 
+      p_one_ortho_per_db(new_db_link.dblink_fdbcont_zdb_id,new_db_link.dblink_linked_recid),
+    execute procedure 
+      p_dblink_has_parent(new_db_link.dblink_linked_recid),
+    execute procedure 
+      p_check_caps_acc_num(new_db_link.dblink_fdbcont_zdb_id,new_db_link.dblink_acc_num),
     execute function 
-      get_dblink_acc_num_display(new_db_link.db_name, new_db_link.acc_num)
-      into dblink_acc_num_display
+      get_dblink_acc_num_display_new(new_db_link.dblink_fdbcont_zdb_id,new_db_link.dblink_acc_num) 
+      into db_link.dblink_acc_num_display
   );
