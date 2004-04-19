036777598a8c71bd2023f966788fdec7dd4d26a1
Tom Conlin <tomc@zfin.org>
2004-04-19 20:48:28 +0000

    inital add of a data pull wrapper script
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3738 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/pull.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+#  name: pull.sh
+#  to: call various data Pull scripts in a particular sequence
+#
+# ZGC-MEOW tightly coupled pull-push 
+# want to avoid curators being able to rename a zgc clone before
+# ncbi has had a chance to attach our zdb_id to their zgc-clone-name
+
+echo "ZGC Pull:"
+<!--|ROOT_PATH|-->/server_apps/data_transfer/ZGC/zgc.pl 
+echo "#########################################################################"
+echo "MEOW Push:"   #(whether the ZGC failed or not)
+<!--|ROOT_PATH|-->/server_apps/data_transfer/MEOW/meow.pl
+echo "#########################################################################"
+echo "RefSeq (LocusLink) Pull:" 
+<!--|ROOT_PATH|-->/server_apps/data_transfer/RefSeq/refseq.pl> <!--|ROOT_PATH|-->/server_apps/data_transfer/RefSeq/refseq.log 2>&1
+echo "#########################################################################"
+echo "GenPept Pull:" 
+#<!--|ROOT_PATH|-->/server_apps/data_transfer/GenPept/fetch_load_GenPept.sh
+echo "#########################################################################"
+#echo "Swiss-Prot Pull:"# -- expected ~ 2004-Jun
+#<!--|ROOT_PATH|-->/server_apps/data_transfer/ ... SP
+#echo "#########################################################################"
