759807ad3b285d613f146380ded15eab6f4cc6fa
Informix User <informix@zfin.org>
2004-04-20 16:22:37 +0000

    BUGZID: none Two changes:  o Commented out the command that sent e-mail to clements.  This script    already produces output which goes to the informix e-mail.  o If the dump is running on chromix it will first unload to chromix disk    and then copy it to the development RAID.  This will greatly speed up    the length of the nightly dumps.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3739 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/unload_production.sh
+++ server_apps/DB_maintenance/unload_production.sh
@@ -19,14 +19,26 @@ while ( -d $pth/$dirname )
 end
 
 #send out a warning
-mailx  -s"production unloaded as $dirname" clements@cs.uoregon.edu << END 
+#mailx  -s"production unloaded as $dirname" clements@cs.uoregon.edu << END 
 #mailx  -s"production unloaded as $dirname" tomc@cs.uoregon.edu << END 
-production is being being unloaded to 
-$pth/$dirname 
-at `date`.
-END
+#production is being being unloaded to 
+#$pth/$dirname 
+#at `date`.
+#END
 
-/research/zcentral/Commons/bin/unloaddb.pl <!--|DB_NAME|--> $pth/$dirname
+# A while back unloads from chromix to the NFS mounted development RAID
+# started taking a really long time, as much as 8 times longer than 
+# unloading the DB directly on to chromix disk and then copying it.
+# Therefore, to speed up the process from over 4 hours to well under
+# an hour, do the unload to chromix disk and then copy it.
+
+if ($HOST == "chromix") then
+  /research/zcentral/Commons/bin/unloaddb.pl <!--|DB_NAME|--> /research/zfin/chromix/backup/$dirname
+  /bin/cp -pr /research/zfin/chromix/backup/$dirname $pth/$dirname
+  /bin/rm -rf /research/zfin/chromix/backup/$dirname
+else 
+  /research/zcentral/Commons/bin/unloaddb.pl <!--|DB_NAME|--> $pth/$dirname
+endif
 
 chgrp -R fishadmin $pth/$dirname
 chmod -R g+rw $pth/$dirname
