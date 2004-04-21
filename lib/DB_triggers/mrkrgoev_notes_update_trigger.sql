139cb3d2207647ad2ee9561ef9dfe4a7a246c1ad
Sierra Moxon <smoxon@zfin.org>
2004-04-21 21:36:07 +0000

    table change no longer has this column BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3766 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/mrkrgoev_notes_update_trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger mrkrgoev_notes_update_trigger 
-       update of mrkrgoev_notes on marker_go_term_evidence 
-       referencing old as oldM
-                   new as newM
-    for each row
-      (
-	execute function scrub_char ( newM.mrkrgoev_notes )
-		into mrkrgoev_notes
-      );
