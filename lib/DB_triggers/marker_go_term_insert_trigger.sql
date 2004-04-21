cfc7e0481a94df7c4a086b436b9617f8ea5fbbbf
Sierra Moxon <smoxon@zfin.org>
2004-04-21 20:44:18 +0000

    added a new checking proc to this trigger BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3764 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/marker_go_term_insert_trigger.sql
+++ lib/DB_triggers/marker_go_term_insert_trigger.sql
@@ -4,5 +4,9 @@ create trigger marker_go_term_insert_trigger
     for each row (
       execute procedure p_goterm_not_obsolete (
 	new_mrkr_goterm.mrkrgo_go_term_zdb_id
+	),
+      execute procedure p_marker_has_goterm (
+	new_mrkr_goterm.mrkrgo_mrkr_zdb_id,
+	new_mrkr_goterm.mrkrgo_go_term_zdb_id
 	)
 );
