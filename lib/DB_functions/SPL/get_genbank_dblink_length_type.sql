9b243e294a79b3ec351af793de59514d83cb658a
Sierra Moxon <smoxon@zfin.org>
2004-04-26 17:24:54 +0000

    new proc for db_link to automatically retrieve fdbcont_zdb_id and length for genbank accession numbers.
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3785 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/get_genbank_dblink_length_type.sql
@@ -0,0 +1,62 @@
+-------------------------------------------------------------------------
+--This function checks on insert or update of db_link, the length of the
+--sequence and the type (represented by the dblink_fdbcont_zdb_id) are 
+--consistant with those values in the table accession_bank (accession_bank
+--holds sequence records updated daily from NCBI/Genbank).  In fact,
+--it replaces dblink_fdbcont_zdb_id, and dblink_length if the accession_number
+--is from Genbank.
+-------------------------------------------------------------------------
+
+  drop function get_genbank_dblink_length_type ;
+
+  create function get_genbank_dblink_length_type (vDblinkAccNum varchar(30),
+						vDblinkLength integer,
+						vDblinkFdbcontZdbId varchar(50))
+  returning varchar(50), integer ;
+
+    define vAccbkLength 	integer ;
+    define vAccbkType   	varchar(40) ;
+    define vFdbcontsType 	varchar(20) ;
+    define vFdbcontZdbID	varchar(50) ;
+
+      if exists (select * 
+                   from accession_bank 
+  		   where accbk_acc_num = vDblinkAccNum 
+                   and accbk_db_name = 'Genbank')
+
+      then 
+        
+        select accbk_length, accbk_data_type
+	  into vAccbkLength, vAccbkType
+    	  from accession_bank
+     	  where accbk_acc_num = vDblinkAccNum 
+    	  and accbk_db_name = 'Genbank' ;
+
+	if vAccbkType = 'mRNA'
+      		then 
+		  let vAccbkType = 'cDNA' ;
+   	end if ;
+   
+   	if vAccbkType = 'DNA'
+      		then 
+		  let vAccbkType = 'Genomic' ;
+
+        end if ;
+       
+        select fdbcont_zdb_id
+    	  into vFdbcontZdbId
+    	  from foreign_db_contains
+    	  where fdbcont_fdbdt_data_type = vAccbkType
+      	  and fdbcont_fdbdt_super_type = 'sequence'
+      	  and fdbcont_organism_common_name = 'Zebrafish'
+          and fdbcont_fdb_db_name =  'Genbank' ;
+
+          return vFdbcontZdbId, vAccbkLength ;
+
+      else  
+          
+	  return vDblinkFdbcontZdbId, vDblinkLength ;
+
+      end if ;
+
+end function ;
