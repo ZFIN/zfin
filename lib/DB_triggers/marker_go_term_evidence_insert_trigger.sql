9fa091dcb458836b44daad1c8805fe169a330008
Dave Clements <clements@zfin.org>
2004-05-10 18:11:21 +0000

    BUGZID: none It turns out that we still need the trigger on mrkrgoev_notes.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3829 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/marker_go_term_evidence_insert_trigger.sql
@@ -0,0 +1,7 @@
+create trigger marker_go_term_evidence_insert_trigger insert on 
+    marker_go_term_evidence referencing new as new_mrkrgoev
+    for each row
+        (
+        execute function scrub_char ( new_mrkrgoev.mrkrgoev_notes )
+                into mrkrgoev_notes
+        );
