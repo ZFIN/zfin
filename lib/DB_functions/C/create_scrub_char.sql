21063dc678b8eba8979bd6f89d6f8e506fcb7755
Dave Clements <clements@zfin.org>
2004-05-11 17:31:22 +0000

    BUGZID: none Converted both input and output parameters from varchar to lvarchar, which is what they should have been all along.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3838 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/C/create_scrub_char.sql
+++ lib/DB_functions/C/create_scrub_char.sql
@@ -1,5 +1,5 @@
-create function scrub_char(src varchar(255))
-  returning varchar(244)
+create function scrub_char(src lvarchar)
+  returning lvarchar
   with (not variant, parallelizable)
   external name
     "<!--|ROOT_PATH|-->/lib/DB_functions/scrub_char.so"
