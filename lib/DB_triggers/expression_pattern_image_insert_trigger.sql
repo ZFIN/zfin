d68598165b77ab5b531315e2ddbb741b9df9abbd
Dave Clements <clements@zfin.org>
2004-05-12 22:15:17 +0000

    BUGZID: none Converted triggers to call p_fimg_overlaps_stg_window instead of p_stg_hours_consistent.  p_fimg_overlaps_stg_window checks that the given stage window overlaps with at least one of the stage windows associated with the given fish image.  If they don't overlap, then the procedure throws an exception. p_stg_hours_consistent checks that the given stage window is consistent. However, we get that automatically from the FK back to expression_pattern_stage.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@3847 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/expression_pattern_image_insert_trigger.sql
+++ lib/DB_triggers/expression_pattern_image_insert_trigger.sql
@@ -1,6 +1,9 @@
 create trigger expression_pattern_image_insert_trigger 
   insert on expression_pattern_image
-  referencing new as new_stage
-  For each row (execute procedure p_stg_hours_consistent
-  	(new_stage.xpatfimg_xpat_start_stg_zdb_id, 
-  	new_stage.xpatfimg_xpat_end_stg_zdb_id));
+  referencing new as new_xpatfimg
+  for each row (
+    execute procedure p_fimg_overlaps_stg_window(
+	new_xpatfimg.xpatfimg_fimg_zdb_id, 
+  	new_xpatfimg.xpatfimg_xpat_start_stg_zdb_id, 
+  	new_xpatfimg.xpatfimg_xpat_end_stg_zdb_id)
+  );
--- lib/DB_triggers/expression_pattern_image_update_trigger.sql
+++ lib/DB_triggers/expression_pattern_image_update_trigger.sql
@@ -1,7 +1,10 @@
   create trigger expression_pattern_image_update_trigger update of 
   	xpatfimg_xpat_start_stg_zdb_id, xpatfimg_xpat_end_stg_zdb_id 
   on expression_pattern_image
-  referencing new as new_stage
-  for each row (execute procedure p_stg_hours_consistent
-  	(new_stage.xpatfimg_xpat_start_stg_zdb_id, 
-  	new_stage.xpatfimg_xpat_end_stg_zdb_id));
+  referencing new as new_xpatfimg
+  for each row (
+    execute procedure p_fimg_overlaps_stg_window(
+	new_xpatfimg.xpatfimg_fimg_zdb_id, 
+  	new_xpatfimg.xpatfimg_xpat_start_stg_zdb_id, 
+  	new_xpatfimg.xpatfimg_xpat_end_stg_zdb_id)
+  );
