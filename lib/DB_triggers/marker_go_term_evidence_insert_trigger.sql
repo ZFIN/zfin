c50741e74e40ef50d12879acc67d27e7a6b61f94
Sierra Moxon <smoxon@zfin.org>
2004-07-12 19:37:18 +0000

    add for Go changes
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4075 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/marker_go_term_evidence_insert_trigger.sql
+++ lib/DB_triggers/marker_go_term_evidence_insert_trigger.sql
@@ -2,8 +2,15 @@ create trigger marker_go_term_evidence_insert_trigger insert on
     marker_go_term_evidence referencing new as new_mrkrgoev
     for each row
         (
-        execute function scrub_char ( new_mrkrgoev.mrkrgoev_source_zdb_id )
-                into mrkrgoev_source_zdb_id,
-        execute function scrub_char ( new_mrkrgoev.mrkrgoev_notes )
-                into mrkrgoev_notes
-        );
+	execute procedure p_goterm_not_obsolete (
+		new_mrkrgoev.mrkrgoev_go_term_zdb_id),
+	execute procedure p_marker_has_goterm (
+			new_mrkrgoev.mrkrgoev_mrkr_zdb_id,
+   			new_mrkrgoev.mrkrgoev_go_term_zdb_id),
+        execute function scrub_char (new_mrkrgoev.mrkrgoev_notes)
+                into mrkrgoev_notes,
+	execute procedure p_insert_into_record_attribution (
+			new_mrkrgoev.mrkrgoev_zdb_id,
+			new_mrkrgoev.mrkrgoev_source_zdb_id,
+			new_mrkrgoev.mrkrgoev_mrkr_zdb_id)
+     );		
