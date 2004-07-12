5ff4d32d5cda293a070c0c26e105757d1ee10ee3
Sierra Moxon <smoxon@zfin.org>
2004-07-12 15:42:40 +0000

    new procedure for insert into recattrib from marker_go_term_evidence and/or new xpatex table.
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4039 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_insert_into_record_attribution.sql
@@ -0,0 +1,63 @@
+--p_insert_into_record_attribution.
+-----------------------------------
+--table_zdb_ids and data_zdb_ids should be attributed to pubs.
+--table_zdb_ids are specific ids generated on entry to tables (like
+--mrkrgoev_zdb_id in marker_go_Term_evidence, or xpatex_zdb_id
+--in expression_experiment.)
+--data zdb_ids are actual data values in those tables (like 
+--mrkrgoev_mrkr_zdb_id in marker_Go_Term_evidence, and 
+--xpatex_probe_feature_zdb_id in expression_experment).  These two tables
+--in particular, have 'source' in each record.  These sources should
+--also be found in record_attribution.
+--this procedure inserts 1 record into record attribution 
+--for every new table_zdb_id entered, and one into record_attribution
+--for every new combo of data_zdb_id, source. 
+
+drop procedure p_insert_into_record_attribution ;
+
+create procedure p_insert_into_record_attribution (vTableZdbId varchar(50), 
+					vSourceZdbId varchar(50),
+					vDataZdbId varchar(50))
+
+	define vOk_tableId integer ;
+	define vOk_dataId integer ;
+
+	let vOk_tableId = (select count(*)
+		     		from record_attribution
+		     		where recattrib_data_zdb_id = vTableZdbId
+		    		 and recattrib_source_zdb_id = 
+					vSourceZdbId) ;
+	if vOk_tableId > 0
+
+	then
+	  let vOk_tableId = 1  ;
+
+	else
+	   	
+	  insert into record_attribution (recattrib_data_zdb_id,
+						recattrib_source_zdb_id)
+		values (vTableZdbId, vSourceZdbId) ;
+
+	end if ;	
+
+	let vOk_dataId = (select count(*)
+				from record_attribution
+				where recattrib_data_zdb_id = 
+					vDataZdbId
+				and recattrib_source_zdb_id = 
+					vSourceZdbId
+		 	) ;
+	if vOk_dataId > 0
+
+	then
+	
+	   let vOk_dataId = 1 ;
+
+        else 
+		insert into record_attribution (recattrib_data_zdb_id,
+						recattrib_source_zdb_id)
+			values (vDataZdbId, vSourceZdbId) ;
+ 
+	end if ;   
+
+end procedure ;
