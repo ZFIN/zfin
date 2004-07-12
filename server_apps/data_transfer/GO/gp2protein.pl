3a8a9fbc4b43be5d63db4528953c0489e17d6a48
Peiran Song <peirans@zfin.org>
2004-07-12 19:18:50 +0000

    The script to generate gp2protein.zfin file.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4065 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/GO/gp2protein.pl
@@ -0,0 +1,65 @@
+#!/private/bin/perl 
+#  This script creates a file that ZFIN sends to Stanford. The file is tab 
+#  delimitted with 2 columns, each gene that has a GO annotations and its corresponding SWISS PROT#  ID.
+#  We must send the file via email to GO after running the script. A reminder
+#  email, containing the path to the file, is sent to a member of ZFIN. 
+
+use DBI;
+
+#set environment variables
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+
+### open a handle on the db
+my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
+                       '', 
+                       '', 
+		       {AutoCommit => 1,RaiseError => 1}
+		      )
+  || emailError("Failed while connecting to <!--|DB_NAME|-->"); 
+
+$dir = "<!--|ROOT_PATH|-->/server_apps/data_transfer/GO/";
+chdir "$dir";
+$outFile = "gp2protein.zfin";
+
+
+&gp2proteinReport();
+
+$dbh->disconnect();
+
+exit;
+
+#-------------------------------------------------------------
+#
+sub gp2proteinReport()
+{
+    system("/bin/rm -f $outFile");
+    open (REPORT, ">$outFile") or die "cannot open report";
+    my $cur = $dbh->prepare('select distinct mrkr_zdb_id,
+                                    dblink_acc_num
+                             from   marker,db_link, foreign_db_contains
+                             where  mrkr_zdb_id = dblink_linked_recid
+                               and  dblink_fdbcont_zdb_id = fdbcont_zdb_id 
+                               and  fdbcont_fdb_db_name = "SWISS-PROT"
+                             order by mrkr_zdb_id;'
+			   );
+    $cur->execute;
+    my($mrkr_id, $acc_num);
+    $cur->bind_columns(\$mrkr_id,\$acc_num);
+    my $gene = '';
+    while ($cur->fetch) {
+	if ($gene eq $mrkr_id) {
+	    print REPORT ";SPTR:$acc_num";
+	}
+	else {
+	    print REPORT "\nZFIN:$mrkr_id\tSPTR:$acc_num";
+	    $gene=$mrkr_id;
+	}	
+    }
+    close(REPORT);
+}
+
+
+
