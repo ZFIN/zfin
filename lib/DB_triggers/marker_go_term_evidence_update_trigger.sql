a6d087eb35301c84379d37b0d755a62e7076e8e1
Sierra Moxon <smoxon@zfin.org>
2004-07-12 15:43:27 +0000

    new trigger for go redo
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4040 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/marker_go_term_evidence_update_trigger.sql
@@ -0,0 +1,20 @@
+create trigger marker_go_term_evidence_update_trigger 
+  update of mrkrgoev_mrkr_zdb_id, mrkrgoev_go_term_zdb_id, 
+	mrkrgoev_source_zdb_id
+  on marker_go_Term_Evidence
+  referencing new as new_mrkrgoev
+    for each row
+	(
+	execute procedure p_goterm_not_obsolete (
+		new_mrkrgoev.mrkrgoev_go_term_zdb_id),
+        execute function scrub_char ( 
+			new_mrkrgoev.mrkrgoev_notes )
+                into mrkrgoev_notes,
+	execute procedure p_marker_has_goterm (
+			new_mrkrgoev.mrkrgoev_mrkr_zdb_id,
+   			new_mrkrgoev.mrkrgoev_go_term_zdb_id),
+	execute procedure p_insert_into_record_attribution (
+			new_mrkrgoev.mrkrgoev_zdb_id,
+			new_mrkrgoev.mrkrgoev_source_zdb_id,
+			new_mrkrgoev.mrkrgoev_mrkr_zdb_id)	
+     );			
