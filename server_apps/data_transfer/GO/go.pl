cd87740a0e03e69455782607b48594f9c353d651
Peiran Song <peirans@zfin.org>
2004-07-12 18:54:53 +0000

    Capture error from each step. Incorporated the gp2protein.zfin file generation script. Send error report to a tech person <!|GO_EMAIL_ERR|-->, send the two files as attachment to a curator <!|GO_EMAIL_CURATOR|-->.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4057 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GO/go.pl
+++ server_apps/data_transfer/GO/go.pl
@@ -1,10 +1,87 @@
 #!/local/bin/perl 
+#
+# have to use /local/bin/perl for MIME::Lite module
+#
 #  This script creates a file that ZFIN sends to Stanford. The file is tab 
 #  delimitted with 14 columns, each GO term/gene association on a seperate  
 #  line. 
-#  We must send the file via email to GO after running the script. A reminder
-#  email, containing the path to the file, is sent to a member of ZFIN. 
+#  One tech people would get error report if any. One curator would get 
+#  gene_association.zfin file and gp2protein.zfin file in email attachment. 
+ 
+use MIME::Lite;
 
+
+# ----------------- Send Error Report -------------
+# Parameter
+#   $    Error message 
+sub sendErrorReport ($) {
+    open (SENDMAIL, "| /usr/lib/sendmail -t -oi") || die "Cannot open mailprog!";
+    print SENDMAIL "To: <!--|GO_EMAIL_ERR|-->\n";
+    print SENDMAIL "Subject: GO file generation error\n";
+
+    print SENDMAIL "$_[0]\n"; 
+    close(SENDMAIL);
+    exit;
+}
+
+#------------------ Send Result Files----------------
+# No parameter
+#
+sub sendResults {
+	
+ #----- One mail send out the gene_association file----
+
+  my $SUBJECT="Auto: gene_association.zfin file";
+  my $MAILTO="<!--|GO_EMAIL_CURATOR|-->";
+  my $ATTFILE ="./gene_association.zfin";
+ 
+  # Create a new multipart message:
+  $msg1 = new MIME::Lite 
+    From    => "$ENV{LOGNAME}",
+    To      => "$MAILTO",
+    Subject => "$SUBJECT",
+    Type    => 'multipart/mixed';
+ 
+  attach $msg1 
+    Type     => 'application/octet-stream',
+    Encoding => 'base64',
+    Path     => "./$ATTFILE",
+    Filename => "$ATTFILE";
+
+  # Output the message to sendmail
+
+  open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
+  $msg1->print(\*SENDMAIL);
+
+  
+ #----- Another mail send out problem files ----
+
+  my $SUBJECT="Auto: gp2protein.zfin file";
+  my $MAILTO="<!--|GO_EMAIL_CURATOR|-->";     
+  my $ATTFILE = "./gp2protein.zfin";
+
+  # Create another new multipart message:
+  $msg2 = new MIME::Lite 
+    From    => "$ENV{LOGNAME}",
+    To      => "$MAILTO",
+    Subject => "$SUBJECT",
+    Type    => 'multipart/mixed';
+
+  attach $msg2 
+    Type     => 'application/octet-stream',
+    Encoding => 'base64',
+    Path     => "./$ATTFILE",
+    Filename => "$ATTFILE";
+
+  # Output the message to sendmail
+  open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
+  $msg2->print(\*SENDMAIL);
+
+  close(SENDMAIL);
+}
+
+
+#--------------- Main --------------------------------
 
 #set environment variables
 
@@ -12,7 +89,18 @@ $ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
 $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
 $ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
 $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
-print 'generating SQL';
-system ("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> gofile.sql >out 2> report.txt");
-system ("goparser.pl");
-system ("perl5 check-gene-association.pl -d gene_association.zfin");
+
+sendErrorReport ("gofile.sql failed") if
+    system ("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> gofile.sql");
+
+sendErrorReport ("goparser.pl failed") if system ("goparser.pl");
+
+sendErrorReport ("check-gene-association.pl reports error") if 
+    system ("perl5 check-gene-association.pl -d gene_association.zfin");
+
+sendErrorReport ("gp2protein.pl failed") if
+    system ("gp2protein.pl");
+
+sendResults();
+
+exit;
