b37eec3a4d5e81b0189b95f18a7a4533b9697eea
Peiran Song <peirans@zfin.org>
2004-08-13 18:14:18 +0000

    It takes ok2acc file from curators with SP acc one per line, matches against problemfile file to get ok2file and appended it to okfile for parsing and loading.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4225 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/SWISS-PROT/sp_match.pl
@@ -0,0 +1,26 @@
+#!/local/bin/perl 
+#
+# this script reads the SwissProt accession list from
+# a curator and match out the detailed records from problemfile
+# and writes it into ok2file which would be appended to okfile
+# in loadsp.pl process. 
+#
+if (@ARGV < 1) {
+    die "Please enter the accession file name. \n";
+}
+
+open ACC, "<$ARGV[0]" or die "Cannot open $ARGV[0] file";
+undef $/;
+$accfile = <ACC>;
+close ACC;
+
+open FILE, "<problemfile" or die "Cannot open problemfile";
+open OK, ">ok2file" or die "Cannot open ok2file";
+$/ = "//\n";
+while (<FILE>) {
+    foreach $acc (split /\n/,$accfile) {
+	print OK if /$acc/;
+    }
+}
+close FILE;
+close OK;
