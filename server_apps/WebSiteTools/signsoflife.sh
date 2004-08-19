7afdbdae1c9b52d4b692bdcda51f50ab873c6125
Sierra Moxon <smoxon@zfin.org>
2004-08-19 16:29:59 +0000

    added check for onback.pl to try to weed out times where ontape -c is not in the process list--also changed ontape -c to ontape as it can be ontape -s at a level 0 backup. BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4262 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/WebSiteTools/signsoflife.sh
+++ server_apps/WebSiteTools/signsoflife.sh
@@ -19,19 +19,20 @@ setenv INFORMIXSQLHOSTS ${INFORMIXDIR}/etc/<!--|SQLHOSTS_FILE|-->
 
 ############################################################################################################
 set modeon=`onstat -| tr -d '\12' | cut -f2- -d\-|cut -c3,4`
-set logon=`ps -ef | grep -c "/private/apps/Informix/informix_wildtype/bin/ontape -c"`
+set logon=`ps -ef | grep -c "/private/apps/Informix/informix_wildtype/bin/ontape"`
+set backupon=`ps -ef | grep -c "onback.pl"`
 
 if ($modeon != "On") then
     set mode="`onstat -` | $modeon"
-    echo $mode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" tomc@cs.uoregon.edu
-    echo $mode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" staylor@cs.uoregon.edu
+    echo $mode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!"  <!--|VALIDATION_EMAIL_OTHER|-->
+
 endif 
 
 if ($logon != 2) then
-     set logmode="check ontape"
-     echo $logmode |  /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" tomc@cs.uoregon.edu
-     echo $logmode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" staylor@cs.uoregon.edu
-
+    if ($backupon != 2) then
+        set logmode="check ontape"
+	echo $logmode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" <!--|VALIDATION_EMAIL_OTHER|-->
+    endif 
 #    echo $mode | /local/bin/mail -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" judys@cs.uoregon.edu
 #    echo $mode | /local/bin/mail -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" clements@cs.uoregon.edu	
 #    cd /research/zfin/users/bionixprod/ZFIN_WWW/;onmode -ky;oninit;echo ""|/private/bin/onlog.pl	
