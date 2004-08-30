dba2a62ab3fb878cb7ea60d624c7c8bff3f80014
Sierra Moxon <smoxon@zfin.org>
2004-08-30 16:18:11 +0000

    reset the logon variable in the second try of the script.
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4301 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/WebSiteTools/signsoflife.sh
+++ server_apps/WebSiteTools/signsoflife.sh
@@ -31,21 +31,21 @@ endif
 #try to make this script check a couple of times before sending an
 #email--during writing of logical logs to disk, ontape process
 #may disapear from process list--but if it disapears for longer
-#than 10 tries, we want to know about it.
+#than 6 seconds (except when backing up the db) we want to know about it.
 
 if ($backupon != 2) then
-endif
 
-if ($logon != 2) then
-    sleep 6
+  if ($logon != 2) then
+      sleep 6
+     set logon=`ps -ef | grep -c "/private/apps/Informix/informix_wildtype/bin/ontape"`
+     if ($logon != 2) then
+         set logmode="check ontape"
+         echo $logmode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" <!--|VALIDATION_EMAIL_OTHER|-->
+     endif 
 
-    if ($logon != 2) then
-        set logmode="check ontape"
-        echo $logmode | /bin/mailx -s "<!--|INFORMIX_SERVER|-->  ABNORMAL!" <!--|VALIDATION_EMAIL_OTHER|-->
-    endif 
+  endif
 
-endif
-	
+endif	
 #cd /research/zfin/users/bionixprod/ZFIN_WWW/;onmode -ky;oninit;echo ""|/private/bin/onlog.pl	
 
 
