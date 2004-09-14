96831d8d211a0500c7299922cdef51a0b158fb7f
Dave Clements <clements@zfin.org>
2004-09-14 17:16:09 +0000

    BUGZID: 196 Added orthologue update trigger to call scrub_char on orthologue name and abbrev.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4346 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/orthologue_update_trigger.sql
@@ -0,0 +1,9 @@
+create trigger orthologue_update_trigger update on 
+    orthologue referencing new as new_ortho
+    for each row
+        (
+	execute function scrub_char ( new_ortho.ortho_name )
+		into ortho_name,
+	execute function scrub_char ( new_ortho.ortho_abbrev )
+		into ortho_abbrev
+        );
