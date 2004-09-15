1c100c9c6b26bf817ddec5a8f9402277bf1765fc
Sierra Moxon <smoxon@zfin.org>
2004-09-15 19:09:28 +0000

    changed the name of the proc called by this trig BUGZID: none
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4354 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/expression_pattern_insert_trigger.sql
+++ lib/DB_triggers/expression_pattern_insert_trigger.sql
@@ -3,12 +3,13 @@ create trigger expression_pattern_insert_trigger
     referencing new as new_xpat
     for each row
         (
-	execute procedure p_insert_into_record_attribution (
+	execute procedure p_insert_into_record_attribution_tablezdbids (
 			new_xpat.xpat_zdb_id,
-			new_xpat.xpat_source_zdb_id,
-			new_xpat.xpat_probe_zdb_id),
-	execute procedure p_insert_into_record_attribution (
-			new_xpat.xpat_zdb_id,
-			new_xpat.xpat_source_zdb_id,
-			new_xpat.xpat_gene_zdb_id)
+			new_xpat.xpat_source_zdb_id),
+	execute procedure p_insert_into_record_attribution_datazdbids (
+			new_xpat.xpat_gene_zdb_id,
+			new_xpat.xpat_source_zdb_id),
+	execute procedure p_insert_into_record_attribution_datazdbids (
+			new_xpat.xpat_probe_zdb_id,
+			new_xpat.xpat_source_zdb_id)	
          );
