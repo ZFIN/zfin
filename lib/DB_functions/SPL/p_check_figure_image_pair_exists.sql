df5d1b88b45187e5b9352db7862d66c851770bfd
Sierra Moxon <smoxon@zfin.org>
2004-09-17 15:13:21 +0000

    added to repository for FX shadow tables
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4363 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_check_figure_image_pair_exists.sql
@@ -0,0 +1,27 @@
+create procedure p_check_figure_image_pair_exists (vImgZdbId varchar(50), 
+						   vFigZdbId varchar(50))
+  define vOk	integer ;
+
+  if vImgZdbId is not null 
+
+  then
+
+    let vOk = (select count(*) 
+		from FX_fish_image_private
+		where fimgp_fig_zdb_id = vFigZdbId
+		and fimgp_zdb_id = vImgZdbId) ;
+
+    if vOk > 0 then
+
+	let vOk = 1;
+
+    else 
+	raise exception -746,0,'FAIL!: image must be associated with a figure before an xpat' ;
+
+    end if ;
+
+  else let vOk = 1 ;
+
+  end if ;
+
+end procedure ;
