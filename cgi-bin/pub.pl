00498613a777c5245facb0fcd760f355b6b4f09f
Prita Mani <pm@zfin.org>
2004-10-05 18:43:29 +0000

    BUGZID: 400 Made changes per Montes suggestions
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4418 908b024a-c82b-635c-c293-f2b15b23277c

--- cgi-bin/pub.pl
+++ cgi-bin/pub.pl
@@ -1,8 +1,8 @@
 #!/usr/bin/perl
 sub pub {
+if ($_[0] ne "all") {
  print <<ENDHTML ; 
 <table width=100%>
- <!------------- Probe --------------------------------->
                 <tr>
           <td align=left colspan=2> <strong> Cunliffe, V.T. (2004) <br>
             </strong>
@@ -11,10 +11,40 @@ o maintain the production of motoneurones in response to hedgehog signalling
              Development 131(12):2983-2995. ( <a href="fxFigures.cgi">All Figures for this publication</a> ) </td>
             </tr>
                 <tr>
+          <td align=left colspan=2> <br>
+          <b><u>ABSTRACT AND ADDITIONAL INFORMATION</u></b>
+          
+            </tr>
+                <tr>
           <td align=left colspan=2> <strong><br>
-          Figures returned by search: </strong></td>
+          Figures returned by search: </strong><a href="fxViewAll.cgi">  All expression for this figure</a></td>
+          
             </tr>
         </table>
 ENDHTML
+}
+else {
+
+ print <<ENDHTML ; 
+<table width=100%>
+                <tr>
+          <td align=left colspan=2> <strong> Cunliffe, V.T. (2004) <br>
+            </strong>
+Histone deacetylase 1 is required to repress Notch target gene expression during zebrafish neurogenesis and t
+o maintain the production of motoneurones in response to hedgehog signalling
+             Development 131(12):2983-2995.
+            </tr>
+                <tr>
+          <td align=left colspan=2>
+          
+          
+            </tr>
+        </table>
+<p>
+<b><u>ABSTRACT AND ADDITIONAL INFORMATION</u></b>
+
+<p><p>
+ENDHTML
+}
 }
 1;
