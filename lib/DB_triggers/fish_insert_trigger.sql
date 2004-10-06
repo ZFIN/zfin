b81c4bbbf700701c9955ef3a8504bd155f1b5910
Dave Clements <clements@zfin.org>
2004-10-06 15:59:09 +0000

    BUGZID: 196 Added line to call scrub_char on name column.  Experiments show that the scrubbed version of name is passed to the zero_pad call on the following line.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4433 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/fish_insert_trigger.sql
+++ lib/DB_triggers/fish_insert_trigger.sql
@@ -2,6 +2,8 @@ create trigger fish_insert_trigger
   insert on fish 
     referencing new as new_fish
     for each row (
+	execute function scrub_char(new_fish.name)
+          into fish.name,
         execute function zero_pad(new_fish.name) 
 	  into fish.fish_name_order,
         execute function zero_pad(new_fish.allele) 
