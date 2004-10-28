da0407eb6eae4669ec17b7c4a579f9e2ab7a1f01
Sierra Moxon <smoxon@zfin.org>
2004-10-28 23:21:23 +0000

    have to make sure that xpatfimg_fimg_zdb_id is either not_specified or in fimg
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4505 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/p_check_figure_image_pair_exists.sql
+++ lib/DB_functions/SPL/p_check_figure_image_pair_exists.sql
@@ -6,21 +6,27 @@ create procedure p_check_figure_image_pair_exists (vImgZdbId varchar(50),
 
   then
 
-    let vOk = (select count(*) 
-		from FX_fish_image_private
-		where fimgp_fig_zdb_id = vFigZdbId
-		and fimgp_zdb_id = vImgZdbId) ;
+	if vImgZdbId != 'not specified'
 
-    if vOk > 0 then
+  	then
 
-	let vOk = 1;
+    	let vOk = (select count(*) 
+			from FX_fish_image_private
+			where fimgp_fig_zdb_id = vFigZdbId
+			and fimgp_zdb_id = vImgZdbId) ;
 
-    else 
-	raise exception -746,0,'FAIL!: image must be associated with a figure before an xpat' ;
+    		if vOk > 0 then
 
-    end if ;
+		let vOk = 1;
 
-  else let vOk = 1 ;
+    		else 
+		raise exception -746,0,'FAIL!: image must be associated with a figure before an xpat' ;
+
+    		end if ;
+
+  	else let vOk = 1 ;
+
+	end if ;
 
   end if ;
 
