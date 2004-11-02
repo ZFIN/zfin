b745a6d6455054fee697970659a1b2bb3aa2ad78
Dave Clements <clements@zfin.org>
2004-11-02 18:00:57 +0000

    BUGZID: 491 Fixed comment to indicate this actually writes to FTP, not to HTTP. Also added mkdir command to create output directory (and all its parent directories) if it does not already exist.  The mkdir is a generally good idea and makes setting up FTP directories for test servers much easier.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4520 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/MEOW/meow.pl
+++ server_apps/data_transfer/MEOW/meow.pl
@@ -1,11 +1,14 @@
 #!/local/bin/perl 
 #  Script to create files for MEOW database
-#  output files are written to <!--|ROOT_PATH|-->/home/transfer/MEOW
+#  output files are written to <!--|FTP_ROOT|-->/pub/transfer/MEOW/
 $ENV{"DBDATE"}="Y4MD-";
 $ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
 $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
 $ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
 $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+if (! -e "<!--|FTP_ROOT|-->/pub/transfer/MEOW") {
+    system("/bin/mkdir -m 755 -p <!--|FTP_ROOT|-->/pub/transfer/MEOW");
+}
 chdir "<!--|ROOT_PATH|-->/server_apps/data_transfer/MEOW";
 system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> MEOW_dump.sql");
 
