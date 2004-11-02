e6240856055ccd5d18561c8ef2b5a5c688600505
Sierra Moxon <smoxon@zfin.org>
2004-11-02 17:10:29 +0000

    FX curator interface
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4516 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_triggers/FX_expression_experiment_insert_trigger.sql
@@ -0,0 +1,14 @@
+create trigger FX_expression_experiment_insert_trigger insert on 
+    FX_expression_experiment referencing new as new_xpatex
+    for each row
+        (
+	execute procedure p_insert_into_record_attribution_datazdbids (
+			new_xpatex.xpatex_probe_feature_zdb_id,
+			new_xpatex.xpatex_source_zdb_id),
+	execute procedure p_insert_into_record_attribution_datazdbids(
+			new_xpatex.xpatex_gene_zdb_id,
+			new_xpatex.xpatex_source_zdb_id),
+	execute procedure p_insert_into_record_attribution_tablezdbids (
+			new_xpatex.xpatex_zdb_id,
+                        new_xpatex.xpatex_source_zdb_id) 
+     );	
