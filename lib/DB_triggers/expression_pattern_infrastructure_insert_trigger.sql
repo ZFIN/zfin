73033c573790997ad3872514b36d3bee1d7d64bc
Dave Clements <clements@zfin.org>
2004-11-17 16:07:28 +0000

    BUGZID: 401 Fixed typo.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4590 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/expression_pattern_infrastructure_insert_trigger.sql
+++ lib/DB_triggers/expression_pattern_infrastructure_insert_trigger.sql
@@ -1,6 +1,5 @@
-create trigger expression_pattern_infrastructure_update_trigger 
-    update of xpatinf_anatitem_zdb_id,xpatinf_expressed,xpatinf_pub_zdb_id,
-    xpatinf_curator_zdb_id on expression_pattern_infrastructure 
+create trigger expression_pattern_infrastructure_insert_trigger 
+    insert on expression_pattern_infrastructure 
     referencing new as new_xpatinf
     for each row
         (
