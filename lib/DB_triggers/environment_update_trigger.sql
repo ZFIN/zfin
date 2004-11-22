a2f2e55e62033a1f7e98654e016ecd526bb12121
Sierra Moxon <smoxon@zfin.org>
2004-11-22 16:39:51 +0000

    added scrub_char to env_name
    BUGZID: none
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4597 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/environment_update_trigger.sql
+++ lib/DB_triggers/environment_update_trigger.sql
@@ -5,5 +5,6 @@ create trigger environment_update_trigger update of env_source_zdb_id
         (
 	execute procedure p_insert_into_record_attribution_tablezdbids (
 			new_env.env_zdb_id,
-			new_env.env_source_zdb_id)
-) ;
+			new_env.env_source_zdb_id),
+	execute function scrub_char(new_env.env_name) into env_name
+	) ;
