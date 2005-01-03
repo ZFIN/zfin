aafc21cea609a13e231439c6d1102513ff454cdb
Brock Sprunge <bsprunge@zfin.org>
2005-01-03 21:56:48 +0000

    BUGZID: none Review: Judy
    Multiple request to the NCBI server return an error. Set the script to download all records as one request.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4714 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GenPept/fetch-genpept.pl
+++ server_apps/data_transfer/GenPept/fetch-genpept.pl
@@ -1,4 +1,4 @@
-#! /local/bin/perl -wT
+#! /local/bin/perl
 
 use Socket;
 init();
@@ -8,24 +8,29 @@ $db       = "Protein"; 		#getparms("Database", "Protein", "Choices: Nucleotide, 
 $term     = "txid7955"; 	#getparms("Entrez Query", "Danio+rerio[orgn]", "Choices: try also biomol+mrna[prop],biomol+genomic[prop]");
 $rettype  = "gp"; 		#getparms("Format", "gp", "Choices: fasta, gb, gp");
 $retmode  = "text"; 		#getparms("Encoding", "text", "Choices: xml, text, asn, html");
-$out_file =  "sequences.gp"; 	#getparms("Output file", "refout", "");
+$out_file =  "<!--|ROOT_PATH|-->/server_apps/data_transfer/GenPept/sequences.gp"; 	#getparms("Output file", "refout", "");
 
 open (OUT,">$out_file");
 
 #run initial search and get number of records, web environment
 
 $tool="esearch";
-$results=getUrl("$ebase$tool.fcgi?usehistory=y&db=$db&term=$term&dopt=GenPept");
+$results=getUrl("$ebase$tool.fcgi?usehistory=y&db=$db&term=$term&dopt=GenPept&email=bsprunge/@cs.uoregon.edu");
 $results=~/<Count>(\d+)<\/Count>.*<QueryKey>(\d+).*<WebEnv>(.+)<\/WebEnv/s;
 ($nrecords,$qkey,$webenv)=($1,$2,$3);
 
-#download in batches of 500 in user-specified format and write to output file
+#download in user-specified format and write to output file
 $tool="efetch";
 $ts=time;
 
+#2005-01-03 
+#download records in one request. 
+#subsequent requests receive an error message from the ncbi server.
+$retmax=$nrecords;
+
 for($retstart=0;$retstart<$nrecords;$retstart+=$retmax){
 
-    $t=time-$ts;if(3-$t>0){sleep(3-$t);}#max one call per 3 seconds for scripts at NCBI
+    #$t=time-$ts;if(3-$t>0){sleep(4-$t);}   #max one call per 3 seconds for scripts at NCBI
     $p=int($retstart/$nrecords*10000)/100; #calculate percentage of records downloaded
 
     ## uncomment to view download progress
