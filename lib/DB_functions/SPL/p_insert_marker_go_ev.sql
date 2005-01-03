a61ba6315588183dec099b311538e95cc506f2f0
Peiran Song <peirans@zfin.org>
2005-01-03 18:37:48 +0000

    BUGZID: 552 It accepts a marker zdb id, GO term zdb id, pub id and evidence code, and generates a new marker go term evidence record with contribution note of "ZFIN auto assignment". Reviewer: Sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4711 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_insert_marker_go_ev.sql
@@ -0,0 +1,32 @@
+------------------------------------------------------------------------
+-- This procedure accepts a marker zdb id, GO term zdb id, pub zdb id
+-- and evidence code, then generates a new marker go term evidence record,
+-- attributing to the "ZFIN auto assignment".
+-----------------------------------------------------------------------
+
+create procedure p_insert_marker_go_ev (mrkrZdbId	varchar(50), 
+									    goTermZdbId	varchar(50), 
+			                            pubZdbId	varchar(50),
+	                                    evCode      varchar(3))
+
+		define mrkrGOEvZdbId	varchar(50);
+
+		let mrkrGOEvZdbId = get_id("MRKRGOEV");
+	
+		insert into zdb_active_data (zactvd_zdb_id ) 
+				  	         values (mrkrGOEvZdbId);
+
+		insert into marker_go_term_evidence (mrkrgoev_zdb_id, mrkrgoev_mrkr_zdb_id,
+											 mrkrgoev_go_term_zdb_id, mrkrgoev_source_zdb_id,
+											 mrkrgoev_evidence_code, 
+											 mrkrgoev_date_entered, mrkrgoev_date_modified,
+                                             mrkrgoev_contributed_by, mrkrgoev_modified_by)
+						       values (mrkrGOEvZdbId, mrkrZdbId,
+                                       goTermZdbId, pubZdbId,
+									   evCode, 
+                                       TODAY, TODAY,
+                                       "ZFIN auto assignment", "ZFIN auto assignment");
+
+
+
+end procedure;
