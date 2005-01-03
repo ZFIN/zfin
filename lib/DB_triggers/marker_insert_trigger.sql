e21b04ee30e1643288da604ac485eaacc386412f
Peiran Song <peirans@zfin.org>
2005-01-03 18:48:06 +0000

    BUGZID: 552 Add on procedure p_populate_go_unknowns. Reviewer: Sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4712 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/marker_insert_trigger.sql
+++ lib/DB_triggers/marker_insert_trigger.sql
@@ -2,11 +2,11 @@ create trigger marker_insert_trigger insert on
     marker referencing new as new_marker
     for each row
         (
-	execute function scrub_char ( new_marker.mrkr_name )
-		into marker.mrkr_name,
-	execute function scrub_char ( new_marker.mrkr_abbrev )
-		into marker.mrkr_abbrev,
- 	execute procedure p_check_mrkr_abbrev (new_marker.mrkr_name, 
+		execute function scrub_char ( new_marker.mrkr_name )
+				into marker.mrkr_name,
+		execute function scrub_char ( new_marker.mrkr_abbrev )
+				into marker.mrkr_abbrev,
+ 		execute procedure p_check_mrkr_abbrev (new_marker.mrkr_name, 
 					       new_marker.mrkr_abbrev, 
 					       new_marker.mrkr_type),
         execute function zero_pad ( new_marker.mrkr_name )
@@ -15,5 +15,7 @@ create trigger marker_insert_trigger insert on
                 into marker.mrkr_abbrev_order,
         execute procedure mhist_event( new_marker.mrkr_zdb_id,
                 'assigned', new_marker.mrkr_name,
-                new_marker.mrkr_abbrev )
+                new_marker.mrkr_abbrev ),
+        execute procedure p_populate_go_unknowns (new_marker.mrkr_zdb_id,
+										new_marker.mrkr_name, new_marker.mrkr_type)
         );
