e8836fc14b37ac594d393a72f83603a0caba74d0
Peiran Song <peirans@zfin.org>
2005-01-12 00:15:38 +0000

    BUGZID: none The filtering value for unqualified BLAST hit is lowered from 97% to 96% on curator's request. Tere is also variable name adjustment so as to be intuitive, and alignment adjustment. Reviewed by: Not code reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4751 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GXP/filterBlast.pl
+++ server_apps/data_transfer/GXP/filterBlast.pl
@@ -6,9 +6,9 @@
 # For each query, if a hit has >=98% identity, alignment(match)  
 # lenth is over 80% of either the query sequence or subject, 
 # and the subject is cDNA sequence, that is a strong candidate.
-# If all the other hits that has >=97% identity are all of the same
-# gene, and no other hits on the same gene has a <97% identity and 
-# no other hits on different genes/BACs/PACs has a >96% identity. then 
+# If all the other hits that has >=96% identity are all of the same
+# gene, and no other hits on the same gene has a <96% identity 
+# (no other hits on different genes/BACs/PACs has a >96% identity). then 
 # output the clone gene assoication into is_gene.unl file. Otherwise, 
 # output to blast2zfin.scnd for manual curation.  
 #
@@ -20,7 +20,13 @@
 #
 use DBI;
 
-die "Usage: filterBlast.pl zfindbname < tableformatblastresultdata.\n" if (@ARGV < 1);
+my $usage =<<END;
+Usage: 
+	filterBlast.pl zfindbname < tableformatblastresultdata
+  output: is_gene.unl blast2zfin.scnd
+END
+
+die "$usage\n" if (@ARGV < 1);
 
 #=====================================================
 # Main
@@ -36,42 +42,43 @@ open ISGENE, ">is_gene.unl" or die "Cannot open is_gene.unl file for write.";
 open OUT, ">blast2zfin.scnd" or die "Cannot open blast2zfin.scnd file for write.";
  
 my $lastQuery = '';
-my $numSuspects = 0;
-my $hasGreatMatch = 0;
-my (@rowArray, $isGene, $matchGeneZdbId, @matchGeneZdbIdArray);
+my $hasSuspect = 0;
+my $hasGreatSbj = 0;
+my (@rowArray, $isGene, $theGeneZdbId, @goodSbjMrkrZdbIdListArray);
 
 print OUT "QUERY|SBJCT|ZFIN|IDENTITY|LENTH|MISMCH|QSTART|QEND|SSTART|SEND|EXPECT|SCORE \n";
 while (<>) {
     s/\|$//;                 # clean ending
     my ($query, $subject, $percent,$mchLength,$qryLength,
 	    $sbjLength, $mismatch, $gap,@restOneRow)= split /\|/;
+
+	output () if ($query ne $lastQuery && $lastQuery);
+
     my $sbjType = pop @restOneRow;
     chomp $sbjType;
-    my $mrkr_zdb_id = queryAccession($subject);
 
-    output () if ($query ne $lastQuery && $lastQuery);
+    my $sbjMrkrZdbIdList = queryAccession($subject);
 
-    if ($percent >= 97) {
+    # good subject is expected to be on the same gene
+    if ($percent >= 96) {
 	
-		push @matchGeneZdbIdArray, $mrkr_zdb_id;
+		push @goodSbjMrkrZdbIdListArray, $sbjMrkrZdbIdList;
 		
+		# identify a great match
 		my $shrtLength = ($qryLength > $sbjLength) ? $sbjLength : $qryLength;
 		if ($percent >= 98 && $sbjType eq "cdna" && $mchLength/$shrtLength > 0.8 ) {
 			
-			$hasGreatMatch = 1;
-			$matchGeneZdbId = $mrkr_zdb_id;
-			$isGene = "$query|$mrkr_zdb_id|";
+			$hasGreatSbj = 1;
+			$theGeneZdbId = $sbjMrkrZdbIdList;      # cdna type guarantee one gene in the list
+			$isGene = "$query|$sbjMrkrZdbIdList|";
 		}
     }
-    elsif ($percent >= 96) {
-		$numSuspects ++;
-	}
-	elsif ($hasGreatMatch && $mrkr_zdb_id ne $matchGeneZdbId) {
-		$numSuspects ++;
+	elsif ($sbjMrkrZdbIdList eq $theGeneZdbId) {
+		$hasSuspect = 1;
 	}
 
     $lastQuery = $query;
-    push @rowArray, join("|",$query, $subject,$mrkr_zdb_id,$percent,$mchLength,$mismatch,@restOneRow);
+    push @rowArray, join("|",$query, $subject,$sbjMrkrZdbIdList,$percent,$mchLength,$mismatch,@restOneRow);
 }
 output ();
 
@@ -79,16 +86,19 @@ close OUT;
 close ISGENE;
 exit;
 
-#======================================
+#=========================================================
 #  subfunction queryAccession  
 #
-#  query against zfin database for the 
-#  gene zdb id or BAC/PAC zdb id of the 
-#  input accession number
+#  Query zfin database for matching genes and/or  
+#  BACs/PACs of the input accession number. Matching
+#  ESTs/cDNAs are converted to the correspondent genes.
 #
-#  input: accession number
-#  return: matching zdb id list
+#  Input: accession number
+#
+#  Return: matching GENE/BAC/PAC zdb id list separated by space
 #   
+#  Output: none
+#
 sub queryAccession ($) {
     my $accession = shift;
     my (@match, $match_id, $match_gene_id);
@@ -103,41 +113,45 @@ sub queryAccession ($) {
     $sth->execute();
     
     while ($match_id = $sth->fetchrow_array) {
-	if ($match_id =~ /[EST|CDNA]/) {
-	    ($match_gene_id) = $dbh->selectrow_array(
-				   "select mrel_mrkr_1_zdb_id
+		if ($match_id =~ /[EST|CDNA]/) {
+			($match_gene_id) = $dbh->selectrow_array(
+								   "select mrel_mrkr_1_zdb_id
                                       from marker_relationship
                                      where mrel_mrkr_2_zdb_id = '$match_id'
-	                               and mrel_type = 'gene encodes small segment';
+	                                   and mrel_type = 'gene encodes small segment';
                                    ");	    
-	}	
-	push @match, $match_gene_id ? $match_gene_id : $match_id ;
-	
+		}	
+		push @match, $match_gene_id ? $match_gene_id : $match_id ;
+		
     }
     return join (" ", @match);
 }
 
 #===============================
-#  subfunciton output 
+# subfunciton output 
 #
+# Input: 
+#    global variable @goodSbjMrkrZdbIdListArray, $theGeneZdbId
+#
+
 sub output () {
 
-    my $matchGeneUniq = 1;
-    foreach my $item (@matchGeneZdbIdArray) {
-		$matchGeneUniq = 0 if $item ne $matchGeneZdbId;
+    my $goodSbjUniq = 1;
+    foreach my $goodSbj (@goodSbjMrkrZdbIdListArray) {
+		$goodSbjUniq = 0 if $goodSbj ne $theGeneZdbId;
     }
 
-    if ($hasGreatMatch && $matchGeneUniq && !$numSuspects) {
+    if ($hasGreatSbj && $goodSbjUniq && !$hasSuspect) {
 		print ISGENE "$isGene\n";
     }
     else {
 		print OUT join ("\n", @rowArray)."\n";
     }
  
-    $numSuspects = 0;
-    $hasGreatMatch = 0;
-    $matchGeneZdbId = "";
-    @matchGeneZdbIdArray = ();
+    $hasSuspect = 0;
+    $hasGreatSbj = 0;
+    $theGeneZdbId = "";
+    @goodSbjMrkrZdbIdListArray = ();
     $isGene = "";
     @rowArray = ();
 }
