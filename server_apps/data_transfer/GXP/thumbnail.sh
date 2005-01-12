470c250d00228c2c863d9c56f970e4408ad8b6e6
Peiran Song <peirans@zfin.org>
2005-01-12 00:35:15 +0000

    BUGZID: none The output is changed from STDOUT to be the third input parameter, so that in the case the cjepg program fails, the exit value could be captured and used to form error message. The usage message is updated as well. Reviewed by: Not code reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4755 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GXP/thumbnail.sh
+++ server_apps/data_transfer/GXP/thumbnail.sh
@@ -1,27 +1,35 @@
-#! /bin/csh 
+#!/bin/tcsh 
 # takes an image in and writes a thumbnail out
 # reduces the height to 'given' pixels 
 # and lets the width float
 
-if ($2 == "") then
-	echo "usage: thumbnail.sh int_height in_name > out_name"
+if ($#argv < 3) then
+	echo "usage: thumbnail.sh int_height in_name out_name"
 	exit 1
 endif
-
 
 
 if ( ($2:e == "jpg") || ($2:e == "jpeg") ) then
 	/local/apps/jpeg/bin/djpeg -pnm $2 |\
 	/local/apps/netpbm/bin/pnmscale -h $1 |\
-	/local/apps/jpeg/bin/cjpeg
+	/local/apps/jpeg/bin/cjpeg > $3
+	if ($? != 0 ) then
+		echo "Error: " $2 " thumbnail generation"
+ 	endif 
 else if ($2:e == "gif") then
 	/local/apps/netpbm/bin/giftopnm $2 |\
 	/local/apps/netpbm/bin/pnmscale -h $1 |\
-	/local/apps/netpbm/bin/ppmtogif
+	/local/apps/netpbm/bin/ppmtogif > $3
+	if ($? != 0 ) then
+		echo "Error: " $2 " thumbnail generation"
+ 	endif
 else if ($2:e == "png") then
 	/local/apps/netpbm/bin/pngtopnm $2 |\
 	/local/apps/netpbm/bin/pnmscale -h $1 |\
-	/local/apps/netpbm/bin/pnmtopng
+	/local/apps/netpbm/bin/pnmtopng > $3
+   	if ($? != 0 ) then
+		echo "Error: " $2 " thumbnail generation"
+ 	endif 
 else
 	# may be worth it to find image type with 'file 
 	# if the extension is buggy
