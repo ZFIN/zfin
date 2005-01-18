4521541717bf5640913fb2063935565f662b6e8c
Peiran Song <peirans@zfin.org>
2005-01-18 22:04:51 +0000

    BUGZID: 484 Add check on stage update to ensure the change wouldn't violate stage rule on any of the anatomy item's current anatomy relationship. Reviewed by: Dave
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4782 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/anatomy_items_update_trigger.sql
+++ lib/DB_triggers/anatomy_items_update_trigger.sql
@@ -7,13 +7,19 @@ create trigger anatomy_items_update_trigger
   update on anatomy_item
     referencing new as new_anatomy_item
     for each row (
-        execute procedure p_stg_hours_consistent
-  	  (new_anatomy_item.anatitem_start_stg_zdb_id, 
-           new_anatomy_item.anatitem_end_stg_zdb_id),
-        execute function 
-	  zero_pad(new_anatomy_item.anatitem_name) into anatitem_name_order,
-	execute function 
-	  lower(new_anatomy_item.anatitem_name) into anatitem_name_lower,
-        execute function 
-	  lower(new_anatomy_item.anatitem_abbrev) into anatitem_abbrev_lower
+		execute procedure p_stg_hours_consistent
+  	  		(new_anatomy_item.anatitem_start_stg_zdb_id, 
+           		 new_anatomy_item.anatitem_end_stg_zdb_id),
+
+		execute procedure p_check_anat_anatrel_stg_consistent
+			(new_anatomy_item.anatitem_zdb_id, 
+			 new_anatomy_item.anatitem_start_stg_zdb_id,
+			 new_anatomy_item.anatitem_end_stg_zdb_id),
+        
+        	execute function 
+	  		zero_pad(new_anatomy_item.anatitem_name) into anatitem_name_order,
+		execute function 
+	  		lower(new_anatomy_item.anatitem_name) into anatitem_name_lower,
+        	execute function 
+	  		lower(new_anatomy_item.anatitem_abbrev) into anatitem_abbrev_lower
     );
