96a104b7db6d889d720702017904154d620766ee
Sierra Moxon <smoxon@zfin.org>
2005-01-20 18:37:08 +0000

    BUGZID: 201 Reviewed by: Not code reviewed commented out image rsync for shorter testing
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4802 908b024a-c82b-635c-c293-f2b15b23277c

--- cgi-bin/rsync.pl
+++ cgi-bin/rsync.pl
@@ -4,9 +4,9 @@
 # DESCRIPTION: This file syncs production and development image/pdf
 # file systems.
 
-system ("cd /research/zprod/loadUp/imageLoadUp/") ;
+#system ("cd /research/zprod/loadUp/imageLoadUp/") ;
 
-system("/local/bin/rsync -auv --temp-dir=/research/zcentral/loadUp/imageLoadUp --delete --ignore-existing --backup-dir=/tmp/staylor --suffix=.del /research/zprod/loadUp/imageLoadUp/ /research/zcentral/loadUp/imageLoadUp") ;
+#system("/local/bin/rsync -auv --temp-dir=/research/zcentral/loadUp/imageLoadUp --delete --ignore-existing --backup-dir=/tmp/staylor --suffix=.del /research/zprod/loadUp/imageLoadUp/ /research/zcentral/loadUp/imageLoadUp") ;
 
 system ("cd /research/zprod/loadUp/PDFLoadUp/") ;
 
