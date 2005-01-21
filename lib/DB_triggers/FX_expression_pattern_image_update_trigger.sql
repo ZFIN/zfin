7c25721cbf5392bb275d19af4fe0242a4d168a06
Sierra Moxon <smoxon@zfin.org>
2005-01-21 16:26:56 +0000

    BUGZID: none Reviewed by: Not code reviewed
    added scrub char function to panel_designation
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4811 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/FX_expression_pattern_image_update_trigger.sql
+++ lib/DB_triggers/FX_expression_pattern_image_update_trigger.sql
@@ -1,10 +1,14 @@
 create trigger FX_expression_pattern_image_update_trigger
-   update of xpatfimg_fimgp_zdb_id, xpatfimg_fig_zdb_id
+   update of xpatfimg_fimgp_zdb_id, 
+	     xpatfimg_fig_zdb_id, 
+ 	     xpatfimg_panel_designation
    on fx_expression_pattern_image
    referencing new as new_xpatfimg
    for each row 
 	(
 	execute procedure p_check_figure_image_pair_exists(
 		new_xpatfimg.xpatfimg_fimgp_zdb_id,
-                new_xpatfimg.xpatfimg_fig_zdb_id)
+                new_xpatfimg.xpatfimg_fig_zdb_id),
+	execute function scrub_char(new_xpatfimg.xpatfimg_panel_designation) 
+		into xpatfimg_panel_designation
 ) ;
