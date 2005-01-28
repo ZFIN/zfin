91e70a47de7d0b86085b2620cdc1985ed3005120
Sierra Moxon <smoxon@zfin.org>
2005-01-28 02:00:45 +0000

    BUGZID: 401 Reviewed by: Not code reviewed table removed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4895 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/environment_condition_update_trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger environment_condition_update_trigger
-   update of envcond_comments 
-   on environment_condition
-   referencing new as new_envcond
-   for each row 
-	(
-	execute function scrub_char(new_envcond.envcond_comments) 
-		into envcond_comments
-) ;
