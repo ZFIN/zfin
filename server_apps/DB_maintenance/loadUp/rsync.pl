391087993a74ba7cc11aa215fd21066984abb036
Sierra Moxon <smoxon@zfin.org>
2005-02-01 15:51:38 +0000

    BUGZID: 201 Reviewed by: Not code reviewed Added new directory for rsync stuff
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@4954 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/DB_maintenance/loadUp/rsync.pl
@@ -0,0 +1,33 @@
+#!/private/bin/perl -w
+
+# FILE: rysnc.pl
+# DESCRIPTION: This file syncs production and development image/pdf
+# file systems. syncing means adding files to development that are missing
+# when compared to the production file system, and deleting files
+# from the development file system when they are not available on production.
+
+# INPUT VARS: none
+# OUTPUT: adds or deletes files from embryonix/development machine
+
+# see man rsync for additional details!
+#
+# -u : update only (don't overwrite newer files)
+# -p : preserve permissions on files that get copied
+# --delete: delete files that don't exist on sender.
+# --ignore-existing: ignore files that already exist on receiver
+#
+# /research/zprod/loadUp/imageLoadUp is production 
+# /research/zcentral/loadUp/imageLoadUp is development
+#
+# we do not need to provide server names in this case because
+# our file systems are nfs mounted on each other.
+
+system ("cd <!--|LOADUP_FULL_PATH|--><!--|IMAGE_LOAD|-->");
+
+system("/local/bin/rsync -up --delete --ignore-existing <!--|LOADUP_FULL_PATH|--><!--|IMAGE_LOAD|--> /research/zcentral/loadUp/imageLoadUp");
+
+system ("cd <!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|--> ");
+
+system("/local/bin/rsync -up --delete --ignore-existing <!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|--> /research/zcentral/loadUp/PDFLoadUp");
+
+exit;
