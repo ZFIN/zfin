d02add5818c234b0901a4b9ec25dda1e43eaadf0
Brock Sprunge <bsprunge@zfin.org>
2005-03-01 22:53:56 +0000

    BUGZID: 503 Reviewed by: Prita Move from LocusLink to Entrez.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5150 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseLoc2RefintoUNL.pl
+++ server_apps/data_transfer/RefSeq/parseLoc2RefintoUNL.pl
@@ -1,12 +1,12 @@
 #! /local/bin/perl
 
 
-open (CUT, "cut -f 1,2,5,6 loc2ref |") or die "cut loc2ref failed";
+open (CUT, "cut -f 1,2,4,6 loc2ref |") or die "cut loc2ref failed";
 open (UNL, ">loc2ref.unl") or die "cannot open loc2ref.unl";
 
 while ($line = <CUT>) {
   chop $line;
-  ($ll_id,$nm_acc,$np_acc,$org_id) = split ' ',$line,4;
+  ($org_id,$ll_id,$nm_acc,$np_acc) = split ' ',$line,4;
   if ($ll_id =~ /[0-9]*/ && $org_id == 7955) {
     if($nm_acc =~ /(.*)\./){$nm_acc = $1;}
     if($np_acc =~ /(.*)\./){$np_acc = $1;}
--- server_apps/data_transfer/RefSeq/parseloc2acc.pl
+++ server_apps/data_transfer/RefSeq/parseloc2acc.pl
@@ -1,15 +1,17 @@
 #! /local/bin/perl
 
 
-open (CUT, "cut -f 1,2,5,6 loc2acc |") or die "cut loc2acc failed";
+open (CUT, "cut -f 1,2,4,6 loc2acc |") or die "cut loc2acc failed";
 open (UNL, ">loc2acc.unl") or die "cannot open loc2acc.unl";
 
 while ($line = <CUT>) {
   chop $line;
-  ($ll_id,$ll_genbank,$ll_genpept,$org) = split ' ',$line,4;
+  ($org,$ll_id,$ll_genbank,$ll_genpept) = split ' ',$line,4;
   if ($ll_id =~ /[0-9]*/ && $org == 7955) {
-    while($ll_genbank =~ /\./){chop $ll_genbank;}
-    while($ll_genpept =~ /\./){chop $ll_genpept;}
+    while($ll_genbank =~ /(.*)\./){$ll_genbank = $1;}
+    while($ll_genpept =~ /(.*)\./){$ll_genpept = $1;} 
+    if ($ll_genbank eq ""){$ll_genbank = "-";}
+    if ($ll_genpept eq ""){$ll_genpept = "-";}
     print UNL "$ll_id|$ll_genbank|$ll_genpept|\n";
   }
 }
