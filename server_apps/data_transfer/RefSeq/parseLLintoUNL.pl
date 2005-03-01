f4c874e59411902ff0a2275b9c8305c7c281d78d
Brock Sprunge <bsprunge@zfin.org>
2005-03-01 22:51:37 +0000

    BUGZID: 503 Reviewed by: Prita Move from LocusLink to Entrez.
    One file contains all the organisms. Parse Zebrafish, Human, and Mouse
    into their own files.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5148 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
+++ server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
@@ -1,19 +1,29 @@
 #! /local/bin/perl
 
 
-open (CUT, "cut -f 1,9 LL.out_dr |") or die "cut LL.out_dr failed";
-open (UNL, ">ll_id.unl") or die "cannot open ll_id.unl";
+open (CUT, "cut -f 1,2,3,4,6 gene_info |") or die "cut LL.out_dr failed";
+open (UNLDR, ">ll_id.unl") or die "cannot open ll_id.unl";
+open (UNLHS, ">ll_hs_id.unl") or die "cannot open ll_hs_id.unl";
+open (UNLMM, ">ll_mm_id.unl") or die "cannot open ll_mm_id.unl";
 
 while ($line = <CUT>) {
   chop $line;
-
-  ($ll_id,$zdb_id) = split ' ',$line,2;
-  if ($zdb_id ne "") {
-    print UNL "$ll_id|$zdb_id|\n";
+  ($org,$ll_id,$abbrev,$org_id,$alias) = split ' ',$line,5;
+  
+  if ($org eq "7955" && $org_id =~ "^ZDB") {
+    print UNLDR "$ll_id|$org_id|\n";
+  }  
+  if ($org eq "9606" && $org_id ) {
+    print UNLHS "$ll_id|$abbrev|$org_id|\n";
+  }
+  if ($org eq "10090" && $org_id ) {
+    print UNLMM "$ll_id|$org_id|$abbrev|\n";
   }
 }
 
-close (UNL);
+close (UNLMM);
+close (UNLHS);
+close (UNLDR);
 close (CUT);
 
 
