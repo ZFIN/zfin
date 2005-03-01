aaf6f145476136bbe90d831f44e59e851eb22d37
Brock Sprunge <bsprunge@zfin.org>
2005-03-01 22:53:23 +0000

    BUGZID: 503 Reviewed by: Prita Merge the parsing of Human and Mouse in with Zebrafish. <parseLLintoUNL.pl>
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5149 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseRefSeq.pl
+++ server_apps/data_transfer/RefSeq/parseRefSeq.pl
@@ -1,8 +1,6 @@
 #! /local/bin/perl
 
 system("parseLLintoUNL.pl");
-system("parseLL_HSintoUNL.pl");
-system("parseLL_MMintoUNL.pl");
 system("parseloc2UG.pl");
 system("parseloc2acc.pl");
 system("parseloc2acclen.pl");
