b4471ab2f89b692c63d5936a9c5a4b1e1cd2df91
Peiran Song <peirans@zfin.org>
2005-04-26 21:41:44 +0000

    BUGZID: none Query the anatomy_display table for staged anatomy display. Reviewed by: to be viewed by Prita
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5473 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/Downloads/generateStagedAnatomy.pl
@@ -0,0 +1,84 @@
+#!/private/bin/perl -w
+#
+# This script queries anatomy_display table mainly to generate
+# staged anatomy terms with relationship structure. The output 
+# is writen to file at public download site. Two terms, unspecified 
+# and cell division/proliferation are excluded on request. Before
+# there is a better solution like database definition, hardcode. 
+#
+# INPUT:
+#     none
+# OUTPUT:
+#     <!--|ROOT_PATH|-->/home/data_transfer/Downloads/anatomy_structure.txt
+#
+
+use strict;
+use DBI;
+
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+
+my ($stgZdbId, $lastStgZdbId, $stgName, $anatName, $dispIndent);
+my $mailprog = '/usr/lib/sendmail -t -oi -oem';
+my $output = "<!--|ROOT_PATH|-->/home/data_transfer/Downloads/staged_anatomy.txt";
+
+open (OUT,">$output") or &emailError ("Can not open $output to write");
+
+my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
+                       '', '', 
+                       {AutoCommit => 1,RaiseError => 1}
+                      )
+    || &emailError("Failed while connecting to <!--|DB_NAME|--> "); 
+
+$lastStgZdbId = "";
+
+my $sql = "select stg_zdb_id, stg_name, anatitem_name, anatdisp_indent
+             from anatomy_display
+                  join stage on anatdisp_stg_zdb_id = stg_zdb_id
+                  join anatomy_item on anatdisp_item_zdb_id = anatitem_zdb_id
+            where anatitem_zdb_id not in ('ZDB-ANAT-041102-1', 'ZDB-ANAT-010921-520')
+         order by stg_hours_start, anatdisp_seq_num;";
+
+my $sth = $dbh->prepare($sql);
+$sth->execute;
+$sth->bind_columns(\$stgZdbId, \$stgName, \$anatName, \$dispIndent);
+
+while ($sth->fetch) {
+
+    if (!$lastStgZdbId || ($stgZdbId ne $lastStgZdbId)) {
+	print OUT "\n" if $lastStgZdbId;  # additional break between stages
+	print OUT "$stgName\n";
+    }
+    $lastStgZdbId = $stgZdbId;
+
+    for (my $i = $dispIndent; $i > 0; $i--) {
+	print OUT "\t";
+    }
+    print OUT "$anatName\n";
+}
+
+exit;
+
+#=========================================
+# emailError
+#
+# INPUT:
+#    string ::  error message
+# OUTPUT:
+#    none
+# EFFECT:
+#    error message is sent to db owner.
+#
+
+sub emailError($)
+  {
+    open(MAIL, "| $mailprog") || die "Cannot open mailprog $mailprog";
+    print MAIL "To: <!--|DB_OWNER|-->\@cs.uoregon.edu";
+    print MAIL "Subject: generateStageAnatomy.pl error\n";
+    print MAIL "$_[0]";
+    close MAIL;
+    exit;
+  }
+
