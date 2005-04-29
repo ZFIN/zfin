eb56ea87af953c4fb8f761edf9b3125375402bc6
Brock Sprunge <bsprunge@zfin.org>
2005-04-29 23:41:02 +0000

    BUGZID: 93 Reviewed by: Tom
    Some records have two MGI numbers. Use the first alias as the abbrev for those records.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5518 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
+++ server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
@@ -1,23 +1,29 @@
 #! /local/bin/perl
 
 
-open (CUT, "cut -f 1,2,3,4,6 gene_info |") or die "cut LL.out_dr failed";
+open (CUT, "cut -f 1,2,3,4,5,6 gene_info |") or die "cut LL.out_dr failed";
 open (UNLDR, ">ll_id.unl") or die "cannot open ll_id.unl";
 open (UNLHS, ">ll_hs_id.unl") or die "cannot open ll_hs_id.unl";
 open (UNLMM, ">ll_mm_id.unl") or die "cannot open ll_mm_id.unl";
 
 while ($line = <CUT>) {
   chop $line;
-  ($org,$ll_id,$abbrev,$org_id,$alias) = split ' ',$line,5;
+  ($org,$ll_id,$abbrev,$org_id,$alias1,$alias2) = split ' ',$line,6;
   
+
   if ($org eq "7955" && $org_id =~ "^ZDB") {
     print UNLDR "$ll_id|$org_id|\n";
   }  
-  if ($org eq "9606" && $org_id ) {
+  elsif ($org eq "9606" && $org_id ) {
     print UNLHS "$ll_id|$abbrev|$org_id|\n";
   }
-  if ($org eq "10090" && $org_id ) {
-    print UNLMM "$ll_id|$org_id|$abbrev|\n";
+  elsif ($org eq "10090" && $org_id ) {
+    if ($abbrev =~ /^MGI:/) 
+    { 
+      $alias1 =~ s/\|/,/g;
+      $abbrev = $alias1;
+    }    
+    print UNLMM "$ll_id|$abbrev|$org_id|\n";
   }
 }
 
