59268def32fcaecd932e83811952aeac91719e06
Tom Conlin <tomc@zfin.org>
2005-04-29 00:02:45 +0000

    BUGZID: 555 Reviewed by: Brock Sprunger
    moved ZGC/zcgCount.pl up one dir to ./Count.pl
    gave it the correct /private/bin/perl sh-bang line
    
    changed the path for the ZGC sql file path to include dir
    
    copied the unparamererised calls to also duplicate their effect
    on the VegaCount.sql
    
    added Count.pl and the Vega dir to the Makefile
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5510 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/Reports/Count.pl
@@ -0,0 +1,67 @@
+#! /private/bin/perl -w 
+#
+# This monthly-run script queries the statistics on 
+# ZFIN nomenclature and expression pattern related 
+# to the ZGC project. The result is mailed to curators.
+#
+use strict;
+use MIME::Lite;
+
+#------------------ Send Result ----------------
+# 
+#
+sub sendResult ($$$){
+		
+  my $SUBJECT=$_[0];
+  my $MAILTO=$_[1];
+  my $TXTFILE=$_[2];
+ 
+  # Create a new multipart message:
+  my $msg = new MIME::Lite 
+    From    => "$ENV{LOGNAME}",
+    To      => "$MAILTO",
+    Subject => "$SUBJECT",
+    Type    => 'multipart/mixed';
+ 
+  attach $msg 
+   Type     => 'text/plain',   
+   Path     => "$TXTFILE";
+
+  # Output the message to sendmail
+
+  open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
+  $msg->print(\*SENDMAIL);
+
+  close(SENDMAIL);
+}
+
+#------------------ Main -------------------------
+# set environment variables
+
+$ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
+$ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
+$ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
+$ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
+
+chdir "<!--|ROOT_PATH|-->/server_apps/Reports/ZGC";
+
+system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> zgcCount.sql > zgcStatistics 2> err");
+
+&sendResult("Monthly ZGC statistics", "sramacha\@uoneuro.uoregon.edu, van_slyke\@uoneuro.uoregon.edu", "./zgcStatistics");
+
+&sendResult("Monthly ZGC statistics Err", "peirans\@cs.uoregon.edu", "./err");
+
+
+#-----------------------------------------------------------------------
+
+chdir "<!--|ROOT_PATH|-->/server_apps/Reports/Vega";
+
+system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> VegaCount.sql > VegaStatistics 2> err");
+
+&sendResult("Monthly Vega statistics", "sramacha\@uoneuro.uoregon.edu, ksf\@cs.uoregon.edu", "./VegaStatistics");
+
+&sendResult("Monthly Vega statistics Err", "tomc\@cs.uoregon.edu", "./err");
+
+#-------------------------------------------------------------------------- 
+
+exit;
