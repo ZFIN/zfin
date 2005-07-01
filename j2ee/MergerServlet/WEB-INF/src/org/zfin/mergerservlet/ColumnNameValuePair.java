1487f3aaacd7d7c3d83e8f9376dadf8e7fc189a4
Dave Clements <clements@zfin.org>
2005-07-01 17:43:18 +0000

    BUGZID: 53 Reviewed by: Not reviewed Added objectType and valueName attributes.  These are used if the value of the column is a ZDB ID.  Added show() method to display the column name value pair back to the user in HTML.  show() displays the value with links to its home app page, if the value is a ZDB ID and has a home page.
    Having a method in this class that is HTML aware seems to violate the
    MVC separation.  However, it also seems like a logical place to put it.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5803 908b024a-c82b-635c-c293-f2b15b23277c

--- j2ee/MergerServlet/WEB-INF/src/org/zfin/mergerservlet/ColumnNameValuePair.java
+++ j2ee/MergerServlet/WEB-INF/src/org/zfin/mergerservlet/ColumnNameValuePair.java
@@ -6,7 +6,8 @@ import java.util.*;
 
 /**
  * Defines a column name / value pair.  That is, what is the name of the 
- * column and what is its value.
+ * column and what is its value.  If the value is a ZDB ID, then we also
+ * store additional information about the value.
  */
 
 class ColumnNameValuePair
@@ -15,7 +16,9 @@ class ColumnNameValuePair
      * ATTRIBUTES
      * -------------------------------------------------------------------- */
 
-    /** Column name */
+    /**
+     * Column name
+     */
     private String columnName;
 
     /**
@@ -24,6 +27,30 @@ class ColumnNameValuePair
      */
     private Object columnValue;
 
+    /**
+     * If value of column is a ZDB ID then also save the ZDB object type of 
+     * the ZDB ID.  This is null if value is not a ZDB ID.
+     */
+    private ZdbObjectType objectType;
+
+
+
+    /**
+     * This is the result of calling get_obj_name(columnValue), if columnValue
+     * is a ZDB ID.  If the ZDB ID exists in the database then this will 
+     * always have a value, although it may just be the ZDB ID itself.
+     *
+     * For ZDB IDs this string is used to display a meaningful name with the
+     * ZDB ID.
+     *
+     * :TODO: Not sure if the output of get_obj_name() is what we want here.
+     *        Historically, get_obj_name() returned names for things that
+     *        clearly had names, like markers, and returned ZDB IDs for things
+     *        that lacked well defined names, such as marker_relationships.
+     */
+    private String valueName;
+
+
 
     /* -------------------------------------------------------------------- 
      * CONSTRUCTORS
@@ -31,17 +58,34 @@ class ColumnNameValuePair
 
     /**
      * Constructor.  Given a column name and its value, construct a column name
-     * / value pair.
+     * / value pair.  If the value is a ZDB ID then save additional information
+     * about the value.
      *
+     * @param db      Database that column name / value pair exists in.
      * @param colName Name of column
      * @param colVal  Value of column
      */
 
-    public ColumnNameValuePair (String colName,
+    public ColumnNameValuePair (MergerDatabase db,
+				String colName,
 				Object colVal)
+	throws SQLException
     {
 	columnName = colName;
 	columnValue = colVal;
+	objectType = null;
+	String colValString = colVal.toString();
+	valueName = colValString;
+
+	// if column value is a ZDB ID then look up and save the object
+	// type and name of the object it identifies.
+	if (columnValue.getClass().getName().equals("java.lang.String")) {
+	    objectType = db.getMetadata().getObjectTypeFromZdbId(colValString);
+	    if (null != objectType) {
+		// We have a ZDB ID, get object name
+		valueName = db.getNameFromZdbId(colValString);
+	    }
+	}
 	return;
     }
 
@@ -84,4 +128,26 @@ class ColumnNameValuePair
     {
 	return "(" + columnName + " = " + columnValue + ")";
     }
+
+
+    /**
+     * Show the column name value pair as HTML, possibly with links.
+     */
+    public String show()
+    {
+	String outString;
+
+	if (null == objectType || null == objectType.getAppPage()) {
+	    outString = valueName;
+	}
+	else {
+	    outString = 
+		"<a href=\"/cgi-bin/webdriver?MIval=" + 
+		objectType.getAppPage() +
+		"&OID=" + columnValue.toString() + "\">" +
+		valueName + " (" + columnValue.toString() +")</a>";
+	}
+	return outString;
+    }
+
 }
