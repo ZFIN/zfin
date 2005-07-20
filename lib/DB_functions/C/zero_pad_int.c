31bf15352b2378a64eecc21862dc17ba545e9abf
Peiran Song <peirans@zfin.org>
2005-07-20 19:44:16 +0000

    Catch negative inputs and throw exception.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5866 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/C/zero_pad_int.c
+++ lib/DB_functions/C/zero_pad_int.c
@@ -16,27 +16,34 @@
 #define		EXCEPTION(msg)	mi_db_error_raise (NULL, MI_EXCEPTION, msg)
 
 mi_lvarchar *
-zero_pad_int (unsigned int unpaddedInt, unsigned short int width)
+zero_pad_int (int unpaddedInt, short int width)
 {
   char outputString[width + 1];
   int i;   /* index for array outputString[] */
 
-  /* initalize the array for safety */
-  for ( i = 0; i <= width; i++ )
-    outputString[i] = '\0';
+  if (unpaddedInt < 0)
+    EXCEPTION ("The padding integer has to be positive.");
 
-  /* Read the right most digit one at a time and shift right the input 
-     integer. Add '0' to meet the width if needed.   
-   */
-  for (i = width - 1; i >= 0; i--) {
-    outputString[i] =  (unpaddedInt % 10) + '0';
-    unpaddedInt /= 10;
+  else if (width < 0)
+    EXCEPTION ("The padding width has to be positive.");
+
+  else {
+    /* initalize the array for safety */
+    for ( i = 0; i <= width; i++ )
+      outputString[i] = '\0';
+    
+    /* Read the right most digit one at a time and shift right the input 
+       integer. Add '0' to meet the width if needed.   
+    */
+    for (i = width - 1; i >= 0; i--) {
+      outputString[i] =  (unpaddedInt % 10) + '0';
+      unpaddedInt /= 10;
+    }
+    
+    if (unpaddedInt)
+      EXCEPTION ("The integer exceeded the padding width.");
+    
+    else 
+      return (mi_string_to_lvarchar(outputString));
   }
-      
-  if (unpaddedInt)
-    EXCEPTION ("The integer exceeded the padding width.");
-
-  else 
-    return (mi_string_to_lvarchar(outputString));
-  
 }
