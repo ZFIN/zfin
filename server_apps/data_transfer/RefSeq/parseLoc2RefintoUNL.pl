408c1f63d35b3f3b79558f6d776d418f52807759
Brock Sprunge <bsprunge@zfin.org>
2005-08-05 21:00:40 +0000

    BUGZID: none Reviewed by: peiran
    remove duplicate records
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@5898 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseLoc2RefintoUNL.pl
+++ server_apps/data_transfer/RefSeq/parseLoc2RefintoUNL.pl
@@ -1,8 +1,15 @@
-#! /local/bin/perl
+#! /private/bin/perl 
 
+# This is not a cgi, so no -wT
+
+# Container for desired data.
+@AoA = ();
+
+# Open the file loc2ref. For details on the file look in the README.
+#
+# Pull out the Zebrafish (7955) accession numbers.
 
 open (CUT, "cut -f 1,2,4,6 loc2ref |") or die "cut loc2ref failed";
-open (UNL, ">loc2ref.unl") or die "cannot open loc2ref.unl";
 
 while ($line = <CUT>) {
   chop $line;
@@ -10,12 +17,28 @@ while ($line = <CUT>) {
   if ($ll_id =~ /[0-9]*/ && $org_id == 7955) {
     if($nm_acc =~ /(.*)\./){$nm_acc = $1;}
     if($np_acc =~ /(.*)\./){$np_acc = $1;}
-    print UNL "$ll_id|$nm_acc||$np_acc||\n";
+    
+    push @AoA, "$ll_id|$nm_acc|$np_acc|" ;
   }
 }
 
-close (UNL);
 close (CUT);
 
+# Sort the data.
+@AoA = sort(@AoA);
+
+
+# Print to a load file. Ignore adjacent records that are duplicates. 
+
+open (UNL, ">loc2ref.unl") or die "cannot open loc2ref.unl";
+for $x ( 0 .. $#AoA )
+{
+  if ($AoA[$x] ne $AoA[$x-1])
+  {
+    print UNL $AoA[$x]."\n";
+  }
+
+}
+close (UNL);
 
 exit;
