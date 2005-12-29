2064759f789ea9204cb8b9dc1d25489d84a35cfd
Peiran Song <peirans@zfin.org>
2005-12-29 00:30:07 +0000

    Add _gxp_ string into the temporary tables' name to avoid temp table names collapse.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6243 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GXP/gxp_load_cleanup.sql
+++ server_apps/data_transfer/GXP/gxp_load_cleanup.sql
@@ -1,11 +1,11 @@
 -- Clean up the temporary tables used in gxp_load_func.sql
 ------------------------------------------------------------
 
-drop table tmp_marker;
-drop table tmp_fake_gene;
-drop table tmp_db_link;
-drop table tmp_expression_experiment;
-drop table tmp_expression_result;
-drop table tmp_figure;
-drop table tmp_marker_relationship;
+drop table tmp_gxp_marker;
+drop table tmp_gxp_fake_gene;
+drop table tmp_gxp_db_link;
+drop table tmp_gxp_expression_experiment;
+drop table tmp_gxp_expression_result;
+drop table tmp_gxp_figure;
+drop table tmp_gxp_marker_relationship;
 
