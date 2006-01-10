2f7f35d0a95e082196ec76dfe186f7083549824a
Paea LePendu <paea@zfin.org>
2006-01-10 16:45:15 +0000

    BUGZID: 804 Reviewed by: Not reviewed QuickSearch infrastructural changes that support the RelatedTerms feature that suggests alternate query terms related to alias and anatomy items.  These files help to create a list of tokens for aliases for quick access much like a "fast search" table.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6277 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/Regen_tokens/tokenize_list.sql
+++ lib/DB_functions/SPL/Regen_tokens/tokenize_list.sql
@@ -32,15 +32,32 @@ create procedure tokenize_list()
   -- later by the calling routine.
 
   define nameZdbId like zdb_active_data.zactvd_zdb_id;
-  define name varchar(255); 
+  define name varchar(255);
+  define token varchar(255);
   define nameLength integer;
   define startColumn integer;
+  define endColumn integer;
 
   foreach
     select tokin_zdb_id, tokin_name, length(tokin_name)
       into nameZdbId, name, nameLength
       from tokenize_in_temp
 
+    -- scrub stray HTML (we may need a function for this)
+    let name = replace(name,"<sup>"," ");
+    let name = replace(name,"</sup>"," ");
+    let name = replace(name,"<sub>"," ");
+    let name = replace(name,"</sub>"," ");
+    let name = replace(name,"<i>"," ");
+    let name = replace(name,"</i>"," ");
+    let name = scrub_char(name);
+
+    -- Split the name into tokens.  A token (word) is defined by the following
+    -- characters (regex format):  ([a-z0-9]+[:'][a-z0-9]+)|([a-z0-9]+)
+    -- 
+    -- VALID token (examples):  gizmo123, wu:123, hydro's, version, 7, a, b
+    -- 
+    -- Originally, we...
     -- Shred the name into every possible substring that starts with a token.
     -- For the string 
     --   "a,b-hydro's gizmo123,version 7"
@@ -51,27 +68,49 @@ create procedure tokenize_list()
     --               "gizmo123,version 7"
     --                        "version 7"
     --                                "7"
+    --
+    -- But this proved to be overkill, so we simply tokenize each "word."  
+    -- The advantage to this is we can use "=" instead of "like" in our
+    -- SQL query for matching tokens -- efficiency.
+
     let startColumn = 1;
-    while startColumn < nameLength
-      insert into tokenize_out_temp
-          ( tokout_zdb_id, tokout_token )
-        values 
-          ( nameZdbId, substr(name, startColumn) );
+
+    -- look for start of first token
+    while startColumn <= nameLength
+      and not (   substr(name, startColumn, 1) between "a" and "z"
+               or substr(name, startColumn, 1) between "0" and "9")
+      let startColumn = startColumn + 1;
+    end while
+
+    while startColumn <= nameLength
       -- look for end of this token
-      let startColumn = startColumn + 1;
-      while startColumn < nameLength
-        and (   (substr(name, startColumn, 1) between "a" and "z")
-             or (substr(name, startColumn, 1) between "0" and "9")
-             or (substr(name, startColumn, 1) = "'"))
-        let startColumn = startColumn + 1;
+      let endColumn = startColumn + 1;
+      while endColumn <= nameLength
+        and (   (substr(name, endColumn, 1) between "a" and "z")
+             or (substr(name, endColumn, 1) between "0" and "9")
+             or (substr(name, endColumn, 1) = "'")
+             or (substr(name, endColumn, 1) = ":"))
+        let endColumn = endColumn + 1;
       end while
+
+      -- insert token
+      let token = substr(name, startColumn, endColumn - startColumn);
+      let token = scrub_char(token);
+      if (token is not null) then
+        insert into tokenize_out_temp
+          ( tokout_zdb_id, tokout_token )
+          values 
+            ( nameZdbId, token );
+      end if
+
       -- look for start of next token
-      let startColumn = startColumn + 1;
-      while startColumn < nameLength
+      let startColumn = endColumn + 1;
+      while startColumn <= nameLength
         and not (   substr(name, startColumn, 1) between "a" and "z"
                  or substr(name, startColumn, 1) between "0" and "9")
         let startColumn = startColumn + 1;
       end while
+
     end while -- still have name to process.
 
   end foreach  -- foreach record in tokenize_in_temp
