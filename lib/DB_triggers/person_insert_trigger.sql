ed9677d598ab49f2dff99d51e12ad319255cf6b2
Sierra Moxon <smoxon@zfin.org>
2006-03-13 19:48:31 +0000

    BUGZID: 861 Reviewed by: Not reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6380 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/person_insert_trigger.sql
+++ lib/DB_triggers/person_insert_trigger.sql
@@ -1,17 +1,18 @@
-create trigger person_insert_trigger insert on 
+create trigger person_insert_trigger insert on
     person referencing new as new_person
     for each row
         (
-	execute function scrub_char ( new_person.nicknames )
-		into nicknames,
-	execute function scrub_char ( new_person.full_name )
-		into full_name,
-	execute function scrub_char ( new_person.url )
-		into url,
-	execute function scrub_char ( new_person.email )
-		into email,
-	execute function scrub_char ( new_person.fax )
-		into fax,
-	execute function scrub_char ( new_person.phone )
-		into phone
-        );
+        execute function scrub_char(new_person.nicknames 
+    ) into person.nicknames,
+        execute function scrub_char(new_person.full_name 
+    ) into person.full_name,
+        execute function scrub_char(new_person.url 
+    ) into person.url,
+        execute function scrub_char(new_person.email 
+    ) into person.email,
+        execute function scrub_char(new_person.fax 
+    ) into person.fax,
+        execute function scrub_char(new_person.phone 
+    ) into person.phone,
+	execute procedure p_new_person_address (new_person.zdb_id, 
+		new_person.full_name));
--- lib/DB_triggers/person_update_trigger.sql
+++ lib/DB_triggers/person_update_trigger.sql
@@ -1,18 +1,17 @@
-create trigger person_update_trigger 
-       update on person
-       referencing new as new_person
+create trigger person_update_trigger update of nicknames, full_name,
+	url, email, fax, phone on person referencing 
+    new as new_person
     for each row
-      (
-	execute function scrub_char ( new_person.nicknames )
-		into nicknames,
-	execute function scrub_char ( new_person.full_name )
-		into full_name,
-	execute function scrub_char ( new_person.url )
-		into url,
-	execute function scrub_char ( new_person.email )
-		into email,
-	execute function scrub_char ( new_person.fax )
-		into fax,
-	execute function scrub_char ( new_person.phone )
-		into phone
-      );
+        (
+        execute function scrub_char(new_person.nicknames 
+    ) into person.nicknames,
+        execute function scrub_char(new_person.full_name 
+    ) into person.full_name,
+        execute function scrub_char(new_person.url 
+    ) into person.url,
+        execute function scrub_char(new_person.email 
+    ) into person.email,
+        execute function scrub_char(new_person.fax 
+    ) into person.fax,
+        execute function scrub_char(new_person.phone 
+    ) into person.phone);
