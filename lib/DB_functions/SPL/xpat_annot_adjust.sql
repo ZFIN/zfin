e2bbffa29abaa38de63349702bebd1ae518db47e
Peiran Song <peirans@zfin.org>
2006-04-06 20:11:25 +0000

    Handle the xpatfig records and potentially duplicate new xpatres records.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6449 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/xpat_annot_adjust.sql
+++ lib/DB_functions/SPL/xpat_annot_adjust.sql
@@ -136,6 +136,7 @@ create function xpat_annot_adjust (
 	(
 	  xrtb_old_xpatres_zdb_id	varchar(50) not null,
 	  xrtb_xpatres_zdb_id 		varchar(50),
+	  xrtb_flag                     varchar(10),
 	  xrtb_xpatex_zdb_id		varchar(50) not null,
           xrtb_start_stg_zdb_id		varchar(50) not null,
 	  xrtb_end_stg_zdb_id		varchar(50) not null,
@@ -144,36 +145,62 @@ create function xpat_annot_adjust (
           xrtb_comments			varchar(255)	  
 	)with no log;
 
+
+    -- -------------------------------------------------------------------
+    --   create xpatfig_to_be_temp as expression_pattern_figure
+    -- -------------------------------------------------------------------    
+     let errorHint = "xpatfig_to_be_temp";
+     create temp table xpatfig_to_be_temp
+	(
+	  xftb_old_xpatres_zdb_id	varchar(50) not null,
+	  xftb_xpatres_zdb_id	varchar(50),
+	  xftb_fig_zdb_id	varchar(50) not null
+	) with no log;
+
     -- -------------------------------------------------------------------
     --  populate  xpatres_to_be_temp  
     -- -------------------------------------------------------------------    
 
      -- for each xpatresult record on the input old anatomy,
-     -- if input window overlaps the stg window of the record,
+     -- if input stage window, which is the translation stage window,
+     -- overlaps the existing stg window of the xpatres record,
      -- create a new xpatresult for the input new anaotmy.
 
-     insert into xpatres_to_be_temp (xrtb_old_xpatres_zdb_id,
+     insert into xpatres_to_be_temp (xrtb_old_xpatres_zdb_id, 
 				     xrtb_xpatex_zdb_id,xrtb_start_stg_zdb_id,
 				     xrtb_end_stg_zdb_id,xrtb_anat_item_zdb_id,
 				     xrtb_expression_found,xrtb_comments)
-	     select xpatres_zdb_id, xpatres_xpatex_zdb_id, xpatres_start_stg_zdb_id, 
-		    xpatres_end_stg_zdb_id, newAnatitemZdbId,
+	     select xpatres_zdb_id, xpatres_xpatex_zdb_id,
+		    xpatres_start_stg_zdb_id, xpatres_end_stg_zdb_id, newAnatitemZdbId,
 		    xpatres_expression_found, 
 		    "Translated from "||oldAnatName||", "||startStgAbbrev||"-"||endStgAbbrev
                from expression_result
 	      where xpatres_anat_item_zdb_id = oldAnatitemZdbId
                 and stg_windows_overlap(startStgZdbId, endStgZdbId, 
 		          xpatres_start_stg_zdb_id, xpatres_end_stg_zdb_id) = "t";
+ 
+    -- -------------------------------------------------------------------
+    --  populate  xpatfig_to_be_temp  
+    -- -------------------------------------------------------------------    
+
+      -- select out xpatfig records on xpatres that are subject to 
+      -- anatomy term change
+
+	insert into xpatfig_to_be_temp (xftb_old_xpatres_zdb_id,xftb_fig_zdb_id)
+	     select xrtb_old_xpatres_zdb_id, xpatfig_fig_zdb_id
+               from xpatres_to_be_temp, expression_pattern_figure
+              where xrtb_old_xpatres_zdb_id = xpatfig_xpatres_zdb_id;
+             
 
     -- -------------------------------------------------------------------
-    --   update the xrtb_xpatres_zdb_id 
-    --   with either existing -XPATRES- flaging a potential duplication
-    --   or generate a new id. 
+    --   update the xrtb_xpatres_zdb_id with existing  -XPATRES- 
+    --   and flag duplication
+    --
     --   We do this because we want to capture the old xpatres id and
-    --   and the replacing xpatres id which could be alreadying existing
-    --   or newly generated into zdb_replaced_data
+    --   and the replacing xpatres id (which could be alreadying existing
+    --   or newly generated) into zdb_replaced_data
     -- -------------------------------------------------------------------    
-     let errorHint = "update xrtb_xpatres_zdb_id";
+     let errorHint = "update existing xrtb_xpatres_zdb_id";
 
      -- records got updated here would be recognized as duplicates and dropped later
      update xpatres_to_be_temp 
@@ -186,20 +213,68 @@ create function xpat_annot_adjust (
 	            and xpatres_anat_item_zdb_id = xrtb_anat_item_zdb_id
 		    and xpatres_expression_found = xrtb_expression_found);
 
-     -- records updated here would be add into zdb_active_data and expression_result
      update xpatres_to_be_temp 
-	set xrtb_xpatres_zdb_id = get_id ("XPATRES")
+        set xrtb_flag = "duplicate"
+      where xrtb_xpatres_zdb_id is not null;
+
+    -- -------------------------------------------------------------------
+    -- for those to get new XPATRES ids, there is slim chance that
+    -- duplication might happen. To be safe and correct, we take the trouble
+    -- to handle it rather then a simple get_id() call. 
+    -- 
+    -- records updated here would be add into zdb_active_data,
+    -- expression_result, and expression_pattern_figure
+    -- -------------------------------------------------------------------
+
+     create temp table xpatres_new_distinct_temp (
+  	  xndt_xpatres_zdb_id 		varchar(50),
+	  xndt_xpatex_zdb_id		varchar(50) not null,
+          xndt_start_stg_zdb_id		varchar(50) not null,
+	  xndt_end_stg_zdb_id		varchar(50) not null,
+          xndt_anat_item_zdb_id		varchar(50) not null,
+	  xndt_expression_found		boolean not null
+	)with no log;
+
+     insert into xpatres_new_distinct_temp (
+			xndt_xpatex_zdb_id, xndt_start_stg_zdb_id,
+			xndt_end_stg_zdb_id, xndt_anat_item_zdb_id,
+			xndt_expression_found)
+	   select distinct xrtb_xpatex_zdb_id,xrtb_start_stg_zdb_id,
+			   xrtb_end_stg_zdb_id,xrtb_anat_item_zdb_id,
+		           xrtb_expression_found
+             from xpatres_to_be_temp
+            where xrtb_xpatres_zdb_id is null;
+
+     update xpatres_new_distinct_temp 	
+	set xndt_xpatres_zdb_id = get_id ("XPATRES")
+      where xndt_xpatres_zdb_id is null;
+		
+     update xpatres_to_be_temp 
+	set xrtb_flag = "new",
+	    xrtb_xpatres_zdb_id = (select xndt_xpatres_zdb_id
+                                     from xpatres_new_distinct_temp
+                                    where xndt_xpatex_zdb_id = xrtb_xpatex_zdb_id
+				      and xndt_start_stg_zdb_id = xrtb_start_stg_zdb_id
+                                      and xndt_end_stg_zdb_id = xrtb_end_stg_zdb_id
+				      and xndt_anat_item_zdb_id = xrtb_anat_item_zdb_id
+                                      and xndt_expression_found = xrtb_expression_found)
       where xrtb_xpatres_zdb_id is null;
+
+    -- -------------------------------------------------------------------
+    -- get the new xpatres id for the to-be-added xpatfig records
+    -- -------------------------------------------------------------------
+     update xpatfig_to_be_temp
+	set xftb_xpatres_zdb_id = (select xrtb_xpatres_zdb_id
+				     from xpatres_to_be_temp
+				    where xrtb_flag = "new"
+                                      and xftb_old_xpatres_zdb_id = xrtb_old_xpatres_zdb_id);
 
      -- we are doing it here to make sure the zdb_replaced_data to be added next step 
      -- would already have a validate reference in zdb_active_data 
      insert into zdb_active_data (zactvd_zdb_id)
 	   select xrtb_xpatres_zdb_id 
              from xpatres_to_be_temp
-	    where not exists 
-		(select 't'	
-		   from expression_result
-		  where xpatres_zdb_id = xrtb_xpatres_zdb_id);
+	    where xrtb_flag = "new";
 
     -- -------------------------------------------------------------------
     --   delete xpatres records that violate stage rule 
@@ -227,10 +302,7 @@ create function xpat_annot_adjust (
     -- -------------------------------------------------------------------    
      let errorHint = "delete duplicates from xpatres_to_be_temp";
      delete from xpatres_to_be_temp
-	   where exists 
-		(select 't'	
-		   from expression_result
-		  where xpatres_zdb_id = xrtb_xpatres_zdb_id);
+	   where xrtb_flag = "duplicate";
 
 
     -- -------------------------------------------------------------------
@@ -247,11 +319,21 @@ create function xpat_annot_adjust (
 		  xrtb_anat_item_zdb_id, xrtb_expression_found,
 		  xrtb_comments
              from xpatres_to_be_temp;
+
+    -- -------------------------------------------------------------------
+    --  insert in the added xpatfig records
+    -- -------------------------------------------------------------------    
+     let errorHint = "insertion into expression_pattern_figure"; 
+     insert into expression_pattern_figure (xpatfig_xpatres_zdb_id, xpatfig_fig_zdb_id)
+          select distinct xftb_xpatres_zdb_id, xftb_fig_zdb_id
+            from xpatfig_to_be_temp
+           where xftb_xpatres_zdb_id is not null; 
           
     -- -------------------------------------------------------------------
     -- temp table clean up
     -- -------------------------------------------------------------------    
      drop table xpatres_to_be_temp;
+     drop table xpatfig_to_be_temp;
 
     end  -- Global exception handler
 
