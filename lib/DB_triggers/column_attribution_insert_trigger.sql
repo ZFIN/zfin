62e6e7680e6e785a601f0ef7490c84cc21a50f93
Sierra Moxon <smoxon@zfin.org>
2006-05-01 15:29:14 +0000

    BUGZID: 839 Reviewed by: Not reviewed removed this trigger from production b/c the table is also removed.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6513 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/column_attribution_insert_trigger.sql
+++
@@ -1,7 +0,0 @@
-create trigger column_attribution_insert_trigger insert on 
-    column_attribution referencing new as new_ca
-    for each row
-        (
-        execute function scrub_char ( new_ca.colattrib_source_zdb_id )
-                into colattrib_source_zdb_id
-        );
