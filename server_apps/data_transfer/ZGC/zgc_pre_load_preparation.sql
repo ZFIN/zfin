b1c075cf356409c87ca393a6dff42674fb24d5f1
Sierra Moxon <smoxon@zfin.org>
2006-05-02 00:10:55 +0000

    BUGZID: none Reviewed by: Not reviewed added index and tblspaces
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6531 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/ZGC/zgc_pre_load_preparation.sql
+++ server_apps/data_transfer/ZGC/zgc_pre_load_preparation.sql
@@ -6,11 +6,14 @@ CREATE TABLE tmp_Zgc_Lib
     zLib_tissue  varchar(100),
     zLib_vector  varchar(80),
     junk         integer  
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 CREATE index zLib_name_index
  ON tmp_Zgc_Lib(zLib_name)
- USING btree;
+ USING btree 
+ in idxdbs3;
  
 CREATE TABLE tmp_Lib_Bank 
   (
@@ -18,7 +21,9 @@ CREATE TABLE tmp_Lib_Bank
     Libbank_zdb_id    varchar(50),
     Libbank_vec_name  varchar(80),
     Libbank_vec_type  varchar(20)
-  ) ;
+  ) 
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 
 CREATE TABLE tmp_Zgc
@@ -34,23 +39,30 @@ CREATE TABLE tmp_Zgc
     zgc_lib		varchar(50),
     zgc_name		varchar(20),
     zgc_abbrev		varchar(40)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128
+;
   
 CREATE index zgc_mrkr_abbrev_index
   ON tmp_Zgc(zgc_mrkr_abbrev)
-  USING btree;
+  USING btree
+  in idxdbs3;
 
 CREATE index zgc_name_index
   ON tmp_Zgc(zgc_name)
-  USING btree;
+  USING btree
+  in idxdbs3;
 
 CREATE index zgc_abbrev_index
   ON tmp_Zgc(zgc_abbrev)
-  USING btree;
+  USING btree
+  in idxdbs3;
 
 CREATE index zgc_lib_index
   ON tmp_Zgc(zgc_lib)
-  USING btree;
+  USING btree
+  in idxdbs3;
   
 
 LOAD from StaticLibList.unl INSERT into tmp_Zgc_Lib;
@@ -67,31 +79,41 @@ CREATE TABLE zgc_tmp_zname_mismatch
     zmis_acc_num           varchar(20),
     zmis_zfin_mrkr_abbrev  varchar(50),
     zmis_zgc_mrkr_abbrev   varchar(50)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 CREATE TABLE zgc_tmp_lib_not_found
   (
     zlib_name              varchar(80)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 CREATE TABLE zgc_tmp_zlib_vector_not_found
   (
     zvec_name              varchar(80)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 CREATE TABLE zgc_tmp_acc_num_assigned_in_zfin
   (
     zaccmis_zfin_abbrev    varchar(40),
     zaccmis_mgc_abbrev     varchar(40),
     zaccmis_acc_num        varchar(20)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
 CREATE TABLE zgc_tmp_no_gene
   (
     nogene_zfin_mrkr_abbrev	varchar(40),
     nogene_zgc_abbrev		varchar(40),
     nogene_acc_num              varchar(40)
-  );
+  )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 128 next size 128;
 
   
 commit work;
