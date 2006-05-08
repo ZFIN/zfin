be0e27dfb6748c69ba2ff03b44b5596a500b4576
Sierra Moxon <smoxon@zfin.org>
2006-05-08 17:45:18 +0000

    BUGZID: none Reviewed by: Not reviewed optimized SQL added checks for duplicate ortho symbols
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6555 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/ORTHO/loadOrthoChromInfo.pl
+++ server_apps/data_transfer/ORTHO/loadOrthoChromInfo.pl
@@ -24,6 +24,8 @@
 # parseSGDdata.pl       parse orthology data file of SGD
 # updatOrthologue.sql   update ZFIN orthologue table with chromosome info
 
+use MIME::Lite;
+
 # set environment variables
 $ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
 $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
@@ -47,7 +49,7 @@ system("/local/bin/wget -q ftp://ftp.informatics.jax.org/pub/reports/HMD_Human1.
 print "\nDownloading from MGI is done\n";
 
 # download data file from FlyBase for updating fly chromosome info 
-system("/local/bin/wget -q ftp://flybase.net/genomes/Drosophila_melanogaster/current/fasta/dmel-all-gene-r4.2.1.fasta -O FlyBase.data") and &emailError("Cannot download data file from FlyBase.");
+system("/local/bin/wget -q ftp://flybase.net/genomes/Drosophila_melanogaster/current/fasta/dmel-all-gene-r4.3.fasta -O FlyBase.data") and &emailError("Cannot download data file from FlyBase.");
 print "\nDownloading from FlyBase is done.\n";
 
 # download data file from SGD for updating fly chromosome info
@@ -70,6 +72,9 @@ print "Parsing SGD data file is done.\n";
 system("$ENV{'INFORMIXDIR'}/bin/dbaccess $ENV{'DATABASE'} updateOrthologue.sql >out 2> errReport.txt") and &emailError("Failed to update orthologue table");
 print "Updating the table orthologue with chromosome info is done\n";
 
+&isEmptyFile ("duplicate_ortho_symbol.unl","No duplicate ortholog symbols",
+	      "<!--|DB_OWNER|-->\@cs.uoregon.edu","Duplicate Ortholog Symbols");
+
 exit;
 
 #=========================================
@@ -87,9 +92,66 @@ sub emailError($)
   {
     open(MAIL, "| $mailprog") || die "Cannot open mailprog $mailprog";
     print MAIL "To: <!--|DB_OWNER|-->\@cs.uoregon.edu\n";
-    print MAIL "Subject: loadOrthoChromInfo.pl error\n";
+    print MAIL "Subject: loadOrthoChromInfo.pl $_[0]\n";
     print MAIL "Error:\n";
     print MAIL "$_[0]";
     close MAIL;
     exit;
   }
+sub isEmptyFile() { # much like the count lines in a file routine, except
+    # this one determines whether or not to send an email.  Email is only
+    # sent if the file is not empty.
+
+    my $filename = $_[0];
+    my $printMessage = $_[1];
+    my $emailAddress = $_[2];
+    my $emailHeader = $_[3];
+    
+    $count = 0;
+    
+# count the number of lines read from the file.
+
+    open(FILE1, "< ./$filename") or die "can't open $filename";
+    
+    $count++ while <FILE1>;
+
+# count now holds the number of lines read
+
+    if ($count < 1) {
+	print "$printMessage" ;
+    }
+    else {
+	&sendLoadReport("$emailHeader","$emailAddress", 
+			"./$filename") ;
+    }
+    close FILE1;
+}
+
+sub sendLoadReport ($) { # send email on error or completion
+    
+# . is concantenate
+# $_[x] means to take from the array of values passed to the fxn, the 
+# number indicated: $_[0] takes the first member.
+    
+    my $SUBJECT="Auto LoadGOTerms:".$_[0];
+    my $MAILTO=$_[1];
+    my $TXTFILE=$_[2];
+    
+    # Create a new multipart message:
+    $msg1 = new MIME::Lite 
+	From    => "$ENV{LOGNAME}",
+	To      => "$MAILTO",
+	Subject => "$SUBJECT",
+	Type    => 'multipart/mixed';
+
+    attach $msg1 
+	Type     => 'text/plain',   
+	Path     => "$TXTFILE";
+
+    # Output the message to sendmail
+    
+    open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
+    $msg1->print(\*SENDMAIL);
+    close (SENDMAIL);
+    
+}
