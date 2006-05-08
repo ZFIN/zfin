2f0c38f15476bea727ef20763b0f6d07bf48b222
Sierra Moxon <smoxon@zfin.org>
2006-05-08 17:45:54 +0000

    BUGZID: none Reviewed by: Not reviewed optimized SQL, added dup symbol check, added send mail routine to send informix duplicate symbol error reports.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6556 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/ORTHO/updateOrthologue.sql
+++ server_apps/data_transfer/ORTHO/updateOrthologue.sql
@@ -13,18 +13,61 @@ create temp table tmp_ortholog_location (
 -- populate the temporary table for updating orthologue table with chromosome info
 load from chromInfo.unl insert into tmp_ortholog_location;
 
+update tmp_ortholog_location 
+  set orthloc_symbol = lower(orthloc_symbol);
+
+unload to duplicate_ortho_symbol.unl
+select count(*), orthloc_symbol, orthloc_organism
+  from tmp_ortholog_location
+  group by orthloc_symbol, orthloc_organism
+  having count(*) > 1;
+
+create temp table tmp_dup (
+     dup_symbol        varchar(30),
+     dup_chromosome    varchar(30),
+     dup_position      varchar(30),
+     dup_organism      varchar(30)
+) with no log ;
+
+delete from tmp_ortholog_location
+  where exists (select 'x'
+  		  from tmp_dup
+  		  where dup_symbol = orthloc_symbol
+		  and orthloc_organism = dup_organism);
+
+create unique index tmp_ortho_loc_index 
+  on tmp_ortholog_location(orthloc_symbol, orthloc_organism)
+  using btree in idxdbs3 ;
+
+update statistics high for table tmp_ortholog_location ;
 
 -- update orthologue table with chromosome info
+
 update orthologue 
-  set (ortho_chromosome, ortho_position) = 
-      ((select orthloc_chromosome, orthloc_position 
-           from tmp_ortholog_location 
-           where lower(orthloc_symbol) = lower(ortho_abbrev)
-                 and orthloc_organism = organism)) 
-  where ortho_abbrev in 
-       (select orthloc_symbol from tmp_ortholog_location) 
-     and organism in 
-       (select orthloc_organism from tmp_ortholog_location);      
-  
+  set ortho_chromosome =  (select orthloc_chromosome
+           		   from tmp_ortholog_location 
+           		   where orthloc_symbol = lower(ortho_abbrev)
+                 	   and orthloc_organism = organism)
+  where exists
+       (select 'x' 
+	  from tmp_ortholog_location
+          where orthloc_symbol = lower(ortho_abbrev)
+	  and orthloc_organism = organism);     
+
+update orthologue 
+  set ortho_position =  (select orthloc_position
+           		   from tmp_ortholog_location 
+           		   where orthloc_symbol = lower(ortho_abbrev)
+                 	   and orthloc_organism = organism)
+  where exists
+       (select 'x' 
+	  from tmp_ortholog_location
+          where orthloc_symbol = lower(ortho_abbrev)
+	  and orthloc_organism = organism);      
+
 commit work ;
--- rollback work ;
+
+--regenerate the display
+
+execute function regen_oevdisp() ;
+
