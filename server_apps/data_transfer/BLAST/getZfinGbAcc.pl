f988317f804d8ece544b05344edf3f62f36abb48
Peiran Song <peirans@zfin.org>
2006-06-01 00:16:39 +0000

    the zfin_gene_xpat_cdna_acc.unl file contains accession number of cDNA sequences as well as Vega transcripts of zfin genes that has expression result data.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@6714 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/BLAST/getZfinGbAcc.pl
+++ server_apps/data_transfer/BLAST/getZfinGbAcc.pl
@@ -21,7 +21,7 @@ my $password = "";
 my $outputdir = "/research/zblastfiles/files/genomix/";
 my $accFile = $outputdir."zfin_genbank_acc.unl";
 my $accFile_cdna = $outputdir."zfin_genbank_cdna_acc.unl";
-my $accFile_xpat = $outputdir."zfin_genbank_gene_xpat_acc.unl";
+my $accFile_xpat = $outputdir."zfin_gene_xpat_cdna_acc.unl";
 
 ################
 # Form the sql 
@@ -31,7 +31,12 @@ my $sql ="unload to \"$accFile\" delimiter \" \" select dblink_acc_num from db_l
      
 my $sql_cdna ="unload to \"$accFile_cdna\" delimiter \" \" select dblink_acc_num from db_link where dblink_fdbcont_zdb_id in (select fdbcont_zdb_id from foreign_db_contains where fdbcont_fdb_db_name = \"GenBank\" and fdbcont_fdbdt_data_type = \"cDNA\") ";  
 
-my $sql_xpat ="unload to \"$accFile_xpat\" delimiter \" \" select distinct dblink_acc_num from db_link, expression_experiment where xpatex_gene_zdb_id = dblink_linked_recid and  dblink_fdbcont_zdb_id in (select fdbcont_zdb_id from foreign_db_contains where fdbcont_fdb_db_name = \"GenBank\" and fdbcont_fdbdt_data_type = \"cDNA\") ";  
+# query genbank cDNA and vega transcripts accessions on genes 
+# that has expression data, and not named microRNA%
+my $sql_xpat ="create temp table tmp_xpatmrkr_zdb_id_list (t_xgl_mrkr_zdb_id	varchar(50) primary key )with no log;
+insert into tmp_xpatmrkr_zdb_id_list select distinct xpatex_gene_zdb_id from expression_experiment, marker where xpatex_gene_zdb_id =  mrkr_zdb_id and  exists (select xpatres_zdb_id from expression_result where xpatres_xpatex_zdb_id = xpatex_zdb_id);
+insert into tmp_xpatmrkr_zdb_id_list select distinct mrel_mrkr_2_zdb_id from tmp_xpatmrkr_zdb_id_list, marker_relationship where t_xgl_mrkr_zdb_id = mrel_mrkr_1_zdb_id and mrel_type = \"gene encodes small segment\";
+unload to \"$accFile_xpat\" delimiter \" \" select distinct dblink_acc_num from db_link where dblink_fdbcont_zdb_id in (select fdbcont_zdb_id from foreign_db_contains where fdbcont_fdb_db_name in (\"GenBank\",\"Vega_Trans\") and fdbcont_fdbdt_data_type = \"cDNA\") and dblink_linked_recid in (select t_xgl_mrkr_zdb_id from tmp_xpatmrkr_zdb_id_list)";  
   
 
 #################
