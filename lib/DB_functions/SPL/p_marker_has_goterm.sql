8f2abc8504af27e7e886f9e3574f81da1e9fca02
Peiran Song <peirans@zfin.org>
2006-08-25 18:20:04 +0000

    BUGZID: 929 Check for root terms instead of 'unknown' terms. Reviewed by: Not reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@7341 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/p_marker_has_goterm.sql
+++ lib/DB_functions/SPL/p_marker_has_goterm.sql
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------
 --called on insert/update of marker_go_term table
 --takes mrkrgo_mrkr_zdb_id, mrkrgo_goterm_zdb_id as input
---returns trigger failure if trying to associate an 'unknown' goterm
---to a marker that already has goterms (not 'unknown' goterms) in this 
+--returns trigger failure if trying to associate a root goterm
+--to a marker that already has non-root goterms in this 
 --table.
 ----------------------------------------------------------------------
 
@@ -13,15 +13,12 @@ create procedure p_marker_has_goterm (vMrkrZdbId varchar(50),
         define vGoID  	  varchar(50) ;
 	define vOntology  varchar(50) ;
 	
-	let vGoID = (select goterm_go_id 
-		       from go_term
-    		       where goterm_zdb_id = vMrkrGotermId) ;
-	
-	let vOntology = (select goterm_ontology 
-			  from go_term
-			  where goterm_zdb_id = vMrkrGotermId) ;
-	
-	if vOntology = 'Molecular Function'
+	select goterm_go_id, goterm_ontology 
+          into vGoID, vOntology
+          from go_term
+         where goterm_zdb_id = vMrkrGotermId ;
+
+	if vGoID = '0003674'  -- Molecular Function
  	   
 		then 
 		  let vCount = (select count(*) 
@@ -29,17 +26,15 @@ create procedure p_marker_has_goterm (vMrkrZdbId varchar(50),
 				where mrkrgoev_mrkr_zdb_id = vMrkrZdbId
 				and mrkrgoev_go_term_zdb_id = goterm_zdb_id
 				and goterm_ontology = vOntology
-				and goterm_go_id != '0005554' 
+				and goterm_go_id <> '0003674' 
 				) ;
 	
  		  if vCount > 0 then
-			if vGoID = '0005554'
-			  then 
-			    raise exception -746,0,'FAIL!: This marker already has terms--it can not be assigned unknown terms.' ;
-			end if ;
+			 raise exception -746,0,'FAIL!: This marker already has non-root go terms--it can not be assigned this root term.' ;
+		
 		  end if ;
 	
-	elif vOntology = 'Biological Process'
+	elif vGoID = '0008150' -- Biological Process
 	   then
 	     
 	        let vCount = (select count(*)
@@ -47,30 +42,28 @@ create procedure p_marker_has_goterm (vMrkrZdbId varchar(50),
 				where mrkrgoev_mrkr_zdb_id = vMrkrZdbId
 				and mrkrgoev_go_term_zdb_id = goterm_zdb_id
 				and goterm_ontology = vOntology
-				and goterm_go_id != '0000004' 
+				and goterm_go_id <> '0008150' 
 				) ;
 	
 		if vCount > 0 then
-			if vGoID = '0000004'
-			  then 
-			    raise exception -746,0,'FAIL!: This marker already has terms--it can not be assigned an unknown term.' ;
-			end if ;
-		end if ;	
-	elif vOntology = 'Cellular Component' 
+		
+			raise exception -746,0,'FAIL!: This marker already has non-root go terms--it can not be assigned this root term.' ;
+
+		end if ;
+	
+	elif vGoID = '0005575' -- Cellular Component
 	   then 	
 		let vCount = (select count(*) 
 				from marker_go_term_evidence, go_term
 				where mrkrgoev_mrkr_zdb_id = vMrkrZdbId
 				and mrkrgoev_go_term_zdb_id = goterm_zdb_id
 				and goterm_ontology = vOntology
-				and goterm_go_id != '0008372' 
+				and goterm_go_id <> '0005575' 
 				) ;
 	
 		if vCount > 0 then
-			if vGoID = '0008372'
-			  then 
-			    raise exception -746,0,'FAIL!: This marker already has terms--it can not be assigned an unknown term.' ;
-			end if ;
+		       raise exception -746,0,'FAIL!: This marker already has non-root go terms--it can not be assigned this root term.' ;
+		
 		end if ;	
 
 	end if ;
