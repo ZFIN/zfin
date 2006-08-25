19ce0be6360b43d3dfd74e46daa306bc436b9f32
Peiran Song <peirans@zfin.org>
2006-08-25 18:24:23 +0000

    BUGZID: 929 Replace p_check_drop_go_unknown.sql. Once there is a new GO annotation to a non-root term in the same ontology category, the annotation to the root term would be dropped. Reviewed by: Not reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@7342 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_check_drop_go_root_term.sql
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------
+-- In case of non-root go term annotation, check if there is 
+-- currently a root term from the same category, if yes,
+-- delete the root term and call another procedure to check and 
+-- drop attribution on root annotation; if no, do nothing.
+--
+-- INPUT:
+--       marker zdb id
+--       go term zdb id 
+-- OUTPUT:
+--       none
+-- EFFECT:
+--       on yes: delete go root annotation, might also lead to 
+--               deletion of curation pub for root annotation.
+--       on no:  nothing
+-- RETURN:
+--       none
+----------------------------------------------------------------------
+
+create procedure p_check_drop_go_root_term (
+		mrkrZdbId	like marker.mrkr_zdb_id,
+		gotermZdbId	like go_term.goterm_zdb_id
+		)
+
+	define goRootTermZdbId	like go_term.goterm_zdb_id;
+
+	if (gotermZdbId not in ('ZDB-GOTERM-031121-2395','ZDB-GOTERM-031121-846','ZDB-GOTERM-031121-4370')) then
+
+	    -- find out the root term's zdb id, which is in the same category
+	    select u.goterm_zdb_id
+	      into goRootTermZdbId
+	      from go_term u, go_term n
+	     where n.goterm_zdb_id = gotermZdbId
+               and u.goterm_ontology = n.goterm_ontology
+	       and u.goterm_go_id in ("0005575", "0003674", "0008150");
+
+	
+	    -- delete the root term annotation
+	    delete from zdb_active_data 
+	          where zactvd_zdb_id in 
+		       	(select mrkrgoev_zdb_id 
+			   from marker_go_term_evidence
+			  where mrkrgoev_mrkr_zdb_id = mrkrZdbId
+			    and mrkrgoev_go_term_zdb_id = goRootTermZdbId
+			);
+
+	      execute procedure p_drop_go_root_term_attribution (mrkrZdbId);
+	    
+	end if; 
+
+end procedure;
