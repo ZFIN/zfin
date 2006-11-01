4c5dc5217db84d34e2aa27a3de9374aba8f7253f
Peiran Song <peirans@zfin.org>
2006-11-01 19:32:22 +0000

    Add populate_direct_submission_date function.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@7849 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/expression_experiment_insert_trigger.sql
+++ lib/DB_triggers/expression_experiment_insert_trigger.sql
@@ -10,5 +10,7 @@ create trigger expression_experiment_insert_trigger insert on
 			new_xpatex.xpatex_source_zdb_id),
 	execute procedure p_insert_into_record_attribution_tablezdbids (
 			new_xpatex.xpatex_zdb_id,
-                        new_xpatex.xpatex_source_zdb_id) 
+                        new_xpatex.xpatex_source_zdb_id), 
+	execute procedure populate_direct_submission_date(
+			new_xpatex.xpatex_zdb_id)
      );	
