d6c327693bd7b82174a552e8edb18fa6d663ec4c
Sierra Moxon <smoxon@zfin.org>
2007-01-22 18:40:50 +0000

    BUGZID: none Reviewed by: Not reviewed added obsolete and secondary check to apato
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@8412 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/atomic_phenotype_update_trigger.sql
+++ lib/DB_triggers/atomic_phenotype_update_trigger.sql
@@ -1,6 +1,6 @@
 create trigger atomic_phenotype_update_trigger update of 
     apato_start_stg_zdb_id, apato_end_stg_zdb_id, apato_entity_a_zdb_id,
-	apato_entity_b_zdb_id, apato_pub_zdb_id
+	apato_entity_b_zdb_id, apato_pub_zdb_id, apato_quality_zdb_id
     on atomic_phenotype
     referencing new as new_apato
     for each row
@@ -12,6 +12,7 @@ create trigger atomic_phenotype_update_trigger update of
 			new_apato.apato_zdb_id,
 			new_apato.apato_entity_a_zdb_id,
 			new_apato.apato_entity_b_zdb_id),
+	execute procedure p_quality_term_not_obsolete_or_secondary(new_apato.apato_quality_zdb_id),
 	execute procedure p_insert_into_record_attribution_datazdbids(
 		new_apato.apato_zdb_id, new_apato.apato_pub_zdb_id)
      ) ;
