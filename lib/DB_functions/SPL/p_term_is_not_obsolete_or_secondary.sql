7ff2b356643c5ca7ab571814b01fc059495fbdfb
Sierra Moxon <smoxon@zfin.org>
2007-01-22 19:01:21 +0000

    BUGZID: none Reviewed by: Not reviewed add a procedure for all 3 term checks
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@8416 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_term_is_not_obsolete_or_secondary.sql
@@ -0,0 +1,75 @@
+create procedure p_term_is_not_obsolete_or_secondary (vTerm varchar(50))
+
+define ok boolean;
+define objtype varchar(30);
+
+let objtype = get_obj_type(vTerm);
+
+if objtype = 'GOTERM'
+then
+	let ok = (select term_is_obsolete 
+        	   from go_term 
+          	   where vTerm = goterm_zdb_id);
+
+	if ok then
+
+  	raise exception -746,0,'FAIL!: GO Term is OBSOLETE!';
+
+	elif not ok then 
+
+  	let ok = (select goterm_is_secondary
+             	    from go_term 
+             	    where vTerm = goterm_zdb_id);
+  		if ok then 
+
+    		raise exception -746,0,'FAIL!: GO Term is SECONDARY!';
+
+  		end if ;
+
+	end if ;
+elif objtype = "ANAT"
+then 
+	let ok = (select anatitem_is_obsolete 
+        	   from anatomy_item
+          	   where vTerm = anatitem_zdb_id);
+
+	if ok then
+
+  	raise exception -746,0,'FAIL!: AO Term is OBSOLETE!';
+
+	elif not ok then 
+
+  	let ok = (select anatitem_is_secondary
+             	    from anatomy_item
+             	    where vTerm = anatitem_zdb_id);
+  		if ok then 
+
+    		raise exception -746,0,'FAIL!: AO Term is SECONDARY!';
+
+  		end if ;
+	end if ;
+elif objtype = "TERM"
+then 
+	let ok = (select term_is_obsolete 
+        	   from term
+          	   where vTerm = term_zdb_id);
+
+	if ok then
+
+  	raise exception -746,0,'FAIL!: PATO Term is OBSOLETE!';
+
+	elif not ok then 
+
+  	let ok = (select term_is_secondary
+             	    from term
+             	    where vTerm = term_zdb_id);
+  		if ok then 
+
+    		raise exception -746,0,'FAIL!: PATO Term is SECONDARY!';
+
+  		end if ;
+	end if ;
+end if;
+
+
+end procedure;
