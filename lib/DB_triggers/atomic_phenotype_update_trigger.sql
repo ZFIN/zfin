9b9c6ed9b6ca5cb6ff59e9c2a226e01717e53e2e
Sierra Moxon <smoxon@zfin.org>
2007-01-22 18:43:53 +0000

    BUGZID: none Reviewed by: Not reviewed added scrub char to apato table
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@8413 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/atomic_phenotype_update_trigger.sql
+++ lib/DB_triggers/atomic_phenotype_update_trigger.sql
@@ -14,5 +14,7 @@ create trigger atomic_phenotype_update_trigger update of
 			new_apato.apato_entity_b_zdb_id),
 	execute procedure p_quality_term_not_obsolete_or_secondary(new_apato.apato_quality_zdb_id),
 	execute procedure p_insert_into_record_attribution_datazdbids(
-		new_apato.apato_zdb_id, new_apato.apato_pub_zdb_id)
+		new_apato.apato_zdb_id, new_apato.apato_pub_zdb_id),
+	execute function scrub_char(new_apato.apato_entity_a_zdb_id)
+		into apato_entity_a_zdb_id
      ) ;
