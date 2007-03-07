8ce14a2ebb9114e37289e5c6fd4b470fc334d3c5
Peiran Song <peirans@zfin.org>
2007-03-07 17:27:48 +0000

    CL id get into db_link table, we need to allow anatomy item to be valid parent.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@8714 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/p_dblink_has_parent.sql
+++ lib/DB_functions/SPL/p_dblink_has_parent.sql
@@ -1,7 +1,7 @@
   --P_CHECK_DB_LINKS.SQL
   -------------------------------------------------------------
   --procedure that checks to see that there is at least one record in either 
-  --the marker or orthologue table for each linked_recid in db_link.  
+  --the marker, orthologue or anatomy_item table for each linked_recid in db_link.  
   --This procedure checks the orthologue table first, then the marker table.
   --we can not do a foreign key to this table b/c orthologue or marker can be 
   --parent table.
@@ -10,22 +10,37 @@
 
   create procedure p_dblink_has_parent (vLinkedRecid varchar(55))
 
-  define vMyCount1	 	integer;
-  define vMyCount2		integer;
+  define vHasParent	 	boolean;
+  let vHasParent = 'f';
 
-  let vMyCount1 = (select count(*) 
-	  	   from orthologue
-		   where orthologue.zdb_id = vLinkedRecid);
+  -- check marker table
+  select 't'
+    into vHasParent
+    from marker
+   where mrkr_zdb_id = vLinkedRecid;
+
+  if ( not vHasParent) then 
+
+    -- check orthologue table
+    select 't'
+      into vHasParent
+      from orthologue
+     where zdb_id = vLinkedRecid;
+
+    if (not vHasParent) then 
+	
+	-- check anatomy_item table
+	select 't'
+          into vHasParent
+          from anatomy_item
+         where anatitem_zdb_id = vLinkedRecid;
   
-  let vMyCount2 = (select count(*) 
-		     from marker
-		     where marker.mrkr_zdb_id = vLinkedRecid);
+	if (not vHasParent) then 
+   	    raise exception -746,0,"FAIL!: dblink must have orth or marker parent";
+  	end if;
+    end if 	
 
-if vMyCount1 == 0 then
-
-  if vMyCount2 == 0 then 
-    raise exception -746,0,"FAIL!: dblink must have orth or marker parent";
   end if;
-end if ;
 
+  
   end procedure;
