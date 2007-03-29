d8d7d9afd952588485944c4ec007c3599755a5e1
Peiran Song <peirans@zfin.org>
2007-03-29 00:24:27 +0000

    BUGZID: 1771 Add more stop words. Reviewed by: Not reviewed
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@8824 908b024a-c82b-635c-c293-f2b15b23277c

--- j2ee/uniquery/Support/src/org/zfin/uniquery/ZfinAnalyzer.java
+++ j2ee/uniquery/Support/src/org/zfin/uniquery/ZfinAnalyzer.java
@@ -39,7 +39,7 @@ public class ZfinAnalyzer extends Analyzer
         "under", "up", "use", "very", "want", "was",
         "way", "we", "well", "were", "what", "when",
         "where", "which", "while", "who", "will",
-        "with", "would", "you", "your", "zfin",
+        "with", "would", "you", "your", "zfin", "fish",
         "anatomy", "publication", "people", "lab", "companies",
         "acc", "email", "home", "mutant", "transgenic", "wild", 
         "type", "gene", "marker", "clone", "expression", "map",
@@ -48,7 +48,8 @@ public class ZfinAnalyzer extends Analyzer
 	"name", "names", "symbol", "blast", "data","zdb","all",
         "bp", "aa", "ab", "citation", "mo", "go", "term", "terms",
         "input", "welcome", "more", "image", "images","information",
-	"page", "pages", "length", "view", "other",
+	"page", "pages", "length", "view", "other", "development",
+	"zebrafish", "book", "protocol", "meeting", "meetings","job", "jobs",
         "a", "b", "c", "d", "e", "f", "g", "h", "i",
         "j", "k", "l", "m", "n", "o", "p", "q", "r",
         "s", "t", "u", "v", "w", "x", "y", "z"
