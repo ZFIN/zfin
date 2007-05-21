912fe171cc697883281e09e61cf034b0b9ec14fb
Sierra Moxon <smoxon@zfin.org>
2007-05-21 22:33:54 +0000

    BUGZID: 1845 Reviewed by: Not reviewed added trigger to genotype handle update so that nickname is automagically updated when  handle is.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9363 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/geno_handle_update_trigger.sql
+++ lib/DB_triggers/geno_handle_update_trigger.sql
@@ -3,5 +3,6 @@ create trigger geno_handle_update_trigger update of geno_handle
     for each row
     (
         execute function scrub_char(new_genotype.geno_handle ) 
-    into genotype.geno_handle
+    into genotype.geno_handle,
+	execute procedure p_update_geno_nickname(new_genotype.geno_zdb_id)
 );
