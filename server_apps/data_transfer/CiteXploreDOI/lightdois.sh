d8af994710ceae88f217597867dcbcf1f09758e2
Nathan Dunn <ndunn@zfin.org>
2007-05-22 17:36:35 +0000

    echoed java output
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9373 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/CiteXploreDOI/lightdois.sh
+++ server_apps/data_transfer/CiteXploreDOI/lightdois.sh
@@ -6,6 +6,7 @@
 THISCLASSPATH=.:citexploredoi.jar:lib/jaxws-rt.jar
 REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
 LOGNAME=doiupdate`date '+%y.%m.%d'`-light.log  
+echo "running:  java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME" ;
 java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME ;
 mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME
 
