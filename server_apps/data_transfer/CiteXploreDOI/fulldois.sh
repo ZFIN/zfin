ece1f04b3d295b7a34571df2f3a295bb66aa43ce
Nathan Dunn <ndunn@zfin.org>
2007-05-23 17:54:53 +0000

    fixed classpath issue
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9405 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/CiteXploreDOI/fulldois.sh
+++ server_apps/data_transfer/CiteXploreDOI/fulldois.sh
@@ -5,10 +5,13 @@
 
 
 BASEDIR=$TARGETROOT/server_apps/data_transfer/CitexploreDOI
-THISCLASSPATH=$TARGETROOT:$TARGETROOT/citexploredoi.jar:$TARGETROOT/lib/Java/jaxws/jaxws-rt.jar
+THISCLASSPATH=$BASEDIR/citexploredoi.jar:$BASEDIR/lib/Java/jaxws/jaxws-rt.jar
 REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
 LOGNAME=doiupdate`date '+%y.%m.%d'`-full.log  
+
+
+
 java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>&1  | tee logs/$LOGNAME ;
-mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME
+mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME 
 
 
--- server_apps/data_transfer/CiteXploreDOI/lightdois.sh
+++ server_apps/data_transfer/CiteXploreDOI/lightdois.sh
@@ -4,10 +4,12 @@
 [ -a 'logs' ] || mkdir logs ; 
 
 BASEDIR=$TARGETROOT/server_apps/data_transfer/CitexploreDOI
-THISCLASSPATH=$TARGETROOT:$TARGETROOT/citexploredoi.jar:$TARGETROOT/lib/Java/jaxws/jaxws-rt.jar
+THISCLASSPATH=$BASEDIR/citexploredoi.jar:$BASEDIR/lib/Java/jaxws/jaxws-rt.jar
 REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
 LOGNAME=doiupdate`date '+%y.%m.%d'`-light.log  
-echo "running:  java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME" ;
+
+
+
 java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME ;
 mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME
 
