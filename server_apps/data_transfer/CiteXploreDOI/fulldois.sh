03fbdfc514e1517a1e496232405e459b331892f2
Nathan Dunn <ndunn@zfin.org>
2007-05-23 18:23:59 +0000

    updated shell script, fixing the class files
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9406 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/CiteXploreDOI/fulldois.sh
+++ server_apps/data_transfer/CiteXploreDOI/fulldois.sh
@@ -5,13 +5,14 @@
 
 
 BASEDIR=$TARGETROOT/server_apps/data_transfer/CitexploreDOI
-THISCLASSPATH=$BASEDIR/citexploredoi.jar:$BASEDIR/lib/Java/jaxws/jaxws-rt.jar
+THISCLASSPATH=$BASEDIR/citexploredoi.jar:$TARGETROOT/lib/Java/jaxws/jaxws-rt.jar
 REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
 LOGNAME=doiupdate`date '+%y.%m.%d'`-full.log  
 
 
 
 java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>&1  | tee logs/$LOGNAME ;
+echo $THISCLASSPATH >> logs/$LOGNAME ; 
 mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME 
 
 
