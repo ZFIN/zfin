1c96afb01e2caa52b18e5f6a4b395661ccea81cd
Nathan Dunn <ndunn@zfin.org>
2007-05-24 01:14:43 +0000

    fixed jdbc driver and added java name
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9419 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/CiteXploreDOI/lightdois.sh
+++ server_apps/data_transfer/CiteXploreDOI/lightdois.sh
@@ -3,15 +3,17 @@
 
 [ -a 'logs' ] || mkdir logs ; 
 
+JAVA=/local/apps/java/bin/java
+
 BASEDIR=<!--|ROOT_PATH|-->/server_apps/data_transfer/CitexploreDOI
-THISCLASSPATH=$BASEDIR/citexploredoi.jar:<!--|ROOT_PATH|-->/lib/Java/jaxws/jaxws-rt.jar
+THISCLASSPATH=$BASEDIR/citexploredoi.jar:<!--|ROOT_PATH|-->/lib/Java/jaxws/jaxws-rt.jar:/private/apps/tomcat/common/lib/ifxjdbc.jar 
 REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
 LOGNAME=doiupdate`date '+%y.%m.%d'`-light.log  
 DEBUG=-DMAX_DOI_PROCESS=$1
 
 
 
-java  -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME ;
+$JAVA -DINFORMIXSERVER=$INFORMIXSERVER -DDBNAME=$DBNAME $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.DOILookupClient 2>/dev/null  | tee logs/$LOGNAME ;
 echo $THISCLASSPATH >> logs/$LOGNAME ; 
 mailx -s "$LOGNAME"  $REPORTEREMAIL < logs/$LOGNAME
 
