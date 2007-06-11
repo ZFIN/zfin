ffae46cd4b8543267b1d32c036886c114d7592d2
Brock Sprunge <bsprunge@zfin.org>
2007-06-11 23:38:14 +0000

    BUGZID: none Reviewed by: Not reviewed be more precise when parsing the ZDB-ID
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9500 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
+++ server_apps/data_transfer/RefSeq/parseLLintoUNL.pl
@@ -8,10 +8,10 @@ open (UNLMM, ">ll_mm_id.unl") or die "cannot open ll_mm_id.unl";
 
 while ($line = <CUT>) {
   chop $line;
-  ($org,$ll_id,$abbrev,$org_id,$alias1,$dbRef) = split ' ',$line,6;
+  ($org,$ll_id,$abbrev,$org_id,$alias1,$dbRef,$tail) = split ' ',$line,7;
   
 
-  if ($org eq "7955" && $dbRef =~ "^ZFIN:(.*)") {
+  if ($org eq "7955" && $dbRef =~ "^ZFIN:(ZDB-GENE-[0-9]*-[0-9]*)") {
     print UNLDR "$ll_id|$1|\n";
   }  
   elsif ($org eq "9606" && $dbRef ) {
