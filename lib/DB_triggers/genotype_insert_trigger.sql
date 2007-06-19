a734bd72eb193141ad804e9e56d1f8b31dd67c94
Sierra Moxon <smoxon@zfin.org>
2007-06-19 17:33:27 +0000

    BUGZID: none Reviewed by: Not reviewed took call out as it conflicts
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9528 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/genotype_insert_trigger.sql
+++ lib/DB_triggers/genotype_insert_trigger.sql
@@ -7,7 +7,6 @@ create trigger genotype_insert_trigger insert on genotype
         execute function scrub_char(new_genotype.geno_handle ) 
     into genotype.geno_handle,
         execute function zero_pad(new_genotype.geno_display_name ) 
-    into genotype.geno_name_order,
-	execute procedure regen_names_genotype(new_genotype.geno_zdb_id)
+    into genotype.geno_name_order
 
 );
