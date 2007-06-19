76f8f2cb22db8e4d00c6069d53490b50508d2f47
Sierra Moxon <smoxon@zfin.org>
2007-06-19 17:40:51 +0000

    BUGZID: none Reviewed by: Not reviewed rollback
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9530 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/geno_display_name_update_trigger.sql
+++ lib/DB_triggers/geno_display_name_update_trigger.sql
@@ -5,6 +5,7 @@ create trigger geno_display_name_update_trigger update of geno_display_name
         execute function scrub_char(new_genotype.geno_display_name ) 
     into genotype.geno_display_name,
         execute function zero_pad(new_genotype.geno_display_name ) 
-    into genotype.geno_name_order,
-	execute procedure regen_names_genotype(new_genotype.geno_zdb_id)
+    into genotype.geno_name_order
+--,
+--	execute procedure regen_names_genotype(new_genotype.geno_zdb_id)
 );
