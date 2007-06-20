419d84f5c19e6e7a8852faa22db95d11930f3eec
Nathan Dunn <ndunn@zfin.org>
2007-06-20 19:35:56 +0000

    updated tab to email directly to me and moved next date to tomorrow
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@9537 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/CiteXploreDOI/fulldois.sh
+++ server_apps/data_transfer/CiteXploreDOI/fulldois.sh
@@ -6,7 +6,8 @@ JAVA=/local/apps/java/bin/java
 
 BASEDIR=<!--|ROOT_PATH|-->/server_apps/data_transfer/CitexploreDOI
 THISCLASSPATH=$BASEDIR/citexploredoi.jar:<!--|ROOT_PATH|-->/lib/Java/jaxws/jaxws-rt.jar:/private/apps/tomcat/common/lib/ifxjdbc.jar 
-REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
+#REPORTEREMAIL=<!--|VALIDATION_DOI_EMAIL|-->
+REPORTEREMAIL=ndunn@uoregon.edu
 LOGNAME=/tmp/doiupdate`date '+%y.%m.%d'`-full.log  
 DEBUG=-DMAX_DOI_PROCESS=$1
 
