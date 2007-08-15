8c5ad514c917e9ff731c97e670fc55abfa3495ef
Nathan Dunn <ndunn@zfin.org>
2007-08-15 19:59:18 +0000

    added additional ips
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@10203 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_schema/elsevier_statistics/insert_internal_ips.sql
+++ lib/DB_schema/elsevier_statistics/insert_internal_ips.sql
@@ -1,12 +1,11 @@
 
 begin work ; 
 
--- Start - new static IPs
--- Nathan
-insert into excluded_ip values( '128.223.56.139' ,'internal' ) ; 
--- End - new static IPs
+delete from excluded_ip ; 
+
 
 -- Start - from awstats.zfin.org.conf
+insert into excluded_ip values( '128.223.56.139' ,'internal' ) ; 
 insert into excluded_ip values( '128.223.56.81' ,'internal' ) ; 
 insert into excluded_ip values( '128.223.56.99' ,'internal' ) ; 
 insert into excluded_ip values( '128.223.56.147' ,'internal' ) ; 
@@ -30,7 +29,23 @@ insert into excluded_ip values( '128.223.57.253' ,'internal' ) ;
 -- End - static Ips from awstats.zfin.org.conf
 
 
+-- Start - dynamic Ips from our stats and awstats.zfin.org.conf
+insert into excluded_ip values( '128.223.56.198' ,'internal' ) ; 
+insert into excluded_ip values( '128.223.56.179' ,'internal' ) ; 
+insert into excluded_ip values( '128.223.56.195' ,'internal' ) ; 
+insert into excluded_ip values( '128.223.56.155' ,'internal' ) ; 
+-- End - dynamic Ips from our stats and awstats.zfin.org.conf
 
+
+-- Start - external MS ips
+insert into excluded_ip values( '65.55.213.*' ,'external' ) ; 
+insert into excluded_ip values( '65.54.188.*' ,'external' ) ; 
+insert into excluded_ip values( '65.55.209.*' ,'external' ) ; 
+-- End - external MS ips
+
+-- Start - external bot IP
+insert into excluded_ip values( '38.99.44.102' ,'external' ) ; 
+-- End - external bot IP
 
 
 --rollback work ; 
