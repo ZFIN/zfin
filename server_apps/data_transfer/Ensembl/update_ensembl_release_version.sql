f417de3f0da2b2bde876a42f0de6364c0cab8fee
Informix User <informix@zfin.org>
2007-08-20 00:15:08 +0000

    change v6 to v7
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@10288 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Ensembl/update_ensembl_release_version.sql
+++ server_apps/data_transfer/Ensembl/update_ensembl_release_version.sql
@@ -5,17 +5,17 @@ set constraints for foreign_db_contains.foreign_db_contains_fdb_db_name_foreign_
 set constraints for foreign_db.foreign_db_primary_key deferred;
 
 UPDATE foreign_db
-SET fdb_db_name = 'Ensembl(Zv6)'
+SET fdb_db_name = 'Ensembl(Zv7)'
 WHERE fdb_db_name = 'Ensembl'
 ;
 
 UPDATE foreign_db_contains
-SET fdbcont_fdb_db_name = 'Ensembl(Zv6)'
+SET fdbcont_fdb_db_name = 'Ensembl(Zv7)'
 WHERE fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
 ;
 
 -- PreEnsembl not normally needed
-
+{
 UPDATE foreign_db_contains
 SET fdbcont_fdb_db_name = 'PreEnsembl(Zv7)'
 WHERE fdbcont_zdb_id = 'ZDB-FDBCONT-070718-1'
@@ -25,6 +25,7 @@ UPDATE foreign_db
 SET fdb_db_name = 'PreEnsembl(Zv7)'
 WHERE fdb_db_name = 'PreEnsembl'
 ;
+}
 --rollback work;
 
 --
