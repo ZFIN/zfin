e1a1d760b819d8b179d5f378bd64cb095c977bff
Nathan Dunn <ndunn@zfin.org>
2007-08-22 18:56:46 +0000

    updated to reflect dynamic ips
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@10355 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_schema/elsevier_statistics/insert_internal_ips.sql
+++ lib/DB_schema/elsevier_statistics/insert_internal_ips.sql
@@ -4,36 +4,9 @@ begin work ;
 delete from excluded_ip ; 
 
 
--- Start - from awstats.zfin.org.conf
-insert into excluded_ip values( '128.223.56.139' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.81' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.99' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.147' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.154' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.157' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.160' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.161' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.163' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.175' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.186' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.189' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.191' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.199' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.196' ,'internal' ) ; 
-
-insert into excluded_ip values( '128.223.57.215' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.57.221' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.57.230' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.57.231' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.57.253' ,'internal' ) ; 
--- End - static Ips from awstats.zfin.org.conf
-
-
 -- Start - dynamic Ips from our stats and awstats.zfin.org.conf
-insert into excluded_ip values( '128.223.56.198' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.179' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.195' ,'internal' ) ; 
-insert into excluded_ip values( '128.223.56.155' ,'internal' ) ; 
+insert into excluded_ip values( '128.223.56' ,'internal' ) ; 
+insert into excluded_ip values( '128.223.57' ,'internal' ) ; 
 -- End - dynamic Ips from our stats and awstats.zfin.org.conf
 
 
--- lib/DB_schema/elsevier_statistics/reports/popularagents.sql
+++ lib/DB_schema/elsevier_statistics/reports/popularagents.sql
@@ -1,1 +1,18 @@
-select first 20 count(*) dacount,es_incoming_ip,es_http_user_agent from elsevier_statistics  group by es_incoming_ip,es_http_user_agent order by dacount desc ;
+
+--select avg(DAY(es_date)) avgdate , es_incoming_ip,es_http_user_agent 
+----select first 3 es_date
+--from elsevier_statistics  
+--where es_date >=  TODAY-2
+--and es_date <=  TODAY-1
+--group by es_incoming_ip,es_http_user_agent, avgdate
+--order by es_date desc
+--order by thecount desc ;
+;
+
+select first 5 TODAY-2, count(*) thecount ,es_incoming_ip,es_http_user_agent 
+from elsevier_statistics  
+where es_date >=  TODAY-2
+and es_date <=  TODAY-1
+group by es_incoming_ip,es_http_user_agent  
+order by thecount desc ;
+
