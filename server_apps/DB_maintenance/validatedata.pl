30e1c637db2253b402d4fd095df648ee36242a53
Brock Sprunge <bsprunge@zfin.org>
2007-09-18 22:35:56 +0000

    BUGZID: 2003 Reviewed by: Not reviewed
    exclude EST contains polymorphism
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@10745 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata.pl
+++ server_apps/DB_maintenance/validatedata.pl
@@ -815,6 +815,7 @@ sub containedInRelationshipsInEST ($) {
              where  mrkr_type = 'EST'
              and    (mrkr_zdb_id = mrel_mrkr_2_zdb_id
                      and    mrel_type = mreltype_name
+                     and    mrel_type != 'contains polymorphism'
                      and    mreltype_2_to_1_comments = 'Contained in')
              union
              select mrel_mrkr_1_zdb_id, mrel_mrkr_2_zdb_id, mrkr_type, mrel_type
@@ -822,6 +823,7 @@ sub containedInRelationshipsInEST ($) {
              where  mrkr_type = 'EST'
              and    (mrkr_zdb_id = mrel_mrkr_1_zdb_id
                      and    mrel_type = mreltype_name
+                     and    mrel_type != 'contains polymorphism'
                      and    mreltype_1_to_2_comments = 'Contains')";
 
   my @colDesc = ("Marker ID 1       ",
