b840370a85d99e1e3cfef919bfae4e36e092a425
Peiran Song <peirans@zfin.org>
2007-09-28 22:14:36 +0000

    Add more comments. and add verify_stage_abbrev.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@10911 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Anatomy/loadMaster.sh
+++ server_apps/data_transfer/Anatomy/loadMaster.sh
@@ -22,34 +22,43 @@ setenv INFORMIXSQLHOSTS <!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->
 #--------------------------------------
 # Check and prepare AO_translation.unl
 #---------------------------------------
+echo "=== Process AO_translation.unl, Verify stage abbrev ==="
 if (-e AO_translation.unl) then
     # ensure one and only one trailing bar 
     /bin/sed 's/\|$//' AO_translation.unl | /bin/sed 's/$/\|/' > n_AO_translation.unl
     /bin/mv n_AO_translation.unl AO_translation.unl
+
+    $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> verify_stage_abbrev.sql
+
 else
-    echo "Cannot find AO_translation.unl, continue?";
-    set goahead = $< 
-    if ($goahead == 'n') then
+    echo "WARNING: Cannot find AO_translation.unl";
+
+endif
+
+echo "Good to continue? ";
+
+set goahead = $< 
+if ($goahead == 'n') then
       exit
-    endif 
 endif 
+
 
 #--------------------------------------
 # Check and prepare stageKeyword.unl
 #---------------------------------------
-
+echo "=== Process stageKeyword.csv  ==="
 if (-e stageKeyword.csv) then
 
     ./parse_stage_keywords.pl
 
 else 
-    echo "Cannot find stageKeyword.unl";
+    echo "WARNING: Cannot find stageKeyword.unl";
 endif 
 
 #--------------------------------------
 # Run loading
 #---------------------------------------
-
+echo "=== Load AO  ==="
 $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> loadAO.sql
 
 #-------------------------------------------
@@ -57,7 +66,7 @@ $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> loadAO.sql
 # and run it on entries in AO_translation.unl
 # then, check annotation again for any omission
 #-------------------------------------------
-
+echo "=== Adjust annotation  ==="
 $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> batch_xpat_annot_adjust.sql
 
 $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> make_annot_translation.sql
@@ -68,8 +77,9 @@ $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> check_annotation.sql
 # Check keywords in Thisse template
 # Email results to <!--|AO_EMAIL_CURATOR|-->
 #---------------------------------------
-
+echo "=== Check Thisse keyword file  ==="
 $INFORMIXDIR/bin/dbaccess <!--|DB_NAME|--> check_thisse_keywords.sql
+
 
 set SUBJECT = "Auto: Problem AO in Thiss Template"
 set MAILTO = `echo "<!--|AO_EMAIL_CURATOR|-->" | tr -d '\\' `;
@@ -80,10 +90,14 @@ echo "Subject: $SUBJECT" >> /tmp/AO_thisse_mail
 echo "Mime-Version: 1.0" >> /tmp/AO_thisse_mail
 echo "Content-Type: text/plain" >> /tmp/AO_thisse_mail
 
-echo "Keywords no long exist:" >> /tmp/AO_thisse_mail
+echo "Keywords no longer being the primary name :" >> /tmp/AO_thisse_mail
+echo "-------------------------------------------" >>  /tmp/AO_thisse_mail
 cat ./kwdNameNotPrim.err >> /tmp/AO_thisse_mail
 
+echo " " >> /tmp/AO_thisse_mail
+
 echo "Keywords has stage problem:" >> /tmp/AO_thisse_mail
+echo "-----------------------------" >>  /tmp/AO_thisse_mail
 cat ./kwdStageInconsis.err >> /tmp/AO_thisse_mail
 
 /usr/lib/sendmail -t -oi < /tmp/AO_thisse_mail
