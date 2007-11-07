2b1426b70c94fcfb3dabc98adb8e128b5e082601
Xiang Shao <xshao@zfin.org>
2007-11-07 18:06:54 +0000

    for FogBugz  Case 1979:   update to gp2protein file
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11376 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GO/gp2protein.pl
+++ server_apps/data_transfer/GO/gp2protein.pl
@@ -15,7 +15,7 @@ $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
 ### open a handle on the db
 my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
                        '', 
-                       '', 
+                       '',                     
 		       {AutoCommit => 1,RaiseError => 1}
 		      )
   || emailError("Failed while connecting to <!--|DB_NAME|-->"); 
@@ -28,47 +28,71 @@ $outFile = "gp2protein.zfin";
 &gp2proteinReport();
 
 $dbh->disconnect();
-
+      
+###undef %validPids;
 exit;
 
-#-------------------------------------------------------------
+#---------------------------------------------------------------------------
 #
 sub gp2proteinReport()
 {
     system("/bin/rm -f $outFile");
     open (REPORT, ">$outFile") or die "cannot open report";
+    ###open (ERRREPORT, ">invalid_id_gp2protein.zfin") or die "cannot open invalid_id_gp2protein.zfin";
     print REPORT "!Version: \$"."Revision\$ \n";
     print REPORT "!Date: \$"."Date\$ \n";
     print REPORT "!From: ZFIN (zfin.org) \n";
     print REPORT "! \n";
 
-    my $cur = $dbh->prepare('select distinct mrkr_zdb_id,
-                                    dblink_acc_num
-                             from   marker,db_link, foreign_db_contains
-                             where  mrkr_zdb_id = dblink_linked_recid
-                               and  dblink_fdbcont_zdb_id = fdbcont_zdb_id 
-                               and  fdbcont_fdb_db_name = "UniProt"
-                             order by mrkr_zdb_id;'
+    my $cur = $dbh->prepare('select distinct mrkr_zdb_id,fdbcont_fdb_db_name,dblink_acc_num
+                               from marker,db_link, foreign_db_contains
+                              where mrkr_zdb_id = dblink_linked_recid
+                                and dblink_fdbcont_zdb_id = fdbcont_zdb_id
+                                and mrkr_zdb_id like "ZDB-GENE-%"
+                                and fdbcont_fdb_db_name in ("UniProt","RefSeq","GenPept");'
 			   );
     $cur->execute;
-    my($mrkr_id, $acc_num);
-    $cur->bind_columns(\$mrkr_id,\$acc_num);
-    my $gene = '';
-    my $start = 1;
+    my($mrkr_id,$db_name,$acc_num);
+    $cur->bind_columns(\$mrkr_id,\$db_name,\$acc_num);
+    
+    my %zdbIDs = ();
+    my %zdbPids = ();
+    my %zdbRefSeqs = ();
+    my %zdbGenPepts = ();
+    ###my %invalidIDs = ();
     while ($cur->fetch) {
-	if ($gene eq $mrkr_id) {
-	    print REPORT ";UniProt:$acc_num";
-	}
-	else {
-	    print REPORT "\n" if !$start;
-	    print REPORT "ZFIN:$mrkr_id\tUniProt:$acc_num";
-	    $gene=$mrkr_id;
-	}
-	$start = 0 if $start;
+      $zdbIDs{$mrkr_id} = 1;
+      if($db_name eq "UniProt") {
+   ### if(exists $validPids{$acc_num}) {
+	  if (!exists $zdbPids{$mrkr_id}) {
+	    $zdbPids{$mrkr_id} = "UniProt:$acc_num";
+	  } else {
+            $zdbPids{$mrkr_id} = "$zdbPids{$mrkr_id};UniProt:$acc_num";
+	  }
+   ### } else {
+   ###     $invalidIDs{$acc_num} = 1;
+   ### }
+      } elsif($db_name eq "RefSeq" && $acc_num =~ m/^NP_\d+$/) {
+          if (!exists $zdbRefSeqs{$mrkr_id}) {  
+            $zdbRefSeqs{$mrkr_id} = "NCBI_NP:$acc_num";
+          } else {
+            $zdbRefSeqs{$mrkr_id} = "$zdbRefSeqs{$mrkr_id};NCBI_NP:$acc_num";
+          }
+      } elsif($db_name eq "GenPept") {
+          if (!exists $zdbGenPepts{$mrkr_id}) {  
+            $zdbGenPepts{$mrkr_id} = "NCBI_GP:$acc_num";
+          } else {
+            $zdbGenPepts{$mrkr_id} = "$zdbGenPepts{$mrkr_id};NCBI_GP:$acc_num";
+          }
+      }
     }
-    print REPORT "\n";
+    
+    foreach $k (sort keys %zdbIDs) {
+      $v1 = $zdbPids{$k} if exists $zdbPids{$k};
+      $v2 = $zdbRefSeqs{$k} if exists $zdbRefSeqs{$k};
+      $v3 = $zdbGenPepts{$k} if exists $zdbGenPepts{$k};
+      print REPORT "ZFIN:$k\t$v1;$v2;$v3\n";     
+    }
+    
     close(REPORT);
 }
-
-
-
