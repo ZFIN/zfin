0376df38bac5782445b48c008b95c5cce73cf9cd
Christian Pich <cmpich@zfin.org>
2007-11-21 18:15:20 +0000

    password.sql
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11527 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_schema/Anatomy_search/password.sql
@@ -0,0 +1,18 @@
+-- These db changes are part of the Anatomy Browser modifications.
+-- Included are a migration of the passwords from unix crypt() to MD5 encryption.
+-- The new encrypted passwords are stored in a file called zdb_md5.unl
+begin work;
+
+-- password conversion to MD5
+create temp table passwordTemp (
+ zdbid   varchar(50),
+ password varchar(40)
+ ) with no log;
+
+load from 'zdb_md5.unl' insert into passwordTemp;
+
+update zdb_submitters set password = (
+ select password from passwordTemp where zdbid = zdb_id);
+
+--rollback work;
+commit work;
