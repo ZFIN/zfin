db01175c8824a81ba8806a33871c5b9daab0848c
Tom Conlin <tomc@zfin.org>
2007-11-28 22:14:13 +0000

    the Sanger FPC links had become stale and obsolete so Kerstin OKed theit removal
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11568 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Sanger/FPC/commit.sql
+++
@@ -1,4 +0,0 @@
-commit work;
-drop table fpc_marker;
-drop table fpc_clone;
-drop table fpc_contig;
--- server_apps/data_transfer/Sanger/FPC/rollback.sql
+++
@@ -1,4 +0,0 @@
-rollback work;
-drop table fpc_marker;
-drop table fpc_clone;
-drop table fpc_contig;
