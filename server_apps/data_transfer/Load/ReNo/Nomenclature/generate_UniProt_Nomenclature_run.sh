66494b34ade6b22a3b9963d43dfee8792a39f61f
Tom Conlin <tomc@zfin.org>
2007-12-07 21:31:38 +0000

    BUGZID: 887 Reviewed by: Not reviewed
    Inital add of ReNo Nomenclature generating script
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11641 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/Load/ReNo/Nomenclature/generate_UniProt_Nomenclature_run.sh
@@ -0,0 +1,51 @@
+#! /bin/tcsh
+# generate_nom_can_fasta.sh
+
+# find and rank zfin objects in need of renaming.
+# generated longest protenin fasta file to blast 
+# 
+
+
+set bin_pth="/private/apps/wublast";
+set current="/research/zblastdb/db/Current";
+set timestamp="`date +%Y%m%d`"
+set here="`pwd`"
+
+dbaccess $DBNAME select_nomemclature_candidates.sql;
+cat nomenclature_candidate_pp.unl | cut -f 4,5 -d \| | sort -u > ! keys.txt
+
+# zero out the fasta file
+cat /dev/null >! accession.pp
+
+foreach key (`cat keys.txt`)
+	echo $key
+	switch ($key)
+		#case "Polypeptide|GenPept":
+		#	$xdget -p  $current/sptr_zf `grep $key nomenclature_candidate_pp.unl | cut -f3 -d \|` >> accession.pp
+		#breaksw
+		case "Polypeptide|RefSeq":
+			$bin_pth/xdget -p  $current/refseq_zf_aa `grep $key nomenclature_candidate_pp.unl | cut -f3 -d \|` >> accession.pp
+		breaksw
+		case "Polypeptide|SWISS-PROT":
+			$bin_pth/xdget -p  $current/sptr_zf `grep $key nomenclature_candidate_pp.unl | cut -f3 -d \|` >> accession.pp
+		breaksw
+		case "cDNA|Genbank":
+			$bin_pth/xdget -n  $current/gbk_gb_zf `grep $key nomenclature_candidate_nt.unl | cut -f3 -d \|` >> accession.nt
+		breaksw
+		case "cDNA|RefSeq":
+			$bin_pth/xdget -n  $current/refseq_zf_rna `grep $key nomenclature_candidate_nt.unl | cut -f3 -d \|` >> accession.nt
+		breaksw
+		case "other|VEGA":
+			$bin_pth/xdget -nd  $current/vega_zfin `grep $key nomenclature_candidate_nt.unl | cut -f3 -d \|` >> accession.nt
+		breaksw
+		default:
+			echo "$key not selected"
+		breaksw
+	endsw
+end # foreach
+
+echo "on EMBRYONIX blast the nomenclature set against Human & mouse & zebrafish proteins"
+
+ssh embryonix "cd $here;nice +10 $bin_pth/blastp \"$current/sptr_hs $current/sptr_ms $current/sptr_zf\" accession.pp -E e-50 >! UniProt_$timestamp.out"
+
+/research/zcentral/Commons/bin/parse-blast-reno.r UniProt_$timestamp.out "UniProt_$timestamp" 
