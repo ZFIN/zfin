27552e09498ba44d317465b260af25685b8e84f0
Kevin Schaper <kschaper@zfin.org>
2007-12-11 22:08:58 +0000

    Fixed two tests that expected reno data to always exist. Now, they check that if there is a run, there are hits and  queries.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11662 908b024a-c82b-635c-c293-f2b15b23277c

--- test/org/zfin/sequence/blast/BlastRepositoryTest.java
+++ test/org/zfin/sequence/blast/BlastRepositoryTest.java
@@ -3,18 +3,22 @@ package org.zfin.sequence.blast ;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.zfin.TestConfiguration;
 
 import org.zfin.sequence.blast.repository.BlastRepository  ; 
-import org.zfin.sequence.blast.repository.HibernateBlastRepository  ; 
+import org.zfin.sequence.blast.repository.HibernateBlastRepository  ;
+import org.zfin.sequence.reno.Run;
 
 import org.hibernate.SessionFactory ;
 import org.hibernate.Session;
+import org.hibernate.Criteria;
 
 
+import org.zfin.framework.HibernateSessionCreator ;
+import org.zfin.framework.HibernateUtil ;
 
-import org.zfin.framework.HibernateSessionCreator ; 
-import org.zfin.framework.HibernateUtil ; 
+import java.util.List;
 
 /**
  *  Class BlastRepositoryTest.
@@ -43,21 +47,33 @@ public class BlastRepositoryTest {
     @Test
     public void testHitEntity(){
         Session session = HibernateUtil.currentSession();
-        String hsqlString = "from org.zfin.sequence.blast.Hit" ; 
-        org.hibernate.Query query = session.createQuery(hsqlString) ;
-        query.setMaxResults(1) ;
-        Hit hit =  (Hit) query.uniqueResult() ; 
-        assertNotNull("database contains at least one blast hit",hit  ) ; 
+        Criteria runCrit = session.createCriteria(Run.class);
+        Criteria hitCrit = session.createCriteria(Hit.class);
+
+        List<Run> runs = runCrit.list();
+        List<Hit> hits = hitCrit.list();
+
+        if (runs.size() > 0) {
+            assertTrue("If there is a run in the database, there should be some hits",  hits.size() > 0  );
+        }
+
+
     }
 
     @Test
     public void testQueryEntity(){
         Session session = HibernateUtil.currentSession();
-        String hsqlString = "from Query" ; 
-        org.hibernate.Query query = session.createQuery(hsqlString) ;
-        query.setMaxResults(1) ;
-        org.zfin.sequence.blast.Query queryEntity =  (org.zfin.sequence.blast.Query) query.uniqueResult() ; 
-        assertNotNull("database contains at least one blast query",queryEntity  ) ; 
+
+        Criteria runCrit = session.createCriteria(Run.class);
+        Criteria queryCrit = session.createCriteria(Query.class);
+
+        List<Run> runs = runCrit.list();
+        List<Hit> queries = queryCrit.list();
+
+        if (runs.size() > 0) {
+            assertTrue("If there is a run in the database, there should be some queries",  queries.size() > 0  );
+        }
+        
     }
 
 
