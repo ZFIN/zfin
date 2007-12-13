1249fa963a74e840992725c386aafd8cdf25bc5d
Kevin Schaper <kschaper@zfin.org>
2007-12-13 20:44:39 +0000

    BUGZID: 2217 Reviewed by: Not reviewed
    Removed ListSelector applet  (the one that used to be used for linking pubs)
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11686 908b024a-c82b-635c-c293-f2b15b23277c

--- client_apps/ListSelector/Selectable.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public interface Selectable {
-	public String get_data();
-        public String get_label();
-	public String expand(String results,String pre,String post,String sep);
-	public Vector get_children();
-}
