130c28a3afa8f01fabb8919e37d1f8c4da9a7446
Tom Conlin <tomc@zfin.org>
2007-12-18 20:34:33 +0000

    BUGZID: 2244 Reviewed by: Not reviewed
    changed Entrez Gene ID to be an integer
    and added an index on symbol/name
    and xref to speed up dropping redundamt/existing rows
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11714 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Load/Entrez/load_entrez_orth.sql
+++ server_apps/data_transfer/Load/Entrez/load_entrez_orth.sql
@@ -9,7 +9,7 @@ drop table tmp_entrez_orth_xref;
 -- entrez_to_protein
 create table tmp_entrez_orth_prot(
 	teop_taxid varchar(30),
-	teop_entrez_id varchar(50),
+	teop_entrez_id integer,
 	teop_protein_acc varchar(50)
 )
 fragment by round robin in tbldbs1,tbldbs2,tbldbs3
@@ -25,7 +25,7 @@ create index  tmp_entrez_orth_prot_eop_entrez_idx
     on  tmp_entrez_orth_prot (teop_entrez_id)
     using btree in idxdbs3
 ;
-create index  tmp_entrez_orth_prot_eop_protein_acc_idx
+create index tmp_entrez_orth_prot_eop_protein_acc_idx
     on  tmp_entrez_orth_prot (teop_protein_acc)
     using btree in idxdbs2
 ;
@@ -37,22 +37,25 @@ update statistics high for table tmp_entrez_orth_prot;
 -- entrez_gene
 
 create table tmp_entrez_orth_name(
-    teon_entrez_id varchar(50), --eg_acc_num
+    teon_entrez_id integer, --eg_acc_num
     teon_symbol varchar(60),
     teon_name varchar(255)
 )
 fragment by round robin in tbldbs1,tbldbs2,tbldbs3
 ;
 
-
 ! echo "import name table"
 load from 'entrez_orth_name.unl' insert into tmp_entrez_orth_name;
 
-! echo "forget names not associated with proteins"
-delete from tmp_entrez_orth_name
- where teon_entrez_id not in (
-    select teop_entrez_id from tmp_entrez_orth_prot
-);
+create unique index tmp_entrez_orth_name_eon_entrez_idx
+ on tmp_entrez_orth_name(teon_entrez_id)
+ using btree in idxdbs3 ;
+
+create unique index tmp_entrez_orth_name_eon_sym_name_idx
+ on tmp_entrez_orth_name(teon_entrez_id,teon_symbol,teon_name)
+ using btree in idxdbs2 ;
+
+update statistics high for table tmp_entrez_orth_name ;
 
 ! echo "need to stuff name table with NULL name records ..."
 select distinct teop_entrez_id
@@ -62,15 +65,17 @@ select distinct teop_entrez_id
      from  tmp_entrez_orth_name
  )into temp tmp_eg_id with no log
 ;
-
 insert into tmp_entrez_orth_name(teon_entrez_id)
-select teop_entrez_id from tmp_eg_id
+select distinct teop_entrez_id from tmp_eg_id
 ;
 drop table tmp_eg_id;
 
-create unique index tmp_entrez_orth_name_eon_entrez_idx
- on tmp_entrez_orth_name(teon_entrez_id)
- using btree in idxdbs3 ;
+! echo "forget names not associated with proteins"
+delete from tmp_entrez_orth_name
+ where teon_entrez_id not in (
+    select teop_entrez_id from tmp_entrez_orth_prot
+);
+
 
 update statistics high for table tmp_entrez_orth_name ;
 
@@ -80,7 +85,7 @@ update statistics high for table tmp_entrez_orth_name ;
 -- entrez_to_xref
 
 create table tmp_entrez_orth_xref(
-    teox_entrez_id varchar(50),
+    teox_entrez_id integer,
     teox_xref varchar(30)
 )
 fragment by round robin in tbldbs1,tbldbs2,tbldbs3
@@ -88,32 +93,41 @@ fragment by round robin in tbldbs1,tbldbs2,tbldbs3
 
 ! echo "import xref table"
 load from 'entrez_orth_xref.unl' insert into tmp_entrez_orth_xref;
+
 create index tmp_entrez_orth_xref_eox_entrez_idx
  on tmp_entrez_orth_xref(teox_entrez_id)
  using btree in idxdbs3 ;
+
+update statistics high for table tmp_entrez_orth_xref ;
 
 delete from tmp_entrez_orth_xref
  where teox_entrez_id not in (
     select teop_entrez_id from tmp_entrez_orth_prot
 );
+
+create index tmp_entrez_orth_xref_eox_xref_idx
+ on tmp_entrez_orth_xref(teox_xref)
+ using btree in idxdbs2;
+
 update statistics high for table tmp_entrez_orth_xref ;
 
 ! echo "entrez_orth_xref loaded `date`"
-
+! echo ""
 -------------------------------------------------------------------
 -------------------------------------------------------------------
 -------------------------------------------------------------------
 
 ! echo "drop existing Entrez orthology that is not in current load"
 ! echo "expect this to cascade to protein and xref tables"
+
 delete from entrez_gene where not exists (
    select 1 from tmp_entrez_orth_prot
-    where teop_protein_acc = eg_acc_num
+    where teop_entrez_id = eg_acc_num
 );
 ! echo "orphaned orth in ZFIN dropped `date`"
 
 
-! echo "existing symbols may have changed"
+! echo "existing symbol may have changed"
 update entrez_gene set eg_symbol = (
     select teon_symbol
      from tmp_entrez_orth_name
@@ -123,10 +137,8 @@ update entrez_gene set eg_symbol = (
      where teon_entrez_id = eg_acc_num
      and teon_symbol != eg_symbol
 );
-! echo "fixed entrez gene orth symbols in zfin updated `date`"
 
-
-! echo " existing names may have changed"
+! echo "existing name may have changed"
 update entrez_gene set eg_name = (
     select teon_name
      from tmp_entrez_orth_name
@@ -136,7 +148,8 @@ update entrez_gene set eg_name = (
      where teon_entrez_id = eg_acc_num
      and teon_name != eg_name
 );
-! echo "fixed entrez gene orth names in zfin updated `date`"  
+
+! echo "fixed entrez gene orth symbols & names in zfin updated `date`"
 
 ! echo "existing xrefs may have changed"
 update entrez_to_xref set ex_xref = (
@@ -148,7 +161,7 @@ update entrez_to_xref set ex_xref = (
      where teox_entrez_id = ex_entrez_acc_num
      and teox_xref != ex_xref
 );
-! echo "fixed entrez gene orth xrefs in zfin updated `date`"  
+! echo "fixed entrez gene orth xrefs in zfin updated `date`"
 
 --------------------------------------------------------------------------
 
@@ -159,8 +172,7 @@ delete from tmp_entrez_orth_prot where exists (
       and teop_protein_acc = ep_protein_acc_num
       and teop_taxid = ep_organism_common_name
 );
-! echo "existing entrez gene in update dropped `date`"  
-
+! echo "existing entrez gene in update dropped `date`"
 
 ! echo "delete the entrez_name associations that already exist"
 delete from tmp_entrez_orth_name where exists (
@@ -168,6 +180,16 @@ delete from tmp_entrez_orth_name where exists (
     where teon_entrez_id = eg_acc_num
       and teon_symbol = eg_symbol
       and teon_name = eg_name
+);
+
+! echo "delete the entrez_name associations that are all NULL"
+delete from tmp_entrez_orth_name where exists (
+   select 1 from entrez_gene
+    where teon_entrez_id = eg_acc_num
+      and teon_symbol is NULL
+      and   eg_symbol is NULL
+      and teon_name is NULL
+      and   eg_name is NULL
 );
 
 
@@ -181,15 +203,29 @@ delete from tmp_entrez_orth_xref where exists (
 ! echo "bulk load filtered `date`"
 --------------------------------------------------------------------
 -- whatever is left is new
+{
+select count(*) dup_check
+ from tmp_entrez_orth_name
+ where exists (
+ 	select 1 from entrez_gene
+ 	 where eg_acc_num = teon_entrez_id
+ 	   and eg_symbol = teon_symbol
+ 	   and eg_name = teon_name
+ );
+
+select count(*) from tmp_entrez_orth_name;
+
+select first 40 * from tmp_entrez_orth_name;
+}
 
 insert into entrez_gene (
 	eg_acc_num,
 	eg_symbol,
 	eg_name
 )
-select  * from  tmp_entrez_orth_name
+select distinct * from  tmp_entrez_orth_name
 ;
-! echo "entrez gene name  updated `date`"  
+! echo "entrez gene name  updated `date`"
 
 insert into entrez_to_protein (
 	ep_organism_common_name,
@@ -198,7 +234,7 @@ insert into entrez_to_protein (
 )
 select distinct * from  tmp_entrez_orth_prot
 ;
-! echo "entrez gene prot  updated `date`"  
+! echo "entrez gene prot  updated `date`"
 
 insert into entrez_to_xref (
 	ex_entrez_acc_num,
@@ -206,7 +242,7 @@ insert into entrez_to_xref (
 )
 select distinct * from  tmp_entrez_orth_xref
 ;
-! echo "entrez gene xref updated `date`"  
+! echo "entrez gene xref updated `date`"
 
 --------------------------------------------------------------------------
 drop table tmp_entrez_orth_prot;
