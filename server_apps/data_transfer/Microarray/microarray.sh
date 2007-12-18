71f77b2b7581e424b225341474c3af2a7d15de8e
Nathan Dunn <ndunn@zfin.org>
2007-12-18 21:15:23 +0000

    BUGZID: 909 Reviewed by: Not reviewed
    Merged from b2007-11-20-r_head_fb909_microarray_ZFIN_WWW.  Introduced microarrays for GEO using the GPL1319 chipset.
    
    Added Microarray directory.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11722 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/Microarray/microarray.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+#- load SOFT format from NCBI using curl for platforms GPL_1319 and GPL2715
+MICROARRAY_PATH=<!--|ROOT_PATH|-->/server_apps/data_transfer/Microarray
+VALUE=`$MICROARRAY_PATH/collect.sh`
+
+#echo ": '$VALUE'"
+
+if [ "$VALUE" = "1" ] ; then
+echo "No new data, exiting" ; 
+exit ; 
+else
+echo "New data, running!" ; 
+fi
+
+# parse soft files to get GenBank accessions 
+# read from !platform_table_begin
+# read to !platform_table_end
+# tab delimited, second column  
+
+#- get gene ZdbIds based on the GenBank accessions
+
+#- add DBLinks for the genes in question
+
+#- in markerview, query DBLink for name versus ncbi, ebi, etc.
+
+WEBINFDIR=<!--|ROOT_PATH|-->/home/WEB-INF
+
+WEBINFJARS=""
+for i in `ls $WEBINFDIR/lib/*.jar`; do
+    WEBINFJARS=$i:$WEBINFJARS ;  
+done
+
+JAVA=/local/apps/java/bin/java
+
+THISCLASSPATH=$WEBINFDIR/classes:<!--|ROOT_PATH|-->/lib/Java/jaxws/jaxws-rt.jar:<!--|ROOT_PATH|-->/lib/Java/jaxws/wsdl-client.jar:$WEBINFJARS:/private/apps/tomcat/common/lib/ifxjdbc.jar
+
+
+DEBUG=-DMAX_DOI_PROCESS=$1
+$JAVA -Dlog4j.configuration=file://<!--|ROOT_PATH|-->/server_apps/data_transfer/Microarray/log4j.xml -DCONFIGURATION_DIRECTORY="<!--|ROOT_PATH|-->/home/WEB-INF/classes/org/zfin" -DDBNAME=<!--|DB_NAME|--> -DSQLHOSTS_HOST=<!--|SQLHOSTS_HOST|--> -DINFORMIX_SERVER=<!--|INFORMIX_SERVER|--> -DINFORMIX_PORT=<!--|INFORMIX_PORT|--> $DEBUG -cp $THISCLASSPATH org.zfin.datatransfer.UpdateMicroArrayMain;
+
+REPORTEREMAIL=<!--|VALIDATION_EMAIL_DBA|-->
+
+DATESTRING="`date '+%y.%m.%d'`"
+
+LOG="/tmp/microarray_info.log" ; 
+SUBJECT="Microarray processing information for $DATESTRING.log"
+mailx -s "$SUBJECT"  $REPORTEREMAIL < $LOG 
+
+LOG="/tmp/microarray_error.log" ; 
+if [ "`cat $LOG`" ] ; then 
+    SUBJECT="Microarray error information for $DATESTRING.log"
+    mailx -s "$SUBJECT"  $REPORTEREMAIL < $LOG
+fi;
+
+LOG="/tmp/microarray_notfound.log" ; 
+if [ "`cat $LOG`" ] ; then 
+    NOTFOUND_SUBJECT="Microarray markers not found in genbank for $DATESTRING.log"
+    mailx -s "$NOTFOUND_SUBJECT"  $REPORTEREMAIL < $LOG
+fi ; 
+
+
+
