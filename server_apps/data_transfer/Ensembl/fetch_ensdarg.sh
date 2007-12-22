a47ed6502bf87d80edebd0e7c2b28f8b5b85a6ce
Tom Conlin <tomc@zfin.org>
2007-12-22 02:55:00 +0000

    BUGZID: 2251 Reviewed by: Not reviewed
    Ensembl has upgraded their MySQL engin and change port on the new instance to 5306
    so they can continue to make old data avilable on the old port. The change also
    resulted in no longer having permission to make temp tables so the MySQL query is
    now a single statement.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11766 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Ensembl/fetch_ensdarg.sh
+++ server_apps/data_transfer/Ensembl/fetch_ensdarg.sh
@@ -2,10 +2,6 @@
 
 
 # find the name of the most current core database
-
-# changed old 2007 October 
-#ftp://ftp.ensembl.org/pub/current_danio_rerio/data/mysql/
-
 /local/bin/curl -slo cur_ens_db.txt ftp://ftp.ensembl.org/pub/current_mysql/
 
 # strip/convert non-unix line endings
@@ -20,11 +16,9 @@ echo "Using Ensembl release: $cur"
 # send a query to the current database returning 1:1 ensdargs and zdbids
 # return in informix's load file format.
 /usr/bin/cat fetch_ensdarg.sql | \
-/local/bin/mysql -A -P3306 -u anonymous -h ensembldb.ensembl.org -si -D $cur |\
+/local/bin/mysql -A -P5306 -u anonymous -h ensembldb.ensembl.org -si -D $cur |\
 /usr/bin/sed 's/\(ZDB-GENE-[0-9\-]*\).*\(ENSDARG[0-9]*\).*/\1|\2|/g' |\
 /usr/bin/tr '\011' \| >!  ensdarg.unl;
-
-
 
 # load the file from Ensembl mysql into the local database
 # rollback if not called with the (first) argument "commit"
--- server_apps/data_transfer/Ensembl/fetch_ensdarg.sql
+++ server_apps/data_transfer/Ensembl/fetch_ensdarg.sql
@@ -1,18 +1,9 @@
-begin work;
-create temporary table tmp_unizdb
-select xref.dbprimary_acc tuz_zdb
- from  gene_stable_id gsi, gene, xref
- where gsi.gene_id = gene.gene_id
- and gene.display_xref_id = xref.xref_id
- and xref.external_db_id in (2510,2530)
- group by 1 having count(*) = 1
+select x.dbprimary_acc, gsi.stable_id 
+ from gene_stable_id gsi, gene g, xref x 
+ where gsi.gene_id = g.gene_id 
+   and g.display_xref_id = x.xref_id 
+   and x.external_db_id in (2510,2530) 
+ group by x.dbprimary_acc having count(x.dbprimary_acc) = 1 
+ order by gsi.stable_id
 ;
 
-select distinct xref.dbprimary_acc, gsi.stable_id
- from  gene_stable_id gsi, gene, xref, tmp_unizdb
-where gsi.gene_id = gene.gene_id
-  and gene.display_xref_id = xref.xref_id
-  and xref.external_db_id in (2510,2530)
-  and xref.dbprimary_acc  = tuz_zdb
-order by 2
-;
