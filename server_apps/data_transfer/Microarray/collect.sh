0d8966c8c9c2a39cb04e1232622dea5473a74555
Nathan Dunn <ndunn@zfin.org>
2007-12-27 19:42:04 +0000

    updated to use wget and added 2715
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11773 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Microarray/collect.sh
+++ server_apps/data_transfer/Microarray/collect.sh
@@ -2,20 +2,20 @@
 
 JAVA=/local/apps/java/bin/java
 
-COLLECT_LOG=/tmp/collect.log
 WEBINFDIR=<!--|ROOT_PATH|-->/home/WEB-INF
 MICROARRAY_PATH=<!--|ROOT_PATH|-->/server_apps/data_transfer/Microarray
 THISCLASSPATH=$WEBINFDIR/classes:$WEBINFJARS:/private/apps/tomcat/common/lib/ifxjdbc.jar
 CP=/usr/bin/cp
-CURL=/usr/local/bin/curl
+#CURL=/usr/local/bin/curl
+WGET=/local/bin/wget
 GUNZIP=/usr/local/bin/gunzip
 TOUCH=/usr/bin/touch
 
 cd $MICROARRAY_PATH ;
 
 FILENAME=GPL1319
-$CP -f ${FILENAME}_family.soft old.${FILENAME}_family.soft 2>&1 | > $COLLECT_LOG
-$CURL -o  ${FILENAME}_family.soft.gz ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SOFT/by_platform/${FILENAME}/${FILENAME}_family.soft.gz 2>&1 | > $COLLECT_LOG 
+$CP -f ${FILENAME}_family.soft old.${FILENAME}_family.soft ; 
+$WGET -q -O ${FILENAME}_family.soft.gz ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SOFT/by_platform/${FILENAME}/${FILENAME}_family.soft.gz   ; 
 $GUNZIP -f ${FILENAME}_family.soft.gz 
 
 # make sure that this file always exists
@@ -29,12 +29,12 @@ CHK2=`cksum ${FILENAME}_family.soft`
 #echo "CHK1 $CHK1" ; 
 #echo "CHK2 $CHK2" ; 
 
-#FILENAME=GPL2715
-#$CP -f $MICROARRAY_PATH/${FILENAME}_family.soft $MICROARRAY_PATH/old.${FILENAME}_family.soft 2>&1 | > $COLLECT_LOG
-#$CURL -o ${FILENAME}_family.soft.gz ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SOFT/by_platform/${FILENAME}/${FILENAME}_family.soft.gz 2>&1 | > $COLLECT_LOG
-#$GUNZIP -f $MICROARRAY_PATH/${FILENAME}_family.soft.gz 2>&1 | > $COLLECT_LOG
-#CHK3=`cksum $MICROARRAY_PATH/old.${FILENAME}_family.soft | cut -f1`
-#CHK4=`cksum $MICROARRAY_PATH/${FILENAME}_family.soft | cut -f1`
+FILENAME=GPL2715
+$CP -f $MICROARRAY_PATH/${FILENAME}_family.soft $MICROARRAY_PATH/old.${FILENAME}_family.soft ; 
+$WGET -q -O ${FILENAME}_family.soft.gz ftp://ftp.ncbi.nih.gov/pub/geo/DATA/SOFT/by_platform/${FILENAME}/${FILENAME}_family.soft.gz ;
+$GUNZIP -f $MICROARRAY_PATH/${FILENAME}_family.soft.gz ;
+CHK3=`cksum $MICROARRAY_PATH/old.${FILENAME}_family.soft | cut -f1`
+CHK4=`cksum $MICROARRAY_PATH/${FILENAME}_family.soft | cut -f1`
 # read from !platform_table_begin
 # read to !platform_table_end
 #cut -f2 GPL1319_family.soft >> accessions.txt
@@ -42,8 +42,8 @@ CHK2=`cksum ${FILENAME}_family.soft`
 #echo "CHK3 $CHK3" ; 
 #echo "CHK4 $CHK4" ; 
 
-#if [ "$CHK1" != "$CHK2" ] || [ "$CHK3" != "$CHK4" ]; then
-if [ "$CHK1" != "$CHK2" ] ; then
+if [ "$CHK1" != "$CHK2" ] || [ "$CHK3" != "$CHK4" ]; then
+#if [ "$CHK1" != "$CHK2" ] ; then
     echo "0" ; 
 else
     echo "1" ; 
