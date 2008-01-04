714a1dad6ea4b87cf38f9689c1e1e36b15dec88c
Nathan Dunn <ndunn@zfin.org>
2008-01-04 20:19:08 +0000

    BUGZID: 2141 Reviewed by: Not reviewed
    Using javamail instead of mailx.  Updated some test properties, as well.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11845 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ test/org/zfin/framework/mail/MailTest.java
@@ -0,0 +1,55 @@
+package org.zfin.framework.mail;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue ;
+
+import org.zfin.TestConfiguration;
+import org.zfin.properties.ApplicationProperties;
+import org.zfin.properties.Path;
+import org.zfin.properties.ZfinProperties;
+import org.zfin.properties.impl.ApplicationPropertiesImpl;
+import org.zfin.properties.impl.PathImpl;
+import junit.framework.JUnit4TestAdapter;
+
+/**
+ * Tests the MailSender classes.
+ */
+public class MailTest {
+
+//    public static final String APP_SETUP_REPOSITORY_DIRECTORY = "home/WEB-INF";
+//    public static final String APP_SETUP_MASTER_FILE = "zfin-properties.xml";
+
+    public static final String APP_SETUP_REPOSITORY_DIRECTORY = "test";
+    public static final String APP_SETUP_MASTER_FILE = "zfin-properties-test.xml";
+
+
+//    @Before
+//    public void setUp() throws Exception {
+//        TestConfiguration.configure();
+//        TestConfiguration.initApplicationProperties();
+//    }
+
+    public void initRealApplicationProperties(){
+        ZfinProperties.init(APP_SETUP_REPOSITORY_DIRECTORY, APP_SETUP_MASTER_FILE);
+        ApplicationProperties properties = new ApplicationPropertiesImpl();
+        Path path = new PathImpl();
+        path.setWebdriver("cgi-bin/webdriver");
+        properties.setPath(path);
+        ZfinProperties.init(properties);
+    }
+
+    @Test
+    public void sending(){
+        TestConfiguration.configure();
+        initRealApplicationProperties() ;
+        TestConfiguration.initApplicationProperties();
+        IntegratedJavaMailSender mailSender = new IntegratedJavaMailSender() ;
+        assertTrue(mailSender.sendMail("test subject","test message","ndunn@uoregon.edu")) ;                
+    }
+
+
+    public static junit.framework.Test suite() {
+        return new JUnit4TestAdapter(MailTest.class);
+    }
+}
