356dab3f42b4f82e20e41eaaf3ab4ec7c08e3d72
Sierra Moxon <smoxon@zfin.org>
2008-01-04 22:15:30 +0000

    BUGZID: none Reviewed by: Not reviewed execute function instead of execute procedure and returned value into column
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11849 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/run_candidate_insert_trigger.sql
+++ lib/DB_triggers/run_candidate_insert_trigger.sql
@@ -2,6 +2,6 @@ create trigger run_candidate_insert_trigger insert on
     run_candidate referencing new as new_runcan
     for each row
     (
-        execute procedure increment_candidate_occurrences (
-		new_runcan.runcan_cnd_zdb_id) 
+        execute function increment_candidate_occurrences (
+		new_runcan.runcan_cnd_zdb_id) into runcan_occurrence_order
     );
