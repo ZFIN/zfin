645696f746dd685cb65b3ade5676e760141094c4
Sierra Moxon <smoxon@zfin.org>
2008-01-29 19:54:05 +0000

    BUGZID: 2292 Reviewed by: Not reviewed see case notes
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11970 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/p_update_unspecified_alleles.sql
@@ -0,0 +1,32 @@
+create procedure p_update_unspecified_alleles (vMarkerZdbId varchar(50),
+       		 			       vFeatureZdbId varchar(50))
+
+    define vFeatureName like feature.feature_name;
+    define vFeatureAbbrev like feature.feature_abbrev;
+    define vUnExists int ;
+    define vUnAllele like feature.feature_name;
+   
+  let vUnAllele = (select feature_name from feature
+        	  	  where feature_zdb_id = vFeatureZdbId);
+
+  if (vUnAllele like 'un_%')
+
+  then
+
+        let vUnExists = (Select count(*) 
+    	          	     from feature, feature_marker_relationship
+			     where feature_zdb_id = fmrel_ftr_zdb_id
+                     	     and feature_name like 'un_%'
+			     and fmrel_type = 'is allele of'
+		     	     and fmrel_mrkr_zdb_id = vMarkerZdbId);
+
+        if (vUnExists > 1)
+
+    	  then raise exception -746,0,"FAIL!: an un_ allele already exists for the gene.  Please use it instead.";
+  	
+        end if ;
+
+  end if ;
+
+
+end procedure ;
