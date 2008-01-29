a9e52762d68d3df6a0d308c8d4991ac65437b994
Sierra Moxon <smoxon@zfin.org>
2008-01-29 19:53:21 +0000

    BUGZID: 2292 Reviewed by: Not reviewed see case notes.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@11969 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/feature_marker_relationship_insert_trigger.sql
+++ lib/DB_triggers/feature_marker_relationship_insert_trigger.sql
@@ -6,4 +6,6 @@ create trigger feature_marker_relationship_insert_trigger
         execute procedure p_fmrel_grpmem_correct(new_ftr_rel.fmrel_ftr_zdb_id,
 		new_ftr_rel.fmrel_mrkr_zdb_id,
 		new_ftr_rel.fmrel_type ),
+	execute procedure p_update_unspecified_alleles(new_ftr_rel.fmrel_mrkr_zdb_id,
+							new_ftr_rel.fmrel_ftr_zdb_id),
 	execute procedure p_update_related_genotype_names(new_ftr_rel.fmrel_ftr_zdb_id));
