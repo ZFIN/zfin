5791b4bc1ca2530963ab7618df9f3f1ec1c5837d
Tom Conlin <tomc@zfin.org>
2008-02-05 21:21:18 +0000

    the executables have been gone for a year the build directoru can safely go as well
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@12035 908b024a-c82b-635c-c293-f2b15b23277c

--- cgi-bin/cgiemail-1.6/acconfig.h
+++
@@ -1,8 +0,0 @@
-/* Define if you want to enable use of environment variables.  */
-#undef ENABLE_CGIENV
-
-/* Define to attempt to send bounces to owner of template file.  */
-#undef ENABLE_OWNER_BOUNCE
-
-/* Define if you want to enable the X-Mailer header.  */
-#undef ENABLE_XHEADERS
