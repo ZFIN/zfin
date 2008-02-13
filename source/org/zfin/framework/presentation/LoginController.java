5fceeea80c0cb80ecb6b3c65439d3eb07598192e
Christian Pich <cmpich@zfin.org>
2008-02-13 22:08:19 +0000

    BUGZID: 2053 Reviewed by: Not reviewed Make sure before login happens always call a non-secure page to obtain a non-secure cookie that can be shared in the secure environment.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@12141 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/framework/presentation/LoginController.java
+++ source/org/zfin/framework/presentation/LoginController.java
@@ -1,7 +1,7 @@
 package org.zfin.framework.presentation;
 
-import org.springframework.web.servlet.mvc.Controller;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,11 +15,19 @@ public class LoginController implements Controller {
 
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+        // Check if we came through /action/login
+        // this is a jumper page that can be accessed insecurely and thus creates a no-ssl session if not already created.
+        // it then redirects to the secure login page.
+        String url = request.getRequestURI();
+        if (url.endsWith("/action/login")) {
+            response.sendRedirect("/action/login-redirect");
+        }
+
         String accessDenied = request.getParameter(ACCESS_DENIED);
         String queryString = request.getQueryString();
         // Sometimes a query string is not recognized as a parameter
         // that's why we check both cases.
-        if (accessDenied != null || (queryString != null && queryString.startsWith(ACCESS_DENIED)) )
+        if (accessDenied != null || (queryString != null && queryString.startsWith(ACCESS_DENIED)))
             return new ModelAndView("access-denied");
         return new ModelAndView("login");
     }
