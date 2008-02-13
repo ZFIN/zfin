4e18adff422577c5e07c0f50259ee0522847766f
Sierra Moxon <smoxon@zfin.org>
2008-02-13 17:25:57 +0000

    BUGZID: 1804 Reviewed by: Not reviewed added changes to snp_download table in response to SNP group meeting (documented in 1804). We just want to display the clone the snp is on and the rs number of the snp and the labs/pubs that identify them. curators also want to add/update a note related to the snp.  these are much different than the talbot snps.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@12131 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/SNP/snpd.sql
+++ server_apps/data_transfer/SNP/snpd.sql
@@ -1,29 +1,14 @@
 begin work ;
 
---looks like ensemble number and dbsnp number are both the same, the 
---rs number; so I just include the rs number here.
-
 create table snp_download(
-        snpd_pk_id serial not null constraint snpd_pk_id_not_null,
-        snpd_name varchar(255) not null constraint snpd_name_not_null,
-        snpd_abbrev varchar(40) not null constraint snpd_abbrev_not_null,
-        snpd_mrkr_zdb_id varchar(50) not null constraint
-                snpd_mrkr_zdb_id_not_null, --clone/gene/est id
-        snpd_sequence lvarchar,
-        snpd_offset int,
-        snpd_type varchar(10) not null constraint snpd_type_not_null,
-        snpd_rs_acc_num varchar(40),
-        snpd_ss_acc_num varchar(40) not null constraint
-                snpd_ss_acc_num_not_null,
-        snpd_pub_zdb_id varchar(50) not null constraint
-                snpd_pub_zdb_id_not_null,
-		 --Jeff Smith attribution needs to be a
-		--default here at some point.
-	snpd_chromosome_number varchar(4),
-	snpd_variation varchar(20),
-	snpd_left_end varchar(3),
-	snpd_seq_type varchar(20)
-)
+        snpd_pk_id serial not null constraint 
+		   	  snpd_pk_id_not_null,
+        snpd_rs_acc_num varchar(40) not null constraint 
+			snpd_rs_acc_num_not_null,
+	snpd_mrkr_zdb_id varchar(50) not null constraint 
+			 snpd_mrkr_zdb_id_not_null,
+	snpd_comment lvarchar
+        )
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 32768 next size 32768
 lock mode page;
@@ -33,46 +18,44 @@ create unique index snp_download_primary_key_index
   using btree in idxdbs3 ;
 
 create index snpd_mrkr_zdb_id_index
-  on snp_download(snpd_mrkr_zdb_id)
+  on snp_download (snpd_mrkr_zdb_id)
   using btree in idxdbs2;
 
-create unique index snp_download_alternate_key_index
-  on snp_download (snpd_ss_acc_num) 
-  using btree in idxdbs2 ;
-
-create unique index snpd_name_alternate_key_index
- on snp_download (snpd_name,snpd_ss_acc_num, snpd_abbrev)
+create unique index snpd_rs_mrkr_alternate_key_index
+ on snp_download (snpd_rs_acc_num, snpd_mrkr_zdb_id)
  using btree in idxdbs1;
-
---these two constraints allow us to mimic marker for the
---time when snps go into marker, or equivalent table.
-
-alter table snp_download
-  add constraint unique (snpd_ss_acc_num)
-  constraint snpd_alternate_ss_acc_num_key ;
-
-alter table snp_download
-  add constraint unique (snpd_name, snpd_abbrev, snpd_ss_acc_num)
-  constraint snpd_alternate_key ;
 
 alter table snp_download  
   add constraint primary key (snpd_pk_id)
   constraint snp_download_primary_key ;
 
+alter table snp_download
+  add constraint unique (snpd_rs_acc_num, snpd_mrkr_zdb_id)
+  constraint snpd_alternate_key ;
+
 alter table snp_download 
-  add constraint (foreign key (snpd_mrkr_Zdb_id)
+  add constraint (foreign key (snpd_mrkr_zdb_id)
   references marker on delete cascade constraint
   snpd_mrkr_zdb_id_foreign_key_odc);
 
-create index snpd_variation_index 
- on snp_download (snpd_variation)
- using btree in idxdbs3 ;
- 
-alter table snp_download
-  add constraint (foreign key (snpd_variation)
-  references sequence_ambiguity_code constraint 
-  snpd_variation_foreign_key) ;
-  
+
+create table snp_download_attribution(
+        snpdattr_snpd_pk_id int not null constraint 
+		   	  snpdattr_snpd_pk_id_not_null,
+        snpdattr_pub_zdb_id varchar(50) not null constraint
+			  snpdattr_pub_zdb_id_not_null
+        )
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 32768 next size 32768
+lock mode page;
+
+create unique index snpdattr_primary_key_index
+  on snp_download_attribution (snpdattr_snpd_pk_id, snpdattr_pub_zdb_id)
+  using btree in idxdbs1 ;
+
+alter table snp_download_attribution
+  add constraint primary key (snpdattr_snpd_pk_id, snpdattr_pub_zdb_id)
+  constraint snpd_download_attribution_primary_key ;
 
 
 rollback work ;
