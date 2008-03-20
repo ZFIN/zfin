70349c2af67bc0362cab3aab73b67820f32fb516
Xiang Shao <xshao@zfin.org>
2008-03-20 20:58:27 +0000

    bugzid:"1804"
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@12416 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/SNP/loadNewSNPAttrs.sql
@@ -0,0 +1,12 @@
+-- loadNewSNPAttrs.sql
+-- load snp_download_attribution table
+-- need forsnpdattrtable.unl 
+begin work;
+
+load from forsnpdattrtable.unl 
+  insert into snp_download_attribution;
+
+! echo "                           into snp_download_attribution table."
+
+commit work;
+--rollback work;
