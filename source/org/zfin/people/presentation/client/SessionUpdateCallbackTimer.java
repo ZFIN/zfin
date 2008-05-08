3b9ea0ed95ad5f7fe7d867e39172eb56f93d6e14
Nathan Dunn <ndunn@zfin.org>
2008-05-08 17:39:01 +0000

    BugzID: 2698
    Moved functionality into an anonymous inner-class.
    
    
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@13054 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/people/presentation/client/SessionUpdateCallbackTimer.java
+++ source/org/zfin/people/presentation/client/SessionUpdateCallbackTimer.java
@@ -1,6 +1,8 @@
 package org.zfin.people.presentation.client;
 
 import com.google.gwt.user.client.Timer;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.rpc.AsyncCallback;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -9,19 +11,24 @@ import java.util.ArrayList;
  */
 public class SessionUpdateCallbackTimer extends Timer {
 
-    private SessionUpdateCallback callback ;
     private List sessionList = new ArrayList() ;
 
     public void scheduleCallback(CuratorSessionDTO sessionUpdate,int time){
         cancel();
         sessionList.add(sessionUpdate) ;
-        callback  = new SessionUpdateCallback() ; 
         schedule(time);
     }
 
     public void run(){
             if(sessionList.size()>0){
-                SessionSaveService.App.getInstance().saveCuratorUpdate( sessionList, callback);
+                SessionSaveService.App.getInstance().saveCuratorUpdate( sessionList, new AsyncCallback(){
+                    public void onSuccess(Object object) {
+                        //Window.alert("success");
+                    }
+                    public void onFailure(Throwable throwable) {
+                        Window.alert("SessionSave update failed: "+ throwable);
+                    }
+                }) ;
                 sessionList.clear();
             }
     }
