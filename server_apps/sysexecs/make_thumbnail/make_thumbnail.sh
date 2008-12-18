e796027dd81d4bb34282a89b29855ee1f0b80e12
Kevin Schaper <kschaper@zfin.org>
2008-12-18 21:06:14 +0000

    bugzid: 3264   Now creates medium images and thumbs with imagemagick
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@15708 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
+++ server_apps/sysexecs/make_thumbnail/make_thumbnail.sh
@@ -5,8 +5,19 @@ my $fullImageFile = $1;
 $ARGV[1] =~  m/([A-Za-z\d\-\_\$\+\=\~\.\,\ \/]+)/;
 my $thumbnailFile = $1;
 
-system("/local/apps/netpbm/bin/anytopnm $fullImageFile | " .
-       "/local/apps/netpbm/bin/pnmscale -h 64 | " .
-       "/local/apps/netpbm/bin/ppmtojpeg > $thumbnailFile");
+# image files will be in a subdirectory called "medium/" and
+# have the same name as the full sized image
+my $mediumImageFile = $fullImageFile;
+$mediumImageFile =~ s/ZDB-IMAGE/medium\/ZDB-IMAGE/;
+
+#system("/local/apps/netpbm/bin/anytopnm $fullImageFile | " .
+#       "/local/apps/netpbm/bin/pnmscale -h 64 | " .
+#       "/local/apps/netpbm/bin/ppmtojpeg > $thumbnailFile");
+
+#the 1000px is a maximum that no sensible thumbnail should achieve
+system("/local/bin/convert -thumbnail 1000x64 $fullImageFile $thumbnailFile");
+system("/local/bin/convert -thumbnail 500x550 $fullImageFile $mediumImageFile");
+
 system("/bin/chmod 644 $thumbnailFile");
+system("/bin/chmod 644 $mediumImageFile");
 exit(0);
