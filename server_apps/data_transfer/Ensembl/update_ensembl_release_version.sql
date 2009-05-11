ef9428b72c175c6a92a3b2083b9f4406c28e39f4
Tom Conlin <tomc@zfin.org>
2009-05-11 18:54:49 +0000

    bugzid 4075
    change Zv7 to Zv8 in foreign dbs
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@17012 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Ensembl/update_ensembl_release_version.sql
+++ server_apps/data_transfer/Ensembl/update_ensembl_release_version.sql
@@ -5,26 +5,27 @@ set constraints for foreign_db_contains.foreign_db_contains_fdb_db_name_foreign_
 set constraints for foreign_db.foreign_db_primary_key deferred;
 
 UPDATE foreign_db
-SET fdb_db_name = 'Ensembl(Zv6)'
-WHERE fdb_db_name = 'Ensembl'
+SET fdb_db_name = 'Ensembl(Zv8)'
+WHERE fdb_db_name = 'Ensembl(Zv7)'
 ;
 
 UPDATE foreign_db_contains
-SET fdbcont_fdb_db_name = 'Ensembl(Zv6)'
+SET fdbcont_fdb_db_name = 'Ensembl(Zv8)'
 WHERE fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
 ;
 
 -- PreEnsembl not normally needed
 
-UPDATE foreign_db_contains
-SET fdbcont_fdb_db_name = 'PreEnsembl(Zv7)'
-WHERE fdbcont_zdb_id = 'ZDB-FDBCONT-070718-1'
-;
+--UPDATE foreign_db_contains
+--SET fdbcont_fdb_db_name = 'PreEnsembl(Zv7)'
+--WHERE fdbcont_zdb_id = 'ZDB-FDBCONT-070718-1'
+--;
+--
+--UPDATE foreign_db
+--SET fdb_db_name = 'PreEnsembl(Zv7)'
+--WHERE fdb_db_name = 'PreEnsembl'
+--;
 
-UPDATE foreign_db
-SET fdb_db_name = 'PreEnsembl(Zv7)'
-WHERE fdb_db_name = 'PreEnsembl'
-;
 --rollback work;
 
 --
