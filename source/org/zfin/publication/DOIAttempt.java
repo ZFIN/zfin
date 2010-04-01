32ccad19fc543a691c1b987c708fc181796ce467
Nathan Dunn <ndunn@zfin.org>
2010-04-01 17:59:04 +0000

    BugzID: 4957
    DOI class was missing.
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@19703 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ source/org/zfin/publication/DOIAttempt.java
@@ -0,0 +1,39 @@
+package org.zfin.publication;
+
+/**
+ */
+public class DOIAttempt {
+    private Long id ;
+    private String doi ;
+    private Publication publication ;
+    private int numAttempts ;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Publication getPublication() {
+        return publication;
+    }
+
+    public void setPublication(Publication publication) {
+        this.publication = publication;
+    }
+
+    public int getNumAttempts() {
+        return numAttempts;
+    }
+
+    public void setNumAttempts(int numAttempts) {
+        this.numAttempts = numAttempts;
+    }
+
+    public int addAttempt(){
+        return ++numAttempts ;
+    }
+}
