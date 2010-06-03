fb9f3465f3684e3316825d16b9c1a7f6ead02245
Brock Sprunge <bsprunge@zfin.org>
2010-06-03 00:25:55 +0000

    BugzId: 5735
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@20473 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/GO/gofile.sql
+++ server_apps/data_transfer/GO/gofile.sql
@@ -4,7 +4,7 @@ unload to 'go.zfin' delimiter '	'
 			 select mrkrgoev_zdb_id,
 				mrkr_zdb_id, mrkr_abbrev, mrkr_name, term_ont_id, mrkrgoev_source_zdb_id,
 				accession_no, mrkrgoev_evidence_code, infgrmem_inferred_from, mrkrgoev_gflag_name,
-				goterm_ontology[1], mrkrgoev_date_modified, mrkrgoev_modified_by
+				upper(term_ontology[1]), mrkrgoev_date_modified, mrkrgoev_modified_by
 			   from marker_go_term_evidence, marker, term, publication,
 					   outer inference_group_member
 			  where mrkrgoev_mrkr_zdb_id = mrkr_zdb_id
