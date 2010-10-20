5ea9c31d6747722ece9fd1f769f2902134376569
Tom Conlin <tomc@zfin.org>
2010-10-20 20:06:13 +0000

    bugzid 5635
    spelling change:  morph a lino  to  morph o lino
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@21844 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Downloads/GFF3/GFF3Files.pl
+++ server_apps/data_transfer/Downloads/GFF3/GFF3Files.pl
@@ -42,7 +42,7 @@ my $cmd = "cat load_drerio_vega_id.sql " .
     "unload_xpat_gff.sql " .
     "unload_pheno_gff.sql " .
     "unload_antibody_gff.sql " .
-    "unload_zfin_morphalino.sql " . # should not need as much updating.
+    "unload_zfin_morpholino.sql " . # should not need as much updating.
 #   "unload_mutant_gff.sql " .
 #   "unload_zfin_transcript.sql ".
     "rollback.sql | $ENV{'INFORMIXDIR'}/bin/dbaccess  <!--|DB_NAME|-->";
---
+++ server_apps/data_transfer/Downloads/GFF3/unload_zfin_morpholino.sql
@@ -0,0 +1,11 @@
+-- just in this form to keep things consistant
+-- and to avoid changing mechanisms if we decide
+-- to do addtional processing/integration in the future
+
+! echo "unload_zfin_morpholino.sql -> zfin_morpholino.gff3"
+
+! cp /research/zprodmore/gff3/zfin_morpholino.gff3 <!--|ROOT_PATH|-->/home/data_transfer/Downloads/zfin_morpholino.gff3
+
+-- to be valid the gff3 requires a header
+-- ! awk '{a[NR]=$0}END{a[0]=h;for(i=0;i<=NR;i++)print a[i]>FILENAME}' h="##gff-version 3" <!--|ROOT_PATH|-->/home/data_transfer/Downloads/zfin_morpholino.gff3
+
