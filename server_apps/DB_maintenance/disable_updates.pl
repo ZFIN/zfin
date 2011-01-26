9088ab3c511de09e908a0355730461cadd7d9163
Sierra Moxon <smoxon@zfin.org>
2011-01-26 00:22:21 +0000

    bugzid: none. refine process for uniprot load
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@22737 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/disable_updates.pl
+++ server_apps/DB_maintenance/disable_updates.pl
@@ -10,9 +10,7 @@ $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
 $ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
 $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
 
-system("/bin/rm -f <!--|ROOT_PATH|-->/server_apps/DB_maintenance/revoke_tables_permissions.sql");
-system("/bin/rm -f <!--|ROOT_PATH|-->/server_apps/DB_maintenance/grant_tables_permissions.sql"); 
-openSQL();
+&openSQL();
 
 ### open a handle on the db
 my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
@@ -23,12 +21,12 @@ my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
   || emailError("Failed while connecting to <!--|DB_NAME|--> "); 
 
 
-    open (REVOKE, ">>revoke_tables_permission.sql") or die "can not open sql file";
-    open (GRANT, ">>grant_table_permission.sql") or die "can not open sql file";
+    open (REVOKE, ">>/tmp/revoke_tables_permission.sql") or die "can not open sql file";
+    open (GRANT, ">>/tmp/grant_table_permission.sql") or die "can not open sql file";
 
-    $query_select = 'select tabname from systables where tabid > 99 and tabname not like "sys%" and tabtype = "T";';
+$query_revoke = 'select T.tabname from systables T, systabauth A where T.tabid > 99 and T.tabname not like "sys%" and T.tabtype = "T" and A.grantor = "informix" and A.tabid = T.tabid and T.tabtype != "V" and T.tabtype != "Q";';
 
-    my $cur = $dbh->prepare($query_select);
+    my $cur = $dbh->prepare($query_revoke);
 
     $cur->execute;
     my($tabname);
@@ -46,17 +44,14 @@ my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
 
     print REVOKE "$query_revoke_insert\n";
     print REVOKE "$query_revoke_update\n";
-    print GRANT "$query_grant_insert\n";
-    print GRANT "$query_grant_update\n";
       
     }
-    
+
     close(GRANT);
     close(REVOKE);
 
 
 exit 0;
-
 
 sub cleanTail () {
   my $var = $_[0];
@@ -70,10 +65,10 @@ sub cleanTail () {
 
 sub openSQL()
   {
-    system("/bin/rm -f revoke_table_permission.sql");
-    system("touch revoke_table_permission.sql");
-    system("/bin/rm -f grant_table_permission.sql");
-    system("touch grant_table_permission.sql");
+    system("/bin/rm -f /tmp/revoke_tables_permission.sql");
+    system("touch /tmp/revoke_tables_permission.sql");
+    system("/bin/rm -f /tmp/grant_table_permission.sql");
+    system("touch /tmp/grant_table_permission.sql");
   }
 
 
--- server_apps/DB_maintenance/loadAndSwitch.pl
+++ server_apps/DB_maintenance/loadAndSwitch.pl
@@ -1,28 +1,25 @@
 #! /private/bin/perl -w 
 
-
-use DBI;
-use MIME::Lite;
-#require "<!--|ROOT_PATH|-->/server_apps/DB_maintenance/disable_updates.pl";
-# runs from the curation database environment
-
+# running from swirl checkout, on swirl environment.  Swirl is the 'curation' db.  Hoover is the db to run the jobs on.
 # curation db is the db in the environment it's running
 # $0 loadDb
 # $1 path to loadDb code 
 # $2 source file for loadDb code
 # $3 absolute path of load to run
+# /www_homes/swirl/server_apps/DB_maintenance/loadAndSwitch.pl hoovdb /research/zusers/staylor/hoover/ZFIN_WWW/ /research/zusers/staylor/hoover/ZFIN_WWW/commons/env/hoover.env
+
+use DBI;
 
 my $curationDb = "<!--|DB_NAME|-->" ;
-my $loadDb = $_[0];
-my $pathToCodeCheckout = $_[1];
-my $sourceFile= $_[2];
-# my $scriptsToRunFile = $_[3];
 
-# lock curation db
-# unload curation db
-# load load db
-# run load
-# unload load db
+my $loadDb = $ARGV[0];
+
+my $pathToCodeCheckout = $ARGV[1];
+
+my $sourceFile= $ARGV[2];
+
+my $pathToUnload = "/research/zunloads/databases/<!--|DB_NAME|-->";
+
 my $revokeTableLine;
 my $grantTableLine;
 
@@ -31,50 +28,76 @@ my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
 		       '',
 		       {AutoCommit => 0, RaiseError => 1}
 		       )
-  || errorExit("Failed while connecting to <!--|DB_NAME|--> ");
+          || errorExit("Failed while connecting to <!--|DB_NAME|--> ");
 
 
-system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/disable_updates.pl") && die "disable_updates.pl failed";
+### MAIN #######
 
-chmod 0755, "/tmp/revoke_tables_permission.sql" ;
+&disableUpdates();
+&loadDb();
+&runScripts();
 
-chmod 0755, "/tmp/grant_table_permission.sql" ;
 
-open (REVOKE,"/tmp/revoke_tables_permission.sql") or die "can not open revoke_tables_permission.sql for reading";
-
-while ($revokeTableLine = <REVOKE>) {
-    $dbh->prepare("$revokeTableLine");
-    $dbh->execute;
-}
-
-##system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/unload_production.sh") && die "unload_production.sh failed";
-
-#system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/loadDb.sh $loadDb /research/zunloads/databases/zfindb/ $pathToCodeCheckout $sourceFile") && die "loadDb.sh failed";
-
-open (GRANT,"/tmp/grant_table_permission.sql") or die "can not open grant_table_permission.sql for reading";
-
-my $connectString ;
-$connectString = "DBI:Informix:$loadDb";
-
-my $dbhLoadDb = DBI->connect($connectString,
-		       '',
-		       '',
-		       {AutoCommit => 0, RaiseError => 1}
-		       )
-  || errorExit("Failed while connecting to $loadDb");
-
-while ($grantTableLine = <GRANT>) {
-    $dbhLoadDb->prepare("$grantTableLine");
-    $dbhLoadDb->execute;
-}
-
-open (COMMANDFILE,"$pathToCheckout/server_apps/DB_maintenance/loadAndSwitchJobs.txt") or die "can not open <!--|ROOT_PATH|-->/server_apps/DB_maintenance/loadAndSwitchJobs.txt for reading";
-
-my $command;
-
-while ($command = <COMMANDFILE>) {
-    print $command."\n";
-    system("$command") && die "$command load failed.";
-
-}
 exit 0;
+#### END MAIN ##
+
+sub disableUpdates() {
+    my $flag = $dbh->prepare ("update zdb_flag set zflag_is_on = 't' where zflag_name = 'disable updates'");
+
+    $flag->execute;
+
+#    system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/disable_updates.pl") && die "disable_updates.pl failed";
+
+#    chmod 0755, "/tmp/revoke_tables_permission.sql" ;
+
+#    chmod 0755, "/tmp/grant_table_permission.sql" ;
+
+#    open (REVOKE,"/tmp/revoke_tables_permission.sql") or die "can not open revoke_tables_permission.sql for reading";
+
+#    while ($revokeTableLine = <REVOKE>) {
+#	my $cur =$dbh->prepare("$revokeTableLine");
+#	$cur->execute;
+#    }
+
+}
+
+
+sub loadDb() {
+
+#    my $connectString ;
+#    $connectString = "DBI:Informix:$loadDb";
+
+#    my $dbhLoadDb = DBI->connect($connectString,
+#		       '',
+#		       '',
+#		       {AutoCommit => 0, RaiseError => 1}
+#		       )
+#	|| errorExit("Failed while connecting to $loadDb");
+
+
+    system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/unload_production.sh") && die "unload_production.sh failed";
+
+    system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/loadDb.sh $loadDb $pathToUnload $pathToCodeCheckout $sourceFile") && die "loadDb.sh failed.";
+
+#    open (GRANT,"/tmp/grant_table_permission.sql") or die "can not open grant_table_permission.sql for reading";
+
+#    while ($grantTableLine = <GRANT>) {
+#	my $curGrant = $dbhLoadDb->prepare("$grantTableLine");
+#	$curGrant->execute;
+#    }
+
+}
+
+sub runScripts() {
+
+    open (COMMANDFILE,"$pathToCodeCheckout/server_apps/DB_maintenance/loadAndSwitchJobs.txt") or die "can not open <!--|ROOT_PATH|-->/server_apps/DB_maintenance/loadAndSwitchJobs.txt for reading";
+
+    my $command;
+
+    while ($command = <COMMANDFILE>) {
+	print $command."\n";
+	system("$command") && die "$command load failed.";
+
+    }
+}
+
