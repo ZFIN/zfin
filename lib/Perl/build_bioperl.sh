252121811945befb258e3d8e480c63baad74764a
Kevin Schaper <kschaper@zfin.org>
2011-02-03 19:16:51 +0000

    bugzid: 6597  changed build process to fit new gbrowse machine (putting perl libraries into /usr/local/lib/site_perl rather than /var/www)
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@22829 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/Perl/build_bioperl.sh
+++ lib/Perl/build_bioperl.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-mkdir -p $TARGETROOT/lib
-rsync -r --delete -C --exclude=Makefile ./bioperl $TARGETROOT/lib/Perl && cp -fr  ./cpan/Bio/* $TARGETROOT/lib/Perl/bioperl/Bio/
+rsync -r -C --exclude=Makefile ./bioperl/* $LIBTARGET && cp -fr  ./cpan/Bio/* $LIBTARGET/Bio/
+
 
