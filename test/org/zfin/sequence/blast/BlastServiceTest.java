65b09a805ac6505e75562ee7cae8c5eb2f105d6f
Nathan Dunn <ndunn@zfin.org>
2011-03-01 23:11:55 +0000

    fogbugz: 6447 GOA Gaf Load
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@23108 908b024a-c82b-635c-c293-f2b15b23277c

--- test/org/zfin/sequence/blast/BlastServiceTest.java
+++ test/org/zfin/sequence/blast/BlastServiceTest.java
@@ -6,42 +6,39 @@ import org.zfin.datatransfer.webservice.EBIFetch;
 import org.zfin.datatransfer.webservice.NCBIEfetch;
 import org.zfin.sequence.Defline;
 import org.zfin.sequence.Sequence;
-import org.zfin.sequence.service.UniprotService;
 
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  */
 public class BlastServiceTest {
 
     @Test
-    public void useEfetchForProtein(){
-        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("P26630") ;
+    public void useEfetchForProtein() {
+        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("P26630");
         assertTrue(CollectionUtils.isNotEmpty(sequences));
-        assertEquals(1,sequences.size());
-        Sequence sequence = sequences.get(0) ;
+        assertEquals(1, sequences.size());
+        Sequence sequence = sequences.get(0);
         Defline defline = sequence.getDefLine();
         assertTrue(defline.getAccession().equals("P26630"));
-        assertTrue(sequence.getFormattedData().length()>100);
-        assertTrue(sequence.getFormattedSequence().length()>100);
-        assertTrue(defline.toString().length()>20);
+        assertTrue(sequence.getFormattedData().length() > 100);
+        assertTrue(sequence.getFormattedSequence().length() > 100);
+        assertTrue(defline.toString().length() > 20);
     }
 
     @Test
-    public void useEfetchForProtein2(){
-        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("NP_571379") ;
+    public void useEfetchForProtein2() {
+        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("NP_571379");
         assertTrue(CollectionUtils.isNotEmpty(sequences));
-        assertEquals(1,sequences.size());
-        Sequence sequence = sequences.get(0) ;
+        assertEquals(1, sequences.size());
+        Sequence sequence = sequences.get(0);
         Defline defline = sequence.getDefLine();
         assertTrue(defline.getAccession().equals("NP_571379"));
-        assertTrue(sequence.getFormattedData().length()>100);
-        assertTrue(sequence.getFormattedSequence().length()>100);
-        assertTrue(defline.toString().length()>20);
+        assertTrue(sequence.getFormattedData().length() > 100);
+        assertTrue(sequence.getFormattedSequence().length() > 100);
+        assertTrue(defline.toString().length() > 20);
     }
 
 //    @Test
@@ -53,55 +50,55 @@ public class BlastServiceTest {
 
 
     @Test
-    public void useEBIForUniprot(){
-        assertFalse(EBIFetch.validateAccession("NP_571379")) ;
-        assertTrue(EBIFetch.validateAccession("B3DJJ0")) ;
+    public void useEBIForUniprot() {
+        assertFalse(EBIFetch.validateAccession("NP_571379"));
+        assertTrue(EBIFetch.validateAccession("B3DJJ0"));
     }
 
     @Test
-    public void useEfetchForNucleotide(){
-        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("AY627769") ;
+    public void useEfetchForNucleotide() {
+        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("AY627769");
         assertTrue(CollectionUtils.isNotEmpty(sequences));
-        assertEquals(1,sequences.size());
-        Sequence sequence = sequences.get(0) ;
+        assertEquals(1, sequences.size());
+        Sequence sequence = sequences.get(0);
         Defline defline = sequence.getDefLine();
         assertTrue(defline.getAccession().equals("AY627769"));
-        assertTrue(sequence.getFormattedData().length()>100);
-        assertTrue(sequence.getFormattedSequence().length()>100);
-        assertTrue(defline.toString().length()>20);
+        assertTrue(sequence.getFormattedData().length() > 100);
+        assertTrue(sequence.getFormattedSequence().length() > 100);
+        assertTrue(defline.toString().length() > 20);
     }
 
     @Test
-    public void useEfetchForNucleotide2(){
-        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("FN428721") ;
+    public void useEfetchForNucleotide2() {
+        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("FN428721");
         assertTrue(CollectionUtils.isNotEmpty(sequences));
-        assertEquals(1,sequences.size());
-        Sequence sequence = sequences.get(0) ;
+        assertEquals(1, sequences.size());
+        Sequence sequence = sequences.get(0);
         Defline defline = sequence.getDefLine();
         assertTrue(defline.getAccession().equals("FN428721"));
-        assertTrue(sequence.getFormattedData().length()>100);
-        assertTrue(sequence.getFormattedSequence().length()>100);
-        assertTrue(defline.toString().length()>20);
+        assertTrue(sequence.getFormattedData().length() > 100);
+        assertTrue(sequence.getFormattedSequence().length() > 100);
+        assertTrue(defline.toString().length() > 20);
     }
 
     @Test
-    public void useEfetchForNucleotideWithMultipleReturn(){
-        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("X63183") ;
+    public void useEfetchForNucleotideWithMultipleReturn() {
+        List<Sequence> sequences = NCBIEfetch.getSequenceForAccession("X63183");
         assertTrue(CollectionUtils.isNotEmpty(sequences));
-        assertEquals(1,sequences.size());
-        Sequence sequence = sequences.get(0) ;
+        assertEquals(1, sequences.size());
+        Sequence sequence = sequences.get(0);
         Defline defline = sequence.getDefLine();
         assertTrue(defline.getAccession().equals("X63183"));
-        assertTrue(sequence.getFormattedData().length()>100);
-        assertTrue(sequence.getFormattedSequence().length()>100);
-        assertTrue(defline.toString().length()>20);
+        assertTrue(sequence.getFormattedData().length() > 100);
+        assertTrue(sequence.getFormattedSequence().length() > 100);
+        assertTrue(defline.toString().length() > 20);
     }
 
     @Test
-    public void validateAccessions(){
-        String[] accessions = {"NP_571379", "AY627769", "FN428721", "X63183"} ;
-        for(String accession : accessions){
-            assertTrue(NCBIEfetch.validateAccession(accession)) ;
+    public void validateAccessions() {
+        String[] accessions = {"NP_571379", "AY627769", "FN428721", "X63183"};
+        for (String accession : accessions) {
+            assertTrue(NCBIEfetch.validateAccession(accession));
         }
     }
 
