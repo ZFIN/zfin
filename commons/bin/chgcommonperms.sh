bbaa8b9bb1c3d9b529ca47bae5f29e5bb4894328
Kevin Schaper <kschaper@zfin.org>
2011-03-07 19:52:53 +0000

    bugzid: 6707  added tomcat scripts for test.zfin.org and trunk.zfin.org and added them to chgcommonperms.sh
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@23162 908b024a-c82b-635c-c293-f2b15b23277c

--- commons/bin/chgcommonperms.sh
+++ commons/bin/chgcommonperms.sh
@@ -10,9 +10,23 @@
 
 cd /private/ZfinLinks/Commons/bin
 # chown informix:fishadmin enableLogging.pl pullfromfrost.pl 
-chmod 550 restartapache.pl starttomcat.pl stoptomcat.pl restarttomcat.pl setContextDescriptor.pl makemediumimages.bash starthappytomcat.pl stophappytomcat.pl restarthappytomcat.pl
-chown root:fishadmin restartapache.pl starttomcat.pl stoptomcat.pl restarttomcat.pl setContextDescriptor.pl
+chmod 550 restartapache.pl starttomcat.pl stoptomcat.pl restarttomcat.pl 
+chmod 550 setContextDescriptor.pl makemediumimages.bash 
+chmod 550 starthappytomcat.pl stophappytomcat.pl restarthappytomcat.pl 
+chmod 550 starttrunktomcat.pl stoptrunktomcat.pl restarttrunktomcat.pl 
+chmod 550 starttesttomcat.pl stoptesttomcat.pl restarttesttomcat.pl
+
+chown root:fishadmin restartapache.pl setContextDescriptor.pl
+chown root:fishadmin starttomcat.pl stoptomcat.pl restarttomcat.pl
 chown root:fishadmin starthappytomcat.pl stophappytomcat.pl restarthappytomcat.pl
-chmod u+s restartapache.pl starttomcat.pl stoptomcat.pl restarttomcat.pl setContextDescriptor.pl starthappytomcat.pl stophappytomcat.pl restarthappytomcat.pl
+chown root:fishadmin starttrunktomcat.pl stoptrunktomcat.pl restarttrunktomcat.pl
+chown root:fishadmin starttesttomcat.pl stoptesttomcat.pl restarttesttomcat.pl
+
+chmod u+s restartapache.pl setContextDescriptor.pl 
+chmod u+s starttomcat.pl stoptomcat.pl restarttomcat.pl
+chmod u+s starthappytomcat.pl stophappytomcat.pl restarthappytomcat.pl
+chmod u+s starttrunktomcat.pl stoptrunktomcat.pl restarttrunktomcat.pl
+chmod u+s starttesttomcat.pl stoptesttomcat.pl restarttesttomcat.pl
+
 chown informix:fishadmin ../env/bionix.env ../env/helix.env ../env/embryonix.env ../env/frost.env ../env/mirror.env
 chmod u+s enableLogging.pl
---
+++ commons/bin/starttrunktomcat.pl
@@ -0,0 +1,25 @@
+#!/private/bin/perl -wT
+#
+# Script to stop and start tomcat on development.  We do this because once
+# servlets are loaded into tomcat, they tend to stay there, even after the
+# files defining the servlet have been changed.  There is supposedly a way
+# to get Tomcat to recognize when the files change, but I haven't figured
+# that out yet.
+
+BEGIN {
+    $ENV{PATH}="/local/bin:/usr/bin";
+    $ENV{SHELL}="/usr/bin/sh";
+    delete $ENV{LD_LIBRARY_PATH};
+};
+
+use English;
+
+
+# set all the user and group ids we can figure out how to.
+my @userData = getpwnam("root");
+$REAL_USER_ID = $userData[2];
+$EFFECTIVE_USER_ID = $userData[2];
+$REAL_GROUP_ID = $userData[3];
+$EFFECTIVE_GROUP_ID = $userData[3];
+system("/private/ZfinLinks/Commons/bin/tomcat.sh start trunk");
+
