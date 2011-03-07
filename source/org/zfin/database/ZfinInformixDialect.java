89dbb7ac76486f74fed38f0f1d59a757b445effd
Nathan Dunn <ndunn@zfin.org>
2011-03-07 18:41:28 +0000

    BugzID: 6562
    Removed systables calls from ZfinInformixDialect. 
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@23161 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/database/ZfinInformixDialect.java
+++ source/org/zfin/database/ZfinInformixDialect.java
@@ -1,11 +1,15 @@
 package org.zfin.database;
 
+import org.hibernate.MappingException;
 import org.hibernate.dialect.InformixDialect;
 
 import java.sql.Types;
 
 /**
  * Customized Informix Dialect for Hibernate.
+ *
+ * See case 6562.  Overriding calls to systables where tabid=1 in favor of single to avoid potential
+ * locking issues.
  */
 public class ZfinInformixDialect extends InformixDialect {
 
@@ -16,4 +20,27 @@ public class ZfinInformixDialect extends InformixDialect {
         registerHibernateType(Types.LONGVARCHAR, "string");
     }
 
+    @Override
+    public String getIdentitySelectString(String table, String column, int type) throws MappingException {
+//  return type==Types.BIGINT ?
+//             "select dbinfo('serial8') from systables where tabid=1" :
+//             "select dbinfo('sqlca.sqlerrd1') from systables where tabid=1";
+        return type==Types.BIGINT ?
+                "select dbinfo('serial8') from single " :
+                "select dbinfo('sqlca.sqlerrd1') from single ";
+    }
+
+    @Override
+    public String getSequenceNextValString(String sequenceName) {
+//         return "select " + getSelectSequenceNextValString( sequenceName )   +
+//                 " from systables where tabid=1";
+        return "select " + getSelectSequenceNextValString( sequenceName )   +
+                " from single ";
+    }
+
+    @Override
+    public String getCurrentTimestampSelectString() {
+//         return "select distinct current timestamp from informix.systables";
+        return "select distinct current timestamp from single";
+    }
 }
