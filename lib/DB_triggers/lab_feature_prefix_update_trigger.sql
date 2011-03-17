922163a06a80642ecb3a86d0842db858a857fe11
Sierra Moxon <smoxon@zfin.org>
2011-03-17 16:17:52 +0000

    bugzid: 6535 interface changes now handle this.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@23312 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/lab_feature_prefix_update_trigger.sql
+++
@@ -1,8 +0,0 @@
-create trigger lab_feature_prefix_update_trigger
-  update of lfp_current_designation on lab_feature_prefix
-  referencing old as oldLfp
-  	      new as newLfp
-   for each row (
-       execute procedure updatePrimaryLabFeaturePrefix (newLfp.lfp_lab_zdb_id, newlfp.lfp_current_designation,
-       	       					      newLfp.lfp_prefix_id)
-);
