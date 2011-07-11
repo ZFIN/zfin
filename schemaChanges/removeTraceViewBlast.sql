01b10d666824a849e059a0b4927d58d94aed270e
Nathan Dunn <ndunn@zfin.org>
2011-07-11 20:26:18 +0000

    bugzid: 6859 refied schema script
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24264 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/removeTraceViewBlast.sql
+++
@@ -1,3 +0,0 @@
-delete from int_fdbcont_analysis_tool 
-where
-ifat_fdbcont_zdb_id='ZDB-FDBCONT-041215-1';
--- schemaChanges/runSchema.sh
+++ schemaChanges/runSchema.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-dbaccess -a $DBNAME removeTraceViewBlast.sql ;
-dbaccess -a $DBNAME add_display_group.sql ;
-dbaccess -a $DBNAME fix_marker_relationship_type.sql ;
-dbaccess -a $DBNAME updateGenox.sql ;
+dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql
