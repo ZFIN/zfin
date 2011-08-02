c3ff93945d81392a93317c9b0e868117c88734b5
Nathan Dunn <ndunn@zfin.org>
2011-08-02 15:43:11 +0000

    BugzID: 7369
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24428 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/runSchema.sh
+++ schemaChanges/runSchema.sh
@@ -3,3 +3,7 @@ dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql
 
 dbaccess -a $DBNAME schema.sql
 
+update foreign_db 
+set fdb_db_query='http://www.ensembl.org/Danio_rerio/Variation/Summary?db=core;vdb=variation;v='
+where fdb_db_name = 'Ensembl_SNP'
+
