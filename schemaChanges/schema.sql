198d806963485c5782f156c1157a42ac180e2cff
Nathan Dunn <ndunn@zfin.org>
2011-08-02 17:36:10 +0000

    BugzID: 3890
    Evidence codes got lost.
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24434 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/schema.sql
+++ schemaChanges/schema.sql
@@ -3,10 +3,19 @@ begin work ;
 insert into foreign_db (fdb_db_name, fdb_db_query, fdb_url_suffix, fdb_db_display_name, fdb_db_significance)
   values ('PANTHER','http://pantree.org/node/annotationNode.jsp?id=','','PANTHER','13');
 
+insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('IBA','inferred from biological aspect of ancestor',12);
+insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('IBD','inferred from biological aspect of descendant',13);
+insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('IKR','inferred from key residues',14);
+insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('IMR','inferred from missingy residues',15);
+insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('IRD','inferred from rapid divergence',16);
+
+
 
 update foreign_db 
 set fdb_db_query='http://www.ensembl.org/Danio_rerio/Variation/Summary?db=core;vdb=variation;v='
 where fdb_db_name = 'Ensembl_SNP'
+
+execute function regen_term();
 
 commit work;
 
