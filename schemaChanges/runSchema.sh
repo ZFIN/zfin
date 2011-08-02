0928dea9f25a35a34d8228303f519dac1718b92e
Nathan Dunn <ndunn@zfin.org>
2011-08-02 16:32:58 +0000

    BugzID: 6859
    Added GEO job to schema script.
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24433 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/runSchema.sh
+++ schemaChanges/runSchema.sh
@@ -3,5 +3,12 @@ dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql
 
 dbaccess -a $DBNAME schema.sql
 
+
 echo "execute function regen_term(); " | dbaccess -a $DBNAME 
+
+echo "running GEO job about 15 minutes.  Without 10 tests will fail. "
+curl http://$DOMAIN_NAME/webapp/quartz/run/org.zfin.datatransfer.microarray.MicroarrayWebserviceJob.class
+echo "Don running GEO job exiting." 
+
+
 
