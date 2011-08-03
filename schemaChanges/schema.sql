9d9cbdaff1e9bb16f9f7cb86f9366bf973c6d858
Nathan Dunn <ndunn@zfin.org>
2011-08-03 21:31:24 +0000

    BugzID: 3890
    Forgot semicolon
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24456 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/schema.sql
+++ schemaChanges/schema.sql
@@ -13,7 +13,7 @@ insert into go_evidence_code (goev_code,goev_name,goev_display_order) values ('I
 
 update foreign_db 
 set fdb_db_query='http://www.ensembl.org/Danio_rerio/Variation/Summary?db=core;vdb=variation;v='
-where fdb_db_name = 'Ensembl_SNP'
+where fdb_db_name = 'Ensembl_SNP';
 
 execute function regen_term();
 
