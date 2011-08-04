c0eb6b7d4988465afb1929967631182303186455
Nathan Dunn <ndunn@zfin.org>
2011-08-04 22:39:09 +0000

    BugzID: 6859
    Removed dependency on the curl job.
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24469 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/runSchema.sh
+++ schemaChanges/runSchema.sh
@@ -3,9 +3,17 @@ dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql
 
 dbaccess -a $DBNAME schema.sql
 
-echo "running GEO job about 15 minutes.  Without 10 tests will fail. "
-curl http://$DOMAIN_NAME/webapp/quartz/run/org.zfin.datatransfer.microarray.MicroarrayWebserviceJob.class
-echo "Don running GEO job exiting." 
+#unload to 'geo_record_attributions.txt'
+#(
+#select * from record_attribution ra
+#where ra.recattrib_source_zdb_id =  'ZDB-PUB-071218-1'
+#and ra.recattrib_data_zdb_id not like 'ZDB-DBLINK%'
+#)
+
+echo "delete from record_attribution where recattrib_source_zdb_id =  'ZDB-PUB-071218-1' and recattrib_data_zdb_id like 'ZDB-DBLINK%'  ;" | dbaccess -a $DBNAME ;
+
+// loads geo microarray records
+load from 'geo_record_attributions.txt' insert into record_attribution ; 
 
 
 
