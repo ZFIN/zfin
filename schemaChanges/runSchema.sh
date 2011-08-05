657104f048277a86c56d990bfedfe12abf60f901
Nathan Dunn <ndunn@zfin.org>
2011-08-05 17:53:16 +0000

    BugzID: 6859
    Synchronized with schema changes.
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24489 908b024a-c82b-635c-c293-f2b15b23277c

--- schemaChanges/runSchema.sh
+++ schemaChanges/runSchema.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
-dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql
+dbaccess -a $DBNAME /research/zunloads/projects/genePage/schema.sql ;
 
-dbaccess -a $DBNAME schema.sql
-
-#unload to 'geo_record_attributions.txt'
-#(
-#select * from record_attribution ra
-#where ra.recattrib_source_zdb_id =  'ZDB-PUB-071218-1'
-#and ra.recattrib_data_zdb_id not like 'ZDB-DBLINK%'
-#)
-
-echo "delete from record_attribution where recattrib_source_zdb_id =  'ZDB-PUB-071218-1' and recattrib_data_zdb_id like 'ZDB-DBLINK%'  ;" | dbaccess -a $DBNAME ;
-
-// loads geo microarray records
-load from 'geo_record_attributions.txt' insert into record_attribution ; 
-
-
+dbaccess -a $DBNAME /research/zunloads/projects/GAF/schema.sql ;
 
