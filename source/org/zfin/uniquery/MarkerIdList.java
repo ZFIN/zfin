4a2bfa1629bf7a83afea234398e408da10d326c3
Christian Pich <cmpich@zfin.org>
2011-09-09 21:30:51 +0000

    bugzID: 7551 merge forward urgent changes to 1013
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24725 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/uniquery/MarkerIdList.java
+++ source/org/zfin/uniquery/MarkerIdList.java
@@ -1,9 +1,11 @@
 package org.zfin.uniquery;
 
 import org.springframework.stereotype.Service;
+import org.zfin.infrastructure.ActiveData;
 import org.zfin.marker.repository.MarkerRepository;
 import org.zfin.repository.RepositoryFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,4 +18,17 @@ public class MarkerIdList extends AbstractEntityIdList {
         MarkerRepository markerRepository = RepositoryFactory.getMarkerRepository();
         return convertIdsIntoUrls(markerRepository.getNMarkersPerType(numberOfRecords));
     }
+
+    public List<String> convertIdsIntoUrls(List<String> ids) {
+        List<String> urlList = new ArrayList<String>(ids.size());
+        for (String id : ids) {
+            String typeName = ActiveData.validateID(id).name();
+            String individualUrl = getIndividualViewUrl(id, typeName);
+            if (individualUrl != null)
+                urlList.add(individualUrl);
+        }
+        return urlList;
+    }
+
+
 }
