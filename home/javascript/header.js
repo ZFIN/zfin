78eaccc3dc160de50a7acc6ae76e705ebf135393
Nathan Dunn <ndunn@zfin.org>
2011-10-04 16:44:51 +0000

    BugzID: 7600
    Fixed header.js gwt layout. 
    
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@24930 908b024a-c82b-635c-c293-f2b15b23277c

--- home/javascript/header.js
+++ home/javascript/header.js
@@ -8,17 +8,18 @@
  document.write('<LINK rel=stylesheet type="text/css" href="/css/spiffycorners.css">');
 
  <!-- Start GOOGLE Analytics -->
- document.write('<script type="text/javascript">'+
- " var _gaq = _gaq || []; "+
-  ' _gaq.push(["_setAccount", "UA-2417927-1"]); '+
-  ' _gaq.push(["_trackPageview"]); ' +
- ' (function() { '+
- '  var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;'+
- '  ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js"'+
- '  var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s); ' +
- ' })() ; ' +
- ' </script>'
-);
+ var _gaq = _gaq || [];
+ _gaq.push(['_setAccount', 'UA-2417927-1']);
+ _gaq.push(['_trackPageview']);
+
+ (function() {
+     var ga = document.createElement('script');
+     ga.type = 'text/javascript';
+     ga.async = true;
+     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+     var s = document.getElementsByTagName('script')[0];
+     s.parentNode.insertBefore(ga, s);
+ })();
  <!-- End GOOGLE Analytics -->
 
 
