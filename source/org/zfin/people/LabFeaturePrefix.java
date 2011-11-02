3886439079210a2f769f714294f9007831663d97
Prita Mani <pm@zfin.org>
2011-11-02 21:19:42 +0000

    BugzID:7257 Changed to accomodate source_feature_prefix table
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@25143 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/people/LabFeaturePrefix.java
+++ source/org/zfin/people/LabFeaturePrefix.java
@@ -10,7 +10,15 @@ public class LabFeaturePrefix implements Serializable{
     private Lab lab ;
     private Boolean isCurrentDesignation;
     private FeaturePrefix featurePrefix ;
+    private Organization organization;
 
+    public Organization getOrganization() {
+        return organization;
+    }
+
+    public void setOrganization(Organization organization) {
+        this.organization = organization;
+    }
 
     public Lab getLab() {
         return lab;
@@ -40,7 +48,7 @@ public class LabFeaturePrefix implements Serializable{
     public String toString() {
         final StringBuilder sb = new StringBuilder();
         sb.append("LabFeaturePrefix");
-        sb.append("{lab=").append(lab);
+        sb.append("{organization=").append(organization);
         sb.append(", isCurrentDesignation=").append(isCurrentDesignation);
         sb.append(", featurePrefix=").append(featurePrefix);
         sb.append('}');
