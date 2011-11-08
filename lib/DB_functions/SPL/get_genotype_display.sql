d9d28ccb04aa2face814f68067b4b2ce03c7be72
Informix User <informix@zfin.org>
2011-11-08 00:01:04 +0000

    revert get_genotype_display
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@25195 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/get_genotype_display.sql
+++ lib/DB_functions/SPL/get_genotype_display.sql
@@ -22,37 +22,27 @@ create function get_genotype_display( genoZdbId varchar(50) )
   --     none
   -------------------------------------------------------------------------- 
 
-  define genoDisplayHtml like genotype.geno_display_name;
+  define genoDisplayHtml lvarchar;
   define featAbbrev	 like feature.feature_abbrev;
-  define gcs	         like genotype_component_significance.gcs_significance;
  
   define featAbbrevHtml  lvarchar;
   define fad2		 lvarchar;
   define genoBackground  lvarchar;
   define zygAllele       lvarchar;
-  define tgRepeat        boolean;
-  define tgLastFeat      lvarchar;
-  define tgLastMrkr      lvarchar;
-  define featOrder       varchar(2);
-  define fmrelType       like feature_marker_relationship.fmrel_type;
 
   define featSig         like feature_type.ftrtype_significance;
   define startName       like genotype.geno_display_name;
   define wildtype        like genotype.geno_is_wildtype;
   define featType	 like feature.feature_type;
   define zygOrder	 like zygocity.zyg_abbrev;
-  define mrkrAbbrev	 like marker.mrkr_abbrev; 
-  define featMrkrAbbrev	 like feature.feature_mrkr_abbrev;
+  define mrkrAbbrev	 like marker.mrkr_abbrev;
+ 
 
   select geno_display_name, geno_is_wildtype 
   into startName, wildtype 
   from genotype where geno_zdb_id = genoZdbId;
 
-  let fad2 = '';  
-  let tgRepeat = 'f';
-  let tgLastMrkr = '';
-  let tgLastFeat = '';
-  let fmrelType = '';
+  let fad2 = '';
 
   if ( wildtype != 't') then  
   
@@ -78,55 +68,46 @@ create function get_genotype_display( genoZdbId varchar(50) )
                 end as fad2,
 	      feature_Abbrev,
 	      feature_type,
-	     zyg_abbrev, 
-	     mrkr_abbrev, 
-	     gcs_significance,
-	     fmrel_type
-
-         from feature, genotype_feature, zygocity, feature_type, feature_marker_relationship, marker, genotype_component_significance
+	      case 
+	      	   when feature_type = 'TRANSGENIC_INSERTION'
+		   then ftrtype_significance +2
+		   when feature_type = 'UNSPECIFIED'
+		   then ftrtype_significance -2
+		   else ftrtype_significance
+		   end,
+	     zyg_abbrev
+         from feature, genotype_feature, zygocity, feature_type,  outer (feature_marker_relationship, outer marker)
         where genofeat_geno_zdb_id = genoZdbId
           and genofeat_feature_zdb_id = feature_zdb_id
           and genofeat_zygocity = zyg_zdb_id
 	  and feature_type = ftrtype_name
 	  and fmrel_mrkr_zdb_id = mrkr_zdb_id
 	  and fmrel_ftr_zdb_id = feature_zdb_id
-	  and fmrel_type = gcs_fmrel_type
-	  and gcs_mrkr_type = mrkr_type
-	  and gcs_ftr_type = feature_type
-	  and fmrel_type = "is allele of"
-
+	  and fmrel_type = 'is allele of'
 	  union 
-
 	select distinct get_feature_abbrev_display(feature_zdb_id) as fad, 
                             zyg_allele_display, 
-              case 
-                when fmrel_type = "contains innocuous sequence feature"
-                then mrkr_abbrev || feature_abbrev
-                else lower(get_feature_abbrev_display(feature_zdb_id)) 
-                end as fad2,
+              lower(get_feature_abbrev_display(feature_zdb_id)) as fad2,
 	      feature_Abbrev,
 	      feature_type,
-              zyg_abbrev, 
-              mrkr_abbrev, 
-              gcs_significance,
-              fmrel_type
-		   
-          into featAbbrevHtml, zygAllele, mrkrAbbrev, featAbbrev, featType, zygOrder, featMrkrAbbrev, gcs, fmrelType
-          
+	      case 
+	      	   when feature_type = 'TRANSGENIC_INSERTION'
+		   then ftrtype_significance +2
+	   when feature_type = 'UNSPECIFIED'
+		   then ftrtype_significance -2
+		   else ftrtype_significance
+		   end,
+		   zyg_abbrev
+          into featAbbrevHtml, zygAllele, mrkrAbbrev,featAbbrev, featType, featSig, zygOrder
          from feature, genotype_feature, zygocity, feature_type,
-	      feature_marker_relationship as fm1, genotype_component_significance, marker
+	      feature_marker_relationship
         where genofeat_geno_zdb_id = genoZdbId
           and genofeat_feature_zdb_id = feature_zdb_id
           and genofeat_zygocity = zyg_zdb_id
 	  and feature_type = ftrtype_name
 	  and fmrel_ftr_zdb_id = feature_zdb_id
-	  and fmrel_mrkr_zdb_id = mrkr_zdb_id
-	  and fmrel_type = gcs_fmrel_type
-	  and gcs_mrkr_type = mrkr_type
-	  and gcs_ftr_type = feature_type
 	  and fmrel_type not in ('is allele of')
-
-       order by gcs_significance asc, mrkr_abbrev asc , zyg_abbrev , fad2, fad asc
+       order by zyg_abbrev  , fad2, fad asc
        
 
   if (fad2 == featAbbrevHtml) then
@@ -147,17 +128,7 @@ create function get_genotype_display( genoZdbId varchar(50) )
 	    else
 	    end if
         
-            if (featType == 'TRANSGENIC_INSERTION') then
-              
-
-              
-              if (featMrkrAbbrev == tgLastMrkr) then
-                  let tgRepeat = 't';
-              
-              end if       
-            
-            end if
-            
+          
 	    if (zygAllele is null) then
                let zygAllele = '';
             end if
@@ -166,15 +137,7 @@ create function get_genotype_display( genoZdbId varchar(50) )
                let genoDisplayHtml =  featAbbrevHtml ;
           
 	    else
-	       if (tgRepeat == 't' and featMrkrAbbrev != tgLastMrkr) then
-		   let genoDisplayHtml = genoDisplayHtml ||'... '||  tgLastFeat;
-		   let tgRepeat = 'f';
-	       
-	       end if
-	       
-	       if (tgRepeat == 'f') then
-		   let genoDisplayHtml = genoDisplayHtml ||'; '||  featAbbrevHtml;
-	       end if
+		   let genoDisplayHtml = genoDisplayHtml ||';'||  featAbbrevHtml;
        
             end if
 
@@ -190,20 +153,12 @@ create function get_genotype_display( genoZdbId varchar(50) )
 
            end if
 
-              
-           let tgLastFeat = featAbbrev;
-           let tgLastMrkr = featMrkrAbbrev;
-              
 	   let fad2 = featAbbrevHtml ;
 	   end if ;
 
    end foreach
 
-    if (tgRepeat == 't') then
-       let genoDisplayHtml = genoDisplayHtml ||'... '||  tgLastFeat;
-    end if
-    
-    let genoDisplayHTML = replace(genoDisplayHTML,'</sup><sup>',''); 
+    let genoDisplayHTML = replace(genoDisplayHTML,'</sup><sup>','');          
 
   else
   
