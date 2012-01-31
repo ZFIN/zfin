57871be86d52d94c9d8712ff5fe671a6c07e64c9
Sierra Moxon <smoxon@zfin.org>
2012-01-31 22:00:08 +0000

    remove dup call
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@25719 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/fishMart/runFishMart.sh
+++ server_apps/DB_maintenance/warehouse/fishMart/runFishMart.sh
@@ -6,7 +6,7 @@ setenv INFROMIXDIR /private/apps/Informix/informix/
 
 /bin/rm -rf <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartAutomated.sql 
 
-/bin/cat begin.sql dropTables.sql schemaTables.sql geneGroup.sql environmentGroup.sql featureGroup.sql morpholinoGroup.sql constructGroup.sql populateFunctionalAnnotation.sql phenoFigs.sql xpatFigs.sql phenoTermGroup.sql addGroupsToFunctionalAnnotation.sql addAliasesToFunctionalAnnotation.sql addCountsToFunctionalAnnotation.sql populateFishAnnotationSearch.sql populateFigureTermFishSearch.sql populateFigureTermFishSearch.sql translateFeatureTypeForTGs.sql addBackgroundsToFish.sql populateNullOrderingColumns.sql createBtsIndexes.sql commit.sql > <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartAutomated.sql
+/bin/cat begin.sql dropTables.sql schemaTables.sql geneGroup.sql environmentGroup.sql featureGroup.sql morpholinoGroup.sql constructGroup.sql populateFunctionalAnnotation.sql phenoFigs.sql xpatFigs.sql phenoTermGroup.sql addGroupsToFunctionalAnnotation.sql addAliasesToFunctionalAnnotation.sql addCountsToFunctionalAnnotation.sql populateFishAnnotationSearch.sql populateFigureTermFishSearch.sql translateFeatureTypeForTGs.sql addBackgroundsToFish.sql populateNullOrderingColumns.sql createBtsIndexes.sql commit.sql > <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartAutomated.sql
 
 echo "ready to start dropTables.sql" ;
 /private/apps/Informix/informix/bin/dbaccess -a $DBNAME <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartAutomated.sql
