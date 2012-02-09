bd5534c1209b232ddcf56c9f3417194b8a4483f6
Xiang Shao <xshao@zfin.org>
2012-02-09 20:24:16 +0000

    bugzid:8042 add code to check remove redundant DR lines with previous gene abbrevs
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@25865 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/SWISS-PROT/pre_loadsp.pl
+++ server_apps/data_transfer/SWISS-PROT/pre_loadsp.pl
@@ -6,6 +6,7 @@ use strict ;
 #
  
 use MIME::Lite;
+use DBI;
 
 
 #------------------- Download -----------
@@ -121,6 +122,29 @@ sub sendRunningResult {
   open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
   $msg4->print(\*SENDMAIL);
 
+ #----- Another mail send out problem files ----
+
+  my $SUBJECT="Auto: report of processing pre_zfin.org";
+  my $MAILTO="<!--|SWISSPROT_EMAIL_REPORT|-->";     
+  my $ATTFILE = "redGeneReport.txt";
+
+  # Create another new multipart message:
+  my $msg5 = new MIME::Lite 
+    From    => "$ENV{LOGNAME}",
+    To      => "$MAILTO",
+    Subject => "$SUBJECT",
+    Type    => 'multipart/mixed';
+
+  attach $msg5 
+    Type     => 'application/octet-stream',
+    Encoding => 'base64',
+    Path     => "./$ATTFILE",
+    Filename => "$ATTFILE";
+
+  # Output the message to sendmail
+  open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
+  $msg5->print(\*SENDMAIL);
+
   close(SENDMAIL);
 }
 
@@ -132,14 +156,14 @@ sub sendRunningResult {
 sub process_vertebrates{
     system("wget -q ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_trembl_vertebrates.dat.gz -O uniprot_trembl_vertebrates.dat.gz");
     system("gunzip uniprot_trembl_vertebrates.dat.gz");
-    system("cp uniprot_trembl_vertebrates.dat pre_zfin.dat");
+    system("cp uniprot_trembl_vertebrates.dat pre_pre_zfin.dat");
     system("wget -q ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/taxonomic_divisions/uniprot_sprot_vertebrates.dat.gz -O uniprot_sprot_vertebrates.dat.gz");
     system("gunzip uniprot_sprot_vertebrates.dat.gz");
-    system("cat uniprot_sprot_vertebrates.dat >> pre_zfin.dat");
+    system("cat uniprot_sprot_vertebrates.dat >> pre_pre_zfin.dat");
 
-    open(DAT, "pre_zfin.dat") || die("Could not open file!");
+    open(DAT, "pre_pre_zfin.dat") || die("Could not open file!");
 
-    open OUTPUT, ">zfin.dat" or die "Cannot open zfin.dat";
+    open OUTPUT, ">pre_zfin.dat" or die "Cannot open zfin.dat";
 
     # find each "//\n" and test to see if its a zfin record.
     my $buffer = "" ; 
@@ -183,6 +207,8 @@ $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
 $ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
 $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
 
+
+
 chdir "<!--|ROOT_PATH|-->/server_apps/data_transfer/SWISS-PROT/";
 
 
@@ -202,6 +228,224 @@ system("mkdir ./ccnote");
 
 
 &downloadGOtermFiles();
+
+
+my $dbname = "<!--|DB_NAME|-->";
+my $username = "";
+my $password = "";
+
+########################################################################################################
+#
+#  for FB case 8042 UniProt load: DR lines redundancy in the input file
+#
+########################################################################################################
+
+### open a handle on the db
+my $dbh = DBI->connect ("DBI:Informix:$dbname", $username, $password) 
+    or die "Cannot connect to Informix database: $DBI::errstr\n";
+    
+my $cur;
+
+$/ = "//\n";
+open(PREDAT, "pre_zfin.dat") || die("Could not open pre_zfin.dat !");
+my @blocks = <PREDAT>;
+close(PREDAT);
+    
+open ZFINDAT, ">zfin.dat" || die ("Cannot open zfin.dat !");
+open ZFINDATDELETED, ">zfinGeneDeleted.dat" || die ("Cannot open zfinGeneDeleted.dat !");
+open ZFINGENES, ">zfinGenes.dat" || die ("Cannot open zfinGenes.dat !");
+open DBG, ">debugfile.dat" || die ("Cannot open debugfile.dat !");
+    
+my $totalOnZfinDat = 0;
+my $totalOnDeleted = 0;
+my $ttt = 0;
+my @lines = ();
+my %toNewInput = ();
+my %deletes = ();
+my $ct = 0;
+my %ZDBgeneIDgeneAbbrevs = ();
+my $line;
+my $lineKey = 0;
+my @fields = ();
+my $ZFINgeneId;
+my $geneAbbrev;
+my $block;
+my $newLineNumber;
+my $key;
+foreach $block (@blocks) {
+   $ttt++;
+   if($block =~ m/OS   Danio rerio/) {
+        @lines = split(/\n/, $block);
+        %toNewInput = ();   
+        %deletes = (); 
+        $ct = 0;
+        %ZDBgeneIDgeneAbbrevs = ();
+        foreach $line (@lines) {
+           if($line !~ m/CC   -------/ && 
+             $line !~ m/CC   Copyrighted/ &&
+             $line !~ m/CC   Distributed/)  {
+                   
+               ## add 10000 to pad so that the sorting would be right
+               $lineKey = 10000 + $ct;
+               $toNewInput{$lineKey} = $line; 
+               $deletes{$lineKey} = 0;
+                   
+               if ($line =~ m/DR   ZFIN; ZDB-GENE-/) {
+                   @fields = split(/;/, $line);
+                   $ZFINgeneId = $fields[1];
+                   $ZFINgeneId =~ s/^\s+//; 
+                   $ZFINgeneId =~ s/\s+$//;                                          
+                     
+                   $geneAbbrev = $fields[2];
+                   $geneAbbrev =~ s/^\s+//; 
+                   $geneAbbrev =~ s/\s+$//;
+                   $geneAbbrev =~ s/\.$//;
+                          
+                   if ($ttt < 1000)  {
+                             print DBG "ZFINgeneId : $ZFINgeneId \t geneAbbrev : $geneAbbrev  \n";
+                             
+                             if (exists($ZDBgeneIDgeneAbbrevs{$ZFINgeneId})) {
+                                                              print DBG "exists  $ZDBgeneIDgeneAbbrevs{$ZFINgeneId} \n";
+                             }
+   
+                             if (!exists($ZDBgeneIDgeneAbbrevs{$ZFINgeneId})) {
+                                                                 print DBG "Not exists  ZDBgeneIDgeneAbbrevs{ZFINgeneId} \n";
+                             }
+   
+   
+                   }
+                          
+
+                   if (!exists($ZDBgeneIDgeneAbbrevs{$ZFINgeneId})) {
+                       $cur = $dbh->prepare('select mrkr_abbrev from marker where mrkr_zdb_id = ?;');
+
+                       $cur->execute($ZFINgeneId);
+                       my ($ZFINgeneAbbrev);
+                       $cur->bind_columns(\$ZFINgeneAbbrev);
+                       while ($cur->fetch()) {
+                           $ZDBgeneIDgeneAbbrevs{$ZFINgeneId} = $ZFINgeneAbbrev;
+                       }
+                       $cur->finish(); 
+                    ###       $line = $fields[0] . " " . ";" . $fields[1] . " " . ";" . $ZFINgeneAbbrev . ".";
+                       $line =~ s/$geneAbbrev/$ZFINgeneAbbrev/g;
+                       $toNewInput{$lineKey} = $line;
+                   } else {
+                       $deletes{$lineKey} = 1;
+                   }
+
+               }
+                                                        
+               $ct++;
+           }
+        }
+            
+        foreach $newLineNumber (sort keys %toNewInput) {
+           if ($deletes{$newLineNumber} == 0) {
+               print ZFINDAT "$newLineNumber ::: $toNewInput{$newLineNumber}\n";
+               $totalOnZfinDat = $totalOnZfinDat + 1;  
+           } else {
+               print ZFINDATDELETED "$toNewInput{$newLineNumber}\n";
+               $totalOnDeleted++;
+           }
+        }   
+            
+        foreach $key (sort keys %ZDBgeneIDgeneAbbrevs) {
+               print ZFINGENES "$ZDBgeneIDgeneAbbrevs{$key}\n";
+        } 
+            
+   }
+
+}
+    
+close(ZFINDAT); 
+close(ZFINDATDELETED); 
+close(ZFINGENES); 
+close(DBG);
+
+$dbh->disconnect();  
+
+
+open(PRE, "pre_zfin.dat") || die("Could not open pre_zfin.dat !");
+my @blocksPRE = <PRE>;
+close(PRE);
+my $prezfin = 0;
+my $totalprezfin = 0;
+    
+foreach $block (@blocksPRE){
+   $totalprezfin++;
+   if($block =~ m/OS   Danio rerio/) {
+      $prezfin++;
+   } 
+}
+
+
+
+open(ZF, "zfin.dat") || die("Could not open zfin.dat !");
+my @blocksZF = <ZF>;
+close(ZF);
+my $zfin = 0;
+my $totalzfin = 0;
+foreach $block (@blocksZF){
+   $totalzfin++;
+   if($block =~ m/OS   Danio rerio/) {
+      $zfin++;
+   }  
+}
+
+
+$/ = "\n";
+
+open(PRE, "pre_zfin.dat") || die("Could not open pre_zfin.dat !");
+my @blocksPRE = <PRE>;
+close(PRE);
+my $prezfinGene = 0;
+my $prezfinLines = 0;
+foreach $block (@blocksPRE){
+   $prezfinLines++;
+   if($block =~ m/DR   ZFIN; ZDB-GENE-/) {
+      $prezfinGene++;
+   }
+}
+
+
+
+open(ZF, "zfin.dat") || die("Could not open zfin.dat !");
+my @blocksZF = <ZF>;
+close(ZF);
+my $zfinGene = 0;
+my $zfinLines = 0;
+foreach $block (@blocksZF){
+   $zfinLines++;
+   if($block =~ m/DR   ZFIN; ZDB-GENE-/) {
+      $zfinGene++;
+   }  
+}
+
+
+print "totalOnZfinDat = $totalOnZfinDat\t totalOnDeleted = $totalOnDeleted\n\n\n";
+print "prezfin = $prezfin\t";
+print "totalprezfin = $totalprezfin\n";
+print "zfin = $zfin\t";
+print "totalzfin = $totalzfin\n";
+
+print "\n\n\n";
+print "prezfinGene = $prezfinGene\t";
+print "prezfinLines = $prezfinLines\n";
+print "zfinGene = $zfinGene\t";
+print "zfinLines = $zfinLines\n";
+
+open(REDGENERPT, ">redGeneReport.txt") || die("Could not open redGeneReport.txt !");
+print REDGENERPT "totalOnZfinDat = $totalOnZfinDat\t totalOnDeleted = $totalOnDeleted\n\n\n";
+print REDGENERPT "prezfin = $prezfin\t";
+print REDGENERPT "totalprezfin = $totalprezfin\n";
+print REDGENERPT "zfin = $zfin\t";
+print REDGENERPT "totalzfin = $totalzfin\n";
+
+print REDGENERPT "\n\n\n";
+print REDGENERPT "prezfinGene = $prezfinGene\t";
+print REDGENERPT "prezfinLines = $prezfinLines\n";
+print REDGENERPT "zfinGene = $zfinGene\t";
+print REDGENERPT "zfinLines = $zfinLines\n";
 
 
 #--------------- Delete records from last SWISS-PROT loading-----
