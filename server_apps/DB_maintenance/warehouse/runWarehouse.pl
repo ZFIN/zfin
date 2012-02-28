4491731c24b204ef871e9f7a33be108abed872a8
Informix User <informix@zfin.org>
2012-02-28 19:31:57 +0000

    try changing the check status of starttomcat.pl:sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26074 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/runWarehouse.pl
+++ server_apps/DB_maintenance/warehouse/runWarehouse.pl
@@ -33,14 +33,14 @@ $dbhZfin = DBI->connect("DBI:Informix:$whoIsZfinDb",
 		       '',
 		       {AutoCommit => 1, RaiseError => 1}
 		       )
-          or die ("Failed while connecting to $whoIsZfin\n");
+          or &logError ("Failed while connecting to $whoIsZfin\n");
 
 $dbhNotZfin = DBI->connect("DBI:Informix:$whoIsNotZfinDb",
 		       '',
 		       '',
 		       {AutoCommit => 1, RaiseError => 1}
 		       )
-          or die ("Failed while connecting to $whoIsNotZfin\n");
+          or &logError ("Failed while connecting to $whoIsNotZfin\n");
 
 ### MAIN ###
 `/bin/rm -rf $globalResultFile`;
@@ -65,15 +65,15 @@ sub cronStart($){
     # hardcode cron starting for kinetix and watson and crick only.  This needs attention to be more
     # generic.
     if (("<!--|MACHINE_NAME|-->" eq "kinetix") && ($whoIsZfinDb eq "watsondb")){
-	chdir("/research/zprod/users/watson/ZFIN_WWW/server_apps/cron") or die "can't chdir to server_apps/cron";
+	chdir("/research/zprod/users/watson/ZFIN_WWW/server_apps/cron") or &logError("can't chdir to server_apps/cron");
 	system("/local/bin/gmake start"); 
-	print "last return from gmake start is: $?";
+	print "last return from gmake start is: $?\n";
     }
      
     if (("<!--|MACHINE_NAME|-->" eq "kinetix") && ($whoIsZfinDb eq "crickdb")){
-	chdir("/research/zprod/users/crick/ZFIN_WWW/server_apps/cron") or die "can't chdir to server_apps/cron";	
+	chdir("/research/zprod/users/crick/ZFIN_WWW/server_apps/cron") or &logError("can't chdir to server_apps/cron");	
 	system("/local/bin/gmake start");
-	print "last return from gmake start is: $?";
+	print "last return from gmake start is: $?\n";
     }
     print "zfin is: $whoIsZfinDb";
     print "zfin is not: $whoIsNotZfinDb";
@@ -82,9 +82,9 @@ sub cronStart($){
 
 sub cronStop($) {
     if ("<!--|MACHINE_NAME|-->" eq "kinetix"){
-	chdir("<!--|SOURCEROOT|-->/server_apps/cron") or die "can't chdir to server_apps/cron";
+	chdir("<!--|SOURCEROOT|-->/server_apps/cron") or &logError("can't chdir to server_apps/cron");
 	system("/local/bin/gmake stop"); 
-	print "last return from gmake stop is: $?";
+	print "last return from gmake stop is: $?\n";
     }
 }
 
@@ -94,7 +94,7 @@ sub getEnvFileName {
 
     #print $whoIsNotZfinDb;
     my $sthEn = $dbhNotZfin->prepare("select denm_env_file_name from database_env_name_matrix where denm_db_name = '$whoIsNotZfinDb';");
-    $sthEn->execute() or die "could not execute";
+    $sthEn->execute() or &logError("could not execute");
     $sthEn->bind_columns(\$env);
     $sthEn->dump_results();
     for ($env) {
@@ -106,8 +106,8 @@ sub getEnvFileName {
 }
 
 sub swapZfin(){
-    chdir("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/") or die "can't chdir to <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/";
-    system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/switch_test.sh") && die "switch zfin to new warehouse failed.";
+    chdir("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/") or &logError("can't chdir to <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/");
+    system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/switch_test.sh") && &logError("switch zfin to new warehouse failed.");
     
 }
 sub disableUpdates() {
@@ -116,7 +116,7 @@ sub disableUpdates() {
     system("/private/ZfinLinks/Commons/bin/stoptomcat.pl $whoIsNotZfinDb");
     print "$? \n";
     if($? ne 0){
-	die "stoptomcat.pl failed";
+	&logError("stoptomcat.pl failed");
     }
 }
 
@@ -125,9 +125,12 @@ sub enableUpdates() {
     $flag2->execute;
     print "updates enabled\n";
     print "restarting tomcat";
-    chdir("/private/ZfinLinks/Commons/bin") or die "can't chdir to /private/ZfinLinks/Commons/bin";
+    chdir("/private/ZfinLinks/Commons/bin") or &logError("can't chdir to /private/ZfinLinks/Commons/bin");
     print ("starttomcat.pl on $whoIsNotZfinDb");
-    system("/private/ZfinLinks/Commons/bin/starttomcat.pl $whoIsNotZfinDb") or die "can't starttomcat.pl";
+    system("/private/ZfinLinks/Commons/bin/starttomcat.pl $whoIsNotZfinDb") ;
+    if($? ne 0){
+	&logError("starttomcat.pl on $whoIsNotZfinDb failed");
+    }
 }
 
 sub loadDb() {
@@ -141,28 +144,28 @@ sub loadDb() {
         s/\s+$//;
     }
     system("/private/ZfinLinks/Commons/bin/unloaddb.pl $whoIsZfinDb <!--|WAREHOUSE_DUMP_DIR|-->/$dirName/");
-    print "return from unloaddb.pl is: $?";
+    print "return from unloaddb.pl is: $?\n";
     $dbhNotZfin->disconnect;
     system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/loadDb.sh $whoIsNotZfinDb <!--|WAREHOUSE_DUMP_DIR|-->/ <!--|SOURCEROOT|--> <!--|SOURCEROOT|-->/commons/env/$envName") ;
     if ($? ne 0){
-	die "loadDb.sh failed";
+	&logError("loadDb.sh failed");
     }
     $dbhNotZfin = DBI->connect("DBI:Informix:$whoIsNotZfinDb",
 		       '',
 		       '',
 		       {AutoCommit => 1, RaiseError => 1}
 		       )
-          or die ("Failed while connecting to $whoIsNotZfin\n");
+          or &logError("Failed while connecting to $whoIsNotZfin\n");
     #cp unload to zfindb location.
 }
 sub runWarehouse() {
-    chdir ("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/") or die "can't chdir to server_apps/DB_maintenance/warehouse/fishMart/";
+    chdir ("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/") or &logError("can't chdir to server_apps/DB_maintenance/warehouse/fishMart/");
     system("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/runFishMart.sh $whoIsNotZfinDb > ../warehouseSqlReport.txt");
     print $?;
     if ($? ne 0){
-	die "runFishMart.sh died";
+	&logError("runFishMart.sh &logErrord");
     }
-    chdir ("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/") or die "can't chdir to DB_maintenance/warehouse/";
+    chdir ("<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/") or &logError("can't chdir to DB_maintenance/warehouse/");
 }
 
 sub execSql {
@@ -170,8 +173,8 @@ sub execSql {
   my $sql = shift;
   my $nRecords = 0;
  
-  my $sthE = $dbhNotZfin->prepare($sql) or die "Prepare fails";
-  $sthE -> execute() or die "could not execute";
+  my $sthE = $dbhNotZfin->prepare($sql) or &logError("Prepare fails");
+  $sthE -> execute() or &logError("could not execute");
   my @row = $sthE->fetchrow_array();
   $nRecords = $row[0];
   return ($nRecords);
@@ -179,8 +182,8 @@ sub execSql {
 
 sub updateWarehouseReleaseTracking {
     my $sql = shift;
-    my $sthU = $dbhNotZfin->prepare($sql) or die "Prepare fails on update release tracking";
-    $sthU -> execute() or die "could not execute update release tracking";
+    my $sthU = $dbhNotZfin->prepare($sql) or &logError("Prepare fails on update release tracking");
+    $sthU -> execute() or &logError("could not execute update release tracking");
 }
 
 sub integrityChecks($){
@@ -191,96 +194,96 @@ sub integrityChecks($){
     my $fishAnnotationSearchCount = execSql ($sql, undef);
 
     if ($fishAnnotationSearchCount < 16000) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "fish_annotation_search table does not have enough records.< 16000";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "fish_annotation_search table does not have enough records.< 16000";	
+	&logError("fish_annotation_search table does not have enough records.< 16000");	
     }
     print "fish_annotation_search has enough records.\n";
     $sql = 'select count(*) as counter from fish_annotation_search where fas_genotype_group is null and fas_genox_group is null';
     my $genotypeGenoxAvailable = execSql ($sql, undef);
 
     if ($genotypeGenoxAvailable >0) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "$genotypeGenoxAvailable records are missing both a genotype and a genox_group";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "$genotypeGenoxAvailable records are missing both a genotype and a genox_group";
+	&logError("$genotypeGenoxAvailable records are missing both a genotype and a genox_group");
     }
     print "fish_annotation_Search geno/genox ok.\n";
     $sql = 'select count(*) as counter from fish_annotation_search where fas_all is null';
     my $checkFasAllNotNull = execSql($sql, undef);
 
     if ($checkFasAllNotNull >0) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "$checkFasAllNotNull records have null fas_all values";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "$checkFasAllNotNull records have null fas_all values";
+	&logError("$checkFasAllNotNull records have null fas_all values");
     }
     print "fas_all populated ok.\n";
     $sql = 'select count(*) as counter from gene_feature_result_view where gfrv_construct_zdb_id is null and gfrv_construct_abbrev_order is not null';
     my $constructAbbrevOrderMismatchCount = execSql($sql, undef);
 
     if ($constructAbbrevOrderMismatchCount >0) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "$constructAbbrevOrderMismatchCount records have mismatched construct abbrev/order mismatches in gene_feature_result_view";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "$constructAbbrevOrderMismatchCount records have mismatched construct abbrev/order mismatches in gene_feature_result_view";
+	&logError("$constructAbbrevOrderMismatchCount records have mismatched construct abbrev/order mismatches in gene_feature_result_view");
     }
     print "construct abbrevs and orders in gene_feature_result_view ok.\n";
     $sql = 'select count(*) as counter from fish_annotation_search where fas_fish_significance < 999999';
     my $fishSignificanceCount = execSql($sql, undef);
 
     if ($fishSignificanceCount < 10) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "only $fishSignificanceCount records have significance < 999999";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "only $fishSignificanceCount records have significance < 999999";
+	&logError("only $fishSignificanceCount records have significance < 999999");
     }
     print "fish_significance in fish_annotation_search ok.\n";
     $sql = 'select count(*) as counter from gene_feature_result_view';
     my $geneFeatureResultViewCount = execSql($sql, undef);
 
     if ($geneFeatureResultViewCount < 20312) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "gene_feature_result_view table does not have enough records. < 20312";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "gene_feature_result_view table does not have enough records. < 20312";
+	&logError("gene_feature_result_view table does not have enough records. < 20312");
     }
    print "gene_feature_result_view has enough records.\n";
     $sql = 'select count(*) as counter from figure_term_fish_search';
     my $figureTermFishSearchCount = execSql($sql, undef);
 
     if ($figureTermFishSearchCount < 13997) {
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "figure_term_fish_search table does not have enough records.< 13997";
 	close(RESULT);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
 	updateWarehouseReleaseTracking($wrt_fail, undef);
-	die "figure_term_fish_search table does not have enough records.< 13997";
+	&logError("figure_term_fish_search table does not have enough records.< 13997");
     }
    print "figure_term_fish_search has enough records ok.\n";
     $sql = 'select count(*) as counter from fish_annotation_search where bts_contains(fas_all, "shha")';
 
     my $btsContainssshhaCount = execSql($sql, undef);
     if ($btsContainssshhaCount < 81){
-	open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+	open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
 	print RESULT "bts index on fish_annotation_search is inacurate: shha < 81";
 	close(RESULT);
 	updateWarehouseReleaseTracking($wrt_fail, undef);
 	&sendMail("ERROR","<!--|WAREHOUSE_REGN_EMAIL|-->","warehouse generation on NotZfinDb: FAIL","$globalResultFile");
-	die "bts index on fish_annotation_search is inacurate: shha < 81";
+	&logError("bts index on fish_annotation_search is inacurate: shha < 81");
     }
    print "fas_all bts_index ok.\n";
 }
@@ -288,7 +291,7 @@ sub integrityChecks($){
 sub updateWarehouseTracking($){
 
     my $sqlUpdate = 'update warehouse_run_tracking set (wrt_mart_load_successful, wrt_last_loaded_date) = ("t",current year to second) where wrt_mart_name = "$martName"';
-    open RESULT, ">$globalResultFile" or die "Cannot open the file to write check result.";
+    open RESULT, ">$globalResultFile" or &logError("Cannot open the file to write check result.");
     print RESULT "$martName updated successfully.";
     close(RESULT);
     updateWarehouseReleaseTracking($sqlUpdate, undef);
@@ -321,3 +324,19 @@ sub sendMail($) {
     
 }
 
+sub logError(@) {
+    open RESULT, ">$globalResultFile" or die "Cannot open the file to write error.";
+    my $line;
+    print(RESULT "\n");
+    foreach $line (@_) {
+        print(RESULT "ERROR: $line\n");
+    }
+    print(RESULT "\n");
+    &cronStart();
+    my $flagError = $dbhZfin->prepare ("update zdb_flag set zflag_is_on = 'f' where zflag_name = 'disable updates'");
+    $flagError->execute;
+    print "updates enabled on $dbhZfin\n";
+    
+    exit -1;
+
+}
