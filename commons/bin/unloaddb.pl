287d49c406c0f3a45c8fd7399ad8300dc396616b
Informix User <informix@zfin.org>
2012-03-01 18:21:06 +0000

    add informix variables to unloaddb.pl so it can run from cron outside of unload_production.pl: sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26096 908b024a-c82b-635c-c293-f2b15b23277c

--- commons/bin/unloaddb.pl
+++ commons/bin/unloaddb.pl
@@ -42,6 +42,13 @@
 use English;
 use DBI;
 
+setenv INFORMIXDIR <!--|INFORMIX_DIR|-->
+setenv INFORMIXSERVER <!--|INFORMIX_SERVER|-->
+setenv ONCONFIG <!--|ONCONFIG_FILE|-->
+setenv INFORMIXSQLHOSTS ${INFORMIXDIR}/etc/<!--|SQLHOSTS_FILE|-->
+setenv LD_LIBRARY_PATH ${INFORMIXDIR}/lib:${INFORMIXDIR}/lib/esql
+setenv PATH <!--|INFORMIX_DIR|-->/bin:/private/ZfinLinks/Commons/bin:$PATH
+
 #------------------------------------------------------------------------
 # Log message.  Writes a message to output.
 #
