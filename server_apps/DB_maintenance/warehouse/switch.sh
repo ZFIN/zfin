31fd74a22acc90f0a665665931fe4968fbc2a0c5
Informix User <informix@zfin.org>
2012-03-05 17:28:52 +0000

    changed instance location in smith.properties:sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26134 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/switch.sh
+++ server_apps/DB_maintenance/warehouse/switch.sh
@@ -5,20 +5,20 @@ ZFINDB=`who_is.sh`
 if [ "$ZFINDB" = "<!--|DB_NAME|-->" ] #smithdb
 then
     #Swap apache config symlinks
-    rm /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->.zfin.org
+    rm /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->
     ln -s /private/etc/apache/vhosts/switch-<!--|PARTNER_INTERNAL_INSTANCE|-->_<!--|SHARED_DOMAIN_NAME|--> \
-          /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->.zfin.org
-    rm /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->.zfin.org
+          /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->
+    rm /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->
     ln -s /private/etc/apache/vhosts/switch-<!--|INSTANCE|-->_not_<!--|SHARED_DOMAIN_NAME|--> \
-          /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->.zfin.org
+          /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->
 else
     #Swap apache config symlinks
-    rm /private/etc/apache/conf-test.zfin.org
+    rm /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->
     ln -s /private/etc/apache/vhosts/switch-<!--|INSTANCE|-->_<!--|SHARED_DOMAIN_NAME|--> \
-          /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->.zfin.org
-    rm /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->.zfin.org
+          /private/etc/apache/conf-<!--|SHARED_DOMAIN_NAME|-->
+    rm /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->
     ln -s /private/etc/apache/vhosts/switch-<!--|PARTNER_INTERNAL_INSTANCE|-->_not_<!--|SHARED_DOMAIN_NAME|--> \
-          /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->.zfin.org
+          /private/etc/apache/conf-not_<!--|SHARED_DOMAIN_NAME|-->
 fi
 
 /private/ZfinLinks/Commons/bin/restartapache.pl
