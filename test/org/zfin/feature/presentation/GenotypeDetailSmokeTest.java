a73534e220800b5763d3662ecf640c70546a9e6b
Christian Pich <cmpich@zfin.org>
2012-03-15 21:13:28 +0000

    bugzID: 7869 added smoke test for wild-type lines page
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26238 908b024a-c82b-635c-c293-f2b15b23277c

--- test/org/zfin/feature/presentation/GenotypeDetailSmokeTest.java
+++ test/org/zfin/feature/presentation/GenotypeDetailSmokeTest.java
@@ -1,9 +1,12 @@
 package org.zfin.feature.presentation;
 
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlLink;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSpan;
 import org.zfin.AbstractSmokeTest;
+
+import java.util.List;
 
 /**
  */
@@ -30,6 +33,26 @@ public class GenotypeDetailSmokeTest extends AbstractSmokeTest {
                 assertNotNull(htmlSpan);
                 assertEquals("MO1-foxn4", htmlSpan.getTextContent());
 
+            } catch (Exception e) {
+                fail(e.toString());
+            }
+        }
+    }
+
+    public void testAllWildtypePAge() {
+        for (WebClient browser : curationWebClients) {
+            browser.setJavaScriptEnabled(true);
+            String pageUrl = "/action/feature/wildtype-list";
+            try {
+                browser.waitForBackgroundJavaScript(2000);
+                HtmlPage page = browser.getPage("http://" + domain + pageUrl);
+                browser.waitForBackgroundJavaScriptStartingBefore(1000);
+                assertEquals("ZFIN: Wild-Type Lines: Summary Listing", page.getTitleText());
+
+                // check that AB genotype is listed.
+                List<Object> links = (List<Object>) page.getByXPath("//a[@id='ZDB-GENO-960809-7']");
+                assertNotNull(links);
+                assertEquals(1, links.size());
             } catch (Exception e) {
                 fail(e.toString());
             }
