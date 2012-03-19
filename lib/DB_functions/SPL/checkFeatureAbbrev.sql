eef9c82d239b01bb7f106c1f83a7190b4f5048be
Informix User <informix@zfin.org>
2012-03-19 18:34:17 +0000

    replace checkfeatureabbrev so gmake postloaddb works:sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26251 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/checkFeatureAbbrev.sql
+++ lib/DB_functions/SPL/checkFeatureAbbrev.sql
@@ -1,8 +1,9 @@
-create procedure checkFeatureAbbrev (vFeatureZdbId varchar(50),
+create procedure checkfeatureabbrev (vFeatureZdbId varchar(50),
        		 		     vFeatureType varchar(30), 
        		 		     vFeatureAbbrev varchar(70), 
 				     vFeatureLabPrefixId int8, 
-				     vFeatureLineNumber varchar(70), 
+				     vFeatureLineNumber varchar(70),
+				    
 				     vFeatureDfTranslocComplexPrefix varchar(60),
 				     vFeatureDominant boolean, 
 				     vFeatureUnspecified boolean,
@@ -10,21 +11,22 @@ create procedure checkFeatureAbbrev (vFeatureZdbId varchar(50),
 				     vFeatureTgSuffix varchar(5),
 				     vFeatureKnownInsertionSite boolean)
 
-       define vFeatureLabPrefix like feature_prefix.fp_prefix;
+     	define vFeatureLabPrefix like feature_prefix.fp_prefix;
 
        let vFeatureLabPrefix =
-       	   (select fp_prefix 
-	   	   from feature_prefix
-		   where vFeatureLabPrefixId = fp_pk_id);
+           (select fp_prefix 
+                   from feature_prefix
+                   where vFeatureLabPrefixId = fp_pk_id);
+
 
        if (vFeatureUnspecified ='t')
        then
-          if (vFeatureAbbrev != vFeatureMrkrAbbrev||"_unspecified")
+          if (vFeatureAbbrev not like "%\_unspecified")
 	  then raise exception -746,0,"FAIL!: unspecified allele must have abbrev like _unspecified. checkFeatureAbbrev.";
 	  end if;
        elif (vFeatureUnrecovered ='t')
        then
-          if (vFeatureAbbrev != vFeatureMrkrAbbrev||"_unrecovered")
+          if (vFeatureAbbrev not like "%\_unrecovered")
 	  then raise exception -746,0,"FAIL!: unrecovered allele must have abbrev like _unrecovered. checkFeatureAbbrev.";
 	  end if;
        elif (vFeatureDominant = 't')
@@ -50,12 +52,9 @@ create procedure checkFeatureAbbrev (vFeatureZdbId varchar(50),
       	    then raise exception -746,0,"FAIL!: complex, DF, T must have abbrev like labPrefix||lineNumber. checkFeatureAbbrev.";
 	    end if;
        else 
-       	    if (vFeatureType != vFeatureLabPrefix||vFeatureLineNumber and vFeatureUnspecified = 'f' and vFeatureDominant = 'f' and vFeatureMrkrAbbrev is null and vFeatureKnownInsertionSite = 'f' and vFeatureDfTranslocComplexPrefix is null and vFeatureTgSuffix = 'f')
+       	    if (vFeatureAbbrev != vFeatureLabPrefix||vFeatureLineNumber and vFeatureUnspecified = 'f' and vFeatureDominant = 'f' and vFeatureKnownInsertionSite = 'f' and vFeatureDfTranslocComplexPrefix is null and vFeatureTgSuffix = 'f')
 	    then raise exception -746,0,"FAIL!: feature_abbrev != fPrefix||fLineNumber. checkFeatureAbbrev.";
 	    end if;
        end if;
-
---check that abbrev is prefix+suffix except when transgenic insertion, then tgconstruct+prefix+suffix
---if unspecified then mrkr_abbrev+_unspecified
 	    
 end procedure;
