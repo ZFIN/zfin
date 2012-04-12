c560721f9ca914a430aea4d4356db21c8af14149
Tom Conlin <tomc@zfin.org>
2012-04-12 22:07:46 +0000

    bugzid: 8077 rewrote the antibody query as it no longer returned results
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26486 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Downloads/GFF3/unload_antibody_gff.sql
+++ server_apps/data_transfer/Downloads/GFF3/unload_antibody_gff.sql
@@ -1,26 +1,25 @@
 ! echo "unload_antibody_gff.sql -> antibody.gff3"
 
 UNLOAD to '<!--|ROOT_PATH|-->/home/data_transfer/Downloads/antibody.gff3' DELIMITER "	"
-select vg.gff_seqname,
+select gff_seqname,
            "ZFIN_Antibody" gff_source,
-           case gene.mrkr_type when 'GENEP' then 'pseudogene'else 'gene' end  feature,
-           min(vg.gff_start) gstart,
-           max(vg.gff_end)   gend,
+           case gene.mrkr_type when 'GENEP' then 'pseudogene'else 'gene' end feature,
+           min(gff_start) gstart,
+           max(gff_end)   gend,
            "." gff_score,
-           vg.gff_strand,
+           gff_strand,
            "." gff_frame,
        'gene_id=' || gene.mrkr_zdb_id ||';Name='
        || gene.mrkr_abbrev  ||';Alias='|| gene.mrkr_zdb_id attribute
+from marker gene 
+ join marker_relationship ab on gene.mrkr_zdb_id == ab.mrel_mrkr_1_zdb_id
+ join marker_relationship gt on ab.mrel_mrkr_1_zdb_id == gt.mrel_mrkr_1_zdb_id
+ join db_link on gt.mrel_mrkr_2_zdb_id == dblink_linked_recid
+ join gff3 on gff_ID = dblink_acc_num
 
-from db_link, marker_relationship, marker gene, gff3 vg, gff3 vt
-
-where mrel_type = 'gene product recognized by antibody'
-  and gene.mrkr_zdb_id   = mrel_mrkr_1_zdb_id --  idx-idx
-  and gene.mrkr_zdb_id   = dblink_linked_recid--  idx-idx
-  and vt.gff_source = 'vega' and vt.gff_feature = 'transcript'
-  and vg.gff_source = 'vega' and vg.gff_feature = 'gene'
-  and dblink_acc_num = vg.gff_ID
-  and vt.gff_Parent = vg.gff_ID
+where ab.mrel_type = 'gene product recognized by antibody'
+  and gt.mrel_type = 'gene produces transcript'
+  and gff_source = 'vega' and gff_feature = 'transcript' 
 group by 1,3,7,9;
 
 -- to be valid the gff3 requires a header
