81fa328b62eaa056080bba4b0aee84ff5bc2f188
Sierra Moxon <smoxon@zfin.org>
2012-04-24 19:13:56 +0000

    bugzid: 8404 correct trigger chain
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26580 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/feature_name_update_trigger.sql
+++ lib/DB_triggers/feature_name_update_trigger.sql
@@ -18,10 +18,10 @@ create trigger feature_name_update_trigger update of
 				     newf.feature_known_insertion_site),
 	execute function zero_pad(newf.feature_name) 
         	into feature.feature_name_order,
-	execute procedure p_update_related_genotype_names(
-		newf.feature_zdb_id),
+
 	execute procedure fhist_event(newf.feature_zdb_id,
        		'reassigned', newf.feature_name,oldf.feature_name),
     execute procedure checkDupFeaturePrefixLineDesignation (newf.feature_lab_prefix_id, newf.feature_line_number),
- execute procedure populate_feature_tracking(newf.feature_Abbrev, newf.feature_name, newf.feature_zdb_id) 
+	execute procedure p_update_related_genotype_names(
+		newf.feature_zdb_id)
 );
