aa66f8b0c2e035b3d19b9a63717c5774b0242805
Brock Sprunge <bsprunge@zfin.org>
2012-04-25 00:11:00 +0000

    BugzId: 8407
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26591 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_functions/SPL/get_genotype_display.sql
+++ lib/DB_functions/SPL/get_genotype_display.sql
@@ -132,7 +132,8 @@ create function get_genotype_display( genoZdbId varchar(50) )
 	          select *
 	          from feature_marker_relationship as fm2
 	          where fm2.fmrel_ftr_zdb_id = fm1.fmrel_ftr_zdb_id
-	            and fm1.fmrel_type = "contains innocuous sequence feature"
+	            and fm1.fmrel_type in ("contains innocuous sequence feature",
+	                                   "contains phenotypic sequence feature")
 	            and fm2.fmrel_type = "is allele of"
 	       )
 
