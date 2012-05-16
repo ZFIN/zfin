80f1e9a4f0d87d673d1543a2e14141212b9ea2de
Sierra Moxon <smoxon@zfin.org>
2012-05-16 20:24:08 +0000

    add xpatfig counts to fish_annotation_search
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26749 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/fishMart/xpatFigs.sql
+++ server_apps/DB_maintenance/warehouse/fishMart/xpatFigs.sql
@@ -3,32 +3,45 @@
 
 --update statistics high for table functional_annotation;
 
-insert into xpat_figure_group (xfigg_genox_zdb_id, xfigg_geno_handle)
-  select distinct xpatex_genox_zdb_id, geno_handle from expression_Experiment, experiment, genotype, genotype_Experiment
-  where exp_name != '_Standard'
-  and exp_name != '_Generic-control'
-  and genox_zdb_id = xpatex_genox_zdb_id
-  and exp_zdb_id = genox_exp_zdb_id
-  and genox_geno_zdb_id = geno_zdb_id
- and geno_is_wildtype = 't';
+
+insert into xpat_figure_group (xfigg_genox_zdb_id)
+  select distinct xpatex_genox_zdb_id
+  from expression_pattern_Figure, expression_Result, expression_experiment
+  where xpatres_zdb_id = xpatfig_xpatres_zdb_id
+  and xpatres_expression_found = 't'
+  and xpatex_zdb_id = xpatres_Xpatex_zdb_id
+ and not exists (Select 'x' from genotype,genotype_Experiment where geno_is_wildtype = 't'
+     	 		and genox_geno_zdb_id = geno_zdb_id
+			and genox_Zdb_id = xpatex_genox_zdb_id
+			and genox_is_std_or_generic_control = 't');
+
+select count(*) from xpat_figure_group 
+where exists (Select 'x' from genotype_experiment where genox_zdb_id = xfigg_genox_zdb_id and genox_is_std_or_Generic_control ='t');
+
+
 
 --update statistics high for table xpat_figure_group;
 
-select distinct xpatfig_fig_Zdb_id, xfigg_genox_zdb_id
-  from expression_pattern_Figure, expression_Result, expression_experiment, xpat_Figure_group
+select distinct xpatfig_fig_Zdb_id, xpatex_genox_zdb_id
+  from expression_pattern_Figure, expression_Result, expression_experiment
   where xpatres_zdb_id = xpatfig_xpatres_zdb_id
   and xpatres_xpatex_zdb_id = xpatex_zdb_id
-  and xpatex_genox_zdb_id = xfigg_genox_zdb_id
+  and xpatres_expression_found = 't'
+ and not exists (Select 'x' from genotype,genotype_Experiment 
+     	 		where geno_is_wildtype = 't'
+     	 		and genox_geno_zdb_id = geno_zdb_id
+			and genox_Zdb_id = xpatex_genox_zdb_id
+			and genox_is_std_or_generic_control = 't')
 into temp tmp_xpat;
 
 create index xfig_genox_index
-  on tmp_xpat (xfigg_genox_zdb_id)
+  on tmp_xpat (xpatex_genox_zdb_id)
   using btree in idxdbs1;
 
 --set explain on avoid_Execute;
 update xpat_figure_group
   set xfigg_group_name = replace(replace(replace(substr(multiset  (select item xpatfig_Fig_Zdb_id from tmp_xpat
-      		       	 					 	 	  where tmp_xpat.xfigg_genox_zdb_id = xpat_figure_group.xfigg_genox_zdb_id
+      		       	 					 	 	  where tmp_xpat.xpatex_genox_zdb_id = xpat_figure_group.xfigg_genox_zdb_id
 										  )::lvarchar,11),""),"'}",""),"'","");
 
 --set explain off;
@@ -38,10 +51,13 @@ insert into xpat_figure_group_member (xfiggm_group_id, xfiggm_member_name, xfigg
     where xpatex_genox_zdb_id = xfigg_genox_zdb_id
     and xpatex_zdb_id = xpatres_xpatex_zdb_id
     and xpatres_zdb_id = xpatfig_xpatres_zdb_id
-    and fig_zdb_id = xpatfig_fig_zdb_id;
+    and fig_zdb_id = xpatfig_fig_zdb_id
+    and not exists (Select 'x' from genotype,genotype_Experiment where geno_is_wildtype = 't'
+     	 		and genox_geno_zdb_id = geno_zdb_id
+			and genox_Zdb_id = xpatex_genox_zdb_id
+			and genox_is_std_or_generic_control = 't');
 
 update functional_annotation
   set fa_xpat_figure_group = (select xfigg_group_name 
       			      	      from xpat_figure_group 
 				      where fa_genox_zdb_id = xfigg_genox_zdb_id);
-
