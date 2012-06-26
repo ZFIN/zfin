58e89c20d2f92f0fe7e0943c4813201b5c008e5b
Tom Conlin <tomc@zfin.org>
2012-06-26 21:09:55 +0000

    adding more documentation and missing files
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@26978 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Load/Vega/update_withdrawn_dblink.sql
+++ server_apps/data_transfer/Load/Vega/update_withdrawn_dblink.sql
@@ -6,6 +6,9 @@
 
 
 begin work;
+
+! echo "If a transcript is flagged as withdrawn "
+! echo "make it's ottdarT dblink point to Vega_Withdrawn"
 update db_link set dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-100114-1' -- Vega_Withdrawn (59)
  where dblink_fdbcont_zdb_id != 'ZDB-FDBCONT-100114-1' 
    and exists (
@@ -16,5 +19,11 @@ update db_link set dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-100114-1' -- Vega_Withdr
 )
 ;
 
-
+! echo "Are any ottdarT pointing at the withdrawn blastdb, but not flagged as withdrawn?"
+select count(*) bezero
+ from db_link join transcript on  dblink_acc_num == tscript_load_id
+ where dblink_fdbcont_zdb_id == 'ZDB-FDBCONT-100114-1' 
+   and tscript_status_id != 1
+;   
+ 
 --rollback work;
