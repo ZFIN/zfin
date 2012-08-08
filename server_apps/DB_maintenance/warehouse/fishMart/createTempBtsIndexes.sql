0b0c3de7cb6206208933d135bd40d07e9c89fbaa
Informix User <informix@zfin.org>
2012-08-08 17:41:16 +0000

    fix bts max clause count post B/L
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27214 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/fishMart/createTempBtsIndexes.sql
+++ server_apps/DB_maintenance/warehouse/fishMart/createTempBtsIndexes.sql
@@ -1,7 +1,7 @@
 create index fish_annotation_search_temp_fas_all_bts_index
   on fish_annotation_search_temp (fas_pheno_term_group bts_lvarchar_ops,
 			     fas_all bts_lvarchar_ops,
-			     fas_affector_type_group bts_lvarchar_ops) USING BTS(query_default_field="*", analyzer="whitespace",max_clause_count="10000") IN smartbs_bts;
+			     fas_affector_type_group bts_lvarchar_ops) USING BTS(query_default_field="*", analyzer="whitespace",max_clause_count="50000") IN smartbs_bts;
 
 
 create index figure_term_fish_search_term_group_temp_bts_index
