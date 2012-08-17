82a8d97236f2cd6cdc0f618a8512593e750e2775
Tom Conlin <tomc@zfin.org>
2012-08-17 21:40:43 +0000

    bugzid: 8770
    reverting changes I thought would continiue for RefSeq but the whole thing should go away and it may as well go away 
    intact.
    
    
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27291 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/RefSeq/refseq.pl
+++ server_apps/data_transfer/RefSeq/refseq.pl
@@ -1,5 +1,5 @@
 #!/private/bin/perl 
-#  Script to create RefSeq/Entrez links in the database
+#  Script to create RefSeq links in the database
 #  This script assumes directory GenPept exists in the 
 #  same folder, and file fetch-genpept.r exist in ../GenPept/.
 
@@ -81,7 +81,6 @@ while( !(
 	  -e "loc2acc.unl" &&
 	  -e "loc2ref.unl" &&
 	  -e "loc2acclen.unl" &&
-	  -e "loc2UG.unl" &&
 	  -e "../GenPept/prot_len_acc.unl"
 	 )
      )
@@ -104,7 +103,7 @@ while( !(
 }
 
 #load RefSeq links
-$sys_status = system("$ENV{'INFORMIXDIR'}/bin/dbaccess <!--|DB_NAME|--> load_refSeq.sql");
+$sys_status = system("$ENV{'INFORMIXDIR'}/bin/dbaccess -a <!--|DB_NAME|--> load_refSeq.sql");
 if ($sys_status > 0)
 {
     &emailError("Failure in load_refseq.pl", "<!--|VALIDATION_EMAIL_AD|-->, <!--|DB_OWNER|-->\@cs.uoregon.edu");
@@ -112,11 +111,12 @@ if ($sys_status > 0)
 
 
 &dblinksReport();
-
+&reportOmimDups();
 
 &reportFile('gene_with_multiple_linked_recid.unl','RefSeq Multiples', 'tech');
 &reportFile('conflict_dblink.unl','Marker/DbLink Conflicts', 'tech');
-
+&reportFile('hs_ortho_abbrev_conflict.unl','Human Ortho Conflicts','bio');
+&reportFile('mm_ortho_abbrev_conflict.unl','Mouse Ortho Conflicts','bio');
 
 &sendReport('tech');
 &sendReport('bio','<!--|VALIDATION_EMAIL_GENE|-->');
@@ -141,10 +141,11 @@ sub downloadRefSeqFiles()
   {
     system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/gene/DATA/gene2refseq.gz -O loc2ref.gz");
     system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/gene/DATA/gene2accession.gz -O loc2acc.gz");
-    system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/gene/DATA/gene2unigene -O loc2UG");
+    system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz");
+    
     system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.rna.gbff.gz -O zebrafish.gbff.gz");
     system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.protein.gpff.gz -O zebrafish.gnp.gz");
-    system("/local/bin/wget -q ftp://ftp.ncbi.nih.gov/gene/DATA/gene_info.gz");
+   
   }
 
 sub writeReport()
@@ -161,6 +162,7 @@ sub openReport()
   {
     system("/bin/rm -f report*");
     system("touch report");
+    system("touch report_ortho");
     dblinksReport();
   }
 
@@ -175,7 +177,7 @@ sub dblinksReport()
                              from db_link, foreign_db_contains, foreign_db
                              where dblink_fdbcont_zdb_id = fdbcont_zdb_id
                               and fdbcont_fdb_db_id = fdb_db_pk_id
-                               and fdb_db_name in ("RefSeq","Gene","UniGene","Genbank","GenPept")
+                              and fdb_db_name in ("RefSeq","Gene","Genbank","GenPept")
                              group by fdb_db_name;'
 			   );
     $cur->execute;
@@ -184,6 +186,35 @@ sub dblinksReport()
     while ($cur->fetch)
     {
       print REPORT "$db_name\t$db_count\n";
+    }
+    print REPORT "\n";
+    close(REPORT);
+  }
+
+
+sub reportOmimDups()
+  {
+    open (REPORT, ">>report") or die "can not open report";
+
+    print REPORT "OMIM dups\n";
+
+    my $cur = $dbh->prepare('select mrkr_abbrev
+                               from db_link, orthologue, marker, 
+                                       foreign_db_contains, foreign_db
+                              where dblink_fdbcont_zdb_id = fdbcont_zdb_id
+                                and fdb_db_name = "OMIM"
+                                and fdbcont_fdb_db_id = fdb_db_pk_id
+                                and dblink_linked_recid = zdb_id
+                                and c_gene_id = mrkr_zdb_id
+                              group by mrkr_abbrev
+                             having count(*) > 1;'
+			   );
+    $cur->execute;
+    my($mrkr_abbrev);
+    $cur->bind_columns(\$mrkr_abbrev);
+    while ($cur->fetch)
+    {
+      print REPORT "$mrkr_abbrev\n";
     }
     print REPORT "\n";
     close(REPORT);
@@ -236,7 +267,7 @@ sub sendReport()
       print MAIL "To: <!--|DB_OWNER|-->\@cs.uoregon.edu, bsprunge\@cs.uoregon.edu\n";
     }
     
-    print MAIL "Subject: refseq report\n";
+    print MAIL "Subject: refseq/ortho report\n";
     while($line = <REPORT>)
     {
       print MAIL $line;
@@ -247,6 +278,8 @@ sub sendReport()
 
 sub getReportName ()
   {
-    if ($_[0] eq "tech") { return "report";}
+    if ($_[0] eq "tech") {  return "report";}
+    elsif ($_[0] eq "bio") { return "report_ortho";}
+    
     return "report";      
   }
