2d75b168609b94b63c965b2d07f29bab03c8d23f
Informix User <informix@zfin.org>
2012-08-20 16:23:43 +0000

    tuning fishmart xpat query
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27298 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/fishMart/addXpatCounts.sql
+++ server_apps/DB_maintenance/warehouse/fishMart/addXpatCounts.sql
@@ -1,7 +1,13 @@
 !echo "xpatfig in fish annotation search";
 
+update fish_annotation_Search_temp
+ set fas_xpat_figure_count = 0;
+
 update fish_annotation_search_temp
   set fas_xpat_figure_count = (Select count(distinct xfiggm_member_id)
       			       	       from xpat_figure_group, xpat_Figure_group_member
 				       where xfiggm_group_id = xfigg_group_pk_id
-				       and xfigg_geno_handle = fas_geno_handle);
+				       and xfigg_geno_handle = fas_geno_handle)
+    where exists (select 'x' from xpat_figure_group
+    	  	 	 where xfigg_geno_handle = fas_geno_handle);
+
