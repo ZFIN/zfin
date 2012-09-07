e5e2b4cc95e776352594acbfb5999586027a1ce0
Xiang Shao <xshao@zfin.org>
2012-09-07 23:35:15 +0000

    bugzid:8770 replace RefSeq/refseq.pl with EntrezGene/entrezGene.pl and remove the already commented out "Swiss-Prot Pull"
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27381 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/pull.sh
+++ server_apps/data_transfer/pull.sh
@@ -4,16 +4,18 @@
 #
 echo "#########################################################################"
 echo "RefSeq (Entrez) Pull:" 
-echo "Log File: <!--|ROOT_PATH|-->/server_apps/data_transfer/RefSeq/refseq.log"
-<!--|ROOT_PATH|-->/server_apps/data_transfer/RefSeq/refseq.pl> <!--|ROOT_PATH|-->/server_apps/data_transfer/RefSeq/refseq.log 2>&1
-echo "#########################################################################"
-#echo "Swiss-Prot Pull:"# -- expected ~ 2004-Jun
-#<!--|ROOT_PATH|-->/server_apps/data_transfer/ ... SP
-#echo "#########################################################################"
-echo "Get GenBank daily update :" 
-<!--|ROOT_PATH|-->/server_apps/data_transfer/Genbank/gbaccession.pl
+echo "Log File 1: <!--|SOURCEROOT|-->/server_apps/data_transfer/EntrezGene/log1"
+echo "Log File 2: <!--|SOURCEROOT|-->/server_apps/data_transfer/EntrezGene/log2"
+<!--|ROOT_PATH|-->/server_apps/data_transfer/EntrezGene/entrezGene.pl
 echo "#########################################################################"
 
-echo "Unload production: "
-<!--|ROOT_PATH|-->/server_apps/DB_maintenance/unload_production.sh
+##echo "Get GenBank daily update :" 
+##<!--|ROOT_PATH|-->/server_apps/data_transfer/Genbank/gbaccession.pl
 echo "#########################################################################"
+
+##echo "Unload production: "
+##<!--|ROOT_PATH|-->/server_apps/DB_maintenance/unload_production.sh
+echo "#########################################################################"
+
+
+echo "#######    Done    ###########"
