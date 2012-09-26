a40b47bb529ec37fc5fce9be7d37a6c3bbc6be80
Informix User <informix@zfin.org>
2012-09-26 17:57:32 +0000

    bugzid: 8800 add daily email for queries that cost more than 10K to informix
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27472 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/DB_maintenance/queryMonitoring/dailyQueryCostCheck.sh
@@ -0,0 +1,23 @@
+#!/bin/tcsh
+
+# rm old reports
+
+setenv INSTANCE <!--|INSTANCE|-->;
+
+if ( -e /tmp/queryCostCheck.txt ) then
+ /bin/rm /tmp/queryCostCheck.txt;
+ /bin/touch /tmp/queryCostCheck.txt;
+
+endif
+
+echo "unloading daily RED alerts from sysadmin";
+
+echo 'unload to /tmp/queryCostCheck.txt select * from mon_long_queries;' | /private/apps/Informix/informix/bin/dbaccess sysadmin ;
+
+echo "sending RED alert (query cost exceeds 10000) email." ;
+
+/local/bin/mutt -a /tmp/queryCostCheck.txt -s "query cost check for waldo" -- <!--|DB_OWNER|-->@cs.uoregon.edu < <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/char ; 
+
+echo "delete from mon_long_queries" | /private/apps/Informix/informix/bin/dbaccess sysadmin;
+
+exit
