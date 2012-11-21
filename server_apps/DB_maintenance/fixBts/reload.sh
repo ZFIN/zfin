c60d70a42ef9f2d516c1364746a1bb918cce6bdd
Informix User <informix@zfin.org>
2012-11-21 17:10:40 +0000

    fixed bad path on reload.sh
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27732 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/fixBts/reload.sh
+++ server_apps/DB_maintenance/fixBts/reload.sh
@@ -1,13 +1,13 @@
-#! /bin/tcsh 
+#!/bin/tcsh 
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/dropBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/dropBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/dropload.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/dropload.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/createBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/createBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/createTempBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/createTempBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fix_bts.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/fix_bts.sql
 
 exit 0;
