4b193f0217ca6e5dce6ad136830424e2ae4a7f9b
Christian Pich <cmpich@zfin.org>
2012-12-13 19:28:16 +0000

    bugzID: 8825 add equals() and hashCode() methods
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27859 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/profile/OrganizationFeaturePrefix.java
+++ source/org/zfin/profile/OrganizationFeaturePrefix.java
@@ -45,4 +45,28 @@ public class OrganizationFeaturePrefix implements Serializable{
         sb.append('}');
         return sb.toString();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrganizationFeaturePrefix that = (OrganizationFeaturePrefix) o;
+
+        if (featurePrefix != null ? !featurePrefix.equals(that.featurePrefix) : that.featurePrefix != null)
+            return false;
+        if (isCurrentDesignation != null ? !isCurrentDesignation.equals(that.isCurrentDesignation) : that.isCurrentDesignation != null)
+            return false;
+        if (organization != null ? !organization.equals(that.organization) : that.organization != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = isCurrentDesignation != null ? isCurrentDesignation.hashCode() : 0;
+        result = 31 * result + (featurePrefix != null ? featurePrefix.hashCode() : 0);
+        result = 31 * result + (organization != null ? organization.hashCode() : 0);
+        return result;
+    }
 }
