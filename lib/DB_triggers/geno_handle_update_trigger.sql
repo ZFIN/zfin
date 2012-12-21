92e0f5f94de9f1ada7ab870f211150984c6b3fc0
Sierra Moxon <smoxon@zfin.org>
2012-12-21 21:42:15 +0000

    bugzid: 9200 add triggers missing from db
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@27916 908b024a-c82b-635c-c293-f2b15b23277c

--- lib/DB_triggers/geno_handle_update_trigger.sql
+++ lib/DB_triggers/geno_handle_update_trigger.sql
@@ -5,5 +5,7 @@ create trigger geno_handle_update_trigger update of geno_handle
         execute function scrub_char(new_genotype.geno_handle ) 
     into genotype.geno_handle,
 	execute procedure p_update_geno_nickname(new_genotype.geno_zdb_id,
-		new_genotype.geno_handle)
+		new_genotype.geno_handle),
+        execute function update_geno_sort_order(new_genotype.geno_zdb_id)
+	into genotype.geno_complexity_order
 );
--- lib/DB_triggers/genotype_insert_trigger.sql
+++ lib/DB_triggers/genotype_insert_trigger.sql
@@ -7,6 +7,8 @@ create trigger genotype_insert_trigger insert on genotype
         execute function scrub_char(new_genotype.geno_handle ) 
     into genotype.geno_handle,
         execute function zero_pad(new_genotype.geno_display_name ) 
-    into genotype.geno_name_order
+    into genotype.geno_name_order,
+    	execute function update_geno_sort_order(new_genotype.geno_zdb_id)
+    into genotype.geno_complexity_order
 
 );
