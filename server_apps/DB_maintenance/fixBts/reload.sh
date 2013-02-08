423c30f591d187b15613cc0c2683f632142b6c04
Sierra Moxon <smoxon@zfin.org>
2013-02-08 22:53:02 +0000

    bugzid: none. take out hardcoded paths from fixBts
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28115 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/fixBts/reload.sh
+++ server_apps/DB_maintenance/fixBts/reload.sh
@@ -1,13 +1,13 @@
 #!/bin/tcsh 
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/dropBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME dropBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/dropload.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME dropload.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/createBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME createBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/createTempBtsIndexes.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME createTempBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/$INSTANCE/server_apps/DB_maintenance/fixBts/fix_bts.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME fix_bts.sql
 
 exit 0;
