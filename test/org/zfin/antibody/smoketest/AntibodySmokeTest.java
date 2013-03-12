60b7ec04151dc5bb039de3278396620b26442c00
Christian Pich <cmpich@zfin.org>
2013-03-12 19:06:19 +0000

    fix testSearchAb_2() smoke test: antibodies are capitalized
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28401 908b024a-c82b-635c-c293-f2b15b23277c

--- test/org/zfin/antibody/smoketest/AntibodySmokeTest.java
+++ test/org/zfin/antibody/smoketest/AntibodySmokeTest.java
@@ -79,7 +79,7 @@ public class AntibodySmokeTest extends AbstractSmokeTest {
             try {
                 String uri = "/action/antibody/antibody-do-search?antibodyCriteria.antibodyNameFilterType=contains&antibodyCriteria.name=ab-2&maxDisplayRecords=25";
                 HtmlPage page = webClient.getPage(nonSecureUrlDomain + uri);
-                List<?> relatedTerms = page.getByXPath("//a[@id='ab-2F11']");
+                List<?> relatedTerms = page.getByXPath("//a[@id='Ab-2F11']");
                 assertEquals(1, relatedTerms.size());
                 HtmlAnchor link = (HtmlAnchor) relatedTerms.get(0);
                 assertEquals("Upper case indicates usage of the antibody name", "Ab-2F11", link.getTextContent());
@@ -257,13 +257,13 @@ public class AntibodySmokeTest extends AbstractSmokeTest {
                 assertTrue(antibodyListPage.getTitleText().contains("Chen"));
 
                 // AB-F59 is one of a few antibodies that are used in this publication.
-                HtmlAnchor antibodyLink = (HtmlAnchor) antibodyListPage.getElementById("ab-f59");
+                HtmlAnchor antibodyLink = (HtmlAnchor) antibodyListPage.getElementById("Ab-F59");
                 assertNotNull(antibodyLink);
 
                 // check that the link to the antibody view page is working as well.
                 HtmlPage antibodyPage = antibodyLink.click();
                 assertNotNull(antibodyPage);
-                assertTrue(antibodyPage.getTitleText().contains("ZFIN Antibody: ab-f59"));
+                assertTrue(antibodyPage.getTitleText().contains("ZFIN Antibody: Ab-F59"));
 
             } catch (IOException e) {
                 fail(e.toString());
