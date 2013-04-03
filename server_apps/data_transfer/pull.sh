e738d6bb8c7252c6981d37f9c5cedf105dc4d544
Xiang Shao <xshao@zfin.org>
2013-04-03 23:29:53 +0000

    bugzid:9425 removed entrezGene.pl from this shell script and put it in cron
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28550 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/pull.sh
+++ server_apps/data_transfer/pull.sh
@@ -2,11 +2,7 @@
 #  name: pull.sh
 #  to: call various data Pull scripts in a particular sequence
 #
-echo "#########################################################################"
-echo "RefSeq (Entrez) Pull:" 
-echo "Log File 1: <!--|SOURCEROOT|-->/server_apps/data_transfer/EntrezGene/log1"
-echo "Log File 2: <!--|SOURCEROOT|-->/server_apps/data_transfer/EntrezGene/log2"
-<!--|ROOT_PATH|-->/server_apps/data_transfer/EntrezGene/entrezGene.pl
+
 echo "#########################################################################"
 
 echo "Get GenBank daily update :" 
