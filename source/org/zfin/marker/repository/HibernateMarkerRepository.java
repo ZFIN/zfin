1fa38177178564e647992173ccaf15cd9cae153c
Prita Mani <pm@zfin.org>
2013-04-24 17:05:48 +0000

    BugzID:9538 Added class Markerfigure.java to accomodate figuers for constructs Changed method getDBLinsk fast to use fdb_display_name rather than fdb_db_name
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28656 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/marker/repository/HibernateMarkerRepository.java
+++ source/org/zfin/marker/repository/HibernateMarkerRepository.java
@@ -1797,7 +1797,7 @@ public class HibernateMarkerRepository implements MarkerRepository {
     }
 
     public List<LinkDisplay> getMarkerDBLinksFast(Marker marker, DisplayGroup.GroupName groupName) {
-        String sql = "select dbl.dblink_linked_recid,dbl.dblink_acc_num,fdb.fdb_db_name,fdb.fdb_db_query,fdb.fdb_url_suffix, " +
+        String sql = "select dbl.dblink_linked_recid,dbl.dblink_acc_num,fdb.fdb_db_display_name,fdb.fdb_db_query,fdb.fdb_url_suffix, " +
                 "ra.recattrib_source_zdb_id, fdb.fdb_db_significance, dbl.dblink_zdb_id " +
                 "from db_link dbl  " +
                 "join foreign_db_contains_display_group_member m on m.fdbcdgm_fdbcont_zdb_id=dbl.dblink_fdbcont_zdb_id " +
