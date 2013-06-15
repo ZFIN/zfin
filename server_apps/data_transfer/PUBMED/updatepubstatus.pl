c2755cd68fc07ac62766ee3be971daf579929b55
Xiang Shao <xshao@zfin.org>
2013-06-15 02:31:16 +0000

    bugzid:9611 refatored to add email and to make it work with cron etc.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28928 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/PUBMED/updatepubstatus.pl
+++ server_apps/data_transfer/PUBMED/updatepubstatus.pl
@@ -2,7 +2,8 @@
 
 
 use DBI;
-$mailprog = '/usr/lib/sendmail -t -oi -oem';
+use lib "<!--|ROOT_PATH|-->/server_apps/";
+use ZFINPerlModules;
 
 #set environment variables
 $ENV{"INFORMIXDIR"}="<!--|INFORMIX_DIR|-->";
@@ -10,82 +11,76 @@ $ENV{"INFORMIXSERVER"}="<!--|INFORMIX_SERVER|-->";
 $ENV{"ONCONFIG"}="<!--|ONCONFIG_FILE|-->";
 $ENV{"INFORMIXSQLHOSTS"}="<!--|INFORMIX_DIR|-->/etc/<!--|SQLHOSTS_FILE|-->";
 
+#remove old files
+system("/bin/rm -f updated");
+system("/bin/rm -f <!--|ROOT_PATH|-->/server_apps/data_transfer/PUBMED/xml*");
+
 ### open a handle on the db
 my $dbh = DBI->connect('DBI:Informix:<!--|DB_NAME|-->',
                        '', 
                        '', 
 		       {AutoCommit => 1,RaiseError => 1}
 		      )
-  || die("Failed while connecting to <!--|DB_NAME|--> "); 
+  || die("Failed while connecting to <!--|DB_NAME|--> ");
 
-#make the PMED dir
-system("mkdir -p PMED");
+$cur = $dbh->prepare('select accession_no, zdb_id from publication where status != "active" and accession_no is not null;');
+$cur->execute();
+my ($pub_acc_no, $pub_zdbId);
+$cur->bind_columns(\$pub_acc_no,\$pub_zdbId);
+%nonActivePubAccessions = ();
+$ctInactivePubs = 0;
+while ($cur->fetch()) {
+  if ($pub_acc_no =~ /^\d+$/) {
+    $nonActivePubAccessions{$pub_acc_no} = $pub_zdbId;
+    $ctInactivePubs++;
+  }
+}
 
-#remove old files
-system("rm -f pubnotactive.unl");
-system("rm -f ppublish.unl");
-system("rm -f ./PMED/*");
+$cur->finish();
 
-&inactive_pubs();
+print "\nctInactivePubs = $ctInactivePubs\n\n";
+
+## do nothing if no inactive pub
+exit if $ctInactivePubs == 0;
 
 my $cur_update_pub = $dbh->prepare_cached('update publication set status = "active" where accession_no = ?;');
 
 my $cur_insert_update = $dbh->prepare_cached('insert into updates (rec_id,field_name,new_value,when) select zdb_id,"status","active",current from publication where accession_no = ?;');
 
-    open (PPUB, ">>ppublish.unl");
-    open (PUBS, "pubnotactive.unl");
-    while ($line = <PUBS>)
-    {
-      chop($line);
-      if ($line =~ /^\d+$/)
-      {
-        system("/local/bin/wget -q 'http://www.ncbi.nlm.nih.gov/pubmed?term=$line&report=xml&format=text' -O ./PMED/$line");
-      
-        open(PMED, "./PMED/$line");
-        while($pstat = <PMED>)
-        {
-          chop($pstat);
-          if ($pstat =~ /PublicationStatus.*ppublish/) 
-          {
-            $cur_update_pub->execute($line);
-            $cur_insert_update->execute($line);
-            print PPUB "$line\n";
-          }
-        }
-        close(PMED);
-      }
-    }
-    close(PUBS);
-    close(PPUB);
+open (PPUB, ">updated");
+$ctUpdated = 0;
+foreach $pubmed (keys %nonActivePubAccessions) {
+    system("/local/bin/wget -q 'http://www.ncbi.nlm.nih.gov/pubmed?term=$pubmed&report=xml&format=text' -O xml$pubmed");
 
+    open(PMED, "xml$pubmed") || die("Could not open xml$pubmed !");
+    @lines=<PMED>;
+    close(PMED);
+
+    foreach $line (@lines) {
+       $line =~ s/>\n+//g;
+       if ($line =~ m/PublicationStatus/ && $line =~ m/ppublish/) {      # PublicationStatus
+            $cur_update_pub->execute($pubmed);
+            $cur_insert_update->execute($pubmed);
+            $ctUpdated++;
+            print PPUB "$nonActivePubAccessions{$pubmed}\t$pubmed\n";
+       }
+    }
+}
+close(PPUB);
+
+$cur_update_pub->finish();
+$cur_insert_update->finish();
 
 $dbh->disconnect();
 
+print "\nctUpdated = $ctUpdated\n\n";
+
+$dbname = "<!--|DB_NAME|-->";
+
+$subject = "Auto from $dbname: " . "$ctUpdated of $ctInactivePubs inactive pubs have been updated to active according to PUBMED";
+ZFINPerlModules->sendMailWithAttachedReport("xshao\@zfin.org","$subject","updated");
+
 exit;
 
-
-
-sub inactive_pubs()
-  {
-    open (REPORT, ">>pubnotactive.unl") or die "can not open report";
-
-    my $cur = $dbh->prepare('select accession_no
-                             from publication
-                             where status != "active"
-                              and accession_no is not null;'
-			   );
-    $cur->execute;
-    my($pub_acc_no);
-    $cur->bind_columns(\$pub_acc_no);
-    while ($cur->fetch)
-    {
-      if ($pub_acc_no =~ /^\d+$/)
-      {
-        print REPORT "$pub_acc_no\n";
-      }
-    }
-    print REPORT "\n";
-    close(REPORT);
-  }
 
 
