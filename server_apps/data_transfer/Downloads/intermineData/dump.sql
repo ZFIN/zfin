3c848ff501d03b83f17f798bef396a82d6b06b03
Informix User <informix@zfin.org>
2013-06-20 23:09:43 +0000

    add snp_download to the mine:sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28963 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Downloads/intermineData/dump.sql
+++ server_apps/data_transfer/Downloads/intermineData/dump.sql
@@ -245,7 +245,7 @@ select dblink_zdb_id, fdb_db_name, dblink_acc_num, dblink_linked_recid, fdbdt_da
   and fdbcont_fdb_db_id = fdb_db_pk_id
   and fdb_db_name not in ('miRBASE Mature','miRBASE Stem Loop','unreleaseRNA','ZFIN','ZFIN_PROT','Curated miRNA Mature','Curated miRNA Stem Loop')
 union all
- select "", "dbSNP",snpd_rs_acc_num, snpd_mrkr_zdb_id, "genomic", "", ""
+ select snpd_mrkr_zdb_id||snpd_rs_acc_num, "dbSNP",snpd_rs_acc_num, snpd_mrkr_zdb_id, "genomic", "", ""
    from snp_download;
 
 unload to "<!--|ROOT_PATH|-->/server_apps/data_transfer/Downloads/intermineData/zfin_kegg/1keggMapping.txt"
