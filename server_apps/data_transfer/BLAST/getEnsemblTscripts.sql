adeecdfe99d64c72a1240a7479fb6aa54c2d5bd4
Informix User <informix@zfin.org>
2013-06-28 04:10:20 +0000

    add dump of ensembl tscripts for zfin blast db: sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@28997 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/data_transfer/BLAST/getEnsemblTscripts.sql
@@ -0,0 +1,8 @@
+begin work;
+
+unload to <!--|ROOT_PATH|-->/server_apps/data_transfer/BLAST/ensemblZFTscripts.txt
+select ensm_ensdarg_id, ensm_ensdart_id
+ from ensdar_mapping
+where exists (Select 'x' from db_link where dblink_acc_num like 'ENSDARG%');
+
+commit work ;
