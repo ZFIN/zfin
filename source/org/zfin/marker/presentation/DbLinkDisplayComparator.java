1911fbf1e6a6a649e8b7bf750192f35aebfab3f6
Christian Pich <cmpich@zfin.org>
2013-07-15 23:11:07 +0000

    bugzID: 9952 fix DbLink comparator to be transitive, i.e. handle null length sequence
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29063 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/marker/presentation/DbLinkDisplayComparator.java
+++ source/org/zfin/marker/presentation/DbLinkDisplayComparator.java
@@ -4,36 +4,51 @@ import org.zfin.sequence.DBLink;
 
 import java.util.Comparator;
 
-/**
- */
-public class DbLinkDisplayComparator implements Comparator<DBLink>{
+public class DbLinkDisplayComparator implements Comparator<DBLink> {
 
+    /**
+     * Sort by:
+     * 1. display of of foreign_db_data_type
+     * 2. foreign_db significance
+     * 3. length of db_link
+     * 4. name of db_link accession number
+     *
+     * @param dbLink1 dbLink
+     * @param dbLink2 dbLink
+     * @return integer
+     */
     @Override
     public int compare(DBLink dbLink1, DBLink dbLink2) {
-        int comparator ;
+
+        if (dbLink1 == null && dbLink2 == null)
+            return 0;
+        if (dbLink1 == null)
+            return 1;
+        if (dbLink2 == null)
+            return -1;
+
+        int comparator;
         comparator = dbLink1.getReferenceDatabase().getForeignDBDataType().getDisplayOrder()
                 - dbLink2.getReferenceDatabase().getForeignDBDataType().getDisplayOrder();
-        if (comparator != 0)  return comparator ;
+        if (comparator != 0)
+            return comparator;
 
         comparator = dbLink1.getReferenceDatabase().getForeignDB().getSignificance()
                 - dbLink2.getReferenceDatabase().getForeignDB().getSignificance();
-        if (comparator != 0)  return comparator ;
-//
-        if(dbLink2.getLength()!=null && dbLink1.getLength()!=null){
-            comparator = dbLink2.getLength()
-                    - dbLink1.getLength();
-        }
-        if(dbLink1==null && dbLink2 !=null){
-            return 1 ;
-        }
-        if(dbLink1!=null && dbLink2 ==null){
-            return -1 ;
-        }
-        if (comparator != 0)  return comparator ;
+        if (comparator != 0)
+            return comparator;
 
-        // compare query, really?
-        // not doing this,
+        if (dbLink2.getLength() == null && dbLink1.getLength() != null)
+            return -1;
+        if (dbLink1.getLength() == null && dbLink2.getLength() != null)
+            return 1;
 
-        return  dbLink2.getAccessionNumberDisplay().compareTo(dbLink1.getAccessionNumberDisplay());
+        if (dbLink1.getLength() != null && dbLink2.getLength() != null) {
+            if (dbLink2.getLength() > dbLink1.getLength())
+                return 1;
+            if (dbLink1.getLength() > dbLink2.getLength())
+                return -1;
+        }
+        return dbLink2.getAccessionNumberDisplay().compareTo(dbLink1.getAccessionNumberDisplay());
     }
 }
