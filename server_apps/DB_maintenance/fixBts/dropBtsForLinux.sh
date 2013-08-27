f16d0422ff61f24a4dbca4e36d5b77ac1aeda7db
Sierra Moxon <smoxon@zfin.org>
2013-08-27 22:29:11 +0000

    try to unregister:seirra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29242 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/fixBts/dropBtsForLinux.sh
+++ server_apps/DB_maintenance/fixBts/dropBtsForLinux.sh
@@ -2,3 +2,6 @@
 
 $INFORMIXDIR/bin/dbaccess $DBNAME /research/zcentral/www_homes/trunk/server_apps/DB_maintenance/fixBts/dropBtsIndexes.sql
 
+EXECUTE FUNCTION SYSBldPrepare('ifxmngr', 'sysblderrorlog'); 
+EXECUTE FUNCTION SYSBldPrepare('bts.3.00', 'sysblderrorlog'); 
+EXECUTE FUNCTION SYSBldUnRegister('bts.3.00','sysblderrorlog');
