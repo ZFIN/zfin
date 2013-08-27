3f9294a21d2e4ef168b681104fa2d591ccf75600
Christian Pich <cmpich@zfin.org>
2013-08-27 16:14:06 +0000

    bugzID: 9762 Remove old Anatomy load directory
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29236 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Anatomy/unloadAOFile.sql
+++
@@ -1,5 +0,0 @@
-begin work ;
-
-select lotofile(obofile_text, '<!--|ROOT_PATH|-->/j2ee/phenote/deploy/WEB-INF/data_transfer/zebrafish_anatomy.obo!','server') from obo_file where obofile_name = 'zebrafish_anatomy.obo';
-
-commit work ;
