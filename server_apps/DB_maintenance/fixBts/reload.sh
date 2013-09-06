94ab2e7376077e3089d542d2dd9ea2623b502141
Sierra Moxon <smoxon@zfin.org>
2013-09-06 17:03:59 +0000

    fix bts
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29261 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/fixBts/reload.sh
+++ server_apps/DB_maintenance/fixBts/reload.sh
@@ -6,8 +6,6 @@ $INFORMIXDIR/bin/dbaccess $DBNAME dropload.sql
 
 $INFORMIXDIR/bin/dbaccess $DBNAME createBtsIndexes.sql
 
-$INFORMIXDIR/bin/dbaccess $DBNAME createTempBtsIndexes.sql
-
 $INFORMIXDIR/bin/dbaccess $DBNAME fix_bts.sql
 
 exit 0;
