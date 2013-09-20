bd6bcf7de8335f1ed6cd9f38dde7d659af1a6067
Sierra Moxon <smoxon@zfin.org>
2013-09-20 19:55:50 +0000

    bugzid: 9971 remove unhelpful reports:sierra
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29368 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/Downloads/FBcase8787.pl
+++ server_apps/data_transfer/Downloads/FBcase8787.pl
@@ -23,44 +23,6 @@ $dbname =~ s/\///;
 
 system("$ENV{'INFORMIXDIR'}/bin/dbaccess -a <!--|DB_NAME|--> runNumbersFBcase8787.sql >FB8787log1 2> FB8787log2");
 
-$SUBJECT="Auto: FB8787log1 from ".$dbname;
-$MAILTO="informix\@cs.uoregon.edu";
-$TXTFILE="./FB8787log1";
-
-# Create a new multipart message:
-$msg1 = new MIME::Lite
-From    => "$ENV{LOGNAME}",
-To      => "$MAILTO",
-Subject => "$SUBJECT",
-Type    => 'multipart/mixed';
-
-attach $msg1
-Type     => 'text/plain',
-Path     => "$TXTFILE";
-
-# Output the message to sendmail
-
-open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
-$msg1->print(\*SENDMAIL);
-
-$SUBJECT="Auto: FB8787log2 from ".$dbname;
-$TXTFILE="./FB8787log2";
-
-# Create another multipart message:
-$msg2 = new MIME::Lite
-From    => "$ENV{LOGNAME}",
-To      => "$MAILTO",
-Subject => "$SUBJECT",
-Type    => 'multipart/mixed';
-
-attach $msg2
-Type     => 'text/plain',
-Path     => "$TXTFILE";
-
-# Output the message to sendmail
-
-open (SENDMAIL, "| /usr/lib/sendmail -t -oi");
-$msg2->print(\*SENDMAIL);
 
 open (XPAT8787PIPIE, "<!--|ROOT_PATH|-->/server_apps/data_transfer/Downloads/downloadsStaging/xpatGenesPipe.txt") || die "Cannot open <!--|ROOT_PATH|-->/server_apps/data_transfer/Downloads/xpatGenesPipe.txt : $!\n";
 @lines=<XPAT8787PIPIE>;
