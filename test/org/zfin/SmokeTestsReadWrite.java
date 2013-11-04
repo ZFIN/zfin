7139edd16be76dff750dd2401c5b31ddaa13000a
Prita Mani <pm@zfin.org>
2013-11-04 18:02:58 +0000

    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@29656 908b024a-c82b-635c-c293-f2b15b23277c

--- test/org/zfin/SmokeTestsReadWrite.java
+++ test/org/zfin/SmokeTestsReadWrite.java
@@ -4,7 +4,7 @@ import junit.framework.JUnit4TestAdapter;
 import org.jenkinsci.testinprogress.runner.ProgressSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
-import org.zfin.gwt.MorpholinoAddSmokeTest;
+//import org.zfin.gwt.MorpholinoAddSmokeTest;
 import org.zfin.gwt.SimpleSmokeTest;
 import org.zfin.gwt.curation.PileConstructionSmokeTest;
 import org.zfin.gwt.lookup.LookupSmokeTest;
