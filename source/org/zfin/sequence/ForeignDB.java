0c3b5b5ff4e586403296220fe9b32790dacd1f12
Sierra Moxon <smoxon@zfin.org>
2014-01-13 22:51:00 +0000

    bugzid: 10409
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@30598 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/sequence/ForeignDB.java
+++ source/org/zfin/sequence/ForeignDB.java
@@ -70,6 +70,7 @@ public class ForeignDB implements Comparable<ForeignDB> {
 
 
     public static enum AvailableName {
+	CREEZOO("CreeZoo"),
         CURATED_MIRNA_MATURE("Curated miRNA Mature"),
         CURATED_MIRNA_STEM_LOOP("Curated miRNA Stem Loop"),
         DBSNP("dbSNP"),
