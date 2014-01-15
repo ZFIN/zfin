0fcbe1d4eaafa4eca5aaed33f0ceae2cc5d0a370
Sierra Moxon <smoxon@zfin.org>
2014-01-15 17:50:57 +0000

    bugzid: 10367 added ensembl_zf to the blast list for Vega
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@30628 908b024a-c82b-635c-c293-f2b15b23277c

--- commons/bin/blast_reno.sh
+++ commons/bin/blast_reno.sh
@@ -3,7 +3,7 @@
 # who knows , might still be valid
 source /private/ZfinLinks/Commons/env/blast-default.env
 set nt_fasta=$1;
-set db_path="${WEBHOST_BLAST_DATABASE_PATH}/Current/"
+set db_path="/research/zblastfiles/zmore/blastRegeneration/Current/"
 set wz_est="/research/zblastfiles/files/LOCAL/wz_est"
 
 set blastn = "/private/apps/wublast/blastn"
@@ -17,7 +17,7 @@ echo ""
 
 rm -f $nt_fasta:r.out
 nice +10 $blastn \
-"$db_path/zfin_cdna_seq $db_path/unreleasedRNA $db_path/vega_transcript $db_path/vega_withdrawn $wz_est" \
+"$db_path/zfin_cdna_seq $db_path/unreleasedRNA $db_path/vega_transcript $db_path/vega_withdrawn $db_path/ensembl_zf $wz_est" \
  $nt_fasta -E e-50 -B 100 > $nt_fasta:r.out
 
 echo ""
