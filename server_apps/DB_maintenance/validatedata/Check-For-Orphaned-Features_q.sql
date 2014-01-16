e49beb4f603170a4b4933b9237ee6300c1b2314a
Christian Pich <cmpich@zfin.org>
2014-01-16 22:16:24 +0000

    bugzID: 10118 add amy to feature orphaned report, add person to report
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@30661 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata/Check-For-Orphaned-Features_q.sql
+++ server_apps/DB_maintenance/validatedata/Check-For-Orphaned-Features_q.sql
@@ -1,5 +1,5 @@
 unload to <!--|ROOT_PATH|-->/server_apps/DB_maintenance/reportRecords.txt
-select feature_zdb_id from feature
+select distinct feature_zdb_id, submitter_name, submitter_id from feature, updates
 where not exists (
  select 'x' from record_attribution where
             recattrib_data_zdb_id = feature_zdb_id)
@@ -14,4 +14,6 @@ not exists (
 select 'x'
         from record_attribution ra , data_alias da
         where da.dalias_zdb_id = ra.recattrib_data_zdb_id
-        and feature_zdb_id = da.dalias_data_zdb_id);
+        and feature_zdb_id = da.dalias_data_zdb_id)
+and rec_id = feature_zdb_id
+and new_value = 'removed';
