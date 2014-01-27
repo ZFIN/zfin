fd4cb4b3f2fe777177985ca04165ebc9528ca5d3
Christian Pich <cmpich@zfin.org>
2014-01-27 19:23:41 +0000

    fix Check-Gene-With-Prefix-Has-EST_w but still need to take care of the colon
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@30797 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ server_apps/DB_maintenance/validatedata/Check-Gene-With-Prefix-Has-EST_w.sql
@@ -0,0 +1,11 @@
+select mrkr_zdb_id, mrkr_name, mrkr_abbrev
+               from marker m1
+               where mrkr_type = "GENE"
+                 and mrkr_name[1,2] not in ("id","si")
+                 and 1 <>
+                     ( select count(*)
+                         from marker m2, marker_relationship
+                         where mrel_mrkr_1_zdb_id = m1.mrkr_zdb_id
+                           and mrel_mrkr_2_zdb_id = m2.mrkr_zdb_id
+                           and mrel_type = "gene encodes small segment" )
+               order by mrkr_name
