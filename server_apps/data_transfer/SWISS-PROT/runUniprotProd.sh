b21f42fe5278f229db7801cc9488ad849cd3b0c1
Sierra Moxon <smoxon@zfin.org>
2014-01-27 17:09:58 +0000

    automated uniprot load on prod for jenkins run
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@30788 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/SWISS-PROT/runUniprotProd.sh
+++ server_apps/data_transfer/SWISS-PROT/runUniprotProd.sh
@@ -1,6 +1,19 @@
-#!/bin/tcsh -e
+#!/bin/bash -e
 
-source <!--|SOURCEROOT|-->/commons/env/<!--|INSTANCE|-->.env
+# $1 is the location on dev system where we should copy
+# the *2go and ok* files to do the run on almost and production
+# this script only runs on almost and production (automates the prod steps
+# of the UniProt load.  See case 2799 for details.
+
+if [ -z "$1" ]      
+then
+    echo "ERROR on usage: no file location to copy from dev to prod";
+    exit;
+fi
+
+/bin/cp $1/ok* <!--|TARGETROOT|-->/server_apps/data_transfer/SWISS-PROT/
+/bin/cp $1/*2go <!--|TARGETROOT|-->/server_apps/data_transfer/SWISS-PROT/
+
 cd <!--|SOURCEROOT|-->/server_apps/data_transfer/SWISS-PROT/
 /local/bin/gmake run ;
 
