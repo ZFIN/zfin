4a539d0fbb78eee10851b5f533e52db84a9c1cf4
Informix User <informix@zfin.org>
2014-05-29 18:11:17 +0000

    fixed cron scheduler on New-Constructs_w
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@31759 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/jenkins/start.sh
+++ server_apps/jenkins/start.sh
@@ -1,5 +1,5 @@
 echo SOURCEROOT    $SOURCEROOT
 echo JENKINS_HOME  $JENKINS_HOME
 echo JENKINS_PORT  $JENKINS_PORT
-nohup java -jar $SOURCEROOT/server_apps/jenkins/jenkins.war --httpPort=$JENKINS_PORT --prefix=/jobs > $JENKINS_HOME/logs/jenkins.log 2>&1 & echo $! > $JENKINS_HOME/jenkins.pid
+nohup java java -Djavax.net.ssl.trustStore=bouncer.zfin.org-cert -jar $SOURCEROOT/server_apps/jenkins/jenkins.war --httpPort=$JENKINS_PORT --prefix=/jobs > $JENKINS_HOME/logs/jenkins.log 2>&1 & echo $! > $JENKINS_HOME/jenkins.pid
 echo pid  $JENKINS_HOME/jenkins.pid
