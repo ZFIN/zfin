4212d2aca535bf6d0450e7dd8847312380cf04e5
Christian Pich <cmpich@zfin.org>
2014-05-30 21:47:22 +0000

    bugzID: 11161 remove email from fishMart script and add report files onto Jenkins email
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@31784 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/regenFishMart.sh
+++ server_apps/DB_maintenance/warehouse/regenFishMart.sh
@@ -4,18 +4,18 @@
 
 setenv INSTANCE <!--|INSTANCE|-->;
 
-if ( -e <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/runFishMartReport.txt) then
- /bin/rm <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/runFishMartReport.txt
+if ( -e <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/runFishMartReport.txt) then
+ /bin/rm <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/runFishMartReport.txt
 
 endif
 
-if ( -e <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/regenFishMartReport.txt) then
- /bin/rm <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/regenFishMartReport.txt
+if ( -e <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/regenFishMartReport.txt) then
+ /bin/rm <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/regenFishMartReport.txt
 
 endif
 
-if ( -e <!--|SOURCEROOT|-->/reports/tests/fishMartUnitTests.txt) then
- /bin/rm <!--|SOURCEROOT|-->/reports/tests/fishMartUnitTests.txt
+if ( -e <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartUnitTests.txt) then
+ /bin/rm <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartUnitTests.txt
 endif
 
 
@@ -36,7 +36,7 @@ cd <!--|SOURCEROOT|-->
 
 echo "cd'd to <!--|SOURCEROOT|-->" ;
 
-/private/bin/ant run-fishmart-unittests >&! reports/tests/fishMartUnitTests.txt
+/private/bin/ant run-fishmart-unittests >&! <!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/fishMart/fishMartUnitTests.txt
 
 if ($? != 0) then
    echo "trying to send notification unit tests";  
