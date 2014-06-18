6ebe3f718126d5aea37db31e2671db5f2de04d30
Christian Pich <cmpich@zfin.org>
2014-06-18 22:37:20 +0000

    bugzID: 10564 add clean queries to mart
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@31963 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/chromosomeMart/refreshChromosomeMart.sql
+++ server_apps/DB_maintenance/warehouse/chromosomeMart/refreshChromosomeMart.sql
@@ -55,6 +55,12 @@ where sfcl_location_source in ('other map location','General Load');
 insert into linkage_membership_search
   select * from linkage_membership_search_temp;
 
+update linkage_membership set lnkgm_metric = null where
+lnkgm_metric is not null and lnkgm_distance is null;
+
+update linkage_membership_search set lms_units = null where
+lms_units is not null and lms_distance is null;
+
 update zdb_flag
   set (zflag_is_on,zflag_last_modified) = ("f",current year to second)
  where zflag_name = "regen_chromosomemart" ;
