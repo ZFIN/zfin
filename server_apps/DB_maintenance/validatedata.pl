5c6c5c202c3262fd3d7e2b8162d3efbf92aa5f55
Christian Pich <cmpich@zfin.org>
2014-09-05 18:33:26 +0000

    bugzID: 11740 convert duplicate publications validation job to Jenkins, remove Validation_EMAIL_PUBLICATION property
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@32747 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata.pl
+++ server_apps/DB_maintenance/validatedata.pl
@@ -2125,94 +2125,6 @@ sub zdbObjectIsSourceDataCorrect($) {
   &recordResult($routineName, $nRecords);
 }
 
-#-------------------------------------------------------------
-# pubTitlesAreUnique
-#
-# Well, sort of.  Publication titles actually aren't unique.  We already have
-# many distinct publications that have the same title.  This test is here to
-# detect any NEW duplicate titles and report them.
-# 
-# Parameter
-# $      Email Address for recipients
-# 
-sub pubTitlesAreUnique($) {
-	
-  my $routineName = "pubTitlesAreUnique";
-	
-  # Only need to exclude 1 record per pair.  That means if a duplicate
-  # becomes a triplicate, we will detect it.
-  # Exclude direct data submission pubs as we want many of these to have
-  # the same title.
-
-  my $sql1 = '
-    select title 
-      from publication, journal
-      where zdb_id not in (
-	"ZDB-PUB-021016-117", { "ZDB-PUB-961014-110", }
-	"ZDB-PUB-961014-169", { "ZDB-PUB-961014-170", }
-	"ZDB-PUB-021016-125", { "ZDB-PUB-961014-1217", }
-	"ZDB-PUB-021016-60",  { "ZDB-PUB-991014-9", }
-	"ZDB-PUB-000125-1",   { "ZDB-PUB-990525-2", }
-	"ZDB-PUB-961014-288", { "ZDB-PUB-961014-289", }
-	"ZDB-PUB-010718-37",  { "ZDB-PUB-010912-30", }
-	"ZDB-PUB-981110-12",  { "ZDB-PUB-990218-4", }
-	"ZDB-PUB-021016-112", "ZDB-PUB-961014-758", { "ZDB-PUB-961014-759", }
-	"ZDB-PUB-000125-3",   { "ZDB-PUB-010131-19", }
-	"ZDB-PUB-021015-13",  { "ZDB-PUB-030211-13", }
-	"ZDB-PUB-961014-1233",{ "ZDB-PUB-961014-1234",}
-	"ZDB-PUB-961014-106", { "ZDB-PUB-961014-107",}
-	"ZDB-PUB-010417-9",   { "ZDB-PUB-990414-35",}
-	"ZDB-PUB-010711-2",   { "ZDB-PUB-010814-8", }
-	"ZDB-PUB-000824-10",  { "ZDB-PUB-990824-40",}
-	"ZDB-PUB-010912-1",   { "ZDB-PUB-021017-13",}
-        "ZDB-PUB-980420-9",   { "ZDB-PUB-030425-13",}
-        "ZDB-PUB-010718-13",  { "ZDB-PUB-020913-1", }
-        "ZDB-PUB-990414-54",  { "ZDB-PUB-021017-3", }
-        "ZDB-PUB-010718-27",  { "ZDB-PUB-010821-1", }
-        "ZDB-PUB-021017-74"   {, "ZDB-PUB-041012-5" }
-        ,"ZDB-PUB-010918-3"   {, "ZDB-PUB-040216-6" }
-        , "ZDB-PUB-050127-1" {, "ZDB-PUB-030408-12"}
-        )
-        and jrnl_abbrev <> "ZFIN Direct Data Submission"
-        and jrnl_zdb_id = pub_jrnl_zdb_id
-      group by title 
-      having count(*) > 1 
-     into temp dup_titles with no log;';
-  
-  # I tried running this through execSql, but if this query returned 0 rows
-  # then the 2nd query would get a DBI error.
-  my $sth = $dbh->prepare($sql1) or die "Prepare fails";
-  $sth -> execute();
-
-  my $sql2 = '
-      select p.title, p.accession_no, p.zdb_id, p.authors, p.pub_date, jrnl_abbrev, p.pub_volume, p.pub_pages 
-        from publication p, dup_titles d, journal
-        where p.title = d.title 
-        and jrnl_zdb_id = pub_jrnl_zdb_id
-        order by p.title, p.zdb_id;';
-  
-  my @colDesc =  ("Title            ",
-		  "Accession Number ",
-		  "Pub ZDB ID       ",
-		  "Authors          ",
-		  "Pub Date         ",
-		  "Jrnl_abbrev      ",
-		  "Pub Volume       ",
-		  "Pub Pages        ");
-
-  my $nRecords = execSql ($sql2, undef, @colDesc);
-
-  if ( $nRecords > 0 ) {
-
-    my $sendToAddress = $_[0];
-    my $subject = "duplicate pub titles detected";
-    my $errMsg = "$nRecords publications have duplicate titles\n";
-    logError ($errMsg);
-    &sendMail($sendToAddress, $subject, $routineName, $errMsg, $sql1, $sql2);  
-  }
-  &recordResult($routineName, $nRecords);
-} 
-
 #---------------------------------------------------------------
 # Each entry in foreign_db should have 1 or more entries in
 # foreign_db_contains.  foreign_db_contains describes what type(s) of
@@ -2693,7 +2605,6 @@ my $webAdminEmail = "<!--|WEB_ADMIN_EMAIL|-->";
 my $strEmail = "<!--|VALIDATION_EMAIL_MORPHOLINO|-->";
 my $genoEmail = "<!--|VALIDATION_EMAIL_GENOCURATOR|-->";
 my $transcriptEmail = "<!--|VALIDATION_EMAIL_TRANSCRIPT|-->";
-my $publicationEmail = "<!--|VALIDATION_EMAIL_PUBLICATION|-->";
 
 
 if($daily) {
@@ -2713,7 +2624,6 @@ if($daily) {
     zdbObjectHomeTableColumnExist($dbaEmail);
     zdbObjectIsSourceDataCorrect($dbaEmail);
 
-    pubTitlesAreUnique($publicationEmail);
     zdbReplacedDataIsReplaced($dbaEmail);
 
     mrkrgoevGoevflagDuplicatesFound($goEmail);
