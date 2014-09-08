181978bedd072ef781dc731756b42f4827df9c41
Christian Pich <cmpich@zfin.org>
2014-09-08 16:02:21 +0000

    bugzID: 11243 add constraint to only delete expression_experiment records associated to GELI figures
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@32755 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/report_data/Remove-GELI-Data.sql
+++ server_apps/DB_maintenance/report_data/Remove-GELI-Data.sql
@@ -5,13 +5,18 @@ FROM   figure
 WHERE  fig_source_zdb_id = '$PUBID'
        AND fig_comments = 'GELI';
 
+-- delete GELI figures via cascade
 DELETE FROM zdb_active_data
 WHERE zactvd_zdb_id IN (SELECT fig_zdb_id
                          FROM   figure
                          WHERE  fig_source_zdb_id = '$PUBID'
                                 AND fig_comments = 'GELI');
 
+-- delete expression_experiment records via cascade for GELI records
 DELETE FROM zdb_active_data
-WHERE zactvd_zdb_id IN (SELECT xpatex_zdb_id
-                         FROM   expression_experiment
-                         WHERE  xpatex_source_zdb_id = '$PUBID');
+WHERE  zactvd_zdb_id IN (SELECT xpatex_zdb_id
+                         FROM   expression_experiment,
+                                figure
+                         WHERE  xpatex_source_zdb_id = '$PUBID'
+                                AND fig_source_zdb_id = xpatex_source_zdb_id
+                                AND fig_comments = 'GELI');
