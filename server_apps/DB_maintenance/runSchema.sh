6c6063c61433abadb5d4f9838f3520f1ff70be90
Sierra Moxon <smoxon@zfin.org>
2014-09-09 16:15:01 +0000

    bugzid: 11456 leftover from GO project
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@32780 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/runSchema.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/tcsh
-
-#$1 db name
-setenv INFORMIXSERVER waldo
-setenv INFROMIXDIR /private/apps/Informix/informix/
-/private/apps/Informix/informix/bin/dbaccess $1 /research/zunloads/projects/GO/schema.sql;
-
-exit
