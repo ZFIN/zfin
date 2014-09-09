a74a82918ef0d8a7698c616677679abf1bfe1f35
Sierra Moxon <smoxon@zfin.org>
2014-09-09 16:45:02 +0000

    bugzid: 11514 removing defunct code.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@32783 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/data_transfer/BLAST/assembleZfinEnsemblTscripts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/bin/sed 's/|//' <!--|ROOT_PATH|-->/server_apps/data_transfer/BLAST/ensemblZFTscripts.txt > <!--|ROOT_PATH|-->/server_apps/data_transfer/BLAST/ensemblZfinTscriptsForBlast.txt
-
--- server_apps/data_transfer/BLAST/getEnsemblTscripts.sql
+++
@@ -1,8 +0,0 @@
-begin work;
-
-unload to <!--|ROOT_PATH|-->/server_apps/data_transfer/BLAST/ensemblZFTscripts.txt
-select ensm_ensdart_id
- from ensdar_mapping
-where exists (Select 'x' from db_link where dblink_acc_num like 'ENSDARG%');
-
-commit work ;
