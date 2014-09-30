b13759161f10d3008a9d5a376cbf939beeefa66a
Christian Pich <cmpich@zfin.org>
2014-09-30 19:41:51 +0000

    add probe and gene symbol to Probe/Gene related validation data check
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33082 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata/Check-Probe-Gene-Relationship_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Probe-Gene-Relationship_m.sql
@@ -1,15 +1,21 @@
 SELECT DISTINCT xpatfig_fig_zdb_id, 
                 xpatex_source_zdb_id, 
                 xpatex_zdb_id, 
-                xpatex_probe_feature_zdb_id, 
-                xpatex_gene_zdb_id 
-FROM   expression_experiment, 
+                xpatex_probe_feature_zdb_id,
+                probe.mrkr_abbrev,
+                xpatex_gene_zdb_id,
+                gene.mrkr_abbrev
+FROM   expression_experiment,
        expression_result, 
-       expression_pattern_figure 
-WHERE  xpatex_probe_feature_zdb_id IS NOT NULL 
+       expression_pattern_figure,
+       marker as probe,
+       marker as gene
+WHERE  xpatex_probe_feature_zdb_id IS NOT NULL
        AND NOT EXISTS (SELECT * 
                        FROM   marker_relationship 
                        WHERE  xpatex_probe_feature_zdb_id = mrel_mrkr_2_zdb_id 
                               AND xpatex_gene_zdb_id = mrel_mrkr_1_zdb_id) 
        AND xpatfig_xpatres_zdb_id = xpatres_zdb_id 
-       AND xpatres_xpatex_zdb_id = xpatex_zdb_id; 
+       AND xpatres_xpatex_zdb_id = xpatex_zdb_id
+       AND gene.mrkr_zdb_id = xpatex_gene_zdb_id
+       AND probe.mrkr_zdb_id = xpatex_probe_feature_zdb_id;
