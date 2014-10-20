5ff83bb02f8b9c70c0627317adad229ea89547e5
Sierra Moxon <smoxon@zfin.org>
2014-10-20 21:21:08 +0000

    bugzid: 11970
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33333 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/warehouse/fishMart/addCountsToFunctionalAnnotation.sql
+++ server_apps/DB_maintenance/warehouse/fishMart/addCountsToFunctionalAnnotation.sql
@@ -4,14 +4,6 @@ update zdb_flag
   set (zflag_is_on,zflag_last_modified) = ("t",current year to second)
  where zflag_name = "regen_fishmart_bts_indexes";
 
-update statistics high for table functional_annotation;
-update statistics high for table feature_group;
-update statistics high for table feature_group_member;
-update statistics high for table affected_gene_group_member;
-update statistics high for table affected_gene_group;
-update statistics high for table str_group;
-update statistics high for table phenotype_experiment;
-
 
 update functional_annotation
  set fa_str_member_count = 0;
