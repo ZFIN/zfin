d0a5308df6c0b875c956f9069180783ebfef5190
Christian Pich <cmpich@zfin.org>
2014-10-30 16:16:28 +0000

    bugzID: 12044 convert routine Check-STR-Abbreviation-Match-Gene_m to Jenkins
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33435 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata.pl
+++ server_apps/DB_maintenance/validatedata.pl
@@ -350,50 +350,6 @@ sub phenotypeAnnotationUnspecified ($$) {
 
 
 
-#---------------------------------------------------------------
-# strAbbrevContainsGeneAbbrev
-#
-# Parameter
-# $ Email Address for recipients
-
-sub strAbbrevContainsGeneAbbrevBasic($) {
-  my $routineName = "strAbbrevContainsGeneAbbrevBasic";
-	
-  my $sql = "select a.mrkr_abbrev, b.mrkr_abbrev
-               from marker a, marker b, marker_relationship c
-               where a.mrkr_zdb_id = c.mrel_mrkr_1_zdb_id
-               and b.mrkr_zdb_id = c.mrel_mrkr_2_zdb_id
-    and exists (Select 'x' from marker_type_group_member where
-                              a.mrkr_type = mtgrpmem_mrkr_type
-                              and mtgrpmem_mrkr_type_group = 'KNOCKDOWN_REAGENT')
-                and b.mrkr_abbrev !=
-               (substring(a.mrkr_abbrev 
-                            from
-                             (length(a.mrkr_abbrev)-length(b.mrkr_abbrev)+1)
-                            for
-                             (length(b.mrkr_abbrev))
-                          )
-                )
-              order by b.mrkr_abbrev";
-
-  my @colDesc = ("STR abbrev         ",
-		 "Gene abbrev       ");
-
-  my $nRecords = execSql ($sql, undef, @colDesc);
-
-  if ( $nRecords > 0 ) {
-    my $sendToAddress = $_[0];
-    my $subject = "Str abbrev not like gene_abbrev";
-    my $errMsg = "There are $nRecords strs without corresponding gene abbrevs. ";
-    
-    logError ($errMsg);
-    &sendMail($sendToAddress, $subject, $routineName, $errMsg, $sql);
-  }
-  &recordResult($routineName, $nRecords);
-
-}
-
-
 #======================== PUB Attribution ========================
 
 #---------------------------------------------------------------
@@ -558,7 +514,6 @@ if($monthly) {
 }
 if ($yearly) {
     
-    strAbbrevContainsGeneAbbrevBasic($strEmail);
 }
 	   
 
