b22bb56dd47fa246e8eae3dc7e512853b9983c1e
Sierra Moxon <smoxon@zfin.org>
2014-11-11 19:30:41 +0000

    bugzid: 12062 updated SQL in accordance with PML project
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33516 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata/Check-Linkage-has-Members_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Linkage-has-Members_m.sql
@@ -3,7 +3,9 @@ SELECT lnkg_zdb_id,
        recattrib_source_zdb_id
 FROM   linkage,
        record_attribution
-WHERE  lnkg_zdb_id NOT IN (SELECT lnkgmem_linkage_zdb_id
-                           FROM   linkage_member)
+WHERE  not exists (select 'x' from linkage_membership 
+       	   	  	  where lnkgm_linkage_zdb_id = lnkg_zdb_id)
        AND lnkg_zdb_id = recattrib_data_zdb_id
        AND lnkg_comments != 'ZFIN historical data'
+and not exists (Select 'x' from linkage_single
+    	       	       where lsingle_lnkg_zdb_id = lnkg_zdb_id)
