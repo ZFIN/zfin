04d8c6ff9511b6f00faf5fb3a80fd8b1dbf6844e
Ryan Martin <ryanm@zfin.org>
2014-11-19 01:01:22 +0000

    BugzID: 9307 Moved or removed files from commons/bin.  Most are now located in /opt/zfin/bin.
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33604 908b024a-c82b-635c-c293-f2b15b23277c

--- commons/bin/getinitrevision.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env perl
-#use strict ; 
-#use warnings ; 
-$line = $ARGV[0] ; 
-@tokens = split( /\ \|/, $line); 
-print substr(@tokens[0],1); 
--- commons/bin/list_runs.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/tcsh
-
-echo " select run_zdb_id[1,25]zdb,run_name name,run_type[1,12] from run order by run_date" | dbaccess -a $DBNAME
