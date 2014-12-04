87f2dddc6c136b11d5df776a0139605334d7e8d2
Christian Pich <cmpich@zfin.org>
2014-12-04 00:00:00 +0000

    bugzID: 11138 separate antibody and STR nomenclature reports into 2 Jenkins jobs
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33708 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata/Check-Nomenclature-Antibody_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Nomenclature-Antibody_m.sql
@@ -1,21 +1,3 @@
-unload to STR-Report
-SELECT a.mrkr_abbrev,
-       b.mrkr_abbrev
-FROM   marker a,
-       marker b,
-       marker_relationship c
-WHERE  a.mrkr_zdb_id = c.mrel_mrkr_1_zdb_id
-       AND b.mrkr_zdb_id = c.mrel_mrkr_2_zdb_id
-       AND EXISTS (SELECT 'x'
-                   FROM   marker_type_group_member
-                   WHERE  a.mrkr_type = mtgrpmem_mrkr_type
-                          AND mtgrpmem_mrkr_type_group = 'KNOCKDOWN_REAGENT')
-       AND b.mrkr_abbrev != ( Substring(a.mrkr_abbrev FROM
-( Length(a.mrkr_abbrev) - Length(b.mrkr_abbrev) + 1 ) FOR
-(
-Length(b.mrkr_abbrev) )) )
-ORDER  BY b.mrkr_abbrev;
-
 -- create temp table of all genes and pseudo genes that have updated symbols within
 -- the last 31 days
 SELECT   rec_id,
@@ -31,7 +13,6 @@ ORDER BY when DESC
 INTO temp gene_names_changed_temp;
 
 
-unload to Antibody-Report
 SELECT b.mrkr_zdb_id,
        up.old_value,
        b.mrkr_abbrev        AS targetGene,
