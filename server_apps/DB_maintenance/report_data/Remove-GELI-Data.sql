f661d028cd146a840f03a876b2fe84563bff54da
Sierra Moxon <smoxon@zfin.org>
2014-12-10 17:01:56 +0000

    bugzid: 11243 fix order of deletes to avoid orphans in db
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33757 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/report_data/Remove-GELI-Data.sql
+++ server_apps/DB_maintenance/report_data/Remove-GELI-Data.sql
@@ -5,13 +5,6 @@ FROM   figure
 WHERE  fig_source_zdb_id = '$PUBID'
        AND fig_comments = 'GELI';
 
--- delete GELI figures via cascade
-DELETE FROM zdb_active_data
-WHERE zactvd_zdb_id IN (SELECT fig_zdb_id
-                         FROM   figure
-                         WHERE  fig_source_zdb_id = '$PUBID'
-                                AND fig_comments = 'GELI');
-
 -- delete expression_experiment records via cascade for GELI records
 DELETE FROM zdb_active_data
 WHERE  zactvd_zdb_id IN (SELECT xpatex_zdb_id
@@ -19,4 +12,11 @@ WHERE  zactvd_zdb_id IN (SELECT xpatex_zdb_id
                                 figure
                          WHERE  xpatex_source_zdb_id = '$PUBID'
                                 AND fig_source_zdb_id = xpatex_source_zdb_id
+                                AND fig_comments = 'GELI');
+
+-- delete GELI figures via cascade
+DELETE FROM zdb_active_data
+WHERE zactvd_zdb_id IN (SELECT fig_zdb_id
+                         FROM   figure
+                         WHERE  fig_source_zdb_id = '$PUBID'
                                 AND fig_comments = 'GELI');
