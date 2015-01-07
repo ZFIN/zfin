c9e8d2fcee2936407e823a6c0cf28a13ed76ab0a
Sierra Moxon <smoxon@zfin.org>
2015-01-07 17:04:36 +0000

    bugzid: 12144
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@33901 908b024a-c82b-635c-c293-f2b15b23277c

---
+++ lib/DB_functions/SPL/restrictGAFEntries.sql
@@ -0,0 +1,12 @@
+create procedure restrictGAFEntries (vTermZdbId varchar(50))
+
+
+if exists (Select 'x' from term_subset, ontology_subset
+   	  	  where termsub_term_zdb_id = term_Zdb_id
+		  and termsub_subset_id = osubset_pk_id
+		  and osubset_subset_name in ('gocheck_do_not_annotate','gocheck_do_not_manually_annotate'))
+then 
+  raise exception -746,0,"FAIL!: GO term can not be in do-no-annotate subset";
+end if;
+
+end procedure;
