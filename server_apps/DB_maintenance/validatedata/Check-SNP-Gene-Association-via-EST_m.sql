428dde479a2f4787f5ca720dc7d65e5b484c1615
Xiang Shao <xshao@zfin.org>
2015-04-10 23:02:03 +0000

    bugzid:12453 added 2 more columns
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@34545 908b024a-c82b-635c-c293-f2b15b23277c

--- server_apps/DB_maintenance/validatedata/Check-SNP-Gene-Association-via-EST_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-SNP-Gene-Association-via-EST_m.sql
@@ -1,27 +1,31 @@
-SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, mr1.mrel_mrkr_2_zdb_id as EST_id
-FROM   marker_relationship mr1 
-WHERE  get_obj_type(mr1.mrel_mrkr_1_zdb_id) = "GENE" 
-      AND mr1.mrel_type = "gene encodes small segment" 
-      AND get_obj_type(mr1.mrel_mrkr_2_zdb_id) = "EST"
-      AND NOT EXISTS (SELECT "x" FROM clone c
-                       WHERE c.clone_mrkr_zdb_id = mr1.mrel_mrkr_2_zdb_id
-                         AND c.clone_problem_type = "Chimeric")
-      AND EXISTS (SELECT "x" FROM marker_relationship mr2
-                   WHERE mr2.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_2_zdb_id
-                     AND mr2.mrel_type = "contains polymorphism"
-                     AND get_obj_type(mr2.mrel_mrkr_2_zdb_id) = "SNP"
-                     AND NOT EXISTS (SELECT "x" FROM marker_relationship mr3
-                                      WHERE mr3.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_1_zdb_id
-                                        AND mr3.mrel_type = "contains polymorphism"
-                                        AND get_obj_type(mr3.mrel_mrkr_2_zdb_id) = "SNP"
-                                    )
-                 )
-UNION
-SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, mr1.mrel_mrkr_2_zdb_id as EST_id
-  FROM marker_relationship mr1 
+SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, gene.mrkr_abbrev as gene_symbol, mr1.mrel_mrkr_2_zdb_id as EST_id, est.mrkr_abbrev as EST_symbol
+  FROM marker_relationship mr1, marker gene, marker est 
  WHERE get_obj_type(mr1.mrel_mrkr_1_zdb_id) = "GENE" 
    AND mr1.mrel_type = "gene encodes small segment" 
    AND get_obj_type(mr1.mrel_mrkr_2_zdb_id) = "EST"
+   AND mr1.mrel_mrkr_1_zdb_id= gene.mrkr_zdb_id
+   AND mr1.mrel_mrkr_2_zdb_id= est.mrkr_zdb_id
+   AND NOT EXISTS (SELECT "x" FROM clone c
+                    WHERE c.clone_mrkr_zdb_id = mr1.mrel_mrkr_2_zdb_id
+                      AND c.clone_problem_type = "Chimeric")
+   AND EXISTS (SELECT "x" FROM marker_relationship mr2
+                WHERE mr2.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_2_zdb_id
+                  AND mr2.mrel_type = "contains polymorphism"
+                  AND get_obj_type(mr2.mrel_mrkr_2_zdb_id) = "SNP"
+                  AND NOT EXISTS (SELECT "x" FROM marker_relationship mr3
+                                   WHERE mr3.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_1_zdb_id
+                                     AND mr3.mrel_type = "contains polymorphism"
+                                     AND get_obj_type(mr3.mrel_mrkr_2_zdb_id) = "SNP"
+                                 )
+              )
+UNION
+SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, gene.mrkr_abbrev as gene_symbol, mr1.mrel_mrkr_2_zdb_id as EST_id, est.mrkr_abbrev as EST_symbol
+  FROM marker_relationship mr1, marker gene, marker est  
+ WHERE get_obj_type(mr1.mrel_mrkr_1_zdb_id) = "GENE" 
+   AND mr1.mrel_type = "gene encodes small segment" 
+   AND get_obj_type(mr1.mrel_mrkr_2_zdb_id) = "EST"
+   AND mr1.mrel_mrkr_1_zdb_id= gene.mrkr_zdb_id
+   AND mr1.mrel_mrkr_2_zdb_id= est.mrkr_zdb_id
    AND NOT EXISTS (SELECT "x" FROM clone c
                     WHERE c.clone_mrkr_zdb_id = mr1.mrel_mrkr_2_zdb_id
                       AND c.clone_problem_type = "Chimeric")
@@ -37,24 +41,27 @@ SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, mr1.mrel_mrkr_2_zdb_id as EST
                                  )
               )   
 UNION
-SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, mr1.mrel_mrkr_2_zdb_id as EST_id
-FROM   marker_relationship mr1 
-WHERE  get_obj_type(mr1.mrel_mrkr_1_zdb_id) = "GENE" 
-      AND mr1.mrel_type = "gene encodes small segment" 
-      AND get_obj_type(mr1.mrel_mrkr_2_zdb_id) = "EST"
-      AND NOT EXISTS (SELECT "x" FROM clone c
-                       WHERE c.clone_mrkr_zdb_id = mr1.mrel_mrkr_2_zdb_id
-                         AND c.clone_problem_type = "Chimeric")
-      AND EXISTS (SELECT "x" FROM marker_relationship mr2
-                   WHERE mr2.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_2_zdb_id
-                     AND mr2.mrel_type = "contains polymorphism"
-                     AND get_obj_type(mr2.mrel_mrkr_2_zdb_id) = "SNP"
-                     AND EXISTS (SELECT "x" FROM marker_relationship mr3
-                                  WHERE mr3.mrel_mrkr_2_zdb_id = mr2.mrel_mrkr_2_zdb_id
-                                    AND mr3.mrel_type = "contains polymorphism"
-                                    AND get_obj_type(mr3.mrel_mrkr_1_zdb_id) = "GENE"
-                                    AND mr3.mrel_mrkr_1_zdb_id != mr1.mrel_mrkr_1_zdb_id
-                                )
-                 )
-;  
+SELECT distinct mr1.mrel_mrkr_1_zdb_id as gene_id, gene.mrkr_abbrev as gene_symbol, mr1.mrel_mrkr_2_zdb_id as EST_id, est.mrkr_abbrev as EST_symbol
+  FROM marker_relationship mr1, marker gene, marker est
+ WHERE get_obj_type(mr1.mrel_mrkr_1_zdb_id) = "GENE" 
+   AND mr1.mrel_type = "gene encodes small segment" 
+   AND get_obj_type(mr1.mrel_mrkr_2_zdb_id) = "EST"
+   AND mr1.mrel_mrkr_1_zdb_id= gene.mrkr_zdb_id
+   AND mr1.mrel_mrkr_2_zdb_id= est.mrkr_zdb_id
+   AND NOT EXISTS (SELECT "x" FROM clone c
+                    WHERE c.clone_mrkr_zdb_id = mr1.mrel_mrkr_2_zdb_id
+                      AND c.clone_problem_type = "Chimeric")
+   AND EXISTS (SELECT "x" FROM marker_relationship mr2
+                WHERE mr2.mrel_mrkr_1_zdb_id = mr1.mrel_mrkr_2_zdb_id
+                  AND mr2.mrel_type = "contains polymorphism"
+                  AND get_obj_type(mr2.mrel_mrkr_2_zdb_id) = "SNP"
+                  AND EXISTS (SELECT "x" FROM marker_relationship mr3
+                               WHERE mr3.mrel_mrkr_2_zdb_id = mr2.mrel_mrkr_2_zdb_id
+                                 AND mr3.mrel_type = "contains polymorphism"
+                                 AND get_obj_type(mr3.mrel_mrkr_1_zdb_id) = "GENE"
+                                 AND mr3.mrel_mrkr_1_zdb_id != mr1.mrel_mrkr_1_zdb_id
+                             )
+              )
+ORDER BY gene_symbol, EST_symbol            
+;   
 
