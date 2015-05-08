af5cdf0cb4abb43fefb84ea67cba340f289b7d63
Kevin Schaper <kschaper@zfin.org>
2015-05-08 22:50:44 +0000

    Bugzid: 12756  Added drug list report, can currently only be run manually from SOURCEROOT
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@34723 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/search/service/SolrService.java
+++ source/org/zfin/search/service/SolrService.java
@@ -32,7 +32,7 @@ public class SolrService {
 
     public static Logger logger = Logger.getLogger(SolrService.class);
 
-    private static final String LUCENE_ESCAPE_CHARS = "[\\\\+\\-\\!\\(\\)\\:\\^\\]\\{\\}\\~\\*\\?\\\"\\,]";
+    private static final String LUCENE_ESCAPE_CHARS = "[\\\\+\\-\\!\\(\\)\\:\\^\\]\\[\\{\\}\\~\\*\\?\\\"\\,]";
     private static final Pattern LUCENE_PATTERN = Pattern.compile(LUCENE_ESCAPE_CHARS);
     private static final String REPLACEMENT_STRING = "\\\\$0";
 
