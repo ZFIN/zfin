233dcb117fc5efa4dc5b07ffd343c60a090fb132
Christian Pich <cmpich@zfin.org>
2015-05-14 20:28:30 +0000

    bugzID: 12799 add Delete Fish rule
    git-svn-id: file:///research/zcentral/Vault/SVNROOT/ZFIN_WWW/trunk@34745 908b024a-c82b-635c-c293-f2b15b23277c

--- source/org/zfin/infrastructure/delete/DeleteFishRule.java
+++ source/org/zfin/infrastructure/delete/DeleteFishRule.java
@@ -1,11 +1,14 @@
 package org.zfin.infrastructure.delete;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.zfin.mutant.DiseaseModel;
 import org.zfin.mutant.Fish;
 import org.zfin.publication.Publication;
 import org.zfin.repository.RepositoryFactory;
 
 import java.util.*;
+
+import static org.zfin.repository.RepositoryFactory.getPhenotypeRepository;
 
 public class DeleteFishRule extends AbstractDeleteEntityRule implements DeleteEntityRule {
 
@@ -17,7 +20,14 @@ public class DeleteFishRule extends AbstractDeleteEntityRule implements DeleteEn
     public List<DeleteValidationReport> validate() {
         Fish fish = RepositoryFactory.getMutantRepository().getFish(zdbID);
         entity = fish;
-
+        List<Publication> publicationList = RepositoryFactory.getMutantRepository().getPublicationWithFish(zdbID);
+        if (CollectionUtils.isNotEmpty(publicationList) && publicationList.size() > 1) {
+            addToValidationReport(fish.getAbbreviation() + " associated with more than one publication: ", publicationList);
+        }
+        List<DiseaseModel> diseaseModelList = getPhenotypeRepository().getHumanDiseaseModelsByFish(zdbID);
+        if (CollectionUtils.isNotEmpty(diseaseModelList) && diseaseModelList.size() > 1) {
+            addToValidationReport(fish.getAbbreviation() + " associated with : "+diseaseModelList.size()+" disease models", diseaseModelList);
+        }
         return validationReportList;
     }
 
