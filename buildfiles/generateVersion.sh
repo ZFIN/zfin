eb41cd3f90d703db2ab58419a936f8300ccde949
Ryan Martin <ryanm@zfin.org>
2015-06-08 12:49:09 -0700

    Updated version setup for git.  Probably still needs more work.

--- buildfiles/generateVersion.sh
+++ buildfiles/generateVersion.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-SVN_REVISION_STRING=`svn info | grep Revision`
-SVN_REVISION=`expr "$SVN_REVISION_STRING" : 'Revision: \(.*\)'`;
-SVN_URL_STRING=`svn info | grep URL`
-SVN_URL=`expr "$SVN_URL_STRING" : '.*ZFIN_WWW\/\(.*\)'`;
+GIT_COMMIT=`git log -n 1| grep -Po 'commit \K.*'`
+GIT_URL=`git branch | grep -Po '\* \K.*'`
 EXPORT_FILE="$1" ; 
 TITLE=`expr "$EXPORT_FILE" : '.*home\/\(.*\)'`;
 echo "<html><head><title>Deploy Version $1</title></head><body>" > $EXPORT_FILE ;  
@@ -10,8 +8,8 @@ echo "<h1>$TITLE</h1>" >> $EXPORT_FILE ;
 echo "<ul>" >> $EXPORT_FILE ;
 
 echo "<li>TIME: `date`" >> $EXPORT_FILE  ; 
-echo "<li>REVISION: $SVN_REVISION" >> $EXPORT_FILE  ; 
-echo "<li>BRANCH: $SVN_URL" >> $EXPORT_FILE  ; 
+echo "<li>REVISION: $GIT_COMMIT" >> $EXPORT_FILE  ; 
+echo "<li>BRANCH: $GIT_URL" >> $EXPORT_FILE  ; 
 echo "<li>DOMAIN_NAME: $DOMAIN_NAME" >> $EXPORT_FILE  ;
 echo "<li>INFORMIXSERVER: $INFORMIXSERVER" >> $EXPORT_FILE  ;
 
