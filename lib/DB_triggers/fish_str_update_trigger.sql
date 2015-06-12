4fffbb627fb9d9999170a748e6f3a1770c52ae31
Sierra Moxon <smoxon@zfin.org>
2015-06-12 16:29:32 -0700

    bugzid: 12971

--- lib/DB_triggers/fish_str_update_trigger.sql
+++
@@ -1,6 +0,0 @@
-create trigger fishstr_str_update_trigger update of fishstr_str_zdb_id, fishstr_fish_zdb_id
-    on fish_str
-    after
-        (
-        execute procedure p_update_related_fish_name() 
-    );
