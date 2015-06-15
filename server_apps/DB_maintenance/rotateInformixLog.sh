b9eef3f7425f0e4f1d78d86fa4a83f1b6cff300b
Sierra Moxon <smoxon@zfin.org>
2015-06-15 09:34:53 -0700

    12857

--- server_apps/DB_maintenance/rotateInformixLog.sh
+++ server_apps/DB_maintenance/rotateInformixLog.sh
@@ -1,17 +1,6 @@
-#!/bin/tcsh 
+#!/bin/tcsh -e
 
-export INFORMIXDIR=<!--|INFORMIX_DIR|-->
-export INFORMIXSERVER=<!--|INFORMIX_SERVER|-->
-export INFORMIX_PORT=<!--|INFORMIX_PORT|-->
-export INFROMIXSQLHOSTS=<!--|INFORMIXSQLHOSTS|-->
-export INSTANCE=<!--|INSTANCE|-->
-
-echo $INFORMIXDIR
-echo $INFORMIXSERVER
-echo $INFORMIX_PORT
-echo $INFORMIXSQLHOSTS
-echo $INSTANCE
-
+source /private/ZfinLinks/Commons/env/<!--|INSTANCE|-->.env
 
 set processID=`$(ps aux | grep '[o]ntape' | awk '{print $2}')`;
 echo $processID;
