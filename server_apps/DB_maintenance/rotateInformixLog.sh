866219c32467168af882cccb25d5d81bbb4461d9
Sierra Moxon <smoxon@zfin.org>
2015-06-15 09:41:53 -0700

    12857

--- server_apps/DB_maintenance/rotateInformixLog.sh
+++ server_apps/DB_maintenance/rotateInformixLog.sh
@@ -2,7 +2,7 @@
 
 source /private/ZfinLinks/Commons/env/<!--|INSTANCE|-->.env
 
-set processID=`$(ps aux | grep '[o]ntape' | awk '{print $2}')`;
+set processID=`ps aux | grep '[o]ntape' | awk '{print $2}'`;
 echo $processID;
 kill $processID;
 
