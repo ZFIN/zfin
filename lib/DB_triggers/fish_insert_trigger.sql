180c0df2a537fe1d981e2565b9fed1a3782be2e3
Sierra Moxon <smoxon@zfin.org>
2015-06-22 09:36:00 -0700

    bugzid: 12966

--- lib/DB_triggers/fish_insert_trigger.sql
+++ lib/DB_triggers/fish_insert_trigger.sql
@@ -4,7 +4,7 @@ create trigger fish_insert_trigger insert on fish
      execute function scrub_char(new_fish.fish_name)
      	     into fish.fish_name,
      execute function zero_pad (new_fish.fish_name)
-     	     into fish.fish_name_order --,
-    	--execute function update_fish_sort_order(new_fish.fish_zdb_id)
-    --into fish.fish_complexity_order 
+     	     into fish.fish_name_order,
+    	execute function getFishOrder(new_fish.fish_zdb_id)
+    into fish.fish_order 
 );
