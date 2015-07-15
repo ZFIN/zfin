e20072ae4c3664807e7c556e231be7b4d75e2c84
Sierra Moxon <smoxon@zfin.org>
2015-07-15 13:55:13 -0700

    bugzid: 13162

---
+++ lib/DB_functions/SPL/Regen_genox/fix.sql
@@ -0,0 +1,11 @@
+alter table genotype_figure_fast_Search
+ drop gffs_superterm_Zdb_id;
+
+alter table genotype_figure_fast_Search
+ drop gffs_subterm_zdb_id;
+
+alter table genotype_figure_fast_Search
+ drop gffs_quality_zdb_id;
+
+alter table genotype_figure_fast_Search
+ drop gffs_tag;
---
+++ lib/DB_functions/SPL/Regen_genox/regen_genofig_genotype.sql
@@ -0,0 +1,33 @@
+create procedure regen_genofig_phenox(phenoxId like phenotype_Experiment.phenox_pk_id, genoxId like fish_Experiment.genox_zdb_id)
+
+  -- ---------------------------------------------------------------------------------------------
+  -- regenerates records in fast search table genotype_figure_fast_search for a given genotype.
+  -- INPUT VARS:
+  --   phenoxId    PK id of phenotype_experiment
+  --
+  -- OUTPUT VARS:
+  --   none
+  --
+  -- RETURNS:
+  --   Success: nothing
+  --   Error:   throws whatever exception occurred.
+  -- ---------------------------------------------------------------------------------------------
+
+
+  -- crank up the parallelism.
+  set pdqpriority high;
+
+  -- create regen_genofig_clean_exp_with_morph_temp, regen_genofig_not_normal_temp,
+  --        regen_genofig_temp, regen_genofig_input_zdb_id_temp
+  execute procedure regen_genofig_create_temp_tables();
+
+  execute procedure regen_genox_create_temp_tables();
+
+  execute procedure regen_genox_genox(genoxId);
+  -- takes regen_genofig_input_zdb_id_temp as input, adds recs to regen_genofig_temp
+  execute procedure regen_genofig_process(phenoxId);
+
+  -- Move from temp tables to permanent tables
+  execute procedure regen_genofig_finish(phenoxId);
+
+end procedure;
