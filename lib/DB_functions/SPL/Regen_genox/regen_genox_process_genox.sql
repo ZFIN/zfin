aaca9dc54e0ea3c37695cc4328adabb440541eec
Sierra Moxon <smoxon@zfin.org>
2015-08-06 14:33:41 -0700

    fixing phenotype bugz

--- lib/DB_functions/SPL/Regen_genox/regen_genox_process_genox.sql
+++ lib/DB_functions/SPL/Regen_genox/regen_genox_process_genox.sql
@@ -25,7 +25,7 @@ insert into regen_genox_temp (rggt_mrkr_zdb_id, rggt_genox_zdb_id)
     and fish_genotype_zdb_id = genofeat_geno_zdb_id
     and genofeat_feature_Zdb_id = fmrel_ftr_zdb_id
     and fish_functional_affected_gene_count = 1
-    and get_obj_type(fmrel_mrkr_Zdb_id) in ('GENE','MRPHLNO','TALEN','CRISPR')
+ and get_obj_type(fmrel_mrkr_Zdb_id) in ('GENE','MRPHLNO','TALEN','CRISPR')
     and genox_is_std_or_generic_control = 't'
     and genox_zdb_id = rggz_zdb_id
 and not exists (Select 'x' from regen_genox_temp
@@ -38,7 +38,7 @@ insert into regen_genox_temp (rggt_mrkr_zdb_id, rggt_genox_zdb_id)
     where fish_Zdb_id =fishstr_fish_Zdb_id
     and fish_zdb_id = genox_fish_zdb_id
     and fish_functional_affected_gene_count =1
-    and get_obj_type(fishstr_str_Zdb_id) in ('GENE','MRPHLNO','TALEN','CRISPR')
+ and get_obj_type(fmrel_mrkr_Zdb_id) in ('GENE','MRPHLNO','TALEN','CRISPR')
     and genox_is_std_or_generic_control = 't'
     and genox_zdb_id = rggz_zdb_id
 and not exists (Select 'x' from regen_genox_temp
--- lib/DB_functions/SPL/updatestandard.sql
+++ lib/DB_functions/SPL/updatestandard.sql
@@ -1,10 +1,9 @@
 create function updatestandard(vExpZdbId like fish_experiment.genox_exp_zdb_id)
   returning boolean;
 
-  define fal boolean;
   define std boolean; 
+  let std = 'f';
  
-  let fal = 'f';
   let std = (select 't'
                       from experiment
                       where exp_zdb_id = vExpZdbId
@@ -14,10 +13,8 @@ create function updatestandard(vExpZdbId like fish_experiment.genox_exp_zdb_id)
 	       	      from experiment
 		      where not exists (Select 'x' from experiment_condition
 		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id));
-  if (std = 't') 
   then 
-       return std; 
-  else return fal;
+       return std;
  end if;
 
 end function;
