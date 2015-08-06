373c58366a20a03c7cc2bee4a9502dd156a52ff7
Sierra Moxon <smoxon@zfin.org>
2015-08-06 14:49:31 -0700

    fixing clean phenotype

--- lib/DB_functions/SPL/updatestandard.sql
+++ lib/DB_functions/SPL/updatestandard.sql
@@ -4,17 +4,19 @@ create function updatestandard(vExpZdbId like fish_experiment.genox_exp_zdb_id)
   define std boolean; 
   let std = 'f';
  
-  let std = (select 't'
-                      from experiment
+  if exists (Select 'x' from experiment
                       where exp_zdb_id = vExpZdbId
                       and exp_name in ('_Standard') 
                union
 	       select 't'
 	       	      from experiment
 		      where not exists (Select 'x' from experiment_condition
-		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id));
+		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id))
+  
   then 
+       let std = 't';
+  end if;
+
        return std;
- end if;
 
 end function;
--- lib/DB_functions/SPL/updatestandardorgenericcontrol.sql
+++ lib/DB_functions/SPL/updatestandardorgenericcontrol.sql
@@ -1,23 +1,23 @@
 create function updatestandardorgenericcontrol(vExpZdbId like fish_experiment.genox_exp_zdb_id)
   returning boolean;
-  define fal boolean;
   
   define stdGc boolean;  
-  let fal = 'f';
-  let stdGc = (select 't'
-                      from experiment
+  let stdGc = 'f';
+
+
+ if exists (Select 'x' from experiment
                       where exp_zdb_id = vExpZdbId
-                      and exp_name in ('_Standard') 
+                      and exp_name in ('_Standard', '_Generic_control') 
                union
 	       select 't'
 	       	      from experiment
 		      where not exists (Select 'x' from experiment_condition
-		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id));
-
-  if (stdGc = 't') 
+		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id))
+  
 then 
+     let stdGc = 't';
+end if;
+
 return stdGc; 
-  else return fal;
- end if;
 
 end function;
