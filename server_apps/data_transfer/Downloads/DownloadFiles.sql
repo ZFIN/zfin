8a34d8b3266eb8b707186b8e9a4e8b363859671b
Sierra Moxon <smoxon@zfin.org>
2015-08-25 14:15:41 -0700

    bugzid: 12347

--- server_apps/data_transfer/Downloads/DownloadFiles.sql
+++ server_apps/data_transfer/Downloads/DownloadFiles.sql
@@ -382,36 +382,30 @@ select gene.mrkr_zdb_id gene_zdb, gene.mrkr_abbrev,
 ! echo "'<!--|ROOT_PATH|-->/server_apps/data_transfer/Downloads/downloadsStaging/abxpat_fish.txt'"
 UNLOAD to '<!--|ROOT_PATH|-->/server_apps/data_transfer/Downloads/downloadsStaging/abxpat_fish.txt'
  DELIMITER "	"
- select antibody.atb_zdb_id, atb.mrkr_abbrev,gene.mrkr_zdb_id as gene_zdb,
+ select xpatex_atb_zdb_id, atb.mrkr_abbrev, xpatex_gene_zdb_id as gene_zdb,
 	gene.mrkr_abbrev, xpatex_assay_name, xpatex_zdb_id as xpat_zdb,
 	xpatex_source_zdb_id, fish_zdb_id, genox_exp_zdb_id
- from expression_experiment
- join fish_experiment
-   on genox_zdb_id = xpatex_genox_zdb_id
- join fish fish
-   on fish.fish_zdb_id = genox_fish_zdb_id
- join antibody antibody
-   on antibody.atb_zdb_id = xpatex_atb_zdb_id
- left join marker gene
-   on gene.mrkr_zdb_id = xpatex_gene_zdb_id, marker as atb
- where atb.mrkr_zdb_id  = antibody.atb_zdb_id
+ from expression_experiment, fish_experiment, fish, marker atb, marker gene, outer clone
+ where xpatex_genox_Zdb_id = genox_zdb_id
+ and genox_fish_zdb_id = fish_Zdb_id
+ and atb.mrkr_zdb_id = xpatex_atb_zdb_id
+ and gene.mrkr_zdb_id = xpatex_gene_zdb_id
    and xpatex_gene_zdb_id is null
+ and clone_mrkr_zdb_id = xpatex_probe_feature_zdb_id
+ and clone_problem_type != 'Chimeric'
 UNION
- select antibody.atb_zdb_id, atb.mrkr_abbrev, gene.mrkr_zdb_id as gene_zdb,
+ select xpatex_atb_zdb_id, atb.mrkr_abbrev, xpatex_gene_zdb_id as gene_zdb,
 	gene.mrkr_abbrev, xpatex_assay_name, xpatex_zdb_id as xpat_zdb,
 	xpatex_source_zdb_id, fish_zdb_id, genox_exp_zdb_id
- from expression_experiment
- join fish_experiment
-   on genox_zdb_id = xpatex_genox_zdb_id
- join fish fish
-   on fish.fish_zdb_id = genox_fish_zdb_id
- join antibody antibody
-   on antibody.atb_zdb_id = xpatex_atb_zdb_id
- join marker gene
-   on gene.mrkr_zdb_id = xpatex_gene_zdb_id, marker as atb
- where gene.mrkr_abbrev[1,10] != 'WITHDRAWN:'
-   and atb.mrkr_zdb_id  = antibody.atb_zdb_id
- order by antibody.atb_zdb_id, xpat_zdb
+ from expression_experiment, fish_experiment, fish, marker atb, marker gene, outer clone
+ where xpatex_genox_Zdb_id = genox_zdb_id
+ and genox_fish_zdb_id = fish_Zdb_id
+ and atb.mrkr_zdb_id = xpatex_atb_zdb_id
+ and gene.mrkr_zdb_id = xpatex_gene_zdb_id
+   and xpatex_gene_zdb_id is not null
+ and gene.mrkr_abbrev not like 'WITHDRAWN:'
+ and clone_mrkr_zdb_id = xpatex_probe_feature_zdb_id
+ and clone_problem_type != 'Chimeric'
 ;
 
 -- generate a file to map experiment id to environment condition description
