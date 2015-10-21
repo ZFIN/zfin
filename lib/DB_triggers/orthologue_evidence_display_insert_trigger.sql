80ea429e9f9c6920e0217d82ffb98cdbe60796a1
Sierra Moxon <smoxon@zfin.org>
2015-10-21 13:44:59 -0700

    removing orthologue triggers

--- lib/DB_triggers/orthologue_evidence_display_insert_trigger.sql
+++
@@ -1,6 +0,0 @@
-create trigger orthologue_evidence_display_insert_trigger insert 
-  on orthologue_evidence_display
-  referencing new as new_oevdisp
-  for each row 
- 	(execute procedure p_check_zdb_id_in_genedom
-		(new_oevdisp.oevdisp_gene_zdb_id)) ;
--- lib/DB_triggers/orthologue_insert_trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger orthologue_insert_trigger insert on 
-    orthologue referencing new as new_ortho
-    for each row
-        (
-	execute function scrub_char ( new_ortho.ortho_name )
-		into ortho_name,
-	execute function scrub_char ( new_ortho.ortho_abbrev )
-		into ortho_abbrev
-        );
