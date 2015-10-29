9abcd7aab70232ca57b3fedd044a14ec9ba31021
Prita Mani <pm@zfin.org>
2015-10-29 15:23:25 -0700

    bugzid: 12108

--- server_apps/DB_maintenance/validatedata/Check-Nomenclature-Pub-DB-Link-List_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Nomenclature-Pub-DB-Link-List_w.sql
@@ -4,4 +4,11 @@ SELECT recattrib_data_zdb_id,
 FROM   record_attribution,
        db_link
 WHERE  recattrib_source_zdb_id = 'ZDB-PUB-030508-1'
-       AND recattrib_data_zdb_id = dblink_zdb_id;
+       AND recattrib_data_zdb_id = dblink_zdb_id
+UNION
+SELECT recattrib_data_zdb_id,
+       '',
+       ''
+FROM   record_attribution
+WHERE  recattrib_source_zdb_id = 'ZDB-PUB-030508-1'
+and recattrib_data_zdb_id like 'ZDB-ORTH%';
