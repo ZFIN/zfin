ecb1865407e0e9b7f6a55459b244bc6a8d0329ff
Kevin Schaper <kevin@zfin.org>
2015-11-10 18:34:09 -0800

    bugzid: 12988  Initial Solr5 commit, start and stop scripts aren't working, SolrJ isn't upgraded

--- server_apps/solr/solr.sh
+++ server_apps/solr/solr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SOLR_GC_LOGGING_OPTS="-verbose:gc -verbose:sizes -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -Xloggc:$SOLR_HOME/solr-gc.log"
-JETTY_OPTS="-Djetty.port=$SOLR_PORT -Djetty.home=$SOLR_HOME/jetty -Djetty.base=$SOLR_HOME/jetty"
-JVM_OPTS="-Xms4g -Xmx5g "
-SOLR_OPTS="-Dsolr.home=$SOLR_HOME -Denable.slave=false -Denable.master=true"
+#SOLR_GC_LOGGING_OPTS="-verbose:gc -verbose:sizes -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintTenuringDistribution -Xloggc:$SOLR_HOME/solr-gc.log"
+#JETTY_OPTS="-Djetty.port=$SOLR_PORT -Djetty.home=$SOLR_HOME/jetty -Djetty.base=$SOLR_HOME/jetty"
+#JVM_OPTS="-Xms4g -Xmx5g "
+#SOLR_OPTS="-Dsolr.home=$SOLR_HOME -Denable.slave=false -Denable.master=true"
 
 # solr seems to need to be a subdir of the working directory
 cd $SOLR_HOME/..
@@ -17,22 +17,22 @@ echo PID_FILE    $PID_FILE
 
 start_solr() {
 echo 'Starting Solr...'
-    nohup java $SOLR_GC_LOGGING_OPTS $JVM_OPTS -jar $SOLR_HOME/jetty/start.jar $JETTY_OPTS $SOLR_OPTS > $SOLR_HOME/solr.log 2>&1 & echo $! > $SOLR_HOME/solr.pid
-    echo pid: $(cat $PID_FILE)
+#    nohup java $SOLR_GC_LOGGING_OPTS $JVM_OPTS -jar $SOLR_HOME/jetty/start.jar $JETTY_OPTS $SOLR_OPTS > $SOLR_HOME/solr.log 2>&1 & echo $! > $SOLR_HOME/solr#.pid
+#    echo pid: $(cat $PID_FILE)
 }
 
 stop_solr() {
-	kill $(cat $PID_FILE)
-	if [ -e $PID_FILE ]
-		then rm $PID_FILE
-	fi
+#	kill $(cat $PID_FILE)
+#	if [ -e $PID_FILE ]
+#		then rm $PID_FILE
+#	fi
 }
 
 kill_solr() {
-	kill -9 $(cat $PID_FILE)
-	if [ -e $PID_FILE ]
-		then rm $PID_FILE
-	fi
+#	kill -9 $(cat $PID_FILE)
+#	if [ -e $PID_FILE ]
+#		then rm $PID_FILE
+#	fi
 }
 
 case $1 in
