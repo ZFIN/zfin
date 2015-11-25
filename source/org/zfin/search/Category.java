cf3b2d41bfca059ae4a3dcaf5ebeebe8f9ea976c
Kevin Schaper <kevin@zfin.org>
2015-11-24 16:13:10 -0800

    bugzid: 13692  updated expression category to use new eap base tables
    (cherry picked from commit 890ec730f0e0f8c819ab6c7b60569b32b03bebf6)

--- source/org/zfin/search/Category.java
+++ source/org/zfin/search/Category.java
@@ -131,6 +131,12 @@ public enum Category {
             FieldName.SEQUENCE_TARGETING_REAGENT,
             IS_WILDTYPE
     ),
+    EXPRESSION2("Expression2",
+            EXPRESSIONS_ANATOMY_TF,
+            STAGE,
+            FieldName.FISH,
+            ASSAY
+            ),
     PHENOTYPE("Phenotype",
             FieldName.GENE,
             SEQUENCE_ALTERATION,
