5a591f2b10fe3915c0075674b213f77085d779b4
Patrick Kalita <pkalita@zfin.org>
2015-11-30 10:44:45 -0800

    bugzid: 13698 Add alias methods to angular marker service

--- home/javascript/marker.service.js
+++ home/javascript/marker.service.js
@@ -1,4 +1,5 @@
-;(function() {
+;
+(function () {
     angular
         .module('app')
         .factory('MarkerService', MarkerService);
@@ -11,22 +12,45 @@
             removeSupplier: removeSupplier
         };
 
+        function returnResponseData(response) {
+            return response.data;
+        }
+
         function getSuppliers(markerId) {
             return $http.get('/action/marker/' + markerId + '/suppliers')
-                .then(function (response) {
-                    return response.data;
-                });
+                .then(returnResponseData);
         }
 
         function addSupplier(markerId, supplier) {
             return $http.post('/action/marker/' + markerId + '/suppliers', supplier)
-                .then(function (response) {
-                    return response.data;
-                });
+                .then(returnResponseData);
         }
 
         function removeSupplier(markerId, supplier) {
             return $http.delete('/action/marker/' + markerId + '/suppliers/' + supplier.zdbID);
+        }
+
+        function getAliases(markerId) {
+            return $http.get('/action/marker/' + markerId + '/aliases')
+                .then(returnResponseData);
+        }
+
+        function addAlias(markerId, name, pubId) {
+            var alias = {
+                alias: name,
+                references: [{zdbID: pubId}]
+            };
+            return $http.post('/action/marker/' + markerId + '/aliases', alias)
+                .then(returnResponseData);
+        }
+
+        function addAliasRerference(alias, pubId) {
+            return $http.post('/action/marker/alias/' + alias.zdbID + '/references', {zdbID: pubId})
+                .then(returnResponseData);
+        }
+
+        function removeAliasReference(alias, reference) {
+            return $http.delete('/action/marker/alias/' + alias.zdbID + '/references/' + reference.zdbID);
         }
     }
 }());
