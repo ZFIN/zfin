7c47e05d878455ec2ea49f6e0db63dbd9c88a720
Patrick Kalita <pkalita@zfin.org>
2015-12-02 09:02:14 -0800

    bugzid: 13698 Pull header out of bootstrap modal directive template Clean up alias edit modal layout

--- home/javascript/bootstrap-modal.directive.js
+++ home/javascript/bootstrap-modal.directive.js
@@ -4,16 +4,10 @@
         .directive('bootstrapModal', bootstrapModal);
 
     function bootstrapModal() {
-        // pushing the bounds of how much template to provide inline?
         var template =
             '<div class="modal fade" data-backdrop="static">' +
             '  <div class="modal-dialog">' +
-            '    <div class="modal-content">' +
-            '      <div class="modal-header">' +
-            '        <h4 class="modal-title">{{title}}</h4>' +
-            '      </div>' +
-            '      <div class="modal-body" ng-transclude></div>' +
-            '   </div>' +
+            '    <div class="modal-content" ng-transclude></div>' +
             '  </div>' +
             '</div>';
         var directive = {
