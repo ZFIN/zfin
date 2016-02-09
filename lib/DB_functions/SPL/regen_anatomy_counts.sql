b3705e813b79bc71d4de8bab6bd9e6f64f6f9f7b
Sierra Moxon <smoxon@zfin.org>
2016-02-08 16:38:00 -0800

    bugzid: 13909

--- lib/DB_functions/SPL/regen_anatomy_counts.sql
+++ lib/DB_functions/SPL/regen_anatomy_counts.sql
@@ -396,7 +396,7 @@ FROM   fish,
        all_term_contains
 WHERE  genox_fish_zdb_id = fish_zdb_id 
        AND pg_genox_zdb_id = genox_zdb_id 
-       AND psg_pg_pk_id = pg_id 
+       AND psg_pg_id = pg_id 
        AND alltermcon_container_zdb_id = term_zdb_id
        AND psg_e1a_zdb_id =alltermcon_contained_zdb_id 
        AND psg_tag != 'normal' 
