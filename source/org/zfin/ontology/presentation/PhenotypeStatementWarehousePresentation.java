50f20040fc9b27d16aec6cb4db55f60c6b1fbfd3
Patrick Kalita <pkalita@zfin.org>
2016-02-18 10:19:01 -0800

    bugzid: 13916 Add normal tag to phenotype statement link

--- source/org/zfin/ontology/presentation/PhenotypeStatementWarehousePresentation.java
+++ source/org/zfin/ontology/presentation/PhenotypeStatementWarehousePresentation.java
@@ -29,6 +29,7 @@ public class PhenotypeStatementWarehousePresentation extends TermPresentation {
         }
 
         StringBuilder phenotypeLink = new StringBuilder(100);
+        phenotypeLink.append(getNormalTagNote(phenotypeStatement));
         phenotypeLink.append(getTomcatLink(uri, String.valueOf(phenotypeStatement.getId()), getNameWithoutNormalText(phenotypeStatement)));
         if (!suppressPopupLink) {
             phenotypeLink.append(getPopupLink(phenotypeStatement));
