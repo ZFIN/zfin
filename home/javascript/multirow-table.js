94d78e0802eb2d92e9830b523478888b63cd4db8
Patrick Kalita <pkalita@zfin.org>
2016-02-25 15:22:23 -0800

    bugzid: 13644 Add multirowTable jQuery plugin

---
+++ home/javascript/multirow-table.js
@@ -0,0 +1,37 @@
+/* Duplicates form elements
+ */
+(function ($) {
+    $.fn.multirowTable = function (rowSelector, buttonLabel) {
+
+        var parent = this;
+
+        function replaceNumber(newNum) {
+            return function(idx, val) {
+                return val.replace('0', newNum);
+            };
+        }
+
+        function addRow(evt) {
+            evt.preventDefault();
+            var rows = parent.find(rowSelector);
+            var newRow = rows.first().clone();
+            newRow.find(':input')
+                .val('')
+                .attr('name', replaceNumber(rows.length))
+                .attr('id', replaceNumber(rows.length));
+            var separator = $('<hr>');
+            rows.last().after(separator);
+            separator.after(newRow);
+        }
+
+        $(function() {
+            parent.append(
+                $("<a href class=\"add-row\">")
+                    .click(addRow)
+                    .append("<i class=\"fa fa-plus-circle fa-lg\"></i> " + buttonLabel)
+            );
+        });
+
+        return this;
+    };
+})(jQuery);
