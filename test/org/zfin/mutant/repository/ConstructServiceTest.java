650bdb86c502a304788a59278d9a991b538c542b
Patrick Kalita <pkalita@zfin.org>
2016-03-08 09:39:17 -0800

    bugzid: 13791 Add test for finding EaP figures through construct search

--- test/org/zfin/mutant/repository/ConstructServiceTest.java
+++ test/org/zfin/mutant/repository/ConstructServiceTest.java
@@ -4,11 +4,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.zfin.AbstractDatabaseTest;
 import org.zfin.expression.presentation.FigureSummaryDisplay;
+import org.zfin.infrastructure.ZfinFigureEntity;
 import org.zfin.mutant.ConstructSearchCriteria;
+import org.zfin.mutant.ConstructSearchResult;
+import org.zfin.mutant.presentation.Construct;
 import org.zfin.mutant.presentation.ConstructSearchFormBean;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertNotNull;
 
 public class ConstructServiceTest extends AbstractDatabaseTest {
@@ -26,7 +34,32 @@ public class ConstructServiceTest extends AbstractDatabaseTest {
         assertNotNull(figureSummaryDisplays);
     }
 
+    @Test
+    public void getConstructShouldFindByEapTerm() {
+        ConstructSearchFormBean form = new ConstructSearchFormBean();
+        form.setAnatomyTermIDs("ZDB-TERM-100331-1023,");
+        form.setAnatomyTermNames("myotome|");
+        form.setAllTypes("allConstructs");
+        ConstructSearchCriteria criteria = new ConstructSearchCriteria(form);
+        ConstructSearchResult result = ConstructService.getConstruct(criteria);
+        assertThat(result, notNullValue());
 
+        List<Construct> results = result.getResults();
+        assertThat(results, notNullValue());
+        assertThat(results, not(empty()));
 
+        String eapFigId = "ZDB-FIG-140325-68";
+        assertThat(getAllExpressionFigureIds(results), hasItem(eapFigId));
+    }
+
+    private Collection<String> getAllExpressionFigureIds(List<Construct> constructs) {
+        Set<String> ids = new HashSet<>();
+        for (Construct construct : constructs) {
+            for (ZfinFigureEntity figure : construct.getExpressionFigures()) {
+                ids.add(figure.getID());
+            }
+        }
+        return ids;
+    }
 
 }
