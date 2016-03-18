84291601d01c8b6c80ca04f22ab67d0ad83b930a
Prita Mani <pm@zfin.org>
2016-03-18 12:24:21 -0700

    bugzid: 14024

--- source/org/zfin/marker/presentation/GeneViewController.java
+++ source/org/zfin/marker/presentation/GeneViewController.java
@@ -116,6 +116,8 @@ public class GeneViewController {
 
         geneBean.setPlasmidDBLinks(
                 markerRepository.getMarkerDBLinksFast(gene, DisplayGroup.GroupName.PLASMIDS));
+        geneBean.setPathwayDBLinks(
+                markerRepository.getMarkerDBLinksFast(gene, DisplayGroup.GroupName.PATHWAYS));
 
         // ORTHOLOGY
         geneBean.setOrthologyPresentationBean(MarkerService.getOrthologyEvidence(gene));
