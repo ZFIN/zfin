e103b0909405ecdf0803fd3797c92b2133eaf8b6
Sierra Moxon <smoxon@zfin.org>
2016-03-29 10:08:56 -0700

    bugzid: 13929 rearrange trigger for it tro respond to ATB changes.

--- lib/DB_triggers/marker_insert_trigger.sql
+++ lib/DB_triggers/marker_insert_trigger.sql
@@ -12,10 +12,10 @@ create trigger marker_insert_trigger insert on
     ) into marker.mrkr_name_order,
         execute function zero_pad(new_marker.mrkr_abbrev 
     ) into marker.mrkr_abbrev_order,
-        execute procedure p_check_mrkr_abbrev(new_marker.mrkr_name 
-    ,new_marker.mrkr_abbrev ,new_marker.mrkr_type ),
     execute function updateAbbrevEqualName (new_marker.mrkr_zdb_id, new_marker.mrkr_name, new_marker.mrkr_type, new_marker.mrkr_abbrev)
 	into marker.mrkr_abbrev,
+        execute procedure p_check_mrkr_abbrev(new_marker.mrkr_name 
+    ,new_marker.mrkr_abbrev ,new_marker.mrkr_type ),
         execute procedure mhist_event(new_marker.mrkr_zdb_id 
     ,'assigned' ,new_marker.mrkr_name ,new_marker.mrkr_abbrev ),
         --      ,
