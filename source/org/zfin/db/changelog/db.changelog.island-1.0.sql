06ed9eec399ab5c8bc7b9d84f12706834c114a46
Sierra Moxon <smoxon@zfin.org>
2016-05-09 07:26:15 -0700

    added octopus/pearl

--- source/org/zfin/db/changelog/db.changelog.island-1.0.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
-
---changeset pm:case-13474
-
-update  publication  set jtype='Thesis' where pub_jrnl_zdb_id='ZDB-JRNL-140407-2';
-update  publication  set jtype='Thesis' where pub_jrnl_zdb_id='ZDB-JRNL-140407-3';
-update  publication  set jtype='Thesis' where pub_jrnl_zdb_id='ZDB-JRNL-14527-1';
-update  publication  set jtype='Thesis' where pub_jrnl_zdb_id='ZDB-JRNL-140422-1';
