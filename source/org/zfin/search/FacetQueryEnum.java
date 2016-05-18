641b2a47613327c2c8b3137e0036afb259f51194
Patrick Kalita <pkalita@zfin.org>
2016-05-18 15:40:55 -0700

    bugzid: 14198 Use half-open intervals for relative date facets

--- source/org/zfin/search/FacetQueryEnum.java
+++ source/org/zfin/search/FacetQueryEnum.java
@@ -2,24 +2,21 @@ package org.zfin.search;
 
 import java.util.Calendar;
 
-/**
- * Created by kschaper on 9/10/14.
- */
 public enum FacetQueryEnum {
 
     DATE_LAST_30_DAYS(FieldName.DATE, "Last 30 Days", "date:[NOW/DAY-30DAYS TO NOW/DAY]"),
     DATE_LAST_90_DAYS(FieldName.DATE, "Last 90 Days", "date:[NOW/DAY-90DAYS TO NOW/DAY]"),
-    DATE_THIS_YEAR(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR)), "date:[NOW/YEAR+0YEAR TO NOW/YEAR+1YEAR]"),
-    DATE_THIS_YEAR_MINUS_1(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 1), "date:[NOW/YEAR-1YEAR TO NOW/YEAR+0YEAR]"),
-    DATE_THIS_YEAR_MINUS_2(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 2), "date:[NOW/YEAR-2YEAR TO NOW/YEAR-1YEAR]"),
-    DATE_THIS_YEAR_MINUS_3(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 3), "date:[NOW/YEAR-3YEAR TO NOW/YEAR-2YEAR]"),
-    DATE_THIS_YEAR_MINUS_4(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 4), "date:[NOW/YEAR-4YEAR TO NOW/YEAR-3YEAR]"),
-    DATE_THIS_YEAR_MINUS_5(FieldName.DATE,Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 5), "date:[NOW/YEAR-5YEAR TO NOW/YEAR-4YEAR]"),
-    DATE_MORE_THAN_5_YEARS(FieldName.DATE,"More than 5 years", "date:[* TO NOW/YEAR-5YEAR]"),
+    DATE_THIS_YEAR(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR)), "date:[NOW/YEAR+0YEAR TO NOW/YEAR+1YEAR}"),
+    DATE_THIS_YEAR_MINUS_1(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 1), "date:[NOW/YEAR-1YEAR TO NOW/YEAR+0YEAR}"),
+    DATE_THIS_YEAR_MINUS_2(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 2), "date:[NOW/YEAR-2YEAR TO NOW/YEAR-1YEAR}"),
+    DATE_THIS_YEAR_MINUS_3(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 3), "date:[NOW/YEAR-3YEAR TO NOW/YEAR-2YEAR}"),
+    DATE_THIS_YEAR_MINUS_4(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 4), "date:[NOW/YEAR-4YEAR TO NOW/YEAR-3YEAR}"),
+    DATE_THIS_YEAR_MINUS_5(FieldName.DATE, Integer.toString(Calendar.getInstance().get(Calendar.YEAR) - 5), "date:[NOW/YEAR-5YEAR TO NOW/YEAR-4YEAR}"),
+    DATE_MORE_THAN_5_YEARS(FieldName.DATE, "More than 5 years", "date:[* TO NOW/YEAR-5YEAR}"),
     ANY_WILDTYPE(FieldName.GENOTYPE, "Any WT", "is_genotype_wildtype:true"),
     ANY_MUTANT(FieldName.GENOTYPE, "Any Mutant", "is_genotype_wildtype:false"),
     ANY_ZEBRAFISH_GENE(FieldName.ZEBRAFISH_GENE, "Any Zebrafish Gene", "zebrafish_gene:[* TO *]"),
-    ANY_REPORTER_GENE(FieldName.REPORTER_GENE ,"Any Reporter Gene", "reporter_gene:[* TO *]");
+    ANY_REPORTER_GENE(FieldName.REPORTER_GENE, "Any Reporter Gene", "reporter_gene:[* TO *]");
 
     private String query;
     private String label;
@@ -32,11 +29,13 @@ public enum FacetQueryEnum {
     }
 
     public static FacetQueryEnum getFacetQueryEnum(String query) {
-        if (query == null)
+        if (query == null) {
             return null;
+        }
         for (FacetQueryEnum facetQueryEnum : values()) {
-            if (facetQueryEnum.getQuery().equals(query))
+            if (facetQueryEnum.getQuery().equals(query)) {
                 return facetQueryEnum;
+            }
         }
         return null;
     }
@@ -49,5 +48,7 @@ public enum FacetQueryEnum {
         return label;
     }
 
-    public FieldName getFieldName() { return fieldName; }
+    public FieldName getFieldName() {
+        return fieldName;
+    }
 }
