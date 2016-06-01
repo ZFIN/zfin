92cbbd05b2755619c9a7f642f84404564c40f29e
Patrick Kalita <pkalita@zfin.org>
2016-06-01 09:57:26 -0700

    Replace Europe PMC SOAP interface with call to REST service
    Version 4.5.1 of the EPMC API introduced a change which broke our SOAP implementation. It turned out
    to be easier to just replace the SOAP library with a call to their REST service rather than trying to
    generate a new JAR file.

--- test/org/zfin/datatransfer/doi/DOITest.java
+++ test/org/zfin/datatransfer/doi/DOITest.java
@@ -1,15 +1,10 @@
 package org.zfin.datatransfer.doi;
 
 import org.hibernate.Criteria;
-import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.zfin.AbstractDatabaseTest;
-import org.zfin.TestConfiguration;
 import org.zfin.datatransfer.webservice.Citexplore;
-import org.zfin.framework.HibernateSessionCreator;
 import org.zfin.framework.HibernateUtil;
 import org.zfin.publication.Publication;
 import org.zfin.repository.RepositoryFactory;
@@ -19,12 +14,14 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
 /**
  */
 public class DOITest extends AbstractDatabaseTest{
 
     private Citexplore citexplore = new Citexplore();
-
 
     /**
      * get 2 pubs with valid dois, set them to null and repopulate them using the service class
@@ -32,34 +29,39 @@ public class DOITest extends AbstractDatabaseTest{
     @Test
     public void testDOIConnectivity(){
         HibernateUtil.createTransaction();
-        List<Publication> pubs = new ArrayList<Publication>();
-        Criteria crit = HibernateUtil.currentSession().createCriteria(Publication.class) ;
-        crit.setMaxResults(2);
-        crit.add(Restrictions.isNotNull("doi"));
-        List<Publication> publications = (List<Publication>) crit.list();
-        assertEquals(2,publications.size());
-        Publication pub1 = publications.get(0);
-        Publication pub2 = publications.get(1);
-        pub1.setDoi(null);
-        pubs.add(pub1) ;
-        pub2.setDoi(null);
-        pubs.add(pub2) ;
-//        List<Publication> pubs = publicationRepository.getPublicationsWithAccessionButNoDOI(2) ;
-        citexplore.getDoisForPubmedID(pubs) ;
+        try {
+            List<Publication> pubs = new ArrayList<>();
+            Criteria crit = HibernateUtil.currentSession().createCriteria(Publication.class);
+            crit.setMaxResults(2);
+            crit.add(Restrictions.isNotNull("doi"));
+            List<Publication> publications = (List<Publication>) crit.list();
+            assertThat(publications, hasSize(2));
 
-        assertNotNull(pub1.getDoi()) ;
-        assertNotNull(pub2.getDoi()) ;
-        HibernateUtil.rollbackTransaction();
+            Publication pub1 = publications.get(0);
+            Publication pub2 = publications.get(1);
+            pub1.setDoi(null);
+            pubs.add(pub1);
+            pub2.setDoi(null);
+            pubs.add(pub2);
+            citexplore.getDoisForPubmedID(pubs);
+
+            assertThat(pub1.getDoi(), notNullValue());
+            assertThat(pub2.getDoi(), notNullValue());
+        } finally {
+            HibernateUtil.rollbackTransaction();
+        }
 
     }
 
     @Test
     public void testValidPub(){
-        List<Publication> pubs = new ArrayList<Publication>();
+        List<Publication> pubs = new ArrayList<>();
         Publication p = (Publication) HibernateUtil.currentSession().get(Publication.class,"ZDB-PUB-101122-23") ;
         pubs.add(p);
         pubs = citexplore.getDoisForPubmedID(pubs) ;
-        assertEquals("10.1095/biolreprod.110.086363",pubs.get(0).getDoi());
+        assertThat(pubs, notNullValue());
+        assertThat(pubs, not(empty()));
+        assertThat(pubs.get(0).getDoi(), is("10.1095/biolreprod.110.086363"));
     }
 
 
@@ -76,37 +78,37 @@ public class DOITest extends AbstractDatabaseTest{
             " update DOIAttempt da set da.numAttempts = 0 " +
             "").executeUpdate() ;
             publications = RepositoryFactory.getPublicationRepository().getPublicationsWithAccessionButNoDOIAndLessAttempts(maxAttempts,maxResults);
-            assertNotNull(publications);
-            assertEquals(maxResults,publications.size());
+            assertThat(publications, notNullValue());
+            assertThat(publications, hasSize(maxResults));
+
             String pubZdbID1 = publications.get(0).getZdbID() ;
             publications = RepositoryFactory.getPublicationRepository().getPublicationsWithAccessionButNoDOIAndLessAttempts(maxAttempts,maxResults);
-            assertNotNull(publications);
-            assertEquals(maxResults,publications.size());
+            assertThat(publications, notNullValue());
+            assertThat(publications, hasSize(maxResults));
             String pubZdbID2 = publications.get(0).getZdbID() ;
-            assertEquals(pubZdbID1,pubZdbID2);
+            assertThat(pubZdbID1, is(pubZdbID2));
 
             RepositoryFactory.getPublicationRepository().addDOIAttempts(publications) ;
             publications = RepositoryFactory.getPublicationRepository(). getPublicationsWithAccessionButNoDOIAndLessAttempts(maxAttempts,maxResults);
-            assertNotNull(publications);
-            assertEquals(maxResults,publications.size());
+            assertThat(publications, notNullValue());
+            assertThat(publications, hasSize(maxResults));
             pubZdbID2 = publications.get(0).getZdbID() ;
-            assertEquals(pubZdbID1,pubZdbID2);
-
+            assertThat(pubZdbID1, is(pubZdbID2));
 
             RepositoryFactory.getPublicationRepository().addDOIAttempts(publications) ;
             publications = RepositoryFactory.getPublicationRepository(). getPublicationsWithAccessionButNoDOIAndLessAttempts(maxAttempts,maxResults);
-            assertNotNull(publications);
-            assertEquals(maxResults,publications.size());
+            assertThat(publications, notNullValue());
+            assertThat(publications, hasSize(maxResults));
             pubZdbID2 = publications.get(0).getZdbID() ;
-            assertEquals(pubZdbID1,pubZdbID2);
+            assertThat(pubZdbID1, is(pubZdbID2));
 
             RepositoryFactory.getPublicationRepository().addDOIAttempts(publications) ;
             HibernateUtil.currentSession().flush();
             publications = RepositoryFactory.getPublicationRepository(). getPublicationsWithAccessionButNoDOIAndLessAttempts(maxAttempts,maxResults);
-            assertNotNull(publications);
-            assertEquals(maxResults,publications.size());
+            assertThat(publications, notNullValue());
+            assertThat(publications, hasSize(maxResults));
             pubZdbID2 = publications.get(0).getZdbID() ;
-            assertFalse(pubZdbID1.equals(pubZdbID2));
+            assertThat(pubZdbID1, is(not(pubZdbID2)));
         } finally {
             HibernateUtil.rollbackTransaction();
         }
