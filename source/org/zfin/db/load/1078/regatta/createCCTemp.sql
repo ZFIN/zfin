9fd49c496e38534e1294302813d0ceb16d36e685
Sierra Moxon <smoxon@zfin.org>
2016-06-06 11:57:49 -0700

    fixing character conversion error.

--- source/org/zfin/db/load/1078/regatta/createCCTemp.sql
+++ source/org/zfin/db/load/1078/regatta/createCCTemp.sql
@@ -6,7 +6,7 @@ create table cc_temp (ccid varchar(50),
 		      cccategory varchar(50),
 		      compid varchar(50),
 		      cccomp varchar(50),
-		      cccasset varchar(50),
+		      cccasset int,
 		      ccorder int) 
 in tbldbs1
 extent size 16 next size 16;
