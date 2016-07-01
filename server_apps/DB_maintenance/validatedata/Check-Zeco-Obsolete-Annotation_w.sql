ae1c071f237f0b1208fd2344e8441947c69652d5
Christian Pich <cmpich@zfin.org>
2016-07-01 15:32:46 -0700

    bugzID: 14311 create jenkins report for obsoleted terms used in experiment_conditions

---
+++ server_apps/DB_maintenance/validatedata/Check-Zeco-Obsolete-Annotation_w.sql
@@ -0,0 +1,34 @@
+SELECT expcond_zdb_id,
+       term_name
+FROM   experiment_condition,
+       term
+WHERE  expcond_zeco_term_zdb_id= term_zdb_id
+       AND term_is_obsolete = "t"
+UNION
+SELECT expcond_zdb_id,
+       term_name
+FROM   experiment_condition,
+       term
+WHERE  expcond_chebi_zdb_id= term_zdb_id
+       AND term_is_obsolete = "t"
+UNION
+SELECT expcond_zdb_id,
+       term_name
+FROM   experiment_condition,
+       term
+WHERE  expcond_taxon_term_zdb_id= term_zdb_id
+       AND term_is_obsolete = "t"
+UNION
+SELECT expcond_zdb_id,
+       term_name
+FROM   experiment_condition,
+       term
+WHERE  expcond_ao_term_zdb_id= term_zdb_id
+       AND term_is_obsolete = "t"
+UNION
+SELECT expcond_zdb_id,
+       term_name
+FROM   experiment_condition,
+       term
+WHERE  expcond_go_cc_term_zdb_id= term_zdb_id
+       AND term_is_obsolete = "t"
