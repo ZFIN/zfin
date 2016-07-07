3eafdf28a5720f256152c23c3fafe6408fb93ec7
Sierra Moxon <smoxon@zfin.org>
2016-07-07 10:22:07 -0700

    bugzid: 14348

--- source/org/zfin/db/postGmakePostloaddb/1079/titanic/convertConditions.sql
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/convertConditions.sql
@@ -5,12 +5,26 @@ update tmp_gap_tt
   set cztId = 'NCBITaxon:'||cztId
  where cztId not like 'CHEBI%'
  and cztId not like 'ZFA%'
- and cztId not like 'GO%';
+ and cztId not like 'GO%'
+ and cztId not like 'none';
+
+select count(*) as counter, expcondId as id from tmp_gap_tt
+ group by expcondId 
+having count(*) > 1
+ into temp tmp_dups;
+
+delete from tmp_gap_tt
+ where exists (Select 'x' from tmp_dups
+       	      	      where id = expcondId);
+
+delete from tmp_gap_tt
+ where expcondId is null or expcondId = '';
 
 update experiment_condition
   set expcond_zeco_term_Zdb_id = (Select term_zdb_id from tmp_gap_tt, term
       			       	 	 where expcond_zdb_id = expcondId
-					 and trim(zecoId) = term_ont_id)
+					 and trim(zecoId) = term_ont_id
+					 )
  where (expcond_zeco_term_Zdb_id is null
        				 or expcond_zeco_term_zdb_id = ''); 
 
