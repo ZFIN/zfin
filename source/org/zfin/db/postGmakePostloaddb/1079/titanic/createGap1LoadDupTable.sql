e1b06e72198acfa0fbf831ab34bf927dca91657a
Sierra Moxon <smoxon@zfin.org>
2016-07-07 11:37:09 -0700

    bugzid: 14349

---
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/createGap1LoadDupTable.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset sierra:createGap1LoadTable
+create table tmp_gap_dup_tt (expcondId varchar(50),
+       	     			   expId varchar(50),
+				   cdtId varchar(50),
+				   zecoId varchar(50),
+				   cztId varchar(50))
+in tbldbs1
+extent size 2048 next size 2048;
+
---
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/splitConditions.sql
@@ -0,0 +1,71 @@
+--liquibase formatted sql
+--changeset sierra:convertConditions
+
+update tmp_gap_dup_tt
+  set cztId = 'NCBITaxon:'||cztId
+ where cztId not like 'CHEBI%'
+ and cztId not like 'ZFA%'
+ and cztId not like 'GO%'
+ and cztId not like 'none';
+
+update tmp_gap_dup_tt
+ set expcondId = get_id('EXPCOND')
+ where expcondId = 'none';
+
+insert into zdb_active_data
+ select expcondId from tmp_gap_dup_tt
+ where not exists (Select 'x' from zdb_active_data
+       	   	  	      where zactvd_zdb_id = expcondId);
+
+select count(*) as counter, expcondId as id from tmp_gap_dup_tt
+ group by expcondId 
+having count(*) > 1
+ into temp tmp_dups;
+
+delete from tmp_gap_dup_tt
+ where exists (Select 'x' from tmp_dups
+       	      	      where id = expcondId);
+
+delete from tmp_gap_dup_tt
+ where expcondId is null or expcondId = '';
+
+insert into experiment_condition (expcond_zdb_id, 
+       	    			  expcond_exp_zdb_id)
+ select expcondId, expId
+   from tmp_gap_dup_tt
+ where not exists (Select 'x' from experiment_condition
+       	   	  	  where expcond_zdb_id = expcondId);
+
+update experiment_condition
+  set expcond_zeco_term_Zdb_id = (Select term_zdb_id from tmp_gap_dup_tt, term
+      			       	 	 where expcond_zdb_id = expcondId
+					 and trim(zecoId) = term_ont_id
+					 )
+ where (expcond_zeco_term_Zdb_id is null
+       				 or expcond_zeco_term_zdb_id = ''); 
+
+
+
+update experiment_condition
+  set expcond_chebi_term_Zdb_id = (Select term_zdb_id from tmp_gap_dup_tt, term
+      			       	 	 where expcond_zdb_id = expcondId
+					 and trim(cztId) = term_ont_id
+					 and cztId like 'CHEBI%')
+ where (expcond_chebi_term_Zdb_id is null
+       				 or expcond_chebi_term_zdb_id = ''); 
+
+update experiment_condition
+  set expcond_ao_term_Zdb_id = (Select term_zdb_id from tmp_gap_dup_tt, term
+      			       	 	 where expcond_zdb_id = expcondId
+					 and trim(cztId) = term_ont_id
+					 and cztId like 'ZFA%')
+ where (expcond_ao_term_Zdb_id is null
+       				 or expcond_ao_term_zdb_id = ''); 
+
+update experiment_condition
+  set expcond_taxon_term_Zdb_id = (Select term_zdb_id from tmp_gap_dup_tt, term
+      			       	 	 where expcond_zdb_id = expcondId
+					 and trim(cztId) = term_ont_id
+					 and cztId like 'NCBI%')
+ where (expcond_taxon_term_Zdb_id is null
+       				 or expcond_taxon_term_zdb_id = ''); 
