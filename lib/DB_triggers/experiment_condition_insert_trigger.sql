1d8182caf9351e25878b223c4245d7f24dcb15c5
Sierra Moxon <smoxon@zfin.org>
2016-07-07 11:41:19 -0700

    removing experiment_condition trigger

--- lib/DB_triggers/experiment_condition_insert_trigger.sql
+++
@@ -1,7 +0,0 @@
-create trigger experiment_condition_insert_trigger
-  insert on experiment_condition
-  referencing new as new_expcond
-  for each row (
-	execute function scrub_char(new_expcond.expcond_comments) 
-		into expcond_comments
-) ;
--- lib/DB_triggers/experiment_condition_update_trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger experiment_condition_update_trigger
-   update of expcond_comments 
-   on experiment_condition
-   referencing new as new_expcond
-   for each row 
-	(
-	execute function scrub_char(new_expcond.expcond_comments) 
-		into expcond_comments
-) ;
