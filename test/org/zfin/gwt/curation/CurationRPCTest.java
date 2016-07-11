71d685625c6ea493372ec2ebea52658eaa749ff0
Prita Mani <pm@zfin.org>
2016-07-11 14:13:21 -0700

    bugzid: 14327

--- test/org/zfin/gwt/curation/CurationRPCTest.java
+++ test/org/zfin/gwt/curation/CurationRPCTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import org.zfin.AbstractDatabaseTest;
 import org.zfin.gwt.curation.server.CurationExperimentRPCImpl;
 import org.zfin.gwt.curation.ui.CurationExperimentRPC;
-import org.zfin.gwt.root.dto.ExperimentDTO;
+import org.zfin.gwt.root.dto.ExpressionExperimentDTO;
 
 import java.util.List;
 
@@ -19,9 +19,9 @@ public class CurationRPCTest extends AbstractDatabaseTest{
     public void getExperimentsByFilter() {
         // dazed gene is necessary for late cell type development ...
         String pubID = "ZDB-PUB-050422-7";
-        ExperimentDTO experimentFilter = new ExperimentDTO();
+        ExpressionExperimentDTO experimentFilter = new ExpressionExperimentDTO();
         experimentFilter.setPublicationID(pubID);
-        List<ExperimentDTO> experiments = curationRpc.getExperimentsByFilter(experimentFilter);
+        List<ExpressionExperimentDTO> experiments = curationRpc.getExperimentsByFilter(experimentFilter);
         assertNotNull(experiments);
     }
 
