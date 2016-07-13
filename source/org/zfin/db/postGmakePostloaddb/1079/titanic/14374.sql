2c6ae04c2a74a6a83965c4b4f60ae5e05d30aba5
Sierra Moxon <smoxon@zfin.org>
2016-07-13 11:55:27 -0700

    checking in changelog issues

--- source/org/zfin/db/postGmakePostloaddb/1079/titanic/14374.sql
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/14374.sql
@@ -1,8 +1,7 @@
-begin work;
+--liquibase formatted sql
+--changeset pm:14374
+
 update construct_component set cc_component_zdb_id='ZDB-GENE-030613-1' where cc_construct_zdb_id='ZDB-TGCONSTRCT-150828-5' and cc_component_zdb_id='ZDB-GENE-000210-20';
 update construct_component set cc_component='dusp6' where cc_construct_zdb_id='ZDB-TGCONSTRCT-150828-5' and cc_component='cat';
 update construct_marker_relationship set conmrkrrel_mrkr_zdb_id='ZDB-GENE-030613-1' where conmrkrrel_construct_zdb_id='ZDB-TGCONSTRCT-150828-5' and conmrkrrel_relationship_type='coding sequence of';
 update marker_relationship set mrel_mrkr_2_zdb_id='ZDB-GENE-030613-1' where mrel_mrkr_1_zdb_id='ZDB-TGCONSTRCT-150828-5' and mrel_type='coding sequence of';
-
---rollback work;
-commit work;
--- source/org/zfin/db/postGmakePostloaddb/1079/titanic/createGap1LoadTable.sql
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/createGap1LoadTable.sql
@@ -3,8 +3,7 @@
 create table tmp_gap_tt (expcondId varchar(50),
 				   zecoId varchar(50),
 				   goccId varchar(50),
-				   cztId varchar(50),
-				   extra varchar(2))
+				   cztId varchar(50))
 in tbldbs1
 extent size 2048 next size 2048;
 
