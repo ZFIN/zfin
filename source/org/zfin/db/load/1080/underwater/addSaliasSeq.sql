dbac9abcbfe1d9894fadc6bff5b0947d4f7afdfd
Sierra Moxon <smoxon@zfin.org>
2016-07-22 14:57:19 -0700

    fixing seq

--- source/org/zfin/db/load/1080/underwater/addSaliasSeq.sql
+++ source/org/zfin/db/load/1080/underwater/addSaliasSeq.sql
@@ -1,6 +1,4 @@
 --liquibase formatted sql
 --changeset sierra:addSaliasSeq
 
-create sequence salias_seq increment by 1 start with 1 maxvalue 9223372036854775807 minvalue 1 cache 20  order;
 
-alter sequence salias_seq restart with 1;
