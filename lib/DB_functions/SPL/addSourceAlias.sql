1df942d790bdf016e95be2921ff94a0522a91c7e
Sierra Moxon <smoxon@zfin.org>
2016-08-03 15:33:35 -0700

    add source alias pro

---
+++ lib/DB_functions/SPL/addSourceAlias.sql
@@ -0,0 +1,17 @@
+create procedure addSourceAlias (vJournalZdbId varchar(50), vOldJournalVaule varchar(255))
+
+       define zdbId varchar(50);
+       let zdbId = get_id('SALIAS');
+
+       insert into zdb_Active_source
+          values(zdbId);
+
+       insert into source_alias (salias_Zdb_id, salias_source_zdb_id,
+       	      	   				salias_alias)
+          select zdbId, vJournalZdbId, vOldJournalValue
+	  	 from single
+		 where not exists (Select 'x' from source_alias
+		       	   	  	  where salias_source_zdb_id = vJournalZdbId
+					  and salias_alias = vOldJournalValue);
+       
+end procedure;
