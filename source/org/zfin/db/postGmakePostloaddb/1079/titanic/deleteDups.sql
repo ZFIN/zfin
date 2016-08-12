77145ef23abba4c7d7d61342f3237d4515a2d62f
Sierra Moxon <smoxon@zfin.org>
2016-08-12 10:20:45 -0700

    deleting more duplicates

--- source/org/zfin/db/postGmakePostloaddb/1079/titanic/deleteDups.sql
+++ source/org/zfin/db/postGmakePostloaddb/1079/titanic/deleteDups.sql
@@ -1,13 +1,40 @@
 --liquibase formatted sql
 --changeset sierra:deleteDups
 
-delete from experiment_condition
- where expcond_zdb_id in ('ZDB-EXPCOND-160729-5',
-'ZDB-EXPCOND-160729-6',
-'ZDB-EXPCOND-160729-4',
-'ZDB-EXPCOND-160729-28',
-'ZDB-EXPCOND-160729-44',
-'ZDB-EXPCOND-160729-43',
-'ZDB-EXPCOND-160729-45',
-'ZDB-EXPCOND-160729-12',
-'ZDB-EXPCOND-160729-13');
+create temp table tmp_dups (counter int,
+				exp_zdb_id varchar(50), 
+       	    	  	   			zeco_term_zdb_id varchar(50),
+     			 		      ao_term_zdb_id varchar(50),
+					      go_cc_term_zdb_id varchar(50),
+					      chebi_term_zdb_id varchar(50),
+					      taxon_term_zdb_id varchar(50))
+with no log;
+
+insert into tmp_dups 
+  select count(*) as counter, expcond_exp_zdb_id , expcond_zeco_term_zdb_id,
+     			 		      expcond_ao_term_zdb_id,
+					      expcond_go_cc_term_zdb_id,
+					      expcond_chebi_term_zdb_id,
+					      expcond_taxon_term_zdb_id
+  from experiment_condition
+ group by expcond_exp_zdb_id, expcond_zeco_term_zdb_id,
+     			 		      expcond_ao_term_zdb_id,
+					      expcond_go_cc_term_zdb_id,
+					      expcond_chebi_term_zdb_id,
+					      expcond_taxon_term_zdb_id
+having count(*) > 1;
+
+select first 1 * from tmp_dups;
+
+Select min(b.expcond_zdb_id) as miner
+  	       	       from experiment_condition b, tmp_dups
+		       where b.expcond_exp_zdb_id = exp_zdb_id
+		       and b.expcond_zeco_term_zdb_id = zeco_term_zdb_id
+		       and b.expcond_ao_term_zdb_id = ao_Term_zdb_id
+		       and b.expcond_go_cc_term_zdb_id = go_Cc_term_zdb_id
+		       and b.expcond_chebi_Term_zdb_id =chebi_Term_zdb_id
+		       and b.expcond_taxon_Term_zdb_id = taxon_term_zdb_id
+into temp tmp_delete;
+
+delete from experiment_condition 
+where expcond_zdb_id in (Select miner from tmp_delete);
