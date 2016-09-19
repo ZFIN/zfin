0386aa34c31af295f559b08266780241f79a0bf1
Sierra Moxon <smoxon@zfin.org>
2016-09-19 09:38:42 -0700

    bugzid: 14557

--- lib/DB_triggers/mrkr_abbrev_update_trigger.sql
+++ lib/DB_triggers/mrkr_abbrev_update_trigger.sql
@@ -9,8 +9,9 @@ create trigger mrkr_abbrev_update_trigger update of
     ,newm.mrkr_abbrev ,newm.mrkr_type ),
         execute function zero_pad(newm.mrkr_abbrev 
     ) into marker.mrkr_abbrev_order,
-        execute procedure mhist_event(newm.mrkr_zdb_id 
-    ,'reassigned' ,newm.mrkr_abbrev ,oldm.mrkr_abbrev ),
+        execute procedure mhist_event(newm.mrkr_zdb_id,oldm.mrkr_name,
+					newm.mrkr_name, oldm.mrkr_abbrev, 
+					newm.mrkr_abbrev ),
         execute procedure p_update_related_names(newm.mrkr_zdb_id 
     ,oldm.mrkr_abbrev ,newm.mrkr_abbrev ),
     execute procedure update_construct_name_component(newm.mrkr_zdb_id, newm.mrkr_abbrev),
