9ce68a3f123bb9ce60356f7f2e32be9027c85722
Sierra Moxon <smoxon@zfin.org>
2016-09-22 11:36:15 -0700

    update marker name trigger as well as mrkr abbrev trigger for markers where mrkr_abbrev = mrkr_name

--- lib/DB_triggers/mrkr_name_update_trigger.sql
+++ lib/DB_triggers/mrkr_name_update_trigger.sql
@@ -12,5 +12,6 @@ create trigger mrkr_name_update_trigger update of
         execute function zero_pad(newm.mrkr_name ) 
     into marker.mrkr_name_order,
     execute procedure p_update_related_fish_names(newm.mrkr_zdb_id),
-        execute procedure mhist_event(newm.mrkr_zdb_id 
-    ,'renamed' ,newm.mrkr_name ,oldm.mrkr_name ));
+        execute procedure mhist_event(newm.mrkr_zdb_id,oldm.mrkr_name,
+					newm.mrkr_name, oldm.mrkr_abbrev, 
+					newm.mrkr_abbrev ));
