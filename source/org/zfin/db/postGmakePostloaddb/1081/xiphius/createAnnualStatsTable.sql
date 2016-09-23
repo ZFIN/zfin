2782f64bef4954dbfb4469f15bd484b172202bb3
Sierra Moxon <smoxon@zfin.org>
2016-09-23 09:56:44 -0700

    bugzid: 14548

--- source/org/zfin/db/postGmakePostloaddb/1081/xiphius/createAnnualStatsTable.sql
+++ source/org/zfin/db/postGmakePostloaddb/1081/xiphius/createAnnualStatsTable.sql
@@ -3,18 +3,15 @@
 
 create table annual_stats (as_pk_id serial8 not null constraint as_pk_id_not_null,
        	     		   as_count int not null constraint as_count_not_null,
-			   as_section varchar(100) not null constraint as_section_not_null,
-			   as_date datetime year to second default current year to second as_date_not_null)
+			   as_section varchar(255) not null constraint as_section_not_null,
+			   as_type varchar(255) not null constraint as_type_not_null,
+			   as_date datetime year to second not null constraint as_year_not_null)
 in tbldbs1
 extent size 32 next size 32;
 
 create unique index annual_stats_primary_key_index
  on annual_stats(as_pk_id)
-using btree in idxdsb2;
-
-create unique index annual_stats_section_index
- on annual_stats(as_section)
- using btree in idxdbs1;
+using btree in idxdbs2;
 
 alter table annual_stats 
   add constraint primary key (as_pk_id)
