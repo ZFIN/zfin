a16bd63d68574a33229cae322d6af8c4a0029f2d
Patrick Kalita <pkalita@zfin.org>
2016-09-23 11:24:43 -0700

    bugzid: 14426 Add basic uploading via pub edit interface

--- source/org/zfin/figure/service/ImageService.java
+++ source/org/zfin/figure/service/ImageService.java
@@ -7,12 +7,12 @@ import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.log4j.Logger;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.web.multipart.MultipartFile;
 import org.zfin.expression.Figure;
 import org.zfin.expression.Image;
 import org.zfin.framework.HibernateUtil;
 import org.zfin.profile.Person;
 import org.zfin.properties.ZfinPropertiesEnum;
-import org.zfin.repository.RepositoryFactory;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -23,55 +23,26 @@ public class ImageService {
 
     public static Logger log = Logger.getLogger(ImageService.class);
 
-    public static Image processImage(Figure figure, String filePath, Boolean isVideoStill,String direction) throws IOException {
+    private final static File IMAGE_LOADUP_DIR = new File(ZfinPropertiesEnum.LOADUP_FULL_PATH.toString(), ZfinPropertiesEnum.IMAGE_LOAD.toString());
+    private final static String THUMB = "_thumb";
 
-        File imageLoadUpDir = new File(ZfinPropertiesEnum.LOADUP_FULL_PATH.toString(), ZfinPropertiesEnum.IMAGE_LOAD.toString());
+    public static Image processImage(Figure figure, MultipartFile file, Person owner) throws IOException {
+        CopyAction<MultipartFile> copier = MultipartFile::transferTo;
+        return processImage(figure, owner, false, file.getOriginalFilename(), file, copier);
+    }
 
-        //create an initial image record, file paths and width/height will be wrong
-
+    public static Image processImage(Figure figure, String filePath, Boolean isVideoStill, String direction) throws IOException {
+        // This method was made for the original Dorsky load, so it has a hard-coded owner
         Person owner = (Person) HibernateUtil.currentSession().createCriteria(Person.class)
                 .add(Restrictions.eq("zdbID", "ZDB-PERS-030520-2"))  //Yvonne
                 .uniqueResult();
-        Image image = createPlaceholderImage(figure, owner, isVideoStill,direction);
-
-        //get the image file
         File initialFile = new File(filePath);
-        String extension = FilenameUtils.getExtension(initialFile.getName());
-        String destinationBasename = image.getZdbID();
-        String destinationFilename = destinationBasename + FilenameUtils.EXTENSION_SEPARATOR + extension;
-        String thumbnailFilename = destinationBasename + "_thumb" + FilenameUtils.EXTENSION_SEPARATOR + extension;
-        File destinationFile = new File(imageLoadUpDir, destinationFilename);
-        File thumbnailFile = new File(imageLoadUpDir, thumbnailFilename);
-
-        FileUtils.copyFile(initialFile, destinationFile);
-
-        image.setImageFilename(destinationFilename);
-        image.setThumbnail(thumbnailFilename);
-
-        BufferedImage bimg = ImageIO.read(destinationFile);
-
-        image.setWidth(bimg.getWidth());
-        image.setHeight(bimg.getHeight());
-        HibernateUtil.currentSession().save(image);
-
-        File scriptDirectory = new File(ZfinPropertiesEnum.TARGETROOT + "/server_apps/sysexecs/make_thumbnail");
-        CommandLine makeThumbnail = new CommandLine("./make_thumbnail.sh");
-        makeThumbnail.addArgument(destinationFile.getAbsolutePath());
-        makeThumbnail.addArgument(thumbnailFile.getAbsolutePath());
-        DefaultExecutor executor = new DefaultExecutor();
-        executor.setWorkingDirectory(scriptDirectory);
-        executor.execute(makeThumbnail);
-
-        return image;
+        CopyAction<File> copier = FileUtils::copyFile;
+        return processImage(figure, owner, isVideoStill, initialFile.getName(), initialFile, copier);
     }
 
-    /*
-
-     */
-    private static Image createPlaceholderImage(Figure figure, Person owner, Boolean isVideoStill,String direction) {
-
+    private static Image createPlaceholderImage(Figure figure, Person owner, Boolean isVideoStill) {
         Image image = new Image();
-
         image.setFigure(figure);
         image.setWidth(-1);
         image.setHeight(-1);
@@ -85,11 +56,44 @@ public class ImageService {
         image.setThumbnail("tmpvalue");
         image.setVideoStill(isVideoStill);
         HibernateUtil.currentSession().save(image);
-
         figure.addImage(image);
-        //HibernateUtil.currentSession().save(figure);
+        return image;
+    }
+
+    private static <T> Image processImage(Figure figure, Person owner, Boolean isVideoStill, String fileName, T originalImage, CopyAction<T> copyAction) throws IOException {
+        Image image = createPlaceholderImage(figure, owner, isVideoStill);
+
+        String extension = FilenameUtils.getExtension(fileName);
+        String destinationBasename = image.getZdbID();
+        String destinationFilename = destinationBasename + FilenameUtils.EXTENSION_SEPARATOR + extension;
+        String thumbnailFilename = destinationBasename + THUMB + FilenameUtils.EXTENSION_SEPARATOR + extension;
+        File destinationFile = new File(IMAGE_LOADUP_DIR, destinationFilename);
+        File thumbnailFile = new File(IMAGE_LOADUP_DIR, thumbnailFilename);
+
+        copyAction.copy(originalImage, destinationFile);
+
+        image.setImageFilename(destinationFilename);
+        image.setThumbnail(thumbnailFilename);
+
+        BufferedImage imageData = ImageIO.read(destinationFile);
+        image.setWidth(imageData.getWidth());
+        image.setHeight(imageData.getHeight());
+        HibernateUtil.currentSession().save(image);
+
+        File scriptDirectory = new File(ZfinPropertiesEnum.TARGETROOT + "/server_apps/sysexecs/make_thumbnail");
+        CommandLine makeThumbnail = new CommandLine("./make_thumbnail.sh");
+        makeThumbnail.addArgument(destinationFile.getAbsolutePath());
+        makeThumbnail.addArgument(thumbnailFile.getAbsolutePath());
+        DefaultExecutor executor = new DefaultExecutor();
+        executor.setWorkingDirectory(scriptDirectory);
+        executor.execute(makeThumbnail);
 
         return image;
+    }
+
+    @FunctionalInterface
+    private interface CopyAction<T> {
+        void copy(T t, File destination) throws IOException;
     }
 
 }
