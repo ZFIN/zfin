a1e162612af79c3ad8672dbc958d369c16d7b560
Sierra Moxon <smoxon@zfin.org>
2016-09-29 09:25:06 -0700

    fixing source alias triggers

---
+++ lib/DB_triggers/source_alias_insert_trigger.sql
@@ -0,0 +1,9 @@
+create trigger source_alias_insert_trigger insert 
+    on source_alias referencing new as new_source_alias
+    
+    for each row
+        (
+        execute function scrub_char(new_source_alias.salias_alias 
+    ) into source_alias.salias_alias,
+        execute function lower(new_source_alias.salias_alias 
+    ) into source_alias.salias_alias_lower);
