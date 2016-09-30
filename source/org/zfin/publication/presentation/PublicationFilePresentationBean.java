401d44979609aa7e34bc7bbe08f56aff982dce8d
Patrick Kalita <pkalita@zfin.org>
2016-09-30 14:01:28 -0700

    bugzid: 14613 Add missing file

---
+++ source/org/zfin/publication/presentation/PublicationFilePresentationBean.java
@@ -0,0 +1,41 @@
+package org.zfin.publication.presentation;
+
+public class PublicationFilePresentationBean {
+
+    private String pubZdbId;
+    private String type;
+    private String fileName;
+    private String description;
+
+    public String getPubZdbId() {
+        return pubZdbId;
+    }
+
+    public void setPubZdbId(String pubZdbId) {
+        this.pubZdbId = pubZdbId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
