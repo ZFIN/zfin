4eca4fed922513d87434ee9fcb3f6bc085785647
Patrick Kalita <pkalita@zfin.org>
2016-10-03 11:10:36 -0700

    bugzid: 14593 Check for file existence before doing copy

--- server_apps/DB_maintenance/movefiles.py
+++ server_apps/DB_maintenance/movefiles.py
@@ -3,25 +3,30 @@
 import re
 import os
 import shutil
-files = os.listdir("<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->")
 
-output = {}
-path = '<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->'
-origPath = '<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->'
+pdf_dir = '<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->'
 
-for f in files:
-        print f;
-        m = re.match("^(ZDB-PUB-)(\d{2})(\d{2})(\d{2})(-\d+)(\.pdf)$", f)
-        if m: 
-            year = m.group(2)
-            month = m.group(3)
-            day = m.group(4)
-       
-            if year.startswith('9'):
-                year = "19"+year
-            else:
-                year = "20"+year
-  
-            if not os.path.isdir(year):
-                os.mkdir(os.path.join(path,year))
-            shutil.copy((os.path.join(origPath,f)),year)
+for f in os.listdir(pdf_dir):
+    m = re.match("^(ZDB-PUB-)(\d{2})(\d{2})(\d{2})(-\d+)(\.pdf)$", f)
+    if m:
+        year = m.group(2)
+        month = m.group(3)
+        day = m.group(4)
+
+        if year.startswith('9'):
+            year = "19" + year
+        else:
+            year = "20" + year
+
+        src_path = os.path.join(pdf_dir, f)
+        year_dir = os.path.join(pdf_dir, year)
+        dst_path = os.path.join(year_dir, f)
+
+        if os.path.isfile(dst_path):
+            continue
+
+        if not os.path.isdir(year_dir):
+            os.mkdir(year_dir)
+
+        print src_path
+        shutil.copy(src_path, year_dir)
