1ffef8139661165db339142632e1b8dfc597fe24
Kevin Schaper <kevin@zfin.org>
2016-10-03 17:15:34 -0700

    bugzid: 14221  Update ZMP gff3 generation to unload from sequence feature chromosome location and skip gff3 table

--- server_apps/data_transfer/Downloads/GFF3/load_zmp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-GLOBALSTORE="/research/zprodmore/gff3"
-TARGETDIR="$TARGETROOT/server_apps/data_transfer/Downloads/GFF3"
-
-cp $GLOBALSTORE/zmp.unl $TARGETDIR
-cd $TARGETDIR && cat load_ZMP.sql commit.sql | dbaccess -a $DBNAME
