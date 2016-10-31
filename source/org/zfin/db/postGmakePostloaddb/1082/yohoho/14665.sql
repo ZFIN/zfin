ead3401e32bfb27a3a3359622bea8678711c8fa6
Sierra Moxon <smoxon@zfin.org>
2016-10-31 12:02:39 -0700

    fixing 14665

--- source/org/zfin/db/postGmakePostloaddb/1082/yohoho/14665.sql
+++ source/org/zfin/db/postGmakePostloaddb/1082/yohoho/14665.sql
@@ -2,6 +2,6 @@
 --changeset christian:14665
 
 
-update term set term_is_secondary = 't' where term_ont_id =  'GO:1903887'
+update term set term_is_secondary = 't' where term_ont_id =  'GO:1903887';
 
-update term set term_is_secondary = 'f' where term_ont_id =  'GO:0044458'
+update term set term_is_secondary = 'f' where term_ont_id =  'GO:0044458';
