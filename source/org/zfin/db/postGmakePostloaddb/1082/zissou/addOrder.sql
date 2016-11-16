f917f38302f6c5a9e929d6e981514308379c9c7a
Sierra Moxon <smoxon@zfin.org>
2016-11-16 09:22:41 -0800

    bugzid: 14604

--- source/org/zfin/db/postGmakePostloaddb/1082/zissou/addOrder.sql
+++ source/org/zfin/db/postGmakePostloaddb/1082/zissou/addOrder.sql
@@ -8,8 +8,8 @@ update pub_tracking_status
   set pts_pipeline_pull_down_order = pts_pk_id
 ;
 
-update pub_tracking_status
-  modify (pts_pipeline_pull_down int8 not null constraint pts_pipeline_pull_down_not_null);
+alter table pub_tracking_status
+  modify (pts_pipeline_pull_down_order int8 not null constraint pts_pipeline_pull_down_not_null);
 
 alter table pub_tracking_status
  add (pts_hidden_status boolean default 'f' not null constraint pts_hidden_status_not_null);
