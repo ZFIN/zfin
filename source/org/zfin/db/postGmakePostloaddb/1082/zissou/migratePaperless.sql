d080a9edf6b02d80f87624d420c3786b9efc1752
Sierra Moxon <smoxon@zfin.org>
2016-11-17 14:10:59 -0800

    bugzid: 14604

--- source/org/zfin/db/postGmakePostloaddb/1082/zissou/migratePaperless.sql
+++ source/org/zfin/db/postGmakePostloaddb/1082/zissou/migratePaperless.sql
@@ -85,15 +85,14 @@ update pub_tracking_Status
  set pts_pipeline_pull_down_order = 12
  where pts_status_display = 'Waiting for Nomenclature';
 
+alter table publication
+ drop pub_is_indexed;
 
---alter table publication
--- drop pub_is_indexed;
+alter table publication
+ drop pub_indexed_date;
 
---alter table publication
--- drop pub_indexed_date;
-
---alter table publication
---- drop pub_geli_removed;
+alter table publication
+ drop pub_geli_removed;
 
 --drop table curation;
 --drop table curation_topic;
