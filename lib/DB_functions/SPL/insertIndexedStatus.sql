ef636c5acbb3e0fc22004bc6d65324d05117052b
Sierra Moxon <smoxon@zfin.org>
2016-11-18 09:40:06 -0800

    bugzid: 14604

--- lib/DB_functions/SPL/insertIndexedStatus.sql
+++ lib/DB_functions/SPL/insertIndexedStatus.sql
@@ -7,7 +7,7 @@ define status varchar(50);
 let status = (Select pts_status from pub_tracking_Status
     	     	     where pts_pk_id = vPubStatusId);
 
-if ((status = 'Ready For Curation' or status like 'Closed%') and exists (Select 'x' from pub_tracking_history, pub_tracking_status where pth_status_id = pts_pk_id and pts_status = 'Indexing') )
+if ((status = 'READY_FOR_CURATION' or status like 'CLOSED%') and exists (Select 'x' from pub_tracking_history, pub_tracking_status where pth_status_id = pts_pk_id and pts_status = 'INDEXING') )
   then 
   insert into pub_tracking_history (pth_pub_zdb_id, pth_status_id, pth_status_set_by) 
   select vPubZdbId, (Select pts_pk_id from pub_tracking_status
