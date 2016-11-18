3f2fc5c6ebbdd6daedb456aa3b3ed9ef803acbb6
Sierra Moxon <smoxon@zfin.org>
2016-11-18 13:12:45 -0800

    bugzid: 14604

--- source/org/zfin/db/postGmakePostloaddb/1082/zissou/migrateClosedStatus.sql
+++ source/org/zfin/db/postGmakePostloaddb/1082/zissou/migrateClosedStatus.sql
@@ -56,7 +56,7 @@ update pub_tracking_history
  set pth_status_id = (Select pts_pk_id
      		     	     from pub_tracking_status
 			     where pts_status_display = 'Closed, Archived')
- where not exists  (Select 'x' from publication_note
+ where exists  (Select 'x' from publication_note
     	   	   where pnote_text = 'This paper closed unread')
  and pth_status_id = (select pts_pk_id from pub_tracking_status
      		     	     where pts_status_display = 'Closed, Curated');
@@ -73,6 +73,18 @@ and not exists  (Select 'x' from publication_note
  and pth_status_id = (select pts_pk_id from pub_tracking_status
      		     	     where pts_status_display = 'Closed, Curated');
 
+update pub_tracking_history
+ set pth_status_id = (Select pts_pk_id
+     		     	     from pub_tracking_status
+			     where pts_status_display = 'Closed, No PDF')
+ where not exists (Select 'x' from publication_file
+       	      	      where pth_pub_zdb_id = pf_pub_zdb_id
+		      and pf_file_type_id = '1')
+and not exists  (Select 'x' from publication_note
+    	   	   where pnote_text = 'Upon review, this publication contains no information currently curated by ZFIN.')
+ and pth_status_id = (select pts_pk_id from pub_tracking_status
+     		     	     where pts_status_display = 'Closed, Curated');
+
 
 delete from publication_note 
 where pnote_text = 'Upon Review, this publication contains no information currently curated by ZFIN.';
@@ -85,3 +97,9 @@ where pnote_text = 'This paper closed unread';
 
 delete from publication_note 
 where pnote_text = 'Upon Review by L. Bayraktaroglu, this publication contains no information currently curated by ZFIN.';
+
+delete from publication_note
+ where pnote_text = 'Closed Paper';
+
+delete from publication_note
+ where pnote_text = 'Indexed Paper';
