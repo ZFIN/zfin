11e3388afe79c04a80b49b186a56e0d5b3ba5fe2
Sierra Moxon <smoxon@zfin.org>
2016-11-21 12:09:10 -0800

    bugzid: 14604 backfilling some indexed dates

--- source/org/zfin/db/postGmakePostloaddb/1082/zissou/migrateGap.sql
+++ source/org/zfin/db/postGmakePostloaddb/1082/zissou/migrateGap.sql
@@ -10,6 +10,11 @@ insert into tmp_insert (id)
 and not exists (Select 'x' from pub_tracking_history
     	       	       where pth_pub_zdb_id = zdb_id);
 
+
+
+
+set triggers for pub_tracking_history disabled;
+
 insert into pub_tracking_history (pth_pub_zdb_id, 
        	    			 pth_status_id,  
 				 pth_status_set_by, 
@@ -19,3 +24,38 @@ select id,
        "ZDB-PERS-030612-1", 
        current year to second
     from tmp_insert;
+
+
+delete from tmp_insert;
+
+insert into tmp_insert (id)
+ select zdb_id 
+   from publication, pub_tracking_history a
+   where a.pth_pub_zdb_id = zdb_id
+    and a.pth_location_id is not null
+ and not exists (Select 'x' from pub_tracking_history b, pub_tracking_status
+     	 		where b.pth_pub_zdb_id = zdb_id
+			and b.pth_status_id = pts_pk_id
+			and pts_status = 'INDEXED');
+
+insert into pub_tracking_history (pth_pub_zdb_id, 
+       	    			 pth_status_id,  
+				 pth_status_set_by, 
+				 pth_status_insert_date) 
+select id, 
+       (Select pts_pk_id from pub_tracking_status where pts_status = "INDEXED"), 
+       "ZDB-PERS-100329-1", 
+       current year to second
+    from tmp_insert;
+
+set triggers for pub_tracking_history enabled;
+
+update publication
+ set pub_indexed_date = (select max(pth_status_insert_date)
+     		      		from pub_tracking_history, pub_tracking_status
+				where pth_pub_zdb_id = zdb_id
+				and pth_status_id = pts_pk_id
+				and pts_status = 'INDEXED')
+ where pub_indexed_date is null
+ and exists (Select 'x' from pub_tracking_history
+     	    	   where pth_pub_zdb_id = zdb_id);
