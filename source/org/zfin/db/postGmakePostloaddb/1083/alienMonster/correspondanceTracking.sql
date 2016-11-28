c0b12c23f4e28c81d466554caf9cf8b2913b14c0
Sierra Moxon <smoxon@zfin.org>
2016-11-28 09:23:30 -0800

    fixing liquibase constraint name

--- source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/correspondanceTracking.sql
+++ source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/correspondanceTracking.sql
@@ -1,11 +1,11 @@
 --liquibase formatted sql
 --changeset sierra:correspondanceTracking
 
-create table pub_correspondance_tracking (pct_pk_id serial8 not null constraint pct_pk_id_not_null,
-       	     				  pct_pub_zdb_id varchar(50) not null constraint pct_pub_zdb_id,
+create table pub_correspondance_tracking (pct_pk_id serial8 not null constraint pubct_pk_id_not_null,
+       	     				  pct_pub_zdb_id varchar(50) not null constraint pubct_pub_zdb_id,
 					  pct_date_entered datetime year to second default current year to second,
-					  pct_type int8 not null constraint pct_type_not_null,
-					  pct_entered_by varchar(50) not null constraint pct_entered_by_not_null)
+					  pct_type int8 not null constraint pubct_type_not_null,
+					  pct_entered_by varchar(50) not null constraint pubct_entered_by_not_null)
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
