991e18c3ecfb776867ea44f708aac0518d64a0dd
Sierra Moxon <smoxon@zfin.org>
2016-11-30 13:17:15 -0800

    bugzid: 14792

--- source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/constraints.sql
+++ source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/constraints.sql
@@ -3,57 +3,186 @@
 
 --pub_correspondence_sent_recipient
 
-create unique index pub_correspondence_sent_recipient_pk_index 
-       on pub_correspondence_sent_recipient (pubcsr_pk_id)
+create unique index pub_correspondence_recipient_pk_index 
+       on pub_correspondence_recipient (pubcr_pk_id)
        using btree in idxdbs2;
 
-create unique index pub_correspondence_sent_recipient_ak_index 
-       on pub_correspondence_sent_recipient (pubcsr_recipient_email_address, pubcsr_pubcs_id)
+create unique index pub_correspondence_recipient_ak_index 
+       on pub_correspondence_recipient (pubcr_recipient_email_address, pubcr_pubcs_id, pubcr_recipient_group_id)
        using btree in idxdbs1;
 
-create index pub_correspondence_sent_recipient_person_fk_index
-    on pub_correspondence_sent_recipient (pubcsr_recipient_person_zdb_id)
+create index pub_correspondence_recipient_person_fk_index
+    on pub_correspondence_recipient (pubcsr_recipient_person_zdb_id)
        using btree in idxdbs3;
 
-alter table pub_correspondence_sent_recipient
-  add constraint unique (pubcsr_recipient_email_address, pubcsr_pubcs_id)
- constraint pub_corresspondence_sent_recipient_ak;
+create index pub_correspondence_recipient_group_fk_index
+    on pub_correspondence_recipient (pubcsr_recipient_group_id)
+       using btree in idxdbs3;
 
-alter table pub_correspondence_sent_recipient
-  add constraint primary key (pubcsr_pk_id)
- constraint pub_corresspondence_sent_recipient_pk;
+alter table pub_correspondence_recipient
+  add constraint unique (pubcr_recipient_email_address, pubcr_recipient_group_id)
+ constraint pub_corresspondence_recipient_ak;
 
-alter table pub_correspondence_sent_recipient
-  add constraint (foreign key (pubcsr_recipient_person_zdb_id) references person 
- constraint pub_corresspondence_sent_recipient_zdb_id_fk);
+alter table pub_correspondence_recipient
+  add constraint primary key (pubcr_pk_id)
+ constraint pub_corresspondence_recipient_pk;
 
---pub_correspondence_sent_text
+alter table pub_correspondence_recipient
+  add constraint (foreign key (pubcr_recipient_person_zdb_id) references person 
+ constraint pub_corresspondence_recipient_person_zdb_id_fk);
 
-create unique index pub_correspondence_sent_text_pk_index
- on pub_correspondence_sent_text (pubcst_pk_id)
+alter table pub_correspondence_recipient
+  add constraint (foreign key (pubcr_recipient_group_id) references person 
+ constraint pub_corresspondence_recipient_group_id_fk);
+
+
+--pub_correspondence_sent_email
+
+create unique index pub_correspondence_sent_email_pk_index
+ on pub_correspondence_sent_email (pubcse_pk_id)
  using btree in idxdbs1;
 
-alter table pub_correspondence_sent_text
-  add constraint primary key (pubcst_pk_id)
- constraint pub_correspondence_sent_text_pk;
+create unique index pub_correspondence_sent_email_ak_index
+ on pub_correspondence_sent_email (pubcse_from, pubcse_subject, pubcse_recipient_group_id, pubcse_date_composed)
+ using btree in idxdbs2;
+
+create index pubcse_from_foreign_key_index
+ on pub_correspondence_sent_email (pubcse_from)
+ using btree in idxdbs3;
+
+create index pubcse_pub_foreign_key_index
+ on pub_correspondence_sent_email (pubcse_pub_zdb_id)
+ using btree in idxdbs3;
 
 
---pub_correspondence_sent
+create index pubcse_recipient_group_id_foreign_key_index
+ on pub_correspondence_sent_email (pubcse_recipient_Group_id)
+ using btree in idxdbs3;
 
-create unique index pub_correspondence_sent_pk_index 
-       on pub_correspondence_sent (pubcs_pk_id)
+alter table pub_correspondence_sent_email
+  add constraint (foreign key (pubcse_from)
+ references person constraint pubcse_from_foreign_key);
+
+alter table pub_correspondence_sent_email
+  add constraint (foreign key (pubcse_pub_zdb_id)
+ references publication on delete cascade constraint pubcse_pub_zdb_id_foreign_key_odc);
+
+alter table pub_correspondence_sent_email
+  add constraint (foreign key (pubcse_recipient_group_id)
+ references pub_correspondence_recipient_group constraint pubcse_recipient_group_id_foreign_key);
+
+alter table pub_correspondence_sent_email
+  add constraint primary key (pubcse_pk_id)
+ constraint pub_correspondence_sent_email_pk;
+
+alter table pub_correspondence_sent_email
+ add constraint unique (pubcse_from, pubcse_subject, pubcse_recipient_group_id, pubcse_date_composed)
+ constraint pub_correspondence_sent_email_ak;
+
+--pub_correspondence_subject
+
+create unique index pcs_subject_pk_id_index 
+  on pub_correspondence_subject (pcs_subject_pk_id)
+ using btree in idxdbs2;
+
+alter table pub_correspondence_subject
+ add constraint primary key (pcs_subject_pk_id)
+ constraint pub_correspondence_subject_pk;
+
+--pub_correspondence_sent_email_contains_subject
+
+create unique index pubcsecs_pk_index
+ on pub_correspondence_sent_email_contains_subject(pubcsecs_sent_email_id,pubcsecs_subject_matter_id)
+ using btree in idxdbs3;
+
+alter table pub_correspondence_sent_email_contains_subject
+ add constraint primary key (pubcsecs_sent_email_id,pubcsecs_subject_matter_id)
+ constraint pubcsecs_pk;
+
+
+--pub_correspondence_sent_tracker
+
+create unique index pub_correspondence_sent_tracker_pk_index 
+       on pub_correspondence_sent_tracker (pubcst_pk_id)
        using btree in idxdbs2;
 
-create unique index pub_correspondence_sent_pk_index 
-       on pub_correspondence_sent (pubcs_pk_id)
+create unique index pub_correspondence_sent_tracker_pk_index 
+       on pub_correspondence_sent_tracker (pubcst_sent_by, pubcst_date_sent, pubcst_sent_email_id, pubcst_pub_zdb_id)
        using btree in idxdbs1;
 
-create unique index pub_correspondence_sent_ak_index 
-       on pub_correspondence_sent (pubcs_date_sent, pubcs_pubc_text_id, pubcs_pub_zdb_id, pubcs_resend, pubcs_)
-       using btree in idxdbs1;
+create index pub_correspondence_sent_tracker_email_id_fk_index
+ on on pub_correspondence_sent_tracker (pubcst_sent_email_id)
+       using btree in idxdbs2;
 
---can't be a resend if pubc_text_id is not already found in the table with the same pub
-
-create index pub_correspondence_sent_recipient_person_fk_index
-    on pub_correspondence_sent_recipient (pubcsr_recipient_person_zdb_id)
+create index pub_correspondence_sent_tracker_pub_zdb_id_fk_index
+ on on pub_correspondence_sent_tracker (pubcst_pub_zdb_id)
        using btree in idxdbs3;
+
+create index pub_correspondence_sent_tracker_sent_by_zdb_id_fk_index
+ on on pub_correspondence_sent_tracker (pubcst_sent_by)
+       using btree in idxdbs3;
+
+alter table pub_correspondence_sent_tracker
+ add constraint primary key (pubcst_pk_id)
+ constraint pubcst_sent_tracker_pk;
+
+alter table pub_correspondence_sent_tracker
+ add constraint unique (pubcst_sent_by, pubcst_date_sent, pubcst_sent_email_id, pubcst_pub_zdb_id)
+ constraint pubcst_sent_tracker_ak;
+
+alter table pub_correspondence_sent_tracker
+ add constraint (foreign key (pubcst_sent_by) 
+references person constraint pub_correspondence_sent_tracker_sent_by_fk);
+
+alter table pub_correspondence_sent_tracker
+ add constraint (foreign key (pubcst_sent_email_id) 
+references pub_correspondence_sent_email constraint pub_correspondence_sent_tracker_sent_email_id_fk);
+
+alter table pub_correspondence_sent_tracker
+ add constraint (foreign key (pubcst_pub_zdb_id) 
+references publication on delete cascade constraint pub_correspondence_sent_tracker_pub_zdb_id_fk_odc);
+
+
+---TODO: check resend is only true when false exists ---
+
+---TODO: check pub in tracker matches pub in sent email --- 
+
+create unique index pub_correspondence_sent_tracker_ak_index 
+       on pub_correspondence_sent_tracker (pubcst_date_sent, pubcst_pubc_text_id, pubcst_pub_zdb_id, pubcst_resend_)
+       using btree in idxdbs1;
+
+--pub_correspondence_recieved_email
+
+create unique index pub_correspondence_received_email_pk_index
+ on pub_correspondence_received_email (pubcre_pk_id) 
+using btree in idxdbs2;
+
+create unique index pub_correspondence_received_email_ak_index
+ on pub_correspondence_received_email (pubcre_correspondence_from_email_address, pubcre_subject,pubcre_pub_zdb_id,pubcre_received_date) 
+using btree in idxdbs3;
+
+create index pub_correspondence_received_email_pub_fk_index
+ on pub_correspondence_received_email (pubcre_pub_zdb_id) 
+using btree in idxdbs1;
+
+create index pub_correspondence_received_email_from_person_zdb_id_fk_index
+ on pub_correspondence_received_email (pubcre_from_person_zdb_id) 
+using btree in idxdbs1;
+
+alter table pub_correspondence_recieved_email
+ add constraint primary key (pubcre_pk_id) 
+ constraint pubcre_pk;
+
+alter table pub_correspondence_recieved_email
+ add constraint unique (pubcre_correspondence_from_email_address, pubcre_subject,pubcre_pub_zdb_id,pubcre_received_date) 
+ constraint pubcre_ak;
+
+alter table pub_correspondence_recieved_email
+ add constraint (foreign key (pubcre_pub_zdb_id) 
+references publication on delete cascade constraint pub_correspondence_recieved_email_pub_fk_odc);
+
+alter table pub_correspondence_recieved_email
+ add constraint (foreign key (pubcre_from_person_zdb_id) 
+references person constraint pub_correspondence_recieved_email_from_person_fk);
+
+
--- source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/correspondenceTracking.sql
+++ source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/correspondenceTracking.sql
@@ -1,53 +1,77 @@
 --liquibase formatted sql
 --changeset sierra:correspondanceTracking
 
-create table pub_correspondence_sent_recipient (pubcsr_pk_id serial8 not null constraint pubcsr_pk_id_not_null,
-       	     				        pubcsr_recipient_first_name varchar(100),
-						pubcsr_recipient_last_name varchar(100),
-						pubcsr_recipient_email_address varchar(100),
-						pubcsr_pubcs_id int8 not null constraint pubcsr_pubcs_id_not_null,
-						pubcsr_recipient_person_zdb_id varchar(50),
-						pubcsr_recipient_group_id int8 not null constraint pubcsr_recipient_group_id_not_null)
+create table pub_correspondence_recipient (pubcr_pk_id serial8 not null constraint pubcr_pk_id_not_null,
+       	     				        pubcr_recipient_first_name varchar(100),
+						pubcr_recipient_last_name varchar(100),
+						pubcr_recipient_email_address varchar(100),
+						pubcr_recipient_person_zdb_id varchar(50),
+						pubcr_recipient_group_id int8 not null constraint pubcr_recipient_group_id_not_null)
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
 
-create table pub_correspondence_sent_recipient_group (pubcsrg_group_id serial8 not null constraint pubcsrg_group_id_not_null)
+create table pub_correspondence_recipient_group (pubcrg_group_id serial8 not null constraint pubcrg_group_id_not_null)
+fragment by round robin in tbldbs1, tbldbs2, tbldbs3
+extent size 4096 next size 4096;
+
+create unique index pubcrg_pk_index
+  on pub_correspondence_recipient_group (pubcrg_group_id)
+ using btree in idxdbs2;
+
+alter table pub_correspondence_recipient_group
+ add constraint primary key (pubcrg_group_id) 
+ constraint pubcrg_group_id_pk;
+
+
+create table pub_correspondence_sent_email (pubcse_pk_id serial8 not null constraint pubcse_pk_id_not_null,
+       	     				   pubcse_date_composed datetime year to day default datetime year to day not null constraint pubcse_date_composed_not_null,
+					   pubcse_from varchar(50) not null constraint pubcse_text_composer_not_null,
+					   pubcse_text lvarchar(10000) not null constraint pubcse_text_not_null,
+					   pubcse_subject varchar(100) not null constraint pubcse_subject_not_null,
+					   pubcse_recipient_group_id int8 not null cosntraint pubcse_recipient_group_not_null,
+					   pubcse_pub_zdb_id varchar(50) not null constraint pubcse_pub_zdb_id_not_null
+					   )
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
 
-create table pub_correspondence_sent_text (pubcst_pk_id serial8 not null constraint pubcst_pk_id_not_null,
-       	     				   pubcst_date_composed datetime year to second default datetime year to second not null constraint pubcst_date_composed_not_null,
-					   pubcst_text_composer varchar(50) not null constraint pubcst_text_composer_not_null,
-					   pubcst_text lvarchar(10000) not null constraint pubcst_text_not_null,
-					   pubcst_subject varchar(100) not null constraint pubcst_subject_not_null)
+create table pub_correspondence_sent_email_contains_subject (pubcsecs_sent_email_id int8 not null constraint pubcsecs_sent_email_id_not_null,
+       	     						     pubcsecs_subject_matter_id int8 not null constraint pubcsecs_subject_matter_id_not_null)
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
 
-create table pub_correspondence_sent (pubcs_pk_id serial8 not null constraint pubcs_pk_id_not_null,
-       	     			     	pubcs_sent_by varchar(50) not null constraint pubcs_sent_by_not_null,
-					pubcs_date_sent datetime year to day default datetime year to second not null constraint pubcs_date_sent_not_null,
-					pubcs_pubc_text_id int8 not null constraint pubcs_pubc_text_id_not_null,
-					pubcs_pub_zdb_id varchar(50) not null constraint pubcs_pub_Zdb_id_not_null,
-					pubcs_resend boolean default 'f' not null constraint pubcs_resend_not_null,
-					pubcs_recipient_group_id int8 not null constraint pubcs_recipient_group_id_not_null)
+create table pub_correspondence_subject (pcs_subject_pk_id serial8 not null constraint pcs_subject_pk_id_not_null,
+       	     				 pcs_subject_text lvarchar(1000) not null constraint pcs_subject_text_not_null,
+					 pcs_subject_type varchar(20) not null constraint pcs_subject_text_not_null,
+					 pcs_handle varchar(100) not null constraint pcs_handle_not_null)
+in tbldbs2
+extent size 32 next size 32;
+
+
+
+create table pub_correspondence_sent_tracker (pubcst_pk_id serial8 not null constraint pubcst_pk_id_not_null,
+       	     			     	pubcst_sent_by varchar(50) not null constraint pubcst_sent_by_not_null,
+					pubcst_date_sent datetime year to day default datetime year to day not null constraint pubcst_date_sent_not_null,
+					pubcst_sent_email_id int8 not null constraint pubcst_sent_email_id_not_null,
+					pubcst_pub_zdb_id varchar(50) not null constraint pubcst_pub_Zdb_id_not_null,
+					pubcst_resend boolean default 'f' not null constraint pubcst_resend_not_null)
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
 
-create table pub_correspondence_received (pubcr_pk_id serial8 not null constraint pubcr_pk_id_not_null,
-       	     				       pubcr_correspondence_from_first_name varchar(100),
-					       pubcr_correspondence_from_last_name varchar(100),
-					       pubcr_correspondence_from_email_address varchar(100) 
-					       		not null constraint pubcr_correspondence_from_email_address_not_null,
-					       pubcr_correspondence_from_person_zdb_id varchar(50),
-					       pubcr_pub_zdb_id varchar(50) not null constraint pubcr_pub_zdb_id_not_null,
-					       pubcr_received_date datetime year to second default current year to second 
-					       	        not null constraint pubcr_received_date_not_null,
-					       pubcr_text lvarchar(10000),
-					       pubcr_subject varchar(100) not null constraint pubcr_subject_not_null)
+create table pub_correspondence_received_email (pubcre_pk_id serial8 not null constraint pubcre_pk_id_not_null,
+       	     				       pubcre_correspondence_from_first_name varchar(100),
+					       pubcre_correspondence_from_last_name varchar(100),
+					       pubcre_correspondence_from_email_address varchar(100) 
+					       		not null constraint pubcre_correspondence_from_email_address_not_null,
+					       pubcre_correspondence_from_person_zdb_id varchar(50),
+					       pubcre_pub_zdb_id varchar(50) not null constraint pubcre_pub_zdb_id_not_null,
+					       pubcre_received_date datetime year to second default current year to second 
+					       	        not null constraint pubcre_received_date_not_null,
+					       pubcre_text lvarchar(10000),
+					       pubcre_subject varchar(100) not null constraint pubcre_subject_not_null)
 fragment by round robin in tbldbs1, tbldbs2, tbldbs3
 extent size 4096 next size 4096;
 
