df6b630bdf814e7ff716b8d4e8b69a1ce33afb1d
Sierra Moxon <smoxon@zfin.org>
2016-11-30 14:46:37 -0800

    bugzid: 14792

--- source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/addSubjects.sql
+++ source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/addSubjects.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
 --changeset sierra:addSubjects.sql
 
-insert into pub_correspondence_subject (pcs_subject_text, psc_subject_type, pcs_handle)
+insert into pub_correspondence_subject (pcs_subject_text, pcs_subject_type, pcs_handle)
  values ('ZFIN uses short line designations to identify mutant and transgenic fish. Each designation consists of a letter code identifying the institution in which the fish line was generated, followed by a number, so that the combination of the code and number is unique. Lab designations and nomenclature conventions are listed on these web pages. https://wiki.zfin.org/display/general/ZFIN+Zebrafish+Nomenclature+Guidelines
 ','Explanation of line numbers','nomenclature');
 
 
-insert into pub_correspondence_subject (pcs_subject_text, psc_subject_type, pcs_handle)
+insert into pub_correspondence_subject (pcs_subject_text, pcs_subject_type, pcs_handle)
  values ('In this article you generated a new transgenic zebrafish line. At ZFIN we create records in our database for all mutant and transgenic zebrafish lines that are published. We would like to add this line to the database, but in order to link it to your lab we will need to set up personal and lab records for you at ZFIN.
 
 Request to set up personal and lab records:
@@ -28,7 +28,7 @@ If you have any questions, please let me know.
 ','New line with no records in the database','addPerson');
 
 
-insert into pub_correspondence_subject (pcs_subject_text, psc_subject_type, pcs_handle)
+insert into pub_correspondence_subject (pcs_subject_text, pcs_subject_type, pcs_handle)
  values ('Your institution designation is "la". If you would like to see a list of the "la" lines already in use follow the link below and click on the "+" next to the "la" in the left hand column.          
 http://zfin.org/action/feature/line-designations
 
