99c1ad8dbd55ccc880da0b777a314977f3b5e546
Sierra Moxon <smoxon@zfin.org>
2016-12-09 08:52:49 -0800

    bugzid: 14628

--- server_apps/Reports/PubTracking/runLongestBinResidents.sh
+++ server_apps/Reports/PubTracking/runLongestBinResidents.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-<!--|INFORMIX_DIR|-->/bin/dbaccess -a <!--|DB_NAME|--> <!--|ROOT_PATH|-->/server_apps/Reports/AnnualStats/longestBinResidents.sql
+<!--|INFORMIX_DIR|-->/bin/dbaccess -a <!--|DB_NAME|--> <!--|ROOT_PATH|-->/server_apps/Reports/PubTracking/longestBinResidents.sql
--- server_apps/Reports/PubTracking/runMonthlyBinLengthReport.sh
+++ server_apps/Reports/PubTracking/runMonthlyBinLengthReport.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-<!--|INFORMIX_DIR|-->/bin/dbaccess -a <!--|DB_NAME|--> <!--|ROOT_PATH|-->/server_apps/Reports/AnnualStats/monthlyBinLengthReport.sql
+<!--|INFORMIX_DIR|-->/bin/dbaccess -a <!--|DB_NAME|--> <!--|ROOT_PATH|-->/server_apps/Reports/PubTracking/monthlyBinLengthReport.sql
