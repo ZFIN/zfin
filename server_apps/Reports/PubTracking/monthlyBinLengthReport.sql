b62575daf581fa091e78beb624a8bf48a23be324
Sierra Moxon <smoxon@zfin.org>
2016-12-09 11:41:27 -0800

    bugzid: 14604

--- server_apps/Reports/PubTracking/monthlyBinLengthReport.sql
+++ server_apps/Reports/PubTracking/monthlyBinLengthReport.sql
@@ -7,81 +7,113 @@ select distinct month(pub_arrival_date), year(pub_arrival_date)
  where exists (Select 'x' from pub_tracking_history, pub_tracking_status
        	      	      where pth_pub_zdb_id = zdb_id
 		      and pth_status_id = pts_pk_id
-		      and pts_status = 'READY_FOR_CURATION');
+		      and pts_status = 'READY_FOR_CURATION'
+		      and pth_status_is_current = 't');
 
 
-update monthly_curated_metric
-  set mcm_number_in_bin_1 = (Select count(*) from pub_tracking_history, 
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
       			    	    	     pub_tracking_location,
 					     publication
 					     where pth_location_id = ptl_pk_id
 					     and pth_pub_Zdb_id = zdb_id
 					     and ptl_location = 'BIN_1'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp bin1;
 
-update monthly_curated_metric
-  set mcm_number_in_bin_2 = (Select count(*) from pub_tracking_history, 
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
       			    	    	     pub_tracking_location,
 					     publication
 					     where pth_location_id = ptl_pk_id
 					     and pth_pub_Zdb_id = zdb_id
 					     and ptl_location = 'BIN_2'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp bin2;
 
-update monthly_curated_metric
-  set mcm_number_in_bin_3 = (Select count(*) from pub_tracking_history, 
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
       			    	    	     pub_tracking_location,
 					     publication
 					     where pth_location_id = ptl_pk_id
 					     and pth_pub_Zdb_id = zdb_id
 					     and ptl_location = 'BIN_3'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp bin3;
 
-update monthly_curated_metric
-  set mcm_number_in_phenotype_bin = (Select count(*) from pub_tracking_history, 
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
       			    	    	     pub_tracking_location,
 					     publication
 					     where pth_location_id = ptl_pk_id
 					     and pth_pub_Zdb_id = zdb_id
 					     and ptl_location = 'NEW_PHENO'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp newpheno;
 
-update monthly_curated_metric
-  set mcm_number_in_expression_bin = (Select count(*) from pub_tracking_history, 
-      			    	    	     pub_tracking_location,
-					     publication
-					     where pth_location_id = ptl_pk_id
-					     and pth_pub_Zdb_id = zdb_id
-					     and ptl_location = 'NEW_XPAT'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
-
-
-update monthly_curated_metric
-  set mcm_number_in_ortho_bin = (Select count(*) from pub_tracking_history, 
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
       			    	    	     pub_tracking_location,
 					     publication
 					     where pth_location_id = ptl_pk_id
 					     and pth_pub_Zdb_id = zdb_id
 					     and ptl_location = 'NEW_ORTHO'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp newortho;
 
-update monthly_curated_metric
-  set mcm_number_archived_this_month = (Select count(*) from pub_tracking_history, pub_tracking_status,	 publication
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+         from pub_tracking_history, 
+      			    	    	     pub_tracking_location,
+					     publication
+					     where pth_location_id = ptl_pk_id
+					     and pth_pub_Zdb_id = zdb_id
+					     and ptl_location = 'NEW_XPAT'
+					     and pth_status_is_current = 't'
+	 group by month, year
+ into temp newxpat;
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+        from pub_tracking_history, pub_tracking_status,	 publication
 					     where pth_pub_Zdb_id = zdb_id
 					     and pts_pk_id = pth_status_id
 					     and pts_status = 'CLOSED'
 					     and pts_status_qualifier = 'Archived'
-					     and year(pth_status_insert_date) = year(current year to month)
-					     and month(pth_status_insert_date) = month(current year to month)
 					     and pth_status_is_current = 't'
-					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+	group by month, year
+into temp closedArchived;
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+        from pub_tracking_history, pub_tracking_status,	 publication
+					     where pth_pub_Zdb_id = zdb_id
+					     and pts_pk_id = pth_status_id
+					     and pts_status = 'CLOSED'
+					     and pts_status_qualifier = 'not a zebrafish paper'
+					     and pth_status_is_current = 't'
+	group by month, year
+into temp closedNotZebrafish;
+
+
+select count(*) as counter, month(pub_arrival_date) as month, year(pub_arrival_date) as year
+        from pub_tracking_history, pub_tracking_status,	 publication
+					     where pth_pub_Zdb_id = zdb_id
+					     and pts_pk_id = pth_status_id
+					     and pts_status = 'CLOSED'
+					     and pts_status_qualifier = 'curated'
+					     and pth_status_is_current = 't'
+	group by month, year
+into temp closedCurated;
+
+
+
+
 
 update monthly_curated_metric
   set mcm_number_closed_unread_this_month = (Select count(*) from pub_tracking_history, pub_tracking_status,	 publication
@@ -93,7 +125,8 @@ update monthly_curated_metric
 					     and month(pth_status_insert_date) = month(current year to month)
 					     and pth_status_is_current = 't'
 					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and year(pub_arrival_date) = mcm_pub_arrival_date_year
+					     and pth_status_is_current = 't');
 
 
 update monthly_curated_metric
@@ -106,7 +139,8 @@ update monthly_curated_metric
 					     and month(pth_status_insert_date) = month(current year to month)
 					     and pth_status_is_current = 't'
 					     and month(pub_arrival_date) = mcm_pub_arrival_date_month
-					     and year(pub_arrival_date) = mcm_pub_arrival_date_year);
+					     and year(pub_arrival_date) = mcm_pub_arrival_date_year
+					     and pth_status_is_current = 't');
 
 
 select first 10 * from monthly_Curated_metric;
