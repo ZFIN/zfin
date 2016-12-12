a862ad731565ffe806302db1fd3c56dc9ef48967
Prita Mani <pm@zfin.org>
2016-12-12 14:11:31 -0800

    chnaged changeset

--- source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/loadChromosome.sql
+++ source/org/zfin/db/postGmakePostloaddb/1083/alienMonster/loadChromosome.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset prita:loadChromosome
+--changeset prita:loadChr
 
 update sequence_feature_chromosome_location set sfcl_chromosome=(select distinct ftrChrom from tmp_sanger10location where sfcl_feature_zdb_id=ftrzdb) where exists (select  'x' from tmp_sanger10location where sfcl_feature_zdb_id=ftrzdb);
 update sequence_feature_chromosome_location set sfcl_start_position=(select locStart from tmp_sanger10location where sfcl_feature_zdb_id =ftrzdb) where exists (select  'x' from tmp_sanger10location where sfcl_feature_zdb_id=ftrzdb);
