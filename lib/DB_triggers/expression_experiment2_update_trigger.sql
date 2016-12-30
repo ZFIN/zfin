96b1cef55d5f2089baace624d8b2534d22eec03c
Sierra Moxon <smoxon@zfin.org>
2016-12-30 09:12:32 -0800

    bugzid: 14895

--- lib/DB_triggers/expression_experiment2_update_trigger.sql
+++ lib/DB_triggers/expression_experiment2_update_trigger.sql
@@ -5,4 +5,5 @@ create trigger expression_experiment2_update_trigger
         execute procedure p_insert_into_record_attribution_tablezdbids(new_xpatex.xpatex_zdb_id 
     ,new_xpatex.xpatex_source_zdb_id ),
         execute procedure p_insert_into_record_attribution_datazdbids(new_xpatex.xpatex_gene_zdb_id 
-    ,new_xpatex.xpatex_source_zdb_id ));
+    ,new_xpatex.xpatex_source_zdb_id ),
+        execute procedure check_xpat_null_valid(new_xpatex.xpatex_gene_zdb_id, xpatex_probe_feature_zdb_id, xpatex_atb_zdb_id));
