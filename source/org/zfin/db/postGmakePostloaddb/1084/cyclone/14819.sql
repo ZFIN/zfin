4a04a46b02043afbcbaee6dc288b4baa10bce158
Prita Mani <pm@zfin.org>
2017-01-20 09:39:12 -0800

    bugzid: 14819 fixed typos

--- source/org/zfin/db/postGmakePostloaddb/1084/cyclone/14819.sql
+++ source/org/zfin/db/postGmakePostloaddb/1084/cyclone/14819.sql
@@ -2,10 +2,10 @@
 --changeset pm:14819
 
 UPDATE db_link
-SET dblink_fdb_cont_id = 'ZDB-FDBCONT-131021-1'
+SET dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-131021-1'
   where dblink_zdb_id in (SELECT tmp_dblink_id
   FROM tmp_old_ensdargs)
-)
+
 WHERE EXISTS (
   SELECT 'x'
   FROM tmp_old_Ensdargs
