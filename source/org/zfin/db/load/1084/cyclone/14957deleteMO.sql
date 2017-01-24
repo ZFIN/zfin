d13a6d3f5328ef7f35eefea020f75a0fc4fa8031
Prita Mani <pm@zfin.org>
2017-01-24 15:29:55 -0800

    bugzid:14957

---
+++ source/org/zfin/db/load/1084/cyclone/14957deleteMO.sql
@@ -0,0 +1,7 @@
+delete from record_attribution where recattrib_source_zdb_id = "ZDB-PUB-170103-4" and recattrib_data_zdb_id = "ZDB-MREL-170111-1";
+
+delete from marker_relationship where mrel_mrkr_1_zdb_id = "ZDB-MRPHLNO-170111-1";
+
+delete from zdb_active_data where zactvd_zdb_id = "ZDB-MRPHLNO-170111-1";
+
+
