73f26d653d2c67f0a521e7b651e280c74f6c36f9
Sierra Moxon <smoxon@zfin.org>
2017-01-31 13:55:53 -0800

    bugzid: 14752

--- server_apps/Reports/PubTracking/paperlessPubTrackingDailyIndexedStats.sql
+++ server_apps/Reports/PubTracking/paperlessPubTrackingDailyIndexedStats.sql
@@ -6,7 +6,8 @@ insert into daily_indexed_metric (dim_date_captured,
 				  dim_number_phenotype_bin,
 				  dim_number_expression_bin,
 				  dim_number_archived,	
-				  dim_number_closed_no_data)
+				  dim_number_closed_no_data,
+				  dim_number_closed_no_pdf)
 select  current year to second, (select count(*) from pub_tracking_history, pub_tracking_status, pub_tracking_location
        	       	       	       	       where pth_status_id = pts_pk_id
 				       and pts_status = 'READY_FOR_CURATION'
@@ -58,6 +59,13 @@ select  current year to second, (select count(*) from pub_tracking_history, pub_
        	       	       	       	       where pth_status_id = pts_pk_id
 				       and pts_status = 'CLOSED'
 				       and pts_status_qualifier = 'no data'
+				       and day(pth_status_insert_date) = day(current year to second)
+				       and year(pth_status_insert_date) = year(current year to second)
+				       and month(pth_status_insert_date) = month(current year to second),
+				 (select count(*) from pub_tracking_history, pub_tracking_status
+       	       	       	       	       where pth_status_id = pts_pk_id
+				       and pts_status = 'CLOSED'
+				       and pts_status_qualifier = 'no PDF'
 				       and day(pth_status_insert_date) = day(current year to second)
 				       and year(pth_status_insert_date) = year(current year to second)
 				       and month(pth_status_insert_date) = month(current year to second)
