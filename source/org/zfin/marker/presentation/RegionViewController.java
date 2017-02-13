af20897784a1e695bffb28be18d681f43b24f0d1
Prita Mani <pm@zfin.org>
2017-02-13 11:23:15 -0800

    bugzid:14947

--- source/org/zfin/marker/presentation/RegionViewController.java
+++ source/org/zfin/marker/presentation/RegionViewController.java
@@ -6,12 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zfin.expression.service.ExpressionService;
 import org.zfin.framework.presentation.Area;
 import org.zfin.framework.presentation.LookupStrings;
 import org.zfin.marker.Marker;
+import org.zfin.marker.MarkerRelationship;
 import org.zfin.marker.repository.MarkerRepository;
 import org.zfin.marker.service.MarkerService;
 import org.zfin.repository.RepositoryFactory;
+import org.zfin.sequence.DisplayGroup;
+import org.zfin.sequence.service.SequenceService;
+import org.zfin.sequence.service.TranscriptService;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  */
@@ -24,11 +32,13 @@ public class RegionViewController {
 //    @Autowired
 //    private ExpressionService expressionService ;
 
+   
     @Autowired
-    private MarkerRepository markerRepository ;
-    
+    private ExpressionService expressionService;
+
     @Autowired
-    private EfgViewController efgViewController;
+    private MarkerRepository markerRepository;
+
 
     @RequestMapping(value ="/region/view/{zdbID}")
     public String getView(
@@ -40,26 +50,50 @@ public class RegionViewController {
 
         logger.info("zdbID: " + zdbID);
         Marker region = markerRepository.getMarkerByID(zdbID);
-        logger.info("gene: " + region);
+        logger.info("region: " + region);
         markerBean.setMarker(region);
 
         // not used, too much stuff excluded
-//        MarkerService.createDefaultViewForMarker(markerBean);
+        MarkerService.createDefaultViewForMarker(markerBean);
 
         markerBean.setMarkerTypeDisplay(MarkerService.getMarkerTypeString(region));
         markerBean.setPreviousNames(markerRepository.getPreviousNamesLight(region));
         markerBean.setHasMarkerHistory(markerRepository.getHasMarkerHistory(zdbID)) ;
 
         // EXPRESSION SECTION
-//        markerBean.setMarkerExpression(expressionService.getExpressionForEfg(region));
+        /*List<LinkDisplay> otherMarkerDBLinksLinks = markerBean.getOtherMarkerPages();
+        otherMarkerDBLinksLinks.addAll(markerRepository.getVegaGeneDBLinksTranscript(
+                region, DisplayGroup.GroupName.SUMMARY_PAGE));
+        Collections.sort(otherMarkerDBLinksLinks, linkDisplayOtherComparator);
+        markerBean.setOtherMarkerPages(otherMarkerDBLinksLinks);
+
+
+        markerBean.setHasChimericClone(markerRepository.isFromChimericClone(region.getZdbID()));*/
+
+        // EXPRESSION SECTION
+        markerBean.setMarkerExpression(expressionService.getExpressionForGene(region));
+
+        // MUTANTS AND TARGETED KNOCKDOWNS
+        markerBean.setMutantOnMarkerBeans(MarkerService.getMutantsOnGene(region));
+
+        // PHENOTYPE
+        markerBean.setPhenotypeOnMarkerBeans(MarkerService.getPhenotypeOnGene(region));
+
+        // region Ontology
+        markerBean.setGeneOntologyOnMarkerBeans(MarkerService.getGeneOntologyOnMarker(region));
+
+        // Protein Products (Protein Families, Domains, and Sites)
+        markerBean.setProteinProductDBLinkDisplay(SequenceService.getProteinProducts(region));
+
+        // (Transcripts)
+        markerBean.setRelatedTranscriptDisplay(TranscriptService.getRelatedTranscriptsForGene(region));
+
+        // region products
+        markerBean.setGeneProductsBean(markerRepository.getGeneProducts(region.getZdbID()));
 
         // (CONSTRUCTS)
-        efgViewController.populateConstructList(markerBean, region);
 
-        // (Antibodies)
-//        markerBean.setRelatedAntibodies(markerRepository
-//                .getRelatedMarkerDisplayForTypes(region, true
-//                        , MarkerRelationship.Type.GENE_PRODUCT_RECOGNIZED_BY_ANTIBODY));
+
 
 
 //      CITATIONS
