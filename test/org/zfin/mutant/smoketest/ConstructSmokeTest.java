b47f6bc67c64a9a9507561c8684983bb0498b7b4
Prita Mani <pm@zfin.org>
2017-02-24 08:51:08 -0800

    Added @ignore to ConstructSearchPageOk test

--- test/org/zfin/mutant/smoketest/ConstructSmokeTest.java
+++ test/org/zfin/mutant/smoketest/ConstructSmokeTest.java
@@ -2,6 +2,7 @@ package org.zfin.mutant.smoketest;
 
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -22,6 +23,7 @@ public class ConstructSmokeTest extends AbstractSmokeTest {
      * Just pull up the fish search form and check for no errors.
      */
     @Test
+    @Ignore("no need of the test?")
     public void testConstructSearchPageOk() throws IOException {
         HtmlPage page = webClient.getPage(nonSecureUrlDomain + "/search?q=&fq=category%3A%22Construct%22&category=Construct");
         assertTrue(page.getTitleText().contains("ZFIN Construct Search"));
