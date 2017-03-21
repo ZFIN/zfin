487ad3fef9e7911ef553dc6f9c55565c139b345a
Christian Pich <cmpich@zfin.org>
2017-03-21 12:41:49 -0700

    jiraid: ONT-294 add correct queries for MPATH obsoletes

--- server_apps/data_transfer/LoadOntology/obsoleteMergeMousePathology.ontology.sql
+++ server_apps/data_transfer/LoadOntology/obsoleteMergeMousePathology.ontology.sql
@@ -5,11 +5,23 @@ SELECT term_zdb_id,
        term_name,
        xpatex_source_zdb_id
 FROM   term
-       join phenotype_statement on phenos_quality_zdb_id = term_zdb_id
+       join phenotype_statement on phenos_entity_1_subterm_zdb_id = term_zdb_id
+       join phenotype_experiment on phenos_Phenox_pk_id = phenox_pk_id
+       join fish_experiment on phenox_genox_zdb_id = genox_zdb_id
+       join expression_experiment on xpatex_genox_zdb_id = genox_zdb_id
+WHERE  term_is_obsolete = 't'
+UNION
+SELECT term_zdb_id,
+       term_ont_id,
+       term_name,
+       xpatex_source_zdb_id
+FROM   term
+       join phenotype_statement on phenos_entity_2_subterm_zdb_id = term_zdb_id
        join phenotype_experiment on phenos_Phenox_pk_id = phenox_pk_id
        join fish_experiment on phenox_genox_zdb_id = genox_zdb_id
        join expression_experiment on xpatex_genox_zdb_id = genox_zdb_id
 WHERE  term_is_obsolete = 't';
+
 
 
 -- report secondary PATO term usage
@@ -19,7 +31,18 @@ SELECT term_zdb_id,
        term_name,
        xpatex_source_zdb_id
 FROM   term
-       join phenotype_statement on phenos_quality_zdb_id = term_zdb_id
+       join phenotype_statement on phenos_entity_1_subterm_zdb_id = term_zdb_id
+       join phenotype_experiment on phenos_Phenox_pk_id = phenox_pk_id
+       join fish_experiment on phenox_genox_zdb_id = genox_zdb_id
+       join expression_experiment on xpatex_genox_zdb_id = genox_zdb_id
+WHERE  term_is_secondary = 't'
+UNION
+SELECT term_zdb_id,
+       term_ont_id,
+       term_name,
+       xpatex_source_zdb_id
+FROM   term
+       join phenotype_statement on phenos_entity_2_subterm_zdb_id = term_zdb_id
        join phenotype_experiment on phenos_Phenox_pk_id = phenox_pk_id
        join fish_experiment on phenox_genox_zdb_id = genox_zdb_id
        join expression_experiment on xpatex_genox_zdb_id = genox_zdb_id
