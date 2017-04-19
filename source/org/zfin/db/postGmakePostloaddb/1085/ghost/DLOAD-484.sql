f2bf49c3ff763f3606a234b9c65374cebbe848fa
Prita Mani <pm@zfin.org>
2017-04-19 09:25:41 -0700

    DLOAD-484

--- source/org/zfin/db/postGmakePostloaddb/1085/ghost/DLOAD-484.sql
+++ source/org/zfin/db/postGmakePostloaddb/1085/ghost/DLOAD-484.sql
@@ -4,7 +4,7 @@
 UPDATE tmp_clone_alias
 SET aliasid = get_id('DALIAS');
 insert into zdb_active_Data (zactvd_zdb_id) select aliasid from tmp_clone_alias;
-insert into data_alias (dalias_zdb_id,dalias_data_zdb_id,dalias_alias,dalias_group_id) select distinct aliasid,cloneid,clonealias,1 from tmp_clone_alias;
+insert into data_alias (dalias_zdb_id,dalias_alias,dalias_data_zdb_id,dalias_group_id) select distinct aliasid,clonealias,cloneid,1 from tmp_clone_alias;
 insert into record_attribution (recattrib_Data_zdb_id,recattrib_source_zdb_id) select aliasid,'ZDB-PUB-040907-1' from tmp_clone_alias;
 DROP TABLE tmp_clone_alias;
 
