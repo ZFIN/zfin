de4ceef32197b998dfd036461903f7218153f46e
Sierra Moxon <smoxon@zfin.org>
2017-04-20 11:46:14 -0700

    rgns-60

--- source/org/zfin/db/load/1085/ghost/addConstructRelationships.sql
+++ source/org/zfin/db/load/1085/ghost/addConstructRelationships.sql
@@ -21,12 +21,16 @@ insert into marker_type_group_member(mtgrpmem_mrkr_type,
 insert into marker_relationship_type(mreltype_name, mreltype_mrkr_type_group_1, mreltype_mrkr_type_group_2, mreltype_1_to_2_comments, mreltype_2_to_1_comments)
  values ('contains region', 'CONSTRUCT', 'CONSTRUCT_COMPONENTS', 'Contains','Contained in');
 
---update marker_relationship
--- set mrel_type = 'contains region' 
---where mrel_type = 'contains engineered region';
+update marker_relationship
+ set mrel_type = 'contains region' 
+where mrel_type = 'contains engineered region';
 
---delete from marker_relationship_type
--- where mreltype_name = 'contains engineered region';
+update construct_marker_relationship
+ set conmrkrrel_relationship_type = 'contains region'
+where conmrkrrel_relationship_type = 'contains engineered region';
+
+delete from marker_relationship_type
+ where mreltype_name = 'contains engineered region';
 
 update marker_relationship_type
   set mreltype_mrkr_type_group_2 = 'CONSTRUCT_COMPONENTS'
