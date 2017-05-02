b5001c94cacd8c8a595bf6845b08dbf0ed102e03
Prita Mani <pm@zfin.org>
2017-05-02 10:32:20 -0700

    ZFIN-2943

--- source/org/zfin/db/postGmakePostloaddb/1085/fire/ZFIN2943.sql
+++ source/org/zfin/db/postGmakePostloaddb/1085/fire/ZFIN2943.sql
@@ -4,7 +4,7 @@
 create temp table sfclg (zdb varchar(50),ftr varchar(50),st int, end int,assembly varchar(50),chr int) with no log;
 
 insert into sfclg values ('ZDB-ALT-110111-1','ZDB-ALT-110111-1',8611002,8611002,'Zv9',4);
-insert into sfclg values ('ZDB-ALT-170119-17','ZDB-ALT-170119-17',3573015,35730415,'Zv9',21);
+insert into sfclg values ('ZDB-ALT-170119-17','ZDB-ALT-170119-17',35730415,35730415,'Zv9',21);
 insert into sfclg values ('ZDB-ALT-170119-16','ZDB-ALT-170119-16',27053807,27053807,'Zv9',1);
 insert into sfclg values ('ZDB-ALT-170119-18','ZDB-ALT-170119-18',12656091,12656091,'Zv9',4);
 update sfclg set zdb=get_id('SFCL');
