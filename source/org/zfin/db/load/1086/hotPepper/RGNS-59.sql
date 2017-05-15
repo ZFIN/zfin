a42a63d34572762f697f46c31114d44f0970a9ff
Sierra Moxon <smoxon@zfin.org>
2017-05-15 16:17:59 -0700

    RGNS-59

--- source/org/zfin/db/load/1086/hotPepper/RGNS-59.sql
+++ source/org/zfin/db/load/1086/hotPepper/RGNS-59.sql
@@ -1,18 +1,16 @@
 --liquibase formated sql
 --changeset sierra:rgns-59
 
-insert into marker_type_Group (mtgrp_name, mtgrp_comments)
- values ('REGION', 'a group containing both region genes and non trascribed regions ala NTRs.');
-
-insert into marker_type_group_member (mtgrpmem_mrkr_type, mtgrpmem_mrkr_type_group)
- select mtgrpmem_mrkr_type, 'REGION'
-  from marker_type_group_member
- where mtgrpmem_mrkr_type_group = 'GENEDOM';
-
-insert into marker_type_group_member (mtgrpmem_mrkr_type, mtgrpmem_mrkr_type_group)
- select mtgrpmem_mrkr_type, 'REGION'
-  from marker_type_group_member
- where mtgrpmem_mrkr_type_group = 'NONTSCRBD_REGION';
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    'NONTSCRBD_REGION',
+		    'GENE',
+		    'interacts with',
+		    'interacts with');
 
 insert into marker_relationship_type (mreltype_name, 
        	    			      mreltype_mrkr_type_group_1, 
@@ -20,7 +18,95 @@ insert into marker_relationship_type (mreltype_name,
 				      mreltype_1_to_2_comments, 
 				      mreltype_2_to_1_comments)
  values ('interacts with',
- 		    'REGION',
-		    'REGION',
+ 		    'NONTSCRBD_REGION',
+		    'GENEP',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    'NONTSCRBD_REGION',
+		    'NONTSCRBD_REGION',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    'GENE',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
+		    'interacts with',
+		    'interacts with');
+
+insert into marker_relationship_type (mreltype_name, 
+       	    			      mreltype_mrkr_type_group_1, 
+				      mreltype_mrkr_type_group_2, 
+				      mreltype_1_to_2_comments, 
+				      mreltype_2_to_1_comments)
+ values ('interacts with',
+ 		    '',
+		    '',
 		    'interacts with',
 		    'interacts with');
