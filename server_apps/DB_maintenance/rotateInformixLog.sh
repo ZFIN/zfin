146192236bd542243a02b77316c050946ff8551b
Sierra Moxon <smoxon@zfin.org>
2017-05-17 16:59:13 -0700

    fixing informix log rotator to run from cron

--- server_apps/DB_maintenance/rotateInformixLog.sh
+++ server_apps/DB_maintenance/rotateInformixLog.sh
@@ -3,7 +3,10 @@
 #source /private/ZfinLinks/Commons/env/watson.env
 echo "set process id"
 
-logToBeDeleted=`readlink -f logs`;
+cd <!--|TARGETROOT|-->/server_apps/DB_maintenance/
+
+logToBeDeleted=`readlink -f <!--|TARGETROOT|-->/server_apps/DB_maintenance/logs`;
+
 echo $logToBeDeleted;
 processID=`pgrep 'ontape'`;
 echo $processID;
