c992058b83c761ee0e7a89f6b27460a12d5b1f11
Sierra Moxon <smoxon@zfin.org>
2017-05-26 14:16:14 -0700

    RGNS-67

--- lib/DB_functions/SPL/checkTscriptType.sql
+++ lib/DB_functions/SPL/checkTscriptType.sql
@@ -4,7 +4,10 @@ define typeName like transcript_type.tscriptt_type;
 
 let typeName = (select tscriptt_type from transcript_type, transcript where vTscriptZdbId = tscript_mrkr_zdb_id and tscript_type_id = tscriptt_pk_id);
 
-if (typeName != 'ncRNA' and vMrelType == 'transcript targets gene')
+if (typeName not in ('aberrant processed transcript', 'pseudogenic transcript', 'lincRNA',
+   	     	    	       'piRNA', 'miRNA', 'pre miRNA', 'rRNA', 'snRNA', 'scRNA',
+			       'snoRNA', 'tRNA', 'ncRNA')
+     and vMrelType == 'transcript targets gene')
   then 
        raise exception -746,0,"FAIL!: only ncRNA transcripts can have targets relationships";
   end if;
