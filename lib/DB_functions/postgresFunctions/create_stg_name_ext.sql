09e13ee7f110201102893e344f8902da9d40d7dc
Sierra Moxon <smoxon@zfin.org>
2017-06-06 14:14:14 -0700

    INF-2970

---
+++ lib/DB_functions/postgresFunctions/create_stg_name_ext.sql
@@ -0,0 +1,51 @@
+create or replace function create_stg_name_ext(
+  stgHrsStart      decimal(7,2),
+  stgHrsEnd        decimal(7,2),
+  stgOtherFeatures varchar(50))
+
+  returns varchar(50) as $stgNameExt$; 
+
+  -- Creates the extension part of the "long name" for a stage, i.e, the part
+  -- that makes a long name long.  The extension includes the hours the 
+  -- stage spans, and the contents of the stg_other_features column.
+  -- In addition, any times greater than 168 hours (7 days) are displayed
+  -- as days, rather than as hours.
+  --
+  -- Examples
+  --
+  --   (60.00h-72.00h)
+  --   (60.00h-72.00h, 3.0mm SL)
+  --   (60.00h-72.00h, 3.0mm NL)
+  --   (60.00h-72.00h, 3.0/4.0mm NL/SL)
+  --   (168.00h-90d)
+  --   (90d-730d)
+  --
+
+  declare stgNameExt      varchar(50) := '(';
+  begin
+  -- get stage start time
+  if (stgHrsStart <= 168.00) then
+     stgNameExt = stgNameExt || stgHrsStart || 'h-';
+  else
+     stgNameExt = stgNameExt || (stgHrsStart / 24)::integer || 'd-';
+  end if;
+
+  -- get stage end time
+  if (stgHrsEnd <= 168.00) then
+     stgNameExt = stgNameExt || stgHrsEnd || 'h';
+  else
+     stgNameExt = stgNameExt || (stgHrsEnd / 24)::integer || 'd';
+  end if;
+
+  -- get other features
+  if (stgOtherFeatures is not NULL) then
+     stgNameExt = stgNameExt || ', ' || stgOtherFeatures;
+  end if;
+
+   stgNameExt = stgNameExt || ')';
+
+  return stgNameExt;
+
+end 
+
+$stgNameExt$ LANGUAGE plpgsql
---
+++ lib/DB_functions/postgresFunctions/create_stg_name_long.sql
@@ -0,0 +1,50 @@
+create or replace function create_stg_name_long(
+  stgName           stage.stg_name%TYPE,
+  stgHrsStart       stage.stg_hours_start%TYPE,
+  stgHrsEnd         stage.stg_hours_end%TYPE,
+  stgOtherFeatures  stage.stg_other_features%TYPE)
+
+  returns text as $stgNameLong$;
+
+  -- Creates the "long name" for a stage.  The long name includes
+  -- the name of the stage, plus the hours it spans, and the contents 
+  -- of the stg_other_features column. The Unknown stage has the long 
+  -- name the same as the name.
+  -- In addition, any times greater than 168 hours (7 days) are displayed
+  -- as days, rather than as hours.
+  --
+  -- If a non-existent stage ZDB ID is passed in then
+  --
+  --   UNKNOWN
+  --
+  -- is returned.
+  -- 
+  -- Examples
+  --
+  --   Large-stage:Sub-stage (60.00h - 72.00h)
+  --   Large-stage:Sub-stage (60.00h - 72.00h, 3.0mm SL)
+  --   Large-stage:Sub-stage (60.00h - 72.00h, 3.0mm NL)
+  --   Large-stage:Sub-stage (60.00h - 72.00h, 3.0/4.0mm NL/SL)
+  --   Larval:Middle to late (168.00h - 90d)
+  --   Adult (90 d to 730 d)
+  --   Unknown
+  -- 
+
+  declare stgNameLong      varchar(130);
+  begin
+  if (stgName is NULL) then
+     stgNameLong = 'UNKNOWN';
+  elsif (stgName = 'Unknown') then
+     stgNameLong = 'Unknown';
+  else
+    -- get stage name 
+     stgNameLong = stgName || ' ' ||
+		      create_stg_name_ext(stgHrsStart, stgHrsEnd,
+					  stgOtherFeatures);
+  end if;
+
+  return stgNameLong;
+
+end 
+
+$stgNameLong$ LANGUAGE plpgsql
