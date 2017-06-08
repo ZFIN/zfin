2cfe21c736d188aae4a266c881296379c98b8645
Sierra Moxon <smoxon@zfin.org>
2017-06-07 21:43:13 -0700

    INF-2927

---
+++ lib/DB_triggers/postgresTriggerProcedures/term_relationship.sql
@@ -0,0 +1,15 @@
+drop trigger if exists term_relationship_trigger on term_relationship;
+
+create or replace function term_relationship()
+returns trigger as
+$BODY$
+begin
+	select p_check_anatrel_stg_consistent (NEW.termrel_term_1_zdb_id,
+                                               NEW.termrel_term_2_zdb_id,
+                                               NEW.termrel_type);
+end;
+$BODY$ LANGUAGE plpgsql;
+
+create trigger term_relationship_trigger before insert or update on term_relationship
+ for each row
+ execute procedure term_relationship();
