cb290c139bf04b1b7481968152f4cccabc5d7b0b
Christian Pich <cmpich@zfin.org>
2017-06-08 11:19:36 -0700

    AGR-58 suppress genome locations that are not trusted or ambiguous

---
+++ source/org/zfin/mapping/ChromosomeService.java
@@ -0,0 +1,37 @@
+package org.zfin.mapping;
+
+import java.util.List;
+
+import static org.zfin.mapping.MappingService.AMBIGUOUS;
+
+public class ChromosomeService<T extends GenomeLocation> {
+
+    private List<T> genomeLocationList;
+    private boolean trustedValue = false;
+    private String chromosomeNumber = "";
+
+    public ChromosomeService(List<T> genomeLocationList) {
+        this.genomeLocationList = genomeLocationList;
+        init();
+    }
+
+    private void init() {
+        String locationDisplay = MappingService.getChromosomeLocationDisplay(genomeLocationList);
+        if (!locationDisplay.equals(AMBIGUOUS) && !locationDisplay.isEmpty()) {
+            trustedValue = true;
+            chromosomeNumber = locationDisplay;
+        }
+    }
+
+    public boolean isTrustedValue() {
+        return trustedValue;
+    }
+
+    public String getChromosomeNumber() {
+        return chromosomeNumber;
+    }
+}
+
+
+
+
