4c89d329b39bc81cef8a357ab60bd566d84bb2c1
Christian Pich <cmpich@zfin.org>
2017-06-09 09:39:08 -0700

    comment out the upgrade. Some issue with it

--- source/org/zfin/db/load/1087/jackOlatern/INF-3020.sql
+++ source/org/zfin/db/load/1087/jackOlatern/INF-3020.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset christian:INF-3020
 
-ALTER TABLE marker_types ALTER COLUMN mrkrtype_type_display TYPE varchar(50);
+ALTER TABLE marker_types ALTER COLUMN mrkrtype_type_display TYPE varchar(50) ;
 
 UPDATE marker_types
 SET    mrkrtype_type_display = 'Transcriptional Regulatory Region'
