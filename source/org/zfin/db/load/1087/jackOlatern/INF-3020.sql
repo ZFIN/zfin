4e453f099953466e867511ae154fd6af1750e4e0
Christian Pich <cmpich@zfin.org>
2017-06-09 09:30:43 -0700

    INF-3020 widen marker_types.display column

---
+++ source/org/zfin/db/load/1087/jackOlatern/INF-3020.sql
@@ -0,0 +1,8 @@
+--liquibase formatted sql
+--changeset christian:INF-3020
+
+ALTER TABLE marker_types ALTER COLUMN mrkrtype_type_display TYPE varchar(50);
+
+UPDATE marker_types
+SET    mrkrtype_type_display = 'Transcriptional Regulatory Region'
+WHERE  marker_type = 'TRR';
