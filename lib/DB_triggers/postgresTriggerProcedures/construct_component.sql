6d0e4d0d6eb1bbee0a23e710e8333b47b136eb03
Sierra Moxon <smoxon@zfin.org>
2017-06-09 16:08:15 -0700

    INF-2927

---
+++ lib/DB_triggers/postgresTriggerProcedures/construct_component.sql
@@ -0,0 +1,17 @@
+drop trigger if exists construct_component_trigger on construct_component;
+
+create or replace function construct_component()
+returns trigger as
+$BODY$
+begin
+
+     select update_construct_name (NEW.cc_construct_zdb_id, NEW.cc_component_zdb_id);
+
+     RETURN NEW;
+
+end;
+$BODY$ LANGUAGE plpgsql;
+
+create trigger construct_component_trigger before update on construct_component
+ for each row
+ execute procedure construct_component();
