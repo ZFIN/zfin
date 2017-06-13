81ab2071a36373603181aefb67fdee63553e8844
Xiang Shao <xshao@zfin.org>
2017-06-13 15:18:57 -0700

    INF-3027 converted the citation list page linked from the bottom CITATIONS link on marker page

--- source/org/zfin/marker/presentation/PublicationMarkerViewController.java
+++ source/org/zfin/marker/presentation/PublicationMarkerViewController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.zfin.marker.Marker;
 import org.zfin.publication.Publication;
+import org.zfin.publication.presentation.PublicationListAdapter;
+import org.zfin.publication.presentation.PublicationListBean;
 import org.zfin.repository.RepositoryFactory;
 
 import java.util.List;
@@ -12,16 +15,18 @@ import java.util.List;
 /**
  */
 @Controller
-@RequestMapping("/antibody")
+@RequestMapping("/marker")
 public class PublicationMarkerViewController {
 
-    @RequestMapping("/publication-list/{zdbID}")
+    @RequestMapping("/citation-list/{zdbID}")
     public String getPublicationList(@PathVariable String zdbID,Model model){
+        Marker marker = RepositoryFactory.getMarkerRepository().getMarkerByID(zdbID);
+        model.addAttribute("marker",marker);
+        List<Publication> publications = RepositoryFactory.getPublicationRepository().getPubsForDisplay(zdbID);
+        model.addAttribute("pubCount",publications.size());
+        PublicationListBean citationBean = new PublicationListAdapter(publications);
+        model.addAttribute("citationList",citationBean);
 
-        List<Publication> publicationList = RepositoryFactory.getPublicationRepository().getPubsForDisplay(zdbID);
-        model.addAttribute("publications",publicationList) ;
-
-
-        return "marker/publication-list.page";
+        return "marker/citation-list.page";
     }
 }
