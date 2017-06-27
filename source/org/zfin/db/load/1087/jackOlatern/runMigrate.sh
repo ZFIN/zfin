2a2cac24be00e5ff25fd5225ad2057520f3c2a3c
Sierra Moxon <smoxon@zfin.org>
2017-06-27 12:55:37 -0700

    RGNS-92

--- source/org/zfin/db/load/1087/jackOlatern/runMigrate.sh
+++ source/org/zfin/db/load/1087/jackOlatern/runMigrate.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-!echo "Linc RNA";
+echo "Linc RNA";
 
 $INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlantern/migrateLincRNAgenes.sql;
 
-!echo "mirna RNA";
+echo "mirna RNA";
 
 $INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlantern/migrateMirnaRegions.sql
 
-!echo "ncrna RNA";
+echo "ncrna RNA";
 
 $INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlantern/migrateNcRNARegions.sql
 
-!echo "sno RNA";
+echo "sno RNA";
 
 $INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlantern/migrateSnoRegions.sql
 
-!echo "trna RNA";
+echo "trna RNA";
 $INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlantern/migrateTrnaRegions.sql
 
 
