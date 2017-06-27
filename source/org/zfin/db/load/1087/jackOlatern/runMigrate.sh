3a30a196659d4bb9dae359911ee99d058d21a072
Sierra Moxon <smoxon@zfin.org>
2017-06-27 13:05:26 -0700

    latern vs. lantern

--- source/org/zfin/db/load/1087/jackOlatern/runMigrate.sh
+++ source/org/zfin/db/load/1087/jackOlatern/runMigrate.sh
@@ -2,22 +2,22 @@
 
 echo "Linc RNA";
 
-$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlatern/migrateLincRNAgenes.sql;
+$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/source/org/zfin/db/load/1087/jackOlatern/migrateLincRNAgenes.sql;
 
 echo "mirna RNA";
 
-$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlatern/migrateMirnaRegions.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/source/org/zfin/db/load/1087/jackOlatern/migrateMirnaRegions.sql
 
 echo "ncrna RNA";
 
-$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlatern/migrateNcRNARegions.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/source/org/zfin/db/load/1087/jackOlatern/migrateNcRNARegions.sql
 
 echo "sno RNA";
 
-$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlatern/migrateSnoRegions.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/source/org/zfin/db/load/1087/jackOlatern/migrateSnoRegions.sql
 
 echo "trna RNA";
-$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/src/org/zfin/db/load/1087/jackOlatern/migrateTrnaRegions.sql
+$INFORMIXDIR/bin/dbaccess $DBNAME $SOURCEROOT/source/org/zfin/db/load/1087/jackOlatern/migrateTrnaRegions.sql
 
 
 
