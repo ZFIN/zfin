3fa2e3eeb43a799b467a34f208ec6c98b1d485cc
Sierra Moxon <smoxon@zfin.org>
2017-07-07 11:14:25 -0700

    RGNS-103

--- source/org/zfin/db/load/1088/kissingFace/RGNS-103.sql
+++ source/org/zfin/db/load/1088/kissingFace/RGNS-103.sql
@@ -1,79 +1,79 @@
 --liquibase formatted sql
 --changeset sierra:RGNS-103.sql
 
-create temp table tmp_id (id varchar(50))
+create temp table tmp_id2 (id varchar(50))
 with no log;
 
-insert into tmp_id (id)
+insert into tmp_id2 (id)
  select get_id('GENE') from single;
 
 insert into zdb_active_data (zactvd_zdb_id)
- select id from tmp_id;
+ select id from tmp_id2;
 
 insert into marker (mrkr_zdb_id, mrkr_abbrev, mrkr_name, mrkr_owner, mrkr_type)
   select id, mrkr_abbrev||"sierra", mrkr_name||"sierra", mrkr_owner, mrkr_type
-    from tmp_id, marker
+    from tmp_id2, marker
     where mrkr_zdb_id = 'ZDB-GENE-131127-502';
 
 insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id)
   select 'ZDB-GENE-131127-502', id
-    from tmp_id;
+    from tmp_id2;
 
 
 update record_attribution
- set recattrib_data_zdb_id = (Select id from tmp_id)
+ set recattrib_data_zdb_id = (Select id from tmp_id2)
   where recattrib_Data_zdb_id = 'ZDB-GENE-131127-502';
 
 update all_map_names
-  set allmapnm_zdb_id = (Select id from tmp_id)
+  set allmapnm_zdb_id = (Select id from tmp_id2)
   where allmapnm_zdb_id = 'ZDB-GENE-131127-502';
 
 update data_alias
-  set dalias_data_zdb_id = (select id from tmp_id)
+  set dalias_data_zdb_id = (select id from tmp_id2)
  where dalias_data_zdb_id = 'ZDB-GENE-131127-502';
 
 update db_link
-  set dblink_linked_recid = (select id from tmp_id)
+  set dblink_linked_recid = (select id from tmp_id2)
  where dblink_linked_recid = 'ZDB-GENE-131127-502';
 
 update expression_experiment
- set xpatex_gene_zdb_id = (select id from tmp_id)
+ set xpatex_gene_zdb_id = (select id from tmp_id2)
  where xpatex_gene_zdb_id = 'ZDB-GENE-131127-502';
 
 update expression_experiment2
-  set xpatex_gene_zdb_id = (select id from tmp_id)
+  set xpatex_gene_zdb_id = (select id from tmp_id2)
  where xpatex_gene_zdb_id = 'ZDB-GENE-131127-502';
 
 update marker_history
- set mhist_mrkr_zdb_id =  (select id from tmp_id)
+ set mhist_mrkr_zdb_id =  (select id from tmp_id2)
  where mhist_mrkr_zdb_id = 'ZDB-GENE-131127-502';
 
 update marker_history_audit
- set mha_mrkr_zdb_id = (select id from tmp_id)
+ set mha_mrkr_zdb_id = (select id from tmp_id2)
  where mha_mrkr_zdb_id = 'ZDB-GENE-131127-502';
 
 update marker_relationship
- set mrel_mrkr_1_zdb_id = (select id from tmp_id)
+ set mrel_mrkr_1_zdb_id = (select id from tmp_id2)
  where mrel_mrkr_1_zdb_id = 'ZDB-GENE-131127-502';
 
 update marker_relationship
- set mrel_mrkr_2_zdb_id = (select id from tmp_id)
+ set mrel_mrkr_2_zdb_id = (select id from tmp_id2)
  where mrel_mrkr_2_zdb_id = 'ZDB-GENE-131127-502';
 
 update updates
- set rec_id = (select id from tmp_id)
+ set rec_id = (select id from tmp_id2)
  where rec_id = 'ZDB-GENE-131127-502';
 
 update sequence_feature_chromosome_location_generated
- set sfclg_data_zdb_id = (select id from tmp_id)
+ set sfclg_data_zdb_id = (select id from tmp_id2)
  where sfclg_data_zdb_id = 'ZDB-GENE-131127-502';
 
 update unique_location
- set ul_data_zdb_id = (select id from tmp_id)
+ set ul_data_zdb_id = (select id from tmp_id2)
  where ul_data_zdb_id = 'ZDB-GENE-131127-502';
 
 update clean_expression_fast_Search
- set cefs_mrkr_Zdb_id = (select id from tmp_id)
+ set cefs_mrkr_Zdb_id = (select id from tmp_id2)
  where cefs_mrkr_zdb_id = 'ZDB-GENE-131127-502';
 
 
@@ -82,8 +82,8 @@ delete from marker
 
 update marker
   set mrkr_Abbrev = replace(mrkr_abbrev, 'sierra', '')
- where mrkr_zdb_id = (Select id from tmp_id);
+ where mrkr_zdb_id = (Select id from tmp_id2);
 
 update marker
   set mrkr_name = replace(mrkr_name, 'sierra', '')
- where mrkr_zdb_id = (Select id from tmp_id);
+ where mrkr_zdb_id = (Select id from tmp_id2);
