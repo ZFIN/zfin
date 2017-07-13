0c1b3a143a6d9914c255d2b34f5247cc3b5cf76e
Prita Mani <pm@zfin.org>
2017-07-13 14:04:56 -0700

    JENK-231

--- source/org/zfin/db/load/1088/kissingFace/updateDailyIndexedMetric.sql
+++ source/org/zfin/db/load/1088/kissingFace/updateDailyIndexedMetric.sql
@@ -1,7 +1,7 @@
 --liquibase formated sql
 --changeset pm:updateDailyIndexedMetric
 
-alter table marker_type_group
+alter table daily_indexed_metric
 add (dim_number_orthology_bin int default 0 not null constraint dim_number_orthology_bin_not_null);
 
 
