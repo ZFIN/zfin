5ddb716553a675093b19cf8e2f319b57ed879677
Prita Mani <pm@zfin.org>
2017-07-20 12:55:51 -0700

    ZFIN-5718

--- source/org/zfin/db/postGmakePostloaddb/1089/Lemon/ZFIN-5718.sql
+++ source/org/zfin/db/postGmakePostloaddb/1089/Lemon/ZFIN-5718.sql
@@ -1,37 +1,25 @@
 --liquibase formatted sql
 --changeset prita:ZFIN-5671
 
-update construct set construct_name='Tg(rnu6-32:CRISPR1-tyr,rnu6-32:CRISPR1-insra,rnu6-14:CRISPR2-insra,rnu6-7:CRISPR1-insrb,rnu6-279:CRISPR2-insrb,cryaa:Cerulean)' where construct_zdb_id='ZDB-TGCONSTRCT-170419-4';
-update marker set mrkr_name='Tg(rnu6-32:CRISPR1-tyr,rnu6-32:CRISPR1-insra,rnu6-14:CRISPR2-insra,rnu6-7:CRISPR1-insrb,rnu6-279:CRISPR2-insrb,cryaa:Cerulean)' where mrkr_zdb_id='ZDB-TGCONSTRCT-170419-4';
-update marker set mrkr_abbrev='Tg(rnu6-32:CRISPR1-tyr,rnu6-32:CRISPR1-insra,rnu6-14:CRISPR2-insra,rnu6-7:CRISPR1-insrb,rnu6-279:CRISPR2-insrb,cryaa:Cerulean)' where mrkr_zdb_id='ZDB-TGCONSTRCT-170419-4';
+update construct set construct_name='Tg(UAS:Hsa.JAG1,myl7:EGFP)' where construct_zdb_id='ZDB-TGCONSTRCT-100726-2';
+update marker set mrkr_name='Tg(UAS:Hsa.JAG1,myl7:EGFP)' where mrkr_zdb_id='ZDB-TGCONSTRCT-100726-2';
+update marker set mrkr_abbrev='Tg(UAS:Hsa.JAG1,myl7:EGFP)' where mrkr_zdb_id='ZDB-TGCONSTRCT-100726-2';
 
-delete from construct_component where cc_construct_Zdb_id='ZDB-TGCONSTRCT-170419-4' and cc_order=14;
+delete from construct_component where cc_construct_Zdb_id='ZDB-TGCONSTRCT-100726-2' and cc_order=8;
 
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-11',',',4,14);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','promoter of','promoter component','ZDB-NCRNAG-170419-3','rnu6-7',4,15);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-10',':',4,16);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','coding sequence of','coding component','ZDB-CRISPR-150924-3','CRISPR1-insrb',4,17);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-100726-2','controlled vocab component','promoter component','ZDB-CV-150506-11',',',2,8);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-100726-2','promoter of','promoter component','ZDB-GENE-991019-3','myl7',2,9);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-100726-2','controlled vocab component','promoter component','ZDB-CV-150506-10',':',2,10);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-100726-2','coding sequence of','coding component','ZDB-EFG-070117-1','EGFP',2,11);
 
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-11',',',5,18);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','promoter of','promoter component','ZDB-NCRNAG-170419-4','rnu6-279',5,19);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-10',':',5,20);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','coding sequence of','coding component','ZDB-CRISPR-150925-1','CRISPR2-insrb',5,21);
 
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-11',',',6,22);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','promoter of','promoter component','ZDB-GENE-020508-1','cryaa',6,23);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','promoter component','ZDB-CV-150506-10',':',6,24);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','coding sequence of','coding component','ZDB-EFG-110824-3','Cerulean',6,25);
 
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-170419-4','controlled vocab component','construct wrapper component','ZDB-CV-150506-8',')',6,26);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-100726-2','controlled vocab component','construct wrapper component','ZDB-CV-150506-8',')',2,12);
 
 create temp table tmp_cmrel (relid varchar(50), consid varchar(50),mrkrid varchar(50),reltype varchar(50)) with no log;
 
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-NCRNAG-170419-3','promoter of');
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-CRISPR-150924-3','coding sequence of');
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-NCRNAG-170419-4','promoter of');
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-CRISPR-150925-1','coding sequence of');
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-GENE-020508-1','promoter of');
-insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-EFG-110824-3','coding sequence of');
+insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-100726-2','ZDB-GENE-991019-3','promoter of');
+insert into tmp_cmrel(relid,consid,mrkrid,reltype) values (get_id('CMREL'),'ZDB-TGCONSTRCT-100726-2','ZDB-EFG-070117-1','coding sequence of');
 
 insert into zdb_active_data (zactvd_zdb_id) select relid from tmp_cmrel;
 
@@ -41,17 +29,13 @@ drop table tmp_cmrel;
 
 create temp table tmp_mrel (relid varchar(50), consid varchar(50),mrkrid varchar(50),reltype varchar(50)) with no log;
 
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-NCRNAG-170419-3','promoter of');
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-CRISPR-150924-3','coding sequence of');
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-NCRNAG-170419-4','promoter of');
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-CRISPR-150925-1','coding sequence of');
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-GENE-020508-1','promoter of');
-insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-170419-4','ZDB-EFG-110824-3','coding sequence of');
+insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-100726-2','ZDB-GENE-991019-3','promoter of');
+insert into tmp_mrel(relid,consid,mrkrid,reltype) values (get_id('MREL'),'ZDB-TGCONSTRCT-100726-2','ZDB-EFG-070117-1','coding sequence of');
 
 insert into zdb_active_data (zactvd_zdb_id) select relid from tmp_mrel;
 
 insert into marker_relationship(mrel_zdb_id,mrel_mrkr_1_zdb_id,mrel_mrkr_2_zdb_id,mrel_type) select relid,consid,mrkrid,reltype from tmp_mrel;
 
-insert into record_attribution (recattrib_Data_zdb_id,recattrib_source_zdb_id,recattrib_source_type) select relid,'ZDB-PUB-150410-5','standard' from tmp_mrel;
+insert into record_attribution (recattrib_Data_zdb_id,recattrib_source_zdb_id,recattrib_source_type) select relid,'ZDB-PUB-100504-15','standard' from tmp_mrel;
 
 drop table tmp_mrel;
