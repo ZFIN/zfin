09acad79ae6958cbf460658af0912183f65d5a8e
Sierra Moxon <smoxon@zfin.org>
2017-07-23 22:41:10 -0700

    JENK-257

--- server_apps/Reports/PubTracking/longestBinResidents.sql
+++ server_apps/Reports/PubTracking/longestBinResidents.sql
@@ -101,7 +101,7 @@ insert into monthly_average_curated_metric (macm_date_captured,
         (select nvl( max(date(current year to second)-date(pth_status_insert_date)),0)
 		from pub_tracking_history, pub_tracking_location, pub_tracking_status
 		     where pth_location_id = ptl_pk_id
-		     and ptl_location = 'NEW_ORTHO'
+		     and ptl_location = 'ORTHO'
 		     and pts_pk_id = pth_status_id
 		     and pts_status = 'READY_FOR_CURATION'
 		     and pth_status_is_current = 't')
@@ -211,17 +211,17 @@ insert into longest_bin_resident_metric (lbrm_date_captured,
        	    			         lbrm_pub_zdb_id,
 					 lbrm_status_counted,
 					 lbrm_days_in_status_on_this_date)
- select distinct id, pth_pub_zdb_id, 'NEW_ORTHO',(date(id)-date(pth_status_insert_date))
+ select distinct id, pth_pub_zdb_id, 'ORTHO',(date(id)-date(pth_status_insert_date))
 		from pub_tracking_history, pub_tracking_location, pub_tracking_status, publication, tmp_id
 		     where pth_location_id = ptl_pk_id
-		     and ptl_location = 'NEW_ORTHO'
+		     and ptl_location = 'ORTHO'
 		     and pts_pk_id = pth_status_id
 		     and pts_status = 'READY_FOR_CURATION'
 		     and pth_status_is_current = 't'
 		     and date(current year to second)-date(pth_status_insert_date) = (select max(date(current year to second)-date(pth_status_insert_date))
 		     	 					  	  from pub_tracking_history, pub_tracking_location, pub_tracking_status
 		     							  where pth_location_id = ptl_pk_id
-		     							  and ptl_location = 'NEW_ORTHO'
+		     							  and ptl_location = 'ORTHO'
 		     							  and pts_pk_id = pth_status_id
 		     							  and pts_status = 'READY_FOR_CURATION'
 									  and pth_status_is_current = 't');			 
