405d46770cc5d7a020d160fb971932749ce3f6e1
Prita Mani <pm@zfin.org>
2017-07-26 09:25:14 -0700

    CLNDTY-1

--- lib/DB_functions/SPL/Regen_genox/regen_genox_process_genox.sql
+++ lib/DB_functions/SPL/Regen_genox/regen_genox_process_genox.sql
@@ -18,12 +18,15 @@ create procedure regen_genox_process_genox()
 
 -- The genotypes include any of the WT lines, which (cannot be with 'Standard' or 'Generic Control' environments),
 -- and has MO(s) which target ONLY this gene
+--exclude features of type DEFICIENCY, TRANSLOCATION and INVERSION
 insert into regen_genox_temp (rggt_mrkr_zdb_id, rggt_genox_zdb_id)
   select distinct fmrel_mrkr_zdb_id , genox_zdb_id
-    from fish, fish_experiment, feature_marker_relationship, genotype_Feature, regen_genox_input_zdb_id_temp
+    from fish, fish_experiment, feature_marker_relationship, genotype_Feature, feature,regen_genox_input_zdb_id_temp
     where fish_zdb_id = genox_fish_Zdb_id
     and fish_genotype_zdb_id = genofeat_geno_zdb_id
     and genofeat_feature_Zdb_id = fmrel_ftr_zdb_id
+    and genofeat_feature_zdb_id=feature_zdb_id
+    and feature_type not in ('DEFICIENCY','TRANSLOC')
     and fish_functional_affected_gene_count = 1
  and get_obj_type(fmrel_mrkr_Zdb_id) in ('GENE','MRPHLNO','TALEN','CRISPR', 'LNCRNAG', 'LINCRNAG','MIRNAG','PIRNAG','SCRNAG','SNORNAG', 'TRNAG','RRNAG','NCRNAG','SRPRNAG')
     and genox_is_std_or_generic_control = 't'
@@ -34,9 +37,13 @@ and not exists (Select 'x' from regen_genox_temp
 
 insert into regen_genox_temp (rggt_mrkr_zdb_id, rggt_genox_zdb_id)
   select fishstr_str_zdb_id, genox_zdb_id
-    from fish, fish_str a, fish_experiment, regen_genox_input_zdb_id_temp
+    from fish, fish_str a, fish_experiment,feature_marker_relationship, genotype_Feature, feature, regen_genox_input_zdb_id_temp
     where fish_Zdb_id =a.fishstr_fish_Zdb_id
     and fish_zdb_id = genox_fish_zdb_id
+    and fish_genotype_zdb_id = genofeat_geno_zdb_id
+    and genofeat_feature_Zdb_id = fmrel_ftr_zdb_id
+    and genofeat_feature_zdb_id=feature_zdb_id
+    and feature_type not in ('DEFICIENCY','TRANSLOC')
     and fish_functional_affected_gene_count =1
     and genox_is_std_or_generic_control = 't'
     and genox_zdb_id = rggz_zdb_id
@@ -49,9 +56,13 @@ and not exists (Select 'x' from regen_genox_temp
 
 insert into regen_genox_temp (rggt_mrkr_zdb_id, rggt_genox_zdb_id)
 select mrel_mrkr_2_zdb_id, genox_zdb_id
-    from fish, fish_str, fish_experiment, marker_relationship, regen_genox_input_zdb_id_temp
+    from fish, fish_str, fish_experiment,feature_marker_relationship, genotype_Feature, feature, marker_relationship, regen_genox_input_zdb_id_temp
     where fish_Zdb_id =fishstr_fish_Zdb_id
     and fish_zdb_id = genox_fish_zdb_id
+    and fish_genotype_zdb_id = genofeat_geno_zdb_id
+    and genofeat_feature_Zdb_id = fmrel_ftr_zdb_id
+    and genofeat_feature_zdb_id=feature_zdb_id
+    and feature_type not in ('DEFICIENCY','TRANSLOC')
     and fish_functional_affected_gene_count =1
     and fishstr_str_zdb_id = mrel_mrkr_1_zdb_id
     and get_obj_type(mrel_mrkr_2_Zdb_id) in ('GENE')
