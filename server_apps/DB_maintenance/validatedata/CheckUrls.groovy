bdfeec40f9ee33bd1486297c4ece2be5cef5ac88
Christian Pich <cmpich@zfin.org>
2017-08-08 07:46:53 -0700

    INF-750 add sorting to lab/person/company

--- server_apps/DB_maintenance/validatedata/CheckUrls.groovy
+++ server_apps/DB_maintenance/validatedata/CheckUrls.groovy
@@ -1,211 +1,194 @@
 #!/bin/bash
 //usr/bin/env groovy -cp "$GROOVY_CLASSPATH" "$0" $@; exit $?
-
-import groovy.json.JsonSlurper
 import org.hibernate.Session
+import org.hibernate.criterion.Order
 import org.zfin.framework.HibernateSessionCreator
 import org.zfin.framework.HibernateUtil
-import org.zfin.Species
-import org.zfin.infrastructure.RecordAttribution
+import org.zfin.profile.Company
+import org.zfin.profile.Lab
+import org.zfin.profile.Person
 import org.zfin.properties.ZfinProperties
-import org.zfin.repository.RepositoryFactory
-import org.zfin.sequence.ForeignDB
-import org.zfin.sequence.ForeignDBDataType
-import org.zfin.sequence.MarkerDBLink
-import org.zfin.sequence.ReferenceDatabase
 import org.zfin.util.ReportGenerator
-import org.zfin.profile.*
 
+import java.util.concurrent.*
 
-import java.util.concurrent.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.*;
-
-
-import org.apache.commons.validator.UrlValidator
-
-import java.util.zip.GZIPInputStream
-import java.util.zip.ZipException
-
- static void main(String... args) {   
-cli = new CliBuilder(usage: 'LoadAddgene')
-cli.propertyFilePath(args: 1, 'Name of the job to be displayed in report')
-cli.dataDirectory(args: 2, 'Name of the job to be displayed in report')
-cli.jobName(args: 3, 'Name of the job to be displayed in report')
-options = cli.parse(args)
-if (!options) {
-    System.exit(1)
-}
-println options.propertyFilePath
-println options.dataDirectory
-println options.jobName
-
-ZfinProperties.init("${System.getenv()['TARGETROOT']}/home/WEB-INF/zfin.properties")
-new HibernateSessionCreator()
-
-Session session = HibernateUtil.currentSession()
-tx = session.beginTransaction()
-
-
-        List<Lab> labList = new ArrayList<>();
-        labList.addAll((List<Lab>) session.createCriteria(Lab.class)
-                .list());
-
-print labList.size() + " labs";
-
-int index = 0;
-List<String> brokenUrls = new ArrayList<>();
-labList.any({lab ->
-    
-    def url = lab.getUrl()
-    if (url != null){
-    if(!isRespondingUrl(url)){
-        println url + " "+ getResponseCode(url);
-	List<String> values = new ArrayList<>();
-	values.add(lab.getZdbID())
-	values.add(lab.getName())
-	values.add(url)
-	brokenUrls.add(values);
+static void main(String... args) {
+    cli = new CliBuilder(usage: 'LoadAddgene')
+    cli.propertyFilePath(args: 1, 'Name of the job to be displayed in report')
+    cli.dataDirectory(args: 2, 'Name of the job to be displayed in report')
+    cli.jobName(args: 3, 'Name of the job to be displayed in report')
+    cli.shortVersion(args: 4, 'Name of the job to be displayed in report')
+    options = cli.parse(args)
+    if (!options) {
+        System.exit(1)
     }
-	index++;
-//	if(index++ > 10)
-//	return true;
+    boolean shortVersion = options.shortVersion
+    println options.propertyFilePath
+    println options.dataDirectory
+    println options.jobName
 
-	return;
-}
-})
+    ZfinProperties.init("${System.getenv()['TARGETROOT']}/home/WEB-INF/zfin.properties")
+    new HibernateSessionCreator()
 
-println index + " Labs have broken links"
-index = 0;
-
-        List<Person> personList = new ArrayList<>();
-        personList.addAll((List<Person>) session.createCriteria(Person.class)
-                .list());
-
-personList.any({person ->
-
-    def url = person.getUrl()
-    if (url != null){
-    if(!isRespondingUrl(url)){
-        println url + " "+ getResponseCode(url);
-        List<String> values = new ArrayList<>();
-        values.add(person.getZdbID())
-        values.add(person.getShortName())
-        values.add(url)
-        brokenUrls.add(values);
-    }
-//   	if(index++ > 10)
-//      return true;
-
-        return;
-}
-})
-
-println index + " Persons have broken links"
-index = 0;
-
-       List<Company> companyList = new ArrayList<>();
-        companyList.addAll((List<Company>) session.createCriteria(Company.class)
-               	.list());
-
-companyList.any({company ->
-
-    def url = company.getUrl()
-    if (url != null){
-    if(!isRespondingUrl(url)){
-        println url + " "+ getResponseCode(url);
-        List<String> values = new ArrayList<>();
-        values.add(company.getZdbID())
-        values.add(company.getName())
-        values.add(url)
-       	brokenUrls.add(values);
-    }
-//      if(index++ > 10)
-//      return true;
-
-        return;
-}
-})
-
-println index + " Companies have broken links"
-
-println brokenUrls.size()
-session.close()
+    Session session = HibernateUtil.currentSession()
+    tx = session.beginTransaction()
 
 
-	File reportDir = new File(options.dataDirectory+"/"+options.jobName)
-	reportDir.deleteDir()
-        reportDir.mkdirs()
-    print "Generating report ... "
-    ReportGenerator rg = new ReportGenerator();
+    List<Lab> labList = new ArrayList<>()
+    labList.addAll((List<Lab>) session.createCriteria(Lab.class)
+            .addOrder(Order.asc("name"))
+            .list())
+
+    println "Total labs: " + labList.size()
+
+    List<List<String>> brokenLabUrls = new ArrayList<>()
+    int index = 0
+    labList.any({ lab ->
+        def url = lab.getUrl()
+        if (url != null) {
+            if (!isRespondingUrl(url)) {
+                println url + " " + getResponseCode(url)
+                List<String> values = new ArrayList<>()
+                values.add(lab.getZdbID())
+                values.add(lab.getName())
+                values.add(url)
+                brokenLabUrls.add(values)
+            }
+            index++
+            if (shortVersion && index > 10)
+                return true
+            return
+        }
+    })
+
+    List<List<String>> brokenPersonUrls = new ArrayList<>()
+    List<Person> personList = new ArrayList<>()
+    personList.addAll((List<Person>) session.createCriteria(Person.class)
+            .addOrder(Order.asc("shortName"))
+            .list())
+
+    index = 0
+    personList.any({ person ->
+
+        def url = person.getUrl()
+        if (url != null) {
+            if (!isRespondingUrl(url)) {
+                println url + " " + getResponseCode(url)
+                List<String> values = new ArrayList<>()
+                values.add(person.getZdbID())
+                values.add(person.getShortName())
+                values.add(url)
+                brokenPersonUrls.add(values)
+            }
+            index++
+            if (shortVersion && index > 10)
+                return true
+
+            return
+        }
+    })
+
+    List<List<String>> brokenCompanyUrls = new ArrayList<>()
+    List<Company> companyList = new ArrayList<>()
+    companyList.addAll((List<Company>) session.createCriteria(Company.class)
+            .addOrder(Order.asc("name"))
+            .list())
+
+    index = 0
+    companyList.any({ company ->
+
+        def url = company.getUrl()
+        if (url != null) {
+            if (!isRespondingUrl(url)) {
+                println url + " " + getResponseCode(url)
+                List<String> values = new ArrayList<>()
+                values.add(company.getZdbID())
+                values.add(company.getName())
+                values.add(url)
+                brokenCompanyUrls.add(values)
+            }
+            index++
+            if (shortVersion && index > 10)
+                return true
+
+            return
+        }
+    })
+
+    session.close()
+
+
+    File reportDir = new File(options.dataDirectory + "/" + options.jobName)
+    reportDir.deleteDir()
+    reportDir.mkdirs()
+    println "Generating report ... "
+    ReportGenerator rg = new ReportGenerator()
     rg.setReportTitle("Report for $options.jobName")
-    rg.includeTimestamp();
-    rg.addIntroParagraph("The following URLs are broken:")
-    rg.addDataTable("${brokenUrls.size()} Lab / Person / Copany records with broken URLs", ["ID", "Name", "URL"], brokenUrls.collect { link -> [link.get(0), link.get(1), link.get(2)] })
-    new File(reportDir,"check-urls-report.html").withWriter { writer ->
+    rg.includeTimestamp()
+    Map<String, Integer> summary = new LinkedHashMap<>()
+    summary.put("Lab", brokenLabUrls.size)
+    summary.put("Company", brokenCompanyUrls.size)
+    summary.put("Person", brokenPersonUrls.size)
+    rg.addSummaryTable("Summary: Broken URL links (404)", summary)
+    rg.addDataTable("Borken Lab URLs: ${brokenLabUrls.size()}", ["ID", "Name", "URL"], brokenLabUrls.collect { link -> [link.get(0), link.get(1), link.get(2)] })
+    rg.addDataTable("Broken Person URLs: ${brokenPersonUrls.size()}", ["ID", "Name", "URL"], brokenPersonUrls.collect { link -> [link.get(0), link.get(1), link.get(2)] })
+    rg.addDataTable("Broken Company URLs: ${brokenCompanyUrls.size()}", ["ID", "Name", "URL"], brokenCompanyUrls.collect { link -> [link.get(0), link.get(1), link.get(2)] })
+    new File(reportDir, "check-urls-report.html").withWriter { writer ->
         rg.write(writer, ReportGenerator.Format.HTML)
     }
 
 
-println "done"
-if(brokenUrls.size() > 0)
-	println "Validation Errors found"
+    if (brokenLabUrls.size() > 0 || brokenPersonUrls.size() > 0 || brokenCompanyUrls.size() > 0)
+        println "Validation Errors found"
 
-System.exit(0)
+    System.exit(0)
 }
 
-    public static int getResponseCode(String urlString) throws MalformedURLException, IOException {
-        URL u = new URL(urlString);
-        HttpURLConnection huc =  (HttpURLConnection)  u.openConnection();
-        huc.setRequestMethod("GET");
-        ExecutorService executor = Executors.newSingleThreadExecutor();
-        Future<Integer> future = executor.submit(new Task(huc));
-        Integer responseCode;
-        try {
-            responseCode = future.get(3, TimeUnit.SECONDS);
-        } catch (TimeoutException e) {
-            return -1;
-        } catch (InterruptedException e) {
-            return -1;
-        } catch (ExecutionException e) {
-            return -1;
-        } finally {
-            executor.shutdownNow();
-        }
-        return responseCode;
+static int getResponseCode(String urlString) throws MalformedURLException, IOException {
+    URL u = new URL(urlString)
+    HttpURLConnection huc = (HttpURLConnection) u.openConnection()
+    huc.setRequestMethod("HEAD")
+    ExecutorService executor = Executors.newSingleThreadExecutor()
+    Future<Integer> future = executor.submit(new Task(huc))
+    Integer responseCode = -1
+    try {
+        responseCode = future.get(3, TimeUnit.SECONDS)
+    } catch (TimeoutException ignored) {
+        return -1
+    } catch (InterruptedException ignored) {
+        return -1
+    } catch (ExecutionException ignored) {
+        return -1
+    } finally {
+        executor.shutdownNow()
+    }
+    return responseCode
+}
+
+static boolean isRespondingUrl(String url) {
+    int responseCode = 404
+    try {
+        responseCode = getResponseCode(url)
+    } catch (IOException ignored) {
+        return false
     }
 
-    public static boolean isRespondingUrl(String url){
-	int responseCode = 404;
-        try {
-            responseCode = getResponseCode(url);
-        } catch (IOException e) {
-            return false;
-        }
-
-	List<Integer> okCodes = new ArrayList<>();
-	okCodes.add(200);
-	okCodes.add(301);
-	okCodes.add(303);
-//	if(!okCodes.contains(responseCode))
-//		println url + ", "+ responseCode;
-        return okCodes.contains(responseCode);
-    }
+    List<Integer> brokenCodes = new ArrayList<>()
+    brokenCodes.add(404)
+    return !brokenCodes.contains(responseCode)
+}
 
 class Task implements Callable<Integer> {
-    private HttpURLConnection huc;
+    private HttpURLConnection huc
 
     Task(HttpURLConnection huc) {
-        this.huc = huc;
+        this.huc = huc
     }
 
     @Override
     public Integer call() throws Exception {
-        huc.connect();
-        return huc.getResponseCode();
+        huc.connect()
+        return huc.getResponseCode()
     }
 }
 
