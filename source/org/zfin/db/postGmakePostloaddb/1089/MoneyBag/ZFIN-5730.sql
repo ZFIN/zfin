ffdbd5e9940a68550d80862ed4ef9c350158be24
Prita Mani <pm@zfin.org>
2017-08-09 11:56:44 -0700

    ZFIN-5730

--- source/org/zfin/db/postGmakePostloaddb/1089/MoneyBag/ZFIN-5730.sql
+++ source/org/zfin/db/postGmakePostloaddb/1089/MoneyBag/ZFIN-5730.sql
@@ -2,4 +2,4 @@
 --changeset prita:ZFIN-5730
 
 insert into zdb_active_Data (zactvd_Zdb_id) values ('ZDB-CV-170809-1');
-insert into controlled_vocabulary (cv_zdb_id,cv_term_name,cv_foreign_species,cv_name_definition) values ('ZDB-CV-170809-1','Pvi.','Asian green mussel','Perna viridis');
+insert into controlled_vocabulary (cv_zdb_id,cv_term_name,cv_foreign_species,cv_name_definition) values ('ZDB-CV-170809-1','Sce.','Yeast','Saccharomyces cerevisiae');
