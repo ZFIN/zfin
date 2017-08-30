3c116d86ae9eb2aff498500bcb41cba1ba5e7503
Sierra Moxon <smoxon@zfin.org>
2017-08-30 14:38:24 -0700

    added more human disease info

--- server_apps/DB_maintenance/postgres/createBinaryDump.sh
+++ server_apps/DB_maintenance/postgres/createBinaryDump.sh
@@ -10,6 +10,8 @@ cd $dumpLocation
 latestDump=`ls -td -- */ | head -n 1 | cut -d'/' -f1`
 echo $latestDump
 
+mkdir /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump
+
 # dump the fixed database
 ${PGBINDIR}/pg_dump ${DBNAME} > /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump
 
