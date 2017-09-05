80f79d6577f0b75de604e1868b8ffff9330ab48b
Sierra Moxon <smoxon@zfin.org>
2017-09-05 09:00:24 -0700

    INF-3078

--- server_apps/DB_maintenance/postgres/createBinaryDump.sh
+++ server_apps/DB_maintenance/postgres/createBinaryDump.sh
@@ -10,9 +10,9 @@ cd $dumpLocation
 latestDump=`ls -td -- */ | head -n 1 | cut -d'/' -f1`
 echo $latestDump
 
-mkdir /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump
+#mkdir /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump
 # dump the fixed database
-${PGBINDIR}/pg_dump -Fd ${DBNAME} -j 5 -f /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump 
+${PGBINDIR}/pg_dump -Fc ${DBNAME} -j 5 -f /research/zunloads/databases/postgres_self_dumps/${DBNAME}/$latestDump 
 
 #latestBinaryDump=$latestDump.bak
 
