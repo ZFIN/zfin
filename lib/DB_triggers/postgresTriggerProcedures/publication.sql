6b2aa06d98a17cbaa78387323bf9e65f777c8ef1
Sierra Moxon <smoxon@zfin.org>
2017-09-06 15:07:37 -0700

    fixing scrub_char

--- lib/DB_triggers/postgresTriggerProcedures/publication.sql
+++ lib/DB_triggers/postgresTriggerProcedures/publication.sql
@@ -3,42 +3,32 @@ drop trigger if exists publication_trigger on publication;
 create or replace function publication()
 returns trigger as
 $BODY$
-declare title publication.title%TYPE;
-	accession_no publication.accession_no%TYPE;
-	pubmed_authors publication.pubmed_authors%TYPE;
-	pub_doi publication.pub_doi%TYPE;
-	pub_authors_lower publication.pub_authors_lower%TYPE;
-	jtype publication.jtype%TYPE;
-	pub_mini_ref publication.pub_mini_ref%TYPE;
-	pub_pages publication.pub_pages%TYPE;
-	pub_can_show_images publication.pub_can_show_images%TYPE;
+declare title publication.title%TYPE := scrub_char(NEW.title);
+	accession_no publication.accession_no%TYPE := scrub_char(NEW.accession_no);
+	pubmed_authors publication.pubmed_authors%TYPE := scrub_char(NEW.authors);
+	pub_doi publication.pub_doi%TYPE := scrub_char(NEW.pub_doi);
+	pub_authors_lower publication.pub_authors_lower%TYPE := lower(NEW.authors);
+	jtype publication.jtype%TYPE := scrub_char(NEW.jtype);
+	pub_mini_ref publication.pub_mini_ref%TYPE := scrub_char(get_pub_mini_ref(NEW.pub_mini_ref));
+	pub_pages publication.pub_pages%TYPE := scrub_char(NEW.pub_pages);
+	pub_can_show_images publication.pub_can_show_images%TYPE := get_pub_default_permissions(NEW.pub_jrnl_zdb_id);
 
 begin
 
-     title = (select scrub_char(NEW.title));
      NEW.title = title;
-
     
-     pubmed_authors = (select scrub_char(NEW.pubmed_authors));
      NEW.pubmed_authors = pubmed_authors;
 
-     jtype = (select scrub_char(NEW.jtype));
      NEW.jtype = jtype;
 
-     pub_authors_lower = lower(NEW.authors);
      NEW.authors = pub_authors_lower;
 
-     pub_pages = (select scrub_char(NEW.pub_pages));
      NEW.pub_pages = pub_pages;
 
-     pub_doi = (select scrub_char(NEW.pub_doi));
      NEW.pub_doi = pub_doi;
 	
-     pub_mini_ref = (select get_pub_mini_ref(NEW.pub_mini_ref));
-     pub_mini_ref = (select scrub_char(pub_mini_ref));
      NEW.pub_mini_ref = pub_mini_ref;
 
-     pub_can_show_images = (select get_pub_default_permissions(NEW.pub_jrnl_zdb_id));
      NEW.pub_can_show_images = pub_can_show_images;
 
      RETURN NEW;
