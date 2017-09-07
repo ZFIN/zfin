3e79849c0cf0da4d50faae0c43eda826a57a709d
Christian Pich <cmpich@zfin.org>
2017-09-06 18:20:42 -0700

    INF-3110 fix query to use JOIN and left outer join

--- server_apps/DB_maintenance/validatedata/Check-Term-Stage-Range_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Term-Stage-Range_d.sql
@@ -1,6 +1,4 @@
-
-select
-   fig_source_zdb_id,
+SELECT fig_source_zdb_id,
    fig_zdb_id,
    s1.stg_name_long,
    s2.stg_name_long,
@@ -13,25 +11,23 @@ select
    s3.stg_name_long,
    s4.stg_name_long,
    xpatres_pk_id
-from
-   expression_result2,
-   term_stage,
-   stage s1,
-   stage s2,
-   stage s3,
-   stage s4,
-   term super,
-   outer term sub,
-   figure,
-   expression_figure_stage
-where
-   aoterm_overlaps_stg_window(xpatres_superterm_zdb_id, efs_start_stg_zdb_id, efs_end_stg_zdb_id) = "f"
-   and ts_start_stg_zdb_id = s3.stg_zdb_id
-   and ts_end_stg_zdb_id = s4.stg_zdb_id
-   and super.term_zdb_id = ts_term_zdb_id
-   and xpatres_superterm_zdb_id = super.term_zdb_id
-   and efs_start_stg_zdb_id = s1.stg_zdb_id
-   and efs_end_stg_zdb_id = s2.stg_zdb_id
-   and efs_fig_zdb_id = fig_zdb_id
-   and xpatres_efs_id = efs_pk_id
-   and xpatres_subterm_zdb_id = sub.term_zdb_id;
+FROM   expression_result2
+   LEFT OUTER JOIN term sub
+      ON xpatres_subterm_zdb_id = sub.term_zdb_id
+   JOIN expression_figure_stage
+      ON xpatres_efs_id = efs_pk_id
+   JOIN figure
+      ON efs_fig_zdb_id = fig_zdb_id
+   JOIN term super
+      ON xpatres_superterm_zdb_id = super.term_zdb_id
+   JOIN stage s1
+      ON efs_start_stg_zdb_id = s1.stg_zdb_id
+   JOIN stage s2
+      ON efs_end_stg_zdb_id = s2.stg_zdb_id
+   JOIN term_stage
+      ON super.term_zdb_id = ts_term_zdb_id
+   JOIN stage s3
+      ON ts_start_stg_zdb_id = s3.stg_zdb_id
+   JOIN stage s4
+      ON ts_end_stg_zdb_id = s4.stg_zdb_id
+WHERE  Aoterm_overlaps_stg_window(xpatres_superterm_zdb_id, efs_start_stg_zdb_id, efs_end_stg_zdb_id) = 'f'  
