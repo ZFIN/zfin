78c093f08aad87c39c7071d0162d6c661b6ec149
Christian Pich <cmpich@zfin.org>
2017-09-06 18:29:29 -0700

    INF-3106 use single quotes instead of double quotes in query

--- server_apps/DB_maintenance/validatedata/Check-EST-No-Clone-No-XX-Gene_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-EST-No-Clone-No-XX-Gene_w.sql
@@ -2,7 +2,7 @@ SELECT mrkr_zdb_id,
        mrkr_name,
        mrkr_abbrev
 FROM   marker est
-WHERE  mrkr_type = "EST"
+WHERE  mrkr_type = 'EST'
        AND NOT EXISTS (SELECT *
                        FROM   clone
                        WHERE  clone_mrkr_zdb_id = est.mrkr_zdb_id)
@@ -11,8 +11,8 @@ WHERE  mrkr_type = "EST"
                               marker_relationship
                        WHERE  mrel_mrkr_1_zdb_id = m1.mrkr_zdb_id
                               AND mrel_mrkr_2_zdb_id = est.mrkr_zdb_id
-                              AND mrel_type = "gene encodes small segment"
-                              AND m1.mrkr_name LIKE "xx:%")
-       AND mrkr_name NOT IN ( "cb23", "cb42", "cb70", "cb104",
-                              "cb109", "cb114" )
+                              AND mrel_type = 'gene encodes small segment'
+                              AND m1.mrkr_name LIKE 'xx:%')
+       AND mrkr_name NOT IN ( 'cb23', 'cb42', 'cb70', 'cb104',
+                              'cb109', 'cb114' )
 ORDER  BY mrkr_name;
