9e7b709959daa047b9e96ea084ebd74cd734eac8
Christian Pich <cmpich@zfin.org>
2017-09-08 11:05:09 -0700

    INF-3128 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Transcript-DbLink-Rule_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Transcript-DbLink-Rule_d.sql
@@ -1,5 +1,5 @@
 SELECT tscript_mrkr_zdb_id
 FROM   transcript
-WHERE  NOT EXISTS (SELECT "x"
+WHERE  NOT EXISTS (SELECT 'x'
                    FROM   db_link
                    WHERE  dblink_linked_recid = tscript_mrkr_zdb_id);
