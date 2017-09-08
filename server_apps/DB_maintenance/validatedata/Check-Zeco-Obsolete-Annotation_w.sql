9bdfcd6a97c25a0357c2969151500cb374f81fd2
Christian Pich <cmpich@zfin.org>
2017-09-08 11:24:47 -0700

    INF-3131 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Zeco-Obsolete-Annotation_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Zeco-Obsolete-Annotation_w.sql
@@ -3,32 +3,32 @@ SELECT expcond_zdb_id,
 FROM   experiment_condition,
        term
 WHERE  expcond_zeco_term_zdb_id= term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
 UNION
 SELECT expcond_zdb_id,
        term_name
 FROM   experiment_condition,
        term
 WHERE  expcond_chebi_term_zdb_id= term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
 UNION
 SELECT expcond_zdb_id,
        term_name
 FROM   experiment_condition,
        term
 WHERE  expcond_taxon_term_zdb_id= term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
 UNION
 SELECT expcond_zdb_id,
        term_name
 FROM   experiment_condition,
        term
 WHERE  expcond_ao_term_zdb_id= term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
 UNION
 SELECT expcond_zdb_id,
        term_name
 FROM   experiment_condition,
        term
 WHERE  expcond_go_cc_term_zdb_id= term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
