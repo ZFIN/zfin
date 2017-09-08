e1feb69dcfe7229dd9ff64d5fa8084317e927b9a
Christian Pich <cmpich@zfin.org>
2017-09-08 11:34:02 -0700

    INF-3132 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/report_data/Update-AccessionBank-DbLink_d.sql
+++ server_apps/DB_maintenance/report_data/Update-AccessionBank-DbLink_d.sql
@@ -1,7 +1,7 @@
 SELECT dblink_acc_num, 'DB_LINK'
 FROM   db_link
 WHERE  dblink_length IS NULL
-       AND EXISTS (SELECT "x"
+       AND EXISTS (SELECT 'x'
                    FROM   accession_bank
                    WHERE  accbk_acc_num = dblink_acc_num
                           AND accbk_fdbcont_zdb_id = dblink_fdbcont_zdb_id
@@ -10,7 +10,7 @@ UNION
 SELECT accbk_acc_num, 'ACCESSION_BANK'
 FROM   accession_bank
 WHERE  accbk_length IS NULL
-       AND EXISTS (SELECT "x"
+       AND EXISTS (SELECT 'x'
                    FROM   db_link
                    WHERE  accbk_acc_num = dblink_acc_num
                           AND accbk_fdbcont_zdb_id = dblink_fdbcont_zdb_id
@@ -25,7 +25,7 @@ SET    dblink_length = (SELECT accbk_length
                                AND dblink_fdbcont_zdb_id = accbk_fdbcont_zdb_id
                                AND accbk_length IS NOT NULL)
 WHERE  dblink_length IS NULL
-AND EXISTS (SELECT "x"
+AND EXISTS (SELECT 'x'
                    FROM   accession_bank
                    WHERE  accbk_acc_num = dblink_acc_num
                           AND accbk_fdbcont_zdb_id = dblink_fdbcont_zdb_id
@@ -39,7 +39,7 @@ SET    accbk_length = (SELECT dblink_length
                               AND dblink_fdbcont_zdb_id = accbk_fdbcont_zdb_id
                               AND dblink_length IS NOT NULL)
 WHERE  accbk_length IS NULL
-        AND EXISTS (SELECT "x"
+        AND EXISTS (SELECT 'x'
                    FROM   db_link
                    WHERE  accbk_acc_num = dblink_acc_num
                           AND accbk_fdbcont_zdb_id = dblink_fdbcont_zdb_id
