01e80265a1f4deb52c88d854010d027c0282eb23
Christian Pich <cmpich@zfin.org>
2017-09-08 11:12:21 -0700

    INF-3129 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Transcript-LoadID-Match-DbLink_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Transcript-LoadID-Match-DbLink_d.sql
@@ -3,13 +3,13 @@ SELECT tscript_mrkr_zdb_id,
        dblink_acc_num
 FROM   transcript,
        db_link
-WHERE  NOT EXISTS (SELECT "x"
+WHERE  NOT EXISTS (SELECT 'x'
                    FROM   db_link b
                    WHERE  b.dblink_linked_recid = tscript_mrkr_zdb_id
                           AND b.dblink_acc_num = tscript_load_id)
        AND tscript_load_id != tscript_mrkr_zdb_id
        AND dblink_linked_recid = tscript_mrkr_zdb_id
-       AND NOT EXISTS (SELECT "x"
+       AND NOT EXISTS (SELECT 'x'
                        FROM   data_alias
                        WHERE  dalias_data_zdb_id = tscript_mrkr_zdb_id
-                              AND dalias_alias LIKE "OTTDAR%");
+                              AND dalias_alias LIKE 'OTTDAR%');
