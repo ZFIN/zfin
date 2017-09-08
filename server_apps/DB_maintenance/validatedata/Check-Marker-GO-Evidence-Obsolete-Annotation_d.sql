c0198f79060ddbe23d63d33df76a6f0b1fa361d0
Christian Pich <cmpich@zfin.org>
2017-09-08 08:37:41 -0700

    INF-3123 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Obsolete-Annotation_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Obsolete-Annotation_d.sql
@@ -6,5 +6,5 @@ FROM   marker_go_term_evidence,
        term,
        marker
 WHERE  mrkrgoev_term_zdb_id = term_zdb_id
-       AND term_is_obsolete = "t"
+       AND term_is_obsolete = 't'
        AND mrkrgoev_mrkr_zdb_id = mrkr_zdb_id;
