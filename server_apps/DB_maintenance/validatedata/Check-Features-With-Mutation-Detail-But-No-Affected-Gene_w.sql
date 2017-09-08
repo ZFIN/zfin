580d2985dc7e9dbfca03d31b973d14604ea6e9d7
Christian Pich <cmpich@zfin.org>
2017-09-07 19:05:05 -0700

    INF-3121 fix query

--- server_apps/DB_maintenance/validatedata/Check-Features-With-Mutation-Detail-But-No-Affected-Gene_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Features-With-Mutation-Detail-But-No-Affected-Gene_w.sql
@@ -1,16 +1,16 @@
 select feature_zdb_id, feature_name
   from feature 
- where not exists (select "x" 
+ where not exists (select 'x' 
                      from feature_marker_relationship 
                     where fmrel_ftr_zdb_id = feature_zdb_id 
-                      and fmrel_type = "is allele of") 
-   and (exists (select "x" 
+                      and fmrel_type = 'is allele of') 
+   and (exists (select 'x' 
                   from feature_dna_mutation_detail 
                  where fdmd_feature_zdb_id = feature_zdb_id) 
-        or exists (select "x" 
+        or exists (select 'x' 
                      from feature_transcript_mutation_detail 
                     where ftmd_feature_zdb_id = feature_zdb_id) 
-        or exists (select "x" 
+        or exists (select 'x' 
                      from feature_protein_mutation_detail 
                     where fpmd_feature_zdb_id = feature_zdb_id));
 
