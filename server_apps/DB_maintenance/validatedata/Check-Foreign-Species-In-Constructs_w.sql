feb8e7002820c0abebb2b8dc0a2629577b103b2c
Christian Pich <cmpich@zfin.org>
2017-09-08 08:17:57 -0700

    INF-3122 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Foreign-Species-In-Constructs_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Foreign-Species-In-Constructs_w.sql
@@ -1,19 +1,44 @@
-select  a.cc_construct_zdb_id, a.cc_component, mrkr_name, recattrib_source_zdb_id
-from construct_component a, construct_component b, controlled_vocabulary, construct, record_Attribution, marker
- where a.cc_construct_zdb_id = b.cc_construct_zdb_id
-and exists (Select 'x' from marker,marker_type_group_member where mrkr_type=mtgrpmem_mrkr_type and mtgrpmem_mrkr_type_group='CONSTRUCT_COMPONENTS' and mrkr_zdb_id = a.cc_component_zdb_id)
-and b.cc_component_zdb_id = cv_zdb_id
-and b.cc_order + 2 = a.cc_order
-and recattrib_datA_zdb_id = mrkr_zdb_id
-and mrkr_zdb_id = a.cc_construct_zdb_id
-and cv_foreign_species is not null
-union
-select  a.cc_construct_zdb_id, a.cc_component, mrkr_name, recattrib_source_zdb_id
-from construct_component a, construct_component b, controlled_vocabulary, construct, record_Attribution, marker
- where a.cc_construct_zdb_id = b.cc_construct_zdb_id
-and exists  (Select 'x' from markermarker_type_group_member where mrkr_type=mtgrpmem_mrkr_type and mtgrpmem_mrkr_type_group='CONSTRUCT_COMPONENTS' and mrkr_Zdb_id = a.cc_component_zdb_id)
-and recattrib_datA_zdb_id = mrkr_zdb_id
-and mrkr_zdb_id = a.cc_construct_zdb_id
-and b.cc_component_zdb_id = cv_zdb_id
-and b.cc_order + 1 = a.cc_order
-and cv_foreign_species is not null;
+SELECT a.cc_construct_zdb_id, 
+       a.cc_component, 
+       mrkr_name, 
+       recattrib_source_zdb_id 
+FROM   construct_component a, 
+       construct_component b, 
+       controlled_vocabulary, 
+       construct, 
+       record_attribution, 
+       marker 
+WHERE  a.cc_construct_zdb_id = b.cc_construct_zdb_id 
+       AND EXISTS (SELECT 'x' 
+                   FROM   marker, 
+                          marker_type_group_member 
+                   WHERE  mrkr_type = mtgrpmem_mrkr_type 
+                          AND mtgrpmem_mrkr_type_group = 'CONSTRUCT_COMPONENTS' 
+                          AND mrkr_zdb_id = a.cc_component_zdb_id) 
+       AND b.cc_component_zdb_id = cv_zdb_id 
+       AND b.cc_order + 2 = a.cc_order 
+       AND recattrib_data_zdb_id = mrkr_zdb_id 
+       AND mrkr_zdb_id = a.cc_construct_zdb_id 
+       AND cv_foreign_species IS NOT NULL 
+UNION 
+SELECT a.cc_construct_zdb_id, 
+       a.cc_component, 
+       mrkr_name, 
+       recattrib_source_zdb_id 
+FROM   construct_component a, 
+       construct_component b, 
+       controlled_vocabulary, 
+       construct, 
+       record_attribution, 
+       marker 
+WHERE  a.cc_construct_zdb_id = b.cc_construct_zdb_id 
+       AND EXISTS (SELECT 'x' 
+                   FROM   marker_type_group_member
+                   WHERE  mrkr_type = mtgrpmem_mrkr_type 
+                          AND mtgrpmem_mrkr_type_group = 'CONSTRUCT_COMPONENTS' 
+                          AND mrkr_zdb_id = a.cc_component_zdb_id) 
+       AND recattrib_data_zdb_id = mrkr_zdb_id 
+       AND mrkr_zdb_id = a.cc_construct_zdb_id 
+       AND b.cc_component_zdb_id = cv_zdb_id 
+       AND b.cc_order + 1 = a.cc_order 
+       AND cv_foreign_species IS NOT NULL; 
