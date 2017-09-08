6140f1fdfcb0506b29b267e3e36ba162b56a6e17
Christian Pich <cmpich@zfin.org>
2017-09-08 11:16:17 -0700

    INF-3130 rewrite query to be compliant to PG as well

--- server_apps/DB_maintenance/validatedata/Check-Transcript-One-Gene-Rule_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Transcript-One-Gene-Rule_d.sql
@@ -6,13 +6,13 @@ FROM   marker_relationship mrel1,
        transcript, 
        transcript_type 
 WHERE  tscriptt_pk_id = tscript_type_id 
-       AND tscriptt_type != "miRNA"
+       AND tscriptt_type != 'miRNA'
        AND tscript_mrkr_zdb_id = mrel1.mrel_mrkr_2_zdb_id 
-       AND (mrel1.mrel_mrkr_1_zdb_id LIKE "ZDB-GENE%"  OR mrel1.mrel_mrkr_1_zdb_id LIKE "%RNAG%")
-       AND EXISTS (SELECT "x" 
+       AND (mrel1.mrel_mrkr_1_zdb_id LIKE 'ZDB-GENE%'  OR mrel1.mrel_mrkr_1_zdb_id LIKE '%RNAG%')
+       AND EXISTS (SELECT 'x' 
                    FROM   marker_relationship mrel2 
                    WHERE  mrel2.mrel_mrkr_1_zdb_id != mrel1.mrel_mrkr_1_zdb_id 
-                          AND (mrel2.mrel_mrkr_1_zdb_id LIKE "ZDB-GENE%" OR mrel1.mrel_mrkr_1_zdb_id LIKE "%RNAG%")
+                          AND (mrel2.mrel_mrkr_1_zdb_id LIKE 'ZDB-GENE%' OR mrel1.mrel_mrkr_1_zdb_id LIKE '%RNAG%')
                           AND mrel2.mrel_mrkr_2_zdb_id = 
                               mrel1.mrel_mrkr_2_zdb_id) 
        AND mrkr_zdb_id = tscript_mrkr_zdb_id 
