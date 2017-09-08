28a971e1f4328350c91dd73291f12038c33a2fe8
Christian Pich <cmpich@zfin.org>
2017-09-08 13:16:45 -0700

    fix DB function call in Java

---
+++ test/org/zfin/database/InformixUtilTest.java
@@ -0,0 +1,12 @@
+package org.zfin.database;
+
+import org.junit.Test;
+import org.zfin.AbstractDatabaseTest;
+
+public class InformixUtilTest extends AbstractDatabaseTest {
+
+    @Test
+    public void testFunctionCall() {
+        InformixUtil.runInformixProcedure("regen_construct_marker", "ZDB-GENE");
+    }
+}
