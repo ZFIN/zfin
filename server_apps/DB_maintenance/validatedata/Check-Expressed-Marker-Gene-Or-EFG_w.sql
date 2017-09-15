1014852206bb4499561497834aeedb7aa1a9c2c7
Christian Pich <cmpich@zfin.org>
2017-09-15 14:46:08 -0700

    INF-3111 replace double quotes with single quotes

--- server_apps/DB_maintenance/validatedata/Check-Expressed-Marker-Gene-Or-EFG_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Expressed-Marker-Gene-Or-EFG_w.sql
@@ -5,7 +5,7 @@ FROM   expression_experiment2,
        marker,
        marker_type_group_member
 WHERE  xpatex_gene_zdb_id = mrkr_zdb_id
-       AND NOT EXISTS (SELECT "t"
+       AND NOT EXISTS (SELECT 't'
                        FROM   marker_type_group_member
-                       WHERE  mtgrpmem_mrkr_type_group = "GENEDOM_AND_EFG"
+                       WHERE  mtgrpmem_mrkr_type_group = 'GENEDOM_AND_EFG'
                               AND mtgrpmem_mrkr_type = mrkr_type);
