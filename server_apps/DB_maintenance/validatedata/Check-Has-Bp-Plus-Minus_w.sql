b79d3dd1100a59b0786df6893d6ef9626194b24d
Prita Mani <pm@zfin.org>
2017-09-19 12:39:37 -0700

    JENK-270

--- server_apps/DB_maintenance/validatedata/Check-Has-Bp-Plus-Minus_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-Has-Bp-Plus-Minus_w.sql
@@ -1,5 +1,5 @@
 select feature_zdb_id, feature_name,fdmd_dna_mutation_term_zdb_id,term_name
-  from feature , feature_dna_mutation_detail
+  from feature , feature_dna_mutation_detail,term
  where feature_type != 'POINT_MUTATION'
  and exists (Select 'x' from feature_dna_mutation_detail
      	    	    	where fdmd_feature_zdb_id = feature_zdb_id
