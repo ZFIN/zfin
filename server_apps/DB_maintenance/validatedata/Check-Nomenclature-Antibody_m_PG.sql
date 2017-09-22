71d71e506ba86ccdac8919c21e68a900b915e28b
Christian Pich <cmpich@zfin.org>
2017-09-22 13:26:06 -0700

    INF-3153 fix query for PG

--- server_apps/DB_maintenance/validatedata/Check-Nomenclature-Antibody_m_PG.sql
+++ server_apps/DB_maintenance/validatedata/Check-Nomenclature-Antibody_m_PG.sql
@@ -1,16 +1,16 @@
 -- create temp table of all genes and pseudo genes that have updated symbols within
 -- the last 31 days
+CREATE TEMP TABLE gene_names_changed_temp as
 SELECT   rec_id,
          old_value,
          new_value,
          upd_when
 FROM     updates
-WHERE    upd_when > today -31
+WHERE    upd_when > now() - interval '31 days'
 AND      Get_obj_type(rec_id) IN ('GENE',
                                   'GENEP')
 AND      field_name ='mrkr_abbrev'
-ORDER BY upd_when DESC
-INTO temp gene_names_changed_temp;
+ORDER BY upd_when DESC;
 
 
 SELECT b.mrkr_zdb_id,
@@ -28,5 +28,5 @@ WHERE  antibody.mrkr_type = 'ATB'
        AND mrel.mrel_mrkr_2_zdb_id = antibody.mrkr_zdb_id
        AND mrel.mrel_mrkr_1_zdb_id = b.mrkr_zdb_id
        AND up.old_value LIKE Substring(antibody.mrkr_abbrev FROM
-                                       Charindex('-', antibody.mrkr_abbrev) + 1
+                                       position('-' in antibody.mrkr_abbrev) + 1
                                        FOR 20)|| '%';
