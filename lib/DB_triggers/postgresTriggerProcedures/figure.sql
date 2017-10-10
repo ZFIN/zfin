e14fc05838255ad954c39732e167bb8a1185c7ea
Sierra Moxon <smoxon@zfin.org>
2017-10-10 09:28:22 -0700

    fixing figure trigger

--- lib/DB_triggers/postgresTriggerProcedures/figure.sql
+++ lib/DB_triggers/postgresTriggerProcedures/figure.sql
@@ -3,13 +3,12 @@ drop trigger if exists figure_trigger on figure;
 create or replace function figure()
 returns trigger as
 $BODY$
-declare fig_label figure.fig_label%TYPE;
+declare fig_label figure.fig_label%TYPE := zero_pad(NEW.fig_label);
 begin
 
      perform p_insert_into_record_attribution_tablezdbids (
 			NEW.fig_zdb_id,
 			NEW.fig_source_zdb_id);
-     fig_label = (select zero_pad(NEW.fig_label));
      NEW.fig_label = fig_label;
 
      RETURN NEW;
