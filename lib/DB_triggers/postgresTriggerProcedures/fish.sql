cd8761daa55bdad2789b47bc1358329c9b39ff84
Sierra Moxon <smoxon@zfin.org>
2017-10-12 08:54:54 -0700

    fixing fish proc

--- lib/DB_triggers/postgresTriggerProcedures/fish.sql
+++ lib/DB_triggers/postgresTriggerProcedures/fish.sql
@@ -7,7 +7,7 @@ $BODY$
 declare fish_name fish.fish_name%TYPE := scrub_char(NEW.fish_name);
 	fish_name_order fish.fish_name_order%TYPE := zero_pad(fish_name);
 	fish_full_name fish.fish_full_name%TYPE := get_fish_full_name(NEW.fish_zdb_id, NEW.fish_genotype_zdb_id, NEW.fish_name);
-	fish_functional_affected_gene_count fish.fish_functional_affected_gene_count%TYPE := getFishOrder(NEW.fish_zdb_id).numAffectedGene;
+	fish_functional_affected_gene_count fish.fish_functional_affected_gene_count%TYPE := getFishOrder(NEW.fish_zdb_id);
 	fish_order fish.fish_order%TYPE := 0;
 
 begin
