d1ae040ac0bef0d26ae47df307f05db5a343b4d7
Christian Pich <pich@anatinus.com>
2017-10-12 15:34:38 -0700

    INF-375 make query work for PG as well in Jenkins

--- server_apps/DB_maintenance/validatedata/Check-Zdb-Object-Source-Data-Correct_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Zdb-Object-Source-Data-Correct_d.sql
@@ -2,15 +2,15 @@ SELECT zobjtype_name,
        zobjtype_is_data,
        zobjtype_is_source
 FROM   zdb_object_type
-WHERE  ( zobjtype_is_data = "t"
+WHERE  ( zobjtype_is_data = 't'
          AND zobjtype_name NOT IN (SELECT Get_obj_type(zactvd_zdb_id)
                                    FROM   zdb_active_data) )
-        OR ( zobjtype_is_data = "f"
+        OR ( zobjtype_is_data = 'f'
              AND zobjtype_name IN (SELECT Get_obj_type(zactvd_zdb_id)
                                    FROM   zdb_active_data) )
-        OR ( zobjtype_is_source = "t"
+        OR ( zobjtype_is_source = 't'
              AND zobjtype_name NOT IN (SELECT Get_obj_type(zactvs_zdb_id)
                                        FROM   zdb_active_source) )
-        OR ( zobjtype_is_source = "f"
+        OR ( zobjtype_is_source = 'f'
              AND zobjtype_name IN (SELECT Get_obj_type(zactvs_zdb_id)
                                    FROM   zdb_active_source) )
