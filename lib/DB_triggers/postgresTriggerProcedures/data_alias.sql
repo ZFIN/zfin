c29d15fa3bb4eda1506712d3e656f756b65444a2
Sierra Moxon <smoxon@zfin.org>
2017-10-16 21:19:31 -0700

    fixing data alias and dblink

--- lib/DB_triggers/postgresTriggerProcedures/data_alias.sql
+++ lib/DB_triggers/postgresTriggerProcedures/data_alias.sql
@@ -5,7 +5,7 @@ returns trigger as
 $BODY$
 
 declare dalias_alias data_alias.dalias_alias%TYPE := scrub_char(NEW.dalias_alias);
-declare dalias_alias_lower data_alias.dalias_alias_lower%TYPE := lower(NEW.dalias_alias);
+declare dalias_alias_lower data_alias.dalias_alias_lower%TYPE := lower(dalias_alias);
 
 begin
 
@@ -18,6 +18,6 @@ begin
 end;
 $BODY$ LANGUAGE plpgsql;
 
-create trigger data_alias_trigger after insert or update on data_alias
+create trigger data_alias_trigger before insert or update on data_alias
  for each row
  execute procedure data_alias();
