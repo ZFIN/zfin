6ecda866c29eff4b7d66801cb76246c5515131b1
Sierra Moxon <smoxon@zfin.org>
2017-10-31 15:14:00 -0700

    fixing bytea

--- server_apps/DB_maintenance/postgres/changeSmartLargeObjects.sql
+++ server_apps/DB_maintenance/postgres/changeSmartLargeObjects.sql
@@ -1,6 +1,6 @@
 begin work;
 
-alter table publication alter column pub_abstract type bytea using pub_abstract:::bytea;
+alter table publication alter column pub_abstract type bytea using pub_abstract::bytea;
 
 alter table person alter column nonzf_pubs type bytea using nonzf_pubs::bytea;
 
