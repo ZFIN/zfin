5d24d07b3f21332912841df5bedfaee2a48468f0
Prita Mani <pm@zfin.org>
2017-11-02 08:49:16 -0700

    ZFIN-5777

--- source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset pm:ZFIN5777
+--changeset pm:ZFIN577
 
 update feature set feature_type='POINT_MUTATION' where feature_zdb_id in (select feature from tmp_feature);
 update feature set feature_type='DELETION' where feature_zdb_id='ZDB-ALT-131112-1';
--- source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577_pre.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577_pre.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
---changeset pm:zkoCRISPRS_pre
+--changeset pm:ZFIN577_pre
 
 CREATE TABLE tmp_feature (
 feature VARCHAR(50),
