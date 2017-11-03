ef8888bc2cebf9b2f88f75a2fc4ca0afde924881
Prita Mani <pm@zfin.org>
2017-11-03 14:52:00 -0700

    ZFIN-5777 temp table name change

--- source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN577.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset pm:ZFIN577
 
-update feature set feature_type='POINT_MUTATION' where feature_zdb_id in (select feature from tmp_feature);
+update feature set feature_type='POINT_MUTATION' where feature_zdb_id in (select feature from tmp_updfeature);
 update feature set feature_type='DELETION' where feature_zdb_id='ZDB-ALT-131112-1';
 drop table tmp_updfeature;
 
