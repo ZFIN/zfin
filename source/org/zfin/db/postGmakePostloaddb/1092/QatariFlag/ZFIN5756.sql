4a5c625916cce2426659e374b0f43699cf30fef7
Prita Mani <pm@zfin.org>
2017-11-10 09:56:25 -0800

    ZFIN-5756

--- source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN5756.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/QatariFlag/ZFIN5756.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset pm:ZFIN5756
 
-update update  paneled_markers set or_lg=8 where zdb_id='ZDB-GENE-030131-4505';
+update  paneled_markers set or_lg=8 where zdb_id='ZDB-GENE-030131-4505';
 
 
 
