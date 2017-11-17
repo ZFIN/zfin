1835d665d2be5ee51f906a3c3d8753bf53a92781
Prita Mani <pm@zfin.org>
2017-11-16 16:04:11 -0800

    JENK-295

--- server_apps/data_transfer/Downloads/GFF3/get_crispr_seq.sql
+++ server_apps/data_transfer/Downloads/GFF3/get_crispr_seq.sql
@@ -1,8 +1,9 @@
 
 -- make a fasta defline that will also work as a gff3 attribute column
 
-unload to mo_seq.fa_line delimiter " "
+unload to crispr_seq.fa_line delimiter " "
 select ">ID=;Name=" || m.mrkr_abbrev || ";zdb_id="|| m.mrkr_zdb_id || ";~" || s.seq_sequence
 from marker m
 inner join marker_sequence s on s.seq_mrkr_zdb_id = m.mrkr_zdb_id
-where m.mrkr_type ='MRPHLNO';
+where m.mrkr_type  = 'CRISPR';
+
