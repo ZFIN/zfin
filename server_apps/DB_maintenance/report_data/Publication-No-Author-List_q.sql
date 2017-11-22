0cfcce937219e8b59bc8a4309588f674e29c5ef7
Christian Pich <cmpich@zfin.org>
2017-11-22 13:55:00 -0800

    JENK-310 convert to postgres

--- server_apps/DB_maintenance/report_data/Publication-No-Author-List_q.sql
+++ server_apps/DB_maintenance/report_data/Publication-No-Author-List_q.sql
@@ -6,4 +6,4 @@ FROM   publication,
 WHERE  NOT EXISTS (SELECT 'x' 
                    FROM   int_person_pub 
                    WHERE  target_id = publication.zdb_id) 
-       AND authors LIKE "%"|| Replace(NAME, "-", ",")|| "%";
+       AND authors LIKE '%'|| Replace(NAME, '-', ',')|| '%';
