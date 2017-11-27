4ef367a70d8615c192db9300843780b35a7c1613
Christian Pich <cmpich@zfin.org>
2017-11-27 12:59:29 -0800

    INF-3250 use groovy for exporting data

--- server_apps/data_transfer/BLAST/getEnsemblTscripts_PG.sql
+++ server_apps/data_transfer/BLAST/getEnsemblTscripts_PG.sql
@@ -1,8 +1,4 @@
-begin work;
-
 copy (
 select ensm_ensdart_id
 from ensdar_mapping
-where exists (Select 'x' from db_link where dblink_acc_num like 'ENSDARG%') )  to '<!--|ROOT_PATH|-->/server_apps/data_transfer/BLAST/ensemblZfinTscriptsForBlast.txt';
-
-commit work ;
+where exists (Select 'x' from db_link where dblink_acc_num like 'ENSDARG%') )  to 'ensemblZfinTscriptsForBlast.txt'  DELIMITER '	';
