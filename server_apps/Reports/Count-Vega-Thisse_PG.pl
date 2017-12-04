e36fc7e466186a9584929be5e45e023f1ae36fee
informix <informix@zfin.org>
2017-12-04 10:16:25 -0800

    INF-3251 Convert Jenkins job to PG

--- server_apps/Reports/Count-Vega-Thisse_PG.pl
+++ server_apps/Reports/Count-Vega-Thisse_PG.pl
@@ -6,11 +6,11 @@
 #
 #-----------------------------------------------------------------------
 
-chdir "<!--|ROOT_PATH|-->/server_apps/Reports/Vega";
+chdir "<!--|TARGETROOT|-->/server_apps/Reports/Vega";
 
 # Run vega_thisse_report.sql before VegaCount.sql.
 # A file is created by vega_thisse_report.sql that is read by VegaCount.sql.
 
-system("../data_transfer/runSqlFiles.groovy Vega/vega_thisse_report_PG.sql");
+system("<!--|TARGETROOT|-->/server_apps/data_transfer/runSqlFiles.groovy vega_thisse_report_PG.sql");
 
 exit;
