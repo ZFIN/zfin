8b14abd7fcd5eda1667b6d8ea983d7ff724c8ab2
Sierra Moxon <smoxon@zfin.org>
2017-12-07 11:21:42 -0800

    INF-3269

--- server_apps/DB_maintenance/regen_PG.sh
+++ server_apps/DB_maintenance/regen_PG.sh
@@ -9,25 +9,25 @@ echo "Starting regen_genox at `date`"
 echo 'select regen_genox(); ' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_anatomy_counts at `date`"
-echo 'execute function regen_anatomy_counts();' | ${PGBINDIR}/psql ${DBNAME}
+echo 'select regen_anatomy_counts();' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_term at `date`"
-echo 'execute function regen_term();' | ${PGBINDIR}/psql ${DBNAME}
+echo 'select regen_term();' | ${PGBINDIR}/psql ${DBNAME}
 
-echo "Starting regen_names at `date`"
-echo 'execute function regen_names();' | ${PGBINDIR}/psql ${DBNAME}
+#echo "Starting regen_names at `date`"
+#echo 'select regen_names();' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_expression_term_fast_search at `date`"
-echo 'execute function regen_expression_term_fast_search();' | ${PGBINDIR}/psql ${DBNAME}
+echo 'select regen_expression_term_fast_search();' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_clean_expression at `date`"
-echo 'execute function regen_clean_expression();' | ${PGBINDIR}/psql ${DBNAME}
+echo 'select regen_clean_expression();' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_fish_Components at `date`"
-echo 'execute function regen_fish_components();' | ${PGBINDIR}/psql ${DBNAME}
+echo 'select regen_fish_components();' | ${PGBINDIR}/psql ${DBNAME}
 
 echo "Starting regen_pheno_fast_search at `date`"
-${PGBINDIR}/psql ${DBNAME} < <!--|TARGETROOT|-->/server_apps/DB_maintenance/pheno/pheno_term_regen.sql 
+${PGBINDIR}/psql ${DBNAME} < <!--|TARGETROOT|-->/server_apps/DB_maintenance/pheno/pheno_term_regen_PG.sql 
 
 echo "vacuum daily"
 echo 'vacuum (analyze)' | ${PGBINDIR}/psql ${DBNAME}
