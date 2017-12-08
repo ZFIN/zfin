0145dda8bba1eb1b5f31286bdaaba49df14a53ef
informix <informix@zfin.org>
2017-12-08 09:20:55 -0800

    INF-3267 use deploy-solr target for solr deployment (Christian)

--- server_apps/solr/prototype/conf/generate-reporter-name-file.sql
+++ server_apps/solr/prototype/conf/generate-reporter-name-file.sql
@@ -1,5 +1,5 @@
 copy (
 select mrkr_abbrev
 from marker
-where mrkr_zdb_id[1,7] = 'ZDB-EFG' ) to '<!--|TARGETROOT|-->/server_apps/solr/prototype/conf/reporter-names.txt' delimiter '|'
+where mrkr_zdb_id[1,7] = 'ZDB-EFG' ) to '@TARGETROOT@/server_apps/solr/prototype/conf/reporter-names.txt' delimiter '|'
 ;
