9692b49f0df2f6fba6e54aa48f8fa851eda22b05
Xiang Shao <xshao@zfin.org>
2018-01-03 14:06:22 -0800

    PUB-411 added a delete SQL

--- source/org/zfin/db/postGmakePostloaddb/1092/Snowman/PUB-411.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/Snowman/PUB-411.sql
@@ -12,5 +12,8 @@ UPDATE pub_tracking_history
    SET pth_pub_zdb_id = 'ZDB-PUB-170218-14'
  WHERE pth_pub_zdb_id = 'ZDB-PUB-010220-6';
 
+DELETE FROM zdb_active_source 
+WHERE zactvs_zdb_id = 'ZDB-PUB-010220-6';
+
 INSERT INTO withdrawn_data (wd_old_zdb_id, wd_new_zdb_id, wd_display_note) 
 VALUES ('ZDB-PUB-010220-6', 'ZDB-PUB-170218-14',  "publication merged");
