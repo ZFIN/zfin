bbf8d0d6772736ab91447687ad9c06099bf7e3db
Sierra Moxon <smoxon@zfin.org>
2018-01-16 10:47:53 -0800

    genofeat trigger removed to let trunk build

--- lib/DB_triggers/genofeat_insert_trigger.sql
+++ lib/DB_triggers/genofeat_insert_trigger.sql
@@ -2,6 +2,5 @@ create trigger genofeat_insert_trigger insert on
     genotype_feature referencing new as new_genofeat
     for each row
         (
-        execute procedure p_check_tginsertion_has_construct_relationship(new_genofeat.genofeat_feature_zdb_id,
-	execute procedure p_update_related_genotype(new_genofeat.genofeat_feature_zdb_id)
+        execute procedure p_check_tginsertion_has_construct_relationship(new_genofeat.genofeat_feature_zdb_id
     ));
