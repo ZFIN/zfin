35d935e6ea6cc042480dbfc2d4883cd5056dc3c2
informix <informix@zfin.org>
2018-01-17 15:08:48 -0800

    INF-3266 fix trigger: make it apply before instead of after (Christian)

--- lib/DB_triggers/postgresTriggerProcedures/run_candidate.sql
+++ lib/DB_triggers/postgresTriggerProcedures/run_candidate.sql
@@ -1,18 +1,13 @@
 drop trigger if exists run_candidate_trigger on run_candidate;
 
 create or replace function run_candidate()
-returns trigger as
-$BODY$
-declare runcan_occurrence_order int;
+returns trigger as $$
 begin
-
-     runcan_occurrence_order = (select increment_candidate_occurrences(NEW.runcan_cnd_zdb_id));
-     NEW.runcan_occurrence_order = runcan_occurrence_order;
+     NEW.runcan_occurrence_order = (select increment_candidate_occurrences(NEW.runcan_cnd_zdb_id));
      RETURN NEW;
-
 end;
-$BODY$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
-create trigger run_candidate_trigger after insert or update on run_candidate
+create trigger run_candidate_trigger BEFORE INSERT OR UPDATE  on run_candidate
  for each row
  execute procedure run_candidate();
