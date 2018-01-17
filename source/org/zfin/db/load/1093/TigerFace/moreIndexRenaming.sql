1cc50e817e0a8f6645d5a9c1ecb841e48c8bdd53
Sierra Moxon <smoxon@zfin.org>
2018-01-17 09:46:02 -0800

    changing structure of paneled_markers_index

--- source/org/zfin/db/load/1093/TigerFace/moreIndexRenaming.sql
+++ source/org/zfin/db/load/1093/TigerFace/moreIndexRenaming.sql
@@ -2,7 +2,7 @@
 --changeset sierra:moreIndexRenaming.sql
 
 alter table lab
-drop constraint lab_contact_person_foreign_key);
+drop constraint lab_contact_person_foreign_key;
 
 create index lab_contact_person_fk_index
  on lab (contact_person)
@@ -39,7 +39,7 @@ alter table feature_type_mutagen_group_member
  drop constraint ftmgm_feature_type_foreign_key_odc;
 
 create index ftmgm_feature_type_fk_odc_index
- on feature_type_mutagen_group_member (tmgm_feature_type)
+ on feature_type_mutagen_group_member (ftmgm_feature_type)
 using btree in idxdbs1;
 
 alter table feature_type_mutagen_group_member add 
@@ -51,7 +51,7 @@ alter table feature_type_mutagen_group_member
 
 
 create index ftmgm_mutagen_fk_odc_index
- on feature_type_mutagen_group_member (tmgm_mutagen)
+ on feature_type_mutagen_group_member (ftmgm_mutagen)
 using btree in idxdbs1;
 
 alter table feature_type_mutagen_group_member add 
@@ -78,12 +78,23 @@ create index dalias_alias_lower_index on
     data_alias (dalias_alias_lower) using 
     btree in idxdbs3 ;
 
+alter table experiment_condition_restricted
+  drop constraint experiment_condition_restricted_cdt_foreign_key;
+
 drop index experiment_condition_restricted_condition_data_type_foreign_key_index;
 
 create index ecr__condition_data_type_fk_index 
     on experiment_condition_restricted (ecr_cdt_zdb_id) 
     using btree in idxdbs2;
 
+
+
+alter table experiment_condition_restricted add constraint 
+    (foreign key (ecr_cdt_zdb_id) references condition_data_type 
+     constraint experiment_condition_restricted_cdt_foreign_key);
+
+alter table feature_protein_mutation_detail
+drop constraint fpmd_mdcv_fk;
 
 drop index feature_mutation_detail_protein_consequenc_term_zdb_id_foreign_key_index;
 
@@ -92,13 +103,19 @@ create index fpmd_protein_consequence_term_zdb_id_fk_index
     using btree  in idxdbs3;
 
 
-drop index lpmem_linkage_member_index
+alter table feature_protein_mutation_detail add constraint 
+    (foreign key (fpmd_protein_consequence_term_zdb_id) references 
+    mutation_detail_controlled_vocabulary  constraint 
+    fpmd_mdcv_fk);
 
-create index lpmem_linkage_member_index on 
+
+drop index lpmem_linkage_member_index;
+
+create index lpmem_linkage_zdb_id_index on 
     linkage_pair_member (lpmem_linkage_zdb_id) 
     using btree in idxdbs2;
 
-create index lpmem_linkage_member_index on 
+create index lpmem_member_zdb_id_index on 
     linkage_pair_member (lpmem_member_zdb_id) 
     using btree in idxdbs1;
 
@@ -119,10 +136,9 @@ create index paneled_markers_or_lg_index
     on paneled_markers (or_lg) 
     using btree in idxdbs1;
 
-create index paneled_markers_mtype_index
-    on paneled_markers (mtype) 
-    using btree in idxdbs3;
+alter table paneled_markers
+ drop constraint r443_1733;
 
-create index paneled_markers_zdb_id_index 
-    on paneled_markers (zdb_id) 
-    using btree in idxdbs1;
+alter table paneled_markers add constraint (foreign 
+    key (zdb_id) references zdb_active_data  on delete 
+    cascade constraint paneled_markers_zdb_id_fk_odc);
