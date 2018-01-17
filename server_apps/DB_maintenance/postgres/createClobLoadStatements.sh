cf842b7f32769fe8150d683fe04d0bf307331bf5
Sierra Moxon <smoxon@zfin.org>
2018-01-17 11:18:06 -0800

    changing clob loader to find zdb-id instead of full path

--- server_apps/DB_maintenance/postgres/createClobLoadStatements.sh
+++ server_apps/DB_maintenance/postgres/createClobLoadStatements.sh
@@ -2,20 +2,22 @@
 
 cd /tmp/abstracts/
 for abstractFile in /tmp/abstracts/* ; do 
-  fulltxt=${abstractFile%%.*}
+  fulltxt=${abstractFile%.*}
+  filename=$(basename $fulltxt)
   prefix="update publication set pub_abstract= (select bytea_import('"
   fullpath="/tmp/abstracts/";
   suffix="')) where zdb_id = "
-  id="'$fulltxt';";
-  echo $prefixabstractFile$suffix$id >> $SOURCEROOT/server_apps/DB_maintenance/postgres/clobLoad.sql;
+  id="'$filename';";
+  echo $prefix$abstractFile$suffix$id >> $SOURCEROOT/server_apps/DB_maintenance/postgres/clobLoad.sql;
 done
 
 cd /tmp/nonzf_pubs/
 for nonzfPubFile in /tmp/nonzf_pubs/* ; do 
   fulltxt=${nonzfPubFile%%.*}
+  filename=$(basename $fulltxt)
   prefix="update person set nonzf_pubs= (select bytea_import('"
   fullpath="/tmp/nonzf_pubs/";
   suffix="')) where zdb_id = "
-  id="'$fulltxt';";
+  id="'$filename';";
   echo $prefix$nonzfPubFile$suffix$id >> $SOURCEROOT/server_apps/DB_maintenance/postgres/clobLoad.sql;
 done
