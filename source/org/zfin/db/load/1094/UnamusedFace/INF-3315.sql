4ee7c1e9c90501fe728949ccb7c042528c00f5a9
Sierra Moxon <smoxon@zfin.org>
2018-02-05 15:47:55 -0800

    INF-3315

---
+++ source/org/zfin/db/load/1094/UnamusedFace/INF-3315.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+--changeset staylor:INF-3315
+
+alter table publication_file
+ drop constraint publication_file_alternate_key;
+
+create index publication_file_pub_fk_index
+ on publication_file (pf_pub_zdb_id)
+ using btree in idxdbs1;
+
+alter table publication_file 
+  add constraint (foreign key (pf_pub_zdb_id)
+  on delete cascade constraint pf_pub_zdb_id_fk_odc);
+
+alter table publication_file add constraint unique(pf_pub_zdb_id,
+    pf_file_name,pf_file_type_id) constraint publication_file_alternate_key 
+     ;
