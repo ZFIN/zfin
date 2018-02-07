e4a3dc0876dadb0605e24f18fe3b7eebaa0a979e
Xiang Shao <xshao@zfin.org>
2018-02-07 14:34:15 -0800

    PUB-419

--- source/org/zfin/db/postGmakePostloaddb/1094/unamusedFace/PUB-419.sql
+++ source/org/zfin/db/postGmakePostloaddb/1094/unamusedFace/PUB-419.sql
@@ -11,6 +11,8 @@ update pub_tracking_history
 
 delete from publication_file where pf_pub_zdb_id = 'ZDB-PUB-150909-9'; 
 
+delete from pub_tracking_history where pth_pub_zdb_id = 'ZDB-PUB-150909-9';
+
 delete from zdb_active_source where zactvs_zdb_id = 'ZDB-PUB-150909-9';
 
 insert into withdrawn_data (wd_old_zdb_id, wd_new_zdb_id, wd_display_note) values ('ZDB-PUB-150909-9', 'ZDB-PUB-050216-43', 'merged');
