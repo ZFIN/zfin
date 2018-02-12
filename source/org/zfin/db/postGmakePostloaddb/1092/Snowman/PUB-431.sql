dba626f5234097e34e5a56f99c8592f324fdb312
Xiang Shao <xshao@zfin.org>
2018-02-12 10:30:36 -0800

    PUB-431

--- source/org/zfin/db/postGmakePostloaddb/1092/Snowman/PUB-431.sql
+++ source/org/zfin/db/postGmakePostloaddb/1092/Snowman/PUB-431.sql
@@ -13,9 +13,8 @@ update int_person_pub
                                 set target_id = 'ZDB-PUB-980313-2'
                               where target_id = 'ZDB-PUB-170217-11';
 
-update pub_tracking_history
-                                set pth_pub_zdb_id = 'ZDB-PUB-980313-2'
-                              where pth_pub_zdb_id = 'ZDB-PUB-170217-11';
+delete from pub_tracking_history 
+ where pth_pub_zdb_id = 'ZDB-PUB-170217-11';
 
 update publication_note
                                 set pnote_pub_zdb_id = 'ZDB-PUB-980313-2'
@@ -26,3 +25,4 @@ delete from publication_file where pf_pub_zdb_id = 'ZDB-PUB-170217-11';
 delete from zdb_active_source where zactvs_zdb_id = 'ZDB-PUB-170217-11';
 
 insert into withdrawn_data (wd_old_zdb_id, wd_new_zdb_id, wd_display_note) values ('ZDB-PUB-170217-11', 'ZDB-PUB-980313-2', 'merged');
+
