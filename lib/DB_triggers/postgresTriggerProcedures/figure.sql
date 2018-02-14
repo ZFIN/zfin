4b3ce48ffb1052ad132650f4f649f61da664899b
Patrick Kalita <pkalita@zfin.org>
2018-02-13 17:19:59 -0800

    INF-3339 Split figure insert/update trigger into before and after steps

--- lib/DB_triggers/postgresTriggerProcedures/figure.sql
+++ lib/DB_triggers/postgresTriggerProcedures/figure.sql
@@ -1,21 +1,36 @@
-drop trigger if exists figure_trigger on figure;
+DROP TRIGGER IF EXISTS figure_before_trigger
+ON figure;
 
-create or replace function figure()
-returns trigger as
+DROP TRIGGER IF EXISTS figure_after_trigger
+ON figure;
+
+CREATE OR REPLACE FUNCTION figure_before()
+  RETURNS trigger AS
 $BODY$
-declare fig_label figure.fig_label%TYPE := zero_pad(NEW.fig_label);
-begin
-
-     perform p_insert_into_record_attribution_tablezdbids (
-			NEW.fig_zdb_id,
-			NEW.fig_source_zdb_id);
-     NEW.fig_label = fig_label;
-
-     RETURN NEW;
-
-end;
+BEGIN
+  NEW.fig_full_label = zero_pad(NEW.fig_label);
+  RETURN NEW;
+END;
 $BODY$ LANGUAGE plpgsql;
 
-create trigger figure_trigger after insert or update on figure
- for each row
- execute procedure figure();
+CREATE OR REPLACE FUNCTION figure_after()
+  RETURNS trigger AS
+$BODY$
+BEGIN
+  PERFORM p_insert_into_record_attribution_tablezdbids(
+      NEW.fig_zdb_id,
+      NEW.fig_source_zdb_id);
+  RETURN NULL;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER figure_before_trigger
+BEFORE INSERT OR UPDATE ON figure
+FOR EACH ROW
+EXECUTE PROCEDURE figure_before();
+
+CREATE TRIGGER figure_after_trigger
+AFTER INSERT OR UPDATE ON figure
+FOR EACH ROW
+EXECUTE PROCEDURE figure_after();
