304f51670ca2e7e59a54c375b39830f0ec787f9b
Xiang Shao <xshao@zfin.org>
2018-02-22 15:19:23 -0800

    CUR-726 added SQLs to merge related genotypes and fish as well

--- source/org/zfin/db/postGmakePostloaddb/1094/unamusedFace/CUR-726.sql
+++ source/org/zfin/db/postGmakePostloaddb/1094/unamusedFace/CUR-726.sql
@@ -38,7 +38,6 @@ update feature_marker_relationship
                               where fmrel_mrkr_zdb_id = 'ZDB-GENE-070117-437';
 
 
-
 update record_attribution set recattrib_data_zdb_id = 'ZDB-GENE-070117-2142' where recattrib_data_zdb_id = 'ZDB-GENE-070117-437';
 
 select get_id('DALIAS') as dalias_id, get_id('NOMEN') as nomen_id from single into temp tmp_ids;
@@ -118,3 +117,128 @@ delete from zdb_active_data where zactvd_zdb_id = 'ZDB-ALT-980203-1826';
 
 insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-ALT-980203-1826', 'ZDB-ALT-980413-591');
 
+delete from record_attribution where recattrib_pk_id = '342675';
+
+update fish 
+                                set fish_genotype_zdb_id = 'ZDB-GENO-120315-11'
+                              where fish_genotype_zdb_id = 'ZDB-GENO-120315-36';
+
+delete from all_map_names where allmapnm_zdb_id = 'ZDB-GENO-120315-36' and allmapnm_name in ('unm tf229', 'kef', 'kefir');
+
+update all_map_names 
+                                set allmapnm_zdb_id = 'ZDB-GENO-120315-11'
+                              where allmapnm_zdb_id = 'ZDB-GENO-120315-36';
+
+select get_id('DALIAS') as dalias_id
+  from single
+ into temp tmp_genoalias_id;
+
+insert into zdb_active_data select dalias_id from tmp_genoalias_id;
+
+insert into data_alias (dalias_zdb_id, dalias_data_zdb_id, dalias_alias, dalias_group_id)
+                            select dalias_id, 'ZDB-GENO-120315-11', 'kef<sup>ztf229/ztf229</sup>', '1'
+                              from tmp_genoalias_id;
+
+delete from record_attribution where recattrib_data_zdb_id = 'ZDB-GENO-120315-36';
+
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-GENO-120315-36';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-GENO-120315-11' where zrepld_new_zdb_id = 'ZDB-GENO-120315-36';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-GENO-120315-36';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-GENO-120315-36', 'ZDB-GENO-120315-11');
+
+delete from record_attribution where recattrib_pk_id = '33888416';
+
+delete from record_attribution where recattrib_pk_id = '33959244';
+
+update record_attribution set recattrib_data_zdb_id = 'ZDB-FISH-150901-13192' where recattrib_data_zdb_id = 'ZDB-FISH-150901-16218';
+
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-FISH-150901-16218';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-FISH-150901-13192' where zrepld_new_zdb_id = 'ZDB-FISH-150901-16218';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-16218';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-FISH-150901-16218', 'ZDB-FISH-150901-13192');
+
+
+delete from record_attribution where recattrib_pk_id = '1677469';
+
+update fish 
+                                set fish_genotype_zdb_id = 'ZDB-GENO-980410-219'
+                              where fish_genotype_zdb_id = 'ZDB-GENO-980202-1564';
+
+update genotype_figure_fast_search 
+                                set gffs_geno_zdb_id = 'ZDB-GENO-980410-219'
+                              where gffs_geno_zdb_id = 'ZDB-GENO-980202-1564';
+
+delete from all_map_names where allmapnm_zdb_id = 'ZDB-GENO-980202-1564' and allmapnm_name in ('unm tf229', 'kef', 'kefir');
+
+update all_map_names 
+                                set allmapnm_zdb_id = 'ZDB-GENO-980410-219'
+                              where allmapnm_zdb_id = 'ZDB-GENO-980202-1564';
+
+
+select get_id('DALIAS') as dalias_id
+  from single
+ into temp tmp_genoalias_id2;
+
+insert into zdb_active_data select dalias_id from tmp_genoalias_id2;
+
+insert into data_alias (dalias_zdb_id, dalias_data_zdb_id, dalias_alias, dalias_group_id)
+                            select dalias_id, 'ZDB-GENO-980410-219', 'kef<sup>ztf229</sup>', '1'
+                              from tmp_genoalias_id2;
+
+
+delete from record_attribution where recattrib_pk_id = 72825822;
+
+delete from record_attribution where recattrib_pk_id = 72825823;
+
+update record_attribution set recattrib_data_zdb_id = 'ZDB-GENO-980410-219' where recattrib_data_zdb_id = 'ZDB-GENO-980202-1564';
+
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-GENO-980202-1564';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-GENO-980410-219' where zrepld_new_zdb_id = 'ZDB-GENO-980202-1564';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-GENO-980202-1564';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-GENO-980202-1564', 'ZDB-GENO-980410-219');
+
+delete from record_attribution where recattrib_pk_id = '33886572';
+
+delete from record_attribution where recattrib_pk_id = '33925449';
+
+delete from record_attribution where recattrib_pk_id = '33969108';
+
+delete from mutant_fast_search where mfs_genox_zdb_id = 'ZDB-GENOX-041102-488' and mfs_mrkr_zdb_id = 'ZDB-GENE-070117-2142';
+
+update mutant_fast_search 
+                                set mfs_genox_zdb_id = 'ZDB-GENOX-041102-2123'
+                              where mfs_genox_zdb_id = 'ZDB-GENOX-041102-488';
+
+update phenotype_experiment 
+                                set phenox_genox_zdb_id = 'ZDB-GENOX-041102-2123'
+                              where phenox_genox_zdb_id = 'ZDB-GENOX-041102-488';
+
+delete from fish_experiment where genox_zdb_id = 'ZDB-GENOX-041102-488';
+
+update fish_experiment 
+                                set genox_fish_zdb_id = 'ZDB-FISH-150901-8841'
+                              where genox_fish_zdb_id = 'ZDB-FISH-150901-15170';
+
+update genotype_figure_fast_search 
+                                set gffs_fish_zdb_id = 'ZDB-FISH-150901-8841'
+                              where gffs_fish_zdb_id = 'ZDB-FISH-150901-15170';
+
+
+update record_attribution set recattrib_data_zdb_id = 'ZDB-FISH-150901-8841' where recattrib_data_zdb_id = 'ZDB-FISH-150901-15170';
+
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-FISH-150901-15170';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-FISH-150901-8841' where zrepld_new_zdb_id = 'ZDB-FISH-150901-15170';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-15170';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-FISH-150901-15170', 'ZDB-FISH-150901-8841');
