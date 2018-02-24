ee88171652883b9dd6d9d72fd57f82b3133e68a8
Christian Pich <cmpich@zfin.org>
2018-02-23 16:11:59 -0800

    ONT-650 add zfin-ro ontology

--- source/org/zfin/db/load/1095/VulcanSalute/zfin-ro-ontology.sql
+++ source/org/zfin/db/load/1095/VulcanSalute/zfin-ro-ontology.sql
@@ -1,12 +1,12 @@
 --liquibase formatted sql
---changeset christian:loadZecoTaxonomy
+--changeset christian:loadZRO
 
 INSERT INTO ontology
             (ont_pk_id,
              ont_ontology_name,
              ont_default_namespace,
              ont_order)
-VALUES      (20,
-             'cl',
-             'cl',
-             20);
+VALUES      (21,
+             'zfin-ro',
+             'zfin-ro',
+             21);
