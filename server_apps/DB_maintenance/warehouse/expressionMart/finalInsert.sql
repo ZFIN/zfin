ab75961cc757279d6ff7d6916a1816ead339a2ec
Sierra Moxon <smoxon@zfin.org>
2018-03-02 08:51:41 -0800

    remove duplicate index

--- server_apps/DB_maintenance/warehouse/expressionMart/finalInsert.sql
+++ server_apps/DB_maintenance/warehouse/expressionMart/finalInsert.sql
@@ -2,5 +2,4 @@ INSERT INTO expression_search_anatomy_generated (esag_efs_id, esag_term_name, es
   SELECT DISTINCT esag_efs_id, esag_term_name, esag_is_direct
   FROM tmp_esag_predistinct;
 
-CREATE INDEX esag_efs_id_index
-  ON expression_search_anatomy_generated (esag_efs_id);
+update statistics high for table expression_search_anatomy_generated;
