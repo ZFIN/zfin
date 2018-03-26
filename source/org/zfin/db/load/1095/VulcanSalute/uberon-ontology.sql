88d0eda32e0260957b72a115e1ef63001d3787ff
Christian Pich <cmpich@zfin.org>
2018-03-26 10:22:42 -0700

    ONT-652 widen tx_accession column

--- source/org/zfin/db/load/1095/VulcanSalute/uberon-ontology.sql
+++ source/org/zfin/db/load/1095/VulcanSalute/uberon-ontology.sql
@@ -10,3 +10,22 @@ VALUES      (23,
              'uberon',
              'uberon',
              23);
+
+alter table term_xref add temp varchar(250);
+
+update term_xref set temp = tx_accession;
+
+alter table term_xref drop tx_accession;
+
+alter table term_xref add tx_accession varchar(250);
+
+update term_xref set tx_accession = temp;
+
+alter table term_xref drop temp;
+
+create unique index term_xref_alternate_key_index
+    on term_xref (tx_term_zdb_id,tx_accession,tx_prefix)
+    using btree in idxdbs2;
+
+alter table term_xref add constraint unique(tx_term_zdb_id,
+    tx_accession,tx_prefix) constraint term_xref_alternate_key;
