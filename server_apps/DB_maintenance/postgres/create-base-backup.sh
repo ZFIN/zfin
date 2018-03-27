12183127cb4824f5d02065c8d83b4764e7bede96
Sierra Moxon <smoxon@zfin.org>
2018-03-27 08:42:31 -0700

    INF-3273

---
+++ server_apps/DB_maintenance/postgres/create-base-backup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+dumpLocation=/research/zunloads/databases/postgres_dumps/${DBNAME}
+echo "dumpLocation"
+echo $dumpLocation
+
+cd $dumpLocation
+
+latestDump=`ls -td -- */ | head -n 1 | cut -d'/' -f1`
+echo $latestDump
+
+mkdir $dumpLocation/$latestDump
+
+pg_basebackup -h localhost -D /research/zunloads/databases/postgres_base_backup/${DBNAME}/$latestDump
