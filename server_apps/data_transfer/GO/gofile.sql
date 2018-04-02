d655d7f04621e918f118049ad6ce0d2aaf6ec4a7
Prita Mani <pm@zfin.org>
2018-04-02 11:30:15 -0700

    ONT-646

--- server_apps/data_transfer/GO/gofile.sql
+++ server_apps/data_transfer/GO/gofile.sql
@@ -27,6 +27,7 @@ create temp table tmp_go_annot (mev_zdb_id varchar(50),anno_group_id int8,annoex
 insert into tmp_go_annot (mev_zdb_id,anno_group_id,annoextn) select distinct mrkrgoev_zdb_id, mgtaeg_annotation_extension_group_id, term_name||'('||mgtae_term_text||')'
 from marker_go_term_evidence, marker_go_term_annotation_extension_group, marker_go_term_annotation_extension,term
 where mrkrgoev_zdb_id= mgtaeg_mrkrgoev_zdb_id and mgtae_relationship_term_zdb_id=term_zdb_id and mgtae_extension_group_id=mgtaeg_annotation_extension_group_id ;
+
 create temp table tmp_go_identifiers (goid varchar(50), goid2 int8,goid3 lvarchar(1500))
 with no log;
 
@@ -38,6 +39,19 @@ update tmp_go_identifiers  set goid3 = replace(replace(replace(substr(multiset (
 
                                                          )::lvarchar(4000),11),""),"'}",""),"'","");
 
+create temp table tmp_go_identifiers_pipes (goidtmp varchar(50), goid3tmp lvarchar(2000))
+with no log;
+
+insert into tmp_go_identifiers_pipes (goidtmp)
+ select distinct goid from tmp_go_identifiers;
+
+update tmp_go_identifiers_pipes  set goid3tmp = replace(replace(replace(substr(multiset (select distinct item replace(goid3,",","Prita") from tmp_go_identifiers
+                                                          where tmp_go_identifiers.goid = tmp_go_identifiers_pipes.goidtmp
+                                                          )::lvarchar(4000),11),""),"'}",""),"'","");
+
+
+
+unload to 'tmp_go_idspipes' select * from tmp_go_identifiers_pipes order by goidtmp;
 
 create temp table tmp_go (mv_zdb_id varchar(50),
        	    	  	 m_zdb_id varchar(50),
@@ -76,16 +90,16 @@ insert into tmp_go (mv_zdb_id,
 select mrkrgoev_zdb_id,
 				mrkr_zdb_id, mrkr_abbrev, mrkr_name, term1.term_ont_id, mrkrgoev_source_zdb_id,
 				accession_no, mrkrgoev_evidence_code, infgrmem_inferred_from, mrkrgoev_gflag_name,
-				upper(term_ontology[1]), mrkrgoev_date_modified, mrkrgoev_annotation_organization_created_by,goid3,lower(szm_term_name)
+				upper(term_ontology[1]), mrkrgoev_date_modified, mrkrgoev_annotation_organization_created_by,goid3tmp,lower(szm_term_name)
 			   from marker_go_term_evidence, marker, term term1, publication, so_zfin_mapping,
 					   outer inference_group_member,
-					   outer  tmp_go_identifiers
+					   outer  tmp_go_identifiers_pipes
 			  where mrkrgoev_mrkr_zdb_id = mrkr_zdb_id
 			    and mrkrgoev_term_zdb_id = term1.term_zdb_id
 			    and mrkrgoev_source_zdb_id  = zdb_id
 			    and mrkr_type = szm_object_type
 			    and mrkrgoev_zdb_id = infgrmem_mrkrgoev_zdb_id
-			    and mrkrgoev_zdb_id=goid;
+			    and mrkrgoev_zdb_id=goidtmp;
 
 
 select distinct gene_type from tmp_go where gene_type is not null;
@@ -95,7 +109,7 @@ update tmp_go
       	    	    where m_zdb_id = id); 
 update tmp_go
 set mv_created_by='UniProt' where mv_created_by='UniProtKB';
-
+select * from tmp_go where mv_zdb_id='ZDB-MRKRGOEV-180328-54';
 select first 1 * from tmp_go
  where m_abbrev = 'pax8';
 
