cfec85d0a719239be3b728bc456cb7184fc757d6
Christian Pich <cmpich@zfin.org>
2018-04-12 15:36:22 -0700

    DLOAD-520 create Noctua foreign_db record

--- source/org/zfin/db/postGmakePostloaddb/1096/XBones/GPAD-Load.sql
+++ source/org/zfin/db/postGmakePostloaddb/1096/XBones/GPAD-Load.sql
@@ -8,3 +8,7 @@ insert into marker_go_term_evidence_annotation_organization
 alter table noctua_model_annotation drop constraint nma_noctua_model_fk_odc;
 
 alter table noctua_model_annotation drop nma_pk_id;
+
+-- create foreign_db record to Noctua site
+insert into foreign_db (fdb_db_name, fdb_db_query, fdb_db_display_name,fdb_db_significance)
+values ('Noctua', 'http://noctua.berkeleybop.org/editor/graph/','Noctua', 20);
