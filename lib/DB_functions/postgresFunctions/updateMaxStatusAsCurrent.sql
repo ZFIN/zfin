e8913039743832ac65fac5268494dcb37f8b5a89
Christian Pich <cmpich@zfin.org>
2018-04-19 15:46:59 -0700

    INF-3417 fix trigger for pub_tracking_history

--- lib/DB_functions/postgresFunctions/updateMaxStatusAsCurrent.sql
+++ lib/DB_functions/postgresFunctions/updateMaxStatusAsCurrent.sql
@@ -8,8 +8,7 @@ begin
   update pub_tracking_history
     set (pth_status_is_current, pth_status_made_non_current_date, pth_days_in_status) =('f', now_time, date(now_time)-date(pth_status_insert_date))
     where pth_status_is_current = 't'
-    and pth_pub_zdb_id = workingPub
-    and pth_status_is_current = 't';
+    and pth_pub_zdb_id = workingPub;
 
 return 't';
 end
--- lib/DB_triggers/postgresTriggerProcedures/pub_tracking_history.sql
+++ lib/DB_triggers/postgresTriggerProcedures/pub_tracking_history.sql
@@ -4,18 +4,22 @@ create or replace function pub_tracking_history()
 returns trigger as
 $BODY$
 begin
-	perform checkPubTrackingLocationOwner (NEW.pth_pub_zdb_id,
+
+       perform updateMaxStatusAsCurrent(NEW.pth_pub_zdb_id);
+       NEW.pth_status_is_current = 't';
+
+        perform checkPubTrackingLocationOwner (NEW.pth_pub_zdb_id,
                                                 NEW.pth_status_id,
                                                 NEW.pth_location_id,
                                                 NEW.pth_claimed_by);
-	perform updatePubCompletionDate(NEW.pth_pub_zdb_id,
+        perform updatePubCompletionDate(NEW.pth_pub_zdb_id,
                                                 NEW.pth_status_id);
-	perform updatePubIndexedDate(NEW.pth_pub_zdb_id,
+        perform updatePubIndexedDate(NEW.pth_pub_zdb_id,
                                                 NEW.pth_status_id);
-        RETURN NULL;
+        RETURN NEW;
 end;
 $BODY$ LANGUAGE plpgsql;
 
-create trigger pub_tracking_history_trigger after insert or update on pub_tracking_history
+create trigger pub_tracking_history_trigger before insert on pub_tracking_history
  for each row
  execute procedure pub_tracking_history();
