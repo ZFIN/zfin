04bcb3dd10eb00a4a318c97c39c19e3ca0fbe66c
informix <informix@zfin.org>
2018-04-20 08:24:00 -0700

    fixing SQL syntax (Christian)

--- source/org/zfin/db/postGmakePostloaddb/1097/YinYang/CUR-757.sql
+++ source/org/zfin/db/postGmakePostloaddb/1097/YinYang/CUR-757.sql
@@ -2,7 +2,7 @@
 --changeset pm:CUR-757
 
 delete from zdb_active_data where zactvd_zdb_id='ZDB-MREL-180129-6';
-delete form zdb_active_Data where zactvd_zdb_id='ZDB-CMREL-180129-4';
+delete from zdb_active_Data where zactvd_zdb_id='ZDB-CMREL-180129-4';
 
 update construct_Component set cc_component_zdb_id='ZDB-CV-150506-13' where cc_construct_Zdb_id='ZDB-TGCONSTRCT-180129-2' and cc_order=5;
 update construct_Component set cc_component='NRAS_Q61K' where cc_construct_Zdb_id='ZDB-TGCONSTRCT-180129-2' and cc_order=6;
