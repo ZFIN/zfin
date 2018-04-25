83e16f6a8ef10189641f158014bd75e9e00b4c12
Sierra Moxon <smoxon@zfin.org>
2018-04-25 13:27:49 -0700

    add hostname param to rotate logs

--- server_apps/DB_maintenance/rotatePostgresLogFiles.sh
+++ server_apps/DB_maintenance/rotatePostgresLogFiles.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # make a base backup 
-/opt/postgres/postgresql/bin/pg_basebackup --xlog --format=t -D /research/zunloads/databases/punkt/base_backups/`date +%Y%m%d`
+/opt/postgres/postgresql/bin/pg_basebackup --wal_method='fetch' --format=t -D /research/zunloads/databases/${HOSTNAME}/base_backups/`date +%Y%m%d`
 
 # compress wal archives, add to data directory.
 cd /research/zunloads/databases/punkt/
 tar -cf archives.tar wal_log_archive
-mv archives.tar /research/zunloads/databases/punkt/base_backups/`date +%Y%m%d`
-cd /research/zunloads/databases/punkt/base_backups/`date +%Y%m%d`
+mv archives.tar /research/zunloads/databases/${HOSTNAME}/base_backups/`date +%Y%m%d`
+cd /research/zunloads/databases/${HOSTNAME}/base_backups/`date +%Y%m%d`
 
 # delete WAL log archive files older than 3 days (assumes the base backup happens nightly)
 
-cd /research/zunloads/databases/punkt/wal_log_archive/
+cd /research/zunloads/databases/${HOSTNAME}/wal_log_archive/
 find -mtime +2 -exec rm {} \;
