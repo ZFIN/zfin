958b2bbdb36d6250baa0889a7d9ac182bfd88d82
Sierra Moxon <smoxon@zfin.org>
2018-04-30 13:14:01 -0700

    fix regen_genofig_phenox

--- lib/DB_functions/postgresFunctions/Regen_genox/regen_genofig_phenox.sql
+++ lib/DB_functions/postgresFunctions/Regen_genox/regen_genofig_phenox.sql
@@ -1,4 +1,5 @@
-create procedure regen_genofig_phenox(pgId int8)
+create or replace function regen_genofig_phenox(pgId int8)
+returns void as $regen_genofig_phenox$
 
   -- ---------------------------------------------------------------------------------------------
   -- regenerates records in fast search table genotype_figure_fast_search for a given genotype.
@@ -12,29 +13,28 @@ create procedure regen_genofig_phenox(pgId int8)
   --   Success: nothing
   --   Error:   throws whatever exception occurred.
   -- ---------------------------------------------------------------------------------------------
-  define genoxId like fish_experiment.genox_zdb_id;
+  declare genoxId text;
 
-  -- crank up the parallelism.
-  set pdqpriority high;
-
-
+begin
 
   -- create regen_genofig_clean_exp_with_morph_temp, regen_genofig_not_normal_temp,
   --        regen_genofig_temp, regen_genofig_input_zdb_id_temp
-  let genoxId = (select pg_genox_zdb_id from phenotype_source_generated 
+  genoxId := (select pg_genox_zdb_id from phenotype_source_generated 
       	      		where pg_id = pgId);
 
-  execute procedure regen_genofig_create_temp_tables();
+  perform regen_genofig_create_temp_tables();
 
-  execute procedure regen_genox_genox(genoxId);
+  perform regen_genox_genox(genoxId);
   -- takes regen_genofig_input_zdb_id_temp as input, adds recs to regen_genofig_temp
 
   insert into  regen_genofig_input_zdb_id_temp ( rgfg_id )
      values (pgId);
 
-  execute procedure regen_genofig_process();
+  perform regen_genofig_process();
 
   -- Move from temp tables to permanent tables
-  execute procedure regen_genofig_finish('t',pgId);
+  perform regen_genofig_finish('t',pgId);
 
-end procedure;
+end
+
+$regen_genofig_phenox$ LANGUAGE plpgsql;
