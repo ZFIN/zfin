307963712496acf68a2324a41d1433986b4c87d2
Sierra Moxon <smoxon@zfin.org>
2018-05-10 13:23:59 -0700

    INF-3450

--- server_apps/DB_maintenance/unload_production_PG.sh
+++ server_apps/DB_maintenance/unload_production_PG.sh
@@ -3,7 +3,7 @@
 set pth=/research/zunloads/databases/${DBNAME}
 set dirname=`date +"%Y.%m.%d.1"`
 
-${PGBINDIR}/psql $DBNAME ${SOURCEROOT}/server_apps/DB_maintenance/set_unload_timestamp.sql
+${PGBINDIR}/psql $DBNAME < ${SOURCEROOT}/server_apps/DB_maintenance/set_unload_timestamp_PG.sql
 
 # increment until we get name which has not been taken
 while ( -d $pth/$dirname )
@@ -13,9 +13,15 @@ while ( -d $pth/$dirname )
 	set dirname=$y.$x
 end
 
-${PGBINDIR}/pg_dumpall --clean --verbose --no-role-passwords >  $pth/$dirname
+mkdir $pth/$dirname
+
+${PGBINDIR}/pg_dump -Fc ${DBNAME} -f $pth/$dirname.bak
+
+${PGBINDIR}/pg_dumpall --clean --verbose --no-role-passwords >  $pth/$dirname/
+
 if ($? != "0") then
   /bin/rm -rf $pth/$dirname
+  echo "unload_production failed"
 else 
   chgrp -R fishadmin $pth/$dirname
   chmod -R g+rw $pth/$dirname
@@ -25,3 +31,4 @@ else
 
   echo "pg_dumpall completed successfully."
 endif
+
