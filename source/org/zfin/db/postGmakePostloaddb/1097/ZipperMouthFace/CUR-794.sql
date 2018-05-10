cdca2f7276d80c2062f3d74a5df5ddb3e46953d4
Prita Mani <pm@zfin.org>
2018-05-10 12:15:07 -0700

    CUR-797 (removed deletion from feature table)

--- source/org/zfin/db/postGmakePostloaddb/1097/ZipperMouthFace/CUR-794.sql
+++ source/org/zfin/db/postGmakePostloaddb/1097/ZipperMouthFace/CUR-794.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset pm:CUR-794
 
-delete from zdb_active_data where zactvd_zdb_id='ZDB-ALT-171004-2';
+
 delete from feature_tracking where where ft_feature_zdb_id='ZDB-ALT-171004-2';
 
 
