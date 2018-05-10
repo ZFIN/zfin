0642fdfb52c3eb8f7b378163f63c25884db4e146
Sierra Moxon <smoxon@zfin.org>
2018-05-10 10:53:28 -0700

    INF-3409

--- server_apps/DB_maintenance/rotatePostgresLogFiles.sh
+++ server_apps/DB_maintenance/rotatePostgresLogFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set pth=/research/zunloads/databases/${HOSTNAME}
+set pth=/opt/zfin/postgres_wal/base_backups
 
 set dirname=`date +"%Y.%m.%d.1"`
 
@@ -14,14 +14,14 @@ end
 
 
 # make a base backup 
-/opt/postgres/postgresql/bin/pg_basebackup --wal-method='fetch' --format=t -D /research/zunloads/databases/${HOSTNAME}/base_backups/`date +%Y%m%d`
+/opt/postgres/postgresql/bin/pg_basebackup --wal-method='fetch' --format=t -D /opt/zfin/postgres_wal/base_backups/`date +%Y%m%d`
 
 # compress wal archives, add to data directory.
-cd /research/zunloads/databases/${HOSTNAME}/
-tar -cf archives.tar wal_log_archive
-mv archives.tar /research/zunloads/databases/${HOSTNAME}/base_backups/`date +%Y%m%d`
+cd /opt/zfin/postgres_wal/
+tar -cf archives.tar wal_archive
+mv archives.tar /opt/zfin/postgres_wal/base_backups/`date +%Y%m%d`
 
 # delete WAL log archive files older than 3 days (assumes the base backup happens nightly)
 
-cd /research/zunloads/databases/${HOSTNAME}/wal_log_archive/
+cd /opt/zfin/postgres_wal/wal_archive
 find -mtime +2 -exec rm {} \;
