ee823a5368a6c59c4988b5a1cc8e559905f442c3
Sierra Moxon <smoxon@zfin.org>
2018-05-10 11:02:16 -0700

    INF-3409

--- server_apps/DB_maintenance/rotatePostgresLogFiles.sh
+++ server_apps/DB_maintenance/rotatePostgresLogFiles.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-set pth=/opt/zfin/postgres_wal/base_backups
-
-set dirname=`date +"%Y.%m.%d.1"`
-
-# increment until we get name which has not been taken
-while ( -d $pth/$dirname )
-	set z=$dirname:e
-	set y=$dirname:r
-@ x = $z + 1
-	set dirname=$y.$x
-end
-
-
 # make a base backup 
 /opt/postgres/postgresql/bin/pg_basebackup --wal-method='fetch' --format=t -D /opt/zfin/postgres_wal/base_backups/`date +%Y%m%d`
 
