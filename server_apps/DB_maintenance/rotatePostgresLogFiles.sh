8c35285251ea040b499ef388b64b247793092df7
Sierra Moxon <smoxon@zfin.org>
2018-05-11 08:54:01 -0700

    gzip the base backup

--- server_apps/DB_maintenance/rotatePostgresLogFiles.sh
+++ server_apps/DB_maintenance/rotatePostgresLogFiles.sh
@@ -1,9 +1,10 @@
-#!/bin/tcsh -e                                                                           
+#!/bin/tcsh -e                                                                                       
 
 set pth=/opt/zfin/postgres_wal/base_backups
 set dirname=`date +"%Y.%m.%d.1"`
 
-# increment until we get name which has not been taken                                               
+# increment until we get name which has not been taken                                              \
+                                                                                                     
 while ( -d $pth/$dirname )
         set z=$dirname:e
         set y=$dirname:r
@@ -13,19 +14,22 @@ end
 
 mkdir $pth/$dirname
 
-# make a base backup                                                                                 
+# make a base backup                                                                                \
+                                                                                                     
 /opt/postgres/postgresql/bin/pg_basebackup --wal-method='fetch' --format=t -D $pth/$dirname
 
-# compress wal archives, add to data directory.                                                      
+# compress wal archives, add to data directory.                                                     \
+                                                                                                     
 cd /opt/zfin/postgres_wal/
 tar -cf $dirname.archives.tar wal_archive
-gzip $dirname.archives.tar
-mv $dirname.archives.tar.gz $pth/$dirname
+mv $dirname.archives.tar $pth/$dirname
 
 
 tar -cf $pth/$dirname/$dirname.filesytem.tar /opt/postgres/data
 gzip $pth/$dirname/$dirname.filesytem.tar
 
-# delete WAL log archive files older than 3 days (assumes the base backup happens nightly)           
+# delete WAL log archive files older than 3 days (assumes the base backup happens nightly)          \
+                                                                                                     
+
 cd /opt/zfin/postgres_wal/wal_archive
 find -mtime +2 -exec rm {} \;
