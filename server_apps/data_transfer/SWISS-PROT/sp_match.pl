c1435069ab82b907b068654ca965fdd047265970
Xiang Shao <xshao@zfin.org>
2018-05-14 16:44:37 -0700

    DLOAD-358 added back some email sending code

--- server_apps/data_transfer/SWISS-PROT/sp_match.pl
+++ server_apps/data_transfer/SWISS-PROT/sp_match.pl
@@ -5,6 +5,10 @@
 # and writes it into ok2file which would be appended to okfile
 # in loadsp.pl process. 
 #
+
+use lib "<!--|ROOT_PATH|-->/server_apps/";
+use ZFINPerlModules;
+
 if (@ARGV < 1) {
     die "Please enter the accession file name. \n";
 }
@@ -19,8 +23,25 @@ open OK, ">ok2file" or die "Cannot open ok2file";
 $/ = "//\n";
 while (<FILE>) {
     foreach $acc (split /\n/,$accfile) {
-	print OK if /$acc/;
+        print OK if /$acc/;
     }
 }
 close FILE;
 close OK;
+
+  $dbname = "<!--|DB_NAME|-->";
+
+  $subject = "Auto from $dbname: SWISS-PROT check report";
+  ZFINPerlModules->sendMailWithAttachedReport("<!--|SWISSPROT_EMAIL_REPORT|-->","$subject","checkreport.txt");
+  		
+  #----- Another mail send out problem files ----
+  $subject = "Auto from $dbname: SWISS-PROT problem file";
+  ZFINPerlModules->sendMailWithAttachedReport("<!--|SWISSPROT_EMAIL_REPORT|-->","$subject","allproblems.txt");
+
+  #----- Another mail send out problem files ----
+  $subject = "Auto from $dbname: PubMed not in ZFIN";
+  ZFINPerlModules->sendMailWithAttachedReport("<!--|SWISSPROT_EMAIL_REPORT|-->","$subject","pubmed_not_in_zfin");  
+
+  #----- Another mail send out problem files ----
+  $subject = "Auto from $dbname: report of processing pre_zfin.org";
+  ZFINPerlModules->sendMailWithAttachedReport("<!--|SWISSPROT_EMAIL_REPORT|-->","$subject","redGeneReport.txt");
