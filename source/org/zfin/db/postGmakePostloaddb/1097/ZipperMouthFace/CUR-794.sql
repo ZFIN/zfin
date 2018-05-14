909123e4927c4a1b3e28ce0714c8d741596ac0a5
Prita Mani <pm@zfin.org>
2018-05-14 08:41:47 -0700

    CUR-794 fixed syntax error

--- source/org/zfin/db/postGmakePostloaddb/1097/ZipperMouthFace/CUR-794.sql
+++ source/org/zfin/db/postGmakePostloaddb/1097/ZipperMouthFace/CUR-794.sql
@@ -2,7 +2,7 @@
 --changeset pm:CUR-794
 
 
-delete from feature_tracking where where ft_feature_zdb_id='ZDB-ALT-171004-2';
+delete from feature_tracking where  ft_feature_zdb_id='ZDB-ALT-171004-2';
 
 
 
