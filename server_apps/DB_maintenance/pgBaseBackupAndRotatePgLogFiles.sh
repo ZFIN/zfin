b00b5c43479ba5f79ab5238620978bf4aaff8e6c
Sierra Moxon <smoxon@zfin.org>
2018-05-23 12:30:59 -0700

    add doc to file, clean up paths

--- server_apps/DB_maintenance/pgBaseBackupAndRotatePgLogFiles.sh
+++ server_apps/DB_maintenance/pgBaseBackupAndRotatePgLogFiles.sh
@@ -2,7 +2,6 @@
 
 set pth=/opt/postgres/postgres_wal/base_backups
 
-set pth=/opt/zfin/postgres_wal/base_backups
 set dirname=`date +"%Y.%m.%d.1"`
 
 
@@ -14,18 +13,36 @@ while ( -d $pth/$dirname )
 	set dirname=$y.$x
 end
 
+mkdir $pth/$dirname
 # make a base backup 
-/opt/postgres/postgresql/bin/pg_basebackup --wal-method='fetch' --format=t -D /opt/postgres/postgres_wal/base_backups/$dirname/`date +%Y%m%d` -v -P
 
-# compress wal archives, add to data directory.
+# -wal-method='stream' -> Stream the write-ahead log while the backup is created. This will open a second connection to the server and start streaming 
+#     the write-ahead log in parallel while running the backup. Therefore, it will use up two connections configured by the max_wal_senders parameter. 
+#     As long as the client can keep up with write-ahead log received, using this mode requires no extra write-ahead logs to be saved on the master. 
+#     The write-ahead log files are written to a separate file named pg_wal.tar (if the server is a version earlier than 10, the file will be named 
+#     pg_xlog.tar).  This value is the default.
+
+# -format=t  -> Write the output as tar files in the target directory. The main data directory will be written to a file named base.tar, and all other tablespaces will be named after the tablespace OID.
+
+# -D output directory for the base.tar file
+# -v verbose
+# -P progress is posted to standard out
+# -z gzip output
+
+/opt/postgres/postgresql/bin/pg_basebackup --wal-method='stream' --format=t -D $pth/$dirname/ -v -P -z
+
+# compress wal archives, add to data directory.  the method above also captures wal files, this command is to help secure a 2 day supply of
+# wal files so we can comfortably remove anything older than a day.
+
 cd /opt/postgres/postgres_wal/
 tar -cf archives.tar wal_archive
-mv archives.tar /opt/postgres/postgres_wal/base_backups/$dirname
+mv archives.tar $pth/$dirname
+
+# take a filesystem backup as well, which does backup /opt/postgres/data
 
 tar -cf $pth/$dirname/$dirname.filesytem.tar /opt/postgres/data
 gzip $pth/$dirname/$dirname.filesytem.tar
-gzip $pth/$dirname/base.tar
-# delete WAL log archive files older than 3 days (assumes the base backup happens nightly)                                                                                       
 
+# delete WAL log archive files older than 3 days (assumes the base backup and filesystem backups above, happen nightly)          
 cd /opt/postgres/postgres_wal/wal_archive/
 find -mtime +2 -exec rm {} \;
