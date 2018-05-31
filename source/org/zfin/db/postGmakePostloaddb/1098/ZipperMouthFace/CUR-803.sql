a97d9bf974ff595debfb4355d18fb542fd887bb9
Christian Pich <cmpich@zfin.org>
2018-05-31 12:26:29 -0700

    Revert "CUR-803 remove record attribution to curation pub ZDB-PUB-150729-10 to allow removing fish and genotype"
    This reverts commit 682b64c0d0ba8e2a0d882fff758c3a7f1639f771.

--- source/org/zfin/db/postGmakePostloaddb/1098/ZipperMouthFace/CUR-803.sql
+++
@@ -1,7 +0,0 @@
---liquibase formatted sql
---changeset Christian:CUR-803
-
-
-delete from record_attribution where  recattrib_data_zdb_id = 'ZDB-FISH-150901-28051' and recattrib_source_zdb_id  = 'ZDB-PUB-150729-10';
-
-delete from record_attribution where  recattrib_data_zdb_id = 'ZDB-GENO-071024-1' and recattrib_source_zdb_id  = 'ZDB-PUB-150729-10';
