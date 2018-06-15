81ffb859401823a8da7529231479e07f136b3cc4
Sierra Moxon <smoxon@zfin.org>
2018-06-15 06:50:22 -0700

    tweaking base backup and archive a bit more

--- server_apps/DB_maintenance/pgBaseBackupAndRotatePgLogFiles.sh
+++ server_apps/DB_maintenance/pgBaseBackupAndRotatePgLogFiles.sh
@@ -41,8 +41,11 @@ mv archives.tar.gz $pth/$dirname
 
 # take a filesystem backup as well, which does backup /opt/postgres/data
 
-tar -cf $pth/$dirname/$dirname.filesytem.tar /opt/postgres/data
+cd /opt/postgres/
+tar -cf $pth/$dirname/$dirname.filesytem.tar -h /opt/postgres/data
+cd /opt/postgres/postgres_wal/
 gzip $pth/$dirname/$dirname.filesytem.tar
+gzip $pth/$dirname/base.tar
 
 # delete WAL log archive files older than 3 days (assumes the base backup and filesystem backups above, happen nightly)          
 cd /opt/postgres/postgres_wal/wal_archive/
