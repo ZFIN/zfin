b9f3cceb73bf7f75bbdb0e7f006c7d6dada7941d
Sierra Moxon <smoxon@zfin.org>
2018-06-15 10:45:08 -0700

    INF-3524

--- server_apps/DB_maintenance/validatedata/Genotype-Name-Function-Mismatch_w.sql
+++ server_apps/DB_maintenance/validatedata/Genotype-Name-Function-Mismatch_w.sql
@@ -1,3 +1,3 @@
 select geno_zdb_id, geno_display_name, get_genotype_display(geno_zdb_id)
  from genotype
- where trim(get_genotype_display(geno_zdb_id)) != trim(geno_display_name);
+ where get_genotype_display(geno_zdb_id) != geno_display_name;
