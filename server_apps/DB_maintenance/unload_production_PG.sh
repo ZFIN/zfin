59f152d3e6189a1b253348b1d96f099720b6f497
Sierra Moxon <smoxon@zfin.org>
2018-06-21 09:20:21 -0700

    move dumpall to main dir

--- server_apps/DB_maintenance/unload_production_PG.sh
+++ server_apps/DB_maintenance/unload_production_PG.sh
@@ -13,14 +13,15 @@ while ( -d $pth/$dirname )
 	set dirname=$y.$x
 end
 
+echo "pg_dumpall starting"
+
+${PGBINDIR}/pg_dumpall --clean --verbose --no-role-passwords >  $pth/`date +"%Y.%m.%d.1"`.dumpall
+
 mkdir $pth/$dirname
 
 echo "pg_dump starting"
 
 ${PGBINDIR}/pg_dump -Fc ${DBNAME} -f $pth/$dirname/`date +"%Y.%m.%d.1"`.bak
-echo "pg_dumpall starting"
-
-${PGBINDIR}/pg_dumpall --clean --verbose --no-role-passwords >  $pth/$dirname/`date +"%Y.%m.%d.1"`.dumpall
 
 if ($? != "0") then
   /bin/rm -rf $pth/$dirname
