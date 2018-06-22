8c383a09d00586be9d731750bb399d82f74b3806
Sierra Moxon <smoxon@zfin.org>
2018-06-22 13:04:34 -0700

    pushing master fixes to release-1097

--- lib/DB_triggers/postgresTriggerProcedures/marker_abbrev.sql
+++ lib/DB_triggers/postgresTriggerProcedures/marker_abbrev.sql
@@ -2,38 +2,28 @@ drop trigger if exists marker_abbrev_trigger on marker;
 drop trigger if exists marker_abbrev_insert_trigger on marker;
 drop trigger if exists marker_abbrev_update_trigger on marker;
 
-create or replace function marker_abbrev()
+
+create or replace function marker_abbrev_insert()
 returns trigger as
 $BODY$
 
-declare mrkr_abbrev marker.mrkr_abbrev%TYPE := scrub_char(NEW.mrkr_abbrev);
-	mrkr_abbrev_order marker.mrkr_abbrev_order%TYPE := zero_pad(mrkr_abbrev);
+declare mrkr_abbrev marker.mrkr_abbrev%TYPE;
+	mrkr_abbrev_order marker.mrkr_abbrev_order%TYPE;
     
 
 begin
 
-     NEW.mrkr_abbrev = mrkr_abbrev;
+     NEW.mrkr_abbrev = scrub_char(NEW.mrkr_abbrev);
      
-     NEW.mrkr_abbrev_order = mrkr_abbrev_order;
+     NEW.mrkr_abbrev_order = zero_pad(NEW.mrkr_abbrev_order);
 
      perform p_check_mrkr_abbrev(NEW.mrkr_name,
 			        NEW.mrkr_abbrev,
 				NEW.mrkr_type );
 
-
-     
-
      raise notice 'mrkr_abbrev_order: %', mrkr_abbrev_order;
      raise notice 'mrkr_abbrev_order: %', NEW.mrkr_abbrev_order;
 
-     perform mhist_event(NEW.mrkr_zdb_id,OLD.mrkr_name,
-					NEW.mrkr_name, 
-					OLD.mrkr_abbrev, 
-					NEW.mrkr_abbrev);
-
-     perform p_update_related_names(NEW.mrkr_zdb_id,
-				   OLD.mrkr_abbrev,
-				   NEW.mrkr_abbrev );
 
      perform update_construct_name_component(NEW.mrkr_zdb_id, 
 					    NEW.mrkr_abbrev);
@@ -43,11 +33,42 @@ begin
 end;
 $BODY$ LANGUAGE plpgsql;
 
+
+create or replace function marker_abbrev_update()
+returns trigger as
+$BODY$
+
+begin
+
+     
+     NEW.mrkr_abbrev = scrub_char(NEW.mrkr_abbrev);
+     
+     NEW.mrkr_abbrev_order = zero_pad(NEW.mrkr_abbrev_order);
+
+     perform p_check_mrkr_abbrev(NEW.mrkr_name,
+			        NEW.mrkr_abbrev,
+				NEW.mrkr_type );
+
+  --   raise notice 'mrkr_abbrev_order: %', mrkr_abbrev_order;
+  --   raise notice 'mrkr_abbrev_order: %', NEW.mrkr_abbrev_order;
+
+    perform p_update_related_names(NEW.mrkr_zdb_id,
+				   OLD.mrkr_abbrev,
+				   NEW.mrkr_abbrev );
+
+    perform update_construct_name_component(NEW.mrkr_zdb_id, 
+					    NEW.mrkr_abbrev);
+     perform p_update_related_fish_names(NEW.mrkr_zdb_id);
+
+     RETURN NEW;
+end;
+$BODY$ LANGUAGE plpgsql;
+
 create trigger marker_abbrev_insert_trigger before insert on marker
  for each row 
- execute procedure marker_abbrev();
+ execute procedure marker_abbrev_insert();
 
 
 create trigger marker_abbrev_update_trigger before update of mrkr_abbrev on marker
  for each row 
- execute procedure marker_abbrev();
+ execute procedure marker_abbrev_update();
