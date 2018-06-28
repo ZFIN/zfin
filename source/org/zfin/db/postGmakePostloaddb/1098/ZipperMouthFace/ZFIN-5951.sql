20335872ce492b4c9bf5ae1b3daf6ec90c829427
Prita Mani <pm@zfin.org>
2018-06-28 08:52:04 -0700

    ZFIN-5951 added mrels to delete
    (cherry picked from commit f5bc36f83ade1a48abbed119d4549422205ff72d)

--- source/org/zfin/db/postGmakePostloaddb/1098/ZipperMouthFace/ZFIN-5951.sql
+++ source/org/zfin/db/postGmakePostloaddb/1098/ZipperMouthFace/ZFIN-5951.sql
@@ -3,7 +3,7 @@
 
 
 delete from record_attribution
-where recattrib_data_zdb_id in ('ZDB-CRISPR-180514-2','ZDB-MRPHLNO-180514-1')
+where recattrib_data_zdb_id in ('ZDB-CRISPR-180514-2','ZDB-MRPHLNO-180514-1','ZDB-GENE-030131-9854','ZDB-GENE-090313-243','ZDB-MREL-180514-21','ZDB-MREL-180514-22')
 and recattrib_source_zdb_id='ZDB-PUB-171216-9';
 
 
