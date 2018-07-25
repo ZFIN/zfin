a50cc18b33eeda9982b094be9e7a1316da6cb078
Prita Mani <pm@zfin.org>
2018-07-25 08:57:17 -0700

    ZFIN-5987 fixed sql

--- source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/ZFIN-5987.sql
+++ source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/ZFIN-5987.sql
@@ -11,7 +11,7 @@ delete from construct_component where cc_construct_Zdb_id='ZDB-TGCONSTRCT-110222
 
 update construct_component set cc_order=11 where cc_construct_zdb_id='ZDB-TGCONSTRCT-110222-3' and cc_order=10;
 insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component,cc_component_zdb_id,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-110222-3','coding sequence of','coding component','NLS','ZDB-EREGION-110816-1',1,9);
-insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-110222-3','coding sequence of','coding component','DsRedx','ZDB-EFG-081029-1',1,10);
+insert into construct_component (cc_construct_Zdb_id,cc_Component_type,cc_component_category,cc_component_zdb_id,cc_component,cc_cassette_number,cc_order) values ('ZDB-TGCONSTRCT-110222-3','coding sequence of','coding component','ZDB-EFG-081029-1','DsRedx',1,10);
 
 update construct_marker_relationship set conmrkrrel_relationship_type='coding sequence of' where conmrkrrel_construct_zdb_id='ZDB-TGCONSTRCT-110222-3' and conmrkrrel_mrkr_zdb_id='ZDB-EREGION-110816-1');
 update construct_marker_relationship set conmrkrrel_relationship_type='coding sequence of' where conmrkrrel_construct_zdb_id='ZDB-TGCONSTRCT-110222-3' and conmrkrrel_mrkr_zdb_id='ZDB-EREGION-110816-1');
