82c77a67a16e4aecddd9f616eb94f667ec72856d
Xiang Shao <xshao@zfin.org>
2018-08-06 16:21:11 -0700

    CUR-826

--- source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/CUR-826.sql
+++ source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/CUR-826.sql
@@ -1,4 +1,4 @@
 --liquibase formatted sql
 --changeset xshao:CUR-826
 
-delete from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-18089';
+select * from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-18089';
