f88545b0b0510a912c8f1f19889e0f56946791f1
Xiang Shao <xshao@zfin.org>
2018-08-07 13:58:08 -0700

    CUR-826 Merge ZDB-FISH-150901-18089 into ZDB-FISH-180803-1

--- source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/CUR-826.sql
+++ source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/CUR-826.sql
@@ -1,4 +1,15 @@
 --liquibase formatted sql
 --changeset xshao:CUR-826
 
-select * from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-18089';
+delete from record_attribution where recattrib_pk_id = '33891081';
+
+update record_attribution set recattrib_data_zdb_id = 'ZDB-FISH-180803-1' where recattrib_data_zdb_id = 'ZDB-FISH-150901-18089';
+
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-FISH-150901-18089';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-FISH-180803-1' where zrepld_new_zdb_id = 'ZDB-FISH-150901-18089';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-FISH-150901-18089';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-FISH-150901-18089', 'ZDB-FISH-180803-1');
+
