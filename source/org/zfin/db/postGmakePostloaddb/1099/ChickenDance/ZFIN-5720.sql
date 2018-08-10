923040324c47adbc17825a20bad990447ef1efa5
Xiang Shao <xshao@zfin.org>
2018-08-10 10:20:07 -0700

    ZFIN-5720 correct a typo

--- source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/ZFIN-5720.sql
+++ source/org/zfin/db/postGmakePostloaddb/1099/ChickenDance/ZFIN-5720.sql
@@ -3,5 +3,5 @@
 
 update genotype set geno_display_name = 'Tüpfel long fin' where geno_zdb_id = 'ZDB-GENO-990623-2';
 
-update data_alias set dalias_alias = 'Tüebingen long fin' where dalias_zdb_id = 'ZDB-DALIAS-070518-18';
+update data_alias set dalias_alias = 'Tübingen long fin' where dalias_zdb_id = 'ZDB-DALIAS-070518-18';
 
