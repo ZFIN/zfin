3007cb9bb6ca39e510eaa984b53a8f61ed09d676
Sierra Moxon <smoxon@zfin.org>
2018-08-21 08:11:28 -0700

    fix missing file for gradle builddatabase

--- source/org/zfin/db/load/1100/restore_ept.sql
+++ source/org/zfin/db/load/1100/restore_ept.sql
@@ -2,8 +2,8 @@
 --changeset sierra:restore_ept.sql
 
 
-delete from tmp_restore_ept
- where ept_pk_id in (select ept_pk_id from expression_phenotype_term));
+delete from tmp_expression_phenotype_term
+ where ept_pk_id in (select ept_pk_id from expression_phenotype_term);
 
 insert into expression_phenotype_term (ept_pk_id,
 	ept_relational_term,
@@ -16,4 +16,4 @@ select
         ept_quality_term_zdb_id,
         ept_tag,
         ept_xpatres_id
-from tmp_restore_ept;
+from tmp_expression_phenotype_term;
