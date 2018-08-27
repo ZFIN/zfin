68ff425a440fe99835e23ea8df629c0881e5928d
Christian Pich <cmpich@zfin.org>
2018-08-27 15:07:15 +0200

    ZFIN-6011 fix query

--- source/org/zfin/db/postGmakePostloaddb/1100/ZFIN-6011.sql
+++ source/org/zfin/db/postGmakePostloaddb/1100/ZFIN-6011.sql
@@ -6,4 +6,4 @@ set ftmd_transcript_consequence_term_zdb_id = 'ZDB-TERM-130401-1577'
 where ftmd_transcript_consequence_term_zdb_id = 'ZDB-TERM-130401-1804';
 
 delete from mutation_detail_controlled_vocabulary where
-term_ont_id = 'SO:0001816';
+mdcv_term_zdb_id = 'ZDB-TERM-130401-1804';
