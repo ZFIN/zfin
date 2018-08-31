f746baf46efa9c79e36c1290125fd427dba82976
Prita Mani <pm@zfin.org>
2018-08-31 14:20:39 -0700

    DLOAD-554 fixed syntax error

--- source/org/zfin/db/postGmakePostloaddb/1100/DLOAD-554.sql
+++ source/org/zfin/db/postGmakePostloaddb/1100/DLOAD-554.sql
@@ -26,7 +26,7 @@ insert into zdb_active_data select sfclid from sanger_location_post;
 
 delete from sequence_feature_chromosome_location where sfcl_feature_zdb_id like 'ZDB-ALT%' and sfcl_Assembly like '%11%';
 insert into sequence_feature_chromosome_location (sfcl_zdb_id, sfcl_feature_zdb_id,sfcl_start_position,sfcl_end_position,sfcl_assembly,sfcl_chromosome)
-select distinct sfclid, feature_zdb_id, location1,location1,assembly1,chromosome1 from sanger_location_post,feature where (chromosome1 not like 'Zv%' or chromosome1 not like 'KN%')  and allele1=feature_abbrev and assembly1 not like '%10%' and assembly1 not in (select sfcl_assembly from sequence_feature_chromosome_location, feature where sfcl_feature_zdb_id=feature_zdb_id)
+select distinct sfclid, feature_zdb_id, location1,location1,assembly1,chromosome1 from sanger_location_post,feature where (chromosome1 not like 'Zv%' or chromosome1 not like 'KN%')  and allele1=feature_abbrev and assembly1 not like '%10%' and assembly1 not in (select sfcl_assembly from sequence_feature_chromosome_location, feature where sfcl_feature_zdb_id=feature_zdb_id);
 
 
 
