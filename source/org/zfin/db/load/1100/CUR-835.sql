d656a80723a882267c3032e38a18ce21f00466df
Prita Mani <pm@zfin.org>
2018-09-05 11:43:08 -0700

    CUR-835

--- source/org/zfin/db/load/1100/CUR-835.sql
+++ source/org/zfin/db/load/1100/CUR-835.sql
@@ -3,7 +3,7 @@
 
 ALTER TABLE feature_assay DROP CONSTRAINT featassay_feature_zdb_id_fk;
 
-ALTER TABLE feature_assay ADD CONSTRAINT (
-  FOREIGN KEY (feature_zdb_id) REFERENCES feature
-  ON DELETE CASCADE CONSTRAINT featassay_feature_zdb_id_fk
-);
+ALTER TABLE feature_assay
+ADD CONSTRAINT "featassay_feature_zdb_id_fk"
+FOREIGN KEY (featassay_feature_zdb_id) REFERENCES feature ON DELETE CASCADE;
+
