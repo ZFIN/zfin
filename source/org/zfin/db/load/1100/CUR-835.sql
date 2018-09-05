bad5678cad53479840559837973bd341b75fe301
Prita Mani <pm@zfin.org>
2018-09-05 10:01:21 -0700

    CUR-835 altered constraint on feature_assay table

---
+++ source/org/zfin/db/load/1100/CUR-835.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset pm:CUR-835
+
+ALTER TABLE feature_assay DROP CONSTRAINT featassay_feature_zdb_id_fk;
+
+ALTER TABLE feature_assay ADD CONSTRAINT (
+  FOREIGN KEY (feature_zdb_id) REFERENCES feature
+  ON DELETE CASCADE CONSTRAINT featassay_feature_zdb_id_fk
+);
