b32fce3fccfa7dfbf9a1873814ca9b122283e129
Prita Mani <pm@zfin.org>
2018-09-06 13:57:41 -0700

    DLOAD-559 fixed typo

--- source/org/zfin/db/postGmakePostloaddb/1100/DLOAD-559a.sql
+++ source/org/zfin/db/postGmakePostloaddb/1100/DLOAD-559a.sql
@@ -11,7 +11,7 @@
 create temp table ftrMutDetsnew (ftr varchar(50), ref1 varchar(50), ref2 varchar(10),mutDisplay varchar(10), fdmd_zdb_id varchar(50)) ;
 
 insert into ftrMutDetsnew (ftr,ref1,ref2,mutDisplay, fdmd_zdb_id)
-select ftr,ref1,ref2,trim(ref1)||">"||trim(ref2), get_id('FDMD')
+select ftr,ref1,ref2,trim(ref1)||'>'||trim(ref2), get_id('FDMD')
        from ftrmutdets
        where ref1!=''
        and ref2!='';
