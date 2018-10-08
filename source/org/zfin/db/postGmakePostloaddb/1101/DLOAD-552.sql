24e7c0211e2c97e65eaa3dd7ba42113738979028
Prita Mani <pm@zfin.org>
2018-10-08 11:29:53 -0700

    DLOAD-552 fixed quotes

--- source/org/zfin/db/postGmakePostloaddb/1101/DLOAD-552.sql
+++ source/org/zfin/db/postGmakePostloaddb/1101/DLOAD-552.sql
@@ -9,7 +9,7 @@ create table  sanger_flanking_sequence_post_proc (
            sequence text not null);
 
 
-insert into sanger_flanking_sequence_post_proc (alleleid, variation, sequence) select distinct  feature_zdb_id,ref||"/"||alt,flankingseq from sanger_flanking_sequence, feature where feature_abbrev=allele;
+insert into sanger_flanking_sequence_post_proc (alleleid, variation, sequence) select distinct  feature_zdb_id,ref||'/'||alt,flankingseq from sanger_flanking_sequence, feature where feature_abbrev=allele;
 
 insert into variant_sequence (vseq_data_zdb_id,vseq_type,veq_offset_start,vseq_offset_stop,vseq_sequence,vseq_variation)
 select distinct alleleid, 'Genomic',50,50,sequence,variation from sanger_flanking_sequence_post_proc;
