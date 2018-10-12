8ea9dd0f8c127155f22df9447b2c5cc6cda2e72a
Sierra Moxon <smoxon@zfin.org>
2018-10-12 07:22:16 -0700

    refactor variant sequence

--- source/org/zfin/db/load/1101/feature_sequence.sql
+++ source/org/zfin/db/load/1101/feature_sequence.sql
@@ -1,37 +1,37 @@
 --liquibase formatted sql
 --changeset sierra:feature_sequence.sql
 
-create table variant_sequence (vseq_data_zdb_id text,
-                               vseq_type text, 
-                               vseq_offset_start integer,
-                               vseq_offset_stop integer,
-                               vseq_sequence text,
-                               vseq_five_prime_flanking_sequence text,
-                               vseq_three_prime_flanking_sequence text,
-                               vseq_flanking_sequence_type text,
-                               vseq_flanking_sequence_origin text,
-                               vseq_variation text)
+create table variant_flanking_sequence (vfseq_data_zdb_id text,
+                               vfseq_type text, 
+                               vfseq_offset_start integer,
+                               vfseq_offset_stop integer,
+                               vfseq_sequence text,
+                               vfseq_five_prime_flanking_sequence text,
+                               vfseq_three_prime_flanking_sequence text,
+                               vfseq_flanking_sequence_type text,
+                               vfseq_flanking_sequence_origin text,
+                               vfseq_variation text)
 ;
 
-alter table variant_sequence
- add primary key (vseq_data_zdb_id, vseq_type);
+alter table variant_flanking_sequence
+ add primary key (vfseq_data_zdb_id, vseq_type);
 
-create index vseq_variantion_index
- on variant_sequence (vseq_variation);
+create index vfseq_variantion_index
+ on variant_flanking_sequence (vfseq_variation);
 
-alter table variant_sequence
+alter table variant_flanking_sequence
  add constraint variant_sequence_zdb_active_data_foreign_key
- foreign key (vseq_data_zdb_id) references zdb_active_data(zactvd_zdb_id)
+ foreign key (vfseq_data_zdb_id) references zdb_active_data(zactvd_zdb_id)
  on delete cascade;
 
-alter table variant_sequence 
- add constraint check vseq_flanking_sequence_type_check vseq_flanking_sequence_type in ('genomic','cDNA');
+alter table variant_flanking_sequence 
+ add constraint check vfseq_flanking_sequence_type_check vfseq_flanking_sequence_type in ('genomic','cDNA');
 
-alter table variant_sequence
- add constraint check vseq_flanking_sequence_origin_check vseq_flanking_sequence_origin in ('inferred from genomic','directly sequenced', 'unknown');
+alter table variant_flanking_sequence
+ add constraint check vfseq_flanking_sequence_origin_check vfseq_flanking_sequence_origin in ('inferred from genomic','directly sequenced', 'unknown');
 
-create unique index vseq_alternate_key_index 
- on variant_sequence (vseq_data_zdb_id, vseq_flanking_sequence_type);
+create unique index vfseq_alternate_key_index 
+ on variant_flanking_sequence (vfseq_data_zdb_id, vfseq_flanking_sequence_type);
 
-alter table variant_sequence
- add constraint vseq_alternate_key unique using index vseq_alternate_key_index;
+alter table variant_flanking_sequence
+ add constraint vfseq_alternate_key unique using index vfseq_alternate_key_index;
