167c23df96654b7cfa32d81950666f6d61dd4d98
Prita Mani <pm@zfin.org>
2018-10-12 09:09:31 -0700

    DLOAD-552

--- source/org/zfin/db/postGmakePostloaddb/1101/DLOAD-552.sql
+++ source/org/zfin/db/postGmakePostloaddb/1101/DLOAD-552.sql
@@ -11,5 +11,5 @@ create table  sanger_flanking_sequence_post_proc (
 
 insert into sanger_flanking_sequence_post_proc (alleleid, variation, sequence) select distinct  feature_zdb_id,ref||'/'||alt,flankingseq from sanger_flanking_sequence, feature where feature_abbrev=allele;
 
-insert into variant_sequence (vseq_data_zdb_id,vseq_type,vseq_offset_start,vseq_offset_stop,vseq_sequence,vseq_variation)
-select distinct alleleid, 'Genomic',50,50,sequence,variation from sanger_flanking_sequence_post_proc;
+insert into variant_flanking_sequence (vfseq_data_zdb_id, vfseq_type, vfseq_offset_start, vfseq_offset_stop, vfseq_sequence, vfseq_five_prime_flanking_sequence, vfseq_three_prime_flanking_sequence, vfseq_flanking_sequence_type, vfseq_flanking_sequence_origin, vfseq_variation )
+select distinct alleleid, 'Genomic',50,50,sequence,SUBSTRING(sequence,1,50),SUBSTRING(sequence,56),'genomic','directly sequenced',variation from sanger_flanking_sequence_post_proc;
