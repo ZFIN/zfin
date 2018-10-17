fded13916c69e3367461a045009360a826f74c0f
Prita Mani <pm@zfin.org>
2018-10-17 12:53:22 -0700

    DLOAD-571 class for varinat_flanking_sequence

---
+++ source/org/zfin/mapping/VariantSequence.java
@@ -0,0 +1,109 @@
+package org.zfin.mapping;
+
+import org.zfin.infrastructure.PublicationAttribution;
+
+import java.io.Serializable;
+
+public class VariantSequence  {
+    private String zdbID;
+    private String vseqDataZDB;
+    private String vfsTargetSequence;
+    private Long vfsOffsetStart;
+    private Long vfsOffsetStop;
+    private String vfsVariation;
+    private String vfsLeftEnd;
+    private String vfsRightEnd;
+    private String vfsType;
+    private String vfsFlankType;
+    private String vfsFlankOrigin;
+
+
+
+    public String getZdbID() {
+        return zdbID;
+    }
+
+    public void setZdbID(String zdbID) {
+        this.zdbID = zdbID;
+    }
+
+    public String getVseqDataZDB() {
+        return vseqDataZDB;
+    }
+
+    public void setVseqDataZDB(String vseqDataZDB) {
+        this.vseqDataZDB = vseqDataZDB;
+    }
+
+    public String getVfsTargetSequence() {
+        return vfsTargetSequence;
+    }
+
+    public void setVfsTargetSequence(String vfsTargetSequence) {
+        this.vfsTargetSequence = vfsTargetSequence;
+    }
+
+    public Long getVfsOffsetStart() {
+        return vfsOffsetStart;
+    }
+
+    public void setVfsOffsetStart(Long vfsOffsetStart) {
+        this.vfsOffsetStart = vfsOffsetStart;
+    }
+
+    public Long getVfsOffsetStop() {
+        return vfsOffsetStop;
+    }
+
+    public void setVfsOffsetStop(Long vfsOffsetStop) {
+        this.vfsOffsetStop = vfsOffsetStop;
+    }
+
+    public String getVfsVariation() {
+        return vfsVariation;
+    }
+
+    public void setVfsVariation(String vfsVariation) {
+        this.vfsVariation = vfsVariation;
+    }
+
+    public String getVfsLeftEnd() {
+        return vfsLeftEnd;
+    }
+
+    public void setVfsLeftEnd(String vfsLeftEnd) {
+        this.vfsLeftEnd = vfsLeftEnd;
+    }
+
+    public String getVfsRightEnd() {
+        return vfsRightEnd;
+    }
+
+    public void setVfsRightEnd(String vfsRightEnd) {
+        this.vfsRightEnd = vfsRightEnd;
+    }
+
+    public String getVfsType() {
+        return vfsType;
+    }
+
+    public void setVfsType(String vfsType) {
+        this.vfsType = vfsType;
+    }
+
+    public String getVfsFlankType() {
+        return vfsFlankType;
+    }
+
+    public void setVfsFlankType(String vfsFlankType) {
+        this.vfsFlankType = vfsFlankType;
+    }
+
+    public String getVfsFlankOrigin() {
+        return vfsFlankOrigin;
+    }
+
+    public void setVfsFlankOrigin(String vfsFlankOrigin) {
+        this.vfsFlankOrigin = vfsFlankOrigin;
+    }
+}
