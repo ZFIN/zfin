ebe016f4bb1ef27abde7e36a84ddd3175644ed93
Prita Mani <pm@zfin.org>
2018-11-30 12:00:26 -0800

    PUB-486

---
+++ source/org/zfin/db/postGmakePostloaddb/1103/PUB-486.sql
@@ -0,0 +1,5 @@
+--liquibase formatted sql
+--changeset pm:PUB-486
+
+update journal set jrnl_is_nice='t' where jrnl_zdb_id='ZDB-JRNL-171003-1';
+update journal set jrnl_is_nice='t' where jrnl_zdb_id='ZDB-JRNL-110706-2';
