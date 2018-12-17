e67f49ebfbea0057ac59351e0e04cede0ee48f32
Prita Mani <pm@zfin.org>
2018-12-17 09:16:26 -0800

    PUB-486 added one more journal to list

--- source/org/zfin/db/postGmakePostloaddb/1103/PUB-486.sql
+++ source/org/zfin/db/postGmakePostloaddb/1103/PUB-486.sql
@@ -3,3 +3,4 @@
 
 update journal set jrnl_is_nice='t' where jrnl_zdb_id='ZDB-JRNL-171003-1';
 update journal set jrnl_is_nice='t' where jrnl_zdb_id='ZDB-JRNL-110706-2';
+update journal set jrnl_is_nice='t' where jrnl_zdb_id='ZDB-JRNL-121105-2';
