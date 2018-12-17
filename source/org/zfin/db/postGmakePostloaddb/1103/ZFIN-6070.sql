cc26cfea792c1b348c3cae443fbab2e925728d46
Prita Mani <pm@zfin.org>
2018-12-17 15:11:18 -0800

    ZFIN-6070 chnaged url for hgnc

--- source/org/zfin/db/postGmakePostloaddb/1103/ZFIN-6070.sql
+++ source/org/zfin/db/postGmakePostloaddb/1103/ZFIN-6070.sql
@@ -2,4 +2,4 @@
 --changeset pm:ZFIN-6070
 
 
-update foreign_db set fdb_db_query='https://www.genenames.org/data/gene-symbol-report/#!/hgnc_id/' where fdb_db_name='HGNC';
+update foreign_db set fdb_db_query='https://www.genenames.org/data/gene-symbol-report/#!/symbol/' where fdb_db_name='HGNC';
