5ebbd767f23b99fd39bdf63f9ec0b548b12087bd
Sierra Moxon <smoxon@zfin.org>
2019-01-07 08:24:49 -0800

    ZFIN-6075

--- source/org/zfin/db/load/1100/restore_ept.sql
+++ source/org/zfin/db/load/1100/restore_ept.sql
@@ -1,19 +1,19 @@
 --liquibase formatted sql
 --changeset sierra:restore_ept.sql
 
+create temp table tmp_expression_phenotype_term (ept_pk_id bigint,
+       ept_relational_term text,
+       ept_qualifier_term_zdb_id text,
+       ept_tag text,
+       ept_xpatres_id bigint)
+;
+
+\copy tmp_expression_phenotype_term from 'ept_merge.csv' with delimiter ','; 
 
 delete from tmp_expression_phenotype_term
  where ept_pk_id in (select ept_pk_id from expression_phenotype_term);
 
-insert into expression_phenotype_term (ept_pk_id,
-	ept_relational_term,
-	ept_quality_term_zdb_id,
-	ept_tag,
-	ept_xpatres_id)
-select 
-	ept_pk_id,
-        ept_relational_term,
-        ept_quality_term_zdb_id,
-        ept_tag,
-        ept_xpatres_id
-from tmp_expression_phenotype_term;
+select * from tmp_expression_phenotype_term;
+
+insert into expression_phenotype_term
+ select * from tmp_expression_phenotype_term;
