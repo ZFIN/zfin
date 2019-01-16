7d082aea3b7d92ae355d552fad7db9d0e8b9cf16
Prita Mani <pm@zfin.org>
2019-01-16 09:00:19 -0800

    CUR-861 added aliases for constructs
    (cherry picked from commit fc797624257465fcc86d6ab174404aa23009b271)

--- source/org/zfin/db/postGmakePostloaddb/1103/CUR-861.sql
+++ source/org/zfin/db/postGmakePostloaddb/1103/CUR-861.sql
@@ -23,7 +23,13 @@ insert into marker_relationship(mrel_zdb_id,mrel_mrkr_1_zdb_id,mrel_mrkr_2_zdb_i
 drop table tmp_mrel;
 
 
+create temp table tmp_dalias (aliasid varchar(50), consid varchar(50),consname text);
+insert into tmp_dalias (aliasid, consid ,consname) select construct1, construct1, construct_name  from constructrel, construct where construct1=construct_zdb_id;
 
+update tmp_dalias set aliasid=get_id('DALIAS');
+insert into zdb_active_data (zactvd_zdb_id) select aliasid from tmp_dalias;
+insert into data_alias(dalias_zdb_id,dalias_data_zdb_id,dalias_alias,dalias_group_id) select aliasid,consid,consname,1 from tmp_dalias;
+drop table tmp_dalias;
 
 
 
