fada49ce08a7d18df96858aa7108996576a5fcc7
Prita Mani <pm@zfin.org>
2019-01-18 08:45:56 -0800

    CUR861 added aliases to all constrct name changes

--- source/org/zfin/db/postGmakePostloaddb/1103/CUR-863.sql
+++ source/org/zfin/db/postGmakePostloaddb/1103/CUR-863.sql
@@ -47,6 +47,17 @@ insert into marker_relationship(mrel_zdb_id,mrel_mrkr_1_zdb_id,mrel_mrkr_2_zdb_i
 
 drop table tmp_mrel;
 
+create temp table tmp_dalias (aliasid varchar(50), consid varchar(50),consname text);
+insert into tmp_dalias (aliasid, consid ,consname) select 'ZDB-TGCONSTRCT-150424-4', 'ZDB-TGCONSTRCT-150424-4', construct_name  from  construct where construct_zdb_id='ZDB-TGCONSTRCT-150424-4';
+insert into tmp_dalias (aliasid, consid ,consname) select 'ZDB-TGCONSTRCT-161017-5', 'ZDB-TGCONSTRCT-161017-5', construct_name  from  construct where construct_zdb_id='ZDB-TGCONSTRCT-161017-5';
+
+update tmp_dalias set aliasid=get_id('DALIAS');
+insert into zdb_active_data (zactvd_zdb_id) select aliasid from tmp_dalias;
+insert into data_alias(dalias_zdb_id,dalias_data_zdb_id,dalias_alias,dalias_group_id) select aliasid,consid,consname,1 from tmp_dalias;
+
+insert into record_attribution(recattrib_data_Zdb_id,recattrib_source_zdb_id) select aliasid,'ZDB-PUB-180105-5' from tmp_dalias;
+drop table tmp_dalias;
+
 
 
 
--- source/org/zfin/db/postGmakePostloaddb/1103/CUR-864.sql
+++ source/org/zfin/db/postGmakePostloaddb/1103/CUR-864.sql
@@ -11,6 +11,17 @@ update construct_component set cc_component='UBCI' where cc_component_zdb_id='ZD
 update construct_marker_relationship set conmrkrrel_mrkr_zdb_id='ZDB-EREGION-181218-2' where  conmrkrrel_construct_zdb_id = 'ZDB-TGCONSTRCT-160824-4' and conmrkrrel_mrkr_zdb_id='ZDB-GENE-061110-88';
 update marker_relationship set mrel_mrkr_2_zdb_id='ZDB-EREGION-181218-2' where  mrel_mrkr_1_zdb_id = 'ZDB-TGCONSTRCT-160824-4' and mrel_mrkr_2_zdb_id='ZDB-GENE-061110-88';
 
+create temp table tmp_dalias (aliasid varchar(50), consid varchar(50),consname text);
+insert into tmp_dalias (aliasid, consid ,consname) select 'ZDB-TGCONSTRCT-101103-3', 'ZDB-TGCONSTRCT-101103-3', construct_name  from  construct where construct_zdb_id='ZDB-TGCONSTRCT-101103-3';
+insert into tmp_dalias (aliasid, consid ,consname) select 'ZDB-TGCONSTRCT-160824-4', 'ZDB-TGCONSTRCT-160824-4', construct_name  from  construct where construct_zdb_id='ZDB-TGCONSTRCT-160824-4';
+
+update tmp_dalias set aliasid=get_id('DALIAS');
+insert into zdb_active_data (zactvd_zdb_id) select aliasid from tmp_dalias;
+insert into data_alias(dalias_zdb_id,dalias_data_zdb_id,dalias_alias,dalias_group_id) select aliasid,consid,consname,1 from tmp_dalias;
+
+insert into record_attribution(recattrib_data_Zdb_id,recattrib_source_zdb_id) select aliasid,'ZDB-PUB-151205-4' from tmp_dalias;
+drop table tmp_dalias;
+
 
 
 
