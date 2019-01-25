443829c4547914e6a541c8b5e75c1981b8a29ae3
Xiang Shao <xshao@zfin.org>
2019-01-25 12:05:22 -0800

    CUR-874

--- source/org/zfin/db/postGmakePostloaddb/1104/CUR-874.sql
+++ source/org/zfin/db/postGmakePostloaddb/1104/CUR-874.sql
@@ -6,3 +6,12 @@ update marker
    set mrkr_name = 'Tg(myog:Hsa.HIST1H2BJ-mRFP)'
  where mrkr_zdb_id = 'ZDB-TGCONSTRCT-160803-2';
 
+update construct
+   set construct_name = 'Tg(myog:Hsa.HIST1H2BJ-mRFP)'
+ where construct_zdb_id = 'ZDB-TGCONSTRCT-160803-2';
+
+update construct_component
+   set cc_component = '-', cc_component_zdb_id = null
+ where cc_construct_zdb_id = 'ZDB-TGCONSTRCT-160803-2'
+   and cc_pk_id = 36521;
+
