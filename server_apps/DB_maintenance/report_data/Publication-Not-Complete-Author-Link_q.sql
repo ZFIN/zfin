b9b63b88d73773e9751f48b5a97cc9ab992c675b
Xiang Shao <xshao@zfin.org>
2019-01-29 09:51:37 -0800

    JENK-416 fixed the problem of no more num_auths by creating and using a function to count the authors

--- server_apps/DB_maintenance/report_data/Publication-Not-Complete-Author-Link_q.sql
+++ server_apps/DB_maintenance/report_data/Publication-Not-Complete-Author-Link_q.sql
@@ -1,3 +1,25 @@
+CREATE OR REPLACE FUNCTION count_substring_plus_one(text, text) RETURNS INTEGER AS '
+	DECLARE
+		sub   ALIAS FOR $2;
+		str   VARCHAR;
+		pos   INTEGER;
+		total INTEGER;
+	BEGIN
+		str   := $1;
+		total := 1;
+		LOOP
+			pos := strpos(str, sub);
+			IF pos = 0 THEN
+				RETURN total;
+			ELSE
+				total := total + 1;
+				str = substr(str, pos + 1);
+			END IF;
+		END LOOP;
+		RETURN total;
+	END;
+' LANGUAGE 'plpgsql';
+
 SELECT publication.zdb_id, 
        person.NAME, 
        person.zdb_id, 
@@ -6,9 +28,9 @@ SELECT publication.zdb_id,
        authors 
 FROM   publication, 
        person 
-WHERE  num_auths > (SELECT Count(*) 
-                    FROM   int_person_pub 
-                    WHERE  target_id = publication.zdb_id) 
+WHERE  count_substring_plus_one(authors, ',') > (SELECT count(*) 
+                                                   FROM   int_person_pub 
+                                                  WHERE  target_id = publication.zdb_id) 
        AND authors LIKE '%' 
                         || Replace(NAME, '-', ',') 
                         || '%' 
@@ -20,3 +42,5 @@ WHERE  num_auths > (SELECT Count(*)
                        WHERE  b.source_id = person.zdb_id 
                               AND b.target_id = publication.zdb_id) 
 ORDER  BY publication.zdb_id; 
+
+DROP FUNCTION count_substring_plus_one(text, text);
