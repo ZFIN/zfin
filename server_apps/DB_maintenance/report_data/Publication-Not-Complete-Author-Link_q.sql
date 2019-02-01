648df16bf2cf50359c9a9851f5a48ce7c869f47e
Xiang Shao <xshao@zfin.org>
2019-02-01 10:40:13 -0800

    JENK-416

--- server_apps/DB_maintenance/report_data/Publication-Not-Complete-Author-Link_q.sql
+++ server_apps/DB_maintenance/report_data/Publication-Not-Complete-Author-Link_q.sql
@@ -1,25 +1,3 @@
-CREATE OR REPLACE FUNCTION count_substring_plus_one(text, text) RETURNS INTEGER AS '
-	DECLARE
-		sub   ALIAS FOR $2;
-		str   VARCHAR;
-		pos   INTEGER;
-		total INTEGER;
-	BEGIN
-		str   := $1;
-		total := 1;
-		LOOP
-			pos := strpos(str, sub);
-			IF pos = 0 THEN
-				RETURN total;
-			ELSE
-				total := total + 1;
-				str = substr(str, pos + 1);
-			END IF;
-		END LOOP;
-		RETURN total;
-	END;
-' LANGUAGE 'plpgsql';
-
 SELECT publication.zdb_id, 
        person.NAME, 
        person.zdb_id, 
@@ -28,12 +6,7 @@ SELECT publication.zdb_id,
        authors 
 FROM   publication, 
        person 
-WHERE  count_substring_plus_one(authors, ',') > (SELECT count(*) 
-                                                   FROM   int_person_pub 
-                                                  WHERE  target_id = publication.zdb_id) 
-       AND authors LIKE '%' 
-                        || Replace(NAME, '-', ',') 
-                        || '%' 
+WHERE  authors LIKE '%' || Replace(NAME, '-', ',') || '%' 
        AND EXISTS (SELECT 'x'
                        FROM   int_person_pub 
                        WHERE  target_id = publication.zdb_id) 
@@ -43,4 +16,3 @@ WHERE  count_substring_plus_one(authors, ',') > (SELECT count(*)
                               AND b.target_id = publication.zdb_id) 
 ORDER  BY publication.zdb_id; 
 
-DROP FUNCTION count_substring_plus_one(text, text);
