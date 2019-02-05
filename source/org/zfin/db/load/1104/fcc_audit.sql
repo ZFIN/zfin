f02c8f9b865adb324752ffcdbedffbdffcc2269c
Sierra Moxon <smoxon@zfin.org>
2019-02-05 13:30:08 -0800

    ZFIN-6114

--- source/org/zfin/db/load/1104/fcc_audit.sql
+++ source/org/zfin/db/load/1104/fcc_audit.sql
@@ -7,6 +7,6 @@ create table feature_community_contribution_audit(fcc_feature_zdb_id text,
 	fcc_maternal_zygosity_examined boolean,
 	fcc_currently_available boolean,
 	fcc_other_line_information text,
-	fcc_date_added timestamp
+	fcc_date_added timestamp,
         fcc_added_by text);
 
