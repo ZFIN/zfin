1433f19b319ac05283b8879c64adfbc0936b7410
Sierra Moxon <smoxon@zfin.org>
2019-02-11 12:12:06 -0800

    migrate transcript name mapping loader to use production table

--- server_apps/data_transfer/TranscriptNameMapping/load_ensdart_ottdart_mapping.sql
+++ server_apps/data_transfer/TranscriptNameMapping/load_ensdart_ottdart_mapping.sql
@@ -44,18 +44,18 @@ insert into ensdart_name_mapping(enm_ensdart_stable_id,
 drop index if exists enm_ensdart_id_index;
 
 create index enm_ensdart_id_index
-  on ensdart_name_mapping(zfin_gene_zdb_id);
+  on ensdart_name_mapping(enm_tscript_zdb_id);
 
-\copy (select distinct tscript_mrkr_zdb_id, tscript_load_id, mrkr_abbrev from transcript, marker where tscript_mrkr_zdb_id = mrkr_zdb_id and not exists (Select 'x' from ensdart_name_mapping where ottdart_id = tscript_load_id) and tscript_status_id != '1' ) to 'missing_ottdart_ensdart_mapping.txt';
+--\copy (select distinct tscript_mrkr_zdb_id, tscript_load_id, mrkr_abbrev from transcript, marker where tscript_mrkr_zdb_id = mrkr_zdb_id and not exists (Select 'x' from ensdart_name_mapping where ottdart_id = tscript_load_id) and tscript_status_id != '1' ) to 'missing_ottdart_ensdart_mapping.txt';
 
-\copy (select distinct tscript_mrkr_zdb_id, tscript_load_id, mrkr_abbrev, ensdart_stable_id from transcript, marker, ensdart_name_mapping where tscript_mrkr_zdb_id = mrkr_zdb_id and tscript_load_id = ottdart_id) to 'mapped_ottdart_ensdart.txt' ;
+---\copy (select distinct tscript_mrkr_zdb_id, tscript_load_id, mrkr_abbrev, ensdart_stable_id from transcript, marker, ensdart_name_mapping where tscript_mrkr_zdb_id = mrkr_zdb_id and tscript_load_id = ottdart_id) to 'mapped_ottdart_ensdart.txt' ;
 
 create temp table tmp_dups (tscript_name text, counter int);
 
 insert into tmp_dups
-  select ensembl_tscript_name, count(*)
+  select enm_ensembl_tscript_name, count(*)
     from ensdart_name_mapping
-    group by ensembl_tscript_name having count(*)> 1;
+    group by enm_ensembl_tscript_name having count(*)> 1;
 
 --delete from ensdart_name_mapping
  -- where ensembl_tscript_name in (select tscript_name from tmp_dups);
@@ -69,16 +69,19 @@ insert into tmp_dups
 
 create temp table tmp_ottdart_dups (counter int, ottdart_id text);
 insert into tmp_ottdart_dups (counter, ottdart_id)
-select count(*), ottdart_id from ensdart_name_mapping
- group by ensdart_stable_id, ottdart_id
+select count(*), enm_ottdart_id from ensdart_name_mapping
+ group by enm_ensdart_stable_id, enm_ottdart_id
 having count(*) > 1;
 
 create index ottdart_index on tmp_ottdart_dups (ottdart_id);
 
 update transcript
- set tscript_ensdart_id = (select ensdart_stable_id from ensdart_name_mapping
- where tscript_load_id = ottdart_id)
+ set tscript_ensdart_id = (select enm_ensdart_stable_id from ensdart_name_mapping
+ where tscript_load_id = enm_ottdart_id)
  where not exists (Select 'x' from tmp_ottdart_dups where ottdart_id = tscript_load_id);
                   
+select count(*) From transcript where tscript_ensdart_id is not null;
+select count(*) From transcript where tscript_ensdart_id is null;
+
 commit work;
 --rollback work;
