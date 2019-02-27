90b7e40b850c80e008893e88032284c5e54b03c3
Christian Pich <cmpich@zfin.org>
2019-02-27 11:52:12 -0800

    INF-3254 upgrade to Jenkins 2.150.3

--- server_apps/jenkins/jenkins.sh
+++ server_apps/jenkins/jenkins.sh
@@ -4,16 +4,19 @@ Name=Jenkins
 DESC="Jenkins CI Server"
 
 PID_FILE=$JENKINS_HOME/jenkins.pid
-COMMAND="java -jar $SOURCEROOT/server_apps/jenkins/jenkins.war --httpPort=$JENKINS_PORT --prefix=/jobs"
+COMMAND="java -jar $SOURCEROOT/server_apps/jenkins/jenkins.war  --httpPort=$JENKINS_PORT --prefix=/jobs"
+
+export JAVA_HOME="/usr/lib/jvm/java-1.8.0"
 
 echo SOURCEROOT    $SOURCEROOT
 echo JENKINS_HOME  $JENKINS_HOME
 echo JENKINS_PORT  $JENKINS_PORT
 echo PID_FILE      $PID_FILE
+echo JAVA_HOME $JAVA_HOME
 
 start_jenkins() {
 echo 'Starting Jenkins...'
-    nohup java -Dinstance=$INSTANCE -jar $SOURCEROOT/server_apps/jenkins/jenkins.war --httpPort=$JENKINS_PORT --sessionTimeout=10080 --prefix=/jobs  > $JENKINS_HOME/logs/jenkins.log 2>&1 & echo $! > $JENKINS_HOME/jenkins.pid
+    nohup $JAVA_HOME/bin/java -Dinstance=$INSTANCE -jar $SOURCEROOT/server_apps/jenkins/jenkins.war  --httpPort=$JENKINS_PORT --sessionTimeout=10080 --prefix=/jobs  > $JENKINS_HOME/logs/jenkins.log 2>&1 & echo $! > $JENKINS_HOME/jenkins.pid
     echo pid: $(cat $PID_FILE)
 #	start-stop-daemon --start --quiet --background --make-pidfile --pidfile $PIDFILE --chuid $RUN_AS --exec $COMMAND
 }
