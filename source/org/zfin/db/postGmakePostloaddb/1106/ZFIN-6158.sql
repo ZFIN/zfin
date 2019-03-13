be480fe268a1efe6b0392413466ce739b7ac055a
Xiang Shao <xshao@zfin.org>
2019-03-13 10:15:53 -0700

    ZFIN-6158

---
+++ source/org/zfin/db/postGmakePostloaddb/1106/ZFIN-6158.sql
@@ -0,0 +1,47 @@
+--liquibase formatted sql
+--changeset xshao:ZFIN-6158
+
+update genotype set geno_display_name = 'sqKR15bEt' where geno_zdb_id = 'ZDB-GENO-110210-8';
+
+update fish set fish_name = 'sqKR15bEt', fish_full_name = 'sqKR15bEt' where fish_zdb_id = 'ZDB-FISH-150901-27037';
+
+
+update genotype set geno_display_name = 'sqKR15Et' where geno_zdb_id = 'ZDB-GENO-110210-10';
+
+update fish set fish_name = 'sqKR15bEt', fish_full_name = 'sqKR15bEt' where fish_zdb_id = 'ZDB-FISH-150901-13589';
+
+
+update genotype set geno_display_name = 's870Tg' where geno_zdb_id = 'ZDB-GENO-061228-1';
+
+update fish set fish_name = 's870Tg', fish_full_name = 's870Tg' where fish_zdb_id = 'ZDB-FISH-150901-29329';
+
+
+update genotype set geno_display_name = 's870Tg/+' where geno_zdb_id = 'ZDB-GENO-090820-1';
+
+update fish set fish_name = 's870Tg/+', fish_full_name = 's870Tg/+(AB)' where fish_zdb_id = 'ZDB-FISH-150901-17116';
+
+
+update genotype set geno_display_name = 'sqKR2Et' where geno_zdb_id = 'ZDB-GENO-110210-5';
+
+update fish set fish_name = 'sqKR2Et', fish_full_name = 'sqKR2Et' where fish_zdb_id = 'ZDB-FISH-150901-12533';
+
+
+update genotype set geno_display_name = 'sqKR1Et' where geno_zdb_id = 'ZDB-GENO-110210-4';
+
+update fish set fish_name = 'sqKR1Et', fish_full_name = 'sqKR1Et' where fish_zdb_id = 'ZDB-FISH-150901-8179';
+
+
+update genotype set geno_display_name = 'sqKR19Et' where geno_zdb_id = 'ZDB-GENO-110210-9';
+
+update fish set fish_name = 'sqKR19Et', fish_full_name = 'sqKR19Et' where fish_zdb_id = 'ZDB-FISH-150901-23506';
+
+
+update genotype set geno_display_name = 'sqKR4Et' where geno_zdb_id = 'ZDB-GENO-110210-6';
+
+update fish set fish_name = 'sqKR4Et', fish_full_name = 'sqKR4Et' where fish_zdb_id = 'ZDB-FISH-150901-15323';
+
+
+update genotype set geno_display_name = 'sqKR11Et' where geno_zdb_id = 'ZDB-GENO-110210-7';
+
+update fish set fish_name = 'sqKR11Et', fish_full_name = 'sqKR11Et' where fish_zdb_id = 'ZDB-FISH-150901-5409';
+
