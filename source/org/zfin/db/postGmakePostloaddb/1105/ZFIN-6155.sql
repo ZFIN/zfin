db8cb7957ca195184b1ef103fc160dd746adec33
Prita Mani <pm@zfin.org>
2019-03-13 14:39:24 -0700

    ZFIN-6155
    (cherry picked from commit be21e5d1c3f7d964eed2baf0a11fadb67be0afb0)

--- source/org/zfin/db/postGmakePostloaddb/1105/ZFIN-6155.sql
+++ source/org/zfin/db/postGmakePostloaddb/1105/ZFIN-6155.sql
@@ -8,5 +8,6 @@ update sfclg set zdb=get_id('SFCL');
 insert into zdb_active_Data select zdb from sfclg;
 insert into sequence_feature_chromosome_location (sfcl_zdb_id, sfcl_feature_zdb_id,sfcl_start_position,sfcl_end_position,sfcl_assembly,sfcl_chromosome,sfcl_evidence_code) select zdb,ftr,st,endloc,assembly,chr,'ZDB-TERM-170419-250' from sfclg;
 insert into record_attribution (recattrib_Data_zdb_id,recattrib_source_zdb_id) select zdb,'ZDB-PUB-180131-10' from sfclg;
+insert into record_attribution (recattrib_Data_zdb_id,recattrib_source_zdb_id) values ('ZDB-SFCL-180815-1','ZDB-PUB-180131-10');
 
 
