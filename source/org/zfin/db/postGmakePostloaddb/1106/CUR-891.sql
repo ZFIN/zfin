17123fa13f6205ee10dda3321988cde0cda3d53a
Prita Mani <pm@zfin.org>
2019-03-28 11:57:03 -0700

    CUR-891

--- source/org/zfin/db/postGmakePostloaddb/1106/CUR-891.sql
+++ source/org/zfin/db/postGmakePostloaddb/1106/CUR-891.sql
@@ -2,3 +2,5 @@
 --changeset pm:CUR-891
 
 update feature set feature_abbrev_order=zero_pad(feature_abbrev) where feature_abbrev_order=feature_abbrev;
+update feature set feature_abbrev_order=zero_pad(feature_abbrev) where feature_abbrev like 'zf%';
+update feature set feature_name_order=zero_pad(feature_name) where feature_abbrev like 'zf%';
