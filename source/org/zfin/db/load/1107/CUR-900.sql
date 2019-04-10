2fa349f6d88a2ad3932988b19b7041c491684462
Prita Mani <pm@zfin.org>
2019-04-10 10:53:00 -0700

    CUR-900

---
+++ source/org/zfin/db/load/1107/CUR-900.sql
@@ -0,0 +1,141 @@
+--liquibase formatted sql
+--changeset prita:CUR-900
+
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('NCCR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('TLNRR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('BR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('BINDSITE','DEFICIENCY_TLOC_MARK');
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('LIGANDBS','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('LNCRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('LINCRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('MIRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('SRPRNAG','DEFICIENCY_TLOC_MARK');
+
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('PIRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('SCRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('SNORNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('TRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('NCRNAG','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+values ('RRNAG','DEFICIENCY_TLOC_MARK');
+
+
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('TFBS','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('EBS','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('NCBS','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('EMR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('HMR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('MDNAB','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('RR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('TRR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('PROMOTER','DEFICIENCY_TLOC_MARK');
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('ENHANCER','DEFICIENCY_TLOC_MARK');
+
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('LCR','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('NUCMO','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('DNAMO','DEFICIENCY_TLOC_MARK');
+
+insert into marker_type_group_member(mtgrpmem_mrkr_type,
+    mtgrpmem_mrkr_type_group)
+
+values ('RNAMO','DEFICIENCY_TLOC_MARK');
+
