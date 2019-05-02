eaf8bb780f9f193d76d56f721eed4f8ac50b8fb1
Patrick Kalita <pkalita@zfin.org>
2019-05-02 13:17:59 -0700

    Remove curate link from student statuses

--- home/javascript/react/components/DashboardPubList.js
+++ home/javascript/react/components/DashboardPubList.js
@@ -21,53 +21,57 @@ class DashboardPubList extends React.Component {
 
         return (
             <React.Fragment>
-                {Object.entries(pubMap).map(([status, pubs]) => (
-                    <div key={status}>
-                        <h4>{status} ({statusCounts[status]})</h4>
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th width="150px">ZDB-ID</th>
-                                <th>Title</th>
-                                <th width="100px">Last Status Change</th>
-                                {status === 'Waiting for Author' && <th width="130px">Last Correspondence</th>}
-                                <th width="150px">Status</th>
-                                <th width="100px">Owner</th>
-                                <th width="40px" />
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {pubs.map(pub => (
-                                <tr key={pub.zdbId}>
-                                    <td>
-                                        <a href={`/${pub.zdbId}`} target="_blank">{pub.zdbId}</a>
-                                    </td>
-                                    <td>
-                                        <p><b dangerouslySetInnerHTML={{__html: pub.title}} /></p>
-                                    </td>
-                                    <td><TimeAgo date={pub.status.updateDate} formatter={formatter} /></td>
-                                    {status === 'Waiting for Author' && <td><TimeAgo date={pub.lastCorrespondenceDate} formatter={formatter} /></td>}
-                                    <td>{pub.status.status.name}</td>
-                                    <td>{pub.status.owner.name}</td>
-                                    <td>
-                                        <div className="dropdown">
-                                            <button className="btn btn-link dropdown-toggle" type="button" data-toggle="dropdown">
-                                                <i className="fas fa-ellipsis-v" />
-                                            </button>
-                                            <ul className="dropdown-menu dropdown-menu-right">
-                                                <li><a href={`/action/curation/${pub.zdbId}`} target="_blank">Curate</a></li>
-                                                <li><a href={`/action/publication/${pub.zdbId}/track`} target="_blank">Track</a></li>
-                                                <li><a href={`/action/publication/${pub.zdbId}/link`} target="_blank">Link</a></li>
-                                                <li><a href={`/action/publication/${pub.zdbId}/edit`} target="_blank">Edit</a></li>
-                                            </ul>
-                                        </div>
-                                    </td>
+                {Object.entries(pubMap).map(([status, pubs]) => {
+                    const hasCorrespondenceColumn = status === 'Waiting for Author';
+                    const hasCurateAction = status !== 'Processing' && status !== 'Manual PDF Acquisition Needed';
+                    return (
+                        <div key={status}>
+                            <h4>{status} ({statusCounts[status]})</h4>
+                            <table className="table">
+                                <thead>
+                                <tr>
+                                    <th width="150px">ZDB-ID</th>
+                                    <th>Title</th>
+                                    <th width="100px">Last Status Change</th>
+                                    {hasCorrespondenceColumn && <th width="130px">Last Correspondence</th>}
+                                    <th width="150px">Status</th>
+                                    <th width="100px">Owner</th>
+                                    <th width="40px" />
                                 </tr>
-                            ))}
-                            </tbody>
-                        </table>
-                    </div>
-                ))}
+                                </thead>
+                                <tbody>
+                                {pubs.map(pub => (
+                                    <tr key={pub.zdbId}>
+                                        <td>
+                                            <a href={`/${pub.zdbId}`} target="_blank">{pub.zdbId}</a>
+                                        </td>
+                                        <td>
+                                            <p><b dangerouslySetInnerHTML={{__html: pub.title}} /></p>
+                                        </td>
+                                        <td><TimeAgo date={pub.status.updateDate} formatter={formatter} /></td>
+                                        {hasCorrespondenceColumn && <td><TimeAgo date={pub.lastCorrespondenceDate} formatter={formatter} /></td>}
+                                        <td>{pub.status.status.name}</td>
+                                        <td>{pub.status.owner.name}</td>
+                                        <td>
+                                            <div className="dropdown">
+                                                <button className="btn btn-link dropdown-toggle" type="button" data-toggle="dropdown">
+                                                    <i className="fas fa-ellipsis-v" />
+                                                </button>
+                                                <ul className="dropdown-menu dropdown-menu-right">
+                                                    {hasCurateAction && <li><a href={`/action/curation/${pub.zdbId}`} target="_blank">Curate</a></li>}
+                                                    <li><a href={`/action/publication/${pub.zdbId}/track`} target="_blank">Track</a></li>
+                                                    <li><a href={`/action/publication/${pub.zdbId}/link`} target="_blank">Link</a></li>
+                                                    <li><a href={`/action/publication/${pub.zdbId}/edit`} target="_blank">Edit</a></li>
+                                                </ul>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    )
+                })}
             </React.Fragment>
         );
     }
