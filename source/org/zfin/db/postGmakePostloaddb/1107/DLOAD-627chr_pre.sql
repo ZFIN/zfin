38585ed0fd56480420cb99c6e11629b0fb5ddec1
Prita Mani <pm@zfin.org>
2019-05-09 11:22:14 -0700

    DLOAD-627

---
+++ source/org/zfin/db/postGmakePostloaddb/1107/DLOAD-627chr_pre.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset pm:DLOAD-627chr_pre
+
+drop table if exists sanger_location ;
+create table  sanger_location (
+ allele text not null,
+        assembly text not null,
+           chromosome text not null,
+            location integer) ;
+
