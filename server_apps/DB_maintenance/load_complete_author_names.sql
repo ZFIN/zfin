257062abf1016ce8376a7bf0e55347801c32cbad
Xiang Shao <xshao@zfin.org>
2019-05-09 18:15:26 -0700

    PUB-505 add a job to load pubmed_publication_author table with complete auther data from pubmed

---
+++ server_apps/DB_maintenance/load_complete_author_names.sql
@@ -0,0 +1,22 @@
+begin work;
+
+create temp table names (
+    pubmedid    text not null,
+    zdbid       text  not null,
+    lastname    text not null,
+    middlename    text,
+    firstname    text    
+) ;
+
+create index names_pid_idx on names(pubmedid);
+create index names_zdbid_idx on names(zdbid);
+
+copy names from '<!--|ROOT_PATH|-->/server_apps/DB_maintenance/authors' (delimiter '|');
+
+insert into pubmed_publication_author(ppa_pubmed_id, ppa_publication_zdb_id, ppa_author_last_name, ppa_author_middle_name, ppa_author_first_name)
+  select pubmedid, zdbid, lastname, middlename, firstname
+    from names;
+
+commit work;
+
+
