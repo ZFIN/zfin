780cf59bc06f15ff7988afb22f33b4b6e60da3dd
Christian Pich <cmpich@zfin.org>
2019-05-13 19:45:22 +0200

    fix groovy script

--- server_apps/solr/prototype/conf/index-solr.groovy
+++ server_apps/solr/prototype/conf/index-solr.groovy
@@ -1,6 +1,6 @@
 //usr/bin/env groovy -cp "$groovy_classpath:." "$0" $@; exit $?
-import org.zfin.infrastructure.ant.runsqlfiles
-import org.zfin.properties.zfinpropertiesenum
+
+import org.zfin.infrastructure.ant.RunSQLFiles
 
 def env = system.getenv()
 
@@ -8,7 +8,7 @@ println 'start unloading synonym, stopwords and keepwords files...'
 
 def solrdir = "${env['targetroot']}/server_apps/solr/prototype/conf/"
 def propertiesfile = "${env['targetroot']}/home/web-inf/zfin.properties"
-runsqlfiles runscriptfiles = new runsqlfiles("build solr index", propertiesfile, solrdir)
+RunSQLFiles runscriptfiles = new RunSQLFiles("build solr index", propertiesfile, solrdir)
 runscriptfiles.initializelogger("./log4j.xml")
 runscriptfiles.setqueryfiles("generate-all-term-contains-synonyms-file.sql",
         "generate-organism-name-file.sql",
