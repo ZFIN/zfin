d68c7ccf0e1af264c94c255f8a132e52fd079426
Christian Pich <cmpich@zfin.org>
2019-05-14 16:43:39 +0200

    fix compiler error

--- server_apps/solr/prototype/conf/index-solr.groovy
+++ server_apps/solr/prototype/conf/index-solr.groovy
@@ -1,18 +1,17 @@
-//usr/bin/env groovy -cp "$groovy_classpath:." "$0" $@; exit $?
-
+//usr/bin/env groovy -cp "$GROOVY_CLASSPATH:." "$0" $@; exit $?
 import org.zfin.infrastructure.ant.RunSQLFiles
 
-def env = system.getenv()
+def env = System.getenv()
 
-println 'start unloading synonym, stopwords and keepwords files...'
+println 'Start unloading synonym, stopwords and keepwords files...'
 
-def solrdir = "${env['targetroot']}/server_apps/solr/prototype/conf/"
-def propertiesfile = "${env['targetroot']}/home/web-inf/zfin.properties"
-RunSQLFiles runscriptfiles = new RunSQLFiles("build solr index", propertiesfile, solrdir)
-runscriptfiles.initializelogger("./log4j.xml")
-runscriptfiles.setqueryfiles("generate-all-term-contains-synonyms-file.sql",
+def solrDir = "${env['TARGETROOT']}/server_apps/solr/prototype/conf/"
+def propertiesFile = "${env['TARGETROOT']}/home/WEB-INF/zfin.properties"
+RunSQLFiles runScriptFiles = new RunSQLFiles("Build solr index", propertiesFile, solrDir)
+runScriptFiles.initializeLogger("./log4j.xml")
+runScriptFiles.setQueryFiles("generate-all-term-contains-synonyms-file.sql",
         "generate-organism-name-file.sql",
         "generate-reporter-name-file.sql")
-runscriptfiles.execute()
+runScriptFiles.execute()
 
-println 'finished unloading synonym, stopwords and keepwords files.'
+println 'Finished unloading synonym, stopwords and keepwords files.'
