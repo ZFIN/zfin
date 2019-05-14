85c04c265397fb351fe751e0e7d4cee78c81e5f2
Xiang Shao <xshao@zfin.org>
2019-05-13 18:29:39 -0700

    ZFIN-6158 modified a bunch of complex genotype and fih names

---
+++ source/org/zfin/db/postGmakePostloaddb/1107/ZFIN-6158b.sql
@@ -0,0 +1,61 @@
+--liquibase formatted sql
+--changeset xshao:ZFIN-6158
+
+update genotype set geno_display_name = 'ephb4b<sup>tsu25/tsu25</sup>; s870Tg' where geno_zdb_id = 'ZDB-GENO-161213-1';
+
+update fish set fish_name = 'ephb4b<sup>tsu25/tsu25</sup>; s870Tg', fish_full_name = 'ephb4b<sup>tsu25/tsu25</sup>; s870Tg' where fish_zdb_id = 'ZDB-FISH-161213-1';
+
+
+update genotype set geno_display_name = 'pitx2<sup>ups6/ups6</sup>; s870Tg' where geno_zdb_id = 'ZDB-GENO-181115-1';
+
+update fish set fish_name = 'pitx2<sup>ups6/ups6</sup>; s870Tg', fish_full_name = 'pitx2<sup>ups6/ups6</sup>; s870Tg' where fish_zdb_id = 'ZDB-FISH-181115-1';
+
+
+update genotype set geno_display_name = 's903Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-180612-6';
+
+update fish set fish_name = 's903Tg; s870Tg', fish_full_name = 's903Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-180612-17';
+
+
+update genotype set geno_display_name = 'dld<sup>tr233/tr233</sup>; s870Tg' where geno_zdb_id = 'ZDB-GENO-140908-3';
+
+update fish set fish_name = 'dld<sup>tr233/tr233</sup>; s870Tg', fish_full_name = 'dld<sup>tr233/tr233</sup>; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-9619';
+
+
+update genotype set geno_display_name = 'fb7Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-150210-11';
+
+update fish set fish_name = 'fb7Tg; s870Tg', fish_full_name = 'fb7Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-560';
+
+
+update genotype set geno_display_name = 'fr13Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-090421-3';
+
+update fish set fish_name = 'fr13Tg; s870Tg', fish_full_name = 'fr13Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-17603';
+
+update genotype set geno_display_name = 'gt3Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-160901-6';
+
+update fish set fish_name = 'gt3Tg; s870Tg', fish_full_name = 'gt3Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-160901-12';
+
+
+update genotype set geno_display_name = 'pd1042Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-131104-1';
+
+update fish set fish_name = 'pd1042Tg; s870Tg', fish_full_name = 'pd1042Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-5067';
+
+
+update genotype set geno_display_name = 'vu119Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-081119-7';
+
+update fish set fish_name = 'vu119Tg; s870Tg', fish_full_name = 'vu119Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-9411';
+
+
+update genotype set geno_display_name = 'vu234Tg; s870Tg' where geno_zdb_id = 'ZDB-GENO-160212-1';
+
+update fish set fish_name = 'vu234Tg; s870Tg', fish_full_name = 'vu234Tg; s870Tg' where fish_zdb_id = 'ZDB-FISH-160212-6';
+
+
+update genotype set geno_display_name = 'cftr<sup>pd1048/pd1048</sup>; s870Tg' where geno_zdb_id = 'ZDB-GENO-131101-8';
+
+update fish set fish_name = 'cftr<sup>pd1048/pd1048</sup>; s870Tg', fish_full_name = 'cftr<sup>pd1048/pd1048</sup>; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-27771';
+
+
+update genotype set geno_display_name = 'cftr<sup>pd1049/pd1049</sup>; s870Tg' where geno_zdb_id = 'ZDB-GENO-131101-9';
+
+update fish set fish_name = 'cftr<sup>pd1049/pd1049</sup>; s870Tg', fish_full_name = 'cftr<sup>pd1049/pd1049</sup>; s870Tg' where fish_zdb_id = 'ZDB-FISH-150901-25050';
+
