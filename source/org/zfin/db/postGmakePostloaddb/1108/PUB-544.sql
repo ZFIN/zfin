29587524cba32ce1ddd22998073a8aff14e070aa
Prita Mani <pm@zfin.org>
2019-05-23 15:18:11 -0700

    PUB-544

---
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-544.sql
@@ -0,0 +1,6 @@
+--liquibase formatted sql
+--changeset pm:PUB-52944
+
+update journal set jrnl_print_issn='0093-7355' where jrnl_zdb_id='ZDB-JRNL-050621-893';
+update journal set jrnl_online_issn='1548-4475' where jrnl_zdb_id='ZDB-JRNL-050621-893';
+
