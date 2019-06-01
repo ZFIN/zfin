3021e96c278b735f5106ceb416edd48ec43ff8df
Sierra Moxon <smoxon@zfin.org>
2019-05-31 17:02:41 -0700

    PUB-543

--- server_apps/DB_maintenance/movefiles.py
+++ server_apps/DB_maintenance/movefiles.py
@@ -1,32 +1,70 @@
 #!/usr/bin/python
 
-import re
+import psycopg2
 import os
-import shutil
+import glob
+from shutil import copy
 
-pdf_dir = '<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->'
+hostname = 'localhost'
+database = 'swrdb'
 
-for f in os.listdir(pdf_dir):
-    m = re.match("^(ZDB-PUB-)(\d{2})(\d{2})(\d{2})(-\d+)(\.pdf)$", f)
-    if m:
-        year = m.group(2)
-        month = m.group(3)
-        day = m.group(4)
 
-        if year.startswith('9'):
-            year = "19" + year
+def do_query(conn):
+    cur = conn.cursor()
+
+    cur.execute("SELECT distinct img_zdb_id, fig_source_zdb_id "
+                "     FROM figure, image WHERE fig_zdb_id = img_fig_zdb_id"
+                "     AND fig_source_zdb_id like 'ZDB-PUB-19%'")
+
+    for img_id, fig_source_id in cur.fetchall():
+        fullPathPDFDir = "/tmp/"+fig_source_id
+        if os.path.isdir(fullPathPDFDir):
+            print fullPathPDFDir
+            pattern = "/research/zcentral/loadUp/imageLoadUp/"+img_id+"*"
+
+            for imgFile in glob.glob(pattern):
+                 print imgFile
+                 copy(imgFile, fullPathPDFDir)
+
         else:
-            year = "20" + year
+            print "cant file dir: " + fullPathPDFDir
+            os.mkdir(fullPathPDFDir)
+            pattern = '/research/zcentral/loadUp/imageLoadUp/' + img_id + '*'
 
-        src_path = os.path.join(pdf_dir, f)
-        year_dir = os.path.join(pdf_dir, year)
-        dst_path = os.path.join(year_dir, f)
+            for imgFile in glob.glob(pattern):
+                 print imgFile
+                 copy(imgFile, fullPathPDFDir)
 
-        if os.path.isfile(dst_path):
-            continue
+myConnection = psycopg2.connect(host=hostname, dbname=database)
+do_query(myConnection)
+myConnection.close()
 
-        if not os.path.isdir(year_dir):
-            os.mkdir(year_dir)
 
-        print src_path
-        shutil.copy(src_path, year_dir)
+
+
+# pdf_dir = '<!--|LOADUP_FULL_PATH|--><!--|PDF_LOAD|-->'
+
+# for f in os.listdir(pdf_dir):
+#     m = re.match("^(ZDB-PUB-)(\d{2})(\d{2})(\d{2})(-\d+)(\.pdf)$", f)
+#     if m:
+#         year = m.group(2)
+#         month = m.group(3)
+#         day = m.group(4)
+#
+#         if year.startswith('9'):
+#             year = "19" + year
+#         else:
+#             year = "20" + year
+#
+#         src_path = os.path.join(pdf_dir, f)
+#         year_dir = os.path.join(pdf_dir, year)
+#         dst_path = os.path.join(year_dir, f)
+#
+#         if os.path.isfile(dst_path):
+#             continue
+#
+#         if not os.path.isdir(year_dir):
+#             os.mkdir(year_dir)
+#
+#         print src_path
+#         shutil.copy(src_path, year_dir)
