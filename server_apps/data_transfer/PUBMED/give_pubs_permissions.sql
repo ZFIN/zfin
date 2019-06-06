20620de43f0b067c62f89250d41c70e79f8b9888
Sierra Moxon <smoxon@zfin.org>
2019-06-06 11:46:27 -0700

    PUB-522

--- server_apps/data_transfer/PUBMED/give_pubs_permissions.sql
+++ server_apps/data_transfer/PUBMED/give_pubs_permissions.sql
@@ -2,11 +2,11 @@ begin work ;
 
 create temp table tmp_pubs_to_give_permissions (pub_zdb_id text);
 
-\copy tmp_pubs_to_give_permissions from  'pubs_to_give_permissions.txt' with delimiter '|' ;
+\copy tmp_pubs_to_give_permissions from  'pubsToGivePermission.txt' with delimiter '|' ;
 
 update publication
  set pub_can_show_images = 't'
- where exists (select 'x' from tmp_pubs_to_give_permisions where pub_zdb_id = zdb_id)
+ where exists (select 'x' from tmp_pubs_to_give_permissions where pub_zdb_id = zdb_id)
  and pub_can_show_images = 'f';
 
 
