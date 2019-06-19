fe3c22cfb397d2cf0989e4cf6fb63e2e9b026718
Prita Mani <pm@zfin.org>
2019-06-19 13:55:55 -0700

    PUB-536 added one more where clause

--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-536.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-536.sql
@@ -6,6 +6,7 @@ into tmp_pub
 from pub_tracking_history, publication,record_attribution,journal
 where pth_pub_zdb_id=zdb_id
 and pth_status_id=4
+and pth_location_id is null
 and jtype='Journal'
 and pub_jrnl_zdb_id=jrnl_zdb_id
 and jrnl_abbrev not like '%Tox%'
