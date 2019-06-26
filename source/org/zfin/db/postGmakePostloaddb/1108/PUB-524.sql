e8ed0c2b312d3c23e85302fa3e04af3daf4cde0c
Prita Mani <pm@zfin.org>
2019-06-26 13:54:16 -0700

    clean up sqls rework

--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
@@ -3,16 +3,18 @@
 
 drop table if exists tmp_pub;
 
-select distinct pth_pub_zdb_id as pubid
+select  pth_pub_zdb_id as pubid
 into tmp_pub
 from pub_tracking_history, publication
 where pth_pub_zdb_id=zdb_id
 and pth_status_id=1
-and pth_status_is_current='t'
+and pth_status_is_current='f'
 and jtype='Journal'
 and pub_date between '2010-01-01' and '2016-12-31'
 and zdb_id not in (select recattrib_source_zdb_id from record_attribution)
-and zdb_id not in (select pf_pub_Zdb_id from publication_file);
+and zdb_id  not in (select pf_pub_Zdb_id from publication_file)
+group by pth_pub_zdb_id
+having count(pth_pub_zdb_id)=1;
 
 update pub_tracking_history
 set pth_status_id=15
--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-534.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-534.sql
@@ -2,20 +2,21 @@
 --changeset pm:PUB-534
 
 drop table if exists tmp_pub;
-select distinct pth_pub_zdb_id as pubid
+select  pth_pub_zdb_id as pubid
 into tmp_pub
-from pub_tracking_history, publication,record_attribution,journal
+from pub_tracking_history, publication,journal
 where pth_pub_zdb_id=zdb_id
 and pth_status_id=1
-and pth_status_is_current='t'
+and pth_status_is_current='f'
 and jtype='Journal'
 and pub_jrnl_zdb_id=jrnl_zdb_id
 and jrnl_abbrev  like '%Tox%'
 and pub_date between '2010-01-01' and '2016-12-31'
-and zdb_id=recattrib_source_zdb_id
-and recattrib_data_zdb_id not like 'ZDB-XPAT%'
+and zdb_id not in (select recattrib_source_zdb_id from record_attribution where recattrib_data_zdb_id like 'ZDB-XPAT%')
 and pth_pub_zdb_id not in (Select pt_pub_zdb_id from pheno_term)
-and zdb_id  in (select pf_pub_Zdb_id from publication_file);
+and zdb_id  in (select pf_pub_Zdb_id from publication_file)
+group by pth_pub_zdb_id
+having count(pth_pub_zdb_id)=1;
 
 update pub_tracking_history
 set pth_status_id=4
