57dc41d13d32c9fe215d362878afca4a5df5c296
Prita Mani <pm@zfin.org>
2019-06-28 09:30:37 -0700

    PUB-529

--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-529.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-529.sql
@@ -3,8 +3,7 @@
 
 drop table if exists tmp_pub;
 select distinct pth_pub_zdb_id as pubid
-into tmp_pub
-from pub_tracking_history, publication,record_attribution,journal
+into tmp_pub from pub_tracking_history, publication,record_attribution,journal
 where pth_pub_zdb_id=zdb_id
 and pth_status_id=1
 and pth_status_is_current='t'
@@ -12,10 +11,8 @@ and jtype='Journal'
 and pub_jrnl_zdb_id=jrnl_zdb_id
 and jrnl_abbrev not like '%Tox%'
 and pub_date between '0012-07-24' and '2005-12-31'
-and zdb_id=recattrib_source_zdb_id
-and recattrib_data_zdb_id not like 'ZDB-FIG%';
+and zdb_id=recattrib_source_zdb_id order by pubid;
 
-update pub_tracking_history
-set pth_status_id=11
-from tmp_pub
-where pth_pub_zdb_id=pubid;
+insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
+select pubid, 11, 'ZDB-PERS-030520-2' from tmp_pub;
+
--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-536.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-536.sql
@@ -8,12 +8,13 @@ where pth_pub_zdb_id=zdb_id
 and pth_status_id=4
 and pth_status_is_current='t'
 and pth_location_id is null
+and pub_is_indexed='f'
 and jtype='Journal'
 and pub_jrnl_zdb_id=jrnl_zdb_id
 and jrnl_abbrev not like '%Tox%'
-and pub_date between '0012-07-24' and '2005-12-31'
+and pub_arrival_date between '0012-07-24' and '2005-12-31'
 and zdb_id=recattrib_source_zdb_id
-and recattrib_data_zdb_id not like 'ZDB-FIG%';
+
 
 update pub_tracking_history
 set pth_status_id=11
