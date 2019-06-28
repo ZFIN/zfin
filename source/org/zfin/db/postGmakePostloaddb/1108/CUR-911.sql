beeb49a6595f7ba8aa2f6cb98a7478bd3cbcd8e9
Prita Mani <pm@zfin.org>
2019-06-28 07:12:09 -0700

    CUR-911 fixed code review comments

--- source/org/zfin/db/postGmakePostloaddb/1108/CUR-911.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/CUR-911.sql
@@ -7,9 +7,9 @@ update marker set mrkr_name = 'Tg(ca8-E1B:Hso.Arch3-TagRFPT,GCaMP5G)' where mrkr
 
 update marker set mrkr_abbrev = 'Tg(ca8-E1B:Hso.Arch3-TagRFPT,GCaMP5G)' where mrkr_zdb_id = 'ZDB-TGCONSTRCT-151204-5';
 
-delete from construct_component where cc_pk_id = 32522;
+delete from construct_component where cc_component_zdb_id = 'ZDB-EREGION-160817-1' and cc_construct_zdb_id='ZDB-TGCONSTRCT-151204-5';
 
-update construct_component set cc_component_zdb_id='ZDB-EFG-160519-3' where cc_pk_id=32523;
+update construct_component set cc_component_zdb_id='ZDB-EFG-160519-3' where cc_construct_zdb_id='ZDB-TGCONSTRCT-151204-5' and cc_component_zdb_id='ZDB-EFG-110425-1';
 
 
 delete from marker_relationship
