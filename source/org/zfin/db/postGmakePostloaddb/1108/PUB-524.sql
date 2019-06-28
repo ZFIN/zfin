9c9d53d6c8b315f41e1d5a8877bff67001678e31
Prita Mani <pm@zfin.org>
2019-06-28 07:22:49 -0700

    PUB-524 pub cleaup

--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
@@ -7,16 +7,12 @@ select  pth_pub_zdb_id as pubid
 into tmp_pub
 from pub_tracking_history, publication
 where pth_pub_zdb_id=zdb_id
-and pth_status_id=1
-and pth_status_is_current='f'
 and jtype='Journal'
 and pub_date between '2010-01-01' and '2016-12-31'
+and pth_status_id=1
 and zdb_id not in (select recattrib_source_zdb_id from record_attribution)
 and zdb_id  not in (select pf_pub_Zdb_id from publication_file)
-group by pth_pub_zdb_id
-having count(pth_pub_zdb_id)=1;
+and pth_status_is_current='t';
 
-update pub_tracking_history
-set pth_status_id=15
-from tmp_pub
-where pth_pub_zdb_id=pubid;
+insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
+select pubid, 15, 'ZDB-PERS-030520-2' from tmp_pub;
