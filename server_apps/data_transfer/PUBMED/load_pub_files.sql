17adf20887405080df5129e9e3dac1e7fa510e7a
Sierra Moxon <smoxon@zfin.org>
2019-07-08 16:33:15 -0700

    add suppliments that don't end in pdf

--- server_apps/data_transfer/PUBMED/load_pub_files.sql
+++ server_apps/data_transfer/PUBMED/load_pub_files.sql
@@ -13,6 +13,21 @@ where exists (select 'x' from tmp_files_to_load
                      where pf_pub_zdb_id = pub_zdb_id
                      and pf_file_name = filename);
 
+delete from tmp_files_to_load
+ where exists (select 'x' from publication_file
+                      where pf_pub_zdb_id = pub_zdb_id
+                      and pf_original_file_name = filename_no_path);
+
+insert into publication_file (pf_pub_zdb_id,
+            pf_file_name,
+            pf_file_type_id,
+            pf_original_file_name)
+ select pub_zdb_id,
+        filename,
+        3,
+        filename_no_path
+   from tmp_files_to_load;
+
 --rollback work ;
 
 commit work;
