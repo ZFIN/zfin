88e7f49fcfb766e6521aca3afa416c4b301c3f3c
Prita Mani <pm@zfin.org>
2019-07-10 10:16:46 -0700

    PUB-524 fixes
    (cherry picked from commit 600a15c23a76dbe0ffff35ff5fc083d7d1c79733)

--- source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
+++ source/org/zfin/db/postGmakePostloaddb/1108/PUB-524.sql
@@ -1,18 +1,26 @@
 --liquibase formatted sql
 --changeset pm:PUB-524
 
-drop table if exists tmp_pub;
 
-select  pth_pub_zdb_id as pubid
-into tmp_pub
-from pub_tracking_history, publication
-where pth_pub_zdb_id=zdb_id
+drop table if exists tmp_pub;
+drop table if exists tmp_pub1;
+
+select pth_pub_zdb_id as pubzdb into tmp_pub from pub_tracking_history
+where not exists(select 1 from publication_file
+where pth_pub_zdb_id = pf_pub_zdb_id)
+group by pth_pub_zdb_id
+having count(pth_pub_zdb_id)=1;
+
+select  pubzdb
+into tmp_pub1
+from tmp_pub,pub_tracking_history, publication
+where pubzdb=pth_pub_zdb_id and pth_pub_zdb_id=zdb_id
 and jtype='Journal'
 and pub_date between '2010-01-01' and '2016-12-31'
 and pth_status_id=1
-and zdb_id not in (select recattrib_source_zdb_id from record_attribution)
-and zdb_id  not in (select pf_pub_Zdb_id from publication_file)
-and pth_status_is_current='t';
+and zdb_id not in (select recattrib_source_zdb_id from record_attribution);
+--and pth_status_is_current='f';
 
+--\copy (select * from tmp_pub1 order by pubzdb) to 'pub524.csv' delimiter ',';
 insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
-select pubid, 15, 'ZDB-PERS-030520-2' from tmp_pub;
+select pubzdb, 15, 'ZDB-PERS-030520-2' from tmp_pub1;
