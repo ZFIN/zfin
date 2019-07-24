64b03aaf0c817003ccc3d5e0ca54edefd9e7d3b5
Sierra Moxon <smoxon@zfin.org>
2019-07-24 09:34:00 -0700

    PUB-592

--- server_apps/data_transfer/PUBMED/getPDFandImages.groovy
+++ server_apps/data_transfer/PUBMED/getPDFandImages.groovy
@@ -182,6 +182,7 @@ def parseLabelCaptionImage(groupMatchString, zdbId, pmcId, imageFilePath, pubYea
     if (imageNameMatch.size() > 0) {
         imageNameMatch.each {
             image = it[2] + ".jpg"
+
             println (image)
             makeThumbnailAndMediumImage(image, image.replace(".jpg", ""), zdbId, pubYear)
             String extension = FilenameUtils.getExtension(image)
@@ -207,8 +208,20 @@ def makeThumbnailAndMediumImage(fileName, fileNameNoExtension, pubZdbId, pubYear
     File fullFile = new File(ZfinPropertiesEnum.LOADUP_FULL_PATH.toString()+"/"+pubYear+"/"+pubZdbId+"/", fileName)
 
     // make thumbnail and medium images in the same directory as their parent images.
-    "/bin/convert -thumbnail 1000x64 ${fullFile} ${thumbnailFile}".execute()
-    "/bin/convert -thumbnail 500x550 ${fullFile} ${mediumFile}".execute()
+
+    def sout = new StringBuilder(), serr = new StringBuilder()
+    println("starting thumbs")
+    def proc = "/bin/convert -thumbnail 1000x64 ${fullFile} ${thumbnailFile}".execute()
+    proc.waitForProcessOutput(sout, serr)
+    if (sout || serr ) {
+        println "out> $sout err> $serr"
+    }
+
+    def procMedium ="/bin/convert -thumbnail 500x550 ${fullFile} ${mediumFile}".execute()
+    procMedium.waitForProcessOutput(sout, serr)
+    if (sout || serr ) {
+        println "out> $sout err> $serr"
+    }
 
 }
 
