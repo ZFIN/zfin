013f33cbfd44f9141a451df33ae737c566e9f488
Sierra Moxon <smoxon@zfin.org>
2019-08-15 12:19:53 -0700

    adding status to activation pubs

--- source/org/zfin/db/load/1109/fix_activation.sql
+++ source/org/zfin/db/load/1109/fix_activation.sql
@@ -5,4 +5,15 @@ update publication
  set status = 'active'
 where exists (select 'x' from to_activate
                      where pub_pmc_id = pmcId)
-and status != 'active';;
+and status != 'active';
+
+insert into pub_tracking_history
+  (pth_pub_zdb_id,
+   pth_status_id,
+   pth_status_set_by)
+select zdb_id, 20, 'ZDB-PERS-170918-1'
+  from publication, to_activate
+  where accession_no = pmcId::integer
+ and not exists (select 'x' from pub_tracking_history
+                        where pth_pub_zdb_id = zdb_id
+                        and pth_status_id != '1');
