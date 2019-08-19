4a4f24a03826e3ef7b5a7b832b996b60f2bba311
Patrick Kalita <pkalita@zfin.org>
2019-08-19 10:53:30 -0700

    Pause placeholder animation on input focus

--- home/javascript/zfin-common/animated-placeholder.jquery.js
+++ home/javascript/zfin-common/animated-placeholder.jquery.js
@@ -4,6 +4,25 @@
 
     function addPlaceholder($input, values) {
         let idx = 0;
+        let intervalId = null;
+
+        const startAnimation = () => {
+            pauseAnimation();
+            intervalId = setInterval(function () {
+                idx = (idx + 1) % values.length;
+                $placeholder.on('transitionend', function () {
+                    $placeholder.text(values[idx]);
+                    $placeholder.off('transitionend');
+                    $placeholder.css('transform', 'perspective(300px) rotateX(0deg)');
+                });
+                $placeholder.css('transform', 'perspective(300px) rotateX(90deg)');
+            }, 5000);
+        };
+
+        const pauseAnimation = () => {
+            clearInterval(intervalId);
+        };
+
         const $placeholder = $('<span>')
             .css({
                 position: 'absolute',
@@ -23,18 +42,11 @@
             })
             .text(values[idx]);
         $placeholder.insertAfter($input);
-        $input.on('input', function () {
-            $placeholder.css('visibility', $input.val() === '' ? 'visible' : 'hidden');
-        });
-        setInterval(function () {
-            idx = (idx + 1) % values.length;
-            $placeholder.on('transitionend', function () {
-                $placeholder.text(values[idx]);
-                $placeholder.off('transitionend');
-                $placeholder.css('transform', 'perspective(300px) rotateX(0deg)');
-            });
-            $placeholder.css('transform', 'perspective(300px) rotateX(90deg)');
-        }, 5000);
+        $input
+            .on('input', function () { $placeholder.css('visibility', $input.val() === '' ? 'visible' : 'hidden'); })
+            .on('focus', function () { pauseAnimation(); })
+            .on('blur', function () { startAnimation(); });
+        startAnimation();
     }
 
     $.fn[pluginName] = function (placeholders) {
