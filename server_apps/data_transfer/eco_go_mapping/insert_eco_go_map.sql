fba4b125cc2c215173ddfa2f042ba7fa0e3979c1
Sierra Moxon <smoxon@zfin.org>
2019-08-21 09:16:24 -0700

    DLOAD-550

---
+++ server_apps/data_transfer/eco_go_mapping/insert_eco_go_map.sql
@@ -0,0 +1,12 @@
+begin work ;
+
+create temp table tmp_eco_map (evcode text, ecoterm text);
+
+\copy tmp_eco_map from '<!--|TARGETROOT|-->/server_apps/data_transfer/eco_go_mapping/gafeco.txt' delimiter ',';
+
+insert into eco_go_mapping (egm_term_zdb_id, egm_go_evidence_code)
+ select distinct term_zdb_id, egm_go_evidence_code
+  from term, tmp_eco_map
+   where term_ont_id = ecoterm;
+
+commit work;
