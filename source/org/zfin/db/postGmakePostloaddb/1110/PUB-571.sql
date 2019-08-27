b83c1bce011de306e3d642443797cb778b02003b
Prita Mani <pm@zfin.org>
2019-08-27 08:07:27 -0700

    PUB-571

--- source/org/zfin/db/postGmakePostloaddb/1110/PUB-571.sql
+++ source/org/zfin/db/postGmakePostloaddb/1110/PUB-571.sql
@@ -22,7 +22,7 @@ where pub_id = cur_pub_zdb_id and cur_closed_date is not null
 group by pub_id,cur_curator_zdb_id having count(cur_curator_zdb_id)>4;
 
 
-insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by,pth_status_is_current )
-select distinct pub_id, 11, cur_curator_zdb_id,'t' from tmp_pub1;
+insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
+select distinct pub_id, 11, cur_curator_zdb_id from tmp_pub1;
 
 
