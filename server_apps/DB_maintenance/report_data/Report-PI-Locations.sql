5d83e1299a345b5ad4ee9d7f9cf590cde0cef36e
Sierra Moxon <smoxon@zfin.org>
2019-09-05 07:07:47 -0700

    JENK-473

--- server_apps/DB_maintenance/report_data/Report-PI-Locations.sql
+++ server_apps/DB_maintenance/report_data/Report-PI-Locations.sql
@@ -1,6 +1,16 @@
+unload to pis_per_country
 select count(*), person_country 
   from person 
   where exists (select 'x' from int_person_lab 
-                           where position_id in (2,7)) 
+                           where position_id in (2,7)
+                           and source_id = zdb_id) 
  and person_country is not null
  group by person_country order by count(*) desc;
+
+unload to count_pis_without_country
+select count(*)
+  from person 
+  where exists (select 'x' from int_person_lab 
+                           where position_id in (2,7)
+                           and source_id = zdb_id) 
+ and person_country is null;
