5ffe63e5105b3dad26ffa99c85ebe0fef8188f32
Prita Mani <pm@zfin.org>
2019-10-14 09:36:35 -0700

    PUB-603 rework
    (cherry picked from commit 264d60c01ebd60492457681430bb1afc39345fd1)

--- source/org/zfin/db/postGmakePostloaddb/1110/PUB-603.sql
+++ source/org/zfin/db/postGmakePostloaddb/1110/PUB-603.sql
@@ -2,6 +2,7 @@
 --changeset pm:PUB-603
 drop table if exists tmp_pub;
 drop table if exists tmp_pub1;
+drop table if exists tmp_pub2;
 
 select pth_pub_zdb_id as pubzdb into tmp_pub from pub_tracking_history
 group by pth_pub_zdb_id
@@ -16,7 +17,15 @@ and pth_status_id=1
 and pth_location_id is null
 and jtype='Thesis';
 
+select distinct zdb_id
+into tmp_pub2
+from  publication
+where jtype='Thesis'
+and zdb_id not in (select pth_pub_zdb_id from pub_tracking_history);
 
 
 insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
 select pubzdb, 12, 'ZDB-PERS-030520-2' from tmp_pub1;
+
+insert into pub_tracking_history(pth_pub_zdb_id, pth_status_id, pth_status_set_by)
+select zdb_id, 12, 'ZDB-PERS-030520-2' from tmp_pub2;
