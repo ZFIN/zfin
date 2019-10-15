a8a7cab9bc9d392c5dbb1fa96e4fab6a125464e3
Prita Mani <pm@zfin.org>
2019-10-15 09:13:36 -0700

    ZFIN-6341

--- source/org/zfin/db/load/1110/add_goref_id.sql
+++ source/org/zfin/db/load/1110/add_goref_id.sql
@@ -4,3 +4,8 @@
 alter table publication
  add pub_goref_id text ;
 
+ update publication set pub_goref_id='GO_REF:0000104' where zdb_id='ZDB-PUB-170525-1';
+update publication set pub_goref_id='GO_REF:0000040' where zdb_id='ZDB-PUB-120306-4';
+update publication set pub_goref_id='GO_REF:0000041' where zdb_id='ZDB-PUB-130131-1';
+update publication set pub_goref_id='GO_REF:0000039' where zdb_id='ZDB-PUB-120306-2';
+
