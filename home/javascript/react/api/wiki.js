b10876940ac87a80bf6a603629435b0dc5dd9d37
Patrick Kalita <pkalita@zfin.org>
2019-10-29 14:54:54 -0700

    Reduce number of blog posts fetched by default

--- home/javascript/react/api/wiki.js
+++ home/javascript/react/api/wiki.js
@@ -3,7 +3,7 @@ import http from '../utils/http';
 export const getBlogPosts = (space, options = {}) => {
     const params = {
         cql: `space=${space} AND type=blogpost${options.query ? ` AND ${options.query}` : ''} ORDER BY created desc`,
-        limit: 10,
+        limit: 5,
         expand: 'history',
     };
     return http.get('https://@WIKI_HOST@/rest/api/content/search', params);
