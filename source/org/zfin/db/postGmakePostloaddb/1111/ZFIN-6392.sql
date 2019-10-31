bc0e4b7ede3b67078de7e633675026ab166a7e58
Xiang Shao <xshao@zfin.org>
2019-10-31 09:50:57 -0700

    ZFIN-6392 add missing genotype and fish names

---
+++ source/org/zfin/db/postGmakePostloaddb/1111/ZFIN-6392.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset xshao:ZFIN-6392
+
+update genotype set geno_display_name = get_genotype_display(geno_zdb_id) where geno_display_name is null or geno_display_name = '';
+
+update genotype set geno_display_name = 'Df(Chr23:mir200a,mir200b,mir429)hza53/hza53' where geno_zdb_id = 'ZDB-GENO-180823-6';
+
+update genotype set geno_display_name = 'Df(Chr23:mir200a,mir200b,mir429)hza53/hza53' where geno_zdb_id = 'ZDB-GENO-180823-5';
+
