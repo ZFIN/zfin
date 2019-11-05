b87adf143e134cf5420a70892ee396cf56dca74b
Christian Pich <cmpich@zfin.org>
2019-11-05 16:52:46 +0100

    Create rest controller for Alliance expression data

---
+++ source/org/zfin/agr/DataController.java
@@ -0,0 +1,20 @@
+package org.zfin.agr;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zfin.marker.agr.AllExpressionDTO;
+import org.zfin.marker.agr.BasicExpressionInfo;
+
+@RestController
+@RequestMapping("/api")
+public class DataController {
+
+    @RequestMapping(value = "/expression")
+    public AllExpressionDTO getFirstDiseases() {
+        BasicExpressionInfo info = new BasicExpressionInfo(5);
+        AllExpressionDTO basicExpressionDTO = info.getBasicExpressionInfo(5);
+        return basicExpressionDTO;
+    }
+
+
+}
