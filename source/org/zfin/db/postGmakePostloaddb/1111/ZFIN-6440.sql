f9926bbbdfc0ed05f0ed9f8025f8ff7e8414f1ec
Christian Pich <cmpich@zfin.org>
2019-11-13 12:19:54 +0100

    ZFIN-6440 update foreign species names

--- source/org/zfin/db/postGmakePostloaddb/1111/ZFIN-6440.sql
+++ source/org/zfin/db/postGmakePostloaddb/1111/ZFIN-6440.sql
@@ -4,3 +4,4 @@
 
 update  controlled_vocabulary set cv_name_definition = 'Drosophila melanogaster' where cv_term_name = 'Dme.';
 update  controlled_vocabulary set cv_name_definition = 'Homo sapiens' where cv_term_name = 'Hsa.';
+update  controlled_vocabulary set cv_name_definition = 'Caenorhabditis elegans' where cv_term_name = 'Cel.';
