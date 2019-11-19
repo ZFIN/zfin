917c5d943b5b122085dd8e6b91b54992773189bf
Sierra Moxon <smoxon@zfin.org>
2019-11-19 08:51:07 -0800

    DLOAD-649

---
+++ server_apps/data_transfer/ResourceCenters/syncFishOrderThisLinks.sql
@@ -0,0 +1,17 @@
+begin work;
+
+
+delete from int_data_supplier where idsup_data_zdb_id like 'ZDB-FISH-%';
+
+insert into int_data_supplier (idsup_data_zdb_id, idsup_supplier_zdb_id) 
+   select fish_Zdb_id, a.idsup_supplier_zdb_id 
+     from int_data_supplier a, fish 
+     where a.idsup_data_zdb_id = fish_genotype_zdb_id 
+      and not exists (Select 'x' from fish_str  
+                      where fishstr_fish_zdb_id = fish_zdb_id) 
+      and not exists (select 'x' from int_data_Supplier b 
+                                 where b.idsup_data_zdb_id = fish_zdb_id 
+                                  and b.idsup_supplier_zdb_id = a.idsup_supplier_zdb_id) ;
+
+
+commit work;
