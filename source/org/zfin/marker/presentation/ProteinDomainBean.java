f23ec3a6532c64ced8f427b48016cb01503fbf35
Prita Mani <pm@zfin.org>
2019-11-22 08:50:32 -0800

    ZFIN-6427

---
+++ source/org/zfin/marker/presentation/ProteinDomainBean.java
@@ -0,0 +1,35 @@
+package org.zfin.marker.presentation;
+
+import org.zfin.marker.Marker;
+
+/**
+ */
+public class ProteinDomainBean {
+    private String ipID;
+    private String ipName;
+    private String ipType;
+
+    public String getIpID() {
+        return ipID;
+    }
+
+    public void setIpID(String ipID) {
+        this.ipID = ipID;
+    }
+
+    public String getIpName() {
+        return ipName;
+    }
+
+    public void setIpName(String ipName) {
+        this.ipName = ipName;
+    }
+
+    public String getIpType() {
+        return ipType;
+    }
+
+    public void setIpType(String ipType) {
+        this.ipType = ipType;
+    }
+}
