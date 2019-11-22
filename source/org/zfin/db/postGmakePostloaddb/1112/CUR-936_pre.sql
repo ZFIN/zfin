cbc6d592c87e8fe6ed14f7bd8be89054497d09a3
Prita Mani <pm@zfin.org>
2019-11-22 12:22:36 -0800

    CUR-936

--- source/org/zfin/db/postGmakePostloaddb/1112/CUR-936_pre.sql
+++ source/org/zfin/db/postGmakePostloaddb/1112/CUR-936_pre.sql
@@ -2,4 +2,4 @@
 --changeset pm:CUR-936_pre
 
 drop table if exists seqvar;
-create table seqvar (featureid text,featureabb text,geneid text,geneabb text,mutterm text,mutname text,location text,mutdesc text,refseq text,varseq text);
+create table seqvar (featureid text,featureabb text,geneid text,geneabb text,muttype text,mutterm text,mutname text,location text,mutdesc text,refseq text,varseq text);
