d5ca702751961e486b87d0cc9a68ebe2aab173d5
Xiang Shao <xshao@zfin.org>
2020-01-01 20:50:28 -0800

    PUB-622

--- source/org/zfin/db/postGmakePostloaddb/1112/PUB-622.sql
+++ source/org/zfin/db/postGmakePostloaddb/1112/PUB-622.sql
@@ -2,5 +2,6 @@
 --changeset xshao:PUB-622
 
 delete from int_data_supplier
- where idsup_supplier_zdb_id not in ('ZDB-LAB-991005-53','ZDB-LAB-130607-1','ZDB-LAB-130226-1','ZDB-LAB-990120-1');
+ where idsup_supplier_zdb_id like 'ZDB-LAB%'
+   and idsup_supplier_zdb_id not in ('ZDB-LAB-991005-53','ZDB-LAB-130607-1','ZDB-LAB-130226-1','ZDB-LAB-990120-1');
 
