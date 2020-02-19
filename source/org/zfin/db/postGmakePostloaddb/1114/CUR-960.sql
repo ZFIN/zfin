8ab519a64dc9566555fa73a5a6ca52150f688611
Prita Mani <pm@zfin.org>
2020-02-19 13:09:30 -0800

    CUR-960

---
+++ source/org/zfin/db/postGmakePostloaddb/1114/CUR-960.sql
@@ -0,0 +1,32 @@
+--liquibase formatted sql
+--changeset pm:CUR-960
+
+
+
+update construct set construct_name=replace(construct_name,'cxxc1:RFP','opn1lw2:DsRedx') where construct_name like '%cxxc1:RFP%';
+update marker set mrkr_name=replace(mrkr_name,'cxxc1:RFP','opn1lw2:DsRedx') where mrkr_name like '%cxxc1:RFP%';
+update marker set mrkr_abbrev=replace(mrkr_name,'cxxc1:RFP','opn1lw2:DsRedx') where mrkr_name like '%cxxc1:RFP%';
+
+
+update construct_component set cc_component='opn1lw2' where cc_component='cxxc1' and cc_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%';
+update construct_component set cc_component='DsRedx' where cc_component='RFP' and cc_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%';
+update construct_component set cc_component_zdb_id='ZDB-GENE-040718-141'  where cc_component='opn1lw2' and cc_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%';
+update construct_component set cc_component_zdb_id='ZDB-EFG-081029-1'  where cc_component='DsRedx' and cc_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%';
+
+
+delete from marker_relationship where mrel_mrkr_2_zdb_id='ZDB-GENE-030728-4' and mrel_mrkr_1_zdb_id like 'ZDB-TGCONSTRCT-110519-%';
+delete from construct_marker_relationship where conmrkrrel_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%' and conmrkrrel_mrkr_zdb_id='ZDB-GENE-030728-4';
+update marker_relationship set mrel_mrkr_2_zdb_id='ZDB-EFG-081029-1' where mrel_mrkr_1_zdb_id like 'ZDB-TGCONSTRCT-110519-%' and mrel_mrkr_2_zdb_id ='ZDB-EFG-070117-3';
+update construct_marker_relationship set conmrkrrel_mrkr_zdb_id='ZDB-EFG-081029-1' where conmrkrrel_construct_zdb_id like 'ZDB-TGCONSTRCT-110519-%' and conmrkrrel_mrkr_zdb_id ='ZDB-EFG-070117-3';
+
+
+
+
+
+
+
+
+
+
+
+
