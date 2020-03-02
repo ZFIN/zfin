1db9dbb1ee2cd8eb96228efa53db828fcee64f31
informix <informix@zfin.org>
2020-03-02 08:59:49 -0800

    CUR-952 fix order of SQL queries (Christian)
    (cherry picked from commit a91797bf58b1edb88fbe74e6fa4b4affa7485858)

--- source/org/zfin/db/postGmakePostloaddb/1113/CUR-952.sql
+++ source/org/zfin/db/postGmakePostloaddb/1113/CUR-952.sql
@@ -3,7 +3,7 @@
 --changeset cmpich:CUR-952
 
 
-Delete from fish_experiment  where not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_ID);
-Delete  from expression_experiment2 where exists (select * from fish_experiment where genox_zdb_id = xpatex_genox_zdb_id AND  not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_id));
-Delete from phenotype_experiment where exists (select * from fish_experiment where phenox_genox_zdb_id = genox_zdb_id and not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_id));
 delete from phenotype_statement where exists (select * From phenotype_experiment where phenos_Phenox_pk_id = phenox_pk_id and exists (select * from fish_experiment where phenox_genox_zdb_id = genox_zdb_id and not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_id)));
+Delete from phenotype_experiment where exists (select * from fish_experiment where phenox_genox_zdb_id = genox_zdb_id and not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_id));
+Delete  from expression_experiment2 where exists (select * from fish_experiment where genox_zdb_id = xpatex_genox_zdb_id AND  not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_id));
+Delete from fish_experiment  where not exists (select * from experiment where exp_zdb_id = genox_exp_zdb_ID);
