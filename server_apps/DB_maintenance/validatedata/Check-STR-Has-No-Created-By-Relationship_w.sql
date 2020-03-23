7770e357924b696f8f1e85b6d9bb6779cdb3a625
Prita Mani <pm@zfin.org>
2020-03-23 10:35:55 -0700

    JENK-487 altered constraint

--- server_apps/DB_maintenance/validatedata/Check-STR-Has-No-Created-By-Relationship_w.sql
+++ server_apps/DB_maintenance/validatedata/Check-STR-Has-No-Created-By-Relationship_w.sql
@@ -1,8 +1,7 @@
 SELECT feature_zdb_id,
   feature_name
 FROM   feature
-WHERE  feature_type = 'INDEL'
-       AND NOT EXISTS (SELECT 'x'
+       WHERE NOT EXISTS (SELECT 'x'
                        FROM   feature_marker_relationship
                        WHERE  fmrel_ftr_zdb_id = feature_zdb_id
                               AND fmrel_type = 'created by')
@@ -10,4 +9,4 @@ WHERE  feature_type = 'INDEL'
                    FROM   feature_assay
                    WHERE  featassay_feature_zdb_id = feature_zdb_id
                           AND ( featassay_mutagen = 'TALEN'
-                                OR featassay_mutagen = 'CRISPR' ))
+                                OR featassay_mutagen = 'CRISPR' )) order by feature_zdb_id;
