b57fba86805574ab96d8518ebc769f232ef80d86
Patrick Kalita <pkalita@zfin.org>
2020-03-27 15:21:16 -0700

    Use environment vaiable for email address

--- home/javascript/react/components/GenericErrorMessage.js
+++ home/javascript/react/components/GenericErrorMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const GenericErrorMessage = () => {
     return (
         <span className='text-danger'>
-            Something went wrong fetching data. Try again later or <a href='mailto:@ZFIN_ADMIN@'>contact us</a>.
+            Something went wrong fetching data. Try again later or <a href={`mailto:${process.env.ZFIN_ADMIN}`}>contact us</a>.
         </span>
     );
 };
