62cf04bc7443f2b2f4d26d1e4ad2d9edc3d476e1
Patrick Kalita <pkalita@zfin.org>
2020-03-30 11:47:42 -0700

    Fix eslint warnings

--- home/javascript/react/components/StagePresentation.js
+++ home/javascript/react/components/StagePresentation.js
@@ -1,18 +1,23 @@
 import React from 'react';
 
 const StagePresentation = ({stages}) => {
-    return (<div>
+    return (
+        <div>
             {stages && Object.entries(stages).map(([key, value]) => {
                 if (value === 'stage-selected') {
-                    return < div className='ontology-ribbon__item' title={key}>
-                        <a href={''}>
-                            <div className={`ontology-ribbon__block__tile ${value}`}/>
-                        </a>
-                    </div>
+                    return (
+                        <div className='ontology-ribbon__item' title={key}>
+                            <a href={''}>
+                                <div className={`ontology-ribbon__block__tile ${value}`}/>
+                            </a>
+                        </div>
+                    );
                 } else {
-                    return < div className='ontology-ribbon__item' title={key}>
-                        <div className={`ontology-ribbon__block__tile ${value}`}/>
-                    </div>
+                    return (
+                        <div className='ontology-ribbon__item' title={key}>
+                            <div className={`ontology-ribbon__block__tile ${value}`}/>
+                        </div>
+                    );
                 }
             })
             }
