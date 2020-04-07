c185ba6aa2a34d92039d7e88f46ad1a3b4130e34
Prita Mani <pm@zfin.org>
2020-04-07 11:29:07 -0700

    JENK-490 altered to include deficiencies wit one affected gene
    (cherry picked from commit 0ae5cfa9261c1ec690ec15b8e847ea5217c4fb44)

--- server_apps/DB_maintenance/report_data/Report-Genetic-Interactions.sql
+++ server_apps/DB_maintenance/report_data/Report-Genetic-Interactions.sql
@@ -3,34 +3,54 @@ drop table if exists tmp_fish_thatfitthebill;
 drop table if exists tmp_fishnoeap;
 drop table if exists tmp_onlyfish;
 drop table if exists tmp_fish490;
+drop table if exists tmp_onlydeficiencies;
 
-select distinct fish_zdb_id , fish_name, concat(fc_affector_zdb_id,E'\t',feature_abbrev,E'\t',fc_gene_zdb_id,E'\t',mrkr_abbrev) as affector
-into tmp_onlyfish
-from fish, fish_components, feature,marker
+select fmrel_ftr_zdb_id as featdef
+into tmp_onlydeficiencies
+from feature, feature_marker_relationship, marker
+where feature_type='DEFICIENCY'
+and fmrel_ftr_zdb_id=feature_zdb_id
+and fmrel_mrkr_zdb_id=mrkr_zdb_id
+group by fmrel_ftr_zdb_id having count(fmrel_ftr_zdb_id)=1;
+
+select distinct fish_zdb_id, fish_name, concat(feature_zdb_id,E'\t',feature_abbrev,E'\t',mrkr_zdb_id,E'\t',mrkr_abbrev,E'\t',feature_type) as affector
+into temp tmp_onlyfish
+from fish, genotype_Feature, feature_marker_relationship, feature,marker
 where fish_functional_affected_gene_count=2
-and fish_zdb_id=fc_fish_zdb_id
-and fc_affector_zdb_id like 'ZDB-ALT%'
-and fc_affector_zdb_id = feature_zdb_id
-and feature_type in ('POINT_MUTATION','COMPLEX','DELETION','INSERTION','INDEL')
-and fc_gene_zdb_id=mrkr_zdb_id
+and fish_genotype_zdb_id = genofeat_geno_zdb_id
+and genofeat_feature_Zdb_id = fmrel_Ftr_zdb_id
+and feature_zdb_id = fmrel_ftr_zdb_id
+and fmrel_mrkr_zdb_id = mrkr_zdb_id
+and feature_type in ('POINT_MUTATION','COMPLEX','DELETION','INSERTION','INDEL');
 union
-select distinct fish_zdb_id , fish_name, concat(fc_affector_zdb_id,E'\t',fc_gene_zdb_id) as affector
-from fish, fish_components, feature, feature_marker_relationship
+select distinct fish_zdb_id, fish_name, concat(featdef,E'\t',feature_abbrev,E'\t',mrkr_zdb_id,E'\t',mrkr_abbrev,E'\t',feature_type) as affector
+from fish, genotype_Feature, feature_marker_relationship, feature,marker,tmp_onlydeficiencies
 where fish_functional_affected_gene_count=2
-and fish_zdb_id=fc_fish_zdb_id
-and fc_affector_zdb_id like 'ZDB-ALT%'
-and fc_affector_zdb_id = feature_zdb_id
+and fish_genotype_zdb_id = genofeat_geno_zdb_id
+and genofeat_feature_Zdb_id = featdef
+and feature_zdb_id = featdef
+and featdef=fmrel_ftr_zdb_id
+and fmrel_mrkr_zdb_id = mrkr_zdb_id
+union
+select distinct fish_zdb_id , fish_name,  concat(feature_zdb_id,E'\t',feature_abbrev,E'\t',mrkr_zdb_id,E'\t',mrkr_abbrev,E'\t',feature_type) as affector
+from fish, genotype_Feature, feature_marker_relationship, feature,marker
+where fish_functional_affected_gene_count=2
+and fish_genotype_zdb_id = genofeat_geno_zdb_id
+and genofeat_feature_Zdb_id = fmrel_Ftr_zdb_id
+and feature_zdb_id = fmrel_ftr_zdb_id
+and fmrel_mrkr_zdb_id = mrkr_zdb_id
 and feature_type in ('TRANSGENIC_INSERTION')
 and fmrel_type='contains innocuous sequence feature'
-and fmrel_ftr_zdb_id=fc_affector_zdb_id
 union
-select distinct fish_zdb_id , fish_name, concat(fc_affector_zdb_id,E'\t',a.mrkr_abbrev,E'\t',fc_gene_zdb_id,E'\t',b.mrkr_abbrev) as affector
-from fish, fish_components, marker a, marker b
+select distinct fish_zdb_id , fish_name, concat(fishstr_str_zdb_id,E'\t',a.mrkr_abbrev,E'\t',b.mrkr_zdb_id,E'\t',b.mrkr_abbrev) as affector
+from fish, fish_str, marker a, marker b, marker_relationship
 where fish_functional_affected_gene_count=2
-and fish_zdb_id=fc_fish_zdb_id
-and (fc_affector_zdb_id like 'ZDB-MRPH%' or fc_affector_zdb_id like 'ZDB-TALEN%' or fc_affector_zdb_id like 'ZDB-CRISPR%')
-and a.mrkr_zdb_id=fc_affector_zdb_id
-and b.mrkr_zdb_id=fc_gene_zdb_id;
+and fish_zdb_id=fishstr_fish_zdb_id
+and fishstr_str_zdb_id=a.mrkr_zdb_id
+and fishstr_str_zdb_id=mrel_mrkr_1_zdb_id
+and mrel_mrkr_2_zdb_id=b.mrkr_zdb_id
+and mrel_type='knockdown reagent targets gene';
+
 
 
 select fish_zdb_id, fish_name, string_agg(affector,',') as affected_components into tmp_fish490 from tmp_onlyfish group by fish_zdb_id, fish_name;
