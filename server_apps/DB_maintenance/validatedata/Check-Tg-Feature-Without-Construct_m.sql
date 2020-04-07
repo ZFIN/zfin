82567ded55648d98f6615f1d444cf8c06996358f
Prita Mani <pm@zfin.org>
2020-04-07 08:30:12 -0700

    JENK-489 excluded nomenclature pubs

--- server_apps/DB_maintenance/validatedata/Check-Tg-Feature-Without-Construct_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Tg-Feature-Without-Construct_m.sql
@@ -1,8 +1,8 @@
-select
+select distinct
    feature_zdb_id,
    feature_name   
 from
-   feature   
+   feature, record_attribution
 where
    not exists (
       Select
@@ -13,4 +13,4 @@ where
          fmrel_ftr_zdb_id = feature_zdb_id  
          and fmrel_type like 'contains%'
    )  
-   and feature_Type = 'TRANSGENIC_INSERTION'  ;
+   and feature_Type = 'TRANSGENIC_INSERTION' and feature_zdb_id=recattrib_data_zdb_id and recattrib_source_zdb_id not in ('ZDB-PUB-190102-5','ZDB-PUB-200102-5','ZDB-PUB-170103-4','ZDB-PUB-180105-5') ;
