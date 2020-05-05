71ceae547386594db6cae2dd6a9e7b8711364b7e
Prita Mani <pm@zfin.org>
2020-05-05 11:19:21 -0700

    ZFIN-6658 phenotype figure gallery

--- home/javascript/react/components/FigureGalleryPhenotypeDetails.js
+++ home/javascript/react/components/FigureGalleryPhenotypeDetails.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { fetchType } from '../utils/types';
 import LoadingSpinner from './LoadingSpinner';
 import CommaSeparatedList from './CommaSeparatedList';
-// import StageLink from './StageLink';
+import OverflowParagraph from './OverflowParagraph';
+import StageLink from './StageLink';
 
 
 const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
@@ -14,16 +15,16 @@ const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
         return null;
     }
 
-    const { expression } = figureDetails.value;
+    const {details, phenotype} = figureDetails.value;
 
     return (
         <dl className='d-sm-table mt-2 mb-0'>
-            {expression.fish.length > 0 &&
+            {phenotype.fish.length > 0 &&
                 <div className='d-sm-table-row'>
                     <dt className='d-sm-table-cell nowrap'>Fish</dt>
                     <dd className='my-sm-0 mx-sm-2'>
                         <CommaSeparatedList>
-                            {expression.fish.map(fish => (
+                            {phenotype.fish.map(fish => (
                                 <a href={`/${fish.zdbID}`} dangerouslySetInnerHTML={{__html: fish.displayName}} key={fish.zdbID} />
                             ))}
                         </CommaSeparatedList>
@@ -31,13 +32,13 @@ const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
                 </div>
             }
 
-            {expression.experiments.length > 0 &&
+            {phenotype.experiments.length > 0 &&
                 <div className='d-sm-table-row'>
                     <dt className='d-sm-table-cell nowrap'>Conditions</dt>
                     <dd className='my-sm-0 mx-sm-2'>
                         <CommaSeparatedList semicolon>
-                            {expression.experiments.map(experiment => (
-                                <a href={`/action/expression/experiment?id=${experiment.zdbID}`} key={experiment.zdbID}>
+                            {phenotype.experiments.map(experiment => (
+                                <a href={`/action/phenotype/experiment?id=${experiment.zdbID}`} key={experiment.zdbID}>
                                     {experiment.experimentConditions
                                         .map(condition => condition.displayName)
                                         .join(', ')
@@ -49,12 +50,12 @@ const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
                 </div>
             }
 
-            {expression.sequenceTargetingReagents.length > 0 &&
+            {phenotype.sequenceTargetingReagents.length > 0 &&
                 <div className='d-sm-table-row'>
                     <dt className='d-sm-table-cell nowrap'>Knockdown Reagents</dt>
                     <dd className='my-sm-0 mx-sm-2'>
                         <CommaSeparatedList>
-                            {expression.sequenceTargetingReagents.map(str => (
+                            {phenotype.sequenceTargetingReagents.map(str => (
                                 <a href={`/${str.zdbID}`} key={str.zdbID}>{str.abbreviation}</a>
                             ))}
                         </CommaSeparatedList>
@@ -62,12 +63,12 @@ const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
                 </div>
             }
 
-            {expression.entities.length > 0 &&
+            {phenotype.entities.length > 0 &&
                 <div className='d-sm-table-row'>
                     <dt className='d-sm-table-cell nowrap'>Anatomical Terms</dt>
                     <dd className='my-sm-0 mx-sm-2'>
                         <CommaSeparatedList>
-                            {expression.entities.map(entity => {
+                            {phenotype.entities.map(entity => {
                                 if (entity.subterm) {
                                     return (
                                         <a href={`/action/ontology/post-composed-term-detail?superTermID=${entity.superterm.oboID}&subTermID=${entity.subterm.oboID}`} key={`${entity.superterm.oboID}-${entity.subterm.oboID}`}>
@@ -87,20 +88,32 @@ const FigureGalleryPhenotypeDetails = ({figureDetails}) => {
                 </div>
             }
 
-            {/*<div className='d-sm-table-row'>
+            {<div className='d-sm-table-row'>
                 <dt className='d-sm-table-cell nowrap'>
-                    {expression.startStage.zdbID === expression.endStage.zdbID ?
+                    {phenotype.startStage.zdbID === phenotype.endStage.zdbID ?
                         'Stage' :
                         'Stage Range'
                     }
                 </dt>
                 <dd className='my-sm-0 mx-sm-2'>
-                    <StageLink stage={expression.startStage} /> {expression.startStage.zdbID !== expression.endStage.zdbID &&
+                    <StageLink stage={phenotype.startStage} /> {phenotype.startStage.zdbID !== phenotype.endStage.zdbID &&
                     <React.Fragment>
-                        to <StageLink stage={expression.endStage} />
+                        to <StageLink stage={phenotype.endStage} />
                     </React.Fragment>}
                 </dd>
-            </div>*/}
+            </div>}
+
+
+            {details &&
+            <div className='d-sm-table-row'>
+                <dt className='d-sm-table-cell nowrap'>Caption</dt>
+                <dd className='my-sm-0 mx-sm-2'>
+                    <OverflowParagraph>
+                        <div dangerouslySetInnerHTML={{__html: details}}/>
+                    </OverflowParagraph>
+                </dd>
+            </div>
+            }
 
 
 
