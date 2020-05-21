bdaadc8a6c1da367e0231cd37411a7e6358d68ea
Prita Mani <pm@zfin.org>
2020-05-21 08:48:52 -0700

    JENK-505 fixed sql statement

--- server_apps/DB_maintenance/validatedata/Check-MNVFeatures_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-MNVFeatures_m.sql
@@ -1,5 +1,5 @@
 SELECT feature_zdb_id, feature_abbrev,recattrib_source_zdb_id
-FROM   distinct feature, record_attribution
+FROM   feature, record_attribution
         WHERE  feature_type='MNV'
         and feature_zdb_id=recattrib_data_zdb_id
         order by feature_zdb_id;
