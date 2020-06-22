2e5ddd27b24c480b7c6d4a5a43fdcd62feff3084
Prita Mani <pm@zfin.org>
2020-06-22 12:44:28 -0700

    ZFIN-6541

--- home/javascript/react/components/DataTableSummaryToggle.js
+++ home/javascript/react/components/DataTableSummaryToggle.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DataTableSummaryToggle = ({detailLabel, value, onChange}) => {
+const DataTableSummaryToggle = ({detailLabel, showPopup, onChange}) => {
     return (
         <div className='mb-2'>
             <div className='btn-group btn-group-sm' role='group'>
                 <button
                     type='button'
-                    className={'btn btn-outline-secondary' + (value ? ' active' : '')}
+                    className={'btn btn-outline-secondary' + (showPopup ? ' active' : '')}
                     onClick={() => onChange(true)}
                 >
                     Overview
                 </button>
                 <button
                     type='button'
-                    className={'btn btn-outline-secondary' + (!value ? ' active' : '')}
+                    className={'btn btn-outline-secondary' + (!showPopup ? ' active' : '')}
                     onClick={() => onChange(false)}
                 >
                     {detailLabel}
@@ -27,7 +27,7 @@ const DataTableSummaryToggle = ({detailLabel, value, onChange}) => {
 DataTableSummaryToggle.propTypes = {
     detailLabel: PropTypes.node,
     onChange: PropTypes.func,
-    value: PropTypes.bool,
+    showPopup: PropTypes.bool,
 };
 
 export default DataTableSummaryToggle;
