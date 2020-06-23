83a61042a49093152bcc466231ee850e926bda29
Prita Mani <pm@zfin.org>
2020-06-23 09:01:34 -0700

    ZFIN-6746 fixed path of file

--- server_apps/data_transfer/RNACentral/loadTranscriptSequences.sql
+++ server_apps/data_transfer/RNACentral/loadTranscriptSequences.sql
@@ -3,7 +3,7 @@
 begin work;
 create  table tscriptsequence (tscriptid text, tscriptottdart text, tscriptseq text );
 
-\copy tscriptsequence from '/opt/zfin/www_homes/polka/server_apps/data_transfer/RNACentral/loadedSeq.txt' delimiter E'\t';
+\copy tscriptsequence from '<!--|ROOT_PATH|-->/server_apps/data_transfer/RNACentral/loadedSeq.txt' delimiter E'\t';
 insert into transcript_sequence (select tscriptid, tscriptottdart, substring(tscriptseq from 2 for length(tscriptseq))  from tscriptsequence where tscriptseq!='') ;
 
 commit work;
