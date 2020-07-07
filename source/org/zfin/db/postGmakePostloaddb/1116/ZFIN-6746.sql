a03f7dd47b1f6945e87ec5a7bfb2676b2d6c51c3
Prita Mani <pm@zfin.org>
2020-07-07 05:35:54 -0700

    ZFIN-6746 added SO ids for all transcript types

---
+++ source/org/zfin/db/postGmakePostloaddb/1116/ZFIN-6746.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+--changeset pm:ZFIN-6746.sql
+
+
+
+ update transcript_type set tscriptt_so_id='SO:0001906' where tscriptt_type='disrupted domain';
+ update transcript_type set tscriptt_so_id='SO:0001833' where tscriptt_type='V-gene';
+ update transcript_type set tscriptt_so_id='SO:0000101' where tscriptt_type='transposable element';
+ update transcript_type set tscriptt_so_id='SO:0000673' where tscriptt_type='transcript';
+ update transcript_type set tscriptt_so_id='SO:0000644' where tscriptt_type='antisense';
+ 
+
