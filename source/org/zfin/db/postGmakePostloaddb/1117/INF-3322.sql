352d4c072bbd399b54d8506669e8056289f7bd28
Sierra Moxon <smoxon@zfin.org>
2020-08-03 08:01:03 -0700

    INF-3322

--- source/org/zfin/db/postGmakePostloaddb/1117/INF-3322.sql
+++ source/org/zfin/db/postGmakePostloaddb/1117/INF-3322.sql
@@ -1,6 +1,8 @@
 --liquibase formatted sql
 --changeset sierra:INF-3322.sql
 
+DROP FUNCTION update_geno_sort_order(text);
+
 update genotype
   set geno_complexity_order = geno_complexity_order
- where geno_complexity_order != update_geno_sort_order(geno_zdb_id);
+ where geno_complexity_order <> update_geno_sort_order(geno_zdb_id);
