4f23b154fa347b129fa8f2197f9cfa241a1d0323
Patrick Kalita <pkalita@zfin.org>
2020-08-06 09:42:03 -0700

    ZFIN-6808 Only call scrollspy on data pages

--- home/javascript/zfin-common/data-page.js
+++ home/javascript/zfin-common/data-page.js
@@ -4,15 +4,17 @@ $(function () {
         .on('click', function () {
             this.blur();
         });
-    $('body.data-page')
-        .scrollspy({
-            target: '.data-page-nav-container',
-            offset: 120,
-        });
-    // event fires on window, not body. see https://github.com/twbs/bootstrap/issues/20086
-    $(window)
-        .on('activate.bs.scrollspy', function (event, { relatedTarget }) {
-            history.replaceState(null, '', relatedTarget);
-        });
-
+    const $body = $('body');
+    if ($body.hasClass('data-page')) {
+        $body
+            .scrollspy({
+                target: '.data-page-nav-container',
+                offset: 120,
+            });
+        // event fires on window, not body. see https://github.com/twbs/bootstrap/issues/20086
+        $(window)
+            .on('activate.bs.scrollspy', function (event, { relatedTarget }) {
+                history.replaceState(null, '', relatedTarget);
+            });
+    }
 });
