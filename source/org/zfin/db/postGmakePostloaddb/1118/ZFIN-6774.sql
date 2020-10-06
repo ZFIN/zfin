b6f6f791c1694a714f6fa5619849821b2f35181c
Prita Mani <pm@zfin.org>
2020-10-06 11:52:29 -0700

    ZFIN-6774

--- source/org/zfin/db/postGmakePostloaddb/1118/ZFIN-6774.sql
+++ source/org/zfin/db/postGmakePostloaddb/1118/ZFIN-6774.sql
@@ -6,12 +6,13 @@ insert into external_note_type(extntype_name) values ('feature and variant');
 alter table tmp_ftrnote add fgmdid varchar(50);
 update tmp_ftrnote set fgmdid=(select fgmd_zdb_id from feature_genomic_mutation_detail where fgmd_feature_zdb_id=featureid);
 
-
 alter table tmp_ftrnote add extnoteid varchar(50);
 --update tmp_ftrnote set extnoteid = (select distinct extnote_zdb_id from external_note where extnote_data_zdb_id=featureid);
 
 --update external_note set extnote_data_zdb_id=(select fgmdid from tmp_ftrnote where variantnote='variant' and allelenote ='' and fgmdid !='') from tmp_ftrnote where extnote_note_type='feature' and extnote_data_zdb_id=featureid;
 update external_note set extnote_note_type='variant' from tmp_ftrnote where variantnote='variant' and allelenote ='' and fgmdid ='' and extnote_data_zdb_id=featureid;
+update external_note set extnote_note_type='variant' from tmp_ftrnote where variantnote='variant' and allelenote ='' and fgmdid!='' and extnote_data_zdb_id=featureid;
+
 
 drop table if exists fgmdnote;
 create table fgmdnote (
@@ -29,4 +30,7 @@ alter table fgmdnote add fgmdnote_zdb_id varchar(50);
 update fgmdnote set fgmdnote_zdb_id=get_id('EXTNOTE');
 insert into zdb_active_data select fgmdnote_zdb_id from fgmdnote;
 insert into external_note (extnote_zdb_id, extnote_data_zdb_id,extnote_note,extnote_source_zdb_id,extnote_note_type) select fgmdnote_zdb_id,fgmdzdb,fgmdnote,fgmdref,fgmdtype from fgmdnote;
+delete from external_note where extnote_note_type ='variant' and extnote_data_zdb_id like 'ZDB-ALT%' ;
+
+rollback work;
 
