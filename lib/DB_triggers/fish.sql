4425bb98856bb45e13ad2803e90fdec14a4eb178
Sierra Moxon <smoxon@zfin.org>
2020-10-09 08:54:13 -0700

    ZFIN-6826

--- lib/DB_triggers/fish.sql
+++ lib/DB_triggers/fish.sql
@@ -4,29 +4,29 @@ DROP TRIGGER IF EXISTS fish_affected_trigger
 ON fish;
 drop trigger if exists fish_after
 on fish;
+DROP TRIGGER IF EXISTS fish_pheno_count_trigger
+on FISH;
 
 
 CREATE OR REPLACE FUNCTION fish_after()
   RETURNS trigger AS $BODY$
 
-begin                                         
-                     update fish                                    
-                     set fish_name = get_fish_full_name(NEW.fish_Zdb_id,NEW.fish_genotype_zdb_id, get_fish_name(NEW.fish_zdb_id))
+  BEGIN                                        
+    update fish                                    
+      set fish_name = get_fish_full_name(NEW.fish_Zdb_id,NEW.fish_genotype_zdb_id, get_fish_name(NEW.fish_zdb_id))
                       where fish_zdb_id = NEW.fish_zdb_id;          
-			
-		     update fish
-		      set fish_phenotypic_construct_count = (Select count(fmrel_ftr_zdb_id) from
-			genotype_feature, feature_marker_relationship
-			where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
-			and fmrel_type = 'contains phenotypic sequence feature'
-			and fmrel_ftr_zdb_id = genofeat_feature_zdb_id)
-		      where exists (select 'x' from genotype_feature, feature_marker_relationship
-                        where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
-                        and fmrel_type = 'contains phenotypic sequence feature'
-                        and fmrel_ftr_zdb_id = genofeat_feature_zdb_id);
+    update fish 
+      set fish_phenotypic_construct_count = (Select count(fmrel_ftr_zdb_id) from genotype_feature, feature_marker_relationship
+     						 where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
+      						  and fmrel_type = 'contains phenotypic sequence feature'
+      						  and fmrel_ftr_zdb_id = genofeat_feature_zdb_id)
+      where exists (select 'x' from genotype_feature, feature_marker_relationship
+                        	where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
+                        	 and fmrel_type = 'contains phenotypic sequence feature'
+                        	 and fmrel_ftr_zdb_id = genofeat_feature_zdb_id);
                                                                     
-                     return new;                                    
-                     end;     
+    return new;                                    
+   END;     
 
 $BODY$ LANGUAGE plpgsql;
 
@@ -39,28 +39,49 @@ BEGIN
 END;
 $BODY$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION fish_affected()
-  RETURNS trigger AS $BODY$
-BEGIN
+CREATE OR REPLACE FUNCTION fish_pheno_count()
+  RETURNS trigger as $BODY$ 
 
+  BEGIN
 
-  select * FROM getFishOrder(NEW.fish_zdb_id,NEW.fish_genotype_zdb_id)
- INTO NEW.fish_order,NEW.fish_functional_affected_gene_count;
--- raise notice 'end: %', NEW.fish_functional_affected_gene_count;
+  Select count(fmrel_ftr_zdb_id) from
+                        genotype_feature, feature_marker_relationship
+                        where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
+                         and fmrel_type = 'contains phenotypic sequence feature'
+                         and fmrel_ftr_zdb_id = genofeat_feature_zdb_id
+                         and exists (select 'x' from genotype_feature, feature_marker_relationship
+                        		where genofeat_geno_zdb_id = NEW.fish_genotype_zdb_id
+                        		and fmrel_type = 'contains phenotypic sequence feature'
+                        		and fmrel_ftr_zdb_id = genofeat_feature_zdb_id) 
+      into NEW.fish_phenotypic_construct_count;
 
--- get_genbank_dblink_length_type
-     RETURN NEW;
+RETURN NEW;
 END;
 $BODY$ LANGUAGE plpgsql;
 
+CREATE OR REPLACE FUNCTION fish_affected()
+  RETURNS trigger AS $BODY$
+
+  BEGIN
+    SELECT * FROM getFishOrder(NEW.fish_zdb_id,NEW.fish_genotype_zdb_id)
+       INTO NEW.fish_order,NEW.fish_functional_affected_gene_count;
+       RETURN NEW;
+  END;
+$BODY$ LANGUAGE plpgsql;
+
 CREATE TRIGGER fish_trigger
-BEFORE  INSERT OR UPDATE  ON fish
-FOR EACH ROW EXECUTE PROCEDURE fish_trigger();
+  BEFORE  INSERT OR UPDATE  ON fish
+  FOR EACH ROW EXECUTE PROCEDURE fish_trigger();
 
 CREATE TRIGGER fish_affected_trigger
-BEFORE  INSERT OR UPDATE  ON fish
-FOR EACH ROW EXECUTE PROCEDURE fish_affected();
+  BEFORE  INSERT OR UPDATE  ON fish
+  FOR EACH ROW EXECUTE PROCEDURE fish_affected();
+
+CREATE TRIGGER fish_pheno_count_trigger
+  BEFORE INSERT OR UPDATE ON fish
+  FOR EACH ROW EXECUTE PROCEDURE fish_pheno_count();
 
 CREATE TRIGGER fish_after
-AFTER  INSERT  ON fish
-FOR EACH ROW EXECUTE PROCEDURE fish_after();
+  AFTER INSERT on fish
+  FOR EACH ROW EXECUTE PROCEDURE fish_after();
+
