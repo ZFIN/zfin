ce02ba5c280aa7b3e03119af910dabe88311fe8f
Prita Mani <pm@zfin.org>
2020-10-16 10:03:52 -0700

    PUB-681 removed EFG's from list of genes

--- source/org/zfin/marker/presentation/PublicationPrioritizationController.java
+++ source/org/zfin/marker/presentation/PublicationPrioritizationController.java
@@ -42,7 +42,7 @@ public class PublicationPrioritizationController {
     public JsonResultResponse<Prioritization> getGenePubPrioritization(@PathVariable String publicationId,
                                                                        @Version Pagination pagination) {
 
-        List<Marker> attributedMarker = getPublicationRepository().getGenesByPublication(publicationId);
+        List<Marker> attributedMarker = getPublicationRepository().getGenesByPublication(publicationId,false);
         Map<Marker, Boolean> isNewGeneMap = getPublicationRepository().areNewGenePubAttribution(attributedMarker, publicationId);
         List<Prioritization> prioList = attributedMarker.stream()
                 .map(marker -> {
