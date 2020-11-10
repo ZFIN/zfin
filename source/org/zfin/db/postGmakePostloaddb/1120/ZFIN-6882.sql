ce030c1023ca58fdd1d36b9e3d7265acbdb5d4ca
Prita Mani <pm@zfin.org>
2020-11-10 11:11:14 -0800

    ZFIN-6882

--- source/org/zfin/db/postGmakePostloaddb/1120/ZFIN-6882.sql
+++ source/org/zfin/db/postGmakePostloaddb/1120/ZFIN-6882.sql
@@ -3,7 +3,7 @@
 --fixing ENSG ids for human orthologs (work initially done as a part of ZFIN-6604)
 
 --table tmp_ortholog_reference created as a part of ZFIN-6604
-delete * from tmp_ortholog_external_reference  where accession like 'ENSG%';
+delete  from tmp_ortholog_external_reference  where accession like 'ENSG%';
 
 insert into tmp_ortholog_external_reference
 select distinct oef_ortho_zdb_id, ensembl_id,
