dfc1d9fbedfe6355c36a85ec659e43ad95ef83fc
Sierra Moxon <smoxon@zfin.org>
2020-11-19 10:47:59 -0800

    ZFIN-6908

--- lib/DB_functions/updatestandardorgenericcontrol.sql
+++ lib/DB_functions/updatestandardorgenericcontrol.sql
@@ -4,22 +4,23 @@ returns boolean as $true$
   declare stdGc boolean := 'f';
   begin
 
-  if exists (Select 'x' from experiment
-                      where exp_zdb_id = vExpZdbId
-                      and exp_name in ('_Standard', '_Generic_control') 
-               union
-	       select 't'
+  if exists (select 'x'
+               from experiment
+               where exp_zdb_id = vExpZdbId
+               and exp_name in ('_Standard', '_Generic_control')
+              union
+	            select 't'
 	       	      from experiment
-		      where not exists (Select 'x' from experiment_condition
-		      	    	       	       where expcond_exp_zdb_id = exp_zdb_id)
-					       and exp_Zdb_id = vExpZdbId
-
-   union
-	       select 't'
+		            where not exists (Select 'x'
+		                                 from experiment_condition
+		      	    	       	           where expcond_exp_zdb_id = exp_zdb_id)
+					                           and exp_Zdb_id = vExpZdbId
+              union
+	            select 't'
 	       	      from experiment_condition, term
-		      where term_zdb_id = expcond_zeco_term_zdb_id
-		      and term_ont_id in ('ZECO:0000103','ZECO:0000102')
-		      and expcond_exp_zdb_id = vExpZdbId)
+		            where term_zdb_id = expcond_zeco_term_zdb_id
+		            and term_ont_id in ('ZECO:0000103','ZECO:0000102')
+		            and expcond_exp_zdb_id = vExpZdbId)
   
   then 
       stdGc = 't';
