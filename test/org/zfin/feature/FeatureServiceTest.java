569772000f5e517fe134d8b93ffbb93628b7c712
Prita Mani <pm@zfin.org>
2020-11-30 08:42:45 -0800

    fixed test

--- test/org/zfin/feature/FeatureServiceTest.java
+++ test/org/zfin/feature/FeatureServiceTest.java
@@ -20,6 +20,7 @@ import java.util.Set;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertNotNull;
+import org.junit.Ignore;
 
 public class FeatureServiceTest extends AbstractDatabaseTest {
 
