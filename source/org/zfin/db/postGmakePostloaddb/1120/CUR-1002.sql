fc39e9c465feb6182b177dcb8f6841bdd49937f2
Prita Mani <pm@zfin.org>
2020-12-21 13:33:36 -0800

    CUR-1002 added a couple more SQLs for merge

--- source/org/zfin/db/postGmakePostloaddb/1120/CUR-1002.sql
+++ source/org/zfin/db/postGmakePostloaddb/1120/CUR-1002.sql
@@ -6,41 +6,50 @@
 
 update marker set mrkr_comments = 'tdEosFP is a pseudomonomeric variant in which two copies of an engineered EosFP variant are fused to form a tandem dimer
 
-Eos is a photo-convertible coral fluorescent protein. tdEosFP/tdEos is a pseudomonomeric variant in which two copies of an engineered EosFP variant are fused to form a tandem dimer' where mrkr_zdb_id = 'ZDB-EFG-121024-2';
+Eos is a photo-convertible coral fluorescent protein. tdEosFP/tdEos is a pseudomonomeric variant in which two copies of an engineered EosFP variant are fused to form a tandem dimer' where mrkr_zdb_id = 'ZDB-EFG-110721-1';
 
-delete from record_attribution where recattrib_pk_id = '101868390';
 
-delete from marker_relationship where mrel_zdb_id = 'ZDB-MREL-140430-2';
 
-delete from marker_relationship where mrel_zdb_id = 'ZDB-MREL-140430-3';
 
 update construct_marker_relationship
-                                set conmrkrrel_mrkr_zdb_id = 'ZDB-EFG-121024-2'
-                              where conmrkrrel_mrkr_zdb_id = 'ZDB-EFG-110721-1';
+                                set conmrkrrel_mrkr_zdb_id = 'ZDB-EFG-110721-1'
+                              where conmrkrrel_mrkr_zdb_id = 'ZDB-EFG-121024-2';
+
+                              update marker_relationship
+                                set mrel_mrkr_2_zdb_id = 'ZDB-EFG-110721-1'
+                              where mrel_mrkr_2_zdb_id = 'ZDB-EFG-121024-2';
 
 update expression_experiment2
-                                set xpatex_gene_zdb_id = 'ZDB-EFG-121024-2'
-                              where xpatex_gene_zdb_id = 'ZDB-EFG-110721-1';
+                                set xpatex_gene_zdb_id = 'ZDB-EFG-110721-1'
+                              where xpatex_gene_zdb_id = 'ZDB-EFG-121024-2';
 
 update marker_relationship
-                                set mrel_mrkr_2_zdb_id = 'ZDB-EFG-121024-2'
-                              where mrel_mrkr_2_zdb_id = 'ZDB-EFG-110721-1';
+                                set mrel_mrkr_2_zdb_id = 'ZDB-EFG-110721-1'
+                              where mrel_mrkr_2_zdb_id = 'ZDB-EFG-121024-2';
 
-delete from data_alias where dalias_zdb_id = 'ZDB-DALIAS-140430-5';
+
 
 update construct_component
-                                set cc_component_zdb_id = 'ZDB-EFG-121024-2'
-                              where cc_component_zdb_id = 'ZDB-EFG-110721-1';
+                                set cc_component_zdb_id = 'ZDB-EFG-110721-1'
+                              where cc_component_zdb_id = 'ZDB-EFG-121024-2';
 
 update data_alias
-                                set dalias_data_zdb_id = 'ZDB-EFG-121024-2'
-                              where dalias_data_zdb_id = 'ZDB-EFG-110721-1';
+                                set dalias_data_zdb_id = 'ZDB-EFG-110721-1'
+                              where dalias_data_zdb_id = 'ZDB-EFG-121024-2';
 
 
 
 
 
+update record_attribution set recattrib_data_zdb_id = 'ZDB-EFG-110721-1' where recattrib_data_zdb_id = 'ZDB-EFG-121024-2';
 
+delete from zdb_replaced_data where zrepld_old_zdb_id = 'ZDB-EFG-121024-2';
+
+update zdb_replaced_data set zrepld_new_zdb_id = 'ZDB-EFG-110721-1' where zrepld_new_zdb_id = 'ZDB-EFG-121024-2';
+
+delete from zdb_active_data where zactvd_zdb_id = 'ZDB-EFG-121024-2';
+
+insert into zdb_replaced_data (zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-EFG-121024-2', 'ZDB-EFG-110721-1');
 
 
 
