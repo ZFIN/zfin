e24f4b10f9bd9531b977d92a2a7d2a0b9bd0dc39
Prita Mani <pm@zfin.org>
2021-02-16 13:12:15 -0800

    CUR-1006

--- source/org/zfin/db/postGmakePostloaddb/1121/CUR-1006.sql
+++ source/org/zfin/db/postGmakePostloaddb/1121/CUR-1006.sql
@@ -7,7 +7,7 @@
 
 update construct_component set cc_component_zdb_id='ZDB-ENHANCER-201208-1' where cc_component_zdb_id='ZDB-EREGION-160620-1';
 update construct_marker_relationship set conmrkrrel_mrkr_zdb_id='ZDB-ENHANCER-201208-1' where conmrkrrel_mrkr_zdb_id='ZDB-EREGION-160620-1';
-update marker_relationship set mrel_mrkr_2_zdb_id='ZDB-ENHANCER-201208-1' where mrel_mrkr_1_zdb_id='ZDB-EREGION-160620-1';
+update marker_relationship set mrel_mrkr_2_zdb_id='ZDB-ENHANCER-201208-1' where mrel_mrkr_2_zdb_id='ZDB-EREGION-160620-1';
 
 
 create temp table tmp_dalias (aliasid varchar(50), consid varchar(50),consname text);
@@ -20,6 +20,8 @@ insert into data_alias(dalias_zdb_id,dalias_data_zdb_id,dalias_alias,dalias_grou
 
 insert into record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_type) select aliasid,'ZDB-PUB-160407-4', 'standard' from tmp_dalias where consname like '%LLE%';
 insert into zdb_replaced_data(zrepld_old_zdb_id, zrepld_new_zdb_id) values ('ZDB-EREGION-160620-1','ZDB-ENHANCER-201208-1');
+insert into withdrawn_data(wd_old_zdb_id,wd_new_zdb_id) values ('ZDB-EREGION-160620-1','ZDB-ENHANCER-201208-1');
+delete from zdb_active_data where zactvd_zdb_id='ZDB-EREGION-160620-1';
 drop table tmp_dalias;
 
 
