db7d49f033e7f6b82dbeada913546fd0fc5b9640
Sierra Moxon <smoxon@zfin.org>
2021-02-23 10:51:32 -0800

    adding stub script for deleting ZFIN GO annotations in preparation for full ingest from noctua

---
+++ source/org/zfin/db/load/1122/delete-zfin-GO-annotations.sql
@@ -0,0 +1,12 @@
+--liquibase formatted sql
+--changeset sierra:delete-zfin-GO-annotations.sql
+
+delete from marker_go_term_evidence
+  where mrkrgoev_annotation_organization in (1,6)
+  and mrkrgoev_evidence_code != 'IEA';
+
+delete from zdb_active_data where zactvd_zdb_id not in (select mrkrgoev_zdb_id
+ from marker_go_term_evidence where mrkrgoev_zdb_id = zdb_id)
+and zactvd_zdb_id like 'ZDB-MRKRGOEV%';
+
+ 
