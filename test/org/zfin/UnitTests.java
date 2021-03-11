a76f7f6b5b900f93e1abca7f537eca0424823549
Christian Pich <cmpich@zfin.org>
2021-03-11 14:30:02 +0100

    JENK-541 remove jenkins jobs and associated files

--- test/org/zfin/UnitTests.java
+++ test/org/zfin/UnitTests.java
@@ -50,7 +50,6 @@ import org.zfin.profile.OrganizationUrlTest;
 import org.zfin.profile.ProfileUnitTests;
 import org.zfin.profile.service.BeanCompareServiceTest;
 import org.zfin.properties.ZfinPropertiesTest;
-import org.zfin.publication.PubMedValidationReportTest;
 import org.zfin.publication.PublicationTest;
 import org.zfin.security.Md5PasswordEncoderTest;
 import org.zfin.sequence.blast.SequenceTest;
@@ -114,7 +113,6 @@ import org.zfin.util.*;
         PhenotypeStructureTest.class,
         ProfileUnitTests.class,
         PublicationTest.class,
-        PubMedValidationReportTest.class,
         RelationshipTypeSortingTest.class,
         ReportConfigurationTest.class,
         RunCandidatePresentationTest.class,
