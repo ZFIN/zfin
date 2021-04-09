9f6d07ab47f35167f3f0797969e38891a4d5dd40
Christian Pich <cmpich@zfin.org>
2021-04-09 17:14:49 +0200

    JENK-546 add more specific info to error message

--- lib/DB_functions/restrictGAFEntries.sql
+++ lib/DB_functions/restrictGAFEntries.sql
@@ -9,7 +9,7 @@ then
 		  and termsub_subset_id = osubset_pk_id
 		  and osubset_subset_name in ('gocheck_do_not_annotate','gocheck_do_not_manually_annotate'))
   then 
-    raise exception 'FAIL!: GO term can not be in do-not-annotate subset';
+    raise exception 'FAIL!: GO term can not be in do-not-annotate subset (non-IEA) %',vTermZdbId;
   end if;
 end if;
 
@@ -19,8 +19,8 @@ then
    	  	  where termsub_term_zdb_id = vTermZdbId
 		  and termsub_subset_id = osubset_pk_id
 		  and osubset_subset_name in ('gocheck_do_not_annotate'))
-  then 
-    raise exception 'FAIL!: GO term can not be in do-not-annotate subset';
+  then
+      raise exception 'FAIL!: GO term can not be in do-not-annotate subset IEA %',vTermZdbId;
   end if;
 end if;
 end
