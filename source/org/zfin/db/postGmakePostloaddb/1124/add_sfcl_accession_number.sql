525ced5e58c97d0fe6e52118e5e07a3da78fbbdd
Christian Pich <cmpich@zfin.org>
2021-07-12 14:21:09 +0200

    ZFIN-7065 create HGVSg names added to missing features / genes

---
+++ source/org/zfin/db/postGmakePostloaddb/1124/add_sfcl_accession_number.sql
@@ -0,0 +1,103 @@
+--liquibase formatted sql
+--changeset christian:add_sfcl_accession_number.sql
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007112.7'
+ where sfcl_chromosome = '1' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007113.7'
+ where sfcl_chromosome = '2' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007114.7'
+ where sfcl_chromosome = '3' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007115.7'
+ where sfcl_chromosome = '4' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007116.7'
+ where sfcl_chromosome = '5' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007117.7'
+ where sfcl_chromosome = '6' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007118.7'
+ where sfcl_chromosome = '7' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007119.7'
+ where sfcl_chromosome = '8' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007120.7'
+ where sfcl_chromosome = '9' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007121.7'
+ where sfcl_chromosome = '10' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007122.7'
+ where sfcl_chromosome = '11' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007123.7'
+ where sfcl_chromosome = '12' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007124.7'
+ where sfcl_chromosome = '13' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007125.7'
+ where sfcl_chromosome = '14' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007126.7'
+ where sfcl_chromosome = '15' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007127.7'
+ where sfcl_chromosome = '16' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007128.7'
+ where sfcl_chromosome = '17' and sfcl_chromosome_reference_accession_number is null;
+
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007129.7'
+ where sfcl_chromosome = '18' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007130.7'
+ where sfcl_chromosome = '19' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007131.7'
+ where sfcl_chromosome = '20' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007132.7'
+ where sfcl_chromosome = '21' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007133.7'
+ where sfcl_chromosome = '22' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007134.7'
+ where sfcl_chromosome = '23' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007135.7'
+ where sfcl_chromosome = '24' and sfcl_chromosome_reference_accession_number is null;
+
+update sequence_feature_chromosome_location
+  set sfcl_chromosome_reference_accession_number = 'NC_007136.7'
+ where sfcl_chromosome = '25' and sfcl_chromosome_reference_accession_number is null;
