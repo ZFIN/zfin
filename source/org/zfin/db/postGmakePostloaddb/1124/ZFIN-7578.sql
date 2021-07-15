1aed699aa3e6233d180d4f93b869120c65ad6878
Christian Pich <cmpich@zfin.org>
2021-07-15 14:52:40 +0200

    fix merging of publications

--- source/org/zfin/db/postGmakePostloaddb/1124/ZFIN-7578.sql
+++ source/org/zfin/db/postGmakePostloaddb/1124/ZFIN-7578.sql
@@ -26,18 +26,31 @@ delete from withdrawn_data where wd_old_zdb_id = 'ZDB-PUB-090225-18';
 
 update withdrawn_data set wd_new_zdb_id = 'ZDB-PUB-060906-3' where wd_new_zdb_id = 'ZDB-PUB-090225-18';
 
-delete from zdb_active_source where zactvs_zdb_id = 'ZDB-PUB-090225-18';
-
 insert into withdrawn_data (wd_old_zdb_id, wd_new_zdb_id, wd_display_note) values ('ZDB-PUB-090225-18', 'ZDB-PUB-060906-3', 'merged');
 
-update expression_experiment set xpatex_source_zdb_id = 'ZDB-PUB-060906-3' where xpatex_source_zdb_id = 'ZDB-PUB-090225-18';
+delete from expression_result where xpatres_fig_zdb_id = 'ZDB-FIG-061009-5';
 
-update record_attribution as r set recattrib_source_zdb_id = 'ZDB-PUB-060906-3' where recattrib_source_zdb_id = 'ZDB-PUB-090225-18'
-and not exists (
-    select 'c' from record_attribution as ra where ra.recattrib_source_zdb_id = 'ZDB-PUB-060906-3'
-    and ra.recattrib_data_zdb_id = r.recattrib_data_zdb_id
-        );
+delete from expression_pattern_figure where xpatfig_fig_zdb_id = 'ZDB-FIG-061009-5';
+-- delete expressions from merged into pub
+delete from expression_experiment where xpatex_source_zdb_id = 'ZDB-PUB-060906-3';
+delete from expression_experiment2 where xpatex_source_zdb_id = 'ZDB-PUB-060906-3';
+
+update expression_experiment set xpatex_source_zdb_id = 'ZDB-PUB-060906-3' where xpatex_source_zdb_id = 'ZDB-PUB-090225-18';
+update expression_experiment2 set xpatex_source_zdb_id = 'ZDB-PUB-060906-3' where xpatex_source_zdb_id = 'ZDB-PUB-090225-18';
+
+--update record_attribution as r set recattrib_source_zdb_id = 'ZDB-PUB-060906-3' where recattrib_source_zdb_id = 'ZDB-PUB-090225-18'
+--and not exists (
+--    select 'c' from record_attribution as ra where ra.recattrib_source_zdb_id = 'ZDB-PUB-060906-3'
+--    and ra.recattrib_data_zdb_id = r.recattrib_data_zdb_id
+--        );
+
+update figure set fig_source_zdb_id = 'ZDB-PUB-060906-3' where fig_zdb_id = 'ZDB-FIG-090309-22';
+
+update experiment set exp_source_zdb_id = 'ZDB-PUB-060906-3' where exp_source_zdb_id = 'ZDB-PUB-090225-18';
+
+--delete from zdb_active_source where zactvs_zdb_id = 'ZDB-PUB-090225-18';
+delete from publication where zdb_id = 'ZDB-PUB-090225-18';
 
 delete from zdb_active_source where zactvs_zdb_id = 'ZDB-FIG-061009-5';
 
---delete from figure where fig_zdb_id = 'ZDB-FIG-061009-5';
+delete from figure where fig_zdb_id = 'ZDB-FIG-061009-5';
