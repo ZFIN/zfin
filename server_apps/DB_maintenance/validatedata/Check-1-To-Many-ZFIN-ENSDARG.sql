d1ad8f50ec51f2827017c9b591d90b7ee82cf89e
Christian Pich <cmpich@zfin.org>
2021-08-31 09:38:45 +0200

    ZFIN-7433 ignore gene / ESNDARG recods in DB_Link table for non GRCz11

--- server_apps/DB_maintenance/validatedata/Check-1-To-Many-ZFIN-ENSDARG.sql
+++ server_apps/DB_maintenance/validatedata/Check-1-To-Many-ZFIN-ENSDARG.sql
@@ -5,9 +5,27 @@ FROM   marker
                dblink_linked_recid AS troublemaker_zdb_id
         FROM   db_link dbl1
         WHERE  dblink_acc_num LIKE 'ENSDARG%'
+          -- use only GRCz11
+          AND dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
         GROUP  BY dblink_linked_recid
         HAVING Count(*) > 1) as query
     ON mrkr_zdb_id = troublemaker_zdb_id
   JOIN db_link
     ON dblink_linked_recid = mrkr_zdb_id
+WHERE  dblink_acc_num LIKE 'ENSDARG%'
+
+
+SELECT count(mrkr_zdb_id)
+FROM   marker
+           JOIN (SELECT Count(*)            AS be_zero,
+                        dblink_linked_recid AS troublemaker_zdb_id
+                 FROM   db_link dbl1
+                 WHERE  dblink_acc_num LIKE 'ENSDARG%'
+                   -- use only GRCz11
+                   AND dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
+                 GROUP  BY dblink_linked_recid
+                 HAVING Count(*) > 1) as query
+                ON mrkr_zdb_id = troublemaker_zdb_id
+           JOIN db_link
+                ON dblink_linked_recid = mrkr_zdb_id
 WHERE  dblink_acc_num LIKE 'ENSDARG%'
