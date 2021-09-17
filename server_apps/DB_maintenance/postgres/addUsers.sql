e0a19ca2837c8a86532175f8cc6ba70dc8ded9da
Ryan Taylor <rtaylor@zfin.org>
2021-09-17 08:31:44 -0700

    removed pkalita, pm, kschaper

--- server_apps/DB_maintenance/postgres/addUsers.sql
+++ server_apps/DB_maintenance/postgres/addUsers.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION add_users() RETURNS void AS $$
 DECLARE
-  userlist text[] := ARRAY['rtaylor', 'staylor','ryanm','pkalita','pm','kschaper','cmpich','xshao','informix','zfishweb','zfinner'];
+  userlist text[] := ARRAY['rtaylor', 'staylor','ryanm','cmpich','xshao','informix','zfishweb','zfinner'];
   username text;
 BEGIN
   FOREACH username IN ARRAY userlist
