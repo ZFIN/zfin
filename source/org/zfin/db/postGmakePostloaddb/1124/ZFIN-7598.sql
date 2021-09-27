d532a0ee36b2d4cbe63aa2e9f0d1e569eafa68f0
Christian Pich <cmpich@zfin.org>
2021-09-27 12:37:56 +0200

    ZFIN-7634 fix zfin-ro to zfin_ro in database
    (cherry picked from commit d381649a12709241c283df9902db60bedfcdf0e8)

--- source/org/zfin/db/postGmakePostloaddb/1124/ZFIN-7598.sql
+++ source/org/zfin/db/postGmakePostloaddb/1124/ZFIN-7598.sql
@@ -2,3 +2,5 @@
 --changeset christian:ZFIN-7598
 
 update ontology set ont_ontology_name = 'zfin_ro', ont_default_namespace = 'zfin_ro' where ont_ontology_name = 'zfin-ro';
+
+update term set term_ontology = 'zfin_ro' where term_ontology = 'zfin-ro';
