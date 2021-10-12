535ad8178ff3637229b79aa28e00bb1c68ef1cf3
Ryan Taylor <rtaylor@zfin.org>
2021-10-12 13:13:06 -0700

    Spock Fix
    Fix some issues with @Autowired annotation.  Autowired properties were not getting set, resulting in NullPointerExceptions.  This change seems to have helped.  Tests went from:
    
    1539 tests completed, 50 failed, 27 skipped
    to
    1549 tests completed, 26 failed, 27 skipped

--- test/org/zfin/ZfinIntegrationSpec.groovy
+++ test/org/zfin/ZfinIntegrationSpec.groovy
@@ -1,7 +1,9 @@
 package org.zfin
 
 import org.hibernate.SessionFactory
+import org.junit.runner.RunWith
 import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.zfin.framework.HibernateSessionCreator
 import org.zfin.framework.HibernateUtil
@@ -9,6 +11,7 @@ import spock.lang.Specification
 
 @WebAppConfiguration
 @ContextConfiguration(locations = "file:home/WEB-INF/spring/mvc-webapp.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
 abstract class ZfinIntegrationSpec extends Specification {
 
     public def setupSpec() {
