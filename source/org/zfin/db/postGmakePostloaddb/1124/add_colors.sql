58177fdb3fd777baef3fc73bc4462f28a472131d
Christian Pich <cmpich@zfin.org>
2021-10-13 17:30:46 +0200

    add table efg-fluoresence

--- source/org/zfin/db/postGmakePostloaddb/1124/add_colors.sql
+++ source/org/zfin/db/postGmakePostloaddb/1124/add_colors.sql
@@ -3,15 +3,23 @@
 
 
 -- define marker - fluorescent-protein association table
-create table fluorescent_efg
+drop table  if exists fpProtein_efg;
+create table fpProtein_efg
 (
     fe_mrkr_zdb_id   VARCHAR(100) NOT NULL REFERENCES marker (mrkr_zdb_id),
     fe_fl_protein_id bigint       NOT NULL REFERENCES fluorescent_protein (fp_pk_id)
 );
 
-insert into fluorescent_efg
+insert into fpProtein_efg
 select distinct mrkr_zdb_id, fp_pk_id
 from marker,
      fluorescent_protein
 where mrkr_type = 'EFG'
-  AND lower(mrkr_name) = lower(fluorescent_protein.fp_name)
+  AND lower(mrkr_name) = lower(fluorescent_protein.fp_name);
+
+insert into efg_fluorescence (ef_mrkr_zdb_id, ef_excitation_length,ef_emission_length) (
+    select fe_mrkr_zdb_id, fp_excitation_length, fp_emission_length
+    FROM fpprotein_efg, fluorescent_protein
+    where fe_fl_protein_id = fp_pk_id
+);
+
--- source/org/zfin/db/postGmakePostloaddb/1124/add_colors_pre.sql
+++ source/org/zfin/db/postGmakePostloaddb/1124/add_colors_pre.sql
@@ -11,3 +11,11 @@ create table fluorescent_protein (
                  fp_emission_length VARCHAR(5),
                  fp_aliases VARCHAR(200)
 );
+
+drop table if exists efg_fluorescence;
+create table efg_fluorescence (
+                 ef_pk_id serial PRIMARY KEY,
+                 ef_mrkr_zdb_id TEXT NOT NULL REFERENCES marker (mrkr_zdb_id),
+                 ef_excitation_length VARCHAR(5),
+                 ef_emission_length VARCHAR(5)
+);
