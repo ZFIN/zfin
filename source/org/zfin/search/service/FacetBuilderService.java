d32f3fad5e180f936aea88c6bf3af44530e56c7d
Christian Pich <cmpich@zfin.org>
2021-10-24 18:42:55 +0200

    add fluorescent info to expression category

--- source/org/zfin/search/service/FacetBuilderService.java
+++ source/org/zfin/search/service/FacetBuilderService.java
@@ -202,6 +202,10 @@ public class FacetBuilderService {
 
         wildtypeGroup.setRootOnly(true);
         facetGroups.add(wildtypeGroup);
+        facetGroups.add(buildSingleFacetGroup("Color", ANY_COLOR.getName(), false));
+        facetGroups.add(buildSingleFacetGroup("Emission Range", EMISSION_COLOR.getName(), false));
+        facetGroups.add(buildSingleFacetGroup("Excitation Range", EXCITATION_COLOR.getName(), false));
+
         facetGroups.add(buildSingleFacetGroup("Assay", FieldName.ASSAY.getName(), false));
 
 
