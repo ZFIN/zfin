6f704aeede04efad3e62ee122305eb3a016ce4dc
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2021-11-02 15:25:04 -0700

    ZFIN-7693
    This test was failing due to an IndexOutOfBoundsException at accessions.get(0) in when clause.  However, the test seems that it may still need some attention.  I'm not sure why the assertion is so flexible in accepting either an empty set or dblinks?  I adjusted the input accession numbers to include some tests that the comment indicated would fail.

--- test/org/zfin/sequence/repository/SequenceRepositorySpec.groovy
+++ test/org/zfin/sequence/repository/SequenceRepositorySpec.groovy
@@ -3,6 +3,7 @@ package org.zfin.sequence.repository
 import org.apache.commons.collections.CollectionUtils
 import org.zfin.AbstractZfinIntegrationSpec
 import org.zfin.framework.HibernateUtil
+import org.zfin.gwt.marker.ui.DBLengthEntryField
 import org.zfin.repository.RepositoryFactory
 import org.zfin.sequence.Accession
 import org.zfin.sequence.DBLink
@@ -14,17 +15,14 @@ import spock.lang.Unroll
 class SequenceRepositorySpec extends AbstractZfinIntegrationSpec {
     @Unroll
     def "should be able to get dblinks from an accession #accessionNumber"() {
-        //HibernateUtil.createTransaction();
         when:
         List<Accession> accessions = RepositoryFactory.sequenceRepository.getAccessionsByNumber(accessionNumber)
-        Set<DBLink> links = accessions.get(0).getDbLinks();
-        //HibernateUtil.rollbackTransaction();
 
         then: "either it's empty, or the first record returned should have dblinks"
-        CollectionUtils.isEmpty(accessions) || accessions.get(0).getDbLinks()
+        CollectionUtils.isEmpty(accessions) || accessions.get(0).getDbLinks() //Why are we so flexible about what we assert here?
 
-        where:  //fails for: "ENSDARG00000002898", "NM_131281" , "CR925797"
-        accessionNumber << [ "OTTDARG00000001297",  "O42278"]
+        where:  //fails for: "ENSDARG00000002898", "NM_131281" , "CR925797", passes for "OTTDARG00000001297",  "O42278"
+        accessionNumber << [ "OTTDARG00000001297",  "O42278", "ENSDARG00000002898", "NM_131281" , "CR925797"]
 
     }
 
