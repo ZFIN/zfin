32280828d82ad86f75cfd80fafcc22e8fc6049ff
Christian Pich <cmpich@zfin.org>
2021-11-04 17:27:38 +0100

    ZFIN-7584 fix variant change

---
+++ source/org/zfin/db/postGmakePostloaddb/1125/ZFIN-7584_pre.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+--changeset cmpich:ZFIN-7584
+
+
+create temp table ensdarg_temp
+(
+    et_id        VARCHAR(100),
+    et_reference VARCHAR(100),
+    et_variant   VARCHAR(100)
+);
