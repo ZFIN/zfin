11ba6f238e92d57b8349427e99fcca454cdc3799
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2021-11-30 16:31:32 -0800

    ZFIN-7704
    Create an SQL output file.
    Fix issue with duplicate direct attributions occurring if there are multiple intermediaries that would create the same attribution.

--- source/org/zfin/feature/repair/FeatureAttributionRepair.java
+++ source/org/zfin/feature/repair/FeatureAttributionRepair.java
@@ -1,11 +1,8 @@
 package org.zfin.feature.repair;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.time.DateUtils;
 import org.apache.log4j.*;
 import org.zfin.feature.Feature;
 import org.zfin.feature.FeatureMarkerRelationship;
-import org.zfin.framework.HibernateUtil;
 import org.zfin.gwt.curation.dto.FeatureMarkerRelationshipTypeEnum;
 import org.zfin.infrastructure.RecordAttribution;
 import org.zfin.marker.Marker;
@@ -13,6 +10,7 @@ import org.zfin.ontology.datatransfer.AbstractScriptWrapper;
 import org.zfin.publication.Publication;
 
 import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -26,21 +24,49 @@ import static org.zfin.repository.RepositoryFactory.getInfrastructureRepository;
 public class FeatureAttributionRepair extends AbstractScriptWrapper {
 
     private static final Level logLevel = Level.INFO;
+    private BufferedWriter outputWriter;
     private Logger logger;
     private static final String DEFAULT_OUTPUT_PATH = "FeatureAttributionRepairReport.log";
+    private static final String DEFAULT_SQL_OUTPUT_PATH = "FeatureAttributionRepairReport.sql";
+    private HashMap<String, Boolean> cachedEntries;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         FeatureAttributionRepair repair = new FeatureAttributionRepair();
-        repair.scanThroughPublicationsAndFix();
+        try {
+            repair.scanThroughPublicationsAndFix();
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing to file.");
+        }
         System.exit(0);
     }
 
     public FeatureAttributionRepair() {
         initAll();
         initLogger();
+        initCache();
+        initSqlWriter();
     }
 
-    public void scanThroughPublicationsAndFix() {
+    private void initCache() {
+        cachedEntries = new HashMap<String, Boolean>();
+    }
+
+    private void initSqlWriter() {
+        String outputPath = System.getProperty("sqlFile", DEFAULT_SQL_OUTPUT_PATH);
+        if (outputPath.equals("")) {
+            //Seems we get empty string if property isn't specified. Maybe due to gradle configuration in console.gradle?
+            outputPath = DEFAULT_SQL_OUTPUT_PATH;
+        }
+
+        outputWriter = null;
+        try {
+            outputWriter = new BufferedWriter(new FileWriter(outputPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void scanThroughPublicationsAndFix() throws IOException {
         List<Publication> publications = getPublications();
         publications = filterToPublicationsBefore(publications);
         publications = filterToPublicationsAfter(publications);
@@ -154,18 +180,22 @@ public class FeatureAttributionRepair extends AbstractScriptWrapper {
         return filteredPublications;
     }
 
-    private void attributeGenesToPublication(Publication publication, Set<Marker> targetedGenes, String intermediaryAbbreviation, String intermediaryID, String reason) {
+    private void attributeGenesToPublication(Publication publication, Set<Marker> targetedGenes, String intermediaryAbbreviation, String intermediaryID, String reason) throws IOException {
         for(Marker gene : targetedGenes) {
+            String hashKey = publication.getZdbID() + "," + gene.getZdbID();
+            if (cachedEntries.containsKey(hashKey)) {
+                continue;
+            }
+            cachedEntries.put(hashKey, true);
             if (getInfrastructureRepository().getRecordAttribution(gene.zdbID, publication.getZdbID(), RecordAttribution.SourceType.STANDARD) == null) {
                 logger.info("Add to publication " + publication.getZdbID() + " direct attribution of " + gene.getZdbID() + " through intermediary " + intermediaryAbbreviation + "(" + intermediaryID + "): " + reason );
                 logger.info("CSV Report:" + publication.getZdbID() + "," + gene.getZdbID() + "," + intermediaryID + "," + reason);
-                logger.info("SQL INSERT (updates) --> " +
-                "INSERT INTO updates (submitter_id,rec_id,field_name,new_value,comments,submitter_name) " +
+                outputWriter.write("INSERT INTO updates (submitter_id,rec_id,field_name,new_value,comments,submitter_name) " +
                                 "SELECT 'ZDB-PERS-210917-1','" + gene.getZdbID() + "','record attribution','" + publication.getZdbID() + "'," + "'Added direct attribution by intermediary " + intermediaryID + " " + reason + "','Ryan Taylor (ZFIN-7704)' WHERE NOT EXISTS ( select 'x' from updates where " +
-                                "submitter_id='ZDB-PERS-210917-1' and rec_id='" + gene.getZdbID() + "' and field_name='record attribution' and new_value = '" + publication.getZdbID() + "' );"
+                                "submitter_id='ZDB-PERS-210917-1' and rec_id='" + gene.getZdbID() + "' and field_name='record attribution' and new_value = '" + publication.getZdbID() + "' );\n"
                 );
-                logger.info("SQL INSERT (record_attribution) --> " +
-                        "INSERT INTO record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_type) VALUES ('" + gene.getZdbID() + "', '" + publication.getZdbID() + "', 'standard') ON CONFLICT DO NOTHING;");
+                outputWriter.write("INSERT INTO record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_type) VALUES ('" + gene.getZdbID() + "', '" + publication.getZdbID() + "', 'standard') ON CONFLICT DO NOTHING;\n");
+                outputWriter.flush();
             }
         }
     }
