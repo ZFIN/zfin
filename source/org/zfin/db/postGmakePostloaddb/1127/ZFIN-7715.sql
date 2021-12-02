8135c7218699e938b6ce34d4821c2dc7bcad30ca
Christian Pich <cmpich@zfin.org>
2021-12-02 14:06:16 +0100

    ZFIN-7715 update signafish URL

--- source/org/zfin/db/postGmakePostloaddb/1127/ZFIN-7715.sql
+++ source/org/zfin/db/postGmakePostloaddb/1127/ZFIN-7715.sql
@@ -1,18 +1,7 @@
 --liquibase formatted sql
---changeset cmpich:ZFIN-7723
+--changeset cmpich:ZFIN-7715
 
--- connect new EFG with FP protein
-insert into fpprotein_efg (fe_mrkr_zdb_id, fe_fl_protein_id) VALUES
-('ZDB-EFG-211117-1',391);
+-- update signafish URL
+update foreign_db set fdb_db_query = 'http://signalink.org/protein/'
+where fdb_db_name = 'SignaFish';
 
--- copy FP protein info into fluorescent_marker table
-insert into fluorescent_marker (fm_mrkr_zdb_id, fm_excitation_length, fm_emission_length, fm_protein_pk_id) (
-    select fe_mrkr_zdb_id, fp_excitation_length::INTEGER, fp_emission_length::INTEGER, fe_fl_protein_id
-    FROM fpprotein_efg,
-         fluorescent_protein
-    where fe_fl_protein_id = fp_pk_id
-    AND fe_mrkr_zdb_id = 'ZDB-EFG-211117-1'
-);
-
--- generate the color info
-select from create_color_info();
