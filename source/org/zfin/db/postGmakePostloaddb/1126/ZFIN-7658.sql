5bc70cb8e530f22dffe50ee8d6a11520f055e366
Christian Pich <cmpich@zfin.org>
2021-12-10 09:43:01 +0100

    ZFIN-7658 refactor SQL script

--- source/org/zfin/db/postGmakePostloaddb/1126/ZFIN-7658.sql
+++ source/org/zfin/db/postGmakePostloaddb/1126/ZFIN-7658.sql
@@ -1,13 +1,13 @@
 --liquibase formatted sql
 --changeset cmpich:ZFIN-7658
 
--- remove sourcebioscoence as a BAC supplier
+-- remove sourcebioscience as a BAC supplier
 delete
 from int_data_supplier
 where idsup_data_zdb_id in (
     select mrkr_zdb_id
     from marker
-    where mrkr_type = 'BAC'
+    where mrkr_type in ('BAC')
 )
   AND idsup_supplier_zdb_id in (
     select zdb_id from company where name = 'Source BioScience (formerly Imagenes)'
