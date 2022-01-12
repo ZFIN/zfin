77b856863916c6f7abc69c572652a7a49785f001
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-12 15:38:30 -0800

    ZFIN-7604 (#115)
    Add an outgoing link from gene page to Single Cell Expression Atlas.
    Use curation publication (ZDB-PUB-220103-3) as reference link.

--- source/org/zfin/expression/presentation/MarkerExpression.java
+++ source/org/zfin/expression/presentation/MarkerExpression.java
@@ -19,6 +19,7 @@ public class MarkerExpression {
     private WildTypeExpression wildTypeStageExpression;
     private String geoLink;
     private LinkDisplay expressionAtlasLink;
+    private String singleCellExpressionAtlasLink;
     private List<String> ensdargGenes;
     private int inSituFigCount;
     private String geoGeneSymbol ;
