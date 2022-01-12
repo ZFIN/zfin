88002a042c3e55d7cc4c0f58605ff797b507478f
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-12 15:39:38 -0800

    ZFIN-7756 (#112)
    Change the updates to direct updates rather than using the p_update_related_fish_names function.
    Ignore "Cooch Behar" fish_name in report of fish name problems.

--- server_apps/DB_maintenance/validatedata/Check-Fish-Name-Function-Mismatch_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Fish-Name-Function-Mismatch_d.sql
@@ -1,4 +1,5 @@
 select fish_zdb_id, fish_name, get_fish_name(fish_zdb_id) as computed_fish_name from fish
 where  fish_name <> get_fish_name(fish_zdb_id)
+and fish_name <> 'Cooch Behar' -- exception
 order by fish_zdb_id
 
