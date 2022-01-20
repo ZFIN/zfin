c58e37e6927fbc7f769015b23931dbf686896a32
Ryan Martin <ryanm@zfin.org>
2022-01-20 14:13:06 -0800

    First pass at working JBrowse 'images' on gene and other pages.

--- source/org/zfin/gbrowse/GBrowseTrack.java
+++ source/org/zfin/gbrowse/GBrowseTrack.java
@@ -4,21 +4,21 @@ package org.zfin.gbrowse;
  * Names of the gBrowse tracks.
  */
 public enum GBrowseTrack {
-    GENES("genes"),
-    TRANSCRIPTS("transcript"),
-    CLONE("clone"),
+    GENES("ZFIN Gene"),
+    TRANSCRIPTS("Transcript"),
+    CLONE("Assembly"),
     GENES_VEGA("genesVega"),
     ENSEMBL_MRNA("Ensembl_mRNA"),
-    PHENOTYPE("phenotype"),
-    EXPRESSIONS("expression"),
-    ANTIBODY("antibody"),
-    KNOCKDOWN_REAGENT("knockdown_reagent"),
-    INSERTION("insertion"),
+    PHENOTYPE("ZFIN Genes with Phenotype"),
+    EXPRESSIONS("ZFIN Genes with Expression"),
+    ANTIBODY("ZFIN Genes with Antibody"),
+    KNOCKDOWN_REAGENT("Knockdown Reagent"),
+    INSERTION("Transgenic Insertion"),
     CNE("CNE"),
-    COMPLETE_CLONES("fullclone"),
+    COMPLETE_CLONES("Complete Assembly Clones"),
     ALLZMP("allzmp"),
-    ZFIN_FEATURES("mutants"),
-    ZMP("zmp");
+    ZFIN_FEATURES("ZFIN Features"),
+    ZMP("Zebrafish Mutation Project");
 
     GBrowseTrack(String trackName) {
         this.trackName = trackName;
