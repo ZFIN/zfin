482e178b02777356b3720840f098048801284c35
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-01-24 08:39:55 -0800

    Use web component version of go ribbon. Get the styling to match the legacy ribbon. Match sizing. Set height via margin. Capitalize first 'a' in 'All annotations'. include a label in API output (instead of null). use "X" for indicating clicked cell.

--- home/javascript/react/components/ribbon/Ribbon.js
+++ home/javascript/react/components/ribbon/Ribbon.js
@@ -1,10 +1,16 @@
 import React, { useEffect, useRef } from 'react';
-import {GenericRibbon} from '@geneontology/ribbon';
+import { applyPolyfills, defineCustomElements } from '@geneontology/wc-ribbon-strips/loader';
 
 import style from './style.scss';
 
-const Ribbon = (props) => {
+applyPolyfills().then(() => {
+    defineCustomElements(window);
+});
+
+const Ribbon = ({subjects, categories, itemClick, selected}) => {
+    const props = {subjects, categories, selected};
     const ribbonRef = useRef(null);
+    const ribbonStripsRef = useRef();
     useEffect(() => {
         if (!ribbonRef.current) {
             return;
@@ -15,16 +21,39 @@ const Ribbon = (props) => {
                 .replace(/class(e?)/, 'term')
                 .replace(/^0 term, 0 annotation$/, 'No annotations');
             item.setAttribute('title', title);
-        })
-    }, [props.subjects])
+        });
+    }, [subjects]);
+
+    useEffect(() => {
+        ribbonStripsRef.current.addEventListener('cellClick', (event) => {
+            try {
+                const subject = event.detail.subjects[0];
+                const group = event.detail.group;
+                if (subject && group) {
+                    itemClick(subject, group);
+                }
+            } catch (e) {
+                //in case event.detail.subjects[0] chaining fails.
+            }
+        });
+    },[]);
+
     return (
         <div className='ontology-ribbon-container horizontal-scroll-container' ref={ribbonRef}>
-            <GenericRibbon
-                hideFirstSubjectLabel
-                colorBy={1} // annotations
-                binaryColor
-                maxColor={[style.primaryR, style.primaryG, style.primaryB]}
-                {...props}
+            <wc-ribbon-strips
+                color-by='1' // annotations
+                binary-color={true}
+                max-color={[style.primaryR, style.primaryG, style.primaryB]}
+                subject-position='0'
+                update-on-subject-change={false}
+                show-other-group={true}
+                group-clickable={false}
+                category-case={0}
+                category-all-style={1} //bold
+                category-other-style={1} //bold
+                fire-event-on-empty-cells={false}
+                ref={ribbonStripsRef}
+                data={JSON.stringify(props)}
             />
             <small className='text-muted'>
                 Click a filled square to see annotations
