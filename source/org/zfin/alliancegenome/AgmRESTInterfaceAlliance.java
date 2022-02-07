57e163c979a95585675d8f769cce290567fea14b
Christian Pich <cmpich@zfin.org>
2022-02-07 11:40:37 +0100

    ZFIN-7810 create fish at the Alliance when new one is created at ZFIN

---
+++ source/org/zfin/alliancegenome/AgmRESTInterfaceAlliance.java
@@ -0,0 +1,28 @@
+package org.zfin.alliancegenome;
+
+import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
+import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
+import org.alliancegenome.curation_api.response.ObjectResponse;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/api")
+public interface AgmRESTInterfaceAlliance {
+
+    @GET
+    @Path("/agm/{curie}")
+    @Produces({MediaType.APPLICATION_JSON})
+    ObjectResponse<AffectedGenomicModel> getAffectedGenomicModel(@PathParam("curie") String curie);
+
+    @POST
+    @Path("/agm")
+    @Consumes({MediaType.APPLICATION_JSON})
+    ObjectResponse<AffectedGenomicModel> addAffectedGenomicModel(AffectedGenomicModel model);
+
+    @PUT
+    @Path("/agm")
+    @Consumes({MediaType.APPLICATION_JSON})
+    ObjectResponse<AffectedGenomicModel> updateAffectedGenomicModel(AffectedGenomicModel model);
+
+}
