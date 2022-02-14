7fed6417788f119df1c29b3bbe79f417b287f133
Christian Pich <cmpich@zfin.org>
2022-02-14 14:48:21 +0100

    ZFIN-7733 fixup expression links for fish miRNA

--- source/org/zfin/db/postGmakePostloaddb/1129/fish_mi_rna_expression_post.sql
+++ source/org/zfin/db/postGmakePostloaddb/1129/fish_mi_rna_expression_post.sql
@@ -10,13 +10,13 @@ create table pre_db_link_temp
 );
 
 insert into pre_db_link_temp (predblink_db_zdb_id, predblink_data_zdb_id)
-select get_id('DBLINK'), mir_gene_id
+select get_id('DBLINK'), gene_zdb_id
 from fishmir_temp ;
 
-insert into db_link (dblink_linked_recid, dblink_acc_num, dblink_info, dblink_zdb_id, dblink_acc_num_display,
+insert into db_link (dblink_acc_num, dblink_linked_recid, dblink_info, dblink_zdb_id, dblink_acc_num_display,
                      dblink_fdbcont_zdb_id)
-select gene_zdb_id,
-       mir_gene_id,
+select mir_gene_id,
+       gene_zdb_id,
        'imported from Fish miRNA Expression',
        predblink_db_zdb_id,
        mir_gene_id,
@@ -27,8 +27,7 @@ from fishmir_temp,
      pre_db_link_temp
 where fdb_db_pk_id = fdbcont_fdb_db_id
   AND fdb_db_name = 'FishMiRNA-Expression'
-  AND predblink_data_zdb_id = mir_gene_id
-  AND mir_gene_id = 'FMDANRERM0002,FMDANRERM0246'
+  AND predblink_data_zdb_id = gene_zdb_id
 ;
 
 insert into zdb_active_data select predblink_db_zdb_id from pre_db_link_temp;
