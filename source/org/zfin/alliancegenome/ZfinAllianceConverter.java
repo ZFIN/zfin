dbd86f135b69bcae33c176da4189f2ac372a1bf1
Christian Pich <cmpich@zfin.org>
2022-02-16 17:55:42 +0100

    upgrade alliance library

--- source/org/zfin/alliancegenome/ZfinAllianceConverter.java
+++ source/org/zfin/alliancegenome/ZfinAllianceConverter.java
@@ -1,11 +1,9 @@
 package org.zfin.alliancegenome;
 
-import org.alliancegenome.curation_api.model.entities.BiologicalEntity;
-import org.alliancegenome.curation_api.model.entities.CrossReference;
-import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
-import org.alliancegenome.curation_api.model.entities.Reference;
+import org.alliancegenome.curation_api.model.entities.*;
 import org.alliancegenome.curation_api.model.entities.ontology.DOTerm;
 import org.alliancegenome.curation_api.model.entities.ontology.EcoTerm;
+import org.alliancegenome.curation_api.model.entities.ontology.NCBITaxonTerm;
 import org.zfin.mutant.DiseaseAnnotationModel;
 import org.zfin.mutant.Fish;
 import org.zfin.ontology.GenericTerm;
@@ -13,17 +11,19 @@ import org.zfin.publication.Publication;
 
 import java.util.List;
 
+import static org.zfin.mutant.service.FishService.ZEBRAFISH_TAXID;
+
 public class ZfinAllianceConverter {
 
 
     public static DiseaseAnnotation convertDiseaseAnnotation(DiseaseAnnotationModel model) {
-        DiseaseAnnotation annotation = new DiseaseAnnotation();
+        AGMDiseaseAnnotation annotation = new AGMDiseaseAnnotation();
         annotation.setObject(convertDisease(model.getDiseaseAnnotation().getDisease(), model.getDiseaseAnnotation().getPublication()));
         annotation.setSubject(convertFish(model.getFishExperiment().getFish()));
         annotation.setEvidenceCodes(convertEvidenceCodes(model.getDiseaseAnnotation().getEvidenceCode()));
         //annotation.setReferenceList(convertReferences(model.getDiseaseAnnotation().getPublication()));
         annotation.setDiseaseRelation(DiseaseAnnotation.DiseaseRelation.is_model_of);
-        annotation.setCurie(model.getDiseaseAnnotation().getZdbID());
+        annotation.setModId(model.getDiseaseAnnotation().getZdbID());
         return annotation;
     }
 
@@ -40,10 +40,12 @@ public class ZfinAllianceConverter {
         return List.of(eco);
     }
 
-    private static BiologicalEntity convertFish(Fish fish) {
-        BiologicalEntity entity = new BiologicalEntity();
+    private static AffectedGenomicModel convertFish(Fish fish) {
+        AffectedGenomicModel entity = new AffectedGenomicModel();
         entity.setCurie("ZFIN:" + fish.getZdbID());
-        entity.setTaxon("NCBITaxon:7955");
+        NCBITaxonTerm term = new NCBITaxonTerm();
+        term.setName(ZEBRAFISH_TAXID);
+        entity.setTaxon(term);
         return entity;
     }
 
--- source/org/zfin/mutant/service/DiseaseAnnotationService.java
+++ source/org/zfin/mutant/service/DiseaseAnnotationService.java
@@ -1,11 +1,13 @@
 package org.zfin.mutant.service;
 
 import lombok.extern.log4j.Log4j2;
-import org.alliancegenome.curation_api.model.entities.BiologicalEntity;
+import org.alliancegenome.curation_api.model.entities.AGMDiseaseAnnotation;
+import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
 import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
 import org.alliancegenome.curation_api.model.entities.Reference;
 import org.alliancegenome.curation_api.model.entities.ontology.DOTerm;
 import org.alliancegenome.curation_api.model.entities.ontology.EcoTerm;
+import org.alliancegenome.curation_api.model.entities.ontology.NCBITaxonTerm;
 import org.zfin.alliancegenome.AllianceRestManager;
 import org.zfin.alliancegenome.DiseaseAnnotationRESTInterfaceAlliance;
 import org.zfin.mutant.DiseaseAnnotationModel;
@@ -15,18 +17,16 @@ import org.zfin.ontology.GenericTerm;
 import java.util.List;
 
 @Log4j2
-public class DiseaseAnnotationService {
-
-    protected static final String ZEBRAFISH_TAXID = "taxon:7955";
+public class DiseaseAnnotationService extends AllianceService{
 
     public static void submitAnnotationToAlliance(DiseaseAnnotationModel dam) {
-        DiseaseAnnotation da = new DiseaseAnnotation();
+        AGMDiseaseAnnotation da = new AGMDiseaseAnnotation();
         da.setObject(getDoTerm(dam.getDiseaseAnnotation().getDisease()));
         da.setSubject(getBiologicalEntity(dam.getFishExperiment().getFish()));
         da.setDiseaseRelation(DiseaseAnnotation.DiseaseRelation.is_model_of);
         da.setNegated(Boolean.FALSE);
         da.setEvidenceCodes(List.of(getEvidenceCodes(dam.getDiseaseAnnotation())));
-        da.setReferenceList(List.of(getCrossReference(dam.getDiseaseAnnotation())));
+        da.setSingleReference(getCrossReference(dam.getDiseaseAnnotation()));
         DiseaseAnnotationRESTInterfaceAlliance api = AllianceRestManager.getDiseaseAnnotationEndpoints();
         try {
             api.addDiseaseAnnotation(da);
@@ -50,10 +50,10 @@ public class DiseaseAnnotationService {
         return evidence;
     }
 
-    private static BiologicalEntity getBiologicalEntity(Fish fish) {
-        BiologicalEntity entity = new BiologicalEntity();
+    private static AffectedGenomicModel getBiologicalEntity(Fish fish) {
+        AffectedGenomicModel entity = new AffectedGenomicModel();
         entity.setCurie("ZFIN:" + fish.getZdbID());
-        entity.setTaxon(ZEBRAFISH_TAXID);
+        entity.setTaxon(getNcbiTaxonTerm());
         return entity;
     }
 
