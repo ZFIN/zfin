5aade774b2e14d5042571772876d3a601696d9dd
Christian Pich <cmpich@zfin.org>
2022-03-04 09:09:23 +0100

    limit blst tools for mirBase Mature and ZFINNUCL to miRNA blast databases

--- source/org/zfin/db/postGmakePostloaddb/1130/ZFIN-7894.sql
+++ source/org/zfin/db/postGmakePostloaddb/1130/ZFIN-7894.sql
@@ -6,3 +6,13 @@
 delete from int_fdbcont_analysis_tool
 where ifat_fdbcont_zdb_id = 'ZDB-FDBCONT-090929-2'
 AND ifat_blastdb_zdb_id in ('ZDB-BLASTDB-090929-16','ZDB-BLASTDB-090929-18','ZDB-BLASTDB-090929-19','ZDB-BLASTDB-090929-9');
+
+-- mirBase Mature
+delete from int_fdbcont_analysis_tool
+where ifat_fdbcont_zdb_id = 'ZDB-FDBCONT-090929-3'
+  AND ifat_blastdb_zdb_id in ('ZDB-BLASTDB-090929-16','ZDB-BLASTDB-090929-18','ZDB-BLASTDB-090929-19','ZDB-BLASTDB-090929-9');
+
+-- ZFINNUCL:
+delete from int_fdbcont_analysis_tool
+where ifat_fdbcont_zdb_id = 'ZDB-FDBCONT-090929-5'
+  AND ifat_blastdb_zdb_id in ('ZDB-BLASTDB-090929-16','ZDB-BLASTDB-090929-18','ZDB-BLASTDB-090929-19','ZDB-BLASTDB-090929-9');
