672852d7cbdac8e09b7a9d04045eae1d82c14512
Christian Pich <cmpich@zfin.org>
2022-03-09 17:46:45 +0100

    ZFIN-7863 modify the output JSON format for disease annotations.

---
+++ source/org/zfin/marker/agr/BasicDiseaseAnnotationLinkML.java
@@ -0,0 +1,41 @@
+package org.zfin.marker.agr;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import lombok.Getter;
+import lombok.Setter;
+import org.alliancegenome.curation_api.model.entities.AffectedGenomicModel;
+import org.alliancegenome.curation_api.model.entities.ontology.EcoTerm;
+import org.alliancegenome.curation_api.model.ingest.dto.AGMDiseaseAnnotationDTO;
+import org.alliancegenome.curation_api.model.ingest.dto.ExperimentalConditionDTO;
+import org.zfin.alliancegenome.ZfinAllianceConverter;
+import org.zfin.expression.ExperimentCondition;
+import org.zfin.infrastructure.ActiveData;
+import org.zfin.marker.Marker;
+import org.zfin.mutant.*;
+import org.zfin.ontology.GenericTerm;
+import org.zfin.ontology.datatransfer.AbstractScriptWrapper;
+import org.zfin.publication.Publication;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+import static org.zfin.repository.RepositoryFactory.getMutantRepository;
+import static org.zfin.repository.RepositoryFactory.getOntologyRepository;
+
+@Setter
+@Getter
+public class BasicDiseaseAnnotationLinkML  {
+
+    @JsonProperty("disease_agm_ingest_set")
+    List<AGMDiseaseAnnotationDTO> diseaseAgmIngest;
+
+}
