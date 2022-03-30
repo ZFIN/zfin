a9dcd28af7b2004eb274a3924fd19acad7a2175a
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-03-30 13:21:01 -0700

    Look for a name that starts with left parenthesis.

--- server_apps/DB_maintenance/validatedata/Check-Fish-Name-Function-Mismatch_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Fish-Name-Function-Mismatch_d.sql
@@ -8,8 +8,8 @@ WHERE (
             fish_name <> get_fish_name (fish_zdb_id)
         OR trim(fish_name) = ''
         OR trim(get_fish_name (fish_zdb_id)) = ''
-        OR trim(fish_name) = '(AB)' -- basically a blank name, but with a background
-        OR trim(get_fish_name (fish_zdb_id)) = '(AB)'
+        OR left(trim(fish_name),1) = '(' -- basically a blank name, but with a background
+        OR left(trim(get_fish_name (fish_zdb_id)),1) = '('
         OR left(trim(fish_name),1) = '+' -- fish name starts with +, so a blank name and STRs
         OR left(trim(get_fish_name (fish_zdb_id)),1) = '+'
         OR trim(fish_name) = '_' -- placeholder for unhandled blank name
