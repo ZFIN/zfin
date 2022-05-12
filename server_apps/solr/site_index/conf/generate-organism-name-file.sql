a001c602417487f2b4ed0904f2fc615561891608
Christian Pich <cmpich@zfin.org>
2022-05-12 15:30:34 +0200

    ZFIN-7215 converting solr index groovy script into ant

--- server_apps/solr/site_index/conf/generate-organism-name-file.sql
+++ server_apps/solr/site_index/conf/generate-organism-name-file.sql
@@ -1,4 +1,1 @@
-copy (
-select organism_common_name
-from organism ) to '@TARGETROOT@/server_apps/solr/prototype/conf/organism-name.txt' delimiter '|'
-;
+\copy (select organism_common_name from organism ) to '@TARGETROOT@/server_apps/solr/prototype/conf/organism-name.txt' delimiter '|';
