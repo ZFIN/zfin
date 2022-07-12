557b7b1fe9f066ca400622b171eceb95df6dca30
Christian Pich <cmpich@zfin.org>
2022-07-12 13:22:36 +0200

    fix up DA endpoint to work with the Alliance endpoint to create a new DiseaseAnnotationDTO object

--- source/org/zfin/alliancegenome/DiseaseAnnotationRESTInterfaceAlliance.java
+++ source/org/zfin/alliancegenome/DiseaseAnnotationRESTInterfaceAlliance.java
@@ -1,10 +1,14 @@
 package org.zfin.alliancegenome;
 
+import org.alliancegenome.curation_api.exceptions.ObjectUpdateException;
+import org.alliancegenome.curation_api.model.entities.AGMDiseaseAnnotation;
 import org.alliancegenome.curation_api.model.entities.DiseaseAnnotation;
+import org.alliancegenome.curation_api.model.ingest.dto.AGMDiseaseAnnotationDTO;
 import org.alliancegenome.curation_api.response.ObjectResponse;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
 
 @Path("/api")
 public interface DiseaseAnnotationRESTInterfaceAlliance {
@@ -14,13 +18,19 @@ public interface DiseaseAnnotationRESTInterfaceAlliance {
     @Produces({MediaType.APPLICATION_JSON})
     ObjectResponse<DiseaseAnnotation> getDiseaseAnnotation(@HeaderParam("Authorization") String auth,
                                                            @PathParam("curie") String curie
-                                                           );
+    );
 
     @POST
     @Path("/agm-disease-annotation")
     @Consumes({MediaType.APPLICATION_JSON})
-    ObjectResponse<DiseaseAnnotation> addDiseaseAnnotation(@HeaderParam("Authorization") String auth,
-                                                           DiseaseAnnotation annotation);
+    ObjectResponse<AGMDiseaseAnnotation> addDiseaseAnnotation(@HeaderParam("Authorization") String auth,
+                                                              AGMDiseaseAnnotation annotation);
+
+    @POST
+    @Path("/agm-disease-annotation/create")
+    @Consumes({MediaType.APPLICATION_JSON})
+    ObjectResponse<AGMDiseaseAnnotation> createZfinAgmDiseaseAnnotations(@HeaderParam("Authorization") String auth,
+                                                                         AGMDiseaseAnnotationDTO annotationData) throws IOException;
 
     @PUT
     @Path("/agm-disease-annotation")
