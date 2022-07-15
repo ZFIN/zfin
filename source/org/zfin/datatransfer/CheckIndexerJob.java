3344cf2bba873805699cbba8c3c5ebc37ba2f70f
Christian Pich <cmpich@zfin.org>
2022-07-15 14:46:37 +0200

    ZFIN-8105 refine checkIndexer

--- source/org/zfin/datatransfer/CheckIndexerJob.java
+++ source/org/zfin/datatransfer/CheckIndexerJob.java
@@ -2,8 +2,6 @@ package org.zfin.datatransfer;
 
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.zfin.alliancegenome.JacksonObjectMapperFactoryZFIN;
 import org.zfin.infrastructure.ant.AbstractValidateDataReportTask;
 import org.zfin.properties.ZfinPropertiesEnum;
@@ -14,8 +12,6 @@ import si.mazi.rescu.RestProxyFactory;
 
 @Log4j2
 public class CheckIndexerJob extends AbstractValidateDataReportTask {
-
-    private static Logger logger = LogManager.getLogger(CheckIndexerJob.class);
 
     public CheckIndexerJob(String jobName, String propertyPath, String baseDir) {
         super(jobName, propertyPath, baseDir);
@@ -28,17 +24,19 @@ public class CheckIndexerJob extends AbstractValidateDataReportTask {
         while (true) {
             IndexerStatus status = getIndexStatus();
 
-            if (status != null && status.getStatusMessages() != null)
-                log.info("Elapsed time: " + status.getStatusMessages().getTimeElapsed());
-
+            if (status != null && status.getStatusMessages() != null) {
+                if (status.getStatusMessages().getTimeElapsed() != null)
+                    log.info("Elapsed Time: " + status.getStatusMessages().getTimeElapsed());
+            }
             if (status == null || !status.getStatus().equals(IndexerStatus.Status.BUSY.name().toLowerCase())) {
+                log.info("Total Time: " + status.getStatusMessages().getTimeTaken());
                 System.exit(0);
             }
             Thread.sleep(60 * 1000);
         }
     }
 
-    private static ClientConfig config = new ClientConfig();
+    private static final ClientConfig config = new ClientConfig();
 
     static {
         config.setJacksonObjectMapperFactory(new JacksonObjectMapperFactoryZFIN());
@@ -46,9 +44,7 @@ public class CheckIndexerJob extends AbstractValidateDataReportTask {
 
     private IndexerStatus getIndexStatus() {
         String baseUrl = "http://localhost:" + ZfinPropertiesEnum.SOLR_PORT;
-        //String baseUrl = "http://localhost:8983";
         log.debug(baseUrl);
-        System.out.println(baseUrl);
         CheckIndexerRESTInterface api = RestProxyFactory.createProxy(CheckIndexerRESTInterface.class, baseUrl, config);
         IndexerStatus response = null;
         try {
@@ -62,7 +58,7 @@ public class CheckIndexerJob extends AbstractValidateDataReportTask {
 
     public static void main(String[] args) {
         initLog4J();
-        setLoggerToInfoLevel(logger);
+        setLoggerToInfoLevel(log);
         System.out.println(args[0]);
         System.out.println(args[1]);
         CheckIndexerJob job = new CheckIndexerJob("CheckInder", args[0], args[1]);
