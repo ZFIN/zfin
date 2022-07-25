bd0c881775a3dd94d5f88c67be27a1bce43d5b37
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-07-25 13:58:00 -0700

    ZFIN-8115
    Typo

--- source/org/zfin/db/postGmakePostloaddb/1134/ZFIN-8115.sql
+++ source/org/zfin/db/postGmakePostloaddb/1134/ZFIN-8115.sql
@@ -2,6 +2,6 @@
 --changeset rtaylor:ZFIN-8115
 
 -- Use the rest API url for validating uniprot ids
-UPDATE "public"."foreign_db" SET "fdb_db_query" = 'https://rest.uniprot.org/uniprotkw/'
+UPDATE "public"."foreign_db" SET "fdb_db_query" = 'https://rest.uniprot.org/uniprotkb/'
 WHERE "fdb_db_pk_id" = 40
 AND "fdb_db_query" = 'http://www.uniprot.org/uniprot/';
