8ef3969b88d2a59cfee36815c475c53ae60fdd7c
Christian Pich <cmpich@zfin.org>
2022-08-17 12:01:50 +0200

    ZFIN-8152 add assembly info and attribution for 113 NCCR markers from 2019

---
+++ source/org/zfin/db/postGmakePostloaddb/1135/ZFIN-8152.sql
@@ -0,0 +1,36 @@
+--liquibase formatted sql
+--changeset cmpich:ZFIN-8152
+
+update sequence_feature_chromosome_location
+set sfcl_assembly = 'Zv9'
+where exists(
+              select *
+              from marker
+              where mrkr_zdb_id = sfcl_feature_zdb_id
+                and mrkr_zdb_id ~ 'ZDB-NCCR-190826'
+                and sfcl_assembly is null
+          )
+;
+
+insert into record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_type)
+select sfcl_zdb_id, 'ZDB-PUB-170214-158', 'standard' from sequence_feature_chromosome_location
+join marker on mrkr_zdb_id= sfcl_feature_zdb_id
+    and mrkr_zdb_id ~ 'ZDB-NCCR-190826';
+
+select count(*)
+from sequence_feature_chromosome_location
+         join marker on mrkr_zdb_id = sfcl_feature_zdb_id
+    and mrkr_zdb_id ~ 'ZDB-NCCR-190826' and sfcl_assembly is null
+and not exists (select * from record_attribution where sfcl_zdb_id = recattrib_data_zdb_id);
+
+select count(*)
+from sequence_feature_chromosome_location
+         join marker on mrkr_zdb_id = sfcl_feature_zdb_id
+    and mrkr_zdb_id ~ 'ZDB-NCCR-190826'
+and not exists (select * from record_attribution where sfcl_zdb_id = recattrib_data_zdb_id);
+
+select count(*)
+from sequence_feature_chromosome_location
+         join marker on mrkr_zdb_id = sfcl_feature_zdb_id
+    and mrkr_zdb_id ~ 'ZDB-NCCR-190826' and sfcl_assembly is null;
+
