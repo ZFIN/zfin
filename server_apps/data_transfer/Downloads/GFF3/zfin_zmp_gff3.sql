522da9f677f527408f47b1a42840ea1e50dff2cc
Christian Pich <cmpich@zfin.org>
2022-08-18 14:35:38 +0200

    create gff3 files per assembly for mutant and zmp

--- server_apps/data_transfer/Downloads/GFF3/zfin_zmp_gff3.sql
+++ server_apps/data_transfer/Downloads/GFF3/zfin_zmp_gff3.sql
@@ -1,16 +1,4 @@
-copy (
-  SELECT
-    sfclg_chromosome,
-    'ZMP',
-    'sequence_alteration',
-    sfclg_start,
-    sfclg_end,
-    '.',
-    '+',
-    '.',
-    'ID=' || feature_zdb_id || ';Name=' || feature_abbrev || ';zdb_id=' || feature_zdb_id || ';Alias=' || feature_zdb_id ||
-      ',' || feature_abbrev || ',' || feature_name
-  FROM sequence_feature_chromosome_location_generated
-  JOIN feature ON feature_zdb_id = sfclg_data_zdb_id
-  WHERE sfclg_assembly = 'GRCz11'
-  ORDER BY 1,4,5,9 ) to 'zfin_zmp.gff3' DELIMITER '	'
+\copy (  SELECT    sfclg_chromosome,    'ZMP',    'sequence_alteration',    sfclg_start,    sfclg_end,    '.',    '+',    '.',    'ID=' || feature_zdb_id || ';Name=' || feature_abbrev || ';zdb_id=' || feature_zdb_id || ';Alias=' || feature_zdb_id ||      ',' || feature_abbrev || ',' || feature_name  FROM sequence_feature_chromosome_location_generated  JOIN feature ON feature_zdb_id = sfclg_data_zdb_id  WHERE sfclg_assembly = 'GRCz11'  ORDER BY 1,4,5,9 ) to 'zfin_zmp_grcz11.gff3' DELIMITER '	';
+\copy (  SELECT    sfclg_chromosome,    'ZMP',    'sequence_alteration',    sfclg_start,    sfclg_end,    '.',    '+',    '.',    'ID=' || feature_zdb_id || ';Name=' || feature_abbrev || ';zdb_id=' || feature_zdb_id || ';Alias=' || feature_zdb_id ||      ',' || feature_abbrev || ',' || feature_name  FROM sequence_feature_chromosome_location_generated  JOIN feature ON feature_zdb_id = sfclg_data_zdb_id  WHERE sfclg_assembly = 'GRCz10'  ORDER BY 1,4,5,9 ) to 'zfin_zmp_grcz10.gff3' DELIMITER '	';
+\copy (  SELECT    sfclg_chromosome,    'ZMP',    'sequence_alteration',    sfclg_start,    sfclg_end,    '.',    '+',    '.',    'ID=' || feature_zdb_id || ';Name=' || feature_abbrev || ';zdb_id=' || feature_zdb_id || ';Alias=' || feature_zdb_id ||      ',' || feature_abbrev || ',' || feature_name  FROM sequence_feature_chromosome_location_generated  JOIN feature ON feature_zdb_id = sfclg_data_zdb_id  WHERE sfclg_assembly = 'Zv9'  ORDER BY 1,4,5,9 ) to 'zfin_zmp_zv9.gff3' DELIMITER '	';
+
