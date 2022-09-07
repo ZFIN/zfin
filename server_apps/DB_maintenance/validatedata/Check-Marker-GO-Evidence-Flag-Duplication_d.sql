63fd5cd523b84d8897d3384b2c58f6e31b682a46
Christian Pich <cmpich@zfin.org>
2022-09-07 11:52:01 +0200

    ZFIN-8148 add constraints to report query to suppress non-duplicate GO records

--- server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Flag-Duplication_d.sql
+++ server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Flag-Duplication_d.sql
@@ -1,24 +1,28 @@
 SELECT mrkrgoev_mrkr_zdb_id,
-       mrkrgoev_term_zdb_id, 
-       term_ont_id, 
+       mrkrgoev_term_zdb_id,
+       term_ont_id,
        mrkrgoev_source_zdb_id,
        mrkrgoev_evidence_code,
        mrkrgoev_gflag_name,
+       mrkrgoev_annotation_organization_created_by,
+       mrkrgoev_relation_term_zdb_id,
        count(*)
-FROM   marker_go_term_evidence, 
-       term 
-WHERE  NOT EXISTS (SELECT * 
-                   FROM   inference_group_member 
-                   WHERE  mrkrgoev_zdb_id = infgrmem_mrkrgoev_zdb_id)
-    AND NOT EXISTS (SELECT *
-                   FROM   marker_go_term_annotation_extension_group
-                   WHERE  mrkrgoev_zdb_id = mgtaeg_mrkrgoev_zdb_id)
-       AND mrkrgoev_gflag_name IS NULL 
-       AND mrkrgoev_term_zdb_id = term_zdb_id 
-GROUP  BY mrkrgoev_mrkr_zdb_id, 
-          mrkrgoev_term_zdb_id, 
-          term_ont_id, 
-          mrkrgoev_source_zdb_id, 
-          mrkrgoev_evidence_code,
-          mrkrgoev_gflag_name
+FROM marker_go_term_evidence,
+     term
+WHERE NOT EXISTS(SELECT *
+                 FROM inference_group_member
+                 WHERE mrkrgoev_zdb_id = infgrmem_mrkrgoev_zdb_id)
+  AND NOT EXISTS(SELECT *
+                 FROM marker_go_term_annotation_extension_group
+                 WHERE mrkrgoev_zdb_id = mgtaeg_mrkrgoev_zdb_id)
+  AND mrkrgoev_gflag_name IS NULL
+  AND mrkrgoev_term_zdb_id = term_zdb_id
+GROUP BY mrkrgoev_mrkr_zdb_id,
+         mrkrgoev_term_zdb_id,
+         term_ont_id,
+         mrkrgoev_source_zdb_id,
+         mrkrgoev_evidence_code,
+         mrkrgoev_gflag_name,
+         mrkrgoev_annotation_organization_created_by,
+         mrkrgoev_relation_term_zdb_id
 HAVING Count(*) > 1 
