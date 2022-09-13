cac099f7820ab7a0633b25983c9a6b87f7dddbf0
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2022-09-13 10:01:58 -0700

    ZFIN-8167 (#349)
    Add columns:
     - recattrib_modified_at
     - recattrib_created_at
     - recattrib_modified_count

---
+++ lib/DB_functions/record_attribution_sync_modified_at.sql
@@ -0,0 +1,17 @@
+-- last modified trigger function
+CREATE OR REPLACE FUNCTION record_attribution_sync_modified_at()
+  RETURNS trigger AS $BODY$
+BEGIN
+  NEW.recattrib_modified_at := NOW();
+  NEW.recattrib_modified_count := COALESCE(NEW.recattrib_modified_count, 0) + 1;
+RETURN NEW;
+END;
+$BODY$ LANGUAGE plpgsql;
+
+-- last modified trigger
+CREATE TRIGGER
+    record_attribution_sync_modified_at_trigger
+    BEFORE UPDATE ON
+    record_attribution
+    FOR EACH ROW EXECUTE PROCEDURE
+    record_attribution_sync_modified_at();
