418ebc5e9602054ad838c82947fa42256e570c66
Christian Pich <cmpich@zfin.org>
2022-11-21 11:00:28 +0100

    ZFIN-8296 add FPbase association

--- source/org/zfin/db/postGmakePostloaddb/1138/ZFIN-8296.sql
+++ source/org/zfin/db/postGmakePostloaddb/1138/ZFIN-8296.sql
@@ -14,5 +14,18 @@ insert into fluorescent_marker (fm_mrkr_zdb_id, fm_excitation_length, fm_emissio
       AND fe_mrkr_zdb_id = 'ZDB-EFG-221110-1'
 );
 
+-- connect new EFG with FP protein
+insert into fpprotein_efg (fe_mrkr_zdb_id, fe_fl_protein_id)
+VALUES ('ZDB-EFG-221115-1', 96);
+
+-- copy FP protein info into fluorescent_marker table
+insert into fluorescent_marker (fm_mrkr_zdb_id, fm_excitation_length, fm_emission_length, fm_protein_pk_id) (
+    select fe_mrkr_zdb_id, fp_excitation_length::INTEGER, fp_emission_length::INTEGER, fe_fl_protein_id
+    FROM fpprotein_efg,
+         fluorescent_protein
+    where fe_fl_protein_id = fp_pk_id
+      AND fe_mrkr_zdb_id = 'ZDB-EFG-221115-1'
+);
+
 -- generate the color info
 select from create_color_info();
