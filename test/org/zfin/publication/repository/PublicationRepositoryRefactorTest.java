decd079aaae5e9110fe6b89e5bd3c9945bba5807
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2022-12-07 17:02:20 -0800

    zfin-8218: refactor: getGenesAndMarkersByPublication, getDoiAttempt, getMarkersByPublicationQuery

--- test/org/zfin/publication/repository/PublicationRepositoryRefactorTest.java
+++ test/org/zfin/publication/repository/PublicationRepositoryRefactorTest.java
@@ -1,5 +1,6 @@
 package org.zfin.publication.repository;
 
+import lombok.SneakyThrows;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,20 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.zfin.AbstractDatabaseTest;
 import org.zfin.AppConfig;
 import org.zfin.expression.Figure;
+import org.zfin.framework.presentation.PaginationBean;
 import org.zfin.framework.presentation.PaginationResult;
+import org.zfin.marker.Clone;
 import org.zfin.marker.Marker;
+import org.zfin.marker.MarkerType;
 import org.zfin.marker.presentation.HighQualityProbe;
+import org.zfin.mutant.SequenceTargetingReagent;
 import org.zfin.ontology.GenericTerm;
 import org.zfin.ontology.Ontology;
 import org.zfin.ontology.repository.OntologyRepository;
 import org.zfin.publication.Journal;
 import org.zfin.publication.Publication;
 
+import java.lang.reflect.Method;
 import java.util.List;
 import static org.junit.Assert.*;
 import static org.zfin.repository.RepositoryFactory.getMarkerRepository;
@@ -206,4 +212,95 @@ public class PublicationRepositoryRefactorTest extends AbstractDatabaseTest {
 //        assertTrue(experiments.size() > 0);
 //        assertTrue(experiments.size() == 6);
     }
+
+    @Test
+    public void getGenesAndMarkersByPublication() {
+        String zdbID = "ZDB-PUB-990507-16";
+
+        List<Marker> markers = publicationRepository.getGenesAndMarkersByPublication(zdbID);
+        assertTrue(markers != null);
+        assertTrue(markers.size() > 0);
+        assertTrue(markers.size() == 2);
+
+        String zdbID2 = "ZDB-PUB-150809-4";
+
+//        List<Marker> markers2 = publicationRepository.getMarkersPulledThroughSTRs(zdbID2);
+        List<Marker> markers2 = getMarkersPulledThroughSTRs(zdbID2);
+
+        markers2 = publicationRepository.getGenesAndMarkersByPublication(zdbID2);
+        assertTrue(markers2 != null);
+        assertTrue(markers2.size() > 0);
+        assertTrue(markers2.size() == 29);
+
+//        List<Marker> markers3 = publicationRepository.getMarkersPulledThroughSTRs_New(zdbID2);
+//        assertEquals(markers3.size(), markers2.size());
+    }
+
+    @Test
+    public void getMarkersByTypeForPublication() {
+        String publicationID = "ZDB-PUB-220412-11";
+        MarkerType efgType = getMarkerRepository().getMarkerTypeByName(Marker.Type.EFG.name());
+        List<Marker> markers = publicationRepository.getMarkersByTypeForPublication(publicationID, efgType);
+
+        assertEquals(3, markers.size());
+
+    }
+
+    @Test
+    public void getSTRsByPublication() {
+        MarkerType morpholinoMarkerType = getMarkerRepository().getMarkerTypeByName("MRPHLNO");
+        MarkerType crisprMarkerType = getMarkerRepository().getMarkerTypeByName("CRISPR");
+        String publicationID = "ZDB-PUB-190215-8";
+        List<SequenceTargetingReagent> morpholinos = publicationRepository.getSTRsByPublication(publicationID, morpholinoMarkerType);
+        List<SequenceTargetingReagent> crisprs = publicationRepository.getSTRsByPublication(publicationID, crisprMarkerType);
+
+        assertEquals(2, morpholinos.size());
+        assertEquals(3, crisprs.size());
+    }
+
+    @Test
+    public void getClonesByPublication() {
+        PaginationBean paginationBean = new PaginationBean();
+        PaginationResult<Clone> clones = publicationRepository.getClonesByPublication("ZDB-PUB-080422-3", paginationBean);
+        assertNotNull(clones);
+        assertEquals(1, clones.getTotalCount());
+    }
+
+    @Test
+    public void getMarkersByPublication() throws Exception {
+        Marker.TypeGroup typeGroup = Marker.TypeGroup.GENEDOM;
+        List<MarkerType> markerTypes = getMarkerRepository().getMarkerTypesByGroup(typeGroup);
+        markerTypes.add(getMarkerRepository().getMarkerTypeByName(Marker.Type.EFG.toString()));
+
+//        test private method, similar to the following if it were a public method:
+//        List<Marker> markers1 = publicationRepository.getMarkersByPublication("ZDB-PUB-080422-3", markerTypes);
+//        List<Marker> markers2 = publicationRepository.getMarkersByPublication("ZDB-PUB-190215-8", markerTypes);
+        List<Marker> markers1 = getMarkersByPublication("ZDB-PUB-080422-3", markerTypes);
+        List<Marker> markers2 = getMarkersByPublication("ZDB-PUB-190215-8", markerTypes);
+
+        assertTrue(markers1.size() > 5);
+        assertTrue(markers2.size() > 5);
+    }
+
+    // similar to the following method call, but with a workaround to access private method:
+    //   List<Marker> markers = publicationRepository.getMarkersPulledThroughSTRs(zdbID2);
+    @SneakyThrows
+    private List<Marker> getMarkersPulledThroughSTRs(String zdbID) {
+        Method getMarkersPulledThroughSTRs = HibernatePublicationRepository.class.getDeclaredMethod(
+                "getMarkersPulledThroughSTRs", String.class);
+
+        getMarkersPulledThroughSTRs.setAccessible(true);
+        return (List<Marker>) getMarkersPulledThroughSTRs.invoke(publicationRepository, zdbID);
+    }
+
+    // similar to the following method call, but with a workaround to access private method:
+    //   List<Marker> markers = publicationRepository.getMarkersByPublication(zdbID);
+    @SneakyThrows
+    private List<Marker> getMarkersByPublication(String zdbID, List<MarkerType> markerTypes) {
+        Method getMarkersByPublication = HibernatePublicationRepository.class.getDeclaredMethod(
+                "getMarkersByPublication", String.class, List.class);
+        getMarkersByPublication.setAccessible(true);
+        return (List<Marker>) getMarkersByPublication.invoke(publicationRepository, zdbID, markerTypes);
+    }
+
 }
