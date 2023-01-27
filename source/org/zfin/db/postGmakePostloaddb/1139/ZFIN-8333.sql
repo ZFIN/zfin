cb23ca76eaaa4dcfcc0694106848f4240d5297b7
Christian Pich <cmpich@zfin.org>
2023-01-27 09:36:57 +0100

    refactor temp table generation

--- source/org/zfin/db/postGmakePostloaddb/1139/ZFIN-8333.sql
+++ source/org/zfin/db/postGmakePostloaddb/1139/ZFIN-8333.sql
@@ -1,30 +1,19 @@
 --liquibase formatted sql
 --changeset cmpich:ZFIN-8333
 
-create temp table antibody_temp
-(
-    antibody_zdb_id varchar(50),
-    gene_zdb_id     varchar(50)
-);
-
-insert into antibody_temp
-select mrel_mrkr_2_zdb_id, mrel_mrkr_1_zdb_id
+select mrel_mrkr_2_zdb_id as antibody_zdb_id, mrel_mrkr_1_zdb_id as gene_zdb_id
+INTO TEMP TABLE antibody_temp
 from marker_relationship
-where mrel_mrkr_2_zdb_id ~'ATB';
+where mrel_mrkr_2_zdb_id ~ 'ATB';
 
-create temp table count_temp
-(
-    antibody_ct_id varchar(50),
-    ct             integer
-);
-
-insert into count_temp
-select antibody_zdb_id, count(*) as ct
+select antibody_zdb_id as antibody_ct_id, count(*) as ct
+INTO TEMP TABLE count_temp
 from antibody_temp
 group by antibody_zdb_id;
 
-delete from antibody_temp
-    where antibody_zdb_id in (select antibody_ct_id from count_temp where antibody_ct_id = antibody_zdb_id and ct > 1);
+delete
+from antibody_temp
+where antibody_zdb_id in (select antibody_ct_id from count_temp where antibody_ct_id = antibody_zdb_id and ct > 1);
 
 update feature_stats
 set fstat_gene_zdb_id = (select gene_zdb_id from antibody_temp where antibody_zdb_id = fstat_feat_zdb_id)
