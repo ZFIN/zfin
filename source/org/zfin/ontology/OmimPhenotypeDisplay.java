4e5da5808ffed44aadafba8f10e72cdda6ce06e2
Christian Pich <cmpich@zfin.org>
2023-02-04 13:51:34 +0100

    disease term closure in name for search, create DB schema

--- source/org/zfin/ontology/OmimPhenotypeDisplay.java
+++ source/org/zfin/ontology/OmimPhenotypeDisplay.java
@@ -3,7 +3,6 @@ package org.zfin.ontology;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Type;
 import org.zfin.framework.api.View;
 import org.zfin.marker.Marker;
 import org.zfin.orthology.Ortholog;
@@ -23,68 +22,57 @@ import java.util.List;
 @Table(name = "UI_OMIM_PHENOTYPE_DISPLAY")
 public class OmimPhenotypeDisplay implements Serializable {
 
-    @Id
-    @JsonView(View.API.class)
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "opd_id", nullable = false)
-    private long id;
+	@Id
+	@JsonView(View.API.class)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "opd_id", nullable = false)
+	private long id;
 
-    @JsonView(View.API.class)
-    @Transient
-    private String omim;
-    @Column(name = "opd_omim_term_name")
-    private String name;
-    @JsonView(View.API.class)
-    @Transient
-    private ArrayList<String> humanGene;
-    @JsonView(View.API.class)
-    @Transient
-    private String symbol;
+	@JsonView(View.API.class)
+	@Transient
+	private String omim;
+	@Column(name = "opd_omim_term_name")
+	private String name;
+	@JsonView(View.API.class)
+	@Transient
+	private ArrayList<String> humanGene;
+	@JsonView(View.API.class)
+	@Transient
+	private String symbol;
 
-    @JsonView(View.API.class)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "opd_human_gene_id")
-    private HumanGeneDetail humanGeneDetail;
+	@JsonView(View.API.class)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "opd_human_gene_id")
+	private HumanGeneDetail humanGeneDetail;
 
-    @Transient
-    public DBLink getHumanAccession() {
-        return humanAccession;
-    }
+	@Transient
+	public DBLink getHumanAccession() {
+		return humanAccession;
+	}
 
-    @JsonView(View.API.class)
-    @Transient
-    public String omimAccession;
+	@JsonView(View.API.class)
+	@Transient
+	public String omimAccession;
 
-    @JsonView(View.API.class)
-    @Transient
-    private Ortholog orthology;
-    @Transient
-    private DBLink humanAccession;
+	@JsonView(View.API.class)
+	@Transient
+	private Ortholog orthology;
+	@Transient
+	private DBLink humanAccession;
 
-    @JsonView(View.API.class)
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JoinTable(name = "UI_OMIM_ZFIN_ASSOCIATION", joinColumns = {
-        @JoinColumn(name = "oza_human_phenotype_id", nullable = false, updatable = false)},
-        inverseJoinColumns = {@JoinColumn(name = "oza_zfin_gene_zdb_id",
-            nullable = false, updatable = false)})
-    private List<Marker> zfinGene;
-    @JsonView(View.API.class)
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "opd_term_zdb_id")
-    private GenericTerm term;
+	@JsonView(View.API.class)
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "UI_OMIM_ZFIN_ASSOCIATION", joinColumns = {
+		@JoinColumn(name = "oza_human_phenotype_id", nullable = false, updatable = false)},
+		inverseJoinColumns = {@JoinColumn(name = "oza_zfin_gene_zdb_id",
+			nullable = false, updatable = false)})
+	private List<Marker> zfinGene;
+	@JsonView(View.API.class)
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "opd_term_zdb_id")
+	private GenericTerm term;
 
-    @Type(type = "org.zfin.database.PostgreSqlStringArrayType")
-    @Column(
-        name = "opd_zfin_gene_symbol_array",
-        columnDefinition = "text[]"
-    )
-    private String[] zfinGeneSymbols;
-
-    @Type(type = "org.zfin.database.PostgreSqlStringArrayType")
-    @Column(
-        name = "opd_disease_term_name_array",
-        columnDefinition = "text[]"
-    )
-    private String[] diseaseNames;
+	@Column(name = "opd_zfin_gene_symbols_search")
+	private String zfinGeneSymbolSearch;
 
 }
