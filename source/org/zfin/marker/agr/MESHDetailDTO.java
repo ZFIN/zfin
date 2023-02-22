233aad4af40896c50700c1b9a6d747a3fce0503f
Christian Pich <cmpich@zfin.org>
2023-02-22 18:55:15 +0100

    ZFIN-8428 mark mesh terms to be ignored for the josn output as it is not needed.

--- source/org/zfin/marker/agr/MESHDetailDTO.java
+++ source/org/zfin/marker/agr/MESHDetailDTO.java
@@ -1,4 +1,5 @@
 package org.zfin.marker.agr;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,8 @@ import lombok.Setter;
 @Setter
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class MESHDetailDTO {
+
+    @JsonIgnore
     private String meshQualifierTerm;
     private String meshHeadingTerm;
     private String referenceId;
