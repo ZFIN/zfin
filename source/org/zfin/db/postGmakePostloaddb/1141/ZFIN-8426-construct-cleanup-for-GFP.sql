ada6a41d260411afe9ee85db532b597f87f4379b
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-02-27 09:07:26 -0800

    ZFIN-8426:
    More dense statement.

--- source/org/zfin/db/postGmakePostloaddb/1141/ZFIN-8426-construct-cleanup-for-GFP.sql
+++ source/org/zfin/db/postGmakePostloaddb/1141/ZFIN-8426-construct-cleanup-for-GFP.sql
@@ -1,20 +1,11 @@
 --liquibase formatted sql
 --changeset rtaylor:ZFIN-8426-construct-cleanup-for-GFP
 
--- sfGFP constructs
-SELECT mrel_mrkr_1_zdb_id
-INTO TEMP TABLE sfgfp_constructs
-FROM marker_relationship
-WHERE mrel_mrkr_2_zdb_id = 'ZDB-EFG-221115-1'
-ORDER BY mrel_mrkr_1_zdb_id;
-
--- GFP constructs
-SELECT mrel_zdb_id
-INTO TEMP TABLE gfp_relations
-FROM marker_relationship
-WHERE mrel_mrkr_2_zdb_id = 'ZDB-EFG-070117-2'
-  AND mrel_mrkr_1_zdb_id IN ( SELECT * FROM sfgfp_constructs );
-
 -- DELETE active_data, cascade delete marker_relationships, record_attributions
-DELETE FROM zdb_active_data where zactvd_zdb_id in ( SELECT mrel_zdb_id FROM gfp_relations );
-
+DELETE FROM zdb_active_data where zactvd_zdb_id in (
+    -- mr1 is sfGFP construct relationships, mr2 is GFP construct relationships
+    SELECT mr2.mrel_zdb_id
+    FROM marker_relationship mr1 JOIN marker_relationship mr2
+                                 ON mr1.mrel_mrkr_1_zdb_id = mr2.mrel_mrkr_1_zdb_id
+    WHERE mr1.mrel_mrkr_2_zdb_id = 'ZDB-EFG-221115-1'
+      AND mr2.mrel_mrkr_2_zdb_id = 'ZDB-EFG-070117-2');
