d30f3d52c50b3a8d166d5ac380f668cc686f6df4
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2023-03-01 08:02:21 -0800

    ZFIN-8448: (#542)
    Fish ID fix.

--- source/org/zfin/db/postGmakePostloaddb/1141/ZFIN-8448-withdraw-fish-with-reference.sql
+++ source/org/zfin/db/postGmakePostloaddb/1141/ZFIN-8448-withdraw-fish-with-reference.sql
@@ -2,4 +2,4 @@
 --changeset rtaylor:ZFIN-8448-withdraw-fish-with-reference
 
 INSERT INTO withdrawn_data ("wd_old_zdb_id", "wd_new_zdb_id", "wd_display_note") VALUES
-         ('ZDB-FISH-170126-22', 'ZDB-PUB-201228-1', 'This fish has been removed from our database. Please see the linked errata for more information');
+         ('ZDB-FISH-180827-12', 'ZDB-PUB-201228-1', 'This fish has been removed from our database. Please see the linked errata for more information');
