236fcb9b4606b8addc1a770263c4de58c6f5c8b6
Christian Pich <cmpich@zfin.org>
2023-04-17 18:56:28 +0200

    remove comment

--- source/org/zfin/ontology/presentation/TermAPIController.java
+++ source/org/zfin/ontology/presentation/TermAPIController.java
@@ -236,7 +236,6 @@ public class TermAPIController {
             pagination.addToFilterMap("chebiPhenotype.phenotypeStatementSearch", filterPhenotype);
         }
         if (StringUtils.isNotEmpty(filterTermName)) {
-            //pagination.addToFilterMap("chebiPhenotype.term.termName", filterTermName);
             pagination.addToFilterMap("chebiPhenotype.expConditionChebiSearch", filterTermName);
         }
 
