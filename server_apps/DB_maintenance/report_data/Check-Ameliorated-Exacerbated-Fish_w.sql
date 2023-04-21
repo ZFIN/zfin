1d211eff0421de1f69f6bb405e8bcc202db74209
Christian Pich <cmpich@zfin.org>
2023-04-21 15:23:18 +0200

    ZFIN-8541 add curator to report

--- server_apps/DB_maintenance/report_data/Check-Ameliorated-Exacerbated-Fish_w.sql
+++ server_apps/DB_maintenance/report_data/Check-Ameliorated-Exacerbated-Fish_w.sql
@@ -1,21 +1,28 @@
-select distinct fish_zdb_id, 
- 		fish_name, 
-		genox_exp_zdb_id, 
-		exp_name, 
-		psg_short_name, 
-		fig_source_zdb_id 
-    from experiment, 
-	figure,genotype, 
-	fish, 
-	fish_experiment, 
-	phenotype_source_generated, 
-	phenotype_observation_generated  
-     where (psg_short_name like '%ameliorated%' or psg_short_name like '%exacerbated%')
-	and psg_pg_id = pg_id 
-	and genox_Zdb_id = pg_genox_zdb_id 
-	and genox_fish_zdb_id = fish_zdb_id 
-	and fish_genotype_Zdb_id = geno_Zdb_id  
-	and fish_functional_Affected_gene_count = 1 
-	and genox_is_std_or_generic_control = 't' 
-	and fig_zdb_id = pg_fig_zdb_id 
-	and exp_zdb_id = genox_exp_zdb_id;
+select distinct fish_zdb_id,
+                fish_name,
+                genox_exp_zdb_id,
+                exp_name,
+                psg_short_name,
+                fig_source_zdb_id,
+                full_name
+from experiment,
+     figure,
+     genotype,
+     fish,
+     fish_experiment,
+     phenotype_source_generated,
+     phenotype_observation_generated,
+     curation,
+     person
+where (psg_short_name like '%ameliorated%' or psg_short_name like '%exacerbated%')
+  and psg_pg_id = pg_id
+  and genox_Zdb_id = pg_genox_zdb_id
+  and genox_fish_zdb_id = fish_zdb_id
+  and fish_genotype_Zdb_id = geno_Zdb_id
+  and fish_functional_Affected_gene_count = 1
+  and genox_is_std_or_generic_control = 't'
+  and fig_zdb_id = pg_fig_zdb_id
+  and exp_zdb_id = genox_exp_zdb_id
+  and cur_pub_zdb_id = fig_source_zdb_id
+  and cur_topic = 'Phenotype'
+  and zdb_id = cur_curator_zdb_id;
