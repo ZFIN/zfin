7588c5f74be2de33beb39d650394be2ba298417e
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-05-02 14:59:59 -0700

    ZFIN-8531: Fix by inserting relationships to construct_marker_relationship which then populates marker_relationship with the regen_construct_marker method.

--- source/org/zfin/db/postGmakePostloaddb/1143/ZFIN-8531-construct-relationship.sql
+++ source/org/zfin/db/postGmakePostloaddb/1143/ZFIN-8531-construct-relationship.sql
@@ -5,21 +5,20 @@
 -- ZDB-EFG-070117-1 (EGFP) - coding sequence of
 -- ZDB-GENE-991019-3 (myl7) - promoter of
 
-INSERT INTO zdb_active_data values ('ZDB-MREL-230409-1');
+INSERT INTO zdb_active_data values ('ZDB-CMREL-230409-1');
 
-INSERT INTO "public"."marker_relationship" ("mrel_zdb_id", "mrel_type", "mrel_mrkr_1_zdb_id", "mrel_mrkr_2_zdb_id", "mrel_comments")
-VALUES ('ZDB-MREL-230409-1', 'coding sequence of', 'ZDB-TGCONSTRCT-171204-1', 'ZDB-EFG-070117-1', NULL);
+INSERT INTO construct_marker_relationship (conmrkrrel_zdb_id, conmrkrrel_construct_zdb_id, conmrkrrel_mrkr_zdb_id, conmrkrrel_relationship_type)
+VALUES ('ZDB-CMREL-230409-1', 'ZDB-TGCONSTRCT-171204-1', 'ZDB-EFG-070117-1', 'coding sequence of');
 
-INSERT INTO "public"."record_attribution" ("recattrib_data_zdb_id", "recattrib_source_zdb_id", "recattrib_source_significance", "recattrib_source_type", "recattrib_created_at", "recattrib_modified_at", "recattrib_modified_count")
-VALUES ('ZDB-MREL-230409-1', 'ZDB-PUB-220706-16', NULL, 'standard', NULL, NULL, NULL);
+INSERT INTO record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_significance, recattrib_source_type, recattrib_created_at, recattrib_modified_at, recattrib_modified_count)
+VALUES ('ZDB-CMREL-230409-1', 'ZDB-PUB-220706-16', NULL, 'standard', NULL, NULL, NULL);
 
-INSERT INTO zdb_active_data values ('ZDB-MREL-230409-2');
+INSERT INTO zdb_active_data values ('ZDB-CMREL-230409-2');
 
-INSERT INTO "public"."marker_relationship" ("mrel_zdb_id", "mrel_type", "mrel_mrkr_1_zdb_id", "mrel_mrkr_2_zdb_id", "mrel_comments")
-VALUES ('ZDB-MREL-230409-2', 'promoter of', 'ZDB-TGCONSTRCT-171204-1', 'ZDB-GENE-991019-3', NULL);
+INSERT INTO construct_marker_relationship (conmrkrrel_zdb_id, conmrkrrel_construct_zdb_id, conmrkrrel_mrkr_zdb_id, conmrkrrel_relationship_type)
+VALUES ('ZDB-CMREL-230409-2', 'ZDB-TGCONSTRCT-171204-1', 'ZDB-GENE-991019-3', 'promoter of');
 
-INSERT INTO "public"."record_attribution" ("recattrib_data_zdb_id", "recattrib_source_zdb_id", "recattrib_source_significance", "recattrib_source_type", "recattrib_created_at", "recattrib_modified_at", "recattrib_modified_count")
-VALUES ('ZDB-MREL-230409-2', 'ZDB-PUB-220706-16', NULL, 'standard', NULL, NULL, NULL);
+INSERT INTO record_attribution (recattrib_data_zdb_id, recattrib_source_zdb_id, recattrib_source_significance, recattrib_source_type, recattrib_created_at, recattrib_modified_at, recattrib_modified_count)
+VALUES ('ZDB-CMREL-230409-2', 'ZDB-PUB-220706-16', NULL, 'standard', NULL, NULL, NULL);
 
 SELECT regen_construct_marker ('ZDB-TGCONSTRCT-171204-1');
-
