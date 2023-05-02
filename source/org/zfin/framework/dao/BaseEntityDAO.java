a7cc8478a2012fdd020b27c660a91ecede2e4309
Christian Pich <cmpich@zfin.org>
2023-05-02 18:48:21 +0200

    remove ^M line endings

--- source/org/zfin/framework/dao/BaseEntityDAO.java
+++ source/org/zfin/framework/dao/BaseEntityDAO.java
@@ -1,1 +1,14 @@
-package org.zfin.framework.dao;import org.zfin.framework.entity.BaseEntity;public abstract class BaseEntityDAO<E extends BaseEntity> {    protected Class<E> myClass;    protected BaseEntityDAO(Class<E> myClass) {        this.myClass = myClass;    }}
+package org.zfin.framework.dao;
+
+
+import org.zfin.framework.entity.BaseEntity;
+
+public abstract class BaseEntityDAO<E extends BaseEntity> {
+
+    protected Class<E> myClass;
+
+    protected BaseEntityDAO(Class<E> myClass) {
+        this.myClass = myClass;
+    }
+
+}
