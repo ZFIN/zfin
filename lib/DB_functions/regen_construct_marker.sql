d35efb4f1677f61df931ac3b9fc403b8513d6e08
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-05-30 09:05:58 -0700

    ZFIN-8620: regen_construct_marker only attributes a single publication per construct component relationship
    - refactored to move a bit of logic into its own function (sync_construct_marker_relationship_to_marker_relationship)
    - limit the new attributions to just the first pub

--- lib/DB_functions/regen_construct_marker.sql
+++ lib/DB_functions/regen_construct_marker.sql
@@ -1,3 +1,35 @@
+create or replace function sync_construct_marker_relationship_to_marker_relationship(constructZdbId text)
+returns void as $$
+    declare mrel_id  marker_relationship.mrel_zdb_id%TYPE;
+        mrel_1_id  marker_relationship.mrel_mrkr_1_zdb_id%TYPE;
+        mrel_2_id  marker_relationship.mrel_mrkr_2_zdb_id%TYPE;
+        mrel_type  marker_relationship.mrel_type%TYPE;
+
+    begin
+        delete from marker_relationship
+        where constructZdbID = mrel_mrkr_1_zdb_id;
+
+        for mrel_id, mrel_1_id, mrel_2_id, mrel_type in
+            select get_id('MREL'), conmrkrrel_construct_zdb_id,conmrkrrel_mrkr_zdb_id,conmrkrrel_relationship_type
+            from construct_marker_relationship
+            where conmrkrrel_construct_zdb_id = constructZdbId
+            loop
+                insert into zdb_Active_data
+                values (mrel_id);
+
+                insert into marker_relationship (mrel_zdb_id, mrel_mrkr_1_zdb_id, mrel_mrkr_2_zdb_id, mrel_type)
+                values (mrel_id, mrel_1_id, mrel_2_id, mrel_type);
+
+                insert into record_Attribution (recattrib_Data_zdb_id, recattrib_source_zdb_id)
+                select mrel_id, recattrib_source_zdb_id
+                from record_attribution
+                where recattrib_data_zdb_id = constructZdbId
+                order by recattrib_pk_id
+                limit 1 ;
+            end loop;
+    end;
+$$ LANGUAGE plpgsql;
+
 create or replace function regen_construct_marker(constructZdbId text)
 returns void as $$
 
@@ -30,26 +62,7 @@ returns void as $$
   	       	             where construct_zdb_id = mrkr_zdb_id
 		             and construct_zdb_id = constructZdbId);
 
-       delete from marker_relationship
-       	      where constructZdbID = mrel_mrkr_1_zdb_id;
-
-
-	for mrel_id, mrel_1_id, mrel_2_id, mrel_type in
- 	       select get_id('MREL'), conmrkrrel_construct_zdb_id,conmrkrrel_mrkr_zdb_id,conmrkrrel_relationship_type 
-	       	      from construct_marker_relationship
-		      where conmrkrrel_construct_zdb_id = constructZdbId 
-	    loop
-		insert into zdb_Active_data
-	       	       values (mrel_id);
-
-	insert into marker_relationship (mrel_zdb_id, mrel_mrkr_1_zdb_id, mrel_mrkr_2_zdb_id, mrel_type)
- 	       values (mrel_id, mrel_1_id, mrel_2_id, mrel_type);
-
-	insert into record_Attribution (recattrib_Data_zdb_id, recattrib_source_zdb_id)
-            select distinct mrel_id, recattrib_source_zdb_id
-    	    	   from record_attribution
-		   where recattrib_data_zdb_id = constructZdbId;
-        end loop;
+       perform sync_construct_marker_relationship_to_marker_relationship(constructZdbId);
 
     elsif exists ( select 'x' from zdb_Active_data where zactvd_zdb_id = constructZdbId) 
     then 
@@ -84,9 +97,11 @@ returns void as $$
  	       values (mrel_id, mrel_1_id, mrel_2_id, mrel_type);
 
 	insert into record_Attribution (recattrib_Data_zdb_id, recattrib_source_zdb_id)
-            select distinct mrel_id, recattrib_source_zdb_id
+            select mrel_id, recattrib_source_zdb_id
     	    	   from record_attribution
-		   where recattrib_data_zdb_id = constructZdbId;
+		   where recattrib_data_zdb_id = constructZdbId
+            order by recattrib_pk_id
+            limit 1 ;
         end loop;
     else
     
