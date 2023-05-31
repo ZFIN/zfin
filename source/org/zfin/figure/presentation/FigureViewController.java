f300aa75dab9c24d5bf7d9e165ffe4df18734402
Christian Pich <cmpich@zfin.org>
2023-05-31 18:15:17 +0200

    ZFIN-8671 remove old figure view page and replace with the prototype figure view page

--- source/org/zfin/figure/presentation/FigureViewController.java
+++ source/org/zfin/figure/presentation/FigureViewController.java
@@ -39,7 +39,7 @@ public class FigureViewController {
     private PublicationRepository publicationRepository;
 
     // get together all of the data that you need later in the JSP it returns
-    @RequestMapping("/view-prototype/{zdbID}")
+    @RequestMapping("/view/{zdbID}")
     public String getFigureViewPrototype(Model model, @PathVariable("zdbID") String zdbID) {
 
         Figure figure = figureRepository.getFigure(zdbID);
@@ -108,63 +108,8 @@ public class FigureViewController {
             "ZDB-PUB-050502-1"
         );
         model.addAttribute("isLargeDataPublication", pubs.contains(figure.getPublication().getZdbID()));
-        return "figure/figure-view-prototype";
-    }
-
-    // get together all of the data that you need later in the JSP it returns
-    @RequestMapping("/view/{zdbID}")
-    public String getFigureView(Model model, @PathVariable("zdbID") String zdbID) {
-
-        Figure figure = figureRepository.getFigure(zdbID);
-
-        if (figure == null) {
-            String replacedZdbID = RepositoryFactory.getInfrastructureRepository().getWithdrawnZdbID(zdbID);
-            if (replacedZdbID != null) {
-
-                return "redirect:/" + replacedZdbID;
-            } else {
-                model.addAttribute(LookupStrings.ZDB_ID, zdbID);
-                return LookupStrings.RECORD_NOT_FOUND_PAGE;
-            }
-        }
-
-        model.addAttribute("figure", figure);
-        Clone probe = figureViewService.getProbeForFigure(figure);
-        model.addAttribute("probe", probe);
-        if (probe != null) {
-            List<OrganizationLink> suppliers = RepositoryFactory.getProfileRepository().getSupplierLinksForZdbId(probe.getZdbID());
-            model.addAttribute("probeSuppliers", suppliers);
-        }
-
-        List<PhenotypeWarehouse> warehouseList = getPhenotypeRepository().getPhenotypeWarehouse(figure.getZdbID());
-        FigureExpressionSummary expressionSummary = figureViewService.getFigureExpressionSummary(figure);
-        model.addAttribute("expressionSummary", expressionSummary);
-        model.addAttribute("phenotypeSummary", figureViewService.getFigurePhenotypeSummary(figure));
-
-        model.addAttribute("submitters", figureRepository.getSubmitters(figure.getPublication(), expressionSummary.getProbe()));
-        model.addAttribute("showThisseInSituLink", figureViewService.showThisseInSituLink(figure.getPublication()));
-        model.addAttribute("showErrataAndNotes", figureViewService.showErrataAndNotes(figure.getPublication()));
-        model.addAttribute("showMultipleMediumSizedImages", figureViewService.showMultipleMediumSizedImages(figure.getPublication()));
-
-        List<ExpressionTableRow> expressionTableRows = figureViewService.getExpressionTableRows(figure);
-        model.addAttribute("expressionTableRows", expressionTableRows);
-        model.addAttribute("showExpressionQualifierColumn", figureViewService.showExpressionQualifierColumn(expressionTableRows));
-
-        List<AntibodyTableRow> antibodyTableRows = figureViewService.getAntibodyTableRows(figure);
-        model.addAttribute("antibodyTableRows", antibodyTableRows);
-        model.addAttribute("showAntibodyQualifierColumn", figureViewService.showAntibodyQualifierColumn(antibodyTableRows));
-
-        List<PhenotypeTableRow> phenotypeTableRows = figureViewService.getPhenotypeTableRows(warehouseList);
-        model.addAttribute("phenotypeTableRows", phenotypeTableRows);
-
-        model.addAttribute(LookupStrings.DYNAMIC_TITLE, "Figure: " + figureViewService.getFullFigureLabel(figure));
-
-        model.addAttribute("showElsevierMessage", figureViewService.showElsevierMessage(figure.getPublication()));
-        model.addAttribute("hasAcknowledgment", figureViewService.hasAcknowledgment(figure.getPublication()));
-
         return "figure/figure-view";
     }
-
 
     // get together all the data that you need later in the JSP it returns
     @RequestMapping("/all-figure-view/{zdbID}")
