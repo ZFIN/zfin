637a871d48c680ab2353d49d506e7ce4403f43e2
Christian Pich <cmpich@zfin.org>
2023-06-29 17:49:27 +0200

    ZFIN-8674 fix Update-Transcript-Sequences_w jenkins job

--- server_apps/data_transfer/RNACentral/loadTranscriptSequences.sql
+++ server_apps/data_transfer/RNACentral/loadTranscriptSequences.sql
@@ -1,10 +1,21 @@
 -- loadTranscriptSequences.sql
 
 begin work;
-create  table tscriptsequence (tscriptid text, tscriptottdart text, tscriptseq text );
+drop table if exists tscriptsequence;
+create table tscriptsequence
+(
+    tscriptid      text,
+    tscriptottdart text,
+    tscriptseq     text
+);
 
 \copy tscriptsequence from '<!--|ROOT_PATH|-->/server_apps/data_transfer/RNACentral/loadedSeq.txt' delimiter E'\t';
-insert into transcript_sequence (select tscriptid, tscriptottdart, substring(tscriptseq from 2 for length(tscriptseq))  from tscriptsequence where tscriptseq!='') ;
+
+delete from transcript_sequence;
+
+insert into transcript_sequence (ts_transcript_zdb_id, ts_transcript_ottdart_id, ts_sequence) (select tscriptid, tscriptottdart, substring(tscriptseq from 2 for length(tscriptseq))  from tscriptsequence where tscriptseq!='') ;
+
+drop table tscriptsequence;
 
 commit work;
 
