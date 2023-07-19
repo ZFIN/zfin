62002417f48a5332bf28b7d8f81fd032937b3acb
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-07-19 08:51:50 -0700

    ZFIN-8621: Account for conflicts.

--- source/org/zfin/db/postGmakePostloaddb/1146/ZFIN-8621.sql
+++ source/org/zfin/db/postGmakePostloaddb/1146/ZFIN-8621.sql
@@ -1,13 +1,28 @@
 --liquibase formatted sql
 --changeset rtaylor:ZFIN-8621.sql
 
-UPDATE
-    record_attribution
-SET
-    recattrib_source_zdb_id = 'ZDB-PUB-180514-4'
-WHERE
-    recattrib_source_zdb_id = ''
-    AND recattrib_data_zdb_id ILIKE '%180717%';
+-- create temporary table
+CREATE TEMP TABLE rec_to_add AS
+SELECT *
+FROM record_attribution
+WHERE recattrib_source_zdb_id = ''
+and recattrib_data_zdb_id ilike '%180717%';
+
+
+-- update the temporary table
+UPDATE rec_to_add
+SET recattrib_source_zdb_id = 'ZDB-PUB-180514-4';
+
+
+-- insert from the temporary table, ignoring duplicates
+INSERT INTO record_attribution (recattrib_data_zdb_id,recattrib_source_zdb_id,recattrib_source_significance,recattrib_source_type,recattrib_created_at,recattrib_modified_at,recattrib_modified_count)
+SELECT recattrib_data_zdb_id,recattrib_source_zdb_id,recattrib_source_significance,recattrib_source_type,recattrib_created_at,recattrib_modified_at,recattrib_modified_count
+FROM rec_to_add
+ON CONFLICT (recattrib_data_zdb_id,recattrib_source_zdb_id,recattrib_source_type) DO NOTHING;
+
+
+-- delete from the record_attribution table
+DELETE FROM record_attribution WHERE recattrib_pk_id in (select recattrib_pk_id from rec_to_add);
 
 
 -- This is a temporary table that was created in HBurgess.sql (liquibase) but never deleted.
