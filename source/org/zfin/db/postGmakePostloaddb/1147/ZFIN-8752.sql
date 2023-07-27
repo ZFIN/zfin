f0cb09759bccd5378511e6e02819dcff5287d045
Christian Pich <cmpich@zfin.org>
2023-07-27 12:34:44 +0200

    ZFIN-8752 tmp into TEMP for Report-Genetic-INteractions_q.sql file

--- source/org/zfin/db/postGmakePostloaddb/1147/ZFIN-8752.sql
+++ source/org/zfin/db/postGmakePostloaddb/1147/ZFIN-8752.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
 --changeset cmpich:ZFIN-8752.sql
 
+-- Report-Genetic-Interactions2
 drop table if exists tmp_go;
 drop table if exists tmp_go1;
 drop table if exists tmp_go4;
@@ -13,3 +14,10 @@ drop table if exists jenk491;
 drop table if exists jenk491b;
 drop table if exists final491;
 drop table if exists tmp_final;
+
+-- Report-Genetic-Interactions
+drop table if exists tmp_fish_thatfitthebill;
+drop table if exists tmp_fishnoeap;
+drop table if exists tmp_onlyfish;
+drop table if exists tmp_fish490;
+drop table if exists tmp_onlydeficiencies;
