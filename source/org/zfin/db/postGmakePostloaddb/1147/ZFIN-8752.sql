7fa012a0e06c10c81437f635e3a8370cf7ff315d
Christian Pich <cmpich@zfin.org>
2023-07-27 12:16:20 +0200

    ZFIN-8752 make tmp table true TEMP tables

---
+++ source/org/zfin/db/postGmakePostloaddb/1147/ZFIN-8752.sql
@@ -0,0 +1,15 @@
+--liquibase formatted sql
+--changeset cmpich:ZFIN-8752.sql
+
+drop table if exists tmp_go;
+drop table if exists tmp_go1;
+drop table if exists tmp_go4;
+drop table if exists tmp_go5;
+drop table if exists tmp_go;
+drop table if exists finalgo;
+drop table if exists finalct3;
+drop table if exists finaltemp;
+drop table if exists jenk491;
+drop table if exists jenk491b;
+drop table if exists final491;
+drop table if exists tmp_final;
