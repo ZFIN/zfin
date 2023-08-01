560031087211006c41964085c1556ad2e241b719
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-08-01 14:03:23 -0700

    zfin-8711:
    Add react components to replace angular for figure image uploads.
    Remove unused angular directives

---
+++ home/javascript/react/components/figure-edit/image-element.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const ImageElement = ({ image, deleteImage }) => (
+    <span className='image-edit-image'>
+        <img src={image.thumbnailPath} alt=''/>
+        <span
+            className='image-delete-button'
+            onClick={() => deleteImage(image)}
+            title='Remove image'
+        >
+            <span className='fa-stack fa-lg'>
+                <i className='fas fa-circle fa-stack-1x'/>
+                <i className='fas fa-times-circle fa-stack-1x'/>
+            </span>
+        </span>
+    </span>
+);
+
+export default ImageElement;
