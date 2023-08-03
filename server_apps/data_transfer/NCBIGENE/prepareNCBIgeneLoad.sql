27b96a6826b55a8471cc5df9ce0997e03c142e5f
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-08-03 13:27:56 -0700

    ZFIN-8722:
    Add conveniences for troubleshooting issues:
    
    - can specify SENDMAIL_COMMAND by env var
    - can specify OVERRIDE_JAVA_HOME by env var

--- server_apps/data_transfer/NCBIGENE/prepareNCBIgeneLoad.sql
+++ server_apps/data_transfer/NCBIGENE/prepareNCBIgeneLoad.sql
@@ -121,11 +121,11 @@ select count(gene) as numberOfGenesWithRNAevidence from genes_supported_by_rna;
 
 --!echo 'Dump the list of genes supported by GenBank RNA sequenecs, as the start set on ZFIN end for mapping'
 
-\copy (select distinct gene from genes_supported_by_rna) to '<!--|ROOT_PATH|-->/server_apps/data_transfer/NCBIGENE/toMap.unl' (delimiter '|');
+\copy (select distinct gene from genes_supported_by_rna) to 'toMap.unl' (delimiter '|');
 
 --!echo 'Dump the delete list'
 
-\copy (select * from pre_delete order by dblink_loaded_zdb_id) to '<!--|ROOT_PATH|-->/server_apps/data_transfer/NCBIGENE/toDelete.unl' (delimiter '|');
+\copy (select * from pre_delete order by dblink_loaded_zdb_id) to 'toDelete.unl' (delimiter '|');
 
 drop view genes_supported_by_rna;
 
