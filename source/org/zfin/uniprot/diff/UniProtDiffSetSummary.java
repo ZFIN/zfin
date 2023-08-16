e01cd0f7d2c6ca1f7f74da5e02982315b92592e4
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-08-16 16:18:49 -0700

    Convert from lombok to record

--- source/org/zfin/uniprot/diff/UniProtDiffSetSummary.java
+++ source/org/zfin/uniprot/diff/UniProtDiffSetSummary.java
@@ -2,31 +2,16 @@ package org.zfin.uniprot.diff;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
-public class UniProtDiffSetSummary {
-
-    private int added;
-    private int removed;
-    private int changed;
-    private int total;
-
-    @JsonProperty("changed RefSeq")
-    private int changedRefSeq;
-
-    @JsonProperty("changed ZFIN")
-    private int changedZFIN;
-
-    @JsonProperty("changed GeneID")
-    private int changedGeneID;
-
-    @JsonProperty("latest update in set 1")
-    private String latestUpdateFromSequence1;
-    @JsonProperty("latest update in set 2")
-    private String latestUpdateFromSequence2;
-
-
-}
+@JsonSerialize
+public record UniProtDiffSetSummary(
+        int added,
+        int removed,
+        int changed,
+        int total,
+        @JsonProperty("changed RefSeq") int changedRefSeq,
+        @JsonProperty("changed ZFIN") int changedZFIN,
+        @JsonProperty("changed GeneID") int changedGeneID,
+        @JsonProperty("latest update in set 1") String latestUpdateFromSequence1,
+        @JsonProperty("latest update in set 2") String latestUpdateFromSequence2
+) {}
