fad77e13553f3c15e744fcaaa16eb6df7dcf321e
Christian Pich <cmpich@zfin.org>
2023-08-24 09:41:21 +0200

    ZFIN-8751 make Check-Marker-GO-Evidence-Flag-Duplication_d Jenkins job monthly and suppress one record from the report (#774)

--- server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Flag-Duplication_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Flag-Duplication_m.sql
@@ -17,6 +17,12 @@ WHERE NOT EXISTS(SELECT *
                  WHERE mrkrgoev_zdb_id = mgtaeg_mrkrgoev_zdb_id)
   AND mrkrgoev_gflag_name IS NULL
   AND mrkrgoev_term_zdb_id = term_zdb_id
+  -- do not report the following record as it has been validated by curators
+  AND not (mrkrgoev_mrkr_zdb_id = 'ZDB-GENE-990708-5'
+    AND mrkrgoev_term_zdb_id = 'ZDB-TERM-091209-2230'
+    AND term_ont_id = 'GO:0003170'
+    AND mrkrgoev_source_zdb_id = 'ZDB-PUB-031014-8'
+    and mrkrgoev_relation_term_zdb_id = 'ZDB-TERM-180228-5')
 GROUP BY mrkrgoev_mrkr_zdb_id,
          mrkrgoev_term_zdb_id,
          term_ont_id,
@@ -25,4 +31,4 @@ GROUP BY mrkrgoev_mrkr_zdb_id,
          mrkrgoev_gflag_name,
          mrkrgoev_annotation_organization_created_by,
          mrkrgoev_relation_term_zdb_id
-HAVING Count(*) > 1 
+HAVING Count(*) > 1;
