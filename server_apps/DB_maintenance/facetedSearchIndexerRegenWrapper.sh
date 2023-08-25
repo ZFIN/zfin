26b9df2c0d6ae411159498d78a3b035117000702
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-08-25 12:55:43 -0700

    ZFIN-8351: deadlock updates
    Since the deadlock always occurs upon trying to delete genotype_figure_fast_search,
    this code avoids dropping the table and instead renames it and its indexes.
    
    The renamed version of the table is later be deleted by regenPhenotypeMartCleanup.sh

---
+++ server_apps/DB_maintenance/facetedSearchIndexerRegenWrapper.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+set -o pipefail
+
+main() {
+  cd $TARGETROOT/server_apps/DB_maintenance/warehouse && ./regenPhenotypeMart.sh
+  cd $TARGETROOT/server_apps/DB_maintenance/warehouse && ./regenExpressionMart.sh
+  cd $TARGETROOT/server_apps/DB_maintenance && ./regen.sh
+}
+
+# Set up a trap to catch errors and exit
+handle_error() {
+    echo "PROBLEM ENCOUNTERED"
+    echo "TODO: Add error handling here if we need any"
+}
+trap handle_error EXIT
+
+# Call the main function
+main
+
+# Reset the trap so it won't trigger on a normal exit
+trap - EXIT
