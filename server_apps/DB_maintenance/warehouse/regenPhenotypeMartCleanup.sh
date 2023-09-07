70bfc8f1f44b4f3d3d7ffe6c20c436948fa9889f
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2023-09-07 09:04:52 -0700

    ZFIN-8351: One more bugfix: (#797)
    - forgot to include the "select" part of running a function
    - take into account constraints when dropping indexes

--- server_apps/DB_maintenance/warehouse/regenPhenotypeMartCleanup.sh
+++ server_apps/DB_maintenance/warehouse/regenPhenotypeMartCleanup.sh
@@ -5,7 +5,7 @@ for i in clean_expression mutant genotype_figure
 do
     TABLE=${i}_fast_search_old_
     echo "Cleaning $TABLE";
-    echo "regen_cleanup_renamed_tables('$TABLE')" | ${PGBINDIR}/psql -v ON_ERROR_STOP=1 $DBNAME;
+    echo "select regen_cleanup_renamed_tables('$TABLE')" | ${PGBINDIR}/psql -v ON_ERROR_STOP=1 $DBNAME;
     if [ $? -ne 0 ]; then
         echo "regen_cleanup_renamed_tables('$TABLE') failed";
         exit 1;
