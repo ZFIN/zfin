8755f81875c42489b805fdfd5ce20acef4d8adb0
Christian Pich <cmpich@zfin.org>
2023-09-21 11:00:18 +0200

    fix sql error in LB script

--- source/org/zfin/db/postGmakePostloaddb/1148/ZFIN-8816.sql
+++ source/org/zfin/db/postGmakePostloaddb/1148/ZFIN-8816.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 --changeset cmpich:ZFIN-8816.sql
 
-select convert_gene_to_ncrna('ZDB-GENE-030616-59');
+delete from ui.publication_expression_display;
 select convert_gene_to_ncrna('ZDB-GENE-031010-46');
 select convert_gene_to_ncrna('ZDB-GENE-041001-123');
 select convert_gene_to_ncrna('ZDB-GENE-030131-2681');
