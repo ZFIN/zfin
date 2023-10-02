810045626622dc5e9c661d5bfa3658c606d28a73
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-10-02 14:04:19 -0700

    ZFIN-7053: Fix selectors for finding external links to non-"zfin.org" sites

--- home/javascript/zfin-common/open-external-links-in-new-tab.js
+++ home/javascript/zfin-common/open-external-links-in-new-tab.js
@@ -1,8 +1,8 @@
 // when the page loads, find all external links and set attributes: target="_blank" and rel="noopener noreferrer"
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('a.external',
-            'a[href^="http://"]:not([href*="zfin.org"])',
-            'a[href^="https://"]:not([href*="zfin.org"])')
+    document.querySelectorAll(`a.external,
+            a[href^="http://"]:not([href*="zfin.org"]),
+            a[href^="https://"]:not([href*="zfin.org"])`)
         .forEach(link => {
             link.setAttribute('target', '_blank');
             link.setAttribute('rel', 'noopener noreferrer');
