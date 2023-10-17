53a85c269f502092c3bed5482d169bec4bbfb9ed
Christian Pich <cmpich@zfin.org>
2023-10-17 17:58:20 +0200

    ZFIN-8868 (#839)
    * Add ResourceBundleMessageSource to configuration
    
    * remove ui schema from temp table

--- source/org/zfin/framework/ZfinConfiguration.java
+++ source/org/zfin/framework/ZfinConfiguration.java
@@ -3,10 +3,12 @@ package org.zfin.framework;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.stereotype.Repository;
 
@@ -28,6 +30,13 @@ public class ZfinConfiguration {
         return builder;
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
     @Bean
     @Primary
     public ObjectMapper objectMapper(Jackson2ObjectMapperBuilder builder) {
