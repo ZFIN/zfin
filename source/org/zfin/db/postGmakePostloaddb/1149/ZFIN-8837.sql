f63e365ef56a61441497d8585f2d9dc116fe63f2
Christian Pich <cmpich@zfin.org>
2023-10-31 15:18:25 +0100

    ZFIN-8837 fix script to handle proper pub_tracking_history records

--- source/org/zfin/db/postGmakePostloaddb/1149/ZFIN-8837.sql
+++ source/org/zfin/db/postGmakePostloaddb/1149/ZFIN-8837.sql
@@ -1,13 +1,10 @@
 --liquibase formatted sql
 --changeset cmpich:ZFIN-8837.sql
 
-select *
-from pub_tracking_history
-where pth_location_id = 17;
-
-update pub_tracking_history
-set pth_status_id = 13
+create temp table pub_tracking_history_temp as
+select * from pub_tracking_history
 where pth_location_id = 17
+and pth_status_is_current = 't'
   and not exists(select 'c'
                  from record_attribution
                           left outer join marker on recattrib_data_zdb_id = mrkr_zdb_id
@@ -15,3 +12,13 @@ where pth_location_id = 17
                  where recattrib_source_zdb_id = pth_pub_zdb_id
                    and (mrkr_zdb_id is not null or feature_zdb_id is not null)
     );
+
+update pub_tracking_history_temp
+set pth_location_id = null,
+    pth_status_id = 13,
+    pth_status_set_by = 'ZDB-PERS-030612-1';
+
+insert into pub_tracking_history (pth_pub_zdb_id, pth_status_id, pth_location_id, pth_status_set_by, pth_claimed_by, pth_status_insert_date, pth_status_is_current, pth_status_made_non_current_date, pth_days_in_status)
+select pth_pub_zdb_id, pth_status_id, pth_location_id, pth_status_set_by, pth_claimed_by, pth_status_insert_date, pth_status_is_current, pth_status_made_non_current_date, pth_days_in_status
+from pub_tracking_history_temp;
+
