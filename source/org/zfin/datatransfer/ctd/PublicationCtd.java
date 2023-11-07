9f8360125a149f9999a33246ea08f286c251c9c4
Christian Pich <cmpich@zfin.org>
2023-11-07 13:35:45 +0100

    remove ^M characters

--- source/org/zfin/datatransfer/ctd/PublicationCtd.java
+++ source/org/zfin/datatransfer/ctd/PublicationCtd.java
@@ -1,1 +1,41 @@
-package org.zfin.datatransfer.ctd;import lombok.Data;import org.apache.commons.lang3.builder.EqualsBuilder;import org.apache.commons.lang3.builder.HashCodeBuilder;import org.zfin.publication.Publication;import javax.persistence.*;@Entity@Table(name = "publication_ctd")@Datapublic class PublicationCtd {    @Id    @GeneratedValue(strategy = GenerationType.IDENTITY)    @Column(name = "pc_id")    private long id;    @Column(name = "pc_ctd_id")    private String ctdID;    @ManyToOne(fetch = FetchType.LAZY)    @JoinColumn(name = "pc_pub_zdb_id")    private Publication publication;    @Override    public boolean equals(Object o) {        if (this == o) return true;        if (o == null || getClass() != o.getClass()) return false;        PublicationCtd that = (PublicationCtd) o;        return new EqualsBuilder().append(getCtdID(), that.getCtdID()).append(getPublication(), that.getPublication()).isEquals();    }    @Override    public int hashCode() {        return new HashCodeBuilder(17, 37).append(getCtdID()).append(getPublication()).toHashCode();    }}
+package org.zfin.datatransfer.ctd;
+
+import lombok.Data;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.zfin.publication.Publication;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "publication_ctd")
+@Data
+public class PublicationCtd {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pc_id")
+    private long id;
+    @Column(name = "pc_ctd_id")
+    private String ctdID;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pc_pub_zdb_id")
+    private Publication publication;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PublicationCtd that = (PublicationCtd) o;
+
+        return new EqualsBuilder().append(getCtdID(), that.getCtdID()).append(getPublication(), that.getPublication()).isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder(17, 37).append(getCtdID()).append(getPublication()).toHashCode();
+    }
+}
--- source/org/zfin/datatransfer/ctd/PublicationCtdDAO.java
+++ source/org/zfin/datatransfer/ctd/PublicationCtdDAO.java
@@ -1,1 +1,49 @@
-package org.zfin.datatransfer.ctd;import org.hibernate.Session;import org.hibernate.query.Query;import java.util.List;public class PublicationCtdDAO {    protected Session entityManager;    public PublicationCtdDAO(Session entityManager) {        this.entityManager = entityManager;    }    public PublicationCtd find(long id) {        return entityManager.load(PublicationCtd.class, id);    }    public PublicationCtd find(String pubID) {        String hql = """            from PublicationCtd where            publication.zdbID = :pubID            """;        Query<PublicationCtd> query = entityManager.createQuery(hql, PublicationCtd.class);        query.setParameter("pubID", pubID);        return query.uniqueResult();    }    public PublicationCtd persist(PublicationCtd publicationCtd) {        entityManager.save(publicationCtd);        return publicationCtd;    }    public void delete(PublicationCtd publicationCtd) {        entityManager.delete(publicationCtd);    }    public List<PublicationCtd> findAll() {        return forceRetrieveAll();    }    public List<PublicationCtd> forceRetrieveAll() {        return entityManager.createQuery("from PublicationCtd ", PublicationCtd.class).getResultList();    }}
+package org.zfin.datatransfer.ctd;
+
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class PublicationCtdDAO {
+
+    protected Session entityManager;
+
+    public PublicationCtdDAO(Session entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    public PublicationCtd find(long id) {
+        return entityManager.load(PublicationCtd.class, id);
+
+    }
+
+    public PublicationCtd find(String pubID) {
+        String hql = """
+            from PublicationCtd where
+            publication.zdbID = :pubID
+            """;
+        Query<PublicationCtd> query = entityManager.createQuery(hql, PublicationCtd.class);
+        query.setParameter("pubID", pubID);
+        return query.uniqueResult();
+
+    }
+
+    public PublicationCtd persist(PublicationCtd publicationCtd) {
+        entityManager.save(publicationCtd);
+        return publicationCtd;
+    }
+
+    public void delete(PublicationCtd publicationCtd) {
+        entityManager.delete(publicationCtd);
+    }
+
+    public List<PublicationCtd> findAll() {
+        return forceRetrieveAll();
+    }
+
+    public List<PublicationCtd> forceRetrieveAll() {
+        return entityManager.createQuery("from PublicationCtd ", PublicationCtd.class).getResultList();
+    }
+
+}
