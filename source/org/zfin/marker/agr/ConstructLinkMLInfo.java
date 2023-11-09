496c39c2393e9f6020d420f1fef4de3bfdc18eb2
Christian Pich <cmpich@zfin.org>
2023-11-09 22:52:31 +0100

    ZFIN-8904 Fix construct submission and create variant link ml submission class to Alliance (#860)
    * ZFIN-8904 fix and create construct submission files to Alliance with version 0.27 and 1.11 scheam version
    
    * ZFIN-8904 create Variant LInk ML  file for Alliance
    
    * small refactoring
    
    * do not include construct components that do not have an accepted type

--- source/org/zfin/marker/agr/ConstructLinkMLInfo.java
+++ source/org/zfin/marker/agr/ConstructLinkMLInfo.java
@@ -4,11 +4,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.alliancegenome.curation_api.model.ingest.dto.ConstructDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.DataProviderDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.IngestDTO;
+import org.alliancegenome.curation_api.model.ingest.dto.associations.constructAssociations.ConstructGenomicEntityAssociationDTO;
+import org.alliancegenome.curation_api.model.ingest.dto.slotAnnotions.NameSlotAnnotationDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.slotAnnotions.constructSlotAnnotations.ConstructComponentSlotAnnotationDTO;
 import org.apache.commons.collections4.CollectionUtils;
 import org.zfin.construct.ConstructComponent;
+import org.zfin.gwt.root.util.StringUtils;
 import org.zfin.infrastructure.ActiveData;
 import org.zfin.marker.Marker;
 import org.zfin.marker.MarkerAlias;
@@ -43,7 +47,7 @@ public class ConstructLinkMLInfo extends LinkMLInfo {
         initAll();
         IngestDTO ingestDTO = getIngestDTO();
         List<org.alliancegenome.curation_api.model.ingest.dto.ConstructDTO> allDiseaseDTO = getAllConstructInfo();
-        ingestDTO.setLinkMLVersion("v1.9.0");
+        ingestDTO.setLinkMLVersion("v1.11.0");
         ingestDTO.setConstructIngestSet(allDiseaseDTO);
         ObjectMapper mapper = new ObjectMapper();
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
@@ -52,7 +56,18 @@ public class ConstructLinkMLInfo extends LinkMLInfo {
         try (PrintStream out = new PrintStream(new FileOutputStream("ZFIN_Construct_ml.json"))) {
             out.print(jsonInString);
         }
+
+        IngestDTO ingestDTOGenomicAssociations = getIngestDTO();
+        ingestDTOGenomicAssociations.setLinkMLVersion("v1.11.0");
+        ingestDTOGenomicAssociations.setConstructGenomicEntityAssociationIngestSet(genomicEntityAssociationDTOList);
+        jsonInString = writer.writeValueAsString(ingestDTOGenomicAssociations);
+        try (PrintStream out = new PrintStream(new FileOutputStream("ZFIN_Construct_Association_ml.json"))) {
+            out.print(jsonInString);
+        }
+
     }
+
+    private List<ConstructGenomicEntityAssociationDTO> genomicEntityAssociationDTOList = new ArrayList<>();
 
     public List<org.alliancegenome.curation_api.model.ingest.dto.ConstructDTO> getAllConstructInfo() {
         List<Marker> allConstructs = getConstructRepository().getAllConstructs();
@@ -62,13 +77,24 @@ public class ConstructLinkMLInfo extends LinkMLInfo {
             .map(
                 construct -> {
 
-                    org.alliancegenome.curation_api.model.ingest.dto.ConstructDTO dto = new org.alliancegenome.curation_api.model.ingest.dto.ConstructDTO();
-                    dto.setName(construct.getName());
-                    org.alliancegenome.curation_api.model.ingest.dto.DataProviderDTO dataProvider = new DataProviderDTO();
+                    ConstructDTO dto = new ConstructDTO();
+                    NameSlotAnnotationDTO name = new NameSlotAnnotationDTO();
+                    name.setDisplayText(construct.getName());
+                    name.setFormatText(construct.getName());
+                    name.setNameTypeName("full_name");
+                    dto.setConstructFullNameDto(name);
+
+                    NameSlotAnnotationDTO symbol = new NameSlotAnnotationDTO();
+                    symbol.setDisplayText(construct.getAbbreviation());
+                    symbol.setFormatText(construct.getAbbreviation());
+                    symbol.setNameTypeName("nomenclature_symbol");
+                    dto.setConstructSymbolDto(symbol);
+
+                    DataProviderDTO dataProvider = new DataProviderDTO();
                     dataProvider.setSourceOrganizationAbbreviation("ZFIN");
                     dto.setDataProviderDto(dataProvider);
                     dto.setCreatedByCurie("ZFIN:CURATOR");
-                    dto.setTaxonCurie(ZfinDTO.taxonId);
+                    //dto.setTaxonCurie(ZfinDTO.taxonId);
                     dto.setModEntityId("ZFIN:" + construct.getZdbID());
                     GregorianCalendar date = ActiveData.getDateFromId(construct.getZdbID());
                     dto.setDateCreated(format(date));
@@ -76,85 +102,69 @@ public class ConstructLinkMLInfo extends LinkMLInfo {
 
                     List<ConstructComponent> components = getConstructRepository().getConstructComponentsByConstructZdbId(construct.getZdbID());
                     List<ConstructComponentSlotAnnotationDTO> componentDTOs = new ArrayList<>();
+
                     if (CollectionUtils.isNotEmpty(components)) {
                         for (ConstructComponent component : components) {
-                            ConstructComponentSlotAnnotationDTO componentSlotAnnotationDTO = new ConstructComponentSlotAnnotationDTO();
-                            componentSlotAnnotationDTO.setComponentSymbol(component.getComponentValue());
-/*
-                            componentSlotAnnotationDTO.setTaxonCurie(ZfinDTO.taxonId);
-                            componentSlotAnnotationDTO.setTaxonText(ZfinDTO.taxonId);
-*/
+                            String componentZdbID = component.getComponentZdbID();
+                            if (componentZdbID != null) {
+                                boolean isGeneOrRNAG = componentZdbID.startsWith("ZDB-GENE") || (componentZdbID.contains("RNAG"));
+                                String relationName = populateRelationship(component);
+                                if(StringUtils.isEmpty(relationName)){
+                                    continue;
+                                }
+                                if (isGeneOrRNAG) {
+                                    switch (component.getType()) {
+                                        case CODING_SEQUENCE_OF, CODING_SEQUENCE_OF_, PROMOTER_OF, PROMOTER_OF_ -> {
+                                            ConstructGenomicEntityAssociationDTO associationDTO = new ConstructGenomicEntityAssociationDTO();
+                                            associationDTO.setConstructIdentifier("ZFIN:" + construct.zdbID);
+                                            associationDTO.setGenomicEntityCurie("ZFIN:" + componentZdbID);
+                                            associationDTO.setGenomicEntityRelationName(relationName);
+                                            genomicEntityAssociationDTOList.add(associationDTO);
+                                        }
+                                        default -> {
+                                        }
+                                    }
+                                } else if (!componentZdbID.startsWith("ZDB-CV")) {
 
-                            ConstructComponentDTO componentDTO = new ConstructComponentDTO();
-                            // currently only populate the component ZDB id when we've already submitted the gene in the BGI (ie: no EFGs, regions at this time)
-                            if (component.getComponentZdbID() != null) {
-                                if ((component.getComponentZdbID().startsWith("ZDB-GENE") || (component.getComponentZdbID().contains("RNAG"))))
-                                    componentDTO.setComponentID("ZFIN:" + component.getComponentZdbID());
+                                    ConstructComponentSlotAnnotationDTO componentSlotAnnotationDTO = new ConstructComponentSlotAnnotationDTO();
+                                    componentSlotAnnotationDTO.setComponentSymbol(component.getComponentValue());
+                                    componentSlotAnnotationDTO.setRelationName(relationName);
+                                    componentSlotAnnotationDTO.setTaxonCurie(ZfinDTO.taxonId);
+                                    componentSlotAnnotationDTO.setTaxonText(ZfinDTO.taxonId);
+                                    componentDTOs.add(componentSlotAnnotationDTO);
+                                }
                             }
-                            if (component.getType().equals(ConstructComponent.Type.CODING_SEQUENCE_OF) || component.getType().equals(ConstructComponent.Type.CODING_SEQUENCE_OF_)) {
-                                componentDTO.setComponentRelation("expresses");
-                            } else if (component.getType().equals(ConstructComponent.Type.PROMOTER_OF) || component.getType().equals(ConstructComponent.Type.PROMOTER_OF_)) {
-                                componentDTO.setComponentRelation("is_regulated_by");
-                            } else {
-                                continue;
-                            }
-                            componentDTOs.add(componentSlotAnnotationDTO);
                         }
                     }
                     dto.setConstructComponentDtos(componentDTOs);
-/*
-                    List<ConstructComponent> components = getConstructRepository().getConstructComponentsByConstructZdbId(construct.getZdbID());
-                    List<ConstructComponentDTO> componentDTOs = new ArrayList<>();
-                    if (CollectionUtils.isNotEmpty(components)) {
-                        for (ConstructComponent component : components) {
-                            ConstructComponentDTO componentDTO = new ConstructComponentDTO();
-                            // currently only populate the component ZDB id when we've already submitted the gene in the BGI (ie: no EFGs, regions at this time)
-                            if (component.getComponentZdbID() != null) {
-                                if ((component.getComponentZdbID().startsWith("ZDB-GENE")||(component.getComponentZdbID().contains("RNAG"))))
-                                    componentDTO.setComponentID("ZFIN:" + component.getComponentZdbID());
-                            }
-                            if (component.getType().equals(ConstructComponent.Type.CODING_SEQUENCE_OF) || component.getType().equals(ConstructComponent.Type.CODING_SEQUENCE_OF_)) {
-                                componentDTO.setComponentRelation("expresses");
-                            } else if (component.getType().equals(ConstructComponent.Type.PROMOTER_OF) || component.getType().equals(ConstructComponent.Type.PROMOTER_OF_)) {
-                                componentDTO.setComponentRelation("is_regulated_by");
-                            } else {
-                                continue;
-                            }
-                            componentDTO.setComponentSymbol(component.getComponentValue());
-                            componentDTOs.add(componentDTO);
-                        }
-                    }
-                    if (!componentDTOs.isEmpty()) {
-                        dto.setConstructComponents(componentDTOs);
-                    }
-*/
                     if (CollectionUtils.isNotEmpty(construct.getAliases())) {
                         List<String> aliasList = new ArrayList<>(construct.getAliases().size());
                         for (MarkerAlias alias : construct.getAliases()) {
                             aliasList.add(alias.getAlias());
                         }
-                        //dto.set(aliasList);
                     }
                     if (CollectionUtils.isNotEmpty(construct.getSecondaryMarkerSet())) {
                         Set<String> secondaryDTOs = new HashSet<>();
                         for (SecondaryMarker secMarker : construct.getSecondaryMarkerSet()) {
                             secondaryDTOs.add(secMarker.getOldID());
                         }
-                        //dto.setSecondaryIds(secondaryDTOs);
                     }
-/*
-                    List<String> pages = new ArrayList<>();
-                    pages.add("construct");
-                    List<CrossReferenceDTO> xRefs = new ArrayList<>();
-                    CrossReferenceDTO xref = new CrossReferenceDTO("ZFIN", construct.getZdbID(), pages);
-                    xRefs.add(xref);
-                    dto.setCrossReferences(xRefs);
-*/
                     return dto;
                 })
             .collect(Collectors.toList());
 
         return allConstructDTOList;
+    }
+
+    private static String populateRelationship(ConstructComponent component) {
+        String value = null;
+        switch (component.getType()) {
+            case CODING_SEQUENCE_OF, CODING_SEQUENCE_OF_ -> value = "expresses";
+            case PROMOTER_OF, PROMOTER_OF_ -> value = "is_regulated_by";
+            default -> {
+            }
+        }
+        return value;
     }
 
 }
---
+++ source/org/zfin/marker/agr/VariantLinkMLInfo.java
@@ -0,0 +1,133 @@
+package org.zfin.marker.agr;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import org.alliancegenome.curation_api.model.ingest.dto.DataProviderDTO;
+import org.alliancegenome.curation_api.model.ingest.dto.IngestDTO;
+import org.apache.commons.collections4.CollectionUtils;
+import org.zfin.feature.Feature;
+import org.zfin.feature.FeatureGenomicMutationDetail;
+import org.zfin.feature.FeatureNote;
+import org.zfin.gwt.root.util.StringUtils;
+import org.zfin.mapping.FeatureLocation;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.zfin.repository.RepositoryFactory.getFeatureRepository;
+
+public class VariantLinkMLInfo extends LinkMLInfo {
+
+    public VariantLinkMLInfo(int number) {
+        super(number);
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        int number = 0;
+        if (args.length > 0) {
+            number = Integer.parseInt(args[0]);
+        }
+        VariantLinkMLInfo variantLinkMLInfo = new VariantLinkMLInfo(number);
+        variantLinkMLInfo.init();
+        System.exit(0);
+    }
+
+    private void init() throws IOException {
+        initAll();
+        IngestDTO ingestDTO = getIngestDTO();
+        List<org.alliancegenome.curation_api.model.ingest.dto.VariantDTO> allVariantDTO = getAllVariantInfo();
+        ingestDTO.setLinkMLVersion("v1.11.0");
+        ingestDTO.setVariantIngestSet(allVariantDTO);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        ObjectWriter writer = mapper.writer(new DefaultPrettyPrinter());
+        String jsonInString = writer.writeValueAsString(ingestDTO);
+        try (PrintStream out = new PrintStream(new FileOutputStream("ZFIN_Variant_ml.json"))) {
+            out.print(jsonInString);
+        }
+    }
+
+    public List<org.alliancegenome.curation_api.model.ingest.dto.VariantDTO> getAllVariantInfo() {
+        List<FeatureGenomicMutationDetail> allVariants = getFeatureRepository().getAllFeatureGenomicMutationDetails();
+
+        System.out.printf("%,d%n", allVariants.size());
+
+        List<org.alliancegenome.curation_api.model.ingest.dto.VariantDTO> allVariantDTOList = allVariants.stream()
+            .map(variant -> {
+                org.alliancegenome.curation_api.model.ingest.dto.VariantDTO dto = new org.alliancegenome.curation_api.model.ingest.dto.VariantDTO();
+                Feature feature = variant.getFeature();
+                dto.setCurie("ZFIN:" + feature.getZdbID().replace("ZDB-ALT","ZDB-VAR"));
+                dto.setTaxonCurie(ZfinDTO.taxonId);
+                org.alliancegenome.curation_api.model.ingest.dto.DataProviderDTO dataProvider = new DataProviderDTO();
+                dataProvider.setSourceOrganizationAbbreviation("ZFIN");
+                dto.setDataProviderDto(dataProvider);
+                dto.setCreatedByCurie("ZFIN:CURATOR");
+
+                FeatureLocation ftrLoc = getFeatureRepository().getAllFeatureLocationsOnGRCz11(feature);
+                if (ftrLoc != null
+                    && ftrLoc.getStartLocation() != null && ftrLoc.getEndLocation() != null
+                    && StringUtils.isNotEmpty(ftrLoc.getAssembly()) && StringUtils.isNotEmpty(ftrLoc.getReferenceSequenceAccessionNumber())
+                ) {
+                    switch (variant.getFeature().getType()) {
+                        case POINT_MUTATION, DELETION, INSERTION, INDEL -> {
+                            switch (variant.getFeature().getType()) {
+                                case POINT_MUTATION -> dto.setVariantTypeCurie("SO:1000008");
+                                case DELETION -> dto.setVariantTypeCurie("SO:0000159");
+                                case INSERTION -> dto.setVariantTypeCurie("SO:0000667");
+                                case INDEL -> dto.setVariantTypeCurie("SO:1000032");
+                                default -> System.out.println("invalid feature type");
+
+                            }
+                            if (CollectionUtils.isNotEmpty(feature.getExternalNotes())) {
+                                ArrayList<NoteDTO> noteDTOS = new ArrayList<>();
+                                for (FeatureNote varNote : feature.getExternalNotes()) {
+                                    if (varNote.isVariantNote()) {
+                                        if (StringUtils.isNotEmpty(varNote.getNote())) {
+                                            if (!varNote.getNote().contains("href") || !varNote.getNote().contains("\\n")) {
+                                                ArrayList<PublicationAgrDTO> datasetPubs = new ArrayList<>();
+                                                PublicationAgrDTO fixedPub = new PublicationAgrDTO();
+                                                List<String> pubPages = new ArrayList<>();
+                                                pubPages.add("reference");
+
+                                                CrossReferenceDTO pubXref = new CrossReferenceDTO("ZFIN", varNote.getPublication().getZdbID(), pubPages);
+                                                if (varNote.getPublication().getAccessionNumber() != null) {
+                                                    fixedPub.setPublicationId("PMID:" + varNote.getPublication().getAccessionNumber());
+                                                    fixedPub.setCrossReference(pubXref);
+                                                } else {
+                                                    fixedPub.setPublicationId("ZFIN:" + varNote.getPublication().getZdbID());
+                                                }
+                                                datasetPubs.add(fixedPub);
+                                                NoteDTO noteDto = new NoteDTO(datasetPubs);
+                                                noteDto.setNote(varNote.getNote());
+                                                noteDTOS.add(noteDto);
+                                            }
+                                        }
+                                    }
+                                }
+                                if (CollectionUtils.isNotEmpty(noteDTOS)) {
+                                    dto.setNoteDtos(noteDTOS.stream().map(noteDTO -> {
+                                        org.alliancegenome.curation_api.model.ingest.dto.NoteDTO noteDote = new org.alliancegenome.curation_api.model.ingest.dto.NoteDTO();
+                                        noteDote.setFreeText(noteDTO.getNote());
+                                        noteDote.setNoteTypeName("comment");
+                                        return noteDote;
+                                    }).toList());
+                                }
+                            }
+                        }
+                        default -> {
+                        }
+                    }
+                }
+                return dto;
+            }).toList();
+        // weed out alleles not in the list
+        return allVariantDTOList.stream().filter(variantDTO -> variantDTO.getVariantTypeCurie() != null).toList();
+    }
+
+}
