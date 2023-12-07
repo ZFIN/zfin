4c758adecbbdca7dca7037adc5359ccc04ee58d0
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-12-06 17:32:27 -0800

    ZFIN-8757: Add summary table to report

--- source/org/zfin/uniprot/UniProtLoadActionsContainer.java
+++ source/org/zfin/uniprot/UniProtLoadActionsContainer.java
@@ -1,11 +1,13 @@
 package org.zfin.uniprot;
 
 import lombok.Builder;
+import lombok.Getter;
 import org.zfin.uniprot.dto.UniProtLoadSummaryDTO;
 
 import java.util.Set;
 
 @Builder
+@Getter
 public class UniProtLoadActionsContainer {
 
     private final UniProtLoadSummaryDTO summary;
