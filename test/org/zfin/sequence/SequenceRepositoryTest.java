469849677a13c409b5a43b97db2192ba4733bee1
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-12-08 14:48:30 -0800

    ZFIN-8801: Add more formatting tests

--- test/org/zfin/sequence/SequenceRepositoryTest.java
+++ test/org/zfin/sequence/SequenceRepositoryTest.java
@@ -79,7 +79,7 @@ public class SequenceRepositoryTest extends AbstractDatabaseTest {
     }
 
     @Test
-    public void testAccessionFormatValidationRules() {
+    public void testGenBankAccessionFormatValidationRulesFail() {
         SequenceRepository sr = RepositoryFactory.getSequenceRepository();
         ReferenceDatabase refDb = sr.getReferenceDatabase(
                 ForeignDB.AvailableName.GENBANK,
@@ -99,6 +99,37 @@ public class SequenceRepositoryTest extends AbstractDatabaseTest {
         assertFalse(refDb.isValidAccessionFormat("CD75461"));
         assertFalse(refDb.isValidAccessionFormat("NM_4214243"));
         assertFalse(refDb.isValidAccessionFormat("XM_123451234"));
+    }
+
+    @Test
+    public void testGenBankAccessionFormatValidationRulesSucceed() {
+        SequenceRepository sr = RepositoryFactory.getSequenceRepository();
+        ReferenceDatabase refDb = sr.getReferenceDatabase(
+                ForeignDB.AvailableName.GENBANK,
+                ForeignDBDataType.DataType.GENOMIC,
+                ForeignDBDataType.SuperType.SEQUENCE,
+                Species.Type.ZEBRAFISH);
+        Set<ReferenceDatabaseValidationRule> rules = refDb.getValidationRules();
+        assertTrue("ReferenceDatabase has validation rules", rules.size() > 0);
+
+
+        //Test some example formats described here: https://zfin.atlassian.net/wiki/spaces/doc/pages/5266079747/Validation+Rules+for+Foreign+DB+Accessions
+//        Nucleotide 1 letter + 5 numerals or 2 letters + 6 numerals or 2 letters + 8 numerals
+        assertTrue(refDb.isValidAccessionFormat("A12345"));
+        assertTrue(refDb.isValidAccessionFormat("AB123456"));
+        assertTrue(refDb.isValidAccessionFormat("AB12345678"));
+
+//        Protein 3 letters + 5 numerals or 3 letters + 7 numerals
+        assertTrue(refDb.isValidAccessionFormat("XYZ12345"));
+        assertTrue(refDb.isValidAccessionFormat("UVW1234567"));
+
+//        WGS 4 letters + 2 numerals for WGS assembly version + 6 or more numerals or
+//        6 letters + 2 numerals for WGS assembly version + 7 or more numerals
+        assertTrue(refDb.isValidAccessionFormat("AAAA00123456"));
+        assertTrue(refDb.isValidAccessionFormat("AABBCC001234567"));
+
+//        MGA 5 letters + 7 numerals
+        assertTrue(refDb.isValidAccessionFormat("ABCDE1234567"));
     }
 
     @Test
