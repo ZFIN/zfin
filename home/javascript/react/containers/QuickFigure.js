58fee2189d6cdf2a877a528676e015501cb613a2
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2023-12-15 12:03:18 -0800

    ZFIN-8759: QuickFigure react component matches angular code

--- home/javascript/react/containers/QuickFigure.js
+++ home/javascript/react/containers/QuickFigure.js
@@ -10,24 +10,53 @@ const QuickFigureDialog = ({pubId, toggle}) => {
     const [errorMessage, setErrorMessage] = useState('');
     const [submitting, setSubmitting] = useState(false);
 
-    function submit() {
-        console.log('submit');
-    }
+    const submit = (event) => {
+        event.preventDefault();
+        let labelFinal = type;
+        if (type !== 'text only') {
+            labelFinal += ' ' + label;
+        }
+        setSubmitting(true);
+
+        const url = `/action/publication/${pubId}/figures`;
+        const formData = new FormData();
+        formData.append('label', labelFinal);
+        formData.append('caption', '');
+
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(err => { throw err; });
+                }
+                return response.json();
+            })
+            .then(data => {
+                setSuccessMessage(data.label + ' created');
+                setLabel('');
+                setErrorMessage('');
+                window.refreshFigures();
+            })
+            .catch(error => {
+                setSuccessMessage('');
+                setErrorMessage(error.message || 'An error occurred');
+            })
+            .finally(() => {
+                setSubmitting(false);
+            });
+    };
 
     function readyToSubmit() {
         return (type === 'text only') || (label !== '');
     }
 
-    const onToggle = () => {
-        console.log('onToggle');
-        toggle();
-    }
-
     return (
-        <div className="quick-fig-content">
-            <button type="button" className='close' onClick={() => onToggle()}>×</button>
+        <div className='quick-fig-content'>
+            <button type='button' className='close' onClick={() => toggle()}>×</button>
             <h4>Quick Figure</h4>
-            <form className='form-inline'>
+            <form className='form-inline' onSubmit={submit}>
                 <select className='form-control mr-1' value={type} onChange={e => setType(e.target.value)}>
                     {types.map(t => (
                         <option key={t} value={t}>{t}</option>
@@ -35,75 +64,36 @@ const QuickFigureDialog = ({pubId, toggle}) => {
                 </select>
                 <input
                     className='form-control form-control-fixed-width-sm mr-1'
-                    type="text"
+                    type='text'
                     value={label}
                     onChange={e => setLabel(e.target.value)}
                     disabled={type === 'text only'}
                 />
-                <i className="fas fa-check"></i>
-                <button className='btn btn-primary' type="button" onClick={submit} disabled={submitting || !readyToSubmit()}>
-                    {submitting ? 'Loading...' : 'Submit'}
+
+                <button className='btn btn-primary' type='button' onClick={submit} disabled={submitting || !readyToSubmit()}>
+                    {submitting ? <i className='fas fa-spinner fa-spin'/> : <i className='fas fa-check'/>}
                 </button>
             </form>
-            {successMessage && <p>{successMessage}</p>}
-            {errorMessage && <p>{errorMessage}</p>}
+            <p/>
+            {successMessage && <p className='text-success'>{successMessage}</p>}
+            {errorMessage && <p className='text-danger'>{errorMessage}</p>}
+            <small>
+                <a href={`/action/publication/${pubId}/edit#figures`}>Add figure with images and caption</a>
+            </small>
         </div>
     );
-}
+};
 
 const QuickFigure = ({ pubId }) => {
     const popoverRef = useRef(null);
-    const [open, setOpen] = useState(false);
-    const [types] = useState(['Fig.', 'text only', 'Table']);
-    const [type, setType] = useState(types[0]);
-    const [label, setLabel] = useState('');
-    const [submitting, setSubmitting] = useState(false);
-    const [successMessage, setSuccessMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
     const [isInitialized, setIsInitialized] = useState(false);
-
-    const popoverTemplate = `<div id="popover-dialog-container"></div>`;
+    const popoverTemplate = '<div id=\'popover-dialog-container\'></div>';
 
     const toggle = () => {
         $(popoverRef.current).popover('toggle');
     };
 
-    const submit = () => {
-        let labelFinal = type;
-        if (type !== 'text only') {
-            labelFinal += ' ' + label;
-        }
-        setSubmitting(true);
-
-        // something like: axios.post('/api/figures', { pubId, label: labelFinal })...
-        // .then(response => {
-        //     setSuccessMessage(response.data.label + " created");
-        //     setLabel('');
-        //     setErrorMessage('');
-        //     // Refresh figures, replace $window.refreshFigures
-        // })
-        // .catch(error => {
-        //     setSuccessMessage('');
-        //     setErrorMessage(error.response.data.message);
-        // })
-        // .finally(() => {
-        //     setSubmitting(false);
-        // });
-    };
-
-    const readyToSubmit = () => {
-        return (type === 'text only') || (label !== '');
-    };
-
-    const reset = () => {
-        setType(types[0]);
-        setLabel('');
-        setSuccessMessage('');
-        setErrorMessage('');
-    };
-
-    const handlePopoverInserted = (event) => {
-        console.log('handlePopoverInserted event:', event);
+    const handlePopoverInserted = () => {
         if (!isInitialized) {
             setIsInitialized(true);
             const popoverEl = document.getElementById('popover-dialog-container');
@@ -112,19 +102,16 @@ const QuickFigure = ({ pubId }) => {
         }
     }
 
+    //set up the popover
     useEffect(() => {
-        console.log('useEffect 001');
         if (popoverRef.current) {
-            console.log('useEffect 002');
-
-            const returnValue = $(popoverRef.current).popover({
+            $(popoverRef.current).popover({
                 trigger: 'manual',
                 html: true,
                 content: () => popoverTemplate,
                 placement: 'bottom',
                 sanitize: false
             }).on('inserted.bs.popover', handlePopoverInserted);
-            console.log('returnValue', returnValue);
         }
 
         return () => {
@@ -136,8 +123,8 @@ const QuickFigure = ({ pubId }) => {
     }, []);
 
     return (
-        <span className='quick-figure-add'>
-            <a href="#" ref={popoverRef} data-toggle='popover' onClick={toggle}>Add Figure XYZ</a>
+        <span className='quick-figure-add quick-fig'>
+            <a href='#' className='small-new-link' ref={popoverRef} data-toggle='popover' onClick={toggle}>Add Figure</a>
         </span>
     );
 };
