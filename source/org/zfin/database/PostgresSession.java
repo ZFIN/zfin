3153a3a69e44af0d2874beef78d7c69f1788e38d
Christian Pich <cmpich@zfin.org>
2024-01-10 16:15:20 +0100

    fix database-summary page

--- source/org/zfin/database/PostgresSession.java
+++ source/org/zfin/database/PostgresSession.java
@@ -1,5 +1,7 @@
 package org.zfin.database;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,6 +12,7 @@ import java.util.Date;
  * Maps to all database records.
  */
 @Entity
+@Data
 @Table(name = "PG_STAT_ACTIVITY")
 public class PostgresSession implements Comparable<PostgresSession> {
 
@@ -28,62 +31,6 @@ public class PostgresSession implements Comparable<PostgresSession> {
     private Date dateCreated;
     @Column(name = "state_change")
     private Date dateLastUsed;
-
-    public long getPid() {
-        return pid;
-    }
-
-    public void setPid(long pid) {
-        this.pid = pid;
-    }
-
-    public String getDbname() {
-        return dbname;
-    }
-
-    public void setDbname(String dbname) {
-        this.dbname = dbname;
-    }
-
-    public String getOwner() {
-        return owner;
-    }
-
-    public void setOwner(String owner) {
-        this.owner = owner;
-    }
-
-    public Date getDateCreated() {
-        return dateCreated;
-    }
-
-    public void setDateCreated(Date dateCreated) {
-        this.dateCreated = dateCreated;
-    }
-
-    public String getQuery() {
-        return query;
-    }
-
-    public void setQuery(String query) {
-        this.query = query;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public Date getDateLastUsed() {
-        return dateLastUsed;
-    }
-
-    public void setDateLastUsed(Date dateLastUsed) {
-        this.dateLastUsed = dateLastUsed;
-    }
 
     @Override
     public int compareTo(PostgresSession o) {
