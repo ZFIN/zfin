9aac926412a820fb2aa5b00ae463134eba2d8466
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2024-03-12 09:35:12 -0700

    Update VocabularyDAO.java (#976)
    testing if this removes "^M" line endings

--- source/org/zfin/framework/dao/VocabularyDAO.java
+++ source/org/zfin/framework/dao/VocabularyDAO.java
@@ -1,1 +1,11 @@
-package org.zfin.framework.dao;import org.zfin.framework.Vocabulary;public class VocabularyDAO extends BaseSQLDAO<Vocabulary> {    public VocabularyDAO() {        super(Vocabulary.class);    }}
+package org.zfin.framework.dao;
+
+import org.zfin.framework.Vocabulary;
+
+public class VocabularyDAO extends BaseSQLDAO<Vocabulary> {
+
+    public VocabularyDAO() {
+        super(Vocabulary.class);
+    }
+
+}
