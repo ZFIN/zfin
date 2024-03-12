7e2daac919abbfb9cbc9396a390d62759ae06cfc
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2024-03-12 09:35:41 -0700

    Update VocabularyTermDAO.java (#977)
    Line endings update

--- source/org/zfin/framework/dao/VocabularyTermDAO.java
+++ source/org/zfin/framework/dao/VocabularyTermDAO.java
@@ -1,1 +1,11 @@
-package org.zfin.framework.dao;import org.zfin.framework.VocabularyTerm;public class VocabularyTermDAO extends BaseSQLDAO<VocabularyTerm> {    public VocabularyTermDAO() {        super(VocabularyTerm.class);    }}
+package org.zfin.framework.dao;
+
+import org.zfin.framework.VocabularyTerm;
+
+public class VocabularyTermDAO extends BaseSQLDAO<VocabularyTerm> {
+
+    public VocabularyTermDAO() {
+        super(VocabularyTerm.class);
+    }
+
+}
