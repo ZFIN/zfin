d7e3ca473ea7bbc9c0a2d46fe6a9640ff543c24c
Christian Pich <cmpich@zfin.org>
2024-04-17 19:33:01 +0200

    ZFIN-9018 refactor or chebi-mesh mapping load and export of a SSSOM file

--- source/org/zfin/datatransfer/ctd/CreateMeshChebiMappingFile.java
+++ source/org/zfin/datatransfer/ctd/CreateMeshChebiMappingFile.java
@@ -1,35 +1,30 @@
 package org.zfin.datatransfer.ctd;
 
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
-import org.apache.commons.lang3.StringUtils;
 import org.zfin.curation.PublicationNote;
 import org.zfin.datatransfer.service.DownloadService;
 import org.zfin.framework.HibernateUtil;
-import org.zfin.framework.dao.VocabularyDAO;
-import org.zfin.framework.dao.VocabularyTermDAO;
-import org.zfin.framework.services.VocabularyEnum;
-import org.zfin.framework.services.VocabularyService;
 import org.zfin.infrastructure.ant.DataReportTask;
 import org.zfin.ontology.TermExternalReference;
 import org.zfin.ontology.datatransfer.AbstractScriptWrapper;
 import org.zfin.publication.Publication;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.zfin.ontology.datatransfer.OntologyCommandLineOptions.loadDir;
 import static org.zfin.ontology.datatransfer.OntologyCommandLineOptions.webrootDirectory;
 import static org.zfin.repository.RepositoryFactory.*;
 
-public class LoadCtdData extends AbstractScriptWrapper {
+public class CreateMeshChebiMappingFile extends AbstractScriptWrapper {
 
     static {
         options.addOption(loadDir);
@@ -37,54 +32,75 @@ public class LoadCtdData extends AbstractScriptWrapper {
         options.addOption(DataReportTask.jobNameOpt);
     }
 
-    public LoadCtdData() {
+    public CreateMeshChebiMappingFile() {
     }
 
     private MeshCasChebiMappings mapping = new MeshCasChebiMappings();
 
     public static void main(String[] arguments) throws IOException {
 
-        LoadCtdData load = new LoadCtdData();
-        load.execute();
+        CreateMeshChebiMappingFile load = new CreateMeshChebiMappingFile();
+        load.executeExport();
         System.exit(0);
     }
 
-
-    private void execute() throws IOException {
+    private void executeExport() {
         initAll();
         dao = new MeshChebiDAO(HibernateUtil.currentSession());
-        vocabularyDao = new VocabularyDAO(HibernateUtil.currentSession());
-        vocabularyTermDao = new VocabularyTermDAO(HibernateUtil.currentSession());
-        pubDao = new PublicationCtdDAO(HibernateUtil.currentSession());
-        publicationNoteDao = new PublicationNoteDAO(HibernateUtil.currentSession());
-        service = new VocabularyService();
-        getReferenceCDT();
-        loadMeshAndChebi();
-        reportNonJointCasIds();
-        List<MeshChebiMapping> allMappingRecords = getMeshChebiMappings();
-        saveOneToOneMeshChebiRelations(allMappingRecords, "mesh-chebi-one-to-one.csv");
-/*
-        reportOneToNRelations();
-*/
-        //load.reportOneToManyRelations();
-        //getChemGeneInteractionCDT();
+        List<MeshChebiMapping> meshChebiList = dao.forceRetrieveAll();
+        createExportFile(meshChebiList);
     }
 
-    private List<MeshChebiMapping> getMeshChebiMappings() {
+    private void createExportFile(List<MeshChebiMapping> meshCehbiList) {
+        List<String> headerNames = List.of(
+            "subject_id",
+            "predicate_id",
+            "object_id",
+            "mapping_justification",
+            "subject_label",
+            "object_label"
+        );
+
+        BufferedWriter writer = null;
+        try {
+            writer = Files.newBufferedWriter(Paths.get("mesh-chebi-mapping.tsv"));
+            CSVPrinter csvPrinterImportant = new CSVPrinter(writer, CSVFormat.TDF
+                .withHeader(headerNames.toArray(String[]::new)));
+            meshCehbiList.forEach((map) -> {
+                List<String> vals = new ArrayList<>();
+                vals.add(map.getMeshID());
+                vals.add("skos:" + map.getPredicate().getName() + "Match");
+                vals.add(map.getChebiID());
+                vals.add("semapv:" + map.getMappingJustification().getName());
+                vals.add(map.getMeshName());
+                vals.add(getOntologyRepository().getTermByOboID(map.getChebiID()).getTermName());
+                Object[] values = vals.toArray();
+
+                try {
+                    csvPrinterImportant.printRecord(values);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
+    private void printOneToOneMeshChebiRelations() {
         Set<MeshCasChebiRelation> oneToOneRelations = mapping.getOneToOneRelations();
-        List<MeshChebiMapping> newRecords = oneToOneRelations.stream().map(this::getMeshChebiMapping).toList();
-        newRecords.forEach(meshChebiMapping -> meshChebiMapping.setInferenceMethod(service.getVocabularyTerm(VocabularyEnum.INFERENCE_METHOD_CHEBI_MESH.getName(), "chebi-cas-mesh")));
-        Set<MeshCasChebiRelation> oneToOneWithDCRelations = mapping.getOneToOneWithDCRelations();
-        List<MeshChebiMapping> newRecordsDC = oneToOneWithDCRelations.stream().map(this::getMeshChebiMapping).toList();
-        newRecordsDC.forEach(meshChebiMapping -> meshChebiMapping.setInferenceMethod(service.getVocabularyTerm(VocabularyEnum.INFERENCE_METHOD_CHEBI_MESH.getName(), "cas-chebi-cas-mesh")));
-        List<MeshChebiMapping> allMappingRecords = new ArrayList<>(newRecords);
-        allMappingRecords.addAll(newRecordsDC);
-        return allMappingRecords;
-    }
+        writeOneToOneFile(oneToOneRelations, "mesh-chebi-one-to-one.csv");
 
-    private void saveOneToOneMeshChebiRelations(List<MeshChebiMapping> newRecords, String reportFileName) {
-        writeMappingToFile(newRecords, reportFileName);
-
+        List<MeshChebiMapping> newRecords = oneToOneRelations.stream().map(CreateMeshChebiMappingFile::getMeshChebiMapping).collect(Collectors.toList());
         HibernateUtil.createTransaction();
         List<MeshChebiMapping> oldRecords = dao.forceRetrieveAll();
         oldRecords.removeAll(newRecords);
@@ -92,26 +108,20 @@ public class LoadCtdData extends AbstractScriptWrapper {
         for (MeshChebiMapping meshChebiMapping : oldRecords) {
             dao.delete(meshChebiMapping);
         }
-        System.out.println("Deleted records: " + oldRecords.size());
-        writeMappingToFile(oldRecords, reportFileName + ".delete.csv");
         List<MeshChebiMapping> updatedOldRecords = dao.forceRetrieveAll();
         newRecords.removeAll(updatedOldRecords);
 
         // add records that are only found in the ingest file
         newRecords.forEach(relation -> dao.persist(relation));
-        System.out.println("Added records: " + newRecords.size());
-        writeMappingToFile(newRecords, reportFileName + ".add.csv");
         HibernateUtil.flushAndCommitCurrentSession();
     }
 
-    private MeshChebiMapping getMeshChebiMapping(MeshCasChebiRelation relation) {
+    private static MeshChebiMapping getMeshChebiMapping(MeshCasChebiRelation relation) {
         MeshChebiMapping mcMapping = new MeshChebiMapping();
         mcMapping.setMeshID(relation.getMesh());
         mcMapping.setMeshName(relation.getMeshName());
         mcMapping.setChebiID(relation.getChebi());
         mcMapping.setCasID(relation.getCas());
-        mcMapping.setPredicate(service.getVocabularyTerm("predicate", "exact"));
-        mcMapping.setMappingJustification(service.getVocabularyTerm("mapping justification", "LexicalMatching"));
         return mcMapping;
     }
 
@@ -183,52 +193,15 @@ public class LoadCtdData extends AbstractScriptWrapper {
 
         HibernateUtil.flushAndCommitCurrentSession();
         //savePublicationCtds();
-        System.out.println("Number of new publications: " + newPubCtds.size());
+        System.out.println("Number of new records: " + newPubCtds.size());
         newPubCtds.forEach(publicationCtd -> System.out.println(publicationCtd.getCtdID()));
 
-        System.out.println("\rNumber of publications not found in ZFIN: " + publicationsNotFound.size());
+        System.out.println("\rNumber of records not found in ZFIN: " + publicationsNotFound.size());
         publicationsNotFound.forEach(System.out::println);
-    }
-
-    private void loadMeshAndChebi() throws IOException {
-        downloadChemicalFile();
-        List<MeshCasChebiRelation> relations = new ArrayList<>();
-
-        Map<String, String> meshToCasMap = new HashMap<>();
-        Reader in = new FileReader(downloadedChemicalFile);
-        Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader(CHEMICALS.class).parse(in);
-        int lineNumber = 0;
-        for (CSVRecord record : records) {
-            lineNumber++;
-            if (lineNumber == 1) continue;
-            // ignore header info
-            if (record.get(0).startsWith("!") || record.get(0).startsWith("#")) continue;
-            MeshCasChebiRelation relation = new MeshCasChebiRelation();
-            relation.setMesh(record.get(CHEMICALS.MESH));
-            relation.setMeshName(record.get(CHEMICALS.NAME));
-            if (StringUtils.isNotEmpty(record.get(CHEMICALS.CAS))) {
-                relation.setCas("CAS:" + record.get(CHEMICALS.CAS));
-            }
-            meshToCasMap.put(record.get(CHEMICALS.MESH), record.get(CHEMICALS.CAS));
-            relations.add(relation);
-        }
-        mapping.addMeshCasRelations(relations);
-        mapping.addChebiCasRelations(getChebiToCasMapping());
-        mapping.generateUniqueMapping();
-        mapping.generateUniqueMappingWithDanglingCas();
-        writeFile(mapping.getOneToManyMeshRelations(), "chebi-mesh-multiple.csv");
-
-        System.out.println("Unique Chebi-Cas relations: " + String.format("%,d", mapping.getChebiCasUnique().size()));
-        System.out.println("Unique Cas-Chebi relations: " + String.format("%,d", mapping.getCasChebiUnique().size()));
-        System.out.println("Unique Mesh-Cas relations: " + String.format("%,d", mapping.getMeshCasUnique().size()));
-        System.out.println("Unique Cas-Mesh relations: " + String.format("%,d", mapping.getCasMeshUnique().size()));
-        System.out.println("Unique Chebi-Cas-Mesh relations: " + String.format("%,d", mapping.getOneToOneRelations().size()));
-        System.out.println("Unique Chebi-Cas-Mesh relations derived from DC: " + String.format("%,d", mapping.getOneToOneWithDCRelations().size()));
     }
 
     protected DownloadService downloadService = new DownloadService();
 
-    private File downloadedChemicalFile;
     private File downloadedPublicationFile;
 
     private void downloadPublicationFile() {
@@ -241,22 +214,9 @@ public class LoadCtdData extends AbstractScriptWrapper {
         }
     }
 
-    private void downloadChemicalFile() {
-        String fileName = "CTD_chemicals.csv";
-        String url = "https://ctdbase.org/reports/" + fileName + ".gz";
-        try {
-            downloadedChemicalFile = downloadService.downloadFile(new File(fileName), new URL(url), false);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private MeshChebiDAO dao;
-    private VocabularyTermDAO vocabularyTermDao;
-    private VocabularyDAO vocabularyDao;
-    private PublicationCtdDAO pubDao;
-    private VocabularyService service;
-    private PublicationNoteDAO publicationNoteDao;
+    MeshChebiDAO dao;
+    PublicationCtdDAO pubDao;
+    PublicationNoteDAO publicationNoteDao;
 
     private List<MeshCasChebiRelation> getChebiToCasMapping() {
 
@@ -364,7 +324,7 @@ public class LoadCtdData extends AbstractScriptWrapper {
         }
     }
 
-    private static void writeFile(Collection<MeshCasChebiRelation> list, String fileName) {
+    private static void writeOneToOneFile(Collection<MeshCasChebiRelation> list, String fileName) {
         Path file = Path.of(fileName);
         StringBuffer buffer = new StringBuffer();
         list.forEach(relation -> {
@@ -383,98 +343,6 @@ public class LoadCtdData extends AbstractScriptWrapper {
             throw new RuntimeException(e);
         }
     }
-    private static void writeMappingToFile(Collection<MeshChebiMapping> list, String fileName) {
-        Path file = Path.of(fileName);
-        StringBuffer buffer = new StringBuffer();
-        list.forEach(relation -> {
-            StringJoiner joiner = new StringJoiner("\t");
-            joiner.add(relation.getChebiID());
-            joiner.add(relation.getMeshID());
-            joiner.add(relation.getMeshName());
-            joiner.add(relation.getCasID());
-            buffer.append(joiner);
-            buffer.append("\n");
-        });
-        try {
-            Files.writeString(file, buffer.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void writeOneToOneFile1(Collection<MeshChebiMapping> list, String fileName) {
-        Path file = Path.of(fileName);
-        StringBuffer buffer = new StringBuffer();
-        list.forEach(relation -> {
-            StringJoiner joiner = new StringJoiner("\t");
-            joiner.add(relation.getChebiID());
-            joiner.add(relation.getMeshID());
-            joiner.add(relation.getMeshName());
-            joiner.add(relation.getCasID());
-            buffer.append(joiner);
-            buffer.append("\n");
-        });
-        try {
-            Files.writeString(file, buffer.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void getChemGeneInteractionCDT() throws IOException {
-        File downloadedFile = new File("CTD_chem_gene_ixns.csv");
-        List<String> pubmedIDs = new ArrayList<>();
-        List<CSVRecord> recordsDR = new ArrayList<>();
-        Reader in = new FileReader(downloadedFile);
-        Iterable<CSVRecord> records = CSVFormat.EXCEL.withHeader(HeaderChem.class).parse(in);
-        int lineNumber = 0;
-        for (CSVRecord record : records) {
-            lineNumber++;
-            // ignore header info
-            if (record.get(0).startsWith("!")) continue;
-            String id = record.get(HeaderChem.PUBMED_ID);
-            String org = record.get(HeaderChem.ORGANISM_ID);
-            if (id != null && org.contains("7955")) recordsDR.add(record);
-        }
-        System.out.println("Number of Danio records: " + recordsDR.size());
-
-//        pubmedIDs.remove(0);
-        // get records with non-null CAS Ids.
-        List<String> existingIDs = new ArrayList<>();
-        recordsDR.stream().filter(record -> StringUtils.isNotEmpty(record.get(HeaderChem.CAS_RN))).forEach(record -> {
-            String casID = record.get(HeaderChem.CAS_RN);
-            TermExternalReference ref = getOntologyRepository().getTermExternalReference(casID, "CAS");
-            if (ref != null) existingIDs.add(ref.getTerm().getOboID());
-        });
-        System.out.println("Number of records with identified CAS Chebi terms: " + existingIDs.size());
-
-        List<String> existingIDsWithMesh = new ArrayList<>();
-        recordsDR.stream().filter(record -> StringUtils.isNotEmpty(record.get(HeaderChem.CHEMID))).forEach(record -> {
-            String meshID = record.get(HeaderChem.CHEMID);
-            TermExternalReference ref = getOntologyRepository().getTermExternalReference(meshID, "MESH");
-            if (ref != null) existingIDsWithMesh.add(ref.getTerm().getOboID());
-        });
-        System.out.println("Number of records with identified Mesh-Chebi terms: " + existingIDsWithMesh.size());
-
-
-    }
-
-
-}
-
-enum CHEMICALS {
-    NAME, MESH, CAS, DEFINITION, CITATION, PARENT_IDS, TREE_NUMBERS, PARENT_TREE_NUMBERS, SYNONYS;
-}
-
-enum Header {
-    PID, AUTHORS, TITLE, YEAR, CITATION, CITED_CHEMICAL, CITED_DISEASES, CITED_GENES, CITED_PHENOTYPE, CITED_AO_TERMS,
-    ;
-}
-
-enum HeaderChem {
-    CHEM_NAME, CHEMID, CAS_RN, GENE_SYMBOL, GENE_ID, GENE_FROM, ORGANISM, ORGANISM_ID, INTERACTION, INTERACTION_ACTIONS, PUBMED_ID;
-
-    String val;
 
 }
 
