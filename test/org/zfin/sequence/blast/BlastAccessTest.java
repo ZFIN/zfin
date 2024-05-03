a0b9692132b005baf11ae57e1cf375699afdc7cc
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-05-03 13:48:58 -0700

    Migrate tests from hibernate 5 to 6

--- test/org/zfin/sequence/blast/BlastAccessTest.java
+++ test/org/zfin/sequence/blast/BlastAccessTest.java
@@ -1,11 +1,11 @@
 package org.zfin.sequence.blast;
 
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.logging.log4j.LogManager; import org.apache.logging.log4j.Logger;
-import org.hibernate.Query;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.zfin.AbstractDatabaseTest;
