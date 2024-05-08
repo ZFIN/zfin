e5ce83c6ab6f3c6ca9a7bace732e4bda49bdb992
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-05-08 13:50:03 -0700

    Clean up dependencies

--- source/org/zfin/infrastructure/presentation/StaticFileController.java
+++ source/org/zfin/infrastructure/presentation/StaticFileController.java
@@ -14,13 +14,14 @@ import java.io.IOException;
 @RequestMapping("/zf_info")
 public class StaticFileController {
 
-    @RequestMapping("/**/*.html")
-    public String viewStaticFile(Model model,
-                                 HttpServletRequest request,
-                                 HttpServletResponse response) throws IOException {
-        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
-        path = FilenameUtils.removeExtension(path.substring(9)).replace("/", "--");
-        return "zf_info/" + path;
-    }
+    //TODO: spring migration change, test this method works for all levels of html files
+//    @RequestMapping("/**/{filename:.+}.html")
+//    public String viewStaticFile(Model model,
+//                                 HttpServletRequest request,
+//                                 HttpServletResponse response) throws IOException {
+//        String path = (String) request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE);
+//        path = FilenameUtils.removeExtension(path.substring(9)).replace("/", "--");
+//        return "zf_info/" + path;
+//    }
 
 }
