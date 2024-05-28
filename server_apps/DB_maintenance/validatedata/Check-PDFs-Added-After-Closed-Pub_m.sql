6689eb9cb8a4d6552f2165776174c7b7dac05067
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2024-05-28 08:07:49 -0700

    ZFIN-9076: Add jenkins job to check for pubs where PDFs were loaded after closing (#1060)
    * ZFIN-9076: Add jenkins job to check for pubs where PDFs were loaded after closing
    
    * ZFIN-9076: Include a monthly trigger
    
    * ZFIN-9076: cleanup
    
    * ZFIN-9076: Include link to artifact-diff pages for help with limiting to only new results
    
    * ZFIN-9076: Match new template for jobs using INSTANCE instead of DOMAIN_NAME

---
+++ server_apps/DB_maintenance/validatedata/Check-PDFs-Added-After-Closed-Pub_m.sql
@@ -0,0 +1,24 @@
+SELECT
+    pth_pub_zdb_id AS "ZDB ID",
+    authors AS "Authors",
+    title AS "Title",
+    pts_status_display AS "Status",
+    pth_status_insert_date AS "Status Changed Date",
+    pf_date_entered AS "File Date",
+    pft.pft_type AS "File Type",
+    pf_original_file_name AS "File Name",
+    'https://zfin.org/PDFLoadUp/' || pf.pf_file_name AS "File Path"
+FROM
+    pub_tracking_history pth
+        LEFT JOIN pub_tracking_status pts ON pth_status_id = pts_pk_id
+        LEFT JOIN publication_file pf ON pf_pub_zdb_id = pth_pub_zdb_id
+        LEFT JOIN publication pub ON pth_pub_zdb_id = pub.zdb_id
+        LEFT JOIN publication_file_type pft ON pf.pf_file_type_id = pft.pft_pk_id
+WHERE
+    pth_status_is_current
+  AND pts_status_display = 'Closed, No PDF'
+  AND pf_file_type_id = 1
+  AND pf_date_entered > pth_status_insert_date
+  AND NOT pub_is_indexed
+ORDER BY
+    pth_pub_zdb_id DESC
