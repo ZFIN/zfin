c263100134223e3de2da7fbc8a95e99c24de5e44
Christian Pich <cmpich@zfin.org>
2024-05-29 14:01:27 +0200

    ZFIN-9220 add cross_reference_dtos to gene linkML submission file (#1059)

--- source/org/zfin/marker/agr/GeneLinkMLInfo.java
+++ source/org/zfin/marker/agr/GeneLinkMLInfo.java
@@ -4,22 +4,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.alliancegenome.curation_api.model.ingest.dto.CrossReferenceDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.DataProviderDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.IngestDTO;
 import org.alliancegenome.curation_api.model.ingest.dto.slotAnnotions.NameSlotAnnotationDTO;
+import org.apache.commons.collections4.CollectionUtils;
 import org.zfin.infrastructure.ActiveData;
 import org.zfin.marker.Marker;
+import org.zfin.sequence.ForeignDB;
+import org.zfin.sequence.MarkerDBLink;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
+import java.util.*;
 
 import static java.util.stream.Collectors.toList;
+import static org.zfin.repository.RepositoryFactory.getExpressionRepository;
 import static org.zfin.repository.RepositoryFactory.getMarkerRepository;
 
 public class GeneLinkMLInfo extends LinkMLInfo {
@@ -49,6 +51,7 @@ public class GeneLinkMLInfo extends LinkMLInfo {
         try (PrintStream out = new PrintStream(new FileOutputStream("ZFIN_1.0.1.4_Gene_ml.json"))) {
             out.print(jsonInString);
         }
+        allPrefixes.forEach(System.out::println);
     }
 
     public List<org.alliancegenome.curation_api.model.ingest.dto.GeneDTO> getAllGenes(int numberOrRecords) {
@@ -66,9 +69,72 @@ public class GeneLinkMLInfo extends LinkMLInfo {
                 dto.setModEntityId("ZFIN:" + marker.getZdbID());
                 GregorianCalendar date = ActiveData.getDateFromId(marker.getZdbID());
                 dto.setDateCreated(format(date));
+
+                List<CrossReferenceDTO> dbLinkList = new ArrayList<>(marker.getDbLinks().size() + 1);
+                if (CollectionUtils.isNotEmpty(marker.getDbLinks())) {
+
+                    for (MarkerDBLink link : marker.getDbLinks()) {
+                        String dbName = DataProvider.getExternalDatabaseName(link.getReferenceDatabase().getForeignDB().getDbName());
+                        if (dbName == null)
+                            continue;
+                        // do not include ENSDARP records
+                        if (dbName.equals(ForeignDB.AvailableName.ENSEMBL.toString()) && link.getAccessionNumber().startsWith("ENSDARP"))
+                            continue;
+
+                        CrossReferenceDTO xRefDto = getCrossReferenceDTO(link, "default");
+                        dbLinkList.add(xRefDto);
+                    }
+                }
+                int hasExpression = getExpressionRepository().getExpressionFigureCountForGene(marker);
+                if (hasExpression > 0) {
+                    int hasWTExpression = getExpressionRepository().getWtExpressionFigureCountForGene(marker);
+                    if (hasWTExpression > 0) {
+                        dbLinkList.add(getCrossReferenceDTO(marker.getZdbID(), "gene/wild_type_expression", "ZFIN"));
+                    }
+                    dbLinkList.add(getCrossReferenceDTO(marker.getZdbID(), "gene/expression", "ZFIN"));
+                    dbLinkList.add(getCrossReferenceDTO(marker.getZdbID(), "gene/expression_images", "ZFIN"));
+                }
+                dbLinkList.add(getCrossReferenceDTO(marker.getZdbID(), "gene", "ZFIN"));
+                dbLinkList.add(getCrossReferenceDTO(marker.getZdbID(), "gene/references", "ZFIN"));
+                dto.setCrossReferenceDtos(dbLinkList);
                 return dto;
             })
             .collect(toList());
+    }
+
+    static Set<String> allPrefixes = new HashSet<>();
+    static Map<String, String> prefixMap = new HashMap<>();
+
+    static {
+        prefixMap.put("ENSEMBL_GRCZ11_", "ENSEMBL");
+        prefixMap.put("UNIPROTKB", "UniProtKB");
+        prefixMap.put("GENE", "NCBI_Gene");
+    }
+
+    private static CrossReferenceDTO getCrossReferenceDTO(MarkerDBLink link, String pageArea) {
+        CrossReferenceDTO xRefDto = new CrossReferenceDTO();
+        xRefDto.setReferencedCurie(link.getAccessionNumber());
+        xRefDto.setDisplayName(link.getAccessionNumber());
+        String name = link.getReferenceDatabase().getForeignDB().getDbName().name();
+        String prefix = prefixMap.get(name);
+        if (prefix == null)
+            prefix = name;
+        if(prefix.equals("ZFIN"))
+            pageArea = "gene";
+        xRefDto.setPageArea(pageArea);
+        allPrefixes.add(prefix);
+        xRefDto.setPrefix(prefix);
+        return xRefDto;
+    }
+
+    private static CrossReferenceDTO getCrossReferenceDTO(String curie, String pageArea, String prefix) {
+        CrossReferenceDTO xRefDto = new CrossReferenceDTO();
+        xRefDto.setReferencedCurie(curie);
+        xRefDto.setDisplayName(curie);
+        xRefDto.setPageArea(pageArea);
+        xRefDto.setPrefix(prefix);
+        allPrefixes.add(prefix);
+        return xRefDto;
     }
 
     public static List<NameSlotAnnotationDTO> getSynonymSlotAnnotationDTOs(List<String> names) {
