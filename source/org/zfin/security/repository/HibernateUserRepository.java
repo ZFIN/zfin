87e1cce244e9a9a3435e8c4660b768323557f4a0
Christian Pich <cmpich@zfin.org>
2024-06-17 15:17:53 +0200

    setup / configure spring security

--- source/org/zfin/security/repository/HibernateUserRepository.java
+++ source/org/zfin/security/repository/HibernateUserRepository.java
@@ -3,8 +3,11 @@ package org.zfin.security.repository;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
 import org.zfin.framework.HibernateUtil;
 import org.zfin.profile.Person;
+
+import java.util.List;
 
 /**
  * Hibernate implementation of the UserRepository.
@@ -14,11 +17,20 @@ public class HibernateUserRepository implements UserRepository {
 
     public Person getPersonByLoginName(String username) {
         Session session = HibernateUtil.currentSession();
-        String hql = "select person from Person person left join fetch person.labs " +
-                "where person.accountInfo.login = :login ";
-        Query query = session.createQuery(hql);
-        query.setParameter("login", username);
-        return (Person) query.uniqueResult();
+        String hql = """
+            select person from Person person, AccountInfo accountInfo
+            where accountInfo in elements(person.accountInfoList)
+             AND accountInfo.login = :login
+            """;
+        Query<Person> query1 = session.createQuery(hql, Person.class);
+        query1.setParameter("login", username);
+        List<Person> list = query1.list();
+        if(CollectionUtils.isEmpty(list)){
+            return null;
+        }
+        Person person = list.get(0);
+        person.getAccountInfo();
+        return person;
     }
 
 }
