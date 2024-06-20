3b4899db745e4a995924c4569020f7011ab9f7a0
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-06-20 14:46:38 -0700

    ZFIN-9230: Add Canary as organism

--- source/org/zfin/Species.java
+++ source/org/zfin/Species.java
@@ -1,11 +1,15 @@
 package org.zfin;
 
 import com.fasterxml.jackson.annotation.JsonView;
+import lombok.Getter;
+import lombok.Setter;
 import org.zfin.framework.api.View;
 
 import javax.persistence.*;
 import java.io.Serializable;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "organism")
 public class Species implements Serializable {
@@ -29,62 +33,6 @@ public class Species implements Serializable {
     boolean antibodyHost;
     @Transient
     private Type organism;
-
-    public int getTaxonomyID() {
-        return taxonomyID;
-    }
-
-    public void setTaxonomyID(int taxonomyID) {
-        this.taxonomyID = taxonomyID;
-    }
-
-    public String getSpeciesName() {
-        return speciesName;
-    }
-
-    public void setSpeciesName(String speciesName) {
-        this.speciesName = speciesName;
-    }
-
-    public String getCommonName() {
-        return commonName;
-    }
-
-    public void setCommonName(String commonName) {
-        this.commonName = commonName;
-    }
-
-    public int getDisplayOrder() {
-        return displayOrder;
-    }
-
-    public void setDisplayOrder(int displayOrder) {
-        this.displayOrder = displayOrder;
-    }
-
-    public boolean isAntibodyImmunogen() {
-        return antibodyImmunogen;
-    }
-
-    public void setAntibodyImmunogen(boolean antibodyImmunogen) {
-        this.antibodyImmunogen = antibodyImmunogen;
-    }
-
-    public boolean isAntibodyHost() {
-        return antibodyHost;
-    }
-
-    public void setAntibodyHost(boolean antibodyHost) {
-        this.antibodyHost = antibodyHost;
-    }
-
-    public Type getOrganism() {
-        return organism;
-    }
-
-    public void setOrganism(Type organism) {
-        this.organism = organism;
-    }
 
     public enum Type {
 
