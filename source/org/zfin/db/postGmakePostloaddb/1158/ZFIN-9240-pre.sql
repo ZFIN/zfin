054a1096f09f3c44cf68d94400c2dde1ccde9422
Christian Pich <cmpich@zfin.org>
2024-06-20 13:59:28 +0200

    add unique constraint to table ensembl_transcript_renaming

--- source/org/zfin/db/postGmakePostloaddb/1158/ZFIN-9240-pre.sql
+++ source/org/zfin/db/postGmakePostloaddb/1158/ZFIN-9240-pre.sql
@@ -5,3 +5,7 @@ create table ensembl_remove_temp
 (
     ert_accession VARCHAR(100) NOT NULL
 );
+
+alter table ensembl_transcript_renaming
+    ADD CONSTRAINT ensembl_transcript_renaming_ensdart_id UNIQUE (etr_ensdart_id);
+
