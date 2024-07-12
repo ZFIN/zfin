f7bd9ab34b5082987a19089ba61d55ab4359476b
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-07-12 16:09:42 -0700

    ZFIN-9291: Fix curation dashboard list of curators - Convert hbm.xml configuration for entities to java annotation based configuration:  - Person  - Lab  - Company  - Organization

--- source/org/zfin/profile/repository/HibernateProfileRepository.java
+++ source/org/zfin/profile/repository/HibernateProfileRepository.java
@@ -1026,17 +1026,22 @@ public class HibernateProfileRepository implements ProfileRepository {
     public List<Person> getCurators() {
         Query<Person> query = currentSession()
             .createQuery("""
-                select person from Person person, AccountInfo accountInfo
-                where accountInfo in elements(person.accountInfoList)
-                AND accountInfo.curator = true
-                ORDER BY fullName
+            select distinct p from Person p
+            join p.accountInfoList a
+            where a.curator = true
+            order by p.fullName
                 """, Person.class);
         return query.list();
     }
 
     public List<Person> getStudents() {
         Query<Person> query = currentSession()
-            .createQuery("from Person where accountInfo.student = true ORDER BY fullName", Person.class);
+            .createQuery("""
+                    select distinct p from Person p
+                    join p.accountInfoList a
+                    where a.student = true
+                    order by p.fullName
+                """, Person.class);
         return query.list();
     }
 
