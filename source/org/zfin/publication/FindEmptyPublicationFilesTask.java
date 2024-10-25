7eee121cff951b8308544e235c82b38c4a406a27
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2024-10-25 08:39:14 -0700

    ZFIN-9356: Fix email sending (#1191)

--- source/org/zfin/publication/FindEmptyPublicationFilesTask.java
+++ source/org/zfin/publication/FindEmptyPublicationFilesTask.java
@@ -3,6 +3,10 @@ package org.zfin.publication;
 import org.zfin.ontology.datatransfer.AbstractScriptWrapper;
 import org.zfin.publication.presentation.PublicationService;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 import static org.zfin.repository.RepositoryFactory.getPublicationRepository;
@@ -12,20 +16,32 @@ import static org.zfin.repository.RepositoryFactory.getPublicationRepository;
  * It prints the filename, publication ZDB ID, and filetype of each empty file.
  */
 public class FindEmptyPublicationFilesTask extends AbstractScriptWrapper {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         FindEmptyPublicationFilesTask task = new FindEmptyPublicationFilesTask();
         task.runTask();
     }
 
-    private void runTask() {
+    private void runTask() throws IOException {
         initAll();
         PublicationService pubService = new PublicationService();
         List<PublicationFile> files = getPublicationRepository().getAllPublicationFiles();
         List<PublicationFile> emptyFiles = files.stream().filter(file -> pubService.getPublicationFileSizeOnDisk(file) == 0).toList();
-        System.out.println("Filename,Publication ZDB ID,Filetype");
-        for (PublicationFile file : emptyFiles) {
-            System.out.println(file.getFileName() + "," + file.getPublication().getZdbID() + "," + file.getType().getName());
+        String outputDir = ".";
+        if (System.getenv("ARTIFACTS_DIR") != null) {
+            outputDir = System.getenv("ARTIFACTS_DIR");
         }
+        File outputReportFile = new File(outputDir, "empty_publication_files.csv");
+        System.out.println("Writing report to " + outputReportFile.getAbsolutePath());
+
+        try (BufferedWriter reportFileHandle = new BufferedWriter(new FileWriter(outputReportFile))) {
+            System.out.println("Filename,Publication ZDB ID,Filetype");
+            reportFileHandle.write("Filename,Publication ZDB ID,Filetype\n");
+            for (PublicationFile file : emptyFiles) {
+                System.out.println(file.getFileName() + "," + file.getPublication().getZdbID() + "," + file.getType().getName());
+                reportFileHandle.write(file.getFileName() + "," + file.getPublication().getZdbID() + "," + file.getType().getName() + "\n");
+            }
+        }
+
         System.out.println("Found " + emptyFiles.size() + " empty publication files");
         if(emptyFiles.size() > 0) {
             System.exit(1);
