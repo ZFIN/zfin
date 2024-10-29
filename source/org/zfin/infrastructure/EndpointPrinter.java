43ed1e3076ed3bd58738a4d5a9ee4ceaafe04f26
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-10-29 15:27:35 -0700

    Helpful debugging output to list all endpoints at startup

---
+++ source/org/zfin/infrastructure/EndpointPrinter.java
@@ -0,0 +1,48 @@
+package org.zfin.infrastructure;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import javax.annotation.PostConstruct;
+import java.util.Map;
+import java.util.Set;
+
+@Component
+public class EndpointPrinter {
+
+    private final RequestMappingHandlerMapping handlerMapping;
+
+    @Autowired
+    public EndpointPrinter(RequestMappingHandlerMapping handlerMapping) {
+        this.handlerMapping = handlerMapping;
+    }
+
+    @PostConstruct
+    public void printEndpoints() {
+        //what is our dev environment?
+        ApplicationContext context = handlerMapping.getApplicationContext();
+        String[] profiles = context.getEnvironment().getActiveProfiles();
+        System.out.println("Active profiles: " + String.join(", ", profiles));
+
+
+
+        Map<RequestMappingInfo, HandlerMethod> handlerMethods = handlerMapping.getHandlerMethods();
+        for (Map.Entry<RequestMappingInfo, HandlerMethod> entry : handlerMethods.entrySet()) {
+            RequestMappingInfo mappingInfo = entry.getKey();
+            HandlerMethod handlerMethod = entry.getValue();
+
+            Set<RequestMethod> requestMethods = mappingInfo.getMethodsCondition().getMethods();
+            String patterns = mappingInfo.getPatternsCondition().toString();
+            String controller = handlerMethod.getBeanType().getName();
+            String method = handlerMethod.getMethod().getName();
+
+            System.out.println("Endpoint: " + patterns + " | HTTP Method: " + requestMethods + " | Controller: " + controller + " | Method: " + method);
+        }
+    }
+}
