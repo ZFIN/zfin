426458e77d3117e7c2661f81c44fe44a9e202d66
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2024-11-01 16:15:37 -0700

    ZFIN-9358: use zdbID string instead of object to avoid issues with passing in Feature object

--- source/org/zfin/mutant/GeneGenotypeExperimentId.java
+++ source/org/zfin/mutant/GeneGenotypeExperimentId.java
@@ -1,7 +1,10 @@
 package org.zfin.mutant;
 
+import lombok.Data;
+
 import java.io.Serializable;
 
+@Data
 public class GeneGenotypeExperimentId implements Serializable {
     private String markerId;
 
