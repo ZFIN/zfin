65ef381984b184fb5fa07eca138bd315fd4416d6
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2025-01-16 13:52:21 -0800

    debug output

--- source/org/zfin/datatransfer/webservice/NCBIRequest.java
+++ source/org/zfin/datatransfer/webservice/NCBIRequest.java
@@ -122,6 +122,9 @@ public class NCBIRequest {
             nvps.add(new BasicNameValuePair(param.getKey(), param.getValue()));
         }
         post.setEntity(new UrlEncodedFormEntity(nvps));
+        log.debug("POST URI: " + post.getURI());
+        System.out.println("POST URI: " + post.getURI());
+
         HttpResponse response = client.execute(post);
         InputStream responseContent = response.getEntity().getContent();
 
