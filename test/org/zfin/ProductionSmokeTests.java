9119c5771faa1b00da1c14458781b2794469664c
Christian Pich <cmpich@zfin.org>
2025-01-22 20:09:48 +0100

    ZFIN-9491 remove SOAP webservice classes (#1293)

--- test/org/zfin/ProductionSmokeTests.java
+++ test/org/zfin/ProductionSmokeTests.java
@@ -5,19 +5,15 @@ import org.junit.runners.Suite;
 import org.zfin.anatomy.AnatomySmokeTest;
 import org.zfin.antibody.smoketest.AntibodySmokeTest;
 import org.zfin.webservice.MarkerRestSmokeTest;
-import org.zfin.webservice.MarkerSoapClientSmokeTest;
-import org.zfin.webservice.MarkerSoapSmokeTest;
 
 /**
  * Smoke tests: Integration tests.
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        AnatomySmokeTest.class,
-        AntibodySmokeTest.class,
-        MarkerSoapSmokeTest.class,
-        MarkerSoapClientSmokeTest.class,
-        MarkerRestSmokeTest.class
+    AnatomySmokeTest.class,
+    AntibodySmokeTest.class,
+    MarkerRestSmokeTest.class
 })
 public class ProductionSmokeTests {
 
