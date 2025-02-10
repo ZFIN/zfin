1d39975851b03d2c76492f803876ae1bb6133775
Christian Pich <cmpich@zfin.org>
2025-02-10 20:07:41 +0100

    Update FishPhenotypeTable.js

--- home/javascript/react/containers/FishPhenotypeTable.js
+++ home/javascript/react/containers/FishPhenotypeTable.js
@@ -24,7 +24,7 @@ const FishPhenotypeTable = ({fishId}) => {
                 />
                 <a
                     className='popup-link data-popup-link'
-                    href={`/action/'ENSDART${row.experiment.zdbID}`}
+                    href={`/action/experiment/popup/${row.experiment.zdbID}`}
                 />
             </span>,
             filterName: 'conditionName',
