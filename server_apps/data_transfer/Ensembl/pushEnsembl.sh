7759c3096cf9f248932757b5db38629095efa79c
Christian Pich <cmpich@zfin.org>
2025-03-05 10:39:21 +0100

    remove synchornization

--- server_apps/data_transfer/Ensembl/pushEnsembl.sh
+++ server_apps/data_transfer/Ensembl/pushEnsembl.sh
@@ -12,17 +12,5 @@ mv /opt/zfin/blastdb/Current/ensembl_zf_only.x* /opt/zfin/blastdb/Backup/.
 cp ensembl_zf.x* /opt/zfin/blastdb/Current/.
 cp ensembl_zf_only.x* /opt/zfin/blastdb/Current/.
 
-
-if ("/research/zfin.org/blastdb" == "/research/zfin.org/blastdb") then
-
-    # this rsync will update the default environments: TRUNK, TEST and
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf.x* /research/zblastfiles/zmore/dev_blastdb/Current
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf_only.x* /research/zblastfiles/zmore/dev_blastdb/Current
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf.x* /research/zblastfiles/zmore/testdb/Current/
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf_only.x* /research/zblastfiles/zmore/testdb/Current/
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf.x* /research/zblastfiles/zmore/trunk/Current/
-    rsync -vu /research/zblastfiles/zmore/blastRegeneration/Current/ensembl_zf_only.x* /research/zblastfiles/zmore/trunk/Current/
-endif
-
 echo "Finish Push"
 exit
