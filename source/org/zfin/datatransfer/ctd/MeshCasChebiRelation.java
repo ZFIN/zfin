a868f0c0a22d78f222a443776291a5509460acef
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-03-11 15:19:34 -0700

    Update MeshCasChebiRelation.java

--- source/org/zfin/datatransfer/ctd/MeshCasChebiRelation.java
+++ source/org/zfin/datatransfer/ctd/MeshCasChebiRelation.java
@@ -1,1 +1,16 @@
-package org.zfin.datatransfer.ctd;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;@Data@AllArgsConstructor@NoArgsConstructorpublic class MeshCasChebiRelation {    private String mesh;    private String meshName;    private String cas;    private String chebi;    private String chebiName;}
+package org.zfin.datatransfer.ctd;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor  
+public class MeshCasChebiRelation {
+    private String mesh;
+    private String meshName;
+    private String cas;
+    private String chebi;
+    private String chebiName;
+}
