2018fd05d7ee68f0061b6c8bb0692f8dafba7a90
rtaylorzfin <90803397+rtaylorzfin@users.noreply.github.com>
2025-03-25 08:41:08 -0700

    ZFIN-9635:
    - Fix Regenerate-Chromosome-Mart_d
    - Schedule Regenerate-Chromosome-Mart_d to run daily
    - Convert from tcsh to bash Regenerate-Phenotype-Mart_d, Regenerate-Chromosome-Mart_d, Regenerate-Expression-Mart_d
    - Remove unused allChromosomeMart.sql
    - Create log of the sql run
    - Remove unnecessary "*_bkup" tables

--- server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/runChromosomeMart.sh
+++ server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/runChromosomeMart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -e
 
 # $1: Database name
-CHROMOSOMEMARTDIR="<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/"
+CHROMOSOMEMARTDIR="$ROOT_PATH/server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/"
 FULL_SCRIPT_FILE="$CHROMOSOMEMARTDIR/chromosomeMartAutomated.sql"
 CONVERT_CHROMOSOMEMART_FILE="$CHROMOSOMEMARTDIR/chromosomeMartRegen.sql"
-ALL_CHROMOSOMEMART_SCRIPTS="$CHROMOSOMEMARTDIR/allChromosomeMart.sql"
 
-rm -f "$FULL_SCRIPT_FILE" "$CONVERT_CHROMOSOMEMART_FILE" "$ALL_CHROMOSOMEMART_SCRIPTS"
+rm -f "$FULL_SCRIPT_FILE" "$CONVERT_CHROMOSOMEMART_FILE"
 
+#These all get combined into chromosomeMartAutomated.sql
 chromosomeMartScripts=(
     "begin.sql"
     "schemaTables.sql"
@@ -18,20 +18,21 @@ chromosomeMartScripts=(
     "commit.sql"
 )
 
+#These all get combined into chromosomeMartRegen.sql
 regenChromosomeMartScripts=(
     "begin.sql"
     "refreshChromosomeMart.sql"
     "commit.sql"
 )
 
-touch "$FULL_SCRIPT_FILE" "$CONVERT_CHROMOSOMEMART_FILE" "$ALL_CHROMOSOMEMART_SCRIPTS"
+touch "$FULL_SCRIPT_FILE" "$CONVERT_CHROMOSOMEMART_FILE"
 
 for name in "${chromosomeMartScripts[@]}"; do
     echo "$CHROMOSOMEMARTDIR$name"
     cat "$CHROMOSOMEMARTDIR/$name" >> "$FULL_SCRIPT_FILE"
 done
 
-cat "<!--|ROOT_PATH|-->/server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/updateSequenceFeatureChromosomeLocationPostgres.sql" >> "$FULL_SCRIPT_FILE"
+cat "$ROOT_PATH/server_apps/DB_maintenance/warehouse/chromosomeMartPostgres/updateSequenceFeatureChromosomeLocationPostgres.sql" >> "$FULL_SCRIPT_FILE"
 
 for name in "${regenChromosomeMartScripts[@]}"; do
     echo "$CHROMOSOMEMARTDIR$name"
