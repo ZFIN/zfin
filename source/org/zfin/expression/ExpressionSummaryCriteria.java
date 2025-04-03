30835ec5c45fc5ae9e7f2789a5eb25e2f90b5a16
Ryan Taylor <rtaylor@zfin.org>
2025-04-02 17:13:37 -0700

    ZFIN-9643: Enforce uniqueness for ExpressedGene.expressionStatements by using a Set internally

--- source/org/zfin/expression/ExpressionSummaryCriteria.java
+++ source/org/zfin/expression/ExpressionSummaryCriteria.java
@@ -1,5 +1,7 @@
 package org.zfin.expression;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.zfin.anatomy.DevelopmentStage;
 import org.zfin.antibody.Antibody;
 import org.zfin.marker.Marker;
@@ -11,6 +13,8 @@ import org.zfin.ontology.PostComposedEntity;
 import org.zfin.ontology.Term;
 
 
+@Setter
+@Getter
 public class ExpressionSummaryCriteria {
 
     private Marker gene;
@@ -54,92 +58,12 @@ public class ExpressionSummaryCriteria {
         return criteria;
     }
 
-    public Marker getGene() {
-        return gene;
-    }
-
-    public void setGene(Marker gene) {
-        this.gene = gene;
-    }
-
-    public FishExperiment getFishExperiment() {
-        return fishExperiment;
-    }
-
-    public void setFishExperiment(FishExperiment fishExperiment) {
-        this.fishExperiment = fishExperiment;
-    }
-
-    public Genotype getGenotype() {
-        return genotype;
-    }
-
-    public void setGenotype(Genotype genotype) {
-        this.genotype = genotype;
-    }
-
-    public Antibody getAntibody() {
-        return antibody;
-    }
-
-    public void setAntibody(Antibody antibody) {
-        this.antibody = antibody;
-    }
-
     public boolean isWildtypeOnly() {
         return isWildtypeOnly;
     }
 
     public void setWildtypeOnly(boolean wildtypeOnly) {
         isWildtypeOnly = wildtypeOnly;
-    }
-
-    public PostComposedEntity getEntity() {
-        return entity;
-    }
-
-    public void setEntity(PostComposedEntity entity) {
-        this.entity = entity;
-    }
-
-    public Term getSingleTermEitherPosition() {
-        return singleTermEitherPosition;
-    }
-
-    public void setSingleTermEitherPosition(Term singleTermEitherPosition) {
-        this.singleTermEitherPosition = singleTermEitherPosition;
-    }
-
-    public boolean isWithImagesOnly() {
-        return withImagesOnly;
-    }
-
-    public void setWithImagesOnly(boolean withImagesOnly) {
-        this.withImagesOnly = withImagesOnly;
-    }
-
-    public DevelopmentStage getStart() {
-        return start;
-    }
-
-    public void setStart(DevelopmentStage start) {
-        this.start = start;
-    }
-
-    public DevelopmentStage getEnd() {
-        return end;
-    }
-
-    public void setEnd(DevelopmentStage end) {
-        this.end = end;
-    }
-
-    public Figure getFigure() {
-        return figure;
-    }
-
-    public void setFigure(Figure figure) {
-        this.figure = figure;
     }
 
     public boolean isStandardEnvironment() {
@@ -156,30 +80,6 @@ public class ExpressionSummaryCriteria {
 
     public void setChemicalEnvironment(boolean chemicalEnvironment) {
         isChemicalEnvironment = chemicalEnvironment;
-    }
-
-    public SequenceTargetingReagent getSequenceTargetingReagent() {
-        return sequenceTargetingReagent;
-    }
-
-    public void setSequenceTargetingReagent(SequenceTargetingReagent sequenceTargetingReagent) {
-        this.sequenceTargetingReagent = sequenceTargetingReagent;
-    }
-
-    public boolean isShowCondition() {
-        return showCondition;
-    }
-
-    public void setShowCondition(boolean showCondition) {
-        this.showCondition = showCondition;
-    }
-
-    public Fish getFish() {
-        return fish;
-    }
-
-    public void setFish(Fish fish) {
-        this.fish = fish;
     }
 
     public boolean isHeatShockEnvironment() {
