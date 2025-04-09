df8806a0cf91487d1c23096c5b7217df4d08ed76
Ryan Taylor <rtaylor@zfin.org>
2025-04-09 10:43:53 -0700

    Remove unneeded classes for troubleshooting

--- source/org/zfin/infrastructure/captcha/CaptchaRedirectRequiredException.java
+++
@@ -1,7 +0,0 @@
-package org.zfin.infrastructure.captcha;
-
-public class CaptchaRedirectRequiredException extends Exception {
-    public CaptchaRedirectRequiredException(String message) {
-        super(message);
-    }
-}
