09dbe8d28085834a8d35c92c459de1e509fe7633
Ryan Taylor <rtaylor@zfin.org>
2025-04-21 16:00:41 -0700

    ZFIN-9684: Log updates

--- source/org/zfin/datatransfer/flankingsequence/FlankSeqProcessor.java
+++ source/org/zfin/datatransfer/flankingsequence/FlankSeqProcessor.java
@@ -1,6 +1,7 @@
 package org.zfin.datatransfer.flankingsequence;
 
 import htsjdk.samtools.reference.IndexedFastaSequenceFile;
+import org.apache.commons.beanutils.PropertyUtils;
 import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.logging.log4j.LogManager;
@@ -19,8 +20,10 @@ import org.zfin.publication.repository.HibernatePublicationRepository;
 import org.zfin.publication.repository.PublicationRepository;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import static org.zfin.framework.HibernateUtil.currentSession;
 
@@ -138,6 +141,7 @@ public class FlankSeqProcessor {
 
                             //progress output
                             System.out.print(".");
+                            System.out.flush();
                             if (i % 100 == 0) {
                                 System.out.println("\n " + i + " / " + nonSaFeaturesWithGenomicMutDets.size() + " features processed");
                             }
@@ -187,50 +191,119 @@ public class FlankSeqProcessor {
     }
 
     private void insertFlankSeq(Feature ftr, String seq1, String seq2, int offset) {
-        VariantSequence vrSeq = new VariantSequence();
-        if (featureRepository.getFeatureVariant(ftr) != null) {
+        boolean changed = false;
+        boolean newSequence = featureRepository.getFeatureVariant(ftr) == null;
+        VariantSequence vrSeq;
+        if (newSequence) {
+            vrSeq = new VariantSequence();
+            changed = true;
+        } else {
             vrSeq = featureRepository.getFeatureVariant(ftr);
         }
-        vrSeq.setVseqDataZDB(ftr.getZdbID());
-        vrSeq.setVfsLeftEnd(seq1);
-        vrSeq.setVfsRightEnd(seq2);
-        vrSeq.setVfsOffsetStart(offset);
-        vrSeq.setVfsOffsetStop(offset);
-        vrSeq.setVfsFlankType("genomic");
-        vrSeq.setVfsFlankOrigin("directly sequenced");
-        vrSeq.setVfsType("Genomic");
-        if (ftr.getType() == FeatureTypeEnum.DELETION) {
-            vrSeq.setVfsTargetSequence(seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + "-" + "]" + seq2);
-            vrSeq.setVfsVariation(ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + "-");
-        }
-        if (ftr.getType() == FeatureTypeEnum.INDEL) {
-            if (ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef().length() != 0) {
-                vrSeq.setVfsTargetSequence(seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2);
-                vrSeq.setVfsVariation(ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar());
+        String vfsTargetSequence = null;
+        String vfsVariation = null;
+        switch (ftr.getType()) {
+            case DELETION -> {
+                vfsTargetSequence = seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + "-" + "]" + seq2;
+                vfsVariation = ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + "-";
+            }
+            case INDEL -> {
+                if (ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef().length() != 0) {
+                    vfsTargetSequence = seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2;
+                    vfsVariation = ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar();
+                }
+            }
+            case INSERTION -> {
+                vfsTargetSequence = seq1 + "[" + "-" + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2;
+                vfsVariation = "-" + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar();
+            }
+            case POINT_MUTATION, MNV -> {
+                vfsTargetSequence = seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2;
+                vfsVariation = ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar();
             }
         }
-        if (ftr.getType() == FeatureTypeEnum.INSERTION) {
-            vrSeq.setVfsTargetSequence(seq1 + "[" + "-" + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2);
-            vrSeq.setVfsVariation("-" + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar());
-        }
-        if (ftr.getType() == FeatureTypeEnum.POINT_MUTATION) {
-            vrSeq.setVfsTargetSequence(seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2);
-            vrSeq.setVfsVariation(ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar());
-        }
-        if (ftr.getType() == FeatureTypeEnum.MNV) {
-            vrSeq.setVfsTargetSequence(seq1 + "[" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar() + "]" + seq2);
-            vrSeq.setVfsVariation(ftr.getFeatureGenomicMutationDetail().getFgmdSeqRef() + "/" + ftr.getFeatureGenomicMutationDetail().getFgmdSeqVar());
-        }
+        changed = changed || this.setFlankSeqIfChanged(vrSeq,
+                ftr.getZdbID(),
+                seq1,
+                seq2,
+                offset,
+                offset,
+                "genomic",
+                "directly sequenced",
+                "Genomic",
+                vfsTargetSequence,
+                vfsVariation);
+
         HibernateUtil.currentSession().save(vrSeq);
-        if (featureRepository.getFeatureVariant(ftr) == null) {
+        if (newSequence) {
             PublicationAttribution pa = new PublicationAttribution();
             pa.setPublication(pubRepo.getPublication("ZDB-PUB-191030-9"));
             pa.setDataZdbID(vrSeq.getZdbID());
             pa.setSourceType(RecordAttribution.SourceType.STANDARD);
             currentSession().save(pa);
         }
+        if (changed) {
+            if (newSequence) {
+                this.updated.add(List.of("NEW: " + ftr.getZdbID(), seq1, seq2));
+            } else {
+                this.updated.add(List.of("UPDATED: " + ftr.getZdbID(), seq1, seq2));
+            }
+        }
     }
 
+    private boolean setFlankSeqIfChanged(VariantSequence vrSeq, String zdbID, String vfsLeftEnd, String vfsRightEnd, int vfsOffsetStart, int vfsOffsetStop, String vfsFlankType, String vfsFlankOrigin, String vfsType, String vfsTargetSequence, String vfsVariation) {
+        boolean changed = false;
+        if (!vrSeq.getVseqDataZDB().equals(zdbID)) {
+            vrSeq.setVseqDataZDB(zdbID);
+            changed = true;
+        }
+        if (!vrSeq.getVfsLeftEnd().equals(vfsLeftEnd)) {
+            vrSeq.setVfsLeftEnd(vfsLeftEnd);
+            changed = true;
+        }
+        if (!vrSeq.getVfsRightEnd().equals(vfsRightEnd)) {
+            vrSeq.setVfsRightEnd(vfsRightEnd);
+            changed = true;
+        }
+        if (vrSeq.getVfsOffsetStart() != vfsOffsetStart) {
+            vrSeq.setVfsOffsetStart(vfsOffsetStart);
+            changed = true;
+        }
+        if (vrSeq.getVfsOffsetStop() != vfsOffsetStop) {
+            vrSeq.setVfsOffsetStop(vfsOffsetStop);
+            changed = true;
+        }
+        if (!vrSeq.getVfsFlankType().equals(vfsFlankType)) {
+            vrSeq.setVfsFlankType(vfsFlankType);
+            changed = true;
+        }
+        if (!vrSeq.getVfsFlankOrigin().equals(vfsFlankOrigin)) {
+            vrSeq.setVfsFlankOrigin(vfsFlankOrigin);
+            changed = true;
+        }
+        if (!vrSeq.getVfsType().equals(vfsType)) {
+            vrSeq.setVfsType(vfsType);
+            changed = true;
+        }
+        if (vfsTargetSequence != null && !vrSeq.getVfsTargetSequence().equals(vfsTargetSequence)) {
+            vrSeq.setVfsTargetSequence(vfsTargetSequence);
+            changed = true;
+        }
+        if (vfsVariation != null && !vrSeq.getVfsVariation().equals(vfsVariation)) {
+            vrSeq.setVfsVariation(vfsVariation);
+            changed = true;
+        }
+        return changed;
+    }
+
+    private boolean setPropertyIfChanged(VariantSequence vrSeq, String propertyName, Object value) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        Object originalProperty = PropertyUtils.getProperty(vrSeq, propertyName);
+        if (Objects.equals(originalProperty, value)) {
+            return false;
+        }
+        PropertyUtils.setProperty(vrSeq, propertyName, value);
+        return true;
+    }
 
     public List<String> getMessages() {
         return messages;
