d24ef9965033900d7736e667d187b9a3833e26fa
Ryan Taylor <rtaylor@zfin.org>
2025-04-22 16:54:55 -0700

    ZFIN-9567: New construct interface is the default Old construct interface is hidden unless enabled by flag. Using ugly front-end code to hide legacy tab due to GWT issues. Eventually, we should strip this out and drop the legacy code altogether as in this PR: : https://github.com/ZFIN/zfin/pull/1404/files

--- source/org/zfin/framework/featureflag/FeatureFlagEnum.java
+++ source/org/zfin/framework/featureflag/FeatureFlagEnum.java
@@ -8,7 +8,7 @@ public enum FeatureFlagEnum {
     CURATOR_JOB_POSTING("Curator Job Posting", true),
     USE_NAVIGATION_COUNTER("Show Navigation Counter", false),
     SHOW_ALLIANCE_DATA("Show Alliance Data", false),
-    USE_REACT_CONSTRUCT_TAB("Use React-Based Construct Tab on Curation UI", false);
+    USE_LEGACY_CONSTRUCT_TAB("Use Legacy Construct Tab", false);
 
 
     private final String name;
