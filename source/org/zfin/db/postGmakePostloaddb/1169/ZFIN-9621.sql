cdb82c20fbdf4abd85346f0b5b7306106ccb1f4a
Christian Pich <cmpich@zfin.org>
2025-04-22 18:12:53 +0200

    ZFIN-9621 more factoring

--- source/org/zfin/db/postGmakePostloaddb/1169/ZFIN-9621.sql
+++ source/org/zfin/db/postGmakePostloaddb/1169/ZFIN-9621.sql
@@ -2,6 +2,6 @@
 --changeset cmpich:ZFIN-9621
 
 delete from accession_bank  where exists (select trim(id) from zfinnucl where trim(id) = accbk_acc_num);
-delete from db_link  where exists (select trim(id) from zfinnucl where lower(trim(id)) = dblink_acc_num);
+delete from db_link  where exists (select trim(id) from zfinnucl where lower(trim(id)) = lower(dblink_acc_num));
 
 drop table if exists zfinnucl cascade;
