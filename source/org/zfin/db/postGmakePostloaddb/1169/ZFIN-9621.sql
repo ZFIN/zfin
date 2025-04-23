33532ad333fe21555d66ef2b5ffe7ff1c631e573
Christian Pich <cmpich@zfin.org>
2025-04-23 21:28:07 +0200

    ZFIN-9621 remove accessions that do not have a blast entry (#1405)
    * ZFIN-9621 remove accessions that do not have a blast entry
    
    * ZFIN-9621 more factoring

---
+++ source/org/zfin/db/postGmakePostloaddb/1169/ZFIN-9621.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+--changeset cmpich:ZFIN-9621
+
+delete from accession_bank  where exists (select trim(id) from zfinnucl where trim(id) = accbk_acc_num);
+delete from db_link  where exists (select trim(id) from zfinnucl where lower(trim(id)) = lower(dblink_acc_num));
+
+drop table if exists zfinnucl cascade;
