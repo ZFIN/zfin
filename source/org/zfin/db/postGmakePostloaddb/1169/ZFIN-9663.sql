74dd64d3abad7ceba8ceff319000403ebefca21a
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-05-05 11:13:10 -0700

    Update ZFIN-9663.sql
    remove unnecessary delete from company since it is deleted by cascade

--- source/org/zfin/db/postGmakePostloaddb/1169/ZFIN-9663.sql
+++ source/org/zfin/db/postGmakePostloaddb/1169/ZFIN-9663.sql
@@ -4,7 +4,6 @@
 update updates set rec_id = 'ZDB-COMPANY-120808-1' where rec_id = 'ZDB-COMPANY-181130-1';
 update int_data_supplier set idsup_supplier_zdb_id = 'ZDB-COMPANY-120808-1' where idsup_supplier_zdb_id = 'ZDB-COMPANY-181130-1';
 
-delete from company where zdb_id = 'ZDB-COMPANY-181130-1';
 delete from zdb_active_source where zactvs_zdb_id = 'ZDB-COMPANY-181130-1';
 
 INSERT INTO updates (submitter_id, rec_id, field_name, new_value, old_value, comments, submitter_name, upd_when) VALUES
