5e0b4af2223d641d2960adccd77c6ddf5b49d76e
Christian Pich <cmpich@zfin.org>
2025-05-17 22:13:13 +0200

    ZFIN-9714 report file

---
+++ source/org/zfin/db/postGmakePostloaddb/1170/ZFIN-9714.sql
@@ -0,0 +1,9 @@
+--liquibase formatted sql
+--changeset cmpich:ZFIN-9714
+
+select db1.dblink_acc_num, db1.dblink_linked_recid as g1, db2.dblink_linked_recid as g2 from db_link as db1, db_link as db2
+    where db1.dblink_acc_num = db2.dblink_acc_num
+    and db1.dblink_linked_recid != db2.dblink_linked_recid
+    and db1.dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
+    and db2.dblink_fdbcont_zdb_id = 'ZDB-FDBCONT-061018-1'
+;
