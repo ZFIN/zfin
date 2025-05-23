b487968f0c5e0ec041a266e2fa2b45afdcf7e340
Ryan Taylor <rtaylor@zfin.org>
2025-05-22 18:00:31 -0700

    Allow passing a workingDirectory. Allow passing other OutputStreams (not limited to ByteArrayOutputStreams) WIP: proof of concept / prototype of using a "porting" approach to converting perl to java.

--- server_apps/data_transfer/NCBIGENE/clear-artifacts.sh
+++ server_apps/data_transfer/NCBIGENE/clear-artifacts.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-rm debug* *md5 *.danio.* loadLog* *unl report* prepareLog* logNCBIgeneLoad
-
+rm debug* *md5 *.danio.* loadLog* *unl report* prepareLog* logNCBIgeneLoad after_db_link.csv after_recattrib.csv before_db_link.csv before_recattrib.csv
