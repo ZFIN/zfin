9b31f8629a2866b6a2d93e2be5fd4c08ce516941
Christian Pich <cmpich@zfin.org>
2025-05-27 22:17:50 +0200

    ZFIN-9737 move Regenerate-ZFIN-BlastDBs_d job into ZFIN_WWW repo (#1444)
    * ZFIN-9736 fix genomic genbank download endpoint
    
    * add aliases for tomcat restart
    
    * add aliases for tomcat restart
    
    * create Blast Generation ZFIN: start
    
    * switch from tcsh to bash
    
    * Update downloadzfin_genomicDNA.sh

---
+++ server_apps/data_transfer/BLAST/convertzfin_genomicDNA.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+# have to provide a path to find the refseq_zf_rna and gbk_zf* blastdbs.  Right now, we cd to the BLASTSERVER_BLAST_DATABASE_PATH to get this in our
+# path.
+
+BLASTSERVER_FASTA_FILE_PATH="/research/zblastfiles/files/blastRegeneration/fasta/ZFIN"
+BLASTSERVER_BLAST_DATABASE_PATH="/opt/zfin/blastdb/Current"
+WEBHOST_FASTA_FILE_PATH="/research/zblastfiles/dev_files"
+
+cd /opt/zfin/blastdb/Current || exit 1
+
+xdformat -n -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_genomicDNA/GenomicDNA -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_genomicDNA/xdformat_zfin_genomic_dna_all.log -I -Tgb1 -Ttpe -t "GenomicDNA" $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_genomicDNA/zfin_genomic_dna_all.fa
+
+echo "done creating the blastdb genomicDNA"
