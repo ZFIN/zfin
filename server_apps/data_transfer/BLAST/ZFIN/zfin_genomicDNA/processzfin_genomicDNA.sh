0a58b346d644121d340a8cd8cf191a9da001a649
Christian Pich <cmpich@zfin.org>
2025-06-02 22:03:20 +0200

    incremental changes

--- server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/processzfin_genomicDNA.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/processzfin_genomicDNA.sh
@@ -4,19 +4,19 @@
 
 echo "getAccFile genomicDNA" 
 
-zfin+genomicDNA//downloadzfin_genomicDNA.sh
+zfin_genomicDNA//downloadzfin_genomicDNA.sh
 
 echo "makeFastaFile genomicDNA" 
 
-zfin+genomicDNA/assemblezfin_genomicDNA.sh
+zfin_genomicDNA/assemblezfin_genomicDNA.sh
 
 echo "convert genomicDNA" 
 
-zfin+genomicDNA/convertzfin_genomicDNA.sh
+zfin_genomicDNA/convertzfin_genomicDNA.sh
 
 echo "push genomicDNA" 
 
-zfin+genomicDNA/pushzfin_genomicDNA.sh
+zfin_genomicDNA/pushzfin_genomicDNA.sh
 
 echo "done with processGenomicDNA.sh" 
 
