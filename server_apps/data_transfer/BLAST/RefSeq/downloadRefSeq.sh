5e8abbfc9e403e083b72cc61df3a49a9ac834f01
Christian Pich <cmpich@zfin.org>
2025-06-02 11:51:31 +0200

    next iteration

--- server_apps/data_transfer/BLAST/RefSeq/downloadRefSeq.sh
+++ server_apps/data_transfer/BLAST/RefSeq/downloadRefSeq.sh
@@ -5,20 +5,20 @@
 
 echo "== Download RefSeq files =="
 
-/local/bin/wget -qN "ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.protein.faa.gz"
-/local/bin/wget -qN "ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.rna.fna.gz"
+wget -N "ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.1.protein.faa.gz"
+wget -N "ftp://ftp.ncbi.nih.gov/refseq/D_rerio/mRNA_Prot/zebrafish.1.rna.fna.gz"
 
-cp zebrafish.protein.faa.gz downloadedProt.gz
-cp zebrafish.rna.fna.gz downloadedRNA.gz
+cp zebrafish.1.protein.faa.gz downloadedProt.gz
+cp zebrafish.1.rna.fna.gz downloadedRNA.gz
 
 echo "== Unzip files for refseq == "
-/local/bin/gunzip downloadedProt.gz
-/local/bin/gunzip downloadedRNA.gz
+gunzip downloadedProt.gz
+gunzip downloadedRNA.gz
 
 echo "== rename the fastafiles from refseq to more familiar names at ZFIN == "
-/bin/cp downloadedProt refseq_zf_aa.fa
-/bin/cp downloadedRNA refseq_zf_rna.fa
+cp downloadedProt refseq_zf_aa.fa
+cp downloadedRNA refseq_zf_rna.fa
 
-/bin/rm -rf downloaded*;
+rm -rf downloaded*;
 
 exit
