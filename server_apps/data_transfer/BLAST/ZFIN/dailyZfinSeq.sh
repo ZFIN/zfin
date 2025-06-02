bb5eca95a2f2bc9bd168fbb9470eb342b4a6c8ad
Christian Pich <cmpich@zfin.org>
2025-06-02 19:49:49 +0200

    add ZFIN cDNA Sequences blast DB

--- server_apps/data_transfer/BLAST/ZFIN/dailyZfinSeq.sh
+++ server_apps/data_transfer/BLAST/ZFIN/dailyZfinSeq.sh
@@ -67,76 +67,23 @@ echo "";
 echo "";
 
 echo "process zfin_publishedRNA" ;
-zfin_publishedRNA/processzfin_publishedRNA.sh
+#zfin_publishedRNA/processzfin_publishedRNA.sh
 
 echo "";
 echo "";
 
 echo "process zfin_crispr" ;
-zfin_crispr/processzfin_crispr.sh
+#zfin_crispr/processzfin_crispr.sh
 
 echo "";
 echo "";
 
 echo "process zfin_talen" ;
-zfin_talen/processzfin_talen.sh
+#zfin_talen/processzfin_talen.sh
 
 echo "";
 echo "";
 
-echo "cp all ZFIN daily updates to almost" ;
-
-if (@HOSTNAME@ == zygotix.zfin.org  && @WEBHOST_BLAST_DATABASE_PATH@ == /research/zfin.org/blastdb) then 
-    
-    # clean out the production files and replace with the newly updated ones.
-    # except for the curated blastdbs; this seems too dangerous to do.
-
-    echo "rm the backup copy that existed before today"
-    cd @WEBHOST_BLAST_DATABASE_PATH@/Backup/
-    rm -f zfin_mrph*
-    rm -f vega_zfin*
-    rm -f zfin_cdna*
-    rm -f ZFINGenesWithExpression*
-    rm -f GenomicDNA*
-    rm -f zfin_talen*
-    rm -f zfin_crispr*
-
-    echo "mv the current files to a backup"
-    cd @WEBHOST_BLAST_DATABASE_PATH@/Current/
-    mv -f zfin_mrph* ../Backup
-    mv -f vega_zfin* ../Backup
-    mv -f zfin_cdna* ../Backup
-    mv -f ZFINGenesWithExpression* ../Backup
-    mv -f GenomicDNA* ../Backup
-    mv -f zfin_talen* ../Backup
-    mv -f zfin_crispr* ../Backup
-
-    echo "cp the new files over"
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/zfin_mrph* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/vega_zfin* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/zfin_cdna* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/ZFINGenesWithExpression* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/GenomicDNA* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/zfin_talen* .
-    cp @BLASTSERVER_BLAST_DATABASE_PATH@/Current/zfin_crispr* .
-
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_mrph*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/vega_zfin*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_cdna*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/ZFINGenesWithExpression*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/GenomicDNA*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_talen*
-    /bin/chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_cripsr*
-
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_mrph*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/vega_zfin*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_cdna*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/ZFINGenesWithExpression*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/GenomicDNA*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_talen*
-    /bin/chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/zfin_crispr*
-  
-endif
 
 
 exit
