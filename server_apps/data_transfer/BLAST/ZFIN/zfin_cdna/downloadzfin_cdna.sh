09d1304748b5d38e4209f883029c7d931c325446
Christian Pich <cmpich@zfin.org>
2025-06-03 13:18:14 +0200

    incremental changes cdna files

--- server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/downloadzfin_cdna.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/downloadzfin_cdna.sh
@@ -11,7 +11,7 @@ WEBHOST_FASTA_FILE_PATH="/research/zblastfiles/files"
 # Download files
 #===============
 
-/local/bin/wget "http://zfin.org/action/blast/blast-files?action=GENBANK_CDNA" -O $WEBHOST_FASTA_FILE_PATH/zfin_cdna/zfin_genbank_cdna_acc.unl
+/local/bin/wget "http://zfin.org/action/blast/blast-files?action=GENBANK_CDNA" -O $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
 
 if [ -f $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl ]; then
 cp  $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl $BLASTSERVER_FASTA_FILE_PATH/zfin_cdna/
