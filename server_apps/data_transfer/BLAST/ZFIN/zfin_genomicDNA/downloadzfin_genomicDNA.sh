6b2e83992b18d27f07d005fe3b8a981fb740c6b3
Christian Pich <cmpich@zfin.org>
2025-06-03 11:00:45 +0200

    incremental changes

--- server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/downloadzfin_genomicDNA.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/downloadzfin_genomicDNA.sh
@@ -8,10 +8,11 @@ WEBHOST_FASTA_FILE_PATH="/research/zblastfiles/files"
 
 /local/bin/wget "http://zfin.org/action/blast/blast-files?action=GENOMIC_GENBANK" -O $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
 
-if ( -f $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl ) then
-cp $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_genomicDNA/;
+# shellcheck disable=SC1010
+if [ -f $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl ] then
+cp $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_genomicDNA/
 else
 echo "file $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl is empty, not copying."
-endif
+fi
 
 exit
