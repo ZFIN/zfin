722825aa4143fd3613af510dcbbbe053ab30de1e
Christian Pich <cmpich@zfin.org>
2025-06-03 11:25:28 +0200

    incremental changes

--- server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/assemblezfin_genomicDNA.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/assemblezfin_genomicDNA.sh
@@ -6,20 +6,18 @@
 BLAST_DATABASE_PATH="/opt/zfin/blastdb"
 WEBHOST_FASTA_FILE_PATH="/research/zblastfiles/files"
 
-BLASTSERVER_FASTA_FILE_PATH="/research/zblastfiles/files/blastRegeneration"
+BLASTSERVER_FASTA_FILE_PATH="/research/zblastfiles/files/blastRegeneration/fasta/ZFIN"
 
 # get a fasta file from the GenBank db
 
 xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/xdget_zfin_genomic_genbank_acc.log -o $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_mrna.fa -Tgb1 $BLAST_DATABASE_PATH/Current/gbk_zf_mrna $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
 
-xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/xdget_zfin_genomic_genbank_acc.log -o $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_rna.fa -Tgb1 BLAST_DATABASE_PATH/Current/gbk_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/xdget_zfin_genomic_genbank_acc.log -o $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_rna.fa -Tgb1 $BLAST_DATABASE_PATH/Current/gbk_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
 
-xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/xdget_zfin_genomic_genbank_acc.log -o $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_dna.fa -Tgb1 BLAST_DATABASE_PATH/Current/gbk_zf_dna $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/xdget_zfin_genomic_genbank_acc.log -o $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_dna.fa -Tgb1 $BLAST_DATABASE_PATH/Current/gbk_zf_dna $WEBHOST_FASTA_FILE_PATH/zfin_genomic_genbank_acc.unl
 
 /bin/cat $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_mrna.fa $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_rna.fa $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all_dna.fa > $BLASTSERVER_FASTA_FILE_PATH/zfin_genomic_dna_all.fa
 
 echo "done making fasta files genomicDNA"
-
-cd -
 
 exit
