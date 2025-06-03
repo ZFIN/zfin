1ada8e5d92ac0fb60042cdffd700e7c897ed9d8d
Christian Pich <cmpich@zfin.org>
2025-06-03 14:38:28 +0200

    incremental changes cdna files

--- server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/assemblezfin_cdna.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/assemblezfin_cdna.sh
@@ -13,13 +13,13 @@ WEBHOST_FASTA_FILE_PATH="/research/zblastfiles/files"
  # Generate fasta file
  #=====================
 
-@BLASTSERVER_XDGET@ -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_mrna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_mrna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_mrna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_mrna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_mrna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_mrna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
 
-@BLASTSERVER_XDGET@ -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_dna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_dna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_dna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_dna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_dna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_dna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_acc.unl
 
-@BLASTSERVER_XDGET@ -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_rna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_rna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_seq_rna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_gb_seq_rna.fa $BLAST_DATABASE_PATH/Current/gbk_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
 
-@BLASTSERVER_XDGET@ -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_refseq_seq_rna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_refseq_rna.fa $BLAST_DATABASE_PATH/Current/refseq_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
+xdget -n -f -e $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/xdget_zfin_refseq_seq_rna.log -o $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/zfin_cdna/new_zfin_refseq_rna.fa $BLAST_DATABASE_PATH/Current/refseq_zf_rna $WEBHOST_FASTA_FILE_PATH/zfin_genbank_cdna_acc.unl
 
 # cat the two new mrna files together to become cdna_seq
 cat new_zfin_refseq_rna.fa >> new_zfin_gb_seq_mrna.fa
