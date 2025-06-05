2fa1f9d013c0c0d49a3813e4162548347bf1bd9d
Christian Pich <cmpich@zfin.org>
2025-06-05 18:06:44 +0200

    refactor refSeq

--- server_apps/data_transfer/BLAST/RefSeq/convertRefSeq.sh
+++ server_apps/data_transfer/BLAST/RefSeq/convertRefSeq.sh
@@ -7,9 +7,9 @@ source "config.sh"
 log_message "Starting RefSeq conversion..."
 log_message "Format the RefSeq files into blast db"
 
-xdformat -p -e xdformat_refseq_zf_aa.log -t "ReqSeq Zebrafish protein" -I -Tref -o refseq_zf_aa refseq_zf_aa.fa
+xdformat -q -p -e xdformat_refseq_zf_aa.log -t "ReqSeq Zebrafish protein" -I -Tref -o refseq_zf_aa refseq_zf_aa.fa
 
-xdformat -n -e xdformat_refseq_zf_rna.log -t "RefSeq Zebrafish mRNA" -I -Tref -o refseq_zf_rna refseq_zf_rna.fa
+xdformat -q -n -e xdformat_refseq_zf_rna.log -t "RefSeq Zebrafish mRNA" -I -Tref -o refseq_zf_rna refseq_zf_rna.fa
 
 log_message "Finished RefSeq conversion"
 exit
