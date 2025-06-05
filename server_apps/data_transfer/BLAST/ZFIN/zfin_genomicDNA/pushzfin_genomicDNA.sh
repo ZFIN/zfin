f17fafa290907ab5386b8f451b4751255a29f549
Christian Pich <cmpich@zfin.org>
2025-06-05 18:49:28 +0200

    refactor refSeq

--- server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/pushzfin_genomicDNA.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/pushzfin_genomicDNA.sh
@@ -2,21 +2,19 @@
 
 source "../config.sh"
 
-BLASTSERVER_FASTA_FILE_PATH="/research/zblastfiles/files/blastRegeneration/fasta/ZFIN"
-
 # move current to backup
 
 mv $BLAST_PATH/Current/GenomicDNA.* $BLAST_PATH/Backup/
 
-# move new to current
+# cp new to current
 
-mv GenomicDNA.* $BLAST_PATH/Current/
+cp GenomicDNA.* $BLAST_PATH/Current/
 
 
 #if (@HOSTNAME@ == genomics.cs.uoregon.edu) then
 #    ./distributeToNodesGenomicDNA.sh
 #endif 
 
-echo "done moving the genomicDNA blastdbs to /Current/"
+log_message "done copying the genomicDNA blastdbs to $BLAST_PATH"
 
 exit
