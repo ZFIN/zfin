5e7d9c9b693337837f8302823ea0e92eb18095be
Christian Pich <cmpich@zfin.org>
2025-06-05 18:35:45 +0200

    refactor refSeq

--- server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/processzfin_genomicDNA.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_genomicDNA/processzfin_genomicDNA.sh
@@ -7,19 +7,19 @@ source "../config.sh"
 log_message "***** Starting <RefSeq Zebrafish mRNA> *****"
 
 log_message "get Accession File"
-zfin_genomicDNA/downloadzfin_genomicDNA.sh
+downloadzfin_genomicDNA.sh
 
 log_message "Make Fasta File genomic DNA"
-zfin_genomicDNA/assemblezfin_genomicDNA.sh
+assemblezfin_genomicDNA.sh
 
 echo "convert genomicDNA" 
 
 log_message "Convert "
-zfin_genomicDNA/convertzfin_genomicDNA.sh
+convertzfin_genomicDNA.sh
 
 echo "push genomicDNA" 
 
-zfin_genomicDNA/pushzfin_genomicDNA.sh
+pushzfin_genomicDNA.sh
 
 echo "done with processGenomicDNA.sh" 
 
