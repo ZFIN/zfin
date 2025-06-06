595985924ad9af9802880133f1f2bda1e862452e
Christian Pich <cmpich@zfin.org>
2025-06-06 15:20:04 +0200

    refactor morpho

--- server_apps/data_transfer/BLAST/ZFIN/zfin_mrph/convertzfin_mrph.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_mrph/convertzfin_mrph.sh
@@ -8,7 +8,7 @@ source "../config.sh"
 # Xdformat
 #=============
 
-xdformat -n -o zfin_mrph -e zfin_mrph/xdformat_zfin_mrph.log -I -Tuser -t "ZFIN Morpholino Sequence Set" zfin_mrph.fa
+xdformat -n -o zfin_mrph -e xdformat_zfin_mrph.log -I -Tuser -t "ZFIN Morpholino Sequence Set" zfin_mrph.fa
 
 log_message "Finished making the new zfin_mrph and microRNA dbs"
 
