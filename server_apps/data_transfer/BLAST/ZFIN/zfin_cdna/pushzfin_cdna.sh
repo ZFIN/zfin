969830d8ae3e853b718f4a3c04935784181f2c3e
Christian Pich <cmpich@zfin.org>
2025-06-06 13:40:43 +0200

    refactor cdna

--- server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/pushzfin_cdna.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_cdna/pushzfin_cdna.sh
@@ -11,13 +11,11 @@ source "../config.sh"
 # Move current to backup
 # update current dir
 #========================
-cp -f $BLAST_PATH/Current/zfin_gb_* $BLAST_PATH/Backup
+cp -f $BLAST_PATH/Current/zfin_gb_* $BLAST_PATH/Backup/
 mv -f zfin_gb_seq.x* $BLAST_PATH/Current
 
-cp -f $BLAST_PATH/Current/zfin_cdna_seq* $BLAST_PATH/Backup
+cp -f $BLAST_PATH/Current/zfin_cdna_seq* $BLAST_PATH/Backup/
 mv -f zfin_cdna_seq.x* $BLAST_PATH/Current
-
-mv -f $*.fa $BLASTSERVER_FASTA_FILE_PATH/fasta/ZFIN/Backup
 
 #if (@HOSTNAME@ == genomix.cs.uoregon.edu) then
 # @TARGET_PATH@/ZFIN/zfin_cdna/distributeToNodeszfin_cdna.sh
