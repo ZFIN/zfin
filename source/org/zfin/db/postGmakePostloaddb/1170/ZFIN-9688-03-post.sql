b7811f9f0a083d3a97b209f156efac02c95b0a23
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-10 13:20:12 -0700

    Update ZFIN-9688-03-post.sql

--- source/org/zfin/db/postGmakePostloaddb/1170/ZFIN-9688-03-post.sql
+++ source/org/zfin/db/postGmakePostloaddb/1170/ZFIN-9688-03-post.sql
@@ -1,5 +1,6 @@
 --liquibase formatted sql
 --changeset rtaylor:ZFIN-9688-01-pre
 
+delete from tmp_sfclg where sfclg_data_zdb_id not in (select zactvd_zdb_id from zdb_active_data);
 insert into sequence_feature_chromosome_location_generated (select * from tmp_sfclg);
 DROP TABLE tmp_sfclg;
