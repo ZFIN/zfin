5633b3fe116e5b8b9f84b0e8188fd9c788e104ef
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-11 16:07:46 -0700

    fix collision possibility

--- lib/DB_functions/get_id_and_insert_active_data.sql
+++ lib/DB_functions/get_id_and_insert_active_data.sql
@@ -1,10 +1,21 @@
 -- Return the next available id for a given zobjtype and insert a new active data record
 CREATE OR REPLACE FUNCTION get_id_and_insert_active_data(obj_name varchar) RETURNS text AS $$
 DECLARE
-    next_id varchar;
+    need_another_id boolean := true;
+    next_id varchar := '';
 BEGIN
-    next_id := get_id(obj_name);
-    INSERT INTO zdb_active_data (zactvd_zdb_id) VALUES (next_id);
+    WHILE need_another_id LOOP
+        need_another_id := false;
+        next_id := get_id(obj_name);
+
+        BEGIN
+            INSERT INTO zdb_active_data (zactvd_zdb_id) VALUES (next_id);
+        EXCEPTION
+            WHEN unique_violation THEN
+                need_another_id := true;
+        END;
+    END LOOP;
+
     RETURN next_id;
 END;
 $$ LANGUAGE plpgsql;
