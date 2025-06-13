4ad12eab68650d044297473bf82d1d54316f57d5
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-13 10:25:47 -0700

    ZFIN-9688: Bugfix CSV mapping to null instead of empty string (#1463)

--- source/org/zfin/db/postGmakePostloaddb/1170/ZFIN-9688-03-post.sql
+++ source/org/zfin/db/postGmakePostloaddb/1170/ZFIN-9688-03-post.sql
@@ -1,6 +1,13 @@
 --liquibase formatted sql
---changeset rtaylor:ZFIN-9688-01-pre
+--changeset rtaylor:ZFIN-9688-03-post
 
-delete from tmp_sfclg where sfclg_data_zdb_id not in (select zactvd_zdb_id from zdb_active_data);
-insert into sequence_feature_chromosome_location_generated (select * from tmp_sfclg);
+DELETE FROM tmp_sfclg
+WHERE NOT EXISTS (
+    SELECT 'x' FROM zdb_active_data WHERE sfclg_data_zdb_id = zactvd_zdb_id);
+
+UPDATE tmp_sfclg set sfclg_gbrowse_track = null where sfclg_gbrowse_track = '';
+
+INSERT INTO sequence_feature_chromosome_location_generated (
+    SELECT * FROM tmp_sfclg);
+
 DROP TABLE tmp_sfclg;
