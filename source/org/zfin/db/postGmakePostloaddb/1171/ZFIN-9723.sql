617e5280f0a2a2c0473112073b12062551f76a3d
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-13 15:52:21 -0700

    ZFIN-9723: Fix features where the abbrev_order is not set correctly

--- source/org/zfin/db/postGmakePostloaddb/1171/ZFIN-9723.sql
+++ source/org/zfin/db/postGmakePostloaddb/1171/ZFIN-9723.sql
@@ -2,5 +2,5 @@
 --changeset rtaylor:ZFIN-9723
 
 -- Fix feature abbreviation order
-update feature set feature_abbrev_order = zero_pad(feature_abbrev_order) where feature_abbrev_order <> zero_pad(feature_abbrev_order);
+update feature set feature_abbrev_order = zero_pad(feature_abbrev) where feature_abbrev_order <> zero_pad(feature_abbrev);
 
