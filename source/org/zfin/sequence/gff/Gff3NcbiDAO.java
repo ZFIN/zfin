825efb3eefdb6286f571cba285e0e94ae5925369
Christian Pich <cmpich@zfin.org>
2025-06-23 12:34:51 +0200

    create zfin_gene and zfin_refSeq gff3 files

--- source/org/zfin/sequence/gff/Gff3NcbiDAO.java
+++ source/org/zfin/sequence/gff/Gff3NcbiDAO.java
@@ -1,11 +1,14 @@
 package org.zfin.sequence.gff;
 
+import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.zfin.framework.dao.BaseSQLDAO;
 
+import java.util.List;
+
 public class Gff3NcbiDAO extends BaseSQLDAO<Gff3Ncbi> {
 
-    protected Session entityManager;
+//    protected Session entityManager;
 
     public Gff3NcbiDAO(Session entityManager) {
         super(Gff3Ncbi.class);
@@ -16,4 +19,23 @@ public class Gff3NcbiDAO extends BaseSQLDAO<Gff3Ncbi> {
         super(Gff3Ncbi.class);
     }
 
+    public List<Gff3Ncbi> findRecordsBySource(String bestRefSeq) {
+        TypedQuery<Gff3Ncbi> query = entityManager.createQuery("""
+            from Gff3Ncbi gff3
+            join fetch gff3.attributePairs
+                        where gff3.source = :bestRefSeq
+            """, Gff3Ncbi.class);
+        query.setParameter("bestRefSeq", bestRefSeq);
+        return query.getResultList();
+    }
+
+    public List<Gff3Ncbi> findRecordsByFeature(String featureName) {
+        TypedQuery<Gff3Ncbi> query = entityManager.createQuery("""
+            from Gff3Ncbi gff3
+            join fetch gff3.attributePairs
+                        where gff3.feature = :feature
+            """, Gff3Ncbi.class);
+        query.setParameter("feature", featureName);
+        return query.getResultList();
+    }
 }
