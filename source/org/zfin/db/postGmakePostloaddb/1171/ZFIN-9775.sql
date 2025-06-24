b9515677c134b20ab16e4e1c9911924a0fe66ca0
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-24 08:40:59 -0700

    Update ZFIN-9775.sql (#1472)

--- source/org/zfin/db/postGmakePostloaddb/1171/ZFIN-9775.sql
+++ source/org/zfin/db/postGmakePostloaddb/1171/ZFIN-9775.sql
@@ -2,5 +2,4 @@
 --changeset rtaylor:ZFIN-9775
 
 DELETE FROM zdb_active_data
-WHERE zactvd_zdb_id IN ('ZDB-NCCR-250521-2'
-        AND 'ZDB-NCCR-250521-1');
+WHERE zactvd_zdb_id IN ('ZDB-NCCR-250521-2', 'ZDB-NCCR-250521-1');
