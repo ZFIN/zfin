e5d2944998fdce863b01672d258e62557a9a3c26
Christian Pich <cmpich@zfin.org>
2025-06-25 09:40:53 +0200

    fix crispr error in script file

--- server_apps/data_transfer/BLAST/ZFIN/zfin_crispr/downloadzfin_crispr.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_crispr/downloadzfin_crispr.sh
@@ -7,10 +7,6 @@
 
 source "../config.sh"
 
-cp -f @BLASTSERVER_BLAST_DATABASE_PATH@/Current/zfin_crispr.xn* @BLASTSERVER_BLAST_DATABASE_PATH@/Backup 
-
-cp @BLASTSERVER_FASTA_FILE_PATH@/fasta/ZFIN/zfin_crispr/*.fa @BLASTSERVER_FASTA_FILE_PATH@/fasta/ZFIN/Backup
-
 wget -q "http://zfin.org/action/blast/blast-files?action=CRISPR" -O zfin_crispr.fa
 
 if [ -f zfin_crispr.fa ]; then
