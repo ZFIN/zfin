c4191294d484facb38e2ce6d0fe9d13fa4cfb8b2
Christian Pich <cmpich@zfin.org>
2025-06-25 10:43:27 +0200

    ZFIN-9802 create jenkins job for ensembl baslt generation

--- server_apps/data_transfer/BLAST/Ensembl/processEnsembl.sh
+++ server_apps/data_transfer/BLAST/Ensembl/processEnsembl.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash -e
 #
 # Usage:
 #       processEnsembl.sh remote_file_name
--- server_apps/data_transfer/BLAST/Ensembl/pushEnsembl.sh
+++ server_apps/data_transfer/BLAST/Ensembl/pushEnsembl.sh
@@ -1,8 +1,9 @@
-#!/bin/tcsh
+#!/bin/bash -e
 #
 # Push Ensembl blastdbs to their production location.
 #
 
+source config.sh
 rm -f /opt/zfin/blastdb/Backup/ensembl_zf.x*
 rm -f /opt/zfin/blastdb/Backup/ensembl_zf_only.x*
 
@@ -12,5 +13,5 @@ mv /opt/zfin/blastdb/Current/ensembl_zf_only.x* /opt/zfin/blastdb/Backup/.
 cp ensembl_zf.x* /opt/zfin/blastdb/Current/.
 cp ensembl_zf_only.x* /opt/zfin/blastdb/Current/.
 
-echo "Finish Push"
+echo "Finish Push Ensembl Blast DBs."
 exit
