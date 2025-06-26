454a1a39138b32fa3aeda2bc1b4264a632333c9d
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-06-26 13:57:56 -0700

    ZFIN-9799: Convert placeholders to actual values

--- server_apps/data_transfer/BLAST/VegaProtein/processVegaProtein.sh
+++ server_apps/data_transfer/BLAST/VegaProtein/processVegaProtein.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-cd @TARGET_PATH@/VegaProtein/
+TARGET_PATH=$TARGETROOT/server_apps/data_transfer/BLAST
 
-@TARGET_PATH@/VegaProtein/downloadVegaProtein.sh
+cd $TARGET_PATH/VegaProtein/
 
-@TARGET_PATH@/VegaProtein/convertVegaProtein.sh 
-@TARGET_PATH@/VegaProtein/pushVegaProtein.sh
+$TARGET_PATH/VegaProtein/downloadVegaProtein.sh
+
+$TARGET_PATH/VegaProtein/convertVegaProtein.sh 
+$TARGET_PATH/VegaProtein/pushVegaProtein.sh
 
 exit
