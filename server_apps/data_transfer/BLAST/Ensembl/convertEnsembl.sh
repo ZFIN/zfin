802ab16e84f861b47cb28fde2be842e64f57738a
Christian Pich <cmpich@zfin.org>
2025-06-30 12:38:10 +0200

    fix rm error

--- server_apps/data_transfer/BLAST/Ensembl/convertEnsembl.sh
+++ server_apps/data_transfer/BLAST/Ensembl/convertEnsembl.sh
@@ -8,8 +8,8 @@
 
 echo "==| start convertEnsembl.sh |=="
 
-rm -r xdformat_ensembl_zf.log
-rm -r xdformat_ensembl_zf_only.log
+rm -f xdformat_ensembl_zf.log
+rm -f xdformat_ensembl_zf_only.log
 
 echo "==| Format the FASTA files into blast db files |== "
 
