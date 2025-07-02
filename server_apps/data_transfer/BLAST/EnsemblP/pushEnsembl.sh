582ae0ef847780cf170b11f59deb09e8ea591a1d
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-07-02 14:40:28 -0700

    ZFIN-9796: Remove template strings in comments.
    Make it easier to search for files that still need to have template strings replaced.

--- server_apps/data_transfer/BLAST/EnsemblP/pushEnsembl.sh
+++ server_apps/data_transfer/BLAST/EnsemblP/pushEnsembl.sh
@@ -2,6 +2,8 @@
 #
 # Push Ensembl blastdbs to their production location.
 #
+BLASTSERVER_BLAST_DATABASE_PATH=/opt/zfin/blastdb
+WEBHOST_BLAST_DATABASE_PATH=/opt/zfin/blastdb
 
 source ../config.sh
 
@@ -19,19 +21,21 @@ rm -rf downloaded*;
 
 # this rsync will update the default environment on zygotix for developers
 
-# TODO: check with Ryan Martin if this is still needed
-# There is a separate job for this push so we should be able to remove this
-  #rsync -rcvuK @BLASTSERVER_BLAST_DATABASE_PATH@/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/dev_blastdb/Current
-  #rsync -rcvuK @BLASTSERVER_BLAST_DATABASE_PATH@/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/testdb/Current/
-  #rsync -rcvuK @BLASTSERVER_BLAST_DATABASE_PATH@/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/trunk/Current/
-  # update webhost only if this is a prod run
-  #rm -f @WEBHOST_BLAST_DATABASE_PATH@/Backup/ensemblProt_zf.x*
-  #mv @WEBHOST_BLAST_DATABASE_PATH@/Current/ensemblProt_zf.x* @WEBHOST_BLAST_DATABASE_PATH@/Backup
-  #cp ensemblProt_zf.x* @WEBHOST_BLAST_DATABASE_PATH@/Current/
-  # Does this need to be done (chgrp and chmod)?
-  #chgrp zfishweb @WEBHOST_BLAST_DATABASE_PATH@/Current/ensemblProt_zf*.x*
-  #chmod 664 @WEBHOST_BLAST_DATABASE_PATH@/Current/ensemblProt_zf*.x*
+if [[ "$WEBHOST_BLAST_DATABASE_PATH" == "/research/zfin.org/blastdb" ]]
+then
+  rsync -rcvuK $BLASTSERVER_BLAST_DATABASE_PATH/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/dev_blastdb/Current
+  rsync -rcvuK $BLASTSERVER_BLAST_DATABASE_PATH/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/testdb/Current/
+  rsync -rcvuK $BLASTSERVER_BLAST_DATABASE_PATH/Current/ensemblProt_zf.x* /research/zblastfiles/zmore/trunk/Current/
 
+  #update webhost only if this is a prod run
+  rm -f $WEBHOST_BLAST_DATABASE_PATH/Backup/ensemblProt_zf.x*
+  mv $WEBHOST_BLAST_DATABASE_PATH/Current/ensemblProt_zf.x* $WEBHOST_BLAST_DATABASE_PATH/Backup
+  cp ensemblProt_zf.x* $WEBHOST_BLAST_DATABASE_PATH/Current/
+
+  #Does this need to be done (chgrp and chmod)?
+  chgrp zfishweb $WEBHOST_BLAST_DATABASE_PATH/Current/ensemblProt_zf*.x*
+  chmod 664 $WEBHOST_BLAST_DATABASE_PATH/Current/ensemblProt_zf*.x*
+fi
 
 
 rm -f ensemblProt_zf.x*
--- server_apps/data_transfer/BLAST/miRBASE/revertmiRBASE.sh
+++ server_apps/data_transfer/BLAST/miRBASE/revertmiRBASE.sh
@@ -8,8 +8,8 @@ rm -f $BLASTSERVER_BLAST_DATABASE_PATH/Current/LoadedMicroRNAStemLoop.xn*
 cp $BLASTSERVER_BLAST_DATABASE_PATH/Backup/LoadedMicroRNAMature.xn* $BLASTSERVER_BLAST_DATABASE_PATH/Current
 cp $BLASTSERVER_BLAST_DATABASE_PATH/Backup/LoadedMicroRNAStemLoop.xn* $BLASTSERVER_BLAST_DATABASE_PATH/Current
 
-#if (@HOSTNAME@ == genomix.cs.uoregon.edu) then
-#   @TARGET_PATH@/miRBASE/distributeToNodesmiRBASE.sh
+#if ($HOSTNAME$ == genomix.cs.uoregon.edu) then
+#   $TARGET_PATH/miRBASE/distributeToNodesmiRBASE.sh
 #endif
 #
 echo "done with revertzfin_cdna.sh"
