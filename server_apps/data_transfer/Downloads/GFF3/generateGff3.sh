ee3e37247b5d88a433ce45c47919ec03948e790e
Christian Pich <cmpich@zfin.org>
2025-07-09 07:10:00 +0200

    generate gff3 files and push into download directory

--- server_apps/data_transfer/Downloads/GFF3/generateGff3.sh
+++ server_apps/data_transfer/Downloads/GFF3/generateGff3.sh
@@ -3,3 +3,9 @@
 psql -v ON_ERROR_STOP=1 -d $DBNAME -f E_zfin_ensembl_gene.sql -f E_expression_gff3.sql -f E_phenotype_gff3.sql -f E_antibody_gff3.sql -f unload_mutants.sql -f zfin_zmp_gff3.sql
 
 ./generateGff3.groovy
+
+cd $SOURCEROOT
+gradle createGff3Files
+
+cp zfin_genes*.gff3 /opt/zfin//www_homes/zfin.org/home/data_transfer/Downloads/.
+cp zfin_ref*.gff3 /opt/zfin//www_homes/zfin.org/home/data_transfer/Downloads/.
