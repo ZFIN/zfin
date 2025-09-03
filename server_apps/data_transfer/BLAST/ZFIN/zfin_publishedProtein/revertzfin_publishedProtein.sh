822145112d76244b60c8a8cbdb412f6848430294
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-09-03 08:37:20 -0700

    Remove genomix host check from script
    Comment out the conditional execution for genomix host.

--- server_apps/data_transfer/BLAST/ZFIN/zfin_publishedProtein/revertzfin_publishedProtein.sh
+++ server_apps/data_transfer/BLAST/ZFIN/zfin_publishedProtein/revertzfin_publishedProtein.sh
@@ -6,10 +6,11 @@ setenv HOSTNAME $INSTANCE
 rm -f $BLASTSERVER_BLAST_DATABASE_PATH/Current/publishedProtein.xn* 
 cp $BLASTSERVER_BLAST_DATABASE_PATH/Backup/publishedProtein.xn* $BLASTSERVER_BLAST_DATABASE_PATH/Current
 
-
-if ($HOSTNAME == genomix.cs.uoregon.edu) then
-  $TARGET_PATH/ZFIN/zfin_publishedProtein.sh
-endif
+# comment out because genomix is no longer with us
+# keeping the commented version in case we need to re-examine the logic
+# if ($HOSTNAME == genomix.cs.uoregon.edu) then
+#  $TARGET_PATH/ZFIN/zfin_publishedProtein.sh
+# endif
 
 echo "done with revertzfin_publishedProtein.sh" ;
 exit
