d5592dbc9a26690c8aee3c5ca22141449c48e20b
Christian Pich <cmpich@zfin.org>
2025-09-23 15:04:10 +0200

    Zfin 9898 (#1565)
    * ZFIN-9898 set GRCz11 for genes without z12 info but grcz11 info in sequence_feture_chromosome_location_generated table
    
    * ZFIN-9898 add date_created column to marker_assembly table
    
    * add default time stamp

--- server_apps/data_transfer/NCBIGENE/markerAssemblyUpdate.sql
+++ server_apps/data_transfer/NCBIGENE/markerAssemblyUpdate.sql
@@ -100,3 +100,26 @@ where gna_key = 'Dbxref'
   and gna_gff_pk_id = gff_pk_id
   and gff_feature in ('gene', 'pseudogene');
 
+-- marker gene as GRCz11 if they do not have a z12 association but have a sequence_feature_chromosome_location_generated record for ZFIN with GRCz11
+insert into marker_assembly
+select distinct gg.sfclg_data_zdb_id, 3 from sequence_feature_chromosome_location_generated as gg
+where gg.sfclg_assembly = 'GRCz11'
+  and gg.sfclg_location_source = 'ZfinGbrowseStartEndLoader'
+  and gg.sfclg_acc_num like 'ENSDARG%'
+  and not exists (
+        select * from zfindb.public.sequence_feature_chromosome_location_generated as ss
+        where gg.sfclg_data_zdb_id = ss.sfclg_data_zdb_id
+          and ss.sfclg_assembly = 'GRCz12tu'
+    )
+  and not exists (
+        select * from db_link
+        where dblink_linked_recid = gg.sfclg_data_zdb_id
+          and dblink_acc_num = gg.sfclg_acc_num
+          and dblink_fdbcont_zdb_id ='ZDB-FDBCONT-040412-1'
+    )
+  and not exists (
+        select * from marker_assembly as g
+        where gg.sfclg_data_zdb_id = g.ma_mrkr_zdb_id
+          and g.ma_a_pk_id =3
+    )
+;
