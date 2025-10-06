65099d7a5653ba4ab0e6fc8520c297d266c68234
Christian Pich <cmpich@zfin.org>
2025-10-06 17:12:45 +0200

    ZFIN-9963 populate the strand info on sequence_feature_chromosome_location_generated table

--- source/org/zfin/db/postGmakePostloaddb/1175/ZFIN-9963.sql
+++ source/org/zfin/db/postGmakePostloaddb/1175/ZFIN-9963.sql
@@ -7,3 +7,17 @@ alter table sequence_feature_chromosome_location_generated
 add column     sfclg_strand CHAR(1),
 add COLUMN     sfclg_date_created TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 ;
+
+CREATE INDEX gff3_feature_index ON gff3 (gff_feature);
+CREATE INDEX gff3_id_index ON gff3 (gff_id);
+
+-- set strand info for ensembl GRCz11 genes
+update sequence_feature_chromosome_location_generated
+set sfclg_strand = (select gff_strand
+                    from gff3
+                    where gff_feature = 'gene'
+                      and gff_id = sfclg_acc_num
+                        )
+where sfclg_location_source = 'EnsemblStartEndLoader'
+and sfclg_fdb_db_id = 7;
+;
