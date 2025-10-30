48474f821ad7bf5afbc076d29c5a988f875bf98f
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-10-30 12:36:50 -0700

    Remove captcha from nomenclature form

--- source/org/zfin/nomenclature/presentation/NomenclatureSubmissionController.java
+++ source/org/zfin/nomenclature/presentation/NomenclatureSubmissionController.java
@@ -1,6 +1,5 @@
 package org.zfin.nomenclature.presentation;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
@@ -17,7 +16,6 @@ import org.zfin.framework.presentation.LookupStrings;
 import org.zfin.gwt.root.dto.PublicationDTO;
 import org.zfin.gwt.root.server.DTOConversionService;
 import org.zfin.infrastructure.PublicationAttribution;
-import org.zfin.infrastructure.captcha.CaptchaService;
 import org.zfin.infrastructure.seo.CanonicalLinkConfig;
 import org.zfin.marker.Marker;
 import org.zfin.marker.MarkerHistory;
@@ -26,7 +24,10 @@ import org.zfin.properties.ZfinPropertiesEnum;
 import org.zfin.publication.Publication;
 import org.zfin.repository.RepositoryFactory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 import static org.zfin.repository.RepositoryFactory.*;
 
@@ -72,12 +73,7 @@ public class NomenclatureSubmissionController {
     }
 
     @RequestMapping(value = "/gene-name", method = RequestMethod.GET)
-    public String newGeneNameForm(Model model, HttpServletRequest request) {
-        //TODO: This would read better if it was an annotation on the method (eg. `@RequiresCaptcha`)
-        Optional<String> captchaRedirectUrl = CaptchaService.getRedirectUrlIfNeeded(request);
-        if (captchaRedirectUrl.isPresent()) {
-            return "redirect:" + captchaRedirectUrl.get();
-        }
+    public String newGeneNameForm(Model model) {
         GeneNameSubmission submission = new GeneNameSubmission();
         submission.setHomologyInfoList(Arrays.asList(new HomologyInfo()));
         model.addAttribute("submission", submission);
@@ -97,12 +93,7 @@ public class NomenclatureSubmissionController {
     }
 
     @RequestMapping(value = "/line-name", method = RequestMethod.GET)
-    public String newLineNameForm(Model model, HttpServletRequest request) {
-        //TODO: This would read better if it was an annotation on the method (eg. `@RequiresCaptcha`)
-        Optional<String> captchaRedirectUrl = CaptchaService.getRedirectUrlIfNeeded(request);
-        if (captchaRedirectUrl.isPresent()) {
-            return "redirect:" + captchaRedirectUrl.get();
-        }
+    public String newLineNameForm(Model model) {
         LineNameSubmission submission = new LineNameSubmission();
         submission.setLineDetails(Arrays.asList(new LineInfo()));
         model.addAttribute("submission", submission);
