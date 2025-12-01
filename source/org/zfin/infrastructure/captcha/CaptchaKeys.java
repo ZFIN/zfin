21d2518adee966d1f8067172bf7924ae603d3145
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2025-12-01 12:43:10 -0800

    ZFIN-10023: Remove support for recaptcha and hcaptcha

--- source/org/zfin/infrastructure/captcha/CaptchaKeys.java
+++ source/org/zfin/infrastructure/captcha/CaptchaKeys.java
@@ -6,40 +6,30 @@ import org.zfin.infrastructure.TokenStorage;
 import java.io.IOException;
 import java.util.Optional;
 
-import static org.zfin.infrastructure.captcha.CaptchaService.getCurrentVersion;
+import static org.zfin.util.ZfinStringUtils.randomString;
 
-/**
- * Requires the existence of captchaSiteKey-V2.txt, captchaSiteKey-V3.txt, captchaSiteKey-hCaptcha.txt
- * to store keys for recaptcha v2, recaptcha v3, and hCaptcha respectively. Similar files for secret keys are needed too.
- */
 @Log4j2
 public class CaptchaKeys {
 
-    public enum Version {
-        V2,
-        V3,
-        hCaptcha,
-        altcha;
-    }
-
-    public static Optional<String> getSiteKey() {
-        TokenStorage.ServiceKey serviceKey = switch (getCurrentVersion()) {
-            case V2 -> TokenStorage.ServiceKey.RECAPTCHA_V2_SITE_KEY;
-            case V3 -> TokenStorage.ServiceKey.RECAPTCHA_V3_SITE_KEY;
-            case hCaptcha -> TokenStorage.ServiceKey.HCAPTCHA_SITE_KEY;
-            case altcha -> TokenStorage.ServiceKey.ALTCHA_SITE_KEY;
-        };
-        return (new TokenStorage()).getValue(serviceKey);
-    }
-
     public static Optional<String> getSecretKey() throws IOException {
-        TokenStorage.ServiceKey serviceKey = switch (getCurrentVersion()) {
-            case V2 -> TokenStorage.ServiceKey.RECAPTCHA_V2_SECRET_KEY;
-            case V3 -> TokenStorage.ServiceKey.RECAPTCHA_V3_SECRET_KEY;
-            case hCaptcha -> TokenStorage.ServiceKey.HCAPTCHA_SECRET_KEY;
-            case altcha -> TokenStorage.ServiceKey.ALTCHA_SECRET_KEY;
-        };
-        return (new TokenStorage()).getValue(serviceKey);
+        TokenStorage tokenStorage = new TokenStorage();
+        TokenStorage.ServiceKey serviceKey = TokenStorage.ServiceKey.ALTCHA_SECRET_KEY;
+        Optional<String> altchaKey = tokenStorage.getValue(serviceKey);
+        if (altchaKey.isPresent()) {
+            return altchaKey;
+        } else {
+            log.error("Altcha key is not set in token storage: " + serviceKey.getFilename());
+            //we can try setting it to a random value once:
+            String randomKey = randomString(50);
+            try {
+                tokenStorage.setValue(serviceKey, randomKey);
+                return Optional.of(randomKey);
+            } catch (IOException e) {
+                log.error("Failed to set random altcha site key in token storage.", e);
+            }
+        }
+
+        return Optional.empty();
     }
 
 }
