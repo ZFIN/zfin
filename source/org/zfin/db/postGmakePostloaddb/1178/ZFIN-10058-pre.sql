cc75893eb931a666decb7007334f640c961c179b
Christian Pich <cmpich@zfin.org>
2026-01-15 10:37:24 +0100

    ZFIN-10058 load mutatnt from zko

--- source/org/zfin/db/postGmakePostloaddb/1178/ZFIN-10058-pre.sql
+++ source/org/zfin/db/postGmakePostloaddb/1178/ZFIN-10058-pre.sql
@@ -2,7 +2,7 @@
 --changeset cmpich:ZFIN-10058-pre
 
 ALTER TABLE gene_allele_mutation_detail ADD COLUMN IF NOT EXISTS exons varchar(255);
-
+ALTER TABLE gene_allele_mutation_detail ADD COLUMN IF NOT EXISTS introns varchar(255);
 ALTER TABLE gene_allele_mutation_detail ADD COLUMN IF NOT EXISTS alias varchar(255);
 ALTER TABLE gene_allele_mutation_detail ADD COLUMN IF NOT EXISTS type varchar(255);
 ALTER TABLE crispr_detail ADD COLUMN IF NOT EXISTS type varchar(255);
