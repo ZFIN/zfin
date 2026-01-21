bfbcd19c5a3163aa43987b73e355b09435b0c8d4
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-01-21 13:45:35 -0800

    Improve Check-Marker-GO-Evidence-Obsolete-Secondary_m report
    - Add evidence code column
    - Add evidence is experimental column
    - Improve readability of true/false values

--- server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Obsolete-Secondary_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-Marker-GO-Evidence-Obsolete-Secondary_m.sql
@@ -3,12 +3,18 @@ SELECT mrkrgoev_zdb_id,
        term_name,
        mrkrgoev_source_zdb_id,
        mrkrgoev_annotation_organization_created_by,
-       term_is_obsolete,
-       term_is_secondary
+       mrkrgoev_evidence_code,
+       case term_is_obsolete when true then 'TRUE' else 'FALSE' end as term_is_obsolete,
+       case term_is_secondary when true then 'TRUE' else 'FALSE' end as term_is_secondary,
+       case
+          when mrkrgoev_evidence_code in ('IMP', 'IEP', 'IGI', 'IPI', 'IDA') then 'TRUE'
+          else 'FALSE'
+        end as evidence_is_experimental
 FROM marker_go_term_evidence,
      term,
      marker
 WHERE mrkrgoev_term_zdb_id = term_zdb_id
-  AND (term_is_obsolete = 't' or term_is_secondary = 't')
+  AND (term_is_obsolete = true or term_is_secondary = true)
   AND mrkrgoev_annotation_organization_created_by = 'ZFIN'
-  AND mrkrgoev_mrkr_zdb_id = mrkr_zdb_id;
+  AND mrkrgoev_mrkr_zdb_id = mrkr_zdb_id
+order by evidence_is_experimental desc, mrkr_abbrev, term_name, mrkrgoev_zdb_id;
