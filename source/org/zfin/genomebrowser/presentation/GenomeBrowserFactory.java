3182d7a0d039b0b7e23eab41e74423e52721cf97
Christian Pich <cmpich@zfin.org>
2026-01-23 16:12:13 +0100

    ZFIN-10088 fix genome browser logic (#1682)

--- source/org/zfin/genomebrowser/presentation/GenomeBrowserFactory.java
+++ source/org/zfin/genomebrowser/presentation/GenomeBrowserFactory.java
@@ -3,27 +3,20 @@ package org.zfin.genomebrowser.presentation;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
-import org.zfin.framework.featureflag.FeatureFlagEnum;
-import org.zfin.framework.featureflag.FeatureFlags;
 import org.zfin.jbrowse.presentation.JBrowse2ImageBuilder;
-import org.zfin.jbrowse.presentation.JBrowseImageBuilder;
 
 @Setter
 @Log4j2
 @Component("genomeBrowserFactory")
 public class GenomeBrowserFactory {
 
-    public static GenomeBrowserImageBuilder getStaticImageBuilder() {
-        GenomeBrowserFactory factory = new GenomeBrowserFactory();
-        return factory.getImageBuilder();
-    }
+	public static GenomeBrowserImageBuilder getStaticImageBuilder() {
+		GenomeBrowserFactory factory = new GenomeBrowserFactory();
+		return factory.getImageBuilder();
+	}
 
-    public GenomeBrowserImageBuilder getImageBuilder() {
-        if (FeatureFlags.isFlagEnabled(FeatureFlagEnum.JBROWSE2)) {
-            return new JBrowse2ImageBuilder();
-        } else {
-            return new JBrowseImageBuilder();
-        }
-    }
+	public GenomeBrowserImageBuilder getImageBuilder() {
+		return new JBrowse2ImageBuilder();
+	}
 
 }
