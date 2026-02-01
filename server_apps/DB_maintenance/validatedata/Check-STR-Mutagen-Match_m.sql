befcedb23b28093c1646ed1ad76a3f3f283d9171
Christian Pich <cmpich@zfin.org>
2026-02-01 14:05:56 +0100

    ZFIN-10036 suppress features that are only aattributed to one or more nomenclature publication

--- server_apps/DB_maintenance/validatedata/Check-STR-Mutagen-Match_m.sql
+++ server_apps/DB_maintenance/validatedata/Check-STR-Mutagen-Match_m.sql
@@ -13,3 +13,9 @@ WHERE  fmrel_ftr_zdb_id = feature.feature_zdb_id
        AND fa.featassay_mutagen not in ('DNA and CRISPR','DNA and TALEN')
        and fa.featassay_mutagen != str.mrkr_type
        AND feature.feature_zdb_id = fa.featassay_feature_zdb_id
+       -- Suppress features where the only distinct pub(s) are from this ZKO pub list
+       AND EXISTS (
+           SELECT 1 FROM record_attribution
+           WHERE recattrib_data_zdb_id = feature.feature_zdb_id
+           AND recattrib_source_zdb_id NOT IN ('ZDB-PUB-190102-5', 'ZDB-PUB-200102-5', 'ZDB-PUB-210105-1', 'ZDB-PUB-220103-2', 'ZDB-PUB-230103-8', 'ZDB-PUB-240103-28', 'ZDB-PUB-250106-1', 'ZDB-PUB-260105-13')
+       )
