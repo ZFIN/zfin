5a5b7a03e67c81e313907608d2495ca26fc6be5b
Christian Pich <cmpich@zfin.org>
2026-02-03 13:06:11 +0100

    ZFIN-9692 upgrade JVM to JDK 21

--- source/org/zfin/infrastructure/PatriciaTrieMultiMap.java
+++ source/org/zfin/infrastructure/PatriciaTrieMultiMap.java
@@ -1,49 +1,122 @@
 package org.zfin.infrastructure;
 
-import org.ardverk.collection.PatriciaTrie;
-import org.ardverk.collection.StringKeyAnalyzer;
-import org.zfin.gwt.root.dto.TermDTO;
+import org.apache.commons.collections4.trie.PatriciaTrie;
 
+import java.io.*;
 import java.util.*;
 
 /**
- * TODO: Replace org.ardverk.collection.PatriciaTrie with org.apache.commons.collections4.trie.PatriciaTrie
+ * PatriciaTrie-based multimap using Apache Commons Collections 4.
+ * Uses composition instead of inheritance to avoid JDK 21 SequencedMap compatibility issues.
  */
-public class PatriciaTrieMultiMap<V> extends PatriciaTrie<String, Set<V>> {
+public class PatriciaTrieMultiMap<V> implements Map<String, Set<V>>, Serializable {
 
-    private static final long serialVersionUID = 8774050446900697693L;
+    private static final long serialVersionUID = 8774050446900697694L;
+
+    private transient PatriciaTrie<Set<V>> trie;
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+        out.writeObject(new HashMap<>(trie));
+    }
+
+    @SuppressWarnings("unchecked")
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        Map<String, Set<V>> map = (Map<String, Set<V>>) in.readObject();
+        this.trie = new PatriciaTrie<>(map);
+    }
 
     public PatriciaTrieMultiMap() {
-        super(new StringKeyAnalyzer());
+        this.trie = new PatriciaTrie<>();
     }
 
     public PatriciaTrieMultiMap(Map<String, Set<V>> map) {
-        super(new StringKeyAnalyzer(), map);
+        this.trie = new PatriciaTrie<>(map);
     }
 
     /**
      * There is an issue reserializing, but rebuilding seems to work, so that is what we will do.
      */
     public void rebuild() {
-        Map<String, Set<V>> map = new HashMap<String, Set<V>>(size());
-        for (String key : keySet()) {
-            map.put(key, get(key));
+        Map<String, Set<V>> map = new HashMap<String, Set<V>>(trie.size());
+        for (String key : trie.keySet()) {
+            map.put(key, trie.get(key));
         }
-        clear();
-        putAll(map);
-        map = null;
+        trie.clear();
+        trie.putAll(map);
     }
 
     public V put(String s, V t) {
-        Set<V> values = get(s.toString());
+        Set<V> values = trie.get(s);
         if (values != null) {
             values.add(t);
         } else {
-            Set set = new HashSet();
+            Set<V> set = new HashSet<>();
             set.add(t);
-            put(s.toString(), set);
+            trie.put(s, set);
         }
         return t;
+    }
+
+    @Override
+    public Set<V> get(Object key) {
+        return trie.get(key);
+    }
+
+    @Override
+    public Set<V> put(String key, Set<V> value) {
+        return trie.put(key, value);
+    }
+
+    @Override
+    public Set<V> remove(Object key) {
+        return trie.remove(key);
+    }
+
+    @Override
+    public void putAll(Map<? extends String, ? extends Set<V>> map) {
+        trie.putAll(map);
+    }
+
+    @Override
+    public void clear() {
+        trie.clear();
+    }
+
+    @Override
+    public int size() {
+        return trie.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return trie.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return trie.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return trie.containsValue(value);
+    }
+
+    @Override
+    public Set<String> keySet() {
+        return trie.keySet();
+    }
+
+    @Override
+    public Collection<Set<V>> values() {
+        return trie.values();
+    }
+
+    @Override
+    public Set<Entry<String, Set<V>>> entrySet() {
+        return trie.entrySet();
     }
 
     /**
@@ -53,7 +126,7 @@ public class PatriciaTrieMultiMap<V> extends PatriciaTrie<String, Set<V>> {
     public Set<V> getSuggestedValues(String prefix) {
         Set<V> terms = new HashSet<V>();
 
-        for (Set<V> termSet : getPrefixedBy(prefix).values()) {
+        for (Set<V> termSet : trie.prefixMap(prefix).values()) {
             for (V term : termSet) {
                 terms.add(term);
             }
@@ -69,7 +142,7 @@ public class PatriciaTrieMultiMap<V> extends PatriciaTrie<String, Set<V>> {
      */
     public Set<V> getAllValues() {
         Set<V> valueSet = new HashSet<V>();
-        for (Set<V> values : values()) {
+        for (Set<V> values : trie.values()) {
             valueSet.addAll(values);
         }
         return valueSet;
@@ -90,7 +163,7 @@ public class PatriciaTrieMultiMap<V> extends PatriciaTrie<String, Set<V>> {
 
     public List<V> getListOfValues() {
         List<V> valueSet = new ArrayList<V>();
-        for (Set<V> values : values())
+        for (Set<V> values : trie.values())
             valueSet.addAll(values);
         return valueSet;
     }
