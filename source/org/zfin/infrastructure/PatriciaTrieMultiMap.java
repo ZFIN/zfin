14693cd57c5ef9cee243a3829b772c54f37bcf45
Christian Pich <cmpich@zfin.org>
2026-02-06 12:03:43 +0100

    Revert "ZFIN-9692 upgrade JVM to JDK 21 (#1699)"
    This reverts commit 294aa3b6b09aac7d252586fc7acaec20de7cf0a0.

--- source/org/zfin/infrastructure/PatriciaTrieMultiMap.java
+++ source/org/zfin/infrastructure/PatriciaTrieMultiMap.java
@@ -1,131 +1,49 @@
 package org.zfin.infrastructure;
 
-import org.apache.commons.collections4.trie.PatriciaTrie;
+import org.ardverk.collection.PatriciaTrie;
+import org.ardverk.collection.StringKeyAnalyzer;
+import org.zfin.gwt.root.dto.TermDTO;
 
-import java.io.*;
 import java.util.*;
 
 /**
- * PatriciaTrie-based multimap using Apache Commons Collections 4.
- * Uses composition instead of inheritance to avoid JDK 21 SequencedMap compatibility issues.
+ * TODO: Replace org.ardverk.collection.PatriciaTrie with org.apache.commons.collections4.trie.PatriciaTrie
  */
-public class PatriciaTrieMultiMap<V> implements Map<String, Set<V>>, Serializable {
+public class PatriciaTrieMultiMap<V> extends PatriciaTrie<String, Set<V>> {
 
-    private static final long serialVersionUID = 8774050446900697694L;
-
-    private transient PatriciaTrie<Set<V>> trie;
-
-    private void writeObject(ObjectOutputStream out) throws IOException {
-        out.defaultWriteObject();
-        out.writeObject(new HashMap<>(trie));
-    }
-
-    @SuppressWarnings("unchecked")
-    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-        in.defaultReadObject();
-        Map<String, Set<V>> map = (Map<String, Set<V>>) in.readObject();
-        this.trie = new PatriciaTrie<>(map);
-    }
+    private static final long serialVersionUID = 8774050446900697693L;
 
     public PatriciaTrieMultiMap() {
-        this.trie = new PatriciaTrie<>();
+        super(new StringKeyAnalyzer());
     }
 
     public PatriciaTrieMultiMap(Map<String, Set<V>> map) {
-        this.trie = new PatriciaTrie<>(map);
+        super(new StringKeyAnalyzer(), map);
     }
 
     /**
      * There is an issue reserializing, but rebuilding seems to work, so that is what we will do.
      */
     public void rebuild() {
-        Map<String, Set<V>> map = new HashMap<String, Set<V>>(trie.size());
-        for (String key : trie.keySet()) {
-            map.put(key, trie.get(key));
+        Map<String, Set<V>> map = new HashMap<String, Set<V>>(size());
+        for (String key : keySet()) {
+            map.put(key, get(key));
         }
-        trie.clear();
-        trie.putAll(map);
+        clear();
+        putAll(map);
+        map = null;
     }
 
     public V put(String s, V t) {
-        Set<V> values = trie.get(s);
+        Set<V> values = get(s.toString());
         if (values != null) {
             values.add(t);
         } else {
-            Set<V> set = new HashSet<>();
+            Set set = new HashSet();
             set.add(t);
-            trie.put(s, set);
+            put(s.toString(), set);
         }
         return t;
-    }
-
-    @Override
-    public Set<V> get(Object key) {
-        return trie.get(key);
-    }
-
-    @Override
-    public Set<V> put(String key, Set<V> value) {
-        return trie.put(key, value);
-    }
-
-    @Override
-    public Set<V> remove(Object key) {
-        return trie.remove(key);
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends Set<V>> map) {
-        trie.putAll(map);
-    }
-
-    @Override
-    public void clear() {
-        trie.clear();
-    }
-
-    @Override
-    public int size() {
-        return trie.size();
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return trie.isEmpty();
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        return trie.containsKey(key);
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        return trie.containsValue(value);
-    }
-
-    @Override
-    public Set<String> keySet() {
-        return trie.keySet();
-    }
-
-    @Override
-    public Collection<Set<V>> values() {
-        return trie.values();
-    }
-
-    @Override
-    public Set<Entry<String, Set<V>>> entrySet() {
-        return trie.entrySet();
-    }
-
-    /**
-     * Returns a view of this map of entries with keys starting with the given prefix.
-     * @param prefix The prefix to search for
-     * @return A SortedMap of entries with keys starting with the prefix
-     */
-    public SortedMap<String, Set<V>> getPrefixedBy(String prefix) {
-        return trie.prefixMap(prefix);
     }
 
     /**
@@ -135,7 +53,7 @@ public class PatriciaTrieMultiMap<V> implements Map<String, Set<V>>, Serializabl
     public Set<V> getSuggestedValues(String prefix) {
         Set<V> terms = new HashSet<V>();
 
-        for (Set<V> termSet : trie.prefixMap(prefix).values()) {
+        for (Set<V> termSet : getPrefixedBy(prefix).values()) {
             for (V term : termSet) {
                 terms.add(term);
             }
@@ -151,7 +69,7 @@ public class PatriciaTrieMultiMap<V> implements Map<String, Set<V>>, Serializabl
      */
     public Set<V> getAllValues() {
         Set<V> valueSet = new HashSet<V>();
-        for (Set<V> values : trie.values()) {
+        for (Set<V> values : values()) {
             valueSet.addAll(values);
         }
         return valueSet;
@@ -172,7 +90,7 @@ public class PatriciaTrieMultiMap<V> implements Map<String, Set<V>>, Serializabl
 
     public List<V> getListOfValues() {
         List<V> valueSet = new ArrayList<V>();
-        for (Set<V> values : trie.values())
+        for (Set<V> values : values())
             valueSet.addAll(values);
         return valueSet;
     }
