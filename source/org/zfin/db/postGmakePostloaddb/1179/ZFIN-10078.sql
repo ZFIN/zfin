1b4cb17270cc5872714da5280ecca57e9ac8a19f
Christian Pich <cmpich@zfin.org>
2026-03-03 17:10:30 +0100

    ZFIN-10078 fix the flankiing sequence  issues (#1747)

--- source/org/zfin/db/postGmakePostloaddb/1179/ZFIN-10078.sql
+++ source/org/zfin/db/postGmakePostloaddb/1179/ZFIN-10078.sql
@@ -4,7 +4,15 @@
 delete from tmp_flank_seq where difftype is null or difftype = '';
 
 update variant_flanking_sequence
-set vfseq_variation = tmp.calculated_sequence
+set vfseq_variation = reverse(translate(upper(left(vfseq_variation, -2)), 'ACGT', 'TGCA')) || '/-'
 from tmp_flank_seq tmp
-where vfseq_data_zdb_id = tmp.feature_id;
+where vfseq_data_zdb_id = tmp.feature_id
+  and tmp.difftype = 'reverse complement'
+  and tmp.feattype = 'DELETION';
+
+update variant_flanking_sequence
+set vfseq_variation = upper(vfseq_variation)
+from tmp_flank_seq tmp
+where vfseq_data_zdb_id = tmp.feature_id
+  and tmp.difftype = 'case-only mismatch';
 
