d02fa5db8d054b29841d3d8df9970d46750b9f9c
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-03-11 13:12:07 -0700

    ZFIN-10175: Replace publication.hbm.xml with JPA annotations
    Convert DOIAttempt, Journal, MeshTerm, MeshHeading, and
    MeshHeadingTerm from XML-based Hibernate mappings to JPA entity
    annotations. Add Lombok @Getter/@Setter to all except Journal
    (which has custom getter logic). Delete publication.hbm.xml.

--- source/org/zfin/publication/Journal.java
+++ source/org/zfin/publication/Journal.java
@@ -1,8 +1,11 @@
 package org.zfin.publication;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 import org.zfin.infrastructure.EntityZdbID;
 import org.zfin.infrastructure.SourceAlias;
 
+import jakarta.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 import java.util.SortedSet;
@@ -10,20 +13,54 @@ import java.util.SortedSet;
 /**
  * Journal domain object.
  */
+@Entity
+@Table(name = "journal")
 public class Journal implements Serializable, EntityZdbID {
 
+    @Id
+    @Column(name = "jrnl_zdb_id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "zdbIdGeneratorForJournal")
+    @GenericGenerator(name = "zdbIdGeneratorForJournal",
+            strategy = "org.zfin.database.ZdbIdGenerator",
+            parameters = {
+                    @Parameter(name = "type", value = "JRNL"),
+                    @Parameter(name = "insertActiveSource", value = "true")
+            })
     private String zdbID;
+
+    @Column(name = "jrnl_name", nullable = false)
     private String name;
+
+    @Column(name = "jrnl_abbrev", nullable = false)
     private String abbreviation;
+
+    @Column(name = "jrnl_medabbrev")
     private String medAbbrev;
+
+    @Column(name = "jrnl_isoabbrev")
     private String isoAbbrev;
+
+    @Column(name = "jrnl_publisher")
     private String publisher;
+
+    @Column(name = "jrnl_print_issn")
     private String printIssn;
+
+    @Column(name = "jrnl_online_issn")
     private String onlineIssn;
+
+    @Column(name = "jrnl_nlmid")
     private String nlmID;
+
+    @Transient
     private SortedSet<Publication> publications;
+
+    @OneToMany
+    @JoinColumn(name = "salias_source_zdb_id")
+    @OrderBy("salias_alias_lower DESC")
     private Set<SourceAlias> aliases;
 
+    @Column(name = "jrnl_is_nice")
     private boolean isNice;
 
     public String getZdbID() {
@@ -150,4 +187,3 @@ public class Journal implements Serializable, EntityZdbID {
         return false;
     }
 }
-
