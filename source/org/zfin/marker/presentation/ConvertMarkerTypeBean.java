9a2809c5313a356ec28fd1dcb20f736717e8579c
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-04-02 17:13:34 -0700

    ZFIN-9689: Use Lombok @Getter/@Setter on ConvertMarkerTypeBean

--- source/org/zfin/marker/presentation/ConvertMarkerTypeBean.java
+++ source/org/zfin/marker/presentation/ConvertMarkerTypeBean.java
@@ -1,10 +1,14 @@
 package org.zfin.marker.presentation;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.zfin.marker.Marker;
 import org.zfin.marker.MarkerType;
 
 import java.util.List;
 
+@Setter
+@Getter
 public class ConvertMarkerTypeBean {
 
     private String zdbIDToConvert;
@@ -12,44 +16,4 @@ public class ConvertMarkerTypeBean {
     private String newMarkerTypeName;
     private String newZdbId;
     private List<MarkerType> availableTypes;
-
-    public String getZdbIDToConvert() {
-        return zdbIDToConvert;
-    }
-
-    public void setZdbIDToConvert(String zdbIDToConvert) {
-        this.zdbIDToConvert = zdbIDToConvert;
-    }
-
-    public Marker getMarker() {
-        return marker;
-    }
-
-    public void setMarker(Marker marker) {
-        this.marker = marker;
-    }
-
-    public String getNewMarkerTypeName() {
-        return newMarkerTypeName;
-    }
-
-    public void setNewMarkerTypeName(String newMarkerTypeName) {
-        this.newMarkerTypeName = newMarkerTypeName;
-    }
-
-    public String getNewZdbId() {
-        return newZdbId;
-    }
-
-    public void setNewZdbId(String newZdbId) {
-        this.newZdbId = newZdbId;
-    }
-
-    public List<MarkerType> getAvailableTypes() {
-        return availableTypes;
-    }
-
-    public void setAvailableTypes(List<MarkerType> availableTypes) {
-        this.availableTypes = availableTypes;
-    }
 }
