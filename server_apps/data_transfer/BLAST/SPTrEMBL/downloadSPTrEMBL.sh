40eb5834543fb4fd74532636b29a652d63c4ae17
Ryan Taylor <90803397+rtaylorzfin@users.noreply.github.com>
2026-04-07 15:09:36 -0700

    ZFIN-10074: sptrembl blast (#1800)
    * Fix SPTrEMBL download by updating to UniProt REST API
    
    The old www.uniprot.org/uniprot/?query= endpoint no longer works.
    UniProt migrated to rest.uniprot.org in 2022. Update to use the
    streaming endpoint which writes FASTA directly to the target file
    instead of saving to oddly-named index.html query string files.
    
    * Use compressed downloads for SPTrEMBL FASTA files
    
    Add &compressed=true to UniProt API requests to download gzip files
    instead of raw FASTA. Significantly faster for large proteomes (human).

--- server_apps/data_transfer/BLAST/SPTrEMBL/downloadSPTrEMBL.sh
+++ server_apps/data_transfer/BLAST/SPTrEMBL/downloadSPTrEMBL.sh
@@ -1,24 +1,20 @@
 #!/bin/bash -e
 #
-# The script download SwissProt and TrEMBL zebrafish file, 
-
+# Download SwissProt and TrEMBL zebrafish, mouse, and human FASTA files
+# from UniProt REST API.
+#
+# UniProt migrated from www.uniprot.org/uniprot/ to rest.uniprot.org in 2022.
+# See: https://www.uniprot.org/help/api
 
 source "../config.sh"
 
-echo "== Downloading the sprot.dat.gz =="
+BASE_URL="https://rest.uniprot.org/uniprotkb/stream"
 
-wget -qN "www.uniprot.org/uniprot/?query=organism%3azebrafish&force=yes&format=fasta"
+for organism in zebrafish mouse human; do
+    echo "== Downloading ${organism} FASTA (compressed) =="
+    wget -q -O ${organism}.fasta.gz "${BASE_URL}?query=organism_name:${organism}&format=fasta&compressed=true"
+    gunzip -f ${organism}.fasta.gz
+    sed -i 's/>tr/>sp/g' ${organism}.fasta
+done
 
-sed 's/>tr/>sp/g' index.html\?query=organism:zebrafish\&force=yes\&format=fasta > zebrafish.fasta
-
-wget -qN "www.uniprot.org/uniprot/?query=organism%3amouse&force=yes&format=fasta"
-
-sed 's/>tr/>sp/g' index.html\?query=organism:mouse\&force=yes\&format=fasta > mouse.fasta
-
-
-wget -qN "www.uniprot.org/uniprot/?query=organism%3ahuman&force=yes&format=fasta"
-
-sed 's/>tr/>sp/g' index.html\?query=organism:human\&force=yes\&format=fasta > human.fasta
-
-echo "==| done downloading SPTrEMBL | =="
-exit
+echo "==| done downloading SPTrEMBL |=="
